eb7076fce6e6c36a22dfd9f34d20aa1547a86dd9
rock <zloirock@zloirock.ru>
2014-12-31 23:42:18 +0600

    add experimental `es6_reflect` module

---
+++ src/es6_reflect.js
@@ -0,0 +1,91 @@
+!function(){
+  function Enumerate(it){
+    var O    = it
+      , keys = new Set;
+    do {
+      forEach.call(getKeys(O), keys.add, keys);
+    } while(O = getPrototypeOf(O));
+    set(this, ITER, {o: it, a: Array.from(keys), i: 0});
+  }
+  createIterator(Enumerate, OBJECT, function(){
+    var iter = this[ITER]
+      , O    = iter.o
+      , keys = iter.a
+      , key;
+    do {
+      if(iter.i >= keys.length)return iterResult(1);
+    } while(!((key = keys[iter.i++]) in O));
+    return iterResult(0, key);
+  });
+  
+  function wrap(fn){
+    return function(it){
+      assertObject(it);
+      try {
+        return fn.apply(this, arguments), true;
+      } catch(e){
+        return false;
+      }
+    }
+  }
+  
+  function reflectGet(target, propertyKey, receiver){
+    var desc = getOwnDescriptor(target, propertyKey), proto;
+    if(desc)return desc.get ? desc.get.call(receiver === undefined ? target : receiver) : desc.value;
+    return isObject(proto = getPrototypeOf(target)) ? reflectGet(proto, propertyKey, receiver) : undefined;
+  }
+  function reflectSet(target, propertyKey, V, receiver){
+    if(receiver === undefined)receiver = target;
+    var desc = getOwnDescriptor(assertObject(target), propertyKey)
+      , proto;
+    if(desc){
+      if(desc.writable === false)return false;
+      if(desc.set)return desc.set.call(receiver, V), true;
+    }
+    if(isObject(proto = getPrototypeOf(target)))return reflectSet(proto, propertyKey, V, receiver);
+    desc = getOwnDescriptor(receiver, propertyKey) || descriptor(0);
+    desc.value = V;
+    return defineProperty(receiver, propertyKey, desc), true;
+  }
+  
+  $define(STATIC, 'Reflect', {
+    // 26.1.1 Reflect.apply(target, thisArgument, argumentsList)
+    apply: ctx(call, FunctionProto.apply, 3),
+    // 26.1.2 Reflect.construct(target, argumentsList)
+    construct: function(target, argumentsList){
+      var instance = create(target[PROTOTYPE])
+        , result   = invoke(target, argumentsList, instance);
+      return isObject(result) ? result : instance;
+    },
+    // 26.1.3 Reflect.defineProperty(target, propertyKey, attributes)
+    defineProperty: wrap(defineProperty),
+    // 26.1.4 Reflect.deleteProperty(target, propertyKey)
+    'delete': function(){
+      return delete target[propertyKey];
+    },
+    // 26.1.5 Reflect.enumerate(target)
+    enumerate: function(target){
+      return new Enumerate(assertObject(target));
+    },
+    // 26.1.6 Reflect.get(target, propertyKey [, receiver])
+    get: reflectGet,
+    // 26.1.7 Reflect.getOwnPropertyDescriptor(target, propertyKey)
+    getOwnPropertyDescriptor: getOwnDescriptor,
+    // 26.1.8 Reflect.getPrototypeOf(target)
+    getPrototypeOf: getPrototypeOf,
+    // 26.1.9 Reflect.has(target, propertyKey)
+    has: function(target, propertyKey){
+      return propertyKey in target;
+    },
+    // 26.1.10 Reflect.isExtensible(target)
+    isExtensible: Object.isExtensible,
+    // 26.1.11 Reflect.ownKeys(target)
+    ownKeys: ownKeys,
+    // 26.1.12 Reflect.preventExtensions(target)
+    preventExtensions: wrap(Object.preventExtensions),
+    // 26.1.13 Reflect.set(target, propertyKey, V [, receiver])
+    set: reflectSet,
+    // 26.1.14 Reflect.setPrototypeOf(target, proto)
+    setPrototypeOf: wrap(Object.setPrototypeOf)
+  });
+}();
