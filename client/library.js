8f79fb437a0006d2f59e39422456444b7bba9fe2
rock <zloirock@zloirock.ru>
2015-01-16 00:35:38 +0600

    fix some math

--- client/library.js
+++ client/library.js
@@ -840,8 +840,8 @@ $define(GLOBAL + FORCED, {global: global});
         return !isObject(it) && isFinite(it) && floor(it) === it;
       }
       // 20.2.2.28 Math.sign(x)
-    , sign = Math.sign || function sign(it){
-        return (it = +it) == 0 || it != it ? it : it < 0 ? -1 : 1;
+    , sign = Math.sign || function sign(x){
+        return (x = +x) == 0 || x != x ? x : x < 0 ? -1 : 1;
       }
     , pow  = Math.pow
     , abs  = Math.abs
@@ -909,17 +909,17 @@ $define(GLOBAL + FORCED, {global: global});
   $define(STATIC, MATH, {
     // 20.2.2.3 Math.acosh(x)
     acosh: function(x){
-      return x < 1 ? NaN : log(x + sqrt(x * x - 1));
+      return (x = +x) < 1 ? NaN : log(x + sqrt(x * x - 1));
     },
     // 20.2.2.5 Math.asinh(x)
     asinh: asinh,
     // 20.2.2.7 Math.atanh(x)
     atanh: function(x){
-      return x == 0 ? +x : log((1 + +x) / (1 - x)) / 2;
+      return (x = +x) == 0 ? x : log((1 + x) / (1 - x)) / 2;
     },
     // 20.2.2.9 Math.cbrt(x)
     cbrt: function(x){
-      return sign(x) * pow(abs(x), 1 / 3);
+      return sign(x = +x) * pow(abs(x), 1 / 3);
     },
     // 20.2.2.11 Math.clz32(x)
     clz32: function(x){
@@ -927,11 +927,11 @@ $define(GLOBAL + FORCED, {global: global});
     },
     // 20.2.2.12 Math.cosh(x)
     cosh: function(x){
-      return (exp(x) + exp(-x)) / 2;
+      return (exp(x = +x) + exp(-x)) / 2;
     },
     // 20.2.2.14 Math.expm1(x)
     expm1: function(x){
-      return x == 0 ? +x : x > -1e-6 && x < 1e-6 ? +x + x * x / 2 : exp(x) - 1;
+      return (x = +x) == 0 ? x : x > -1e-6 && x < 1e-6 ? x + x * x / 2 : exp(x) - 1;
     },
     // 20.2.2.16 Math.fround(x)
     // TODO: fallback for IE9-
@@ -939,17 +939,21 @@ $define(GLOBAL + FORCED, {global: global});
       return new Float32Array([x])[0];
     },
     // 20.2.2.17 Math.hypot([value1[, value2[, … ]]])
-    // TODO: work with very large & small numbers
     hypot: function(value1, value2){
-      var sum    = 0
-        , length = arguments.length
-        , value;
-      while(length--){
-        value = +arguments[length];
-        if(value == Infinity || value == -Infinity)return Infinity;
-        sum += value * value;
+      var sum  = 0
+        , len1 = arguments.length
+        , len2 = len1
+        , args = Array(len1)
+        , larg = -Infinity
+        , arg;
+      while(len1--){
+        arg = args[len1] = +arguments[len1];
+        if(arg == Infinity || arg == -Infinity)return Infinity;
+        if(arg > larg)larg = arg;
       }
-      return sqrt(sum);
+      larg = arg || 1;
+      while(len2--)sum += pow(args[len2] / larg, 2);
+      return larg * sqrt(sum);
     },
     // 20.2.2.18 Math.imul(x, y)
     imul: function(x, y){
@@ -962,7 +966,7 @@ $define(GLOBAL + FORCED, {global: global});
     },
     // 20.2.2.20 Math.log1p(x)
     log1p: function(x){
-      return x > -1e-8 && x < 1e-8 ? x - x * x / 2 : log(1 + +x);
+      return (x = +x) > -1e-8 && x < 1e-8 ? x - x * x / 2 : log(1 + x);
     },
     // 20.2.2.21 Math.log10(x)
     log10: function(x){
@@ -976,11 +980,11 @@ $define(GLOBAL + FORCED, {global: global});
     sign: sign,
     // 20.2.2.30 Math.sinh(x)
     sinh: function(x){
-      return x == 0 ? +x : (exp(x) - exp(-x)) / 2;
+      return (x = +x) == 0 ? x : (exp(x) - exp(-x)) / 2;
     },
     // 20.2.2.33 Math.tanh(x)
     tanh: function(x){
-      return isFinite(x) ? x == 0 ? +x : (exp(x) - exp(-x)) / (exp(x) + exp(-x)) : sign(x);
+      return isFinite(x = +x) ? x == 0 ? x : (exp(x) - exp(-x)) / (exp(x) + exp(-x)) : sign(x);
     },
     // 20.2.2.34 Math.trunc(x)
     trunc: trunc
