4540e6460d1ca2d3f305e2602a79dbf7b1ffc3bb
rock <zloirock@zloirock.ru>
2015-03-07 04:36:26 +0600

    ref

--- src/core.delay.js
+++ src/core.delay.js
@@ -1,11 +1,11 @@
 require('./es6.promise');
-var $ = require('./$');
-var $def = require('./$.def');
-var partial = require('./$.partial');
+var Promise = require('./$').core.Promise
+  , $def    = require('./$.def')
+  , partial = require('./$.partial');
 // https://esdiscuss.org/topic/promise-returning-delay-function
 $def($def.G + $def.F, {
   delay: function(time){
-    return new $.core.Promise(function(resolve){
+    return new Promise(function(resolve){
       setTimeout(partial.call(resolve, true), time);
     });
   }
--- src/core.log.js
+++ src/core.log.js
@@ -1,23 +1,23 @@
 var $    = require('./$')
-  , $def = require('./$.def');
-!function(log, enabled){
-  // Methods from https://github.com/DeveloperToolsWG/console-object/blob/master/api.md
-  $.each.call($.a('assert,clear,count,debug,dir,dirxml,error,exception,' +
-      'group,groupCollapsed,groupEnd,info,isIndependentlyComposed,log,' +
-      'markTimeline,profile,profileEnd,table,time,timeEnd,timeline,' +
-      'timelineEnd,timeStamp,trace,warn'), function(key){
-    log[key] = function(){
-      if(enabled && $.g.console && $.isFunction(console[key])){
-        return Function.apply.call(console[key], console, arguments);
-      }
-    };
-  });
-  $def($def.G + $def.F, {log: require('./$.assign')(log.log, log, {
-    enable: function(){
-      enabled = true;
-    },
-    disable: function(){
-      enabled = false;
+  , $def = require('./$.def')
+  , log  = {}
+  , enabled = true;
+// Methods from https://github.com/DeveloperToolsWG/console-object/blob/master/api.md
+$.each.call($.a('assert,clear,count,debug,dir,dirxml,error,exception,' +
+    'group,groupCollapsed,groupEnd,info,isIndependentlyComposed,log,' +
+    'markTimeline,profile,profileEnd,table,time,timeEnd,timeline,' +
+    'timelineEnd,timeStamp,trace,warn'), function(key){
+  log[key] = function(){
+    if(enabled && $.g.console && $.isFunction(console[key])){
+      return Function.apply.call(console[key], console, arguments);
     }
-  })});
-}({}, true);
+  };
+});
+$def($def.G + $def.F, {log: require('./$.assign')(log.log, log, {
+  enable: function(){
+     enabled = true;
+  },
+  disable: function(){
+    enabled = false;
+  }
+})});
