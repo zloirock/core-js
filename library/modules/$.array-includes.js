1bc06381ace499380f49b99c6168b3717f5d3cf0
rock <zloirock@zloirock.ru>
2015-03-24 18:57:17 +0600

    IS_CONTAINS -> IS_INCLUDES

--- library/modules/$.array-includes.js
+++ library/modules/$.array-includes.js
@@ -2,17 +2,17 @@
 // false -> Array#indexOf
 // true  -> Array#includes
 var $ = require('./$');
-module.exports = function(IS_CONTAINS){
+module.exports = function(IS_INCLUDES){
   return function(el /*, fromIndex = 0 */){
     var O      = $.toObject(this)
       , length = $.toLength(O.length)
       , index  = $.toIndex(arguments[1], length)
       , value;
-    if(IS_CONTAINS && el != el)while(length > index){
+    if(IS_INCLUDES && el != el)while(length > index){
       value = O[index++];
       if(value != value)return true;
-    } else for(;length > index; index++)if(IS_CONTAINS || index in O){
-      if(O[index] === el)return IS_CONTAINS || index;
-    } return !IS_CONTAINS && -1;
+    } else for(;length > index; index++)if(IS_INCLUDES || index in O){
+      if(O[index] === el)return IS_INCLUDES || index;
+    } return !IS_INCLUDES && -1;
   }
 }
