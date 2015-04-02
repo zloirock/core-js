bd8d26679b929a6735a70239c4afe4f744d894e2
rock <zloirock@zloirock.ru>
2015-04-02 17:17:17 +0600

    fix static join & slice with strings in ES3 environment and `library` version

--- library/modules/js.array.statics.js
+++ library/modules/js.array.statics.js
@@ -1,10 +1,12 @@
 // JavaScript 1.6 / Strawman array statics shim
 var $       = require('./$')
   , $def    = require('./$.def')
+  , core    = $.core
   , statics = {};
 function setStatics(keys, length){
   $.each.call(keys.split(','), function(key){
-    if(key in [])statics[key] = require('./$.ctx')(Function.call, [][key], length);
+    if(length == undefined && key in core.Array)statics[key] = core.Array[key];
+    else if(key in [])statics[key] = require('./$.ctx')(Function.call, [][key], length);
   });
 }
 setStatics('pop,reverse,shift,keys,values,entries', 1);
