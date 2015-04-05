5a15707ebe2805fa68479a171222bf3d0007247d
rock <zloirock@zloirock.ru>
2015-04-06 00:04:52 +0600

    ref Promise

--- modules/es6.promise.js
+++ modules/es6.promise.js
@@ -19,94 +19,96 @@ var $       = require('./$')
   , assertFunction = assert.fn
   , assertObject   = assert.obj
   , test;
+
+// helpers
 function getConstructor(C){
   var S = assertObject(C)[SPECIES];
   return S != undefined ? S : C;
 }
-isFunction(P) && isFunction(P.resolve)
-&& P.resolve(test = new P(function(){})) == test
-|| function(){
-  function isThenable(it){
-    var then;
-    if(isObject(it))then = it.then;
-    return isFunction(then) ? then : false;
-  }
-  function handledRejectionOrHasOnRejected(promise){
-    var record = promise[RECORD]
-      , chain  = record.c
-      , i      = 0
-      , react;
-    if(record.h)return true;
-    while(chain.length > i){
-      react = chain[i++];
-      if(react.fail || handledRejectionOrHasOnRejected(react.P))return true;
-    }
-  }
-  function notify(record, isReject){
-    var chain = record.c;
-    if(isReject || chain.length)asap(function(){
-      var promise = record.p
-        , value   = record.v
-        , ok      = record.s == 1
-        , i       = 0;
-      if(isReject && !handledRejectionOrHasOnRejected(promise)){
-        setTimeout(function(){
-          if(!handledRejectionOrHasOnRejected(promise)){
-            if(cof(process) == 'process'){
-              process.emit('unhandledRejection', value, promise);
-            } else if(global.console && isFunction(console.error)){
-              console.error('Unhandled promise rejection', value);
-            }
+function isThenable(it){
+  var then;
+  if(isObject(it))then = it.then;
+  return isFunction(then) ? then : false;
+}
+function isUnhandled(promise){
+  var record = promise[RECORD]
+    , chain  = record.c
+    , i      = 0
+    , react;
+  if(record.h)return false;
+  while(chain.length > i){
+    react = chain[i++];
+    if(react.fail || !isUnhandled(react.P))return false;
+  } return true;
+}
+function notify(record, isReject){
+  var chain = record.c;
+  if(isReject || chain.length)asap(function(){
+    var promise = record.p
+      , value   = record.v
+      , ok      = record.s == 1
+      , i       = 0;
+    if(isReject && isUnhandled(promise)){
+      setTimeout(function(){
+        if(isUnhandled(promise)){
+          if(cof(process) == 'process'){
+            process.emit('unhandledRejection', value, promise);
+          } else if(global.console && isFunction(console.error)){
+            console.error('Unhandled promise rejection', value);
           }
-        }, 1e3);
-      } else while(chain.length > i)!function(react){
-        var cb = ok ? react.ok : react.fail
-          , ret, then;
-        try {
-          if(cb){
-            if(!ok)record.h = true;
-            ret = cb === true ? value : cb(value);
-            if(ret === react.P){
-              react.rej(TypeError(PROMISE + '-chain cycle'));
-            } else if(then = isThenable(ret)){
-              then.call(ret, react.res, react.rej);
-            } else react.res(ret);
-          } else react.rej(value);
-        } catch(err){
-          react.rej(err);
         }
-      }(chain[i++]);
-      chain.length = 0;
-    });
-  }
-  function reject(value){
-    var record = this;
-    if(record.d)return;
-    record.d = true;
-    record = record.r || record; // unwrap
-    record.v = value;
-    record.s = 2;
-    notify(record, true);
-  }
-  function resolve(value){
-    var record = this
-      , then, wrapper;
-    if(record.d)return;
-    record.d = true;
-    record = record.r || record; // unwrap
-    try {
-      if(then = isThenable(value)){
-        wrapper = {r: record, d: false}; // wrap
-        then.call(value, ctx(resolve, wrapper, 1), ctx(reject, wrapper, 1));
-      } else {
-        record.v = value;
-        record.s = 1;
-        notify(record);
+      }, 1e3);
+    } else while(chain.length > i)!function(react){
+      var cb = ok ? react.ok : react.fail
+        , ret, then;
+      try {
+        if(cb){
+          if(!ok)record.h = true;
+          ret = cb === true ? value : cb(value);
+          if(ret === react.P){
+            react.rej(TypeError(PROMISE + '-chain cycle'));
+          } else if(then = isThenable(ret)){
+            then.call(ret, react.res, react.rej);
+          } else react.res(ret);
+        } else react.rej(value);
+      } catch(err){
+        react.rej(err);
       }
-    } catch(err){
-      reject.call(wrapper || {r: record, d: false}, err); // wrap
+    }(chain[i++]);
+    chain.length = 0;
+  });
+}
+function $reject(value){
+  var record = this;
+  if(record.d)return;
+  record.d = true;
+  record = record.r || record; // unwrap
+  record.v = value;
+  record.s = 2;
+  notify(record, true);
+}
+function $resolve(value){
+  var record = this
+    , then, wrapper;
+  if(record.d)return;
+  record.d = true;
+  record = record.r || record; // unwrap
+  try {
+    if(then = isThenable(value)){
+      wrapper = {r: record, d: false}; // wrap
+      then.call(value, ctx($resolve, wrapper, 1), ctx($reject, wrapper, 1));
+    } else {
+      record.v = value;
+      record.s = 1;
+      notify(record);
     }
+  } catch(err){
+    $reject.call(wrapper || {r: record, d: false}, err); // wrap
   }
+}
+
+// constructor polyfill
+if(!(isFunction(P) && isFunction(P.resolve) && P.resolve(test = new P(function(){})) == test)){
   // 25.4.3.1 Promise(executor)
   P = function Promise(executor){
     assertFunction(executor);
@@ -120,9 +122,9 @@ isFunction(P) && isFunction(P.resolve)
     };
     $.hide(this, RECORD, record);
     try {
-      executor(ctx(resolve, record, 1), ctx(reject, record, 1));
+      executor(ctx($resolve, record, 1), ctx($reject, record, 1));
     } catch(err){
-      reject.call(record, err);
+      $reject.call(record, err);
     }
   };
   $.mix(P.prototype, {
@@ -147,8 +149,14 @@ isFunction(P) && isFunction(P.resolve)
       return this.then(undefined, onRejected);
     }
   });
-}();
+}
+
+// export
 $def($def.G + $def.W + $def.F * (P != Base), {Promise: P});
+cof.set(P, PROMISE);
+require('./$.species')(P);
+
+// statics
 $def($def.S, PROMISE, {
   // 25.4.4.5 Promise.reject(r)
   reject: function reject(r){
@@ -171,28 +179,26 @@ $def($def.S + $def.F * ($iter.fail(function(iter){
   all: function all(iterable){
     var C      = getConstructor(this)
       , values = [];
-    return new C(function(resolve, reject){
+    return new C(function(res, rej){
       forOf(iterable, false, values.push, values);
       var remaining = values.length
         , results   = Array(remaining);
       if(remaining)$.each.call(values, function(promise, index){
         C.resolve(promise).then(function(value){
           results[index] = value;
-          --remaining || resolve(results);
-        }, reject);
+          --remaining || res(results);
+        }, rej);
       });
-      else resolve(results);
+      else res(results);
     });
   },
   // 25.4.4.4 Promise.race(iterable)
   race: function race(iterable){
     var C = getConstructor(this);
-    return new C(function(resolve, reject){
+    return new C(function(res, rej){
       forOf(iterable, false, function(promise){
-        C.resolve(promise).then(resolve, reject);
+        C.resolve(promise).then(res, rej);
       });
     });
   }
 });
-cof.set(P, PROMISE);
-require('./$.species')(P);
