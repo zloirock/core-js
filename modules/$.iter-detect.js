9ad93e490950cd5460df1c0cacedb2884e200326
rock <zloirock@zloirock.ru>
2015-04-08 05:35:14 +0600

    simplify $.iter-detect

--- modules/$.iter-detect.js
+++ modules/$.iter-detect.js
@@ -1,17 +1,15 @@
 var SYMBOL_ITERATOR = require('./$.wks')('iterator')
   , SAFE_CLOSING    = false;
-!function(){
-  try {
-    var iter = [1][SYMBOL_ITERATOR]();
-    iter['return'] = function(){ SAFE_CLOSING = true; };
-    Array.from(iter, function(){ throw 2; });
-  } catch(e){ /* empty */ }
-}();
+try {
+  var riter = [7][SYMBOL_ITERATOR]();
+  riter['return'] = function(){ SAFE_CLOSING = true; };
+  Array.from(riter, function(){ throw 2; });
+} catch(e){ /* empty */ }
 module.exports = function(exec){
   if(!SAFE_CLOSING)return false;
   var safe = false;
   try {
-    var arr  = [[{}, 1]]
+    var arr  = [7]
       , iter = arr[SYMBOL_ITERATOR]();
     iter.next = function(){ safe = true; };
     arr[SYMBOL_ITERATOR] = function(){ return iter; };
