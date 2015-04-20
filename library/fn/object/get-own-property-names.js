0cada150a0cf28c44d0e95d766798869d8424f35
rock <zloirock@zloirock.ru>
2015-04-21 05:30:42 +0600

    simplify entry points

--- library/fn/object/get-own-property-names.js
+++ library/fn/object/get-own-property-names.js
@@ -1,3 +1,2 @@
-require('../../modules/es6.object.statics-accept-primitives');
 require('../../modules/es6.symbol');
 module.exports = require('../../modules/$').core.Object.getOwnPropertyNames;
--- library/fn/object/index.js
+++ library/fn/object/index.js
@@ -1,9 +1,9 @@
+require('../../modules/es6.symbol');
 require('../../modules/es6.object.assign');
 require('../../modules/es6.object.is');
 require('../../modules/es6.object.set-prototype-of');
 require('../../modules/es6.object.to-string');
 require('../../modules/es6.object.statics-accept-primitives');
-require('../../modules/es6.symbol');
 require('../../modules/es7.object.get-own-property-descriptors');
 require('../../modules/es7.object.to-array');
 require('../../modules/core.object');
