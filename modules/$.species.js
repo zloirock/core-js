bba6d8d3c499a3eecaaa82e8a2483151f832eed6
rock <zloirock@zloirock.ru>
2015-04-25 02:25:56 +0600

    set @@species in `library` version

--- modules/$.species.js
+++ modules/$.species.js
@@ -1,6 +1,7 @@
-var $ = require('./$');
+var $       = require('./$')
+  , SPECIES = require('./$.wks')('species');
 module.exports = function(C){
-  if($.DESC && $.FW)$.setDesc(C, require('./$.wks')('species'), {
+  if($.DESC && !(SPECIES in C))$.setDesc(C, SPECIES, {
     configurable: true,
     get: $.that
   });
