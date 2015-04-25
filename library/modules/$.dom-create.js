0102d4c66cf300bc1c2fee82ff867cbcfaddb025
rock <zloirock@zloirock.ru>
2015-04-26 04:55:25 +0600

    fix $.dom-create for old IE - wrong typeof

--- library/modules/$.dom-create.js
+++ library/modules/$.dom-create.js
@@ -1,6 +1,8 @@
 var $        = require('./$')
   , document = $.g.document
-  , is = $.isObject(document) && $.isFunction(document.createElement);
+  , isObject = $.isObject
+  // in old IE typeof document.createElement is 'object'
+  , is = isObject(document) && isObject(document.createElement);
 module.exports = function(it){
   return is ? document.createElement(it) : {};
 };
