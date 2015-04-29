cf254d6484e9837fb5d1bd23e8e06be2ec248de7
rock <zloirock@zloirock.ru>
2015-04-30 02:47:45 +0600

    set Function#@@hasInstance as writable: false, configurable: false

--- library/modules/es6.function.has-instance.js
+++ library/modules/es6.function.has-instance.js
@@ -2,10 +2,10 @@ var $             = require('./$')
   , HAS_INSTANCE  = require('./$.wks')('hasInstance')
   , FunctionProto = Function.prototype;
 // 19.2.3.6 Function.prototype[@@hasInstance](V)
-if(!(HAS_INSTANCE in FunctionProto))$.hide(FunctionProto, HAS_INSTANCE, function(O){
+if(!(HAS_INSTANCE in FunctionProto))$.setDesc(FunctionProto, HAS_INSTANCE, {value: function(O){
   if(!$.isFunction(this) || !$.isObject(O))return false;
   if(!$.isObject(this.prototype))return O instanceof this;
   // for environment w/o native `@@hasInstance` logic enough `instanceof`, but add this:
   while(O = $.getProto(O))if(this.prototype === O)return true;
   return false;
-});
+}});
