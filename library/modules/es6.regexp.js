f8eceae93cd9e47af0cacf69de9a0488fc411c4d
rock <zloirock@zloirock.ru>
2015-04-30 02:28:05 +0600

    fix for IE8- buggy named functions

--- library/modules/es6.regexp.js
+++ library/modules/es6.regexp.js
@@ -1,38 +1,38 @@
-var $      = require('./$')
-  , cof    = require('./$.cof')
-  , RegExp = $.g.RegExp
-  , Base   = RegExp
-  , proto  = RegExp.prototype
-  , re     = /a/g
+var $       = require('./$')
+  , cof     = require('./$.cof')
+  , $RegExp = $.g.RegExp
+  , Base    = $RegExp
+  , proto   = $RegExp.prototype
+  , re      = /a/g
   // "new" creates a new object
-  , CORRECT_NEW = new RegExp(re) !== re
+  , CORRECT_NEW = new $RegExp(re) !== re
   // RegExp allows a regex with flags as the pattern
   , ALLOWS_RE_WITH_FLAGS = function(){
     try {
-      return RegExp(re, 'i') == '/a/i';
+      return $RegExp(re, 'i') == '/a/i';
     } catch(e){ /* empty */ }
   }();
 if($.FW && $.DESC){
   if(!CORRECT_NEW || !ALLOWS_RE_WITH_FLAGS){
-    RegExp = function RegExp(pattern, flags){
+    $RegExp = function RegExp(pattern, flags){
       var patternIsRegExp  = cof(pattern) == 'RegExp'
         , flagsIsUndefined = flags === undefined;
-      if(!(this instanceof RegExp) && patternIsRegExp && flagsIsUndefined)return pattern;
+      if(!(this instanceof $RegExp) && patternIsRegExp && flagsIsUndefined)return pattern;
       return CORRECT_NEW
         ? new Base(patternIsRegExp && !flagsIsUndefined ? pattern.source : pattern, flags)
         : new Base(patternIsRegExp ? pattern.source : pattern
           , patternIsRegExp && flagsIsUndefined ? pattern.flags : flags);
     };
     $.each.call($.getNames(Base), function(key){
-      key in RegExp || $.setDesc(RegExp, key, {
+      key in $RegExp || $.setDesc($RegExp, key, {
         configurable: true,
         get: function(){ return Base[key]; },
         set: function(it){ Base[key] = it; }
       });
     });
-    proto.constructor = RegExp;
-    RegExp.prototype = proto;
-    $.hide($.g, 'RegExp', RegExp);
+    proto.constructor = $RegExp;
+    $RegExp.prototype = proto;
+    $.hide($.g, 'RegExp', $RegExp);
   }
   // 21.2.5.3 get RegExp.prototype.flags()
   if(/./g.flags != 'g')$.setDesc(proto, 'flags', {
@@ -40,4 +40,4 @@ if($.FW && $.DESC){
     get: require('./$.replacer')(/^.*\/(\w*)$/, '$1')
   });
 }
-require('./$.species')(RegExp);
+require('./$.species')($RegExp);
