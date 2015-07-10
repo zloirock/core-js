0f790a17f97be4b380e381a1dbbbfa6748cb1124
Oliver Hoffmann <oliverh855@gmail.com>
2015-07-10 16:14:11 +0200

    Use simplified method to check for global object
    For more information see PR #91

--- modules/$.js
+++ modules/$.js
@@ -1,35 +1,5 @@
 'use strict';
-var objectTypes = {
-  'function': true,
-  'object': true
-};
-
-/** Detect free variable `exports`. */
-var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
-
-/** Detect free variable `module`. */
-var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
-
-/** Detect free variable `global` from Node.js. */
-var freeGlobal = freeExports && freeModule && typeof global == 'object' && global && global.Object
-                 && global;
-
-/** Detect free variable `self`. */
-var freeSelf = objectTypes[typeof self] && self && self.Object && self;
-
-/** Detect free variable `window`. */
-var freeWindow = objectTypes[typeof window] && window && window.Object && window;
-
-/**
- * Used as a reference to the global object.
- *
- * The `this` value is used if it's the global object to avoid Greasemonkey's
- * restricted `window` object, otherwise the `window` object is used.
- */
-var root = freeGlobal || freeWindow !== (this && this.window) && freeWindow || freeSelf || this;
-var sandbox = Function('return this')();
-
-var global = root === sandbox ? root : sandbox
+var global = typeof self == 'object' && self.Math == Math ? self : Function('return this')() 
   , core   = {}
   , defineProperty = Object.defineProperty
   , hasOwnProperty = {}.hasOwnProperty
