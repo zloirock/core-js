4919e212e2c5644a66c59a23441124aed2772050
Oliver Hoffmann <oliverh855@gmail.com>
2015-07-10 14:28:17 +0200

    Get correct global object in Firefox extensions
    Fixes #86
    
    Line 2-31 is taken from https://github.com/lodash/lodash/blob/3.10.0/lodash.js#L251-274

--- modules/$.js
+++ modules/$.js
@@ -1,5 +1,37 @@
 'use strict';
-var global = typeof self != 'undefined' ? self : Function('return this')()
+var objectTypes = {
+  'function': true,
+  'object': true
+};
+
+/** Detect free variable `exports`. */
+var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+/** Detect free variable `module`. */
+var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+/** Detect free variable `global` from Node.js. */
+var freeGlobal = freeExports && freeModule && typeof global == 'object' && global && global.Object && global;
+
+/** Detect free variable `self`. */
+var freeSelf = objectTypes[typeof self] && self && self.Object && self;
+
+/** Detect free variable `window`. */
+var freeWindow = objectTypes[typeof window] && window && window.Object && window;
+
+/** Detect the popular CommonJS extension `module.exports`. */
+var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+/**
+ * Used as a reference to the global object.
+ *
+ * The `this` value is used if it's the global object to avoid Greasemonkey's
+ * restricted `window` object, otherwise the `window` object is used.
+ */
+var root = freeGlobal || ((freeWindow !== (this && this.window)) && freeWindow) || freeSelf || this;
+var sandbox = Function('return this')();
+
+var global = (root === sandbox) ? root : sandbox
   , core   = {}
   , defineProperty = Object.defineProperty
   , hasOwnProperty = {}.hasOwnProperty
