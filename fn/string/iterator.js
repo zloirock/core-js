8184a6a09b7b8ac1758e987945d51cd457e98bb1
Denis Pushkarev <zloirock@zloirock.ru>
2015-07-13 03:15:17 +0600

    ref {Number, String}#@@iterator entry points

--- fn/string/iterator.js
+++ fn/string/iterator.js
@@ -1,5 +1,5 @@
 require('../../modules/es6.string.iterator');
-var get = require('../../modules/$.iter').get;
+var get = require('../../modules/$.iter').Iterators.String;
 module.exports = function(it){
-  return get(it = '' + it).call(it);
+  return get.call(it);
 };
--- library/fn/number/iterator.js
+++ library/fn/number/iterator.js
@@ -1,5 +1,5 @@
 require('../../modules/core.number.iterator');
-var get = require('../../modules/$.iter').get;
+var get = require('../../modules/$.iter').Iterators.Number;
 module.exports = function(it){
-  return get(it = +it).call(it);
+  return get.call(it);
 };
