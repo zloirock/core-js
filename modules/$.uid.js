13c334f04d76719690350ae6def97b98db9b04cb
zloirock <zloirock@zloirock.ru>
2015-07-24 17:44:24 +0600

    use one random postfix for all symbols

--- modules/$.uid.js
+++ modules/$.uid.js
@@ -1,4 +1,5 @@
-var id = 0;
+var id = 0
+  , px = Math.random();
 module.exports = function(key){
-  return 'Symbol('.concat(key === undefined ? '' : key, ')_', (++id + Math.random()).toString(36));
+  return 'Symbol('.concat(key === undefined ? '' : key, ')_', (++id + px).toString(36));
 };
