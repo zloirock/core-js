93f75f0c64b4574e0a06049e7fd888ef69cf7874
zloirock <zloirock@zloirock.ru>
2015-07-29 17:21:38 +0600

    remove export $.iter-call.close

--- modules/$.iter-call.js
+++ modules/$.iter-call.js
@@ -3,13 +3,11 @@ function close(iterator){
   var ret = iterator['return'];
   if(ret !== undefined)anObject(ret.call(iterator));
 }
-function call(iterator, fn, value, entries){
+module.exports = function(iterator, fn, value, entries){
   try {
     return entries ? fn(anObject(value)[0], value[1]) : fn(value);
   } catch(e){
     close(iterator);
     throw e;
   }
-}
-call.close = close;
-module.exports = call;
+};
