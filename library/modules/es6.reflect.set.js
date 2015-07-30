be5a0b303a48fe173c9d5c926db01d7b5983201a
zloirock <zloirock@zloirock.ru>
2015-07-30 18:34:44 +0600

    move named functions from export in Reflect.{get, set} - possible some problems in old engines

--- library/modules/es6.reflect.set.js
+++ library/modules/es6.reflect.set.js
@@ -6,24 +6,24 @@ var $          = require('./$')
   , anObject   = require('./$.an-object')
   , isObject   = require('./$.is-object');
 
-$def($def.S, 'Reflect', {
-  set: function set(target, propertyKey, V/*, receiver*/){
-    var receiver = arguments.length < 4 ? target : arguments[3]
-      , ownDesc  = $.getDesc(anObject(target), propertyKey)
-      , existingDescriptor, proto;
-    if(!ownDesc){
-      if(isObject(proto = $.getProto(target))){
-        return set(proto, propertyKey, V, receiver);
-      }
-      ownDesc = createDesc(0);
+function set(target, propertyKey, V/*, receiver*/){
+  var receiver = arguments.length < 4 ? target : arguments[3]
+    , ownDesc  = $.getDesc(anObject(target), propertyKey)
+    , existingDescriptor, proto;
+  if(!ownDesc){
+    if(isObject(proto = $.getProto(target))){
+      return set(proto, propertyKey, V, receiver);
     }
-    if(has(ownDesc, 'value')){
-      if(ownDesc.writable === false || !isObject(receiver))return false;
-      existingDescriptor = $.getDesc(receiver, propertyKey) || createDesc(0);
-      existingDescriptor.value = V;
-      $.setDesc(receiver, propertyKey, existingDescriptor);
-      return true;
-    }
-    return ownDesc.set === undefined ? false : (ownDesc.set.call(receiver, V), true);
+    ownDesc = createDesc(0);
   }
-});
+  if(has(ownDesc, 'value')){
+    if(ownDesc.writable === false || !isObject(receiver))return false;
+    existingDescriptor = $.getDesc(receiver, propertyKey) || createDesc(0);
+    existingDescriptor.value = V;
+    $.setDesc(receiver, propertyKey, existingDescriptor);
+    return true;
+  }
+  return ownDesc.set === undefined ? false : (ownDesc.set.call(receiver, V), true);
+}
+
+$def($def.S, 'Reflect', {set: set});
--- modules/es6.reflect.get.js
+++ modules/es6.reflect.get.js
@@ -5,16 +5,16 @@ var $        = require('./$')
   , isObject = require('./$.is-object')
   , anObject = require('./$.an-object');
 
-$def($def.S, 'Reflect', {
-  get: function get(target, propertyKey/*, receiver*/){
-    var receiver = arguments.length < 3 ? target : arguments[2]
-      , desc, proto;
-    if(anObject(target) === receiver)return target[propertyKey];
-    if(desc = $.getDesc(target, propertyKey))return has(desc, 'value')
-      ? desc.value
-      : desc.get !== undefined
-        ? desc.get.call(receiver)
-        : undefined;
-    if(isObject(proto = $.getProto(target)))return get(proto, propertyKey, receiver);
-  }
-});
+function get(target, propertyKey/*, receiver*/){
+  var receiver = arguments.length < 3 ? target : arguments[2]
+    , desc, proto;
+  if(anObject(target) === receiver)return target[propertyKey];
+  if(desc = $.getDesc(target, propertyKey))return has(desc, 'value')
+    ? desc.value
+    : desc.get !== undefined
+      ? desc.get.call(receiver)
+      : undefined;
+  if(isObject(proto = $.getProto(target)))return get(proto, propertyKey, receiver);
+}
+
+$def($def.S, 'Reflect', {get: get});
