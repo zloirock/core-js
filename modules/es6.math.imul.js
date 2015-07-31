41dc2e5ea1cc88c844dff2f71ae840c64a861401
zloirock <zloirock@zloirock.ru>
2015-08-01 05:40:15 +0600

    strange output, trying break travis tests

--- modules/es6.math.imul.js
+++ modules/es6.math.imul.js
@@ -3,7 +3,7 @@ var $def = require('./$.def');
 
 // WebKit fails with big numbers
 $def($def.S + $def.F * require('./$.throws')(function(){
-  return Math.imul(0xffffffff, 5) != -5;
+  //return Math.imul(0xffffffff, 5) != -5;
 }), 'Math', {
   imul: function imul(x, y){
     var UINT16 = 0xffff
