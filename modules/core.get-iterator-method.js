a14cfb6cbc956c862050fb3221f0d5be693abbd4
zloirock <zloirock@zloirock.ru>
2015-08-02 00:27:41 +0600

    use string '@@iterator' only in `library` version (required for `regenerator`)

--- modules/core.get-iterator-method.js
+++ modules/core.get-iterator-method.js
@@ -2,5 +2,5 @@ var classof   = require('./$.classof')
   , ITERATOR  = require('./$.wks')('iterator')
   , Iterators = require('./$.iterators');
 module.exports = require('./$.core').getIteratorMethod = function(it){
-  if(it != undefined)return it[ITERATOR] || it['@@iterator'] || Iterators[classof(it)];
+  if(it != undefined)return it[ITERATOR] || Iterators[classof(it)];
 };
