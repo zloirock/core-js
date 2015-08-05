3df5b1b28031c536f02cc529b7b6554dcbc3fcfa
Denis Pushkarev <zloirock@zloirock.ru>
2015-08-05 23:59:24 +0600

    add comments to `Reflect.construct`

--- modules/es6.reflect.construct.js
+++ modules/es6.reflect.construct.js
@@ -10,6 +10,7 @@ $def($def.S, 'Reflect', {
   construct: function construct(Target, args /*, newTarget*/){
     aFunction(Target);
     if(arguments.length < 3){
+      // w/o newTarget, optimization for 0-4 arguments
       if(args != undefined)switch(anObject(args).length){
         case 0: return new Target;
         case 1: return new Target(args[0]);
@@ -17,10 +18,12 @@ $def($def.S, 'Reflect', {
         case 3: return new Target(args[0], args[1], args[2]);
         case 4: return new Target(args[0], args[1], args[2], args[3]);
       }
+      // w/o newTarget, lot of arguments case
       var $args = [null];
       $args.push.apply($args, args);
       return new (bind.apply(Target, $args));
     }
+    // with newTarget, not support built-in constructors
     var proto    = aFunction(arguments[2]).prototype
       , instance = $.create(isObject(proto) ? proto : Object.prototype)
       , result   = Function.apply.call(Target, instance, args);
