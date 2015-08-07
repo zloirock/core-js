061907b2d868481b1c438ebc4f6a8de2c05c5cd3
Denis Pushkarev <zloirock@zloirock.ru>
2015-08-08 04:28:35 +0600

    update eslint

--- library/modules/core.string.escape-html.js
+++ library/modules/core.string.escape-html.js
@@ -1,11 +1,11 @@
 'use strict';
 var $def = require('./$.def')
   , $re  = require('./$.replacer')(/[&<>"']/g, {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      '"': '&quot;',
-      "'": '&apos;'
-    });
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&apos;'
+  });
 
 $def($def.P + $def.F, 'String', {escapeHTML: function escapeHTML(){ return $re(this); }});
