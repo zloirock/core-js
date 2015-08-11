0a82337107483d9386b91e5a6c6cb8a0c0d80481
Denis Pushkarev <zloirock@zloirock.ru>
2015-08-11 23:15:08 +0600

    simplify `Reflect.enumerate`
    https://esdiscuss.org/topic/question-about-enumerate-and-property-decision-timing

--- modules/es6.reflect.enumerate.js
+++ modules/es6.reflect.enumerate.js
@@ -4,17 +4,15 @@ var $def     = require('./$.def')
   , anObject = require('./$.an-object');
 var Enumerate = function(iterated){
   this._t = anObject(iterated); // target
-  this._k = undefined;          // keys
   this._i = 0;                  // next index
+  var keys = this._k = []       // keys
+    , key;
+  for(key in iterated)keys.push(key);
 };
 require('./$.iter-create')(Enumerate, 'Object', function(){
   var that = this
     , keys = that._k
     , key;
-  if(keys == undefined){
-    that._k = keys = [];
-    for(key in that._t)keys.push(key);
-  }
   do {
     if(that._i >= keys.length)return {value: undefined, done: true};
   } while(!((key = keys[that._i++]) in that._t));
