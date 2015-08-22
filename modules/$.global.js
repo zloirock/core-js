2f62d278b781ab60bb74c7fa112dafe25226029d
Denis Pushkarev <zloirock@zloirock.ru>
2015-08-22 16:00:18 +0600

    check `window` before `self` for global detection, ~IE8 with changed `document.domain` case, close #100, #101

--- modules/$.global.js
+++ modules/$.global.js
@@ -1,3 +1,4 @@
-var global = typeof self != 'undefined' && self.Math == Math ? self : Function('return this')();
-module.exports = global;
+var UNDEFINED = 'undefined';
+var global = module.exports = typeof window != UNDEFINED && window.Math == Math
+  ? window : typeof self != UNDEFINED && self.Math == Math ? self : Function('return this')();
 if(typeof __g == 'number')__g = global; // eslint-disable-line no-undef
