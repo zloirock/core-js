d47e54d1091a1fcb315800309437bfad8e12e2ca
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-04 22:53:25 +0600

    replace non-generic `Array.of` in WebKit

--- library/modules/es6.array.of.js
+++ library/modules/es6.array.of.js
@@ -1,6 +1,11 @@
 'use strict';
 var $def = require('./$.def');
-$def($def.S, 'Array', {
+
+// WebKit Array.of isn't generic
+$def($def.S + $def.F * require('./$.fails')(function(){
+  function F(){}
+  return !(Array.of.call(F) instanceof F);
+}), 'Array', {
   // 22.1.2.3 Array.of( ...items)
   of: function of(/* ...args */){
     var index  = 0
