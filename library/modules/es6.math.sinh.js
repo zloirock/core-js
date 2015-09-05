708236ecbd4f5fe65dc90d5a9483be0a55e0207a
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-05 10:20:46 +0600

    fix `Math.sinh` with very small numbers in V8 near Chromium 38

--- library/modules/es6.math.sinh.js
+++ library/modules/es6.math.sinh.js
@@ -3,7 +3,10 @@ var $def  = require('./$.def')
   , expm1 = require('./$.expm1')
   , exp   = Math.exp;
 
-$def($def.S, 'Math', {
+// V8 near Chromium 38 has a problem with very small numbers
+$def($def.S + $def.F * require('./$.fails')(function(){
+  return !Math.sinh(-2e-17) != -2e-17;
+}), 'Math', {
   sinh: function sinh(x){
     return Math.abs(x = +x) < 1
       ? (expm1(x) - expm1(-x)) / 2
