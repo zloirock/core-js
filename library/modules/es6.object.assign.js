9b0289539466893ce85a797f0643d0cb34dab34e
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-05 07:07:54 +0600

    fix support symbols in FF34-35 `Object.assign`

--- library/modules/es6.object.assign.js
+++ library/modules/es6.object.assign.js
@@ -1,3 +1,5 @@
 // 19.1.3.1 Object.assign(target, source)
-var $def = require('./$.def');
-$def($def.S, 'Object', {assign: require('./$.assign')});
+var $def    = require('./$.def')
+  , $assign = require('./$.assign');
+
+$def($def.S + $def.F, 'Object', {assign: $assign});
--- modules/$.assign.js
+++ modules/$.assign.js
@@ -2,9 +2,10 @@
 var toObject = require('./$.to-object')
   , IObject  = require('./$.iobject')
   , enumKeys = require('./$.enum-keys');
-/* eslint-disable no-unused-vars */
-module.exports = Object.assign || function assign(target, source){
-/* eslint-enable no-unused-vars */
+
+module.exports = require('./$.fails')(function(){
+  return Symbol() in Object.assign({}); // Object.assign available and Symbol is native
+}) ? function assign(target, source){   // eslint-disable-line no-unused-vars
   var T = toObject(target)
     , l = arguments.length
     , i = 1;
@@ -17,4 +18,4 @@ module.exports = Object.assign || function assign(target, source){
     while(length > j)T[key = keys[j++]] = S[key];
   }
   return T;
-};
+} : Object.assign;
