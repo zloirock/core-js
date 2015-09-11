2705275035035e6f40f1f9db2509f9e7f0f9b57c
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-11 23:58:38 +0600

    fix compatibility problem with some alternative `Symbol` polyfills

--- modules/$.unscope.js
+++ modules/$.unscope.js
@@ -1,6 +1,6 @@
 // 22.1.3.31 Array.prototype[@@unscopables]
 var UNSCOPABLES = require('./$.wks')('unscopables');
-if(!(UNSCOPABLES in []))require('./$.hide')(Array.prototype, UNSCOPABLES, {});
+if([][UNSCOPABLES] == undefined)require('./$.hide')(Array.prototype, UNSCOPABLES, {});
 module.exports = function(key){
   [][UNSCOPABLES][key] = true;
 };
