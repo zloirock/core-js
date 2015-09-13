177a4d83b79f86863b2144338db6d09015e7a83d
Denis Pushkarev <zloirock@zloirock.ru>
2015-09-13 14:18:45 +0600

    fix `NodeList` and `HTMLCollection` iterators dependencies on `WeakMap` entry point

--- library/fn/weak-map.js
+++ library/fn/weak-map.js
@@ -1,4 +1,4 @@
 require('../modules/es6.object.to-string');
-require('../modules/es6.array.iterator');
+require('../modules/web.dom.iterable');
 require('../modules/es6.weak-map');
 module.exports = require('../modules/$.core').WeakMap;
