aef86d2b374fa67731c7b17367faa86ce03ac4b3
Denis Pushkarev <zloirock@zloirock.ru>
2015-10-07 23:20:46 +0600

    change `core.{isIterable, getIteratorMethod}` for work with `{Array, String}.prototype` - by ES6 prototypes not instances

--- library/modules/core.get-iterator-method.js
+++ library/modules/core.get-iterator-method.js
@@ -1,6 +1,9 @@
-var classof   = require('./$.classof')
-  , ITERATOR  = require('./$.wks')('iterator')
-  , Iterators = require('./$.iterators');
+var classof     = require('./$.classof')
+  , ITERATOR    = require('./$.wks')('iterator')
+  , Iterators   = require('./$.iterators')
+  , ArrayProto  = Array.prototype
+  , StringProto = String.prototype;
 module.exports = require('./$.core').getIteratorMethod = function(it){
-  if(it != undefined)return it[ITERATOR] || it['@@iterator'] || Iterators[classof(it)];
+  if(it != undefined)return it[ITERATOR] || it['@@iterator']
+    || Iterators[it === ArrayProto ? 'Array' : it === StringProto ? 'String' : classof(it)];
 };
--- modules/core.is-iterable.js
+++ modules/core.is-iterable.js
@@ -1,7 +1,13 @@
-var classof   = require('./$.classof')
-  , ITERATOR  = require('./$.wks')('iterator')
-  , Iterators = require('./$.iterators');
+var classof     = require('./$.classof')
+  , ITERATOR    = require('./$.wks')('iterator')
+  , Iterators   = require('./$.iterators')
+  , ArrayProto  = Array.prototype
+  , StringProto = String.prototype;
 module.exports = require('./$.core').isIterable = function(it){
   var O = Object(it);
-  return ITERATOR in O || '@@iterator' in O || Iterators.hasOwnProperty(classof(O));
+  return ITERATOR in O
+    || '@@iterator' in O
+    || it === ArrayProto
+    || it === StringProto
+    || Iterators.hasOwnProperty(classof(O));
 };
