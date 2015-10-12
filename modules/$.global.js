766b40d62fa13e101a3cbd81107eade9e6e533c0
Denis Pushkarev <zloirock@zloirock.ru>
2015-10-12 13:50:49 +0600

    remove 'undefined' shortcut for make global detection little more readable

--- modules/$.global.js
+++ modules/$.global.js
@@ -1,5 +1,4 @@
 // https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
-var UNDEFINED = 'undefined';
-var global = module.exports = typeof window != UNDEFINED && window.Math == Math
-  ? window : typeof self != UNDEFINED && self.Math == Math ? self : Function('return this')();
+var global = module.exports = typeof window != 'undefined' && window.Math == Math
+  ? window : typeof self != 'undefined' && self.Math == Math ? self : Function('return this')();
 if(typeof __g == 'number')__g = global; // eslint-disable-line no-undef
