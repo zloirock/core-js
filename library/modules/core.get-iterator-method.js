5c9f9559c65a2a08decac6aa8a1cf29b858c8972
Denis Pushkarev <zloirock@zloirock.ru>
2015-10-15 22:43:26 +0600

    remove special cases for native prototypes from iterator helpers, revert aef86d2b374fa67731c7b17367faa86ce03ac4b3
    `String.prototype` semantic reverted to ES5,
    https://github.com/tc39/ecma262/pull/83, `Array.prototype` should work
    without a special case,
    http://www.ecma-international.org/ecma-262/6.0/#sec-properties-of-the-array-prototype-object

--- library/modules/core.get-iterator-method.js
+++ library/modules/core.get-iterator-method.js
@@ -1,9 +1,8 @@
-var classof     = require('./$.classof')
-  , ITERATOR    = require('./$.wks')('iterator')
-  , Iterators   = require('./$.iterators')
-  , ArrayProto  = Array.prototype
-  , StringProto = String.prototype;
+var classof   = require('./$.classof')
+  , ITERATOR  = require('./$.wks')('iterator')
+  , Iterators = require('./$.iterators');
 module.exports = require('./$.core').getIteratorMethod = function(it){
-  if(it != undefined)return it[ITERATOR] || it['@@iterator']
-    || Iterators[it === ArrayProto ? 'Array' : it === StringProto ? 'String' : classof(it)];
+  if(it != undefined)return it[ITERATOR]
+    || it['@@iterator']
+    || Iterators[classof(it)];
 };
--- library/modules/core.is-iterable.js
+++ library/modules/core.is-iterable.js
@@ -1,13 +1,9 @@
-var classof     = require('./$.classof')
-  , ITERATOR    = require('./$.wks')('iterator')
-  , Iterators   = require('./$.iterators')
-  , ArrayProto  = Array.prototype
-  , StringProto = String.prototype;
+var classof   = require('./$.classof')
+  , ITERATOR  = require('./$.wks')('iterator')
+  , Iterators = require('./$.iterators');
 module.exports = require('./$.core').isIterable = function(it){
   var O = Object(it);
   return ITERATOR in O
     || '@@iterator' in O
-    || it === ArrayProto
-    || it === StringProto
     || Iterators.hasOwnProperty(classof(O));
 };
