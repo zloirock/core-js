79e3a913e3d9f9232ef754ae2bc7ff4743ab0b2b
Denis Pushkarev <zloirock@zloirock.ru>
2015-11-01 21:32:51 +0600

    add some comments

--- modules/es6.number.constructor.js
+++ modules/es6.number.constructor.js
@@ -11,27 +11,34 @@ var $          = require('./$')
   , proto      = $Number.prototype
   // Opera ~12 has broken Object#toString
   , BROKEN_COF = cof($.create(proto)) == NUMBER;
+
+// 7.1.1 ToPrimitive(input [, PreferredType]), currently - only "number" hint case
 var toPrimitive = function(it){
   var fn, val;
   if(typeof (fn = it.valueOf) == 'function' && !isObject(val = fn.call(it)))return val;
   if(typeof (fn = it.toString) == 'function' && !isObject(val = fn.call(it)))return val;
   throw TypeError("Can't convert object to number");
 };
+
+// 7.1.3 ToNumber(argument)
 var toNumber = function(it){
   if(isObject(it))it = toPrimitive(it);
   if(typeof it == 'string' && it.length > 2 && it.charCodeAt(0) == 48){
     var radix, maxCode;
     switch(it.charCodeAt(1)){
-      case 66 : case 98  : radix = 2; maxCode = 49; break;
-      case 79 : case 111 : radix = 8; maxCode = 55; break;
+      case 66 : case 98  : radix = 2; maxCode = 49; break; // fast equal /^0b[01]+$/i
+      case 79 : case 111 : radix = 8; maxCode = 55; break; // fast equal /^0o[0-7]+$/i
       default : return +it;
     }
     for(var digits = it.slice(2), i = 0, l = digits.length, code; i < l; i++){
       code = digits.charCodeAt(i);
+      // parseInt parses a string to a first unavailable symbol
+      // but ToNumber should return NaN if a string contains unavailable symbols
       if(code < 48 || code > maxCode)return NaN;
     } return parseInt(digits, radix);
   } return +it;
 };
+
 if(!($Number('0o1') && $Number('0b1'))){
   $Number = function Number(it){
     var that = this;
