573491b46362ad12b12314cb367c4b0e97d67b57
Denis Pushkarev <zloirock@zloirock.ru>
2015-11-02 06:25:45 +0600

    rename `$.same` -> `$.same-value`

--- modules/es6.object.is.js
+++ modules/es6.object.is.js
@@ -1,5 +1,3 @@
 // 19.1.3.10 Object.is(value1, value2)
 var $def = require('./$.def');
-$def($def.S, 'Object', {
-  is: require('./$.same')
-});
+$def($def.S, 'Object', {is: require('./$.same-value')});
