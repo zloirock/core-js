d24337c232efe4dab5c19978e34b60c720cb1f73
Denis Pushkarev <zloirock@zloirock.ru>
2015-11-21 20:38:05 +0600

    one more fix redefinition dom collections iterators, chromium ~42 case

--- modules/web.dom.iterable.js
+++ modules/web.dom.iterable.js
@@ -1,23 +1,27 @@
-var $iterators    = require('./es6.array.iterator')
-  , redefineAll   = require('./$.redefine-all')
-  , global        = require('./$.global')
-  , hide          = require('./$.hide')
-  , Iterators     = require('./$.iterators')
-  , wks           = require('./$.wks')
-  , ITERATOR      = wks('iterator')
-  , TO_STRING_TAG = wks('toStringTag')
-  , ArrayValues   = Iterators.Array;
+var $iterators     = require('./es6.array.iterator')
+  , redefine       = require('./$.redefine')
+  , global         = require('./$.global')
+  , hide           = require('./$.hide')
+  , Iterators      = require('./$.iterators')
+  , wks            = require('./$.wks')
+  , CORRECT_SYMBOL = require('./$.correct-symbol')
+  , ITERATOR       = wks('iterator')
+  , TO_STRING_TAG  = wks('toStringTag')
+  , ArrayValues    = Iterators.Array;
 
 require('./$').each.call((
   'CSSRuleList,CSSStyleDeclaration,DOMStringList,DOMTokenList,FileList,HTMLCollection,MediaList,' +
   'MimeTypeArray,NamedNodeMap,NodeList,NodeListOf,Plugin,PluginArray,StyleSheetList,TouchList'
 ).split(','), function(NAME){
   var Collection = global[NAME]
-    , proto      = Collection && Collection.prototype;
+    , proto      = Collection && Collection.prototype
+    , key;
   if(proto){
     if(!proto[ITERATOR])hide(proto, ITERATOR, ArrayValues);
     if(!proto[TO_STRING_TAG])hide(proto, TO_STRING_TAG, NAME);
     Iterators[NAME] = ArrayValues;
-    redefineAll(proto, $iterators, true);
+    for(key in $iterators){
+      if(!CORRECT_SYM || !proto[key])redefine(proto, key, $iterators[key], true);
+    }
   }
 });
