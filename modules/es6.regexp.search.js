7a90ad4004a437bbbf96e29051904eedda3f7457
Denis Pushkarev <zloirock@zloirock.ru>
2016-01-26 17:43:39 +0600

    add fixes for `String#split` / `RegExp#@@split`, #156

--- modules/es6.regexp.search.js
+++ modules/es6.regexp.search.js
@@ -1,10 +1,10 @@
 // @@search logic
-require('./_fix-re-wks')('search', 1, function(defined, SEARCH){
+require('./_fix-re-wks')('search', 1, function(defined, SEARCH, $search){
   // 21.1.3.15 String.prototype.search(regexp)
-  return function search(regexp){
+  return [function search(regexp){
     'use strict';
     var O  = defined(this)
       , fn = regexp == undefined ? undefined : regexp[SEARCH];
     return fn !== undefined ? fn.call(regexp, O) : new RegExp(regexp)[SEARCH](String(O));
-  };
+  }, $search];
 });
