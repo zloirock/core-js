270fabccd38e0210caf1a4ffc44ca328880fba69
Denis Pushkarev <zloirock@zloirock.ru>
2016-01-26 21:43:48 +0600

    fix Chrome 34 `Number.parseInt`

--- library/modules/es6.number.parse-int.js
+++ library/modules/es6.number.parse-int.js
@@ -1,3 +1,4 @@
-var $export = require('./_export');
+var $export   = require('./_export')
+  , $parseInt = require('./_parse-int');
 // 20.1.2.13 Number.parseInt(string, radix)
-$export($export.S, 'Number', {parseInt: require('./_parse-int')});
+$export($export.S + $export.F * (Number.parseInt != $parseInt), 'Number', {parseInt: $parseInt});
