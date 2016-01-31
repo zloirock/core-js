8f939d5dd50bf1f51a6569f192baeaf6e437fb80
Denis Pushkarev <zloirock@zloirock.ru>
2016-01-31 19:19:46 +0600

    [tests] add comments to `Number#toPrecision` tests

--- tests/library.js
+++ tests/library.js
@@ -2801,33 +2801,33 @@
     var toPrecision;
     toPrecision = core.Number.toPrecision;
     assert.isFunction(toPrecision);
-    assert.same(toPrecision(0.00008, 3), '0.0000800');
-    assert.same(toPrecision(1.255, 2), '1.3');
-    assert.same(toPrecision(1843654265.0774949, 13), '1843654265.077');
-    assert.same(toPrecision(NaN, 1), 'NaN');
-    assert.same(toPrecision(123.456), '123.456');
-    assert.same(toPrecision(123.456, void 8), '123.456');
+    assert.same(toPrecision(0.00008, 3), '0.0000800', '0.00008.toPrecision(3)');
+    assert.same(toPrecision(1.255, 2), '1.3', '1.255.toPrecision(2)');
+    assert.same(toPrecision(1843654265.0774949, 13), '1843654265.077', '1843654265.0774949.toPrecision(13)');
+    assert.same(toPrecision(NaN, 1), 'NaN', 'If x is NaN, return the String "NaN".');
+    assert.same(toPrecision(123.456), '123.456', 'If precision is undefined, return ! ToString(x).');
+    assert.same(toPrecision(123.456, void 8), '123.456', 'If precision is undefined, return ! ToString(x).');
     assert.throws(function(){
       toPrecision(0.9, 0);
-    }, RangeError);
+    }, RangeError, 'If p < 1 or p > 21, throw a RangeError exception.');
     assert.throws(function(){
       toPrecision(0.9, 101);
-    }, RangeError);
+    }, RangeError, 'If p < 1 or p > 21, throw a RangeError exception.');
     assert.throws(function(){
-      return toPrecision({}, 1);
-    }, TypeError);
+      toPrecision({}, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision('123', 1);
-    }, TypeError);
+      toPrecision('123', 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision(false, 1);
-    }, TypeError);
+      toPrecision(false, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision(null, 1);
-    }, TypeError);
+      toPrecision(null, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision(void 8, 1);
-    }, TypeError);
+      toPrecision(void 8, 1);
+    }, TypeError, '? thisNumberValue(this value)');
   });
 }).call(this);
 
--- tests/tests.js
+++ tests/tests.js
@@ -3556,33 +3556,33 @@
     assert.name(toPrecision, 'toPrecision');
     assert.arity(toPrecision, 1);
     assert.looksNative(toPrecision);
-    assert.same(0.00008.toPrecision(3), '0.0000800');
-    assert.same(1.255.toPrecision(2), '1.3');
-    assert.same(1843654265.0774949.toPrecision(13), '1843654265.077');
-    assert.same(NaN.toPrecision(1), 'NaN');
-    assert.same(123.456.toPrecision(), '123.456');
-    assert.same(123.456.toPrecision(void 8), '123.456');
+    assert.same(0.00008.toPrecision(3), '0.0000800', '0.00008.toPrecision(3)');
+    assert.same(1.255.toPrecision(2), '1.3', '1.255.toPrecision(2)');
+    assert.same(1843654265.0774949.toPrecision(13), '1843654265.077', '1843654265.0774949.toPrecision(13)');
+    assert.same(NaN.toPrecision(1), 'NaN', 'If x is NaN, return the String "NaN".');
+    assert.same(123.456.toPrecision(), '123.456', 'If precision is undefined, return ! ToString(x).');
+    assert.same(123.456.toPrecision(void 8), '123.456', 'If precision is undefined, return ! ToString(x).');
     assert.throws(function(){
       0.9.toPrecision(0);
-    }, RangeError);
+    }, RangeError, 'If p < 1 or p > 21, throw a RangeError exception.');
     assert.throws(function(){
       0.9.toPrecision(101);
-    }, RangeError);
+    }, RangeError, 'If p < 1 or p > 21, throw a RangeError exception.');
     assert.throws(function(){
-      return toPrecision.call({}, 1);
-    }, TypeError);
+      toPrecision.call({}, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision.call('123', 1);
-    }, TypeError);
+      toPrecision.call('123', 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision.call(false, 1);
-    }, TypeError);
+      toPrecision.call(false, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision.call(null, 1);
-    }, TypeError);
+      toPrecision.call(null, 1);
+    }, TypeError, '? thisNumberValue(this value)');
     assert.throws(function(){
-      return toPrecision.call(void 8, 1);
-    }, TypeError);
+      toPrecision.call(void 8, 1);
+    }, TypeError, '? thisNumberValue(this value)');
   });
 }).call(this);
 
