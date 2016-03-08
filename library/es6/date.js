66593cee27b2ad40c55e808159a370e718a56e40
Denis Pushkarev <zloirock@zloirock.ru>
2016-03-09 01:49:41 +0600

    One more fallback for Qt Script bug, #173

--- library/es6/date.js
+++ library/es6/date.js
@@ -1,5 +1,5 @@
 require('../modules/es6.date.now');
-require('../modules/es6.date.to-string');
-require('../modules/es6.date.to-iso-string');
 require('../modules/es6.date.to-json');
+require('../modules/es6.date.to-iso-string');
+require('../modules/es6.date.to-string');
 module.exports = Date;
