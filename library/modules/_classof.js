fb3ff3454a9997369de4ff22778fe59ecda7bf43
Denis Pushkarev <zloirock@zloirock.ru>
2016-03-08 21:06:20 +0600

    Additional fallback for IE11 Script Access Denied error, close #165

--- library/modules/_classof.js
+++ library/modules/_classof.js
@@ -4,11 +4,18 @@ var cof = require('./_cof')
   // ES3 wrong here
   , ARG = cof(function(){ return arguments; }()) == 'Arguments';
 
+// fallback for IE11 Script Access Denied error
+var tryGet = function(it, key){
+  try {
+    return it[key];
+  } catch(e){ /* empty */ }
+};
+
 module.exports = function(it){
   var O, T, B;
   return it === undefined ? 'Undefined' : it === null ? 'Null'
     // @@toStringTag case
-    : typeof (T = (O = Object(it))[TAG]) == 'string' ? T
+    : typeof (T = tryGet(O = Object(it), TAG)) == 'string' ? T
     // builtinTag case
     : ARG ? cof(O)
     // ES3 arguments fallback
