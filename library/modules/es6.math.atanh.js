e0300c874634f09c3cc95ca6a8c2cc7ddf72079e
Denis Pushkarev <zloirock@zloirock.ru>
2016-03-25 07:11:00 +0600

    fix Math.atanh in Tor Browser

--- library/modules/es6.math.atanh.js
+++ library/modules/es6.math.atanh.js
@@ -1,7 +1,9 @@
 // 20.2.2.7 Math.atanh(x)
-var $export = require('./_export');
+var $export = require('./_export')
+  , $atanh  = Math.atanh;
 
-$export($export.S, 'Math', {
+// Tor Browser bug: Math.atanh(-0) -> 0 
+$export($export.S + $export.F * !($atanh && 1 / $atanh(-0) < 0), 'Math', {
   atanh: function atanh(x){
     return (x = +x) == 0 ? x : Math.log((1 + x) / (1 - x)) / 2;
   }
