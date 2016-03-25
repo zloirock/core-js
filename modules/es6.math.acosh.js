3f60c2b5c04a15c36d10ef3911f2c9ada4807ac9
Denis Pushkarev <zloirock@zloirock.ru>
2016-03-25 06:55:40 +0600

    fix Math.acosh in Tor Browser

--- modules/es6.math.acosh.js
+++ modules/es6.math.acosh.js
@@ -4,8 +4,12 @@ var $export = require('./_export')
   , sqrt    = Math.sqrt
   , $acosh  = Math.acosh;
 
-// V8 bug https://code.google.com/p/v8/issues/detail?id=3509
-$export($export.S + $export.F * !($acosh && Math.floor($acosh(Number.MAX_VALUE)) == 710), 'Math', {
+$export($export.S + $export.F * !($acosh
+  // V8 bug: https://code.google.com/p/v8/issues/detail?id=3509
+  && Math.floor($acosh(Number.MAX_VALUE)) == 710
+  // Tor Browser bug: Math.acosh(Infinity) -> NaN 
+  && $acosh(Infinity) == Infinity
+), 'Math', {
   acosh: function acosh(x){
     return (x = +x) < 1 ? NaN : x > 94906265.62425156
       ? Math.log(x) + Math.LN2
