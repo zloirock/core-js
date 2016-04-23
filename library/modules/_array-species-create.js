020a9736b618ed911a533159fc79c1757f3b0d25
Denis Pushkarev <zloirock@zloirock.ru>
2016-04-24 04:28:08 +0700

    extract `_array-species-constructor` helper

--- library/modules/_array-species-create.js
+++ library/modules/_array-species-create.js
@@ -1,16 +1,6 @@
 // 9.4.2.3 ArraySpeciesCreate(originalArray, length)
-var isObject = require('./_is-object')
-  , isArray  = require('./_is-array')
-  , SPECIES  = require('./_wks')('species');
+var speciesConstructor = require('./_array-species-constructor');
+
 module.exports = function(original, length){
-  var C;
-  if(isArray(original)){
-    C = original.constructor;
-    // cross-realm fallback
-    if(typeof C == 'function' && (C === Array || isArray(C.prototype)))C = undefined;
-    if(isObject(C)){
-      C = C[SPECIES];
-      if(C === null)C = undefined;
-    }
-  } return new (C === undefined ? Array : C)(length);
+  return new (speciesConstructor(original))(length);
 };
