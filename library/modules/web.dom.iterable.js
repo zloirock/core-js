14b3ad23960b2c5f52ade0e0a1d96b05b41490c6
Denis Pushkarev <zloirock@zloirock.ru>
2016-11-04 23:30:48 +0700

    fix some problems of #249

--- library/modules/web.dom.iterable.js
+++ library/modules/web.dom.iterable.js
@@ -4,8 +4,14 @@ var global        = require('./_global')
   , Iterators     = require('./_iterators')
   , TO_STRING_TAG = require('./_wks')('toStringTag');
 
-for(var collections = ['NodeList', 'DOMTokenList', 'MediaList', 'StyleSheetList', 'CSSRuleList'], i = 0; i < 5; i++){
-  var NAME       = collections[i]
+var DOMIterables = ('CSSRuleList,CSSStyleDeclaration,CSSValueList,ClientRectList,DOMRectList,DOMStringList,' + 
+  'DOMTokenList,DataTransferItemList,FileList,HTMLAllCollection,HTMLCollection,HTMLFormElement,HTMLSelectElement,' +
+  'MediaList,MimeTypeArray,NamedNodeMap,NodeList,PaintRequestList,Plugin,PluginArray,SVGLengthList,SVGNumberList,' +
+  'SVGPathSegList,SVGPointList,SVGStringList,SVGTransformList,SourceBufferList,StyleSheetList,TextTrackCueList,' +
+  'TextTrackList,TouchList').split(',');
+
+for(var i = 0; i < DOMIterables.length; i++){
+  var NAME       = DOMIterables[i]
     , Collection = global[NAME]
     , proto      = Collection && Collection.prototype;
   if(proto && !proto[TO_STRING_TAG])hide(proto, TO_STRING_TAG, NAME);
--- modules/library/web.dom.iterable.js
+++ modules/library/web.dom.iterable.js
@@ -1,46 +1,17 @@
 require('./es6.array.iterator');
 var global        = require('./_global')
-  , getKeys       = require('./_object-keys')
   , hide          = require('./_hide')
   , Iterators     = require('./_iterators')
   , TO_STRING_TAG = require('./_wks')('toStringTag');
 
-var DOMIterables = {
-  CSSRuleList: true, // TODO: Not spec compliant, should be false.
-  CSSStyleDeclaration: false,
-  CSSValueList: false,
-  ClientRectList: false,
-  DOMRectList: false,
-  DOMStringList: false,
-  DOMTokenList: true,
-  DataTransferItemList: false,
-  FileList: false,
-  HTMLAllCollection: false,
-  HTMLCollection: false,
-  HTMLFormElement: false,
-  HTMLSelectElement: false,
-  MediaList: true, // TODO: Not spec compliant, should be false.
-  MimeTypeArray: false,
-  NamedNodeMap: false,
-  NodeList: true,
-  PaintRequestList: false,
-  Plugin: false,
-  PluginArray: false,
-  SVGLengthList: false,
-  SVGNumberList: false,
-  SVGPathSegList: false,
-  SVGPointList: false,
-  SVGStringList: false,
-  SVGTransformList: false,
-  SourceBufferList: false,
-  StyleSheetList: true, // TODO: Not spec compliant, should be false.
-  TextTrackCueList: false,
-  TextTrackList: false,
-  TouchList: false
-};
+var DOMIterables = ('CSSRuleList,CSSStyleDeclaration,CSSValueList,ClientRectList,DOMRectList,DOMStringList,' + 
+  'DOMTokenList,DataTransferItemList,FileList,HTMLAllCollection,HTMLCollection,HTMLFormElement,HTMLSelectElement,' +
+  'MediaList,MimeTypeArray,NamedNodeMap,NodeList,PaintRequestList,Plugin,PluginArray,SVGLengthList,SVGNumberList,' +
+  'SVGPathSegList,SVGPointList,SVGStringList,SVGTransformList,SourceBufferList,StyleSheetList,TextTrackCueList,' +
+  'TextTrackList,TouchList').split(',');
 
-for(var collections = getKeys(DOMIterables), i = 0; i < collections.length; i++){
-  var NAME       = collections[i]
+for(var i = 0; i < DOMIterables.length; i++){
+  var NAME       = DOMIterables[i]
     , Collection = global[NAME]
     , proto      = Collection && Collection.prototype;
   if(proto && !proto[TO_STRING_TAG])hide(proto, TO_STRING_TAG, NAME);
