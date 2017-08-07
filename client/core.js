e083821d808f1bb6da8420e0e13dee63e679f95e
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-07 21:41:55 +0700

    remove `Object.isObject` (non-standard)

--- client/core.js
+++ client/core.js
@@ -3872,8 +3872,7 @@ __webpack_require__(49);
 __webpack_require__(317);
 __webpack_require__(121);
 __webpack_require__(318);
-__webpack_require__(320);
-module.exports = __webpack_require__(321);
+module.exports = __webpack_require__(320);
 
 
 /***/ }),
@@ -8339,15 +8338,6 @@ module.exports = function (/* ...pargs */) {
 
 /***/ }),
 /* 320 */
-/***/ (function(module, exports, __webpack_require__) {
-
-var $export = __webpack_require__(0);
-
-$export($export.S + $export.F, 'Object', { isObject: __webpack_require__(4) });
-
-
-/***/ }),
-/* 321 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var $export = __webpack_require__(0);
