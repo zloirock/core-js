dc74b386d2d8e1f21e9fadc743e880ccc8d4c42e
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-07 21:20:33 +0700

    remove `String#{escapeHTML, unescapeHTML}` (non-standard)

--- library/index.js
+++ library/index.js
@@ -6,6 +6,4 @@ require('./modules/core.is-iterable');
 require('./modules/core.function.part');
 require('./modules/core.object.is-object');
 require('./modules/core.object.classof');
-require('./modules/core.string.escape-html');
-require('./modules/core.string.unescape-html');
 module.exports = require('./modules/_core');
