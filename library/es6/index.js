b44c0b6431ea2d5e740486b14dcf513f24cb6934
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-07 13:48:49 +0700

    remove `Reflect.enumerate` (removed from the spec)

--- library/es6/index.js
+++ library/es6/index.js
@@ -125,7 +125,6 @@ require('../modules/es6.reflect.apply');
 require('../modules/es6.reflect.construct');
 require('../modules/es6.reflect.define-property');
 require('../modules/es6.reflect.delete-property');
-require('../modules/es6.reflect.enumerate');
 require('../modules/es6.reflect.get');
 require('../modules/es6.reflect.get-own-property-descriptor');
 require('../modules/es6.reflect.get-prototype-of');
