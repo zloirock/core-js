bb871868e50b6b28f477d5bfd9a990d62587735b
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-07 20:25:17 +0700

    finally remove `RegExp.escape` (rejected -> non-standard)

--- library/fn/regexp/index.js
+++ library/fn/regexp/index.js
@@ -5,5 +5,3 @@ require('../../modules/es6.regexp.match');
 require('../../modules/es6.regexp.replace');
 require('../../modules/es6.regexp.search');
 require('../../modules/es6.regexp.split');
-require('../../modules/core.regexp.escape');
-module.exports = require('../../modules/_core').RegExp;
