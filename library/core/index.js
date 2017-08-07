c110611ebee6d061f017a98aa58880f0368f1235
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-07 16:52:25 +0700

    remove `delay` (non-standard)

--- library/core/index.js
+++ library/core/index.js
@@ -2,7 +2,6 @@ require('../modules/core.dict');
 require('../modules/core.get-iterator-method');
 require('../modules/core.get-iterator');
 require('../modules/core.is-iterable');
-require('../modules/core.delay');
 require('../modules/core.function.part');
 require('../modules/core.object.is-object');
 require('../modules/core.object.classof');
