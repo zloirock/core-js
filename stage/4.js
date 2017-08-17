5844febe6e9457b0157db06255104f3eb0195580
Denis Pushkarev <zloirock@zloirock.ru>
2017-08-17 15:26:17 +0700

    move `Object.getOwnPropertyDescriptors` to the stable ES namespace

--- stage/4.js
+++ stage/4.js
@@ -2,7 +2,6 @@ require('../modules/esnext.object.define-getter');
 require('../modules/esnext.object.define-setter');
 require('../modules/esnext.object.lookup-getter');
 require('../modules/esnext.object.lookup-setter');
-require('../modules/esnext.object.get-own-property-descriptors');
 require('../modules/esnext.string.pad-start');
 require('../modules/esnext.string.pad-end');
 module.exports = require('../modules/_core');
