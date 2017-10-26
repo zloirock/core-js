60b58e3e1fc98abd5e67d8141735315792b6b697
Denis Pushkarev <zloirock@zloirock.ru>
2017-10-27 02:47:35 +0700

    rewrite Gruntfile from LiveScript to JavaScript

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,117 @@
-require('LiveScript');
-// eslint-disable-next-line import/no-unresolved
-module.exports = require('./build/Gruntfile');
+const build = require('./build');
+const fs = require('fs');
+const config = require('./build/config');
+module.exports = grunt => {
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-livescript');
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('./package.json'),
+    uglify: {
+      build: {
+        files: {
+          '<%=grunt.option("path")%>.min.js': '<%=grunt.option("path")%>.js'
+        },
+        options: {
+          mangle: {
+            keep_fnames: true,
+          },
+          compress: {
+            keep_fnames: true,
+            pure_getters: true,
+          },
+          output: {
+            max_line_len: 32000,
+          },
+          ie8: true,
+          sourceMap: true,
+          banner: config.banner,
+        },
+      },
+    },
+    livescript: {
+      src: {
+        files: {
+          './tests/helpers.js': './tests/helpers/*',
+          './tests/tests.js': './tests/tests/*',
+          './tests/library.js': './tests/library/*',
+          './build/index.js': './build/build.ls*',
+        },
+      },
+    },
+    clean: ['./library'],
+    copy: {
+      lib: {
+        files: [
+          {
+            expand: true,
+            cwd: './',
+            src: ['es/**', 'stage/**', 'web/**', 'core/**', 'fn/**', 'index.js'],
+            dest: './library/',
+          }, {
+            expand: true,
+            cwd: './',
+            src: ['modules/*'],
+            dest: './library/',
+            filter: 'isFile',
+          }, {
+            expand: true,
+            cwd: './modules/library/',
+            src: '*',
+            dest: './library/modules/',
+          },
+        ],
+      },
+    },
+    watch: {
+      core: {
+        files: './modules/*',
+        tasks: 'default',
+      },
+      tests: {
+        files: './tests/tests/*',
+        tasks: 'livescript',
+      },
+    },
+    karma: {
+      options: {
+        configFile: './tests/karma.conf.js',
+        browsers: ['PhantomJS'],
+        singleRun: true,
+      },
+      default: {},
+      library: {
+        files: ['client/library.js', 'tests/helpers.js', 'tests/library.js'].map(it => ({ src: it })),
+      },
+    },
+  });
+  grunt.registerTask('build', function (options) {
+    const done = this.async();
+    return build({
+      modules: (options || 'es,esnext,web,core').split(','),
+      blacklist: (grunt.option('blacklist') || '').split(','),
+      library: !!~['yes', 'on', 'true'].indexOf(grunt.option('library')),
+      umd: !~['no', 'off', 'false'].indexOf(grunt.option('umd')),
+    }).then(it => {
+      grunt.option('path') || grunt.option('path', './custom');
+      fs.writeFile(grunt.option('path') + '.js', it, done);
+    }).catch(it => {
+      console.error(it);
+      process.exit(1);
+    });
+  });
+  grunt.registerTask('client', () => {
+    grunt.option('library', '');
+    grunt.option('path', './client/core');
+    return grunt.task.run(['build:es,esnext,web', 'uglify']);
+  });
+  grunt.registerTask('library', () => {
+    grunt.option('library', 'true');
+    grunt.option('path', './client/library');
+    return grunt.task.run(['build:es,esnext,web,core', 'uglify']);
+  });
+  return grunt.registerTask('default', ['clean', 'copy', 'client', 'library']);
+};
--- library/stage/2.js
+++ library/stage/2.js
@@ -1,3 +1,5 @@
+require('../modules/esnext.array.flat-map');
+require('../modules/esnext.array.flatten');
 require('../modules/esnext.string.trim-left');
 require('../modules/esnext.string.trim-right');
 module.exports = require('./3');
