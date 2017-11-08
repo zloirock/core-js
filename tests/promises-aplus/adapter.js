ce7f90493fee3644440b5b82c77fd7c4e943fec6
Denis Pushkarev <zloirock@zloirock.ru>
2017-11-08 18:32:09 +0700

    improve `eslint` coverage, move tests bundles to `tests/bundles` directory

--- tests/promises-aplus/adapter.js
+++ tests/promises-aplus/adapter.js
@@ -1,26 +1,26 @@
 delete global.Promise;
 
 var Promise = require('../../').Promise;
-var assert  = require('assert')
+var assert = require('assert');
 
 module.exports = {
   deferred: function () {
-  	var deferred = {};
-  	deferred.promise = new Promise(function (resolve, reject) {
-  		deferred.resolve = resolve;
-  		deferred.reject  = reject;
-  	});
-  	return deferred;
+    var deferred = {};
+    deferred.promise = new Promise(function (resolve, reject) {
+      deferred.resolve = resolve;
+      deferred.reject = reject;
+    });
+    return deferred;
   },
   resolved: function (value) {
-	  return Promise.resolve(value);
+    return Promise.resolve(value);
   },
   rejected: function (reason) {
-	 return Promise.reject(reason);
+    return Promise.reject(reason);
   },
-  defineGlobalPromise: function (global ){
+  defineGlobalPromise: function (global) {
     global.Promise = Promise;
-    global.assert  = assert;
+    global.assert = assert;
   },
   removeGlobalPromise: function () {
     delete global.Promise;
