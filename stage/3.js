9012d8e60570ec3b0001afd758f9b15f82f397c1
Denis Pushkarev <zloirock@zloirock.ru>
2017-11-29 02:40:03 +0700

    move `Array#flatten` and `Array#flatMap` to the stage 3

--- stage/3.js
+++ stage/3.js
@@ -1,3 +1,5 @@
+require('../modules/esnext.array.flat-map');
+require('../modules/esnext.array.flatten');
 require('../modules/esnext.global');
 require('../modules/esnext.promise.finally');
 require('../modules/esnext.symbol.async-iterator');
