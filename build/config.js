cfd8a3202391a79c6588c06e22946b448ae46e63
Denis Pushkarev <zloirock@zloirock.ru>
2017-12-29 23:10:40 +0700

    remove UMD wrapper from builder, add some debugging info and links

--- build/config.js
+++ build/config.js
@@ -204,7 +204,7 @@ module.exports = {
           ' * core-js ' + require('../package').version + '\n' +
           ' * https://github.com/zloirock/core-js\n' +
           ' * License: http://rock.mit-license.org\n' +
-          ' * © ' + new Date().getFullYear() + ' Denis Pushkarev\n' +
+          ' * © ' + new Date().getFullYear() + ' Denis Pushkarev (zloirock.ru)\n' +
           ' */',
   /* eslint-enable prefer-template */
 };
