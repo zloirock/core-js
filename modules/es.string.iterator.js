c81d5f5114a1e4fb39828062e0ebdd3e84299970
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-05 23:19:44 +0700

    hide internal state of array and string iterators

--- modules/es.string.iterator.js
+++ modules/es.string.iterator.js
@@ -1,17 +1,21 @@
 'use strict';
 var $at = require('./_string-at')(true);
+var $ = require('./_state');
 
 // 21.1.3.27 String.prototype[@@iterator]()
 require('./_iter-define')(String, 'String', function (iterated) {
-  this._t = String(iterated); // target
-  this._i = 0;                // next index
+  $(this, {
+    target: String(iterated),
+    index: 0
+  });
 // 21.1.5.2.1 %StringIteratorPrototype%.next()
 }, function () {
-  var O = this._t;
-  var index = this._i;
+  var state = $(this);
+  var target = state.target;
+  var index = state.index;
   var point;
-  if (index >= O.length) return { value: undefined, done: true };
-  point = $at(O, index);
-  this._i += point.length;
+  if (index >= target.length) return { value: undefined, done: true };
+  point = $at(target, index);
+  state.index += point.length;
   return { value: point, done: false };
 });
