dfed83554f523a0f54696afe1bc560d2a44adfdf
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-12 04:15:49 +0700

    hide Function#toString wrapper data

--- modules/_redefine.js
+++ modules/_redefine.js
@@ -1,10 +1,10 @@
 var global = require('./_global');
 var hide = require('./_hide');
 var has = require('./_has');
-var SRC = require('./_uid')('src');
+var $ = require('./_state');
 var TO_STRING = 'toString';
 var $toString = Function[TO_STRING];
-var TPL = ('' + $toString).split(TO_STRING);
+var TEMPLATE = ('' + $toString).split(TO_STRING);
 
 require('./_shared')('inspectSource', function (it) {
   return $toString.call(it);
@@ -12,9 +12,9 @@ require('./_shared')('inspectSource', function (it) {
 
 (module.exports = function (O, key, val, safe) {
   var isFunction = typeof val == 'function';
-  if (isFunction) has(val, 'name') || hide(val, 'name', key);
+  if (isFunction && !has(val, 'name')) hide(val, 'name', key);
   if (O[key] === val) return;
-  if (isFunction) has(val, SRC) || hide(val, SRC, O[key] ? '' + O[key] : TPL.join(String(key)));
+  if (isFunction) $(val, true).source = has(O, key) ? String(O[key]) : TEMPLATE.join(String(key));
   if (O === global) {
     O[key] = val;
   } else if (!safe) {
@@ -27,5 +27,5 @@ require('./_shared')('inspectSource', function (it) {
   }
 // add fake Function#toString for correct work wrapped methods / constructors with methods like LoDash isNative
 })(Function.prototype, TO_STRING, function toString() {
-  return typeof this == 'function' && this[SRC] || $toString.call(this);
+  return typeof this == 'function' && $(this).source || $toString.call(this);
 });
