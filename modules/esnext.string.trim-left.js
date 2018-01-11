c2e2622380f1c0f385caf18d3f6978231d82fef2
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-11 17:48:19 +0700

    update `String#{trimLeft, trimRight}` links

--- modules/esnext.string.trim-left.js
+++ modules/esnext.string.trim-left.js
@@ -1,5 +1,5 @@
 'use strict';
-// https://github.com/sebmarkbage/ecmascript-string-left-right-trim
+// https://github.com/tc39/ecmascript-string-left-right-trim
 require('./_string-trim')('trimLeft', function ($trim) {
   return function trimLeft() {
     return $trim(this, 1);
