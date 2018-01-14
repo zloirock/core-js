6eb0de58e3f5c332b50325893e96ff11bef20fc0
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-15 03:29:18 +0700

    start refactoring of collections

--- modules/es.weak-map.js
+++ modules/es.weak-map.js
@@ -2,15 +2,11 @@
 var each = require('./_array-methods')(0);
 var redefine = require('./_redefine');
 var meta = require('./_meta');
-var assign = require('./_object-assign');
 var weak = require('./_collection-weak');
 var isObject = require('./_is-object');
 var fails = require('./_fails');
-var validate = require('./_validate-collection');
 var WEAK_MAP = 'WeakMap';
-var getWeak = meta.getWeak;
 var isExtensible = Object.isExtensible;
-var uncaughtFrozenStore = weak.ufstore;
 var tmp = {};
 var InternalMap;
 
@@ -20,28 +16,12 @@ var wrapper = function (get) {
   };
 };
 
-var methods = {
-  // 23.3.3.3 WeakMap.prototype.get(key)
-  get: function get(key) {
-    if (isObject(key)) {
-      var data = getWeak(key);
-      if (data === true) return uncaughtFrozenStore(validate(this, WEAK_MAP)).get(key);
-      return data ? data[this._i] : undefined;
-    }
-  },
-  // 23.3.3.5 WeakMap.prototype.set(key, value)
-  set: function set(key, value) {
-    return weak.def(validate(this, WEAK_MAP), key, value);
-  }
-};
-
 // 23.3 WeakMap Objects
-var $WeakMap = module.exports = require('./_collection')(WEAK_MAP, wrapper, methods, weak, true, true);
+var $WeakMap = module.exports = require('./_collection')(WEAK_MAP, wrapper, weak, true, true);
 
 // IE11 WeakMap frozen keys fix
 if (fails(function () { return new $WeakMap().set((Object.freeze || Object)(tmp), 7).get(tmp) != 7; })) {
   InternalMap = weak.getConstructor(wrapper, WEAK_MAP);
-  assign(InternalMap.prototype, methods);
   meta.NEED = true;
   each(['delete', 'has', 'get', 'set'], function (key) {
     var proto = $WeakMap.prototype;
