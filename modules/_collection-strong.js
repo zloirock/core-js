aed1d59d3190607f7d6343c0828b1a38a9e6fe12
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-15 03:29:18 +0700

    start refactoring of collections

--- modules/_collection-strong.js
+++ modules/_collection-strong.js
@@ -20,8 +20,8 @@ var getEntry = function (that, key) {
   var entry;
   if (index !== 'F') return that._i[index];
   // frozen object case
-  for (entry = that._f; entry; entry = entry.n) {
-    if (entry.k == key) return entry;
+  for (entry = that._f; entry; entry = entry.next) {
+    if (entry.key == key) return entry;
   }
 };
 
@@ -36,14 +36,39 @@ module.exports = {
       that[SIZE] = 0;         // size
       if (iterable != undefined) forOf(iterable, IS_MAP, that[ADDER], that);
     });
+
+    var define = function (that, key, value) {
+      var entry = getEntry(validate(that, NAME), key);
+      var prev, index;
+      // change existing entry
+      if (entry) {
+        entry.value = value;
+      // create new entry
+      } else {
+        that._l = entry = {
+          index: index = fastKey(key, true),
+          key: key,
+          value: value,
+          previous: prev = that._l,
+          next: undefined,
+          removed: false
+        };
+        if (!that._f) that._f = entry;
+        if (prev) prev.next = entry;
+        that[SIZE]++;
+        // add to index
+        if (index !== 'F') that._i[index] = entry;
+      } return that;
+    };
+
     redefineAll(C.prototype, {
       // 23.1.3.1 Map.prototype.clear()
       // 23.2.3.2 Set.prototype.clear()
       clear: function clear() {
-        for (var that = validate(this, NAME), data = that._i, entry = that._f; entry; entry = entry.n) {
-          entry.r = true;
-          if (entry.p) entry.p = entry.p.n = undefined;
-          delete data[entry.i];
+        for (var that = validate(this, NAME), data = that._i, entry = that._f; entry; entry = entry.next) {
+          entry.removed = true;
+          if (entry.previous) entry.previous = entry.previous.next = undefined;
+          delete data[entry.index];
         }
         that._f = that._l = undefined;
         that[SIZE] = 0;
@@ -54,12 +79,12 @@ module.exports = {
         var that = validate(this, NAME);
         var entry = getEntry(that, key);
         if (entry) {
-          var next = entry.n;
-          var prev = entry.p;
-          delete that._i[entry.i];
-          entry.r = true;
-          if (prev) prev.n = next;
-          if (next) next.p = prev;
+          var next = entry.next;
+          var prev = entry.previous;
+          delete that._i[entry.index];
+          entry.removed = true;
+          if (prev) prev.next = next;
+          if (next) next.previous = prev;
           if (that._f == entry) that._f = next;
           if (that._l == entry) that._l = prev;
           that[SIZE]--;
@@ -71,16 +96,32 @@ module.exports = {
         validate(this, NAME);
         var f = ctx(callbackfn, arguments.length > 1 ? arguments[1] : undefined, 3);
         var entry;
-        while (entry = entry ? entry.n : this._f) {
-          f(entry.v, entry.k, this);
+        while (entry = entry ? entry.next : this._f) {
+          f(entry.value, entry.key, this);
           // revert to the last existing entry
-          while (entry && entry.r) entry = entry.p;
+          while (entry && entry.removed) entry = entry.previous;
         }
       },
       // 23.1.3.7 Map.prototype.has(key)
       // 23.2.3.7 Set.prototype.has(value)
       has: function has(key) {
         return !!getEntry(validate(this, NAME), key);
+      }
+    });
+    redefineAll(C.prototype, IS_MAP ? {
+      // 23.1.3.6 Map.prototype.get(key)
+      get: function get(key) {
+        var entry = getEntry(validate(this, NAME), key);
+        return entry && entry.value;
+      },
+      // 23.1.3.9 Map.prototype.set(key, value)
+      set: function set(key, value) {
+        return define(this, key === 0 ? 0 : key, value);
+      }
+    } : {
+      // 23.2.3.1 Set.prototype.add(value)
+      add: function add(value) {
+        return define(this, value = value === 0 ? 0 : value, value);
       }
     });
     if (DESCRIPTORS) dP(C.prototype, 'size', {
@@ -90,30 +131,6 @@ module.exports = {
     });
     return C;
   },
-  def: function (that, key, value) {
-    var entry = getEntry(that, key);
-    var prev, index;
-    // change existing entry
-    if (entry) {
-      entry.v = value;
-    // create new entry
-    } else {
-      that._l = entry = {
-        i: index = fastKey(key, true), // <- index
-        k: key,                        // <- key
-        v: value,                      // <- value
-        p: prev = that._l,             // <- previous entry
-        n: undefined,                  // <- next entry
-        r: false                       // <- removed
-      };
-      if (!that._f) that._f = entry;
-      if (prev) prev.n = entry;
-      that[SIZE]++;
-      // add to index
-      if (index !== 'F') that._i[index] = entry;
-    } return that;
-  },
-  getEntry: getEntry,
   setStrong: function (C, NAME, IS_MAP) {
     // add .keys, .values, .entries, [@@iterator]
     // 23.1.3.4, 23.1.3.8, 23.1.3.11, 23.1.3.12, 23.2.3.5, 23.2.3.8, 23.2.3.10, 23.2.3.11
@@ -128,17 +145,17 @@ module.exports = {
       var kind = state.kind;
       var entry = state.last;
       // revert to the last existing entry
-      while (entry && entry.r) entry = entry.p;
+      while (entry && entry.removed) entry = entry.previous;
       // get next entry
-      if (!state.target || !(state.last = entry = entry ? entry.n : state.target._f)) {
+      if (!state.target || !(state.last = entry = entry ? entry.next : state.target._f)) {
         // or finish the iteration
         state.target = undefined;
         return step(1);
       }
       // return step by kind
-      if (kind == 'keys') return step(0, entry.k);
-      if (kind == 'values') return step(0, entry.v);
-      return step(0, [entry.k, entry.v]);
+      if (kind == 'keys') return step(0, entry.key);
+      if (kind == 'values') return step(0, entry.value);
+      return step(0, [entry.key, entry.value]);
     }, IS_MAP ? 'entries' : 'values', !IS_MAP, true);
 
     // add [@@species], 23.1.2.2, 23.2.2.2
