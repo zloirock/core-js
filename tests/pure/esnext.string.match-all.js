4d0bdf576c87cd8317197eed42af4d5977d3b7cb
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-19 05:23:21 +0700

    update `String#matchAll` proposal

--- tests/pure/esnext.string.match-all.js
+++ tests/pure/esnext.string.match-all.js
@@ -9,7 +9,7 @@ QUnit.test('String#matchAll', assert => {
     return 'aabc';
   } }];
   for (const target of data) {
-    const iterator = matchAll(target, /[ac]/);
+    const iterator = matchAll(target, /[ac]/g);
     assert.isIterator(iterator);
     assert.isIterable(iterator);
     assert.deepEqual(iterator.next(), {
@@ -38,7 +38,7 @@ QUnit.test('String#matchAll', assert => {
       done: true,
     });
   }
-  const iterator = matchAll('1111a2b3cccc', /(\d)(\D)/);
+  let iterator = matchAll('1111a2b3cccc', /(\d)(\D)/g);
   assert.isIterator(iterator);
   assert.isIterable(iterator);
   assert.deepEqual(iterator.next(), {
@@ -66,14 +66,75 @@ QUnit.test('String#matchAll', assert => {
     value: null,
     done: true,
   });
-  data = [null, undefined, 'qwe', NaN, 42, new Date(), {}, []];
+  iterator = matchAll('1111a2b3cccc', /(\d)(\D)/);
+  assert.isIterator(iterator);
+  assert.isIterable(iterator);
+  assert.deepEqual(iterator.next(), {
+    value: assign(['1a', '1', 'a'], {
+      input: '1111a2b3cccc',
+      index: 3,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: null,
+    done: true,
+  });
+  iterator = matchAll('1111a2b3cccc', '(\\d)(\\D)');
+  assert.isIterator(iterator);
+  assert.isIterable(iterator);
+  assert.deepEqual(iterator.next(), {
+    value: assign(['1a', '1', 'a'], {
+      input: '1111a2b3cccc',
+      index: 3,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: assign(['2b', '2', 'b'], {
+      input: '1111a2b3cccc',
+      index: 5,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: assign(['3c', '3', 'c'], {
+      input: '1111a2b3cccc',
+      index: 7,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: null,
+    done: true,
+  });
+  iterator = matchAll('abc', /\B/g);
+  assert.isIterator(iterator);
+  assert.isIterable(iterator);
+  assert.deepEqual(iterator.next(), {
+    value: assign([''], {
+      input: 'abc',
+      index: 1,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: assign([''], {
+      input: 'abc',
+      index: 2,
+    }),
+    done: false,
+  });
+  assert.deepEqual(iterator.next(), {
+    value: null,
+    done: true,
+  });
+  data = [null, undefined, NaN, 42, {}, []];
   for (const target of data) {
-    assert.throws(() => {
-      matchAll('', target);
-    }, TypeError, `Throws on ${ target } as first argument`);
+    assert.notThrows(() => matchAll('', target), `Not throws on ${ target } as the first argument`);
   }
   if (STRICT) {
-    assert.throws(() => matchAll(null, /./), TypeError, 'Throws on null as `this`');
-    assert.throws(() => matchAll(undefined, /./), TypeError, 'Throws on undefined as `this`');
+    assert.throws(() => matchAll(null, /./g), TypeError, 'Throws on null as `this`');
+    assert.throws(() => matchAll(undefined, /./g), TypeError, 'Throws on undefined as `this`');
   }
 });
