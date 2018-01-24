b46d45cad8a9afca364f6338b988ef280f6cbf62
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-25 05:24:07 +0700

    move `Promise#finally` to stage 4

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -1,7 +1,6 @@
 require('../modules/esnext.array.flat-map');
 require('../modules/esnext.array.flatten');
 require('../modules/esnext.global');
-require('../modules/esnext.promise.finally');
 require('../modules/esnext.symbol.async-iterator');
 
 module.exports = require('./4');
