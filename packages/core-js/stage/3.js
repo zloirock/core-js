01c8ca295456a306be68d8ad6b4d63185e4f78d1
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-25 05:30:04 +0700

    move `String#matchAll` to stage 3

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -1,6 +1,7 @@
 require('../modules/esnext.array.flat-map');
 require('../modules/esnext.array.flatten');
 require('../modules/esnext.global');
+require('../modules/esnext.string.match-all');
 require('../modules/esnext.symbol.async-iterator');
 
 module.exports = require('./4');
