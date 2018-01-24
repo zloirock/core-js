7682ec26a6fa36ebca3736a146cea54f96def037
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-25 05:32:03 +0700

    move `String#{trimLeft, trimRight}` to stage 3

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -2,6 +2,8 @@ require('../modules/esnext.array.flat-map');
 require('../modules/esnext.array.flatten');
 require('../modules/esnext.global');
 require('../modules/esnext.string.match-all');
+require('../modules/esnext.string.trim-left');
+require('../modules/esnext.string.trim-right');
 require('../modules/esnext.symbol.async-iterator');
 
 module.exports = require('./4');
