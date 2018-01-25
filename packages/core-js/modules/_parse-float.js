7d6e5e05b11566059e121e9c39115e0cef73f4d1
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-26 06:05:12 +0700

    fix the list of unicode whitespaces

--- packages/core-js/modules/_parse-float.js
+++ packages/core-js/modules/_parse-float.js
@@ -1,7 +1,8 @@
 var $parseFloat = require('core-js-internals/global').parseFloat;
 var $trim = require('./_string-trim').trim;
+var whitespaces = require('core-js-internals/whitespaces');
 
-module.exports = 1 / $parseFloat(require('./_string-ws') + '-0') !== -Infinity ? function parseFloat(str) {
+module.exports = 1 / $parseFloat(whitespaces + '-0') !== -Infinity ? function parseFloat(str) {
   var string = $trim(String(str), 3);
   var result = $parseFloat(string);
   return result === 0 && string.charAt(0) == '-' ? -0 : result;
