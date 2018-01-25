fd3dba733e83b7e086f8a84254643cb840f84e92
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-26 06:05:12 +0700

    fix the list of unicode whitespaces

--- tests/tests/esnext.string.trim-right.js
+++ tests/tests/esnext.string.trim-right.js
@@ -1,6 +1,4 @@
-import { STRICT } from '../helpers/constants';
-// eslint-disable-next-line max-len
-const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+import { STRICT, WHITESPACES } from '../helpers/constants';
 
 QUnit.test('String#trimRight', assert => {
   const { trimRight } = String.prototype;
@@ -10,7 +8,7 @@ QUnit.test('String#trimRight', assert => {
   assert.looksNative(trimRight);
   assert.nonEnumerable(String.prototype, 'trimRight');
   assert.strictEqual(' \n  q w e \n  '.trimRight(), ' \n  q w e', 'removes whitespaces at right side of string');
-  assert.strictEqual(whitespaces.trimRight(), '', 'removes all whitespaces');
+  assert.strictEqual(WHITESPACES.trimRight(), '', 'removes all whitespaces');
   assert.strictEqual('\u200b\u0085'.trimRight(), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimRight.call(null, 0), TypeError);
@@ -27,7 +25,7 @@ QUnit.test('String#trimEnd', assert => {
   assert.nonEnumerable(String.prototype, 'trimEnd');
   assert.same(trimEnd, String.prototype.trimRight, 'same #trimRight');
   assert.strictEqual(' \n  q w e \n  '.trimEnd(), ' \n  q w e', 'removes whitespaces at right side of string');
-  assert.strictEqual(whitespaces.trimEnd(), '', 'removes all whitespaces');
+  assert.strictEqual(WHITESPACES.trimEnd(), '', 'removes all whitespaces');
   assert.strictEqual('\u200b\u0085'.trimEnd(), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimEnd.call(null, 0), TypeError);
