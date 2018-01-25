dfea3eb5e7cc09ba265c35d03772cdcb06b9855c
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-26 06:05:12 +0700

    fix the list of unicode whitespaces

--- tests/pure/es.number.parse-int.js
+++ tests/pure/es.number.parse-int.js
@@ -1,3 +1,5 @@
+import { WHITESPACES } from '../helpers/constants';
+
 import parseInt from 'core-js-pure/fn/number/parse-int';
 
 QUnit.test('Number.parseInt', assert => {
@@ -11,13 +13,11 @@ QUnit.test('Number.parseInt', assert => {
     assert.same(parseInt(string), parseInt(string, 10), `default radix is 10: ${ string }`);
   }
   assert.same(parseInt('0x16'), parseInt('0x16', 16), 'default radix is 16: 0x16');
-  // eslint-disable-next-line max-len
-  const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
   assert.same(parseInt('  0x16'), parseInt('0x16', 16), 'ignores leading whitespace #1');
   assert.same(parseInt('  42'), parseInt('42', 10), 'ignores leading whitespace #2');
   assert.same(parseInt('  08'), parseInt('08', 10), 'ignores leading whitespace #3');
-  assert.same(parseInt(`${ whitespaces }08`), parseInt('08', 10), 'ignores leading whitespace #4');
-  assert.same(parseInt(`${ whitespaces }0x16`), parseInt('0x16', 16), 'ignores leading whitespace #5');
+  assert.same(parseInt(`${ WHITESPACES }08`), parseInt('08', 10), 'ignores leading whitespace #4');
+  assert.same(parseInt(`${ WHITESPACES }0x16`), parseInt('0x16', 16), 'ignores leading whitespace #5');
   const fakeZero = {
     valueOf() {
       return 0;
--- tests/tests/esnext.string.trim-left.js
+++ tests/tests/esnext.string.trim-left.js
@@ -1,6 +1,4 @@
-import { STRICT } from '../helpers/constants';
-// eslint-disable-next-line max-len
-const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+import { STRICT, WHITESPACES } from '../helpers/constants';
 
 QUnit.test('String#trimLeft', assert => {
   const { trimLeft } = String.prototype;
@@ -10,7 +8,7 @@ QUnit.test('String#trimLeft', assert => {
   assert.looksNative(trimLeft);
   assert.nonEnumerable(String.prototype, 'trimLeft');
   assert.strictEqual(' \n  q w e \n  '.trimLeft(), 'q w e \n  ', 'removes whitespaces at left side of string');
-  assert.strictEqual(whitespaces.trimLeft(), '', 'removes all whitespaces');
+  assert.strictEqual(WHITESPACES.trimLeft(), '', 'removes all whitespaces');
   assert.strictEqual('\u200b\u0085'.trimLeft(), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimLeft.call(null, 0), TypeError);
@@ -27,7 +25,7 @@ QUnit.test('String#trimStart', assert => {
   assert.nonEnumerable(String.prototype, 'trimStart');
   assert.same(trimStart, String.prototype.trimLeft, 'same #trimLeft');
   assert.strictEqual(' \n  q w e \n  '.trimStart(), 'q w e \n  ', 'removes whitespaces at left side of string');
-  assert.strictEqual(whitespaces.trimStart(), '', 'removes all whitespaces');
+  assert.strictEqual(WHITESPACES.trimStart(), '', 'removes all whitespaces');
   assert.strictEqual('\u200b\u0085'.trimStart(), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimStart.call(null, 0), TypeError);
