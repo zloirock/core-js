c4d177df9c99fc551619ba1322e72c4519b80498
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-26 06:05:12 +0700

    fix the list of unicode whitespaces

--- tests/pure/es.parse-float.js
+++ tests/pure/es.parse-float.js
@@ -1,3 +1,4 @@
+import { WHITESPACES } from '../helpers/constants';
 import parseFloat from 'core-js-pure/fn/parse-float';
 
 QUnit.test('parseFloat', assert => {
@@ -9,10 +10,8 @@ QUnit.test('parseFloat', assert => {
   assert.same(parseFloat(' +0'), 0);
   assert.same(parseFloat('-0'), -0);
   assert.same(parseFloat(' -0'), -0);
-  // eslint-disable-next-line max-len
-  const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
-  assert.same(parseFloat(`${ whitespaces }+0`), 0);
-  assert.same(parseFloat(`${ whitespaces }-0`), -0);
+  assert.same(parseFloat(`${ WHITESPACES }+0`), 0);
+  assert.same(parseFloat(`${ WHITESPACES }-0`), -0);
   assert.same(parseFloat(null), NaN);
   assert.same(parseFloat(undefined), NaN);
 });
