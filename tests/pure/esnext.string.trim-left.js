469408545e9cc5c2dd6b89722334ac8cf6601541
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-26 06:05:12 +0700

    fix the list of unicode whitespaces

--- tests/pure/esnext.string.trim-left.js
+++ tests/pure/esnext.string.trim-left.js
@@ -1,14 +1,11 @@
-import { STRICT } from '../helpers/constants';
+import { STRICT, WHITESPACES } from '../helpers/constants';
 
 import { trimLeft, trimStart } from 'core-js-pure/fn/string';
-
-// eslint-disable-next-line max-len
-const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
 
 QUnit.test('String#trimLeft', assert => {
   assert.isFunction(trimLeft);
   assert.strictEqual(trimLeft(' \n  q w e \n  '), 'q w e \n  ', 'removes whitespaces at left side of string');
-  assert.strictEqual(trimLeft(whitespaces), '', 'removes all whitespaces');
+  assert.strictEqual(trimLeft(WHITESPACES), '', 'removes all whitespaces');
   assert.strictEqual(trimLeft('\u200b\u0085'), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimLeft(null, 0), TypeError);
@@ -19,7 +16,7 @@ QUnit.test('String#trimLeft', assert => {
 QUnit.test('String#trimStart', assert => {
   assert.isFunction(trimStart);
   assert.strictEqual(trimStart(' \n  q w e \n  '), 'q w e \n  ', 'removes whitespaces at left side of string');
-  assert.strictEqual(trimStart(whitespaces), '', 'removes all whitespaces');
+  assert.strictEqual(trimStart(WHITESPACES), '', 'removes all whitespaces');
   assert.strictEqual(trimStart('\u200b\u0085'), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trimStart(null, 0), TypeError);
--- tests/tests/es.string.trim.js
+++ tests/tests/es.string.trim.js
@@ -1,7 +1,4 @@
-import { STRICT } from '../helpers/constants';
-
-// eslint-disable-next-line max-len
-const whitespaces = '\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
+import { STRICT, WHITESPACES } from '../helpers/constants';
 
 QUnit.test('String#trim', assert => {
   const { trim } = String.prototype;
@@ -11,7 +8,7 @@ QUnit.test('String#trim', assert => {
   assert.looksNative(trim);
   assert.nonEnumerable(String.prototype, 'trim');
   assert.strictEqual(' \n  q w e \n  '.trim(), 'q w e', 'removes whitespaces at left & right side of string');
-  assert.strictEqual(whitespaces.trim(), '', 'removes all whitespaces');
+  assert.strictEqual(WHITESPACES.trim(), '', 'removes all whitespaces');
   assert.strictEqual('\u200b\u0085'.trim(), '\u200b\u0085', "shouldn't remove this symbols");
   if (STRICT) {
     assert.throws(() => trim.call(null, 0), TypeError);
