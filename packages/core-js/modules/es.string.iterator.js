6e84d26f8f7a79ced6233903f6cb47a1637d83a0
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-29 04:31:40 +0700

    add `String#codePoints` proposal
    https://github.com/RReverser/string-prototype-codepoints

--- packages/core-js/modules/es.string.iterator.js
+++ packages/core-js/modules/es.string.iterator.js
@@ -5,17 +5,17 @@ var $ = require('./_state');
 // 21.1.3.27 String.prototype[@@iterator]()
 require('./_iter-define')(String, 'String', function (iterated) {
   $(this, {
-    target: String(iterated),
+    string: String(iterated),
     index: 0
   });
 // 21.1.5.2.1 %StringIteratorPrototype%.next()
-}, function () {
+}, function next() {
   var state = $(this);
-  var target = state.target;
+  var string = state.string;
   var index = state.index;
   var point;
-  if (index >= target.length) return { value: undefined, done: true };
-  point = $at(target, index);
+  if (index >= string.length) return { value: undefined, done: true };
+  point = $at(string, index);
   state.index += point.length;
   return { value: point, done: false };
 });
