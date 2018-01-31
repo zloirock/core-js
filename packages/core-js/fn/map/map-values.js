ee63c700b273851d11c21e1c967fd5dc8d42a770
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-01 06:56:55 +0700

    add `Set#{addAll, deleteAll}`
    https://github.com/Ginden/collection-methods

--- packages/core-js/fn/map/map-values.js
+++ packages/core-js/fn/map/map-values.js
@@ -1,4 +1,4 @@
 require('../../modules/es.map');
 require('../../modules/esnext.map.map-values');
 
-module.exports = require('../../modules/_entry-unbind')('Map', 'map-values');
+module.exports = require('../../modules/_entry-unbind')('Map', 'mapValues');
