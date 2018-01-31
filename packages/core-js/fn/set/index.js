9d33315ae3b113b1d2f9e9e1087ecc51e72d4d0d
Denis Pushkarev <zloirock@zloirock.ru>
2018-01-31 07:09:14 +0700

    add `Set#{every, some}`
    https://github.com/Ginden/collection-methods

--- packages/core-js/fn/set/index.js
+++ packages/core-js/fn/set/index.js
@@ -4,9 +4,11 @@ require('../../modules/web.dom-collections.iterator');
 require('../../modules/es.set');
 require('../../modules/esnext.set.of');
 require('../../modules/esnext.set.from');
+require('../../modules/esnext.set.every');
 require('../../modules/esnext.set.filter');
 require('../../modules/esnext.set.find');
 require('../../modules/esnext.set.join');
 require('../../modules/esnext.set.map');
+require('../../modules/esnext.set.some');
 
 module.exports = require('../../modules/_path').Set;
