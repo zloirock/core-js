189b09749baf921ab9c73778e32f75ce8043fdcb
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-02 05:18:49 +0700

    add `Map.{groupBy, keyBy}`
    https://github.com/Ginden/collection-methods

--- packages/core-js/fn/map/index.js
+++ packages/core-js/fn/map/index.js
@@ -2,11 +2,13 @@ require('../../modules/es.object.to-string');
 require('../../modules/es.string.iterator');
 require('../../modules/web.dom-collections.iterator');
 require('../../modules/es.map');
-require('../../modules/esnext.map.of');
-require('../../modules/esnext.map.from');
 require('../../modules/esnext.map.filter');
+require('../../modules/esnext.map.from');
+require('../../modules/esnext.map.group-by');
+require('../../modules/esnext.map.key-by');
 require('../../modules/esnext.map.map-keys');
 require('../../modules/esnext.map.map-values');
 require('../../modules/esnext.map.merge');
+require('../../modules/esnext.map.of');
 
 module.exports = require('../../modules/_path').Map;
---
+++ tests/pure/esnext.map.group-by.js
@@ -0,0 +1,24 @@
+import { createIterable } from '../helpers/helpers';
+
+import Map from 'core-js-pure/fn/map';
+import toArray from 'core-js-pure/fn/array/from';
+
+QUnit.test('Map.groupBy', assert => {
+  const { groupBy } = Map;
+
+  assert.isFunction(groupBy);
+  assert.arity(groupBy, 2);
+  if ('name' in groupBy) assert.name(groupBy, 'groupBy');
+
+  assert.ok(Map.groupBy([], it => it) instanceof Map);
+
+  assert.deepEqual(toArray(Map.groupBy([], it => it)), []);
+  assert.deepEqual(toArray(Map.groupBy([1, 2], it => it ** 2)), [[1, [1]], [4, [2]]]);
+  assert.deepEqual(toArray(Map.groupBy([1, 2, 1], it => it ** 2)), [[1, [1, 1]], [4, [2]]]);
+  assert.deepEqual(toArray(Map.groupBy(createIterable([1, 2]), it => it ** 2)), [[1, [1]], [4, [2]]]);
+
+  const element = {};
+  Map.groupBy([element], it => assert.same(it, element));
+
+  assert.throws(() => groupBy([1, 2], it => it));
+});
