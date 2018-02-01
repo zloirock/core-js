a2c310611d95f86372a235e1535872a9f5338c4f
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-02 05:18:49 +0700

    add `Map.{groupBy, keyBy}`
    https://github.com/Ginden/collection-methods

---
+++ packages/core-js/modules/esnext.map.key-by.js
@@ -0,0 +1,13 @@
+'use strict';
+var iterate = require('./_iterate');
+
+// https://github.com/Ginden/collection-methods
+require('./_export')({ target: 'Map', stat: true, forced: require('./_is-pure') }, {
+  keyBy: function keyBy(iterable, keyDerivative) {
+    var newMap = new this();
+    iterate(iterable, false, function (element) {
+      newMap.set(keyDerivative(element), element);
+    });
+    return newMap;
+  }
+});
