6506e8fe511b8824ce3dc516c626b6c0037cc244
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-02 05:18:49 +0700

    add `Map.{groupBy, keyBy}`
    https://github.com/Ginden/collection-methods

--- packages/core-js/fn/set/index.js
+++ packages/core-js/fn/set/index.js
@@ -2,15 +2,15 @@ require('../../modules/es.object.to-string');
 require('../../modules/es.string.iterator');
 require('../../modules/web.dom-collections.iterator');
 require('../../modules/es.set');
-require('../../modules/esnext.set.of');
-require('../../modules/esnext.set.from');
 require('../../modules/esnext.set.add-all');
 require('../../modules/esnext.set.delete-all');
 require('../../modules/esnext.set.every');
 require('../../modules/esnext.set.filter');
 require('../../modules/esnext.set.find');
+require('../../modules/esnext.set.from');
 require('../../modules/esnext.set.join');
 require('../../modules/esnext.set.map');
+require('../../modules/esnext.set.of');
 require('../../modules/esnext.set.reduce');
 require('../../modules/esnext.set.some');
 
---
+++ packages/core-js/modules/esnext.map.group-by.js
@@ -0,0 +1,15 @@
+'use strict';
+var iterate = require('./_iterate');
+
+// https://github.com/Ginden/collection-methods
+require('./_export')({ target: 'Map', stat: true, forced: require('./_is-pure') }, {
+  groupBy: function groupBy(iterable, keyDerivative) {
+    var newMap = new this();
+    iterate(iterable, false, function (element) {
+      var derivedKey = keyDerivative(element);
+      if (!newMap.has(derivedKey)) newMap.set(derivedKey, [element]);
+      else newMap.get(derivedKey).push(element);
+    });
+    return newMap;
+  }
+});
