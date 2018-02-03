e0128effda559991530f66c96664a64374bf1067
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-03 07:34:28 +0700

    add `Set#{difference, symmetricDifference}`
    https://github.com/tc39/set-methods

---
+++ tests/pure/esnext.set.difference.js
@@ -0,0 +1,26 @@
+import { createIterable } from '../helpers/helpers';
+
+import Set from 'core-js-pure/fn/set';
+import from from 'core-js-pure/fn/array/from';
+
+QUnit.test('Set#difference', assert => {
+  const { difference } = Set.prototype;
+
+  assert.isFunction(difference);
+  assert.arity(difference, 1);
+  if ('name' in difference) assert.name(difference, 'difference');
+  assert.nonEnumerable(Set.prototype, 'difference');
+
+  const set = new Set([1]);
+  assert.ok(set.difference([2]) !== set);
+
+  assert.deepEqual(from(new Set([1, 2, 3]).difference([4, 5])), [1, 2, 3]);
+  assert.deepEqual(from(new Set([1, 2, 3]).difference([3, 4])), [1, 2]);
+  assert.deepEqual(from(new Set([1, 2, 3]).difference(createIterable([3, 4]))), [1, 2]);
+
+  assert.throws(() => new Set([1, 2, 3]).difference(), TypeError);
+
+  assert.throws(() => difference.call({}, [1, 2, 3]), TypeError);
+  assert.throws(() => difference.call(undefined, [1, 2, 3]), TypeError);
+  assert.throws(() => difference.call(null, [1, 2, 3]), TypeError);
+});
