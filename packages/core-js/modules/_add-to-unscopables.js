4a4925acca579ed8b8322dbdf3adc63e30f964b7
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-05 07:35:11 +0700

    minor fix of `Array#@@unscopables`

--- packages/core-js/modules/_add-to-unscopables.js
+++ packages/core-js/modules/_add-to-unscopables.js
@@ -1,7 +1,11 @@
-// 22.1.3.31 Array.prototype[@@unscopables]
 var UNSCOPABLES = require('core-js-internals/well-known-symbol')('unscopables');
+var ObjectCreate = require('./_object-create');
 var ArrayProto = Array.prototype;
-if (ArrayProto[UNSCOPABLES] == undefined) require('./_hide')(ArrayProto, UNSCOPABLES, {});
+
+if (ArrayProto[UNSCOPABLES] == undefined) require('./_hide')(ArrayProto, UNSCOPABLES, ObjectCreate(null));
+
+// Array.prototype[@@unscopables]
+// https://tc39.github.io/ecma262/#sec-array.prototype-@@unscopables
 module.exports = function (key) {
   ArrayProto[UNSCOPABLES][key] = true;
 };
