010bca3674ab9be37da62ca9307e2db9d27a7529
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-14 06:39:11 +0700

    some stylistic changes

--- packages/core-js/modules/es.string.includes.js
+++ packages/core-js/modules/es.string.includes.js
@@ -1,11 +1,11 @@
 'use strict';
 var context = require('../internals/string-context');
 var INCLUDES = 'includes';
-var FAILS_IS_REGEXP = require('../internals/fails-is-regexp')(INCLUDES);
+var CORRECT_IS_REGEXP_LOGIC = require('../internals/correct-is-regexp-logic')(INCLUDES);
 
 // `String.prototype.includes` method
 // https://tc39.github.io/ecma262/#sec-string.prototype.includes
-require('../internals/export')({ target: 'String', proto: true, forced: FAILS_IS_REGEXP }, {
+require('../internals/export')({ target: 'String', proto: true, forced: !CORRECT_IS_REGEXP_LOGIC }, {
   includes: function includes(searchString /* , position = 0 */) {
     return !!~context(this, searchString, INCLUDES)
       .indexOf(searchString, arguments.length > 1 ? arguments[1] : undefined);
--- packages/core-js/modules/web.dom-collections.for-each.js
+++ packages/core-js/modules/web.dom-collections.for-each.js
@@ -3,8 +3,10 @@ var forEach = require('../internals/array-for-each');
 var redefine = require('../internals/redefine');
 var global = require('../internals/global');
 
-for (var NAME in DOMIterables) {
-  var Collection = global[NAME];
-  var proto = Collection && Collection.prototype;
-  if (proto && !proto.forEach) redefine(proto, 'forEach', forEach);
+for (var COLLECTION_NAME in DOMIterables) {
+  var Collection = global[COLLECTION_NAME];
+  var CollectionPrototype = Collection && Collection.prototype;
+  if (CollectionPrototype && !CollectionPrototype.forEach) {
+    redefine(CollectionPrototype, 'forEach', forEach);
+  }
 }
