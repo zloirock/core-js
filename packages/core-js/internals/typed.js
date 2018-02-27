590a9880f6b65735c409585a8b03ce4c982d004b
Denis Pushkarev <zloirock@zloirock.ru>
2018-02-27 12:35:49 +0700

    some stylistic changes, start refactoring of typed arrays

--- packages/core-js/internals/typed.js
+++ packages/core-js/internals/typed.js
@@ -1,26 +1,15 @@
 var global = require('../internals/global');
 var hide = require('../internals/hide');
 var uid = require('../internals/uid');
+var TypedArrayConstructorsList = require('../internals/typed-array-constructors-list');
 var TYPED = uid('typed_array');
 var VIEW = uid('view');
 var ABV = !!(global.ArrayBuffer && global.DataView);
 var CONSTR = ABV;
 var Typed;
 
-var TypedArrayConstructors = {
-  Int8Array: 1,
-  Uint8Array: 1,
-  Uint8ClampedArray: 1,
-  Int16Array: 1,
-  Uint16Array: 1,
-  Int32Array: 1,
-  Uint32Array: 1,
-  Float32Array: 1,
-  Float64Array: 1
-};
-
-for (var name in TypedArrayConstructors) {
-  if (Typed = global[name]) {
+for (var NAME in TypedArrayConstructorsList) {
+  if (Typed = global[NAME]) {
     hide(Typed.prototype, TYPED, true);
     hide(Typed.prototype, VIEW, true);
   } else CONSTR = false;
