bbb5ea2846565e0c6c39835a457319acafecd7b8
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-14 23:12:39 +0700

    refactoring of typed arrays, extract some methods to separate modules, fix prototype chain

--- packages/core-js/modules/es.typed-array.reverse.js
+++ packages/core-js/modules/es.typed-array.reverse.js
@@ -2,6 +2,8 @@
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 
+// `%TypedArray%.prototype.reverse` method
+// https://tc39.github.io/ecma262/#sec-%typedarray%.prototype.reverse
 ArrayBufferViewCore.exportProto('reverse', function reverse() {
   var that = this;
   var length = aTypedArray(that).length;
