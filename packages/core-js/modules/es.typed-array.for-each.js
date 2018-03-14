f7d44a3e7a81ee0e99ccdbf0d17c6f54a6ced63c
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-14 23:12:39 +0700

    refactoring of typed arrays, extract some methods to separate modules, fix prototype chain

--- packages/core-js/modules/es.typed-array.for-each.js
+++ packages/core-js/modules/es.typed-array.for-each.js
@@ -3,6 +3,8 @@ var arrayForEach = require('../internals/array-methods')(0);
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 
+// `%TypedArray%.prototype.forEach` method
+// https://tc39.github.io/ecma262/#sec-%typedarray%.prototype.foreach
 ArrayBufferViewCore.exportProto('forEach', function forEach(callbackfn /* , thisArg */) {
   arrayForEach(aTypedArray(this), callbackfn, arguments.length > 1 ? arguments[1] : undefined);
 });
---
+++ packages/core-js/modules/es.typed-array.from.js
@@ -0,0 +1,7 @@
+'use strict';
+var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
+var typedArrayFrom = require('../internals/typed-array-from');
+
+// `%TypedArray%.from` method
+// https://tc39.github.io/ecma262/#sec-%typedarray%.from
+ArrayBufferViewCore.exportStatic('from', typedArrayFrom);
