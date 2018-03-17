acdd5f76e3399c21119cb440eda69f35ed22a296
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-18 02:11:37 +0700

    refactoring of typed arrays

--- packages/core-js/modules/es.typed-array.from.js
+++ packages/core-js/modules/es.typed-array.from.js
@@ -2,6 +2,11 @@
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
 var typedArrayFrom = require('../internals/typed-array-from');
 
+var FORCED = require('../internals/fails')(function () {
+  // eslint-disable-next-line no-undef
+  Int8Array.from([1]);
+});
+
 // `%TypedArray%.from` method
 // https://tc39.github.io/ecma262/#sec-%typedarray%.from
-ArrayBufferViewCore.exportStatic('from', typedArrayFrom);
+ArrayBufferViewCore.exportStatic('from', typedArrayFrom, FORCED);
