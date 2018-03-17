5c9d67ba1f1c7589b2d7cee351836954e1999e8f
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-18 02:11:37 +0700

    refactoring of typed arrays

--- packages/core-js/features/typed/uint32-array.js
+++
@@ -1,3 +0,0 @@
-require('../../modules/es.typed.uint32-array');
-
-module.exports = require('../../internals/path').Uint32Array;
--- packages/core-js/features/typed/uint8-array.js
+++
@@ -1,3 +0,0 @@
-require('../../modules/es.typed.uint8-array');
-
-module.exports = require('../../internals/path').Uint8Array;
