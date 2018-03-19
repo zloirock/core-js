7a3b95833e53fc1f25c97a05cd8bd00775c1014d
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-20 05:09:30 +0700

    rename `Array#end` -> `Array#lastItem`, add `Array#lastIndex`
    per
    https://github.com/keithamus/proposal-array-last/commit/dcfb925af6e6acfb
    bb77e36a1f48d00eb2190b20 and
    https://github.com/keithamus/proposal-array-last/commit/c5139f6b1e8c6db4
    1c9b74a986ca692181172dac

--- packages/core-js/features/array/index.js
+++ packages/core-js/features/array/index.js
@@ -21,7 +21,8 @@ require('../../modules/es.array.find-index');
 require('../../modules/es.array.species');
 require('../../modules/es.array.iterator');
 require('../../modules/es.array.includes');
-require('../../modules/esnext.array.end');
+require('../../modules/esnext.array.last-item');
+require('../../modules/esnext.array.last-index');
 require('../../modules/esnext.array.flat-map');
 require('../../modules/esnext.array.flatten');
 
