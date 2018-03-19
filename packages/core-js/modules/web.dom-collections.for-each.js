36f944592020edec0a2f975867bc721515bff51c
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-20 05:51:01 +0700

    DOM collections iterator methods should be the same with `Array` methods, Chrome 57- issue

--- packages/core-js/modules/web.dom-collections.for-each.js
+++ packages/core-js/modules/web.dom-collections.for-each.js
@@ -1,12 +1,12 @@
 var DOMIterables = require('../internals/dom-iterables');
 var forEach = require('../internals/array-for-each');
-var redefine = require('../internals/redefine');
+var hide = require('../internals/hide');
 var global = require('../internals/global');
 
 for (var COLLECTION_NAME in DOMIterables) {
   var Collection = global[COLLECTION_NAME];
   var CollectionPrototype = Collection && Collection.prototype;
-  if (CollectionPrototype && !CollectionPrototype.forEach) {
-    redefine(CollectionPrototype, 'forEach', forEach);
+  if (CollectionPrototype && CollectionPrototype.forEach !== forEach) {
+    hide(CollectionPrototype, 'forEach', forEach);
   }
 }
