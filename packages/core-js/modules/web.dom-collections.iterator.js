6d03a730941e509fa382d5dd8f6d7f251bda8a15
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-20 05:51:01 +0700

    DOM collections iterator methods should be the same with `Array` methods, Chrome 57- issue

--- packages/core-js/modules/web.dom-collections.iterator.js
+++ packages/core-js/modules/web.dom-collections.iterator.js
@@ -1,24 +1,24 @@
 var DOMIterables = require('../internals/dom-iterables');
 var ArrayIteratorMethods = require('../modules/es.array.iterator');
-var redefine = require('../internals/redefine');
 var global = require('../internals/global');
 var hide = require('../internals/hide');
-var Iterators = require('../internals/iterators');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 var ITERATOR = wellKnownSymbol('iterator');
 var TO_STRING_TAG = wellKnownSymbol('toStringTag');
-var ArrayValues = Iterators.Array;
+var ArrayValues = ArrayIteratorMethods.values;
 
 for (var COLLECTION_NAME in DOMIterables) {
   var Collection = global[COLLECTION_NAME];
   var CollectionPrototype = Collection && Collection.prototype;
   if (CollectionPrototype) {
-    if (!CollectionPrototype[ITERATOR]) hide(CollectionPrototype, ITERATOR, ArrayValues);
+    // some Chrome versions die on an attempt to redefine iterator, for example, on DOMTokenList
+    if (CollectionPrototype[ITERATOR] !== ArrayValues) try {
+      hide(CollectionPrototype, ITERATOR, ArrayValues);
+    } catch (e) { /* empty */ }
     if (!CollectionPrototype[TO_STRING_TAG]) hide(CollectionPrototype, TO_STRING_TAG, COLLECTION_NAME);
-    Iterators[COLLECTION_NAME] = ArrayValues;
     if (DOMIterables[COLLECTION_NAME]) for (var METHOD_NAME in ArrayIteratorMethods) {
-      if (!CollectionPrototype[METHOD_NAME]) {
-        redefine(CollectionPrototype, METHOD_NAME, ArrayIteratorMethods[METHOD_NAME], true);
+      if (CollectionPrototype[METHOD_NAME] !== ArrayIteratorMethods[METHOD_NAME]) {
+        hide(CollectionPrototype, METHOD_NAME, ArrayIteratorMethods[METHOD_NAME], true);
       }
     }
   }
