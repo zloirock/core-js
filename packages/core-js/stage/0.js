4b1b648cbdbb0f04dea67d8abaf11e23543794ce
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-20 22:46:14 +0700

    move `Object.fromEntries` to stage 1

--- packages/core-js/stage/0.js
+++ packages/core-js/stage/0.js
@@ -3,7 +3,6 @@ require('../modules/esnext.math.iaddh');
 require('../modules/esnext.math.isubh');
 require('../modules/esnext.math.imulh');
 require('../modules/esnext.math.umulh');
-require('../modules/esnext.object.from-entries');
 require('../modules/esnext.string.at');
 
 module.exports = require('./1');
