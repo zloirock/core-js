a011dd8f07c01084c007eee0e0002489cc900266
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-21 04:19:51 +0700

    some `sham` flag fixes for the `pure` version

--- packages/core-js/features/object/define-properties.js
+++ packages/core-js/features/object/define-properties.js
@@ -1,6 +1,8 @@
 require('../../modules/es.object.define-properties');
 var $Object = require('../../internals/path').Object;
 
-module.exports = function defineProperties(T, D) {
+var defineProperties = module.exports = function defineProperties(T, D) {
   return $Object.defineProperties(T, D);
 };
+
+if ($Object.defineProperties.sham) defineProperties.sham = true;
