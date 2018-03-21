75b3d6ed005f948c67ab799bcf6ea6ec5bf444ba
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-21 16:10:17 +0700

    improve accuracy  of `Math,cosh`, FF45- fix

--- packages/core-js/modules/es.math.cosh.js
+++ packages/core-js/modules/es.math.cosh.js
@@ -1,9 +1,13 @@
-var exp = Math.exp;
+var expm1 = require('../internals/math-expm1');
+var nativeCosh = Math.cosh;
+var abs = Math.abs;
+var E = Math.E;
 
 // `Math.cosh` method
 // https://tc39.github.io/ecma262/#sec-math.cosh
-require('../internals/export')({ target: 'Math', stat: true }, {
+require('../internals/export')({ target: 'Math', stat: true, forced: !nativeCosh || nativeCosh(710) === Infinity }, {
   cosh: function cosh(x) {
-    return (exp(x = +x) + exp(-x)) / 2;
+    var t = expm1(abs(x) - 1) + 1;
+    return (t + 1 / (t * E * E)) * (E / 2);
   }
 });
