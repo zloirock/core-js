69518b239fca6008eb0cff08230164574aef548f
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-21 16:10:17 +0700

    improve accuracy  of `Math,cosh`, FF45- fix

--- tests/pure/es.math.cosh.js
+++ tests/pure/es.math.cosh.js
@@ -7,8 +7,9 @@ QUnit.test('Math.cosh', assert => {
   assert.strictEqual(cosh(-0), 1);
   assert.strictEqual(cosh(Infinity), Infinity);
   assert.strictEqual(cosh(-Infinity), Infinity);
-  assert.epsilon(cosh(12), 81377.39571257407, 3e-11);
+  assert.epsilon(cosh(12), 81377.395712574, 1e-9);
   assert.epsilon(cosh(22), 1792456423.065795780980053377, 1e-5);
   assert.epsilon(cosh(-10), 11013.23292010332313972137);
   assert.epsilon(cosh(-23), 4872401723.1244513000, 1e-5);
+  assert.epsilon(cosh(710), 1.1169973830808557e+308, 1e+295);
 });
