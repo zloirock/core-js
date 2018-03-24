12a6444db1cbae551856b33feaf965cabff76c01
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-24 17:41:43 +0700

    refactoring of iterators definition, use native `%IteratorPrototype%` where it's possible, fix an issue related own `%IteratorPrototype%` `@@iterator` method

--- packages/core-js/modules/es.string.iterator.js
+++ packages/core-js/modules/es.string.iterator.js
@@ -1,13 +1,14 @@
 'use strict';
 var at = require('../internals/string-at')(true);
 var InternalStateModule = require('../internals/internal-state');
+var defineIterator = require('../internals/iterators-core').defineIterator;
 var STRING_ITERATOR = 'StringIterator';
 var setInternalState = InternalStateModule.set;
 var getInternalState = InternalStateModule.getterFor(STRING_ITERATOR);
 
 // `String.prototype[@@iterator]` method
 // https://tc39.github.io/ecma262/#sec-string.prototype-@@iterator
-require('../internals/define-iterator')(String, 'String', function (iterated) {
+defineIterator(String, 'String', function (iterated) {
   setInternalState(this, {
     type: STRING_ITERATOR,
     string: String(iterated),
