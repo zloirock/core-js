95b00290d0c793deff0b73319d8db037a71a2305
Denis Pushkarev <zloirock@zloirock.ru>
2018-03-27 05:28:36 +0700

    add `Symbol.{patternMatch, patternValue}` for updated pattern matching proposal
    https://github.com/tc39/proposal-pattern-matching

--- packages/core-js/features/symbol/index.js
+++ packages/core-js/features/symbol/index.js
@@ -3,5 +3,7 @@ require('../../modules/es.object.to-string');
 require('../../modules/es.symbol.async-iterator');
 require('../../modules/esnext.symbol.description');
 require('../../modules/esnext.symbol.observable');
+require('../../modules/esnext.symbol.pattern-match');
+require('../../modules/esnext.symbol.pattern-value');
 
 module.exports = require('../../internals/path').Symbol;
