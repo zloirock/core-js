74672189f0ffe45f13c16ba14b2602047dc9421a
Denis Pushkarev <zloirock@zloirock.ru>
2018-04-02 01:32:01 +0700

    minor stylistic changes in collections, hide internal state in the pure version of collections

--- packages/core-js/internals/collection.js
+++ packages/core-js/internals/collection.js
@@ -12,73 +12,84 @@ var setToStringTag = require('../internals/set-to-string-tag');
 var inheritIfRequired = require('../internals/inherit-if-required');
 
 module.exports = function (NAME, wrapper, common, IS_MAP, IS_WEAK) {
-  var Base = global[NAME];
-  var C = Base;
+  var NativeConstructor = global[NAME];
+  var NativePrototype = NativeConstructor && NativeConstructor.prototype;
+  var Constructor = NativeConstructor;
   var ADDER = IS_MAP ? 'set' : 'add';
-  var proto = C && C.prototype;
   var exported = {};
+
   var fixMethod = function (KEY) {
-    var fn = proto[KEY];
-    redefine(proto, KEY,
-      KEY == 'delete' ? function (a) {
-        return IS_WEAK && !isObject(a) ? false : fn.call(this, a === 0 ? 0 : a);
-      } : KEY == 'has' ? function has(a) {
-        return IS_WEAK && !isObject(a) ? false : fn.call(this, a === 0 ? 0 : a);
+    var nativeMethod = NativePrototype[KEY];
+    redefine(NativePrototype, KEY,
+      KEY == 'add' ? function add(a) {
+        nativeMethod.call(this, a === 0 ? 0 : a);
+        return this;
+      } : KEY == 'delete' ? function (a) {
+        return IS_WEAK && !isObject(a) ? false : nativeMethod.call(this, a === 0 ? 0 : a);
       } : KEY == 'get' ? function get(a) {
-        return IS_WEAK && !isObject(a) ? undefined : fn.call(this, a === 0 ? 0 : a);
-      } : KEY == 'add' ? function add(a) { fn.call(this, a === 0 ? 0 : a); return this; }
-        : function set(a, b) { fn.call(this, a === 0 ? 0 : a, b); return this; }
+        return IS_WEAK && !isObject(a) ? undefined : nativeMethod.call(this, a === 0 ? 0 : a);
+      } : KEY == 'has' ? function has(a) {
+        return IS_WEAK && !isObject(a) ? false : nativeMethod.call(this, a === 0 ? 0 : a);
+      } : function set(a, b) {
+        nativeMethod.call(this, a === 0 ? 0 : a, b);
+        return this;
+      }
     );
   };
-  if (typeof C != 'function' || !(IS_WEAK || proto.forEach && !fails(function () {
-    new C().entries().next();
+
+  if (typeof NativeConstructor != 'function' || !(IS_WEAK || NativePrototype.forEach && !fails(function () {
+    new NativeConstructor().entries().next();
   }))) {
     // create collection constructor
-    C = common.getConstructor(wrapper, NAME, IS_MAP, ADDER);
+    Constructor = common.getConstructor(wrapper, NAME, IS_MAP, ADDER);
     InternalMetadataModule.REQUIRED = true;
   } else {
-    var instance = new C();
+    var instance = new Constructor();
     // early implementations not supports chaining
     var HASNT_CHAINING = instance[ADDER](IS_WEAK ? {} : -0, 1) != instance;
     // V8 ~  Chromium 40- weak-collections throws on primitives, but should return false
     var THROWS_ON_PRIMITIVES = fails(function () { instance.has(1); });
     // most early implementations doesn't supports iterables, most modern - not close it correctly
     // eslint-disable-next-line no-new
-    var ACCEPT_ITERABLES = checkCorrectnessOfIteration(function (iterable) { new C(iterable); });
+    var ACCEPT_ITERABLES = checkCorrectnessOfIteration(function (iterable) { new NativeConstructor(iterable); });
     // for early implementations -0 and +0 not the same
     var BUGGY_ZERO = !IS_WEAK && fails(function () {
       // V8 ~ Chromium 42- fails only with 5+ elements
-      var $instance = new C();
+      var $instance = new NativeConstructor();
       var index = 5;
       while (index--) $instance[ADDER](index, index);
       return !$instance.has(-0);
     });
+
     if (!ACCEPT_ITERABLES) {
-      C = wrapper(function (target, iterable) {
-        anInstance(target, C, NAME);
-        var that = inheritIfRequired(new Base(), target, C);
+      Constructor = wrapper(function (target, iterable) {
+        anInstance(target, Constructor, NAME);
+        var that = inheritIfRequired(new NativeConstructor(), target, Constructor);
         if (iterable != undefined) iterate(iterable, that[ADDER], that, IS_MAP);
         return that;
       });
-      C.prototype = proto;
-      proto.constructor = C;
+      Constructor.prototype = NativePrototype;
+      NativePrototype.constructor = Constructor;
     }
+
     if (THROWS_ON_PRIMITIVES || BUGGY_ZERO) {
       fixMethod('delete');
       fixMethod('has');
       IS_MAP && fixMethod('get');
     }
+
     if (BUGGY_ZERO || HASNT_CHAINING) fixMethod(ADDER);
+
     // weak collections should not contains .clear method
-    if (IS_WEAK && proto.clear) delete proto.clear;
+    if (IS_WEAK && NativePrototype.clear) delete NativePrototype.clear;
   }
 
-  setToStringTag(C, NAME);
+  exported[NAME] = Constructor;
+  $export({ global: true, forced: Constructor != NativeConstructor }, exported);
 
-  exported[NAME] = C;
-  $export({ global: true, wrap: true, forced: C != Base }, exported);
+  setToStringTag(Constructor, NAME);
 
-  if (!IS_WEAK) common.setStrong(C, NAME, IS_MAP);
+  if (!IS_WEAK) common.setStrong(Constructor, NAME, IS_MAP);
 
-  return C;
+  return Constructor;
 };
