db8a7ea46ad6d316141bda99d13cecaa3d804b77
Denis Pushkarev <zloirock@zloirock.ru>
2018-04-03 16:20:18 +0700

    minor stylistic changes in `Promise`, prevent `Promise.prototype` pollution in some cases

--- packages/core-js/modules/es.promise.finally.js
+++ packages/core-js/modules/es.promise.finally.js
@@ -1,11 +1,11 @@
-// `Promise#finally` method
-// https://tc39.github.io/ecma262/#sec-promise.prototype.finally
 'use strict';
 var path = require('../internals/path');
 var global = require('../internals/global');
 var speciesConstructor = require('../internals/species-constructor');
 var promiseResolve = require('../internals/promise-resolve');
 
+// `Promise.prototype.finally` method
+// https://tc39.github.io/ecma262/#sec-promise.prototype.finally
 require('../internals/export')({ target: 'Promise', proto: true, real: true }, { 'finally': function (onFinally) {
   var C = speciesConstructor(this, typeof path.Promise == 'function' ? path.Promise : global.Promise);
   var isFunction = typeof onFinally == 'function';
