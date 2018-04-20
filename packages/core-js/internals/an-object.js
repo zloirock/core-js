bc401d2878ad2e58fda28600cf493e18a6bb01b6
Denis Pushkarev <zloirock@zloirock.ru>
2018-04-21 01:12:31 +0700

    use `String` constructor in conversion error arguments for prevent possible problems with symbols

--- packages/core-js/internals/an-object.js
+++ packages/core-js/internals/an-object.js
@@ -1,6 +1,6 @@
 var isObject = require('../internals/is-object');
 
 module.exports = function (it) {
-  if (!isObject(it)) throw TypeError(it + ' is not an object!');
+  if (!isObject(it)) throw TypeError(String(it) + ' is not an object!');
   return it;
 };
