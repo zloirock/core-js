f24b006bf647e6b4f852f87877ab69d78bacb6be
Denis Pushkarev <zloirock@zloirock.ru>
2018-05-05 19:50:19 +0700

    update `.name` properties of `String#{trimStart, trimEnd , trimLeft, trimRight}` by proposal, mark mongolian vowel separator as non-whitespace in one more place

--- packages/core-js/modules/esnext.string.trim-right.js
+++ packages/core-js/modules/esnext.string.trim-right.js
@@ -1,8 +1,8 @@
 'use strict';
 // `String.prototype.{ trimRight, trimEnd }` methods
 // https://github.com/tc39/ecmascript-string-left-right-trim
-require('../internals/string-trim')('trimRight', function (internalTrim) {
-  return function trimRight() {
+require('../internals/string-trim')('trimEnd', function (internalTrim) {
+  return function trimEnd() {
     return internalTrim(this, 2);
   };
-}, 'trimEnd');
+}, 'trimRight');
