99d12cf64b99a9cff9cd7bcd8264620ca678c8d4
Denis Pushkarev <zloirock@zloirock.ru>
2018-05-08 00:23:38 +0700

    add a comment about LG WebOS 2 `Promise.resolve` issue

--- packages/core-js/internals/microtask.js
+++ packages/core-js/internals/microtask.js
@@ -41,6 +41,7 @@ module.exports = function () {
     };
   // environments with maybe non-completely correct, but existent Promise
   } else if (Promise && Promise.resolve) {
+    // Promise.resolve without an argument throws an error in LG WebOS 2
     var promise = Promise.resolve(undefined);
     notify = function () {
       promise.then(flush);
