bce9800d9a592f20e517b7cc74a8c0601e88a7bc
Denis Pushkarev <zloirock@zloirock.ru>
2018-11-14 03:51:20 +0700

    add a fallback for rhino bug, fix #440

--- packages/core-js/internals/indexed-object.js
+++ packages/core-js/internals/indexed-object.js
@@ -1,7 +1,12 @@
 // fallback for non-array-like ES3 and non-enumerable old V8 strings
+var fails = require('../internals/fails');
 var classof = require('../internals/classof-raw');
 var split = ''.split;
-// eslint-disable-next-line no-prototype-builtins
-module.exports = Object('z').propertyIsEnumerable(0) ? Object : function (it) {
+
+module.exports = fails(function () {
+  // throws an error in rhino, see https://github.com/mozilla/rhino/issues/346
+  // eslint-disable-next-line no-prototype-builtins
+  return !Object('z').propertyIsEnumerable(0);
+}) ? function (it) {
   return classof(it) == 'String' ? split.call(it, '') : Object(it);
-};
+} : Object;
