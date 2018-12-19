1cd5c84909e99de6fd6c1294d4411124295052bd
Denis Pushkarev <zloirock@zloirock.ru>
2018-12-19 18:12:19 +0700

    fix a typo in `Math.sinh` feature detection

--- packages/core-js/modules/es.math.sinh.js
+++ packages/core-js/modules/es.math.sinh.js
@@ -7,7 +7,7 @@ var E = Math.E;
 // https://tc39.github.io/ecma262/#sec-math.sinh
 // V8 near Chromium 38 has a problem with very small numbers
 require('../internals/export')({ target: 'Math', stat: true, forced: require('../internals/fails')(function () {
-  return !Math.sinh(-2e-17) != -2e-17;
+  return Math.sinh(-2e-17) != -2e-17;
 }) }, {
   sinh: function sinh(x) {
     return abs(x = +x) < 1 ? (expm1(x) - expm1(-x)) / 2 : (exp(x - 1) - exp(-x - 1)) * (E / 2);
