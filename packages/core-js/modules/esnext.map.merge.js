71ae9d24f1f2c8838418a82cf8c46a934bf5fb97
Denis Pushkarev <zloirock@zloirock.ru>
2019-01-10 10:19:35 +0700

    update `Map.merge` per https://github.com/tc39/proposal-collection-methods/commit/7da035b43418abf1b48550cd12032aff779111be

--- packages/core-js/modules/esnext.map.merge.js
+++ packages/core-js/modules/esnext.map.merge.js
@@ -1,19 +1,19 @@
 'use strict';
 var anObject = require('../internals/an-object');
 var aFunction = require('../internals/a-function');
-var speciesConstructor = require('../internals/species-constructor');
-var Map = require('../internals/path').Map;
 var iterate = require('../internals/iterate');
 
 // `Map.prototype.merge` method
 // https://github.com/tc39/collection-methods
 require('../internals/export')({ target: 'Map', proto: true, real: true, forced: require('../internals/is-pure') }, {
-  merge: function merge(iterable) {
+  // eslint-disable-next-line no-unused-vars
+  merge: function merge(iterable /* ...iterbles */) {
     var map = anObject(this);
-    var newMap = new (speciesConstructor(map, Map))();
-    var setter = aFunction(newMap.set);
-    iterate(map, setter, newMap, true);
-    iterate(iterable, setter, newMap, true);
-    return newMap;
+    var setter = aFunction(map.set);
+    var i = 0;
+    while (i < arguments.length) {
+      iterate(arguments[i++], setter, map, true);
+    }
+    return map;
   }
 });
--- tests/pure/esnext.map.merge.js
+++ tests/pure/esnext.map.merge.js
@@ -11,12 +11,14 @@ QUnit.test('Map#merge', assert => {
 
   const map = new Map([[1, 2]]);
   const result = map.merge([[3, 4]]);
-  assert.ok(result !== map);
+  assert.ok(result === map);
   assert.ok(result instanceof Map);
 
   assert.deepEqual(from(new Map([[1, 2], [3, 4]]).merge([[5, 6]])), [[1, 2], [3, 4], [5, 6]]);
   assert.deepEqual(from(new Map([[1, 2], [3, 4]]).merge([[3, 5], [5, 6]])), [[1, 2], [3, 5], [5, 6]]);
   assert.deepEqual(from(new Map([[1, 2], [3, 4]]).merge([])), [[1, 2], [3, 4]]);
+
+  assert.deepEqual(from(new Map([[1, 2], [3, 4]]).merge([[3, 5]], [[5, 6]])), [[1, 2], [3, 5], [5, 6]]);
 
   assert.throws(() => merge.call({}, [[1, 2]]), TypeError);
   assert.throws(() => merge.call(undefined, [[1, 2]]), TypeError);
