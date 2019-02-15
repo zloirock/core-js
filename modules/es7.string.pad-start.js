990ae2feda576cf6bbea7d0440c68f562413485f
Denis Pushkarev <zloirock@zloirock.ru>
2019-02-15 16:57:23 +0700

    fix buggy String#padStart and String#padEnd mobile Safari implementations, backport of https://github.com/zloirock/core-js/commit/16dbccf66f0400e16d6bcb3943dddfdedd98fe66

--- modules/es7.string.pad-start.js
+++ modules/es7.string.pad-start.js
@@ -5,7 +5,9 @@ var $pad = require('./_string-pad');
 var userAgent = require('./_user-agent');
 
 // https://github.com/zloirock/core-js/issues/280
-$export($export.P + $export.F * /Version\/10\.\d+(\.\d+)? Safari\//.test(userAgent), 'String', {
+var WEBKIT_BUG = /Version\/10\.\d+(\.\d+)?( Mobile\/\w+)? Safari\//.test(userAgent);
+
+$export($export.P + $export.F * WEBKIT_BUG, 'String', {
   padStart: function padStart(maxLength /* , fillString = ' ' */) {
     return $pad(this, maxLength, arguments.length > 1 ? arguments[1] : undefined, true);
   }
