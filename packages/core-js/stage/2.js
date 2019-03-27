14d6d89611ed0be8a358c6f33746ddf5a69d1772
Denis Pushkarev <zloirock@zloirock.ru>
2019-03-27 16:58:44 +0700

    move `Promise.allSettled` to stage 3
    per https://github.com/babel/proposals/issues/57#issuecomment-476837985

--- packages/core-js/stage/2.js
+++ packages/core-js/stage/2.js
@@ -1,4 +1,3 @@
 require('../proposals/set-methods');
-require('../proposals/promise-all-settled');
 
 module.exports = require('./3');
--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -1,4 +1,5 @@
 require('../proposals/global-this');
 require('../proposals/string-match-all');
+require('../proposals/promise-all-settled');
 
 module.exports = require('./4');
