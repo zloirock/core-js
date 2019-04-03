f813c4e3d436e56b6d3f28446e47219e8dcbdaae
Denis Pushkarev <zloirock@zloirock.ru>
2019-04-03 11:08:01 +0700

    add a workaround for rollup issue, fix #513

--- packages/core-js/internals/native-symbol.js
+++ packages/core-js/internals/native-symbol.js
@@ -1,5 +1,5 @@
 // Chrome 38 Symbol has incorrect toString conversion
 module.exports = !require('../internals/fails')(function () {
   // eslint-disable-next-line no-undef
-  String(Symbol());
+  return !String(Symbol());
 });
