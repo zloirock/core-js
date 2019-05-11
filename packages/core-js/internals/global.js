499a8bfc377da44a09b7352c89a0fcce8bac0d46
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-11 16:11:33 +0700

    just in case check `globalThis` and `global` before `Function` constructor usage in global object detection

--- packages/core-js/internals/global.js
+++ packages/core-js/internals/global.js
@@ -1,5 +1,14 @@
+var O = 'object';
+var check = function (it) {
+  return it && it.Math == Math && it;
+};
+
 // https://github.com/zloirock/core-js/issues/86#issuecomment-115759028
-module.exports = typeof window == 'object' && window && window.Math == Math ? window
-  : typeof self == 'object' && self && self.Math == Math ? self
+module.exports =
+  // eslint-disable-next-line no-undef
+  check(typeof globalThis == O && globalThis) ||
+  check(typeof window == O && window) ||
+  check(typeof self == O && self) ||
+  check(typeof global == O && global) ||
   // eslint-disable-next-line no-new-func
-  : Function('return this')();
+  Function('return this')();
