1d77ba0225c34520bd2e691ff61f16e76b30f16d
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-19 17:45:25 +0700

    show a message on postinstall

---
+++ scripts/postinstall.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console,max-len */
+var env = process.env;
+var CI = !!env.CI && env.CI !== '0' && env.CI !== 'false';
+var SILENT = !!~['silent', 'error', 'warn'].indexOf(env.npm_config_loglevel);
+
+if (!CI && !SILENT) {
+  console.log('\u001B[96mThank you for using core-js (\u001B[94mhttps://github.com/zloirock/core-js\u001B[96m)!\u001B[0m\n');
+  console.log('\u001B[96mPlease consider supporting of core-js on Open Collective or Patreon:\u001B[0m');
+  console.log('\u001B[96m> \u001B[94mhttps://opencollective.com/core-js\u001B[0m');
+  console.log('\u001B[96m> \u001B[94mhttps://www.patreon.com/zloirock\u001B[0m\n');
+  console.log('\u001B[96mAlso, the author of core-js (\u001B[94mhttps://github.com/zloirock\u001B[96m) is looking for a good job -)\u001B[0m\n');
+}
