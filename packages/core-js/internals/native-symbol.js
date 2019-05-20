08d2f566558f7fdc30f4fed0cc39abb19bbb8e9a
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-20 18:44:21 +0700

    add an additional check of native `Symbol`

--- packages/core-js/internals/native-symbol.js
+++ packages/core-js/internals/native-symbol.js
@@ -1,7 +1,7 @@
 var fails = require('../internals/fails');
 
-// Chrome 38 Symbol has incorrect toString conversion
-module.exports = !fails(function () {
+module.exports = !!Object.getOwnPropertySymbols && !fails(function () {
+  // Chrome 38 Symbol has incorrect toString conversion
   // eslint-disable-next-line no-undef
   return !String(Symbol());
 });
