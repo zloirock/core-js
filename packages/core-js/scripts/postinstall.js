f2eb6fa6f388a27fc1784d869e99da416d874ddb
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-23 04:16:35 +0700

    improve `postinstall` message

--- packages/core-js/scripts/postinstall.js
+++ packages/core-js/scripts/postinstall.js
@@ -4,7 +4,7 @@ var CI = !!env.CI && env.CI !== '0' && env.CI !== 'false';
 var SILENT = !!~['silent', 'error', 'warn'].indexOf(env.npm_config_loglevel);
 
 if (!CI && !SILENT) {
-  console.log('\u001B[96mThank you for using core-js (\u001B[94m https://github.com/zloirock/core-js \u001B[96m)!\u001B[0m\n');
+  console.log('\u001B[96mThank you for using core-js (\u001B[94m https://github.com/zloirock/core-js \u001B[96m) for polyfilling JavaScript standard library!\u001B[0m\n');
   console.log('\u001B[96mPlease consider supporting of core-js on Open Collective or Patreon: \u001B[0m');
   console.log('\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m');
   console.log('\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n');
