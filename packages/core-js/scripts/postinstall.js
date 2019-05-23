7fd1c97a3cc8afc0645d6b603a0322f7ecf0e31a
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-24 04:19:02 +0700

    add support of `ADBLOCK` env variable, https://github.com/zloirock/core-js/issues/548#issuecomment-495388335

--- packages/core-js/scripts/postinstall.js
+++ packages/core-js/scripts/postinstall.js
@@ -1,5 +1,6 @@
 /* eslint-disable max-len */
 var env = process.env;
+var ADBLOCK = is(env.ADBLOCK);
 var CI = is(env.CI);
 var COLOR = is(env.npm_config_color);
 var SILENT = !!~['silent', 'error', 'warn'].indexOf(env.npm_config_loglevel);
@@ -13,7 +14,7 @@ function log(it) {
   console.log(COLOR ? it : it.replace(/\u001B\[\d+m/g, ''));
 }
 
-if (!CI && !SILENT) {
+if (!ADBLOCK && !CI && !SILENT) {
   log('\u001B[96mThank you for using core-js (\u001B[94m https://github.com/zloirock/core-js \u001B[96m) for polyfilling JavaScript standard library!\u001B[0m\n');
   log('\u001B[96mPlease consider supporting of core-js on Open Collective or Patreon: \u001B[0m');
   log('\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m');
