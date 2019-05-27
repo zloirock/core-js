8a108bd8cb33847aed8f100df7ab34d0a5177a83
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-27 19:16:11 +0700

    update the postinstall message

--- packages/core-js/scripts/postinstall.js
+++ packages/core-js/scripts/postinstall.js
@@ -16,7 +16,7 @@ function log(it) {
 
 if (!ADBLOCK && !CI && !SILENT) {
   log('\u001B[96mThank you for using core-js (\u001B[94m https://github.com/zloirock/core-js \u001B[96m) for polyfilling JavaScript standard library!\u001B[0m\n');
-  log('\u001B[96mPlease consider supporting of core-js on Open Collective or Patreon: \u001B[0m');
+  log('\u001B[96mThe project needs your help! Please consider supporting of core-js on Open Collective or Patreon: \u001B[0m');
   log('\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m');
   log('\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n');
   log('\u001B[96mAlso, the author of core-js (\u001B[94m https://github.com/zloirock \u001B[96m) is looking for a good job -)\u001B[0m\n');
