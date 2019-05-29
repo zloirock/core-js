11f025d294c09147d7cea672fe4011aa477c69f3
Denis Pushkarev <zloirock@zloirock.ru>
2019-05-29 17:04:47 +0700

    use `string-pad` helper in `Date#toISOString`

--- packages/core-js/internals/date-to-iso-string.js
+++ packages/core-js/internals/date-to-iso-string.js
@@ -1,13 +1,10 @@
 'use strict';
 var fails = require('../internals/fails');
+var padStart = require('../internals/string-pad').start;
 
 var prototype = Date.prototype;
 var getTime = prototype.getTime;
 var nativeDateToISOString = prototype.toISOString;
-
-var leadingZero = function (number) {
-  return number > 9 ? number : '0' + number;
-};
 
 // `Date.prototype.toISOString` method implementation
 // https://tc39.github.io/ecma262/#sec-date.prototype.toisostring
@@ -22,12 +19,12 @@ module.exports = (fails(function () {
   var year = date.getUTCFullYear();
   var milliseconds = date.getUTCMilliseconds();
   var sign = year < 0 ? '-' : year > 9999 ? '+' : '';
-  return sign + ('00000' + Math.abs(year)).slice(sign ? -6 : -4) +
-    '-' + leadingZero(date.getUTCMonth() + 1) +
-    '-' + leadingZero(date.getUTCDate()) +
-    'T' + leadingZero(date.getUTCHours()) +
-    ':' + leadingZero(date.getUTCMinutes()) +
-    ':' + leadingZero(date.getUTCSeconds()) +
-    '.' + (milliseconds > 99 ? milliseconds : '0' + leadingZero(milliseconds)) +
+  return sign + padStart(Math.abs(year), sign ? 6 : 4, 0) +
+    '-' + padStart(date.getUTCMonth() + 1, 2, 0) +
+    '-' + padStart(date.getUTCDate(), 2, 0) +
+    'T' + padStart(date.getUTCHours(), 2, 0) +
+    ':' + padStart(date.getUTCMinutes(), 2, 0) +
+    ':' + padStart(date.getUTCSeconds(), 2, 0) +
+    '.' + padStart(milliseconds, 3, 0) +
     'Z';
 } : nativeDateToISOString;
