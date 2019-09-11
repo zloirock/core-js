96c97208e8a27ae9d9858384c3ae8a9c709a014a
Sergey Aksenov <saksenov@fun.co>
2019-09-11 18:25:28 +0300

    WIP: Work for Fun Corp? :)

--- packages/core-js/postinstall.js
+++ packages/core-js/postinstall.js
@@ -14,8 +14,7 @@ var MINUTE = 60 * 1000;
 var BANNER = '\u001B[96mThank you for using core-js (\u001B[94m https://github.com/zloirock/core-js \u001B[96m) for polyfilling JavaScript standard library!\u001B[0m\n\n' +
              '\u001B[96mThe project needs your help! Please consider supporting of core-js on Open Collective or Patreon: \u001B[0m\n' +
              '\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m\n' +
-             '\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n\n' +
-             '\u001B[96mAlso, the author of core-js (\u001B[94m https://github.com/zloirock \u001B[96m) is looking for a good job -)\u001B[0m\n';
+             '\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n';
 
 function is(it) {
   return !!it && it !== '0' && it !== 'false';
