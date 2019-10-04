43c9fafed4f85cb3ba463c30a6368ad57123f8fe
Denis Pushkarev <zloirock@zloirock.ru>
2019-10-04 07:53:15 +0700

    make `AggregateError.prototype.errors` accessor property
    https://github.com/tc39/proposal-promise-any/pull/43

--- packages/core-js/modules/esnext.aggregate-error.js
+++ packages/core-js/modules/esnext.aggregate-error.js
@@ -1,11 +1,18 @@
+'use strict';
 var $ = require('../internals/export');
+var DESCRIPTORS = require('../internals/descriptors');
 var getPrototypeOf = require('../internals/object-get-prototype-of');
 var setPrototypeOf = require('../internals/object-set-prototype-of');
 var create = require('../internals/object-create');
+var defineProperty = require('../internals/object-define-property');
 var createPropertyDescriptor = require('../internals/create-property-descriptor');
 var iterate = require('../internals/iterate');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var anObject = require('../internals/an-object');
+var InternalStateModule = require('../internals/internal-state');
+
+var setInternalState = InternalStateModule.set;
+var getInternalAggregateErrorState = InternalStateModule.getterFor('AggregateError');
 
 var $AggregateError = function AggregateError(errors, message) {
   var that = this;
@@ -15,7 +22,8 @@ var $AggregateError = function AggregateError(errors, message) {
   }
   var errorsArray = [];
   iterate(errors, errorsArray.push, errorsArray);
-  createNonEnumerableProperty(that, 'errors', errorsArray);
+  if (DESCRIPTORS) setInternalState(that, { errors: errorsArray, type: 'AggregateError' });
+  else that.errors = errorsArray;
   if (message !== undefined) createNonEnumerableProperty(that, 'message', String(message));
   return that;
 };
@@ -31,6 +39,13 @@ $AggregateError.prototype = create(Error.prototype, {
     message = message === undefined ? '' : String(message);
     return name + ': ' + message;
   })
+});
+
+if (DESCRIPTORS) defineProperty.f($AggregateError.prototype, 'errors', {
+  get: function () {
+    return getInternalAggregateErrorState(this).errors;
+  },
+  configurable: true
 });
 
 $({ global: true }, {
