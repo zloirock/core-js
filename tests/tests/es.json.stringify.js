bac46f5f34e66682b2316bde9a3c6db3fe3147be
Denis Pushkarev <zloirock@zloirock.ru>
2019-10-30 03:40:43 +0700

    add some more tests

--- tests/tests/es.json.stringify.js
+++ tests/tests/es.json.stringify.js
@@ -13,5 +13,9 @@ if (GLOBAL.JSON) {
     assert.same(stringify('\uDF06\uD834'), '"\\udf06\\ud834"', 'r2');
     assert.same(stringify('\uDF06ab\uD834'), '"\\udf06ab\\ud834"', 'r3');
     assert.same(stringify('𠮷'), '"𠮷"', 'r4');
+    assert.same(stringify('\uD834\uDF06'), '"𝌆"', 'r5');
+    assert.same(stringify('\uD834\uD834\uDF06'), '"\\ud834𝌆"', 'r6');
+    assert.same(stringify('\uD834\uDF06\uDF06'), '"𝌆\\udf06"', 'r7');
+    assert.same(stringify({ '𠮷': ['\uDF06\uD834'] }), '{"𠮷":["\\udf06\\ud834"]}', 'r8');
   });
 }
