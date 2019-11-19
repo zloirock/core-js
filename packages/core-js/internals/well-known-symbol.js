79a973f1832f1f9a98f6d27c480015e10dcb17f2
Denis Pushkarev <zloirock@zloirock.ru>
2019-11-19 19:01:19 +0700

    refactor `well-known-symbol` helper

--- packages/core-js/internals/well-known-symbol.js
+++ packages/core-js/internals/well-known-symbol.js
@@ -1,13 +1,17 @@
 var global = require('../internals/global');
 var shared = require('../internals/shared');
+var has = require('../internals/has');
 var uid = require('../internals/uid');
 var NATIVE_SYMBOL = require('../internals/native-symbol');
 var USE_SYMBOL_AS_UID = require('../internals/use-symbol-as-uid');
 
+var WellKnownSymbolsStore = shared('wks');
 var Symbol = global.Symbol;
-var store = shared('wks');
+var createWellKnownSymbol = USE_SYMBOL_AS_UID ? Symbol : uid;
 
 module.exports = function (name) {
-  return store[name] || (store[name] = NATIVE_SYMBOL && Symbol[name]
-    || (USE_SYMBOL_AS_UID ? Symbol : uid)('Symbol.' + name));
+  if (!has(WellKnownSymbolsStore, name)) {
+    if (NATIVE_SYMBOL && has(Symbol, name)) WellKnownSymbolsStore[name] = Symbol[name];
+    else WellKnownSymbolsStore[name] = createWellKnownSymbol('Symbol.' + name);
+  } return WellKnownSymbolsStore[name];
 };
