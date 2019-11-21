6e4153a8d220419d916e712b80a7fd96df34463f
Emanuel Kluge <kraemerseele@gmx.de>
2019-11-21 11:07:00 +0100

    Fix smiley in postinstall message

--- packages/core-js/postinstall.js
+++ packages/core-js/postinstall.js
@@ -15,7 +15,7 @@ var BANNER = '\u001B[96mThank you for using core-js (\u001B[94m https://github.c
              '\u001B[96mThe project needs your help! Please consider supporting of core-js on Open Collective or Patreon: \u001B[0m\n' +
              '\u001B[96m>\u001B[94m https://opencollective.com/core-js \u001B[0m\n' +
              '\u001B[96m>\u001B[94m https://www.patreon.com/zloirock \u001B[0m\n\n' +
-             '\u001B[96mAlso, the author of core-js (\u001B[94m https://github.com/zloirock \u001B[96m) is looking for a good job -)\u001B[0m\n';
+             '\u001B[96mAlso, the author of core-js (\u001B[94m https://github.com/zloirock \u001B[96m) is looking for a good job :-)\u001B[0m\n';
 
 function is(it) {
   return !!it && it !== '0' && it !== 'false';
