80616a049e0a9773e3b388e3e5a4b90604df1132
Denis Pushkarev <zloirock@zloirock.ru>
2019-11-26 21:32:13 +0700

    simplify `commonjs-entries-content`

--- tests/commonjs-entries-content.js
+++ tests/commonjs-entries-content.js
@@ -1,5 +1,5 @@
 'use strict';
-const { ok } = require('assert');
+const { deepEqual } = require('assert');
 const data = require('../packages/core-js-compat/data');
 const entries = require('../packages/core-js-compat/entries');
 
@@ -7,11 +7,11 @@ const modules = Object.keys(data);
 const es = modules.filter(it => /^es\./.test(it));
 const stable = modules.filter(it => !/^esnext\./.test(it));
 
-ok(entries['core-js'].length === modules.length);
-ok(new Set([...modules, ...entries['core-js']]).size === modules.length);
-ok(entries['core-js/es'].length === es.length);
-ok(new Set([...es, ...entries['core-js/es']]).size === es.length);
-ok(entries['core-js/stable'].length === stable.length);
-ok(new Set([...stable, ...entries['core-js/stable']]).size === stable.length);
-ok(entries['core-js/features'].length === modules.length);
-ok(new Set([...modules, ...entries['core-js/features']]).size === modules.length);
+function sameModules(a, b) {
+  deepEqual(new Set(a), new Set(b));
+}
+
+sameModules(entries['core-js'], modules);
+sameModules(entries['core-js/es'], es);
+sameModules(entries['core-js/stable'], stable);
+sameModules(entries['core-js/features'], modules);
