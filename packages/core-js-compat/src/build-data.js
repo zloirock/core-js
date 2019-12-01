fc30a1266f56f27f4d16acd4bdf293dc326fed55
Denis Pushkarev <zloirock@zloirock.ru>
2019-12-02 03:52:03 +0700

    simplify `build-data` script

--- packages/core-js-compat/src/build-data.js
+++ packages/core-js-compat/src/build-data.js
@@ -11,54 +11,44 @@ const {
   SafariToIOS,
   SafariToPhantomJS,
 } = require('./mapping');
-const has = Function.call.bind({}.hasOwnProperty);
-
-function map(version, mapping) {
-  const coercedVersion = coerce(String(version));
-  for (const [from, to] of mapping) {
-    if (lte(coercedVersion, coerce(String(from)))) return to;
-  }
-}
 
 for (const key in data) {
   const module = data[key];
-  if (!has(module, 'edge') && has(module, 'ie') && key !== 'web.immediate') {
+  const { chrome, edge, ie, opera, safari } = module;
+
+  const map = function (mapping, version, targetKey) {
+    if (module[targetKey]) return;
+    const coercedVersion = coerce(String(version));
+    for (const [from, to] of mapping) {
+      if (lte(coercedVersion, coerce(String(from)))) {
+        module[targetKey] = to;
+        return;
+      }
+    }
+  };
+
+  if (!edge && ie && key !== 'web.immediate') {
     module.edge = '12';
   }
-  if (has(module, 'chrome')) {
-    const { chrome } = module;
-    if (!has(module, 'edge')) {
+
+  if (chrome) {
+    if (!edge) {
       module.edge = String(Math.max(chrome, 74));
     }
-    if (!has(module, 'opera')) {
+    if (!opera) {
       module.opera = String(chrome <= 23 ? 15 : chrome <= 29 ? 16 : chrome - 13);
     }
-    if (!has(module, 'node') && key.startsWith('es')) {
-      const node = map(chrome, ChromeToNode);
-      if (node) module.node = node;
+    if (key.startsWith('es')) {
+      map(ChromeToNode, chrome, 'node');
     }
-    if (!has(module, 'samsung')) {
-      const samsung = map(chrome, ChromeToSamsung);
-      if (samsung) module.samsung = samsung;
-    }
-    if (!has(module, 'android')) {
-      const android = map(chrome, ChromeToAndroid);
-      if (android) module.android = android;
-    }
-    if (!has(module, 'electron')) {
-      const electron = map(chrome, ChromeToElectron);
-      if (electron) module.electron = electron;
-    }
+    map(ChromeToSamsung, chrome, 'samsung');
+    map(ChromeToAndroid, chrome, 'android');
+    map(ChromeToElectron, chrome, 'electron');
   }
-  if (has(module, 'safari')) {
-    if (!has(module, 'ios')) {
-      const ios = map(module.safari, SafariToIOS);
-      if (ios) module.ios = ios;
-    }
-    if (!has(module, 'phantom')) {
-      const phantom = map(module.safari, SafariToPhantomJS);
-      if (phantom) module.phantom = phantom;
-    }
+
+  if (safari) {
+    map(SafariToIOS, safari, 'ios');
+    map(SafariToPhantomJS, safari, 'phantom');
   }
 }
 
