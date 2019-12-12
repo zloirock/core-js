8b2f30281739d571be63a7875d1bd6097eb48403
Denis Pushkarev <zloirock@zloirock.ru>
2019-12-12 08:02:52 +0700

    add compat tests for object iteration

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1245,6 +1245,15 @@ GLOBAL.tests = {
   'esnext.number.from-string': function () {
     return Number.fromString;
   },
+  'esnext.object.iterate-entries': function () {
+    return Object.iterateEntries;
+  },
+  'esnext.object.iterate-keys': function () {
+    return Object.iterateKeys;
+  },
+  'esnext.object.iterate-values': function () {
+    return Object.iterateValues;
+  },
   'esnext.observable': function () {
     return Observable;
   },
