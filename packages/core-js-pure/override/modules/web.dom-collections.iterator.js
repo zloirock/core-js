a2fae8e02929bbc5be288221590efef5b003bf07
Denis Pushkarev <zloirock@zloirock.ru>
2019-12-24 19:27:22 +0700

    set toStringTag to DOM collections in the pure version only if they can't be identified by `classof`

--- packages/core-js-pure/override/modules/web.dom-collections.iterator.js
+++ packages/core-js-pure/override/modules/web.dom-collections.iterator.js
@@ -1,6 +1,7 @@
 require('./es.array.iterator');
 var DOMIterables = require('../internals/dom-iterables');
 var global = require('../internals/global');
+var classof = require('../internals/classof');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var Iterators = require('../internals/iterators');
 var wellKnownSymbol = require('../internals/well-known-symbol');
@@ -10,7 +11,7 @@ var TO_STRING_TAG = wellKnownSymbol('toStringTag');
 for (var COLLECTION_NAME in DOMIterables) {
   var Collection = global[COLLECTION_NAME];
   var CollectionPrototype = Collection && Collection.prototype;
-  if (CollectionPrototype && !CollectionPrototype[TO_STRING_TAG]) {
+  if (CollectionPrototype && classof(CollectionPrototype) !== TO_STRING_TAG) {
     createNonEnumerableProperty(CollectionPrototype, TO_STRING_TAG, COLLECTION_NAME);
   }
   Iterators[COLLECTION_NAME] = Iterators.Array;
