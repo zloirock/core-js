ea15373629a0a75ed3335335a490aa5ab8d9643c
Denis Pushkarev <zloirock@zloirock.ru>
2020-10-29 00:29:45 +0700

    by the current spec, `AggregateError#errors` is own data property

--- packages/core-js/modules/esnext.aggregate-error.js
+++ packages/core-js/modules/esnext.aggregate-error.js
@@ -1,17 +1,11 @@
 'use strict';
 var $ = require('../internals/export');
-var DESCRIPTORS = require('../internals/descriptors');
 var getPrototypeOf = require('../internals/object-get-prototype-of');
 var setPrototypeOf = require('../internals/object-set-prototype-of');
 var create = require('../internals/object-create');
-var defineProperty = require('../internals/object-define-property');
+var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var createPropertyDescriptor = require('../internals/create-property-descriptor');
 var iterate = require('../internals/iterate');
-var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
-var InternalStateModule = require('../internals/internal-state');
-
-var setInternalState = InternalStateModule.set;
-var getInternalAggregateErrorState = InternalStateModule.getterFor('AggregateError');
 
 var $AggregateError = function AggregateError(errors, message) {
   var that = this;
@@ -21,8 +15,7 @@ var $AggregateError = function AggregateError(errors, message) {
   }
   var errorsArray = [];
   iterate(errors, errorsArray.push, errorsArray);
-  if (DESCRIPTORS) setInternalState(that, { errors: errorsArray, type: 'AggregateError' });
-  else that.errors = errorsArray;
+  createNonEnumerableProperty(that, 'errors', errorsArray);
   if (message !== undefined) createNonEnumerableProperty(that, 'message', String(message));
   return that;
 };
@@ -31,13 +24,6 @@ $AggregateError.prototype = create(Error.prototype, {
   constructor: createPropertyDescriptor(5, $AggregateError),
   message: createPropertyDescriptor(5, ''),
   name: createPropertyDescriptor(5, 'AggregateError')
-});
-
-if (DESCRIPTORS) defineProperty.f($AggregateError.prototype, 'errors', {
-  get: function () {
-    return getInternalAggregateErrorState(this).errors;
-  },
-  configurable: true
 });
 
 $({ global: true }, {
