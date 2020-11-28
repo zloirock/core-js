b971707c2fdef17d497c2fc08acd4f5d1bcb300d
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-28 17:58:31 +0700

    move iterators dependencies of `URL` and `URLSearchParams` to entries

--- packages/core-js/modules/web.url.js
+++ packages/core-js/modules/web.url.js
@@ -1,6 +1,4 @@
 'use strict';
-// TODO: in core-js@4, move /modules/ dependencies to public entries for better optimization by tools like `preset-env`
-require('../modules/es.string.iterator');
 var $ = require('../internals/export');
 var DESCRIPTORS = require('../internals/descriptors');
 var USE_NATIVE_URL = require('../internals/native-url');
--- packages/core-js/proposals/url.js
+++ packages/core-js/proposals/url.js
@@ -1,3 +1,5 @@
+require('../modules/es.array.iterator');
+require('../modules/es.string.iterator');
 require('../modules/web.url');
 require('../modules/web.url.to-json');
 require('../modules/web.url-search-params');
