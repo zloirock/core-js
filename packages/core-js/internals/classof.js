e6f48e0dc01e57ca3ceefdb61b8ca52c63e50d07
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-06 00:45:19 +0700

    remove ES3 `arguments` fallback from `classof`

--- packages/core-js/internals/classof.js
+++ packages/core-js/internals/classof.js
@@ -3,8 +3,6 @@ var classofRaw = require('../internals/classof-raw');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
 var TO_STRING_TAG = wellKnownSymbol('toStringTag');
-// ES3 wrong here
-var CORRECT_ARGUMENTS = classofRaw(function () { return arguments; }()) == 'Arguments';
 
 // fallback for IE11 Script Access Denied error
 var tryGet = function (it, key) {
@@ -15,12 +13,10 @@ var tryGet = function (it, key) {
 
 // getting tag from ES6+ `Object.prototype.toString`
 module.exports = TO_STRING_TAG_SUPPORT ? classofRaw : function (it) {
-  var O, tag, result;
+  var O, tag;
   return it === undefined ? 'Undefined' : it === null ? 'Null'
     // @@toStringTag case
     : typeof (tag = tryGet(O = Object(it), TO_STRING_TAG)) == 'string' ? tag
     // builtinTag case
-    : CORRECT_ARGUMENTS ? classofRaw(O)
-    // ES3 arguments fallback
-    : (result = classofRaw(O)) == 'Object' && typeof O.callee == 'function' ? 'Arguments' : result;
+    : classofRaw(O);
 };
