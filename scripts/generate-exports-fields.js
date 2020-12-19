87d3191feaf53b03279788c75981957b06e2ea09
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-20 02:12:24 +0700

    add `exports` fields to the rest packages

--- scripts/generate-exports-fields.js
+++ scripts/generate-exports-fields.js
@@ -1,12 +1,15 @@
 'use strict';
 const { relative } = require('path');
 const { readFile, writeFile } = require('fs').promises;
-const compat = require('core-js-compat/entries');
+const base = require('core-js-compat/entries');
+const builder = require('../packages/core-js-builder/exports');
+const bundle = require('../packages/core-js-bundle/exports');
+const compat = require('../packages/core-js-compat/exports');
 
-const entries = Object.keys(compat);
+const entries = Object.keys(base);
 entries.push('core-js/configurator');
 
-const map = entries.reduce((accumulator, it) => {
+const core = entries.reduce((accumulator, it) => {
   const entry = it.replace(/^core-js(\/)?/, './');
   const path = `./${ relative('./packages/core-js', require.resolve(`../packages/${ it }`)).replace(/\\/g, '/') }`;
   accumulator[entry] = path;
@@ -15,15 +18,28 @@ const map = entries.reduce((accumulator, it) => {
   return accumulator;
 }, {});
 
-async function writeExportsField(path) {
+function expland(array) {
+  const map = {};
+  for (const it of array) {
+    const path = `./${ it }`;
+    if (path.endsWith('/index.js')) map[path.replace(/\/index\.js(on)?$/, '').replace(/^\.$/, './')] = path;
+    map[path.replace(/\.js(on)?$/, '')] = path;
+    map[path] = path;
+  } return map;
+}
+
+async function writeExportsField(path, exports) {
   const pkg = JSON.parse(await readFile(path));
-  pkg.exports = map;
+  pkg.exports = exports;
   await writeFile(path, `${ JSON.stringify(pkg, null, '  ') }\n`);
 }
 
 (async function () {
-  await writeExportsField('./packages/core-js/package.json');
-  await writeExportsField('./packages/core-js-pure/package.json');
+  await writeExportsField('./packages/core-js/package.json', core);
+  await writeExportsField('./packages/core-js-pure/package.json', core);
+  await writeExportsField('./packages/core-js-builder/package.json', expland(builder));
+  await writeExportsField('./packages/core-js-bundle/package.json', expland(bundle));
+  await writeExportsField('./packages/core-js-compat/package.json', expland(compat));
   // eslint-disable-next-line no-console
-  console.log('\u001B[32m`exports` fields generated\u001B[0m');
+  console.log('\u001B[32m`exports` fields updated\u001B[0m');
 })();
