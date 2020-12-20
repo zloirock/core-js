4a1027f590c117f47ebade6e9393ea84da832160
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-20 14:09:06 +0700

    drop generation of `exports` field for tooling since with the current approach it makes no sense

--- scripts/generate-exports-fields.js
+++ scripts/generate-exports-fields.js
@@ -2,9 +2,6 @@
 const { relative } = require('path');
 const { readFile, writeFile } = require('fs').promises;
 const entries = require('core-js-compat/entries');
-const builder = require('../packages/core-js-builder/exports');
-const bundle = require('../packages/core-js-bundle/exports');
-const compat = require('../packages/core-js-compat/exports');
 
 const core = Object.keys(entries).reduce((accumulator, it) => {
   const entry = it.replace(/^core-js(\/)?/, './');
@@ -17,14 +14,6 @@ const core = Object.keys(entries).reduce((accumulator, it) => {
   './configurator': './configurator.js',
 });
 
-function expland(array) {
-  return array.reduce((accumulator, it) => {
-    const path = `./${ it }`;
-    accumulator[path.replace(/(\/index)?\.js(on)?$/, '').replace(/^\.$/, './')] = path;
-    return accumulator;
-  }, {});
-}
-
 async function writeExportsField(path, exports) {
   const pkg = JSON.parse(await readFile(path));
   exports['./package'] = './package.json';
@@ -35,9 +24,6 @@ async function writeExportsField(path, exports) {
 (async function () {
   await writeExportsField('./packages/core-js/package.json', core);
   await writeExportsField('./packages/core-js-pure/package.json', core);
-  await writeExportsField('./packages/core-js-builder/package.json', expland(builder));
-  await writeExportsField('./packages/core-js-bundle/package.json', expland(bundle));
-  await writeExportsField('./packages/core-js-compat/package.json', expland(compat));
   // eslint-disable-next-line no-console
   console.log('\u001B[32m`exports` fields updated\u001B[0m');
 })();
