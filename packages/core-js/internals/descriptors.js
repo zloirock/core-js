9289dd3cba62201c723e6f2c864de605c8770297
Chris Barker <bluquar@gmail.com>
2020-12-28 09:39:40 -0800

    Rephrase confusing comment in descriptors.js

--- packages/core-js/internals/descriptors.js
+++ packages/core-js/internals/descriptors.js
@@ -1,6 +1,6 @@
 var fails = require('../internals/fails');
 
-// Thank's IE8 for his funny defineProperty
+// Handle IE8's unusual defineProperty implementation
 module.exports = !fails(function () {
   return Object.defineProperty({}, 1, { get: function () { return 7; } })[1] != 7;
 });
