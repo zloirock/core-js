7dc9c7d6e05ad179796615d9ab71e1bede874761
Denis Pushkarev <zloirock@zloirock.ru>
2021-02-26 20:19:32 +0700

    fix linting

--- packages/core-js/modules/esnext.bigint.range.js
+++ packages/core-js/modules/esnext.bigint.range.js
@@ -9,6 +9,6 @@ if (typeof BigInt == 'function') {
   $({ target: 'BigInt', stat: true }, {
     range: function range(start, end, option) {
       return new NumericRangeIterator(start, end, option, 'bigint', BigInt(0), BigInt(1));
-    }
+    },
   });
 }
