2d78e53022f0506d9efee3a7922abdd95aaecdc0
Denis Pushkarev <zloirock@zloirock.ru>
2021-04-09 23:08:48 +0700

    (temporarily?) drop MIT license

--- packages/core-js-builder/config.js
+++ packages/core-js-builder/config.js
@@ -6,7 +6,6 @@ module.exports = {
   banner: '/**\n' +
           ' * core-js ' + version + '\n' +
           ' * https://github.com/zloirock/core-js\n' +
-          ' * License: http://rock.mit-license.org\n' +
           ' * © ' + new Date().getFullYear() + ' Denis Pushkarev (zloirock.ru)\n' +
           ' */',
   /* eslint-enable prefer-template -- for better formatting */
