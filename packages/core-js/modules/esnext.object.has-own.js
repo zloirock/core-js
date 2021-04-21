12e8ad960f565305110b7dad5b5f560d01663e8e
Denis Pushkarev <zloirock@zloirock.ru>
2021-04-21 12:50:32 +0300

    update after rebasing

--- packages/core-js/modules/esnext.object.has-own.js
+++ packages/core-js/modules/esnext.object.has-own.js
@@ -4,5 +4,5 @@ var hasOwn = require('../internals/has');
 // `Object.hasOwn` method
 // https://github.com/tc39/proposal-accessible-object-hasownproperty
 $({ target: 'Object', stat: true }, {
-  hasOwn: hasOwn
+  hasOwn: hasOwn,
 });
--- tests/pure/esnext.object.has-own.js
+++ tests/pure/esnext.object.has-own.js
@@ -1,5 +1,5 @@
-import create from 'core-js-pure/features/object/create';
-import hasOwn from 'core-js-pure/features/object/has-own';
+import create from 'core-js-pure/full/object/create';
+import hasOwn from 'core-js-pure/full/object/has-own';
 
 QUnit.test('Object.hasOwn', assert => {
   assert.isFunction(hasOwn);
