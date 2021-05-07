1f0fe8dbe0054dcd57fc4bbb2047080a0fe9ba8a
Denis Pushkarev <zloirock@zloirock.ru>
2021-05-08 00:33:39 +0300

    update compat data

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -84,7 +84,6 @@ export const data = {
     safari: '10.0',
     rhino: '1.7.13',
   },
-  // TODO: confirm
   'es.symbol.to-primitive': {
     chrome: '47',
     edge: '15',
@@ -92,7 +91,6 @@ export const data = {
     safari: '10.0',
     rhino: '1.7.13',
   },
-  // TODO: confirm
   'es.symbol.to-string-tag': {
     chrome: '49',
     edge: '15',
@@ -794,7 +792,6 @@ export const data = {
     firefox: '69',
     safari: '11.0',
   },
-  // TODO: confirm
   'es.promise.all': {
     chrome: '67',
     firefox: '69',
@@ -824,7 +821,6 @@ export const data = {
     ios: '13.2.3', // need to clarify the patch release, >13.0.0 && <= 13.2.3
     safari: '13.0.3', // need to clarify the patch release, >13.0.0 && <= 13.0.3
   },
-  // TODO: confirm
   'es.promise.race': {
     chrome: '67',
     firefox: '69',
