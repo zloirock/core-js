43095ad73963268ee361169c489c07189ecfde48
Denis Pushkarev <zloirock@zloirock.ru>
2021-05-27 01:38:20 +0700

    use native `Object.hasOwn` if it's available in internal `has` helper

--- packages/core-js/internals/has.js
+++ packages/core-js/internals/has.js
@@ -2,6 +2,6 @@ var toObject = require('../internals/to-object');
 
 var hasOwnProperty = {}.hasOwnProperty;
 
-module.exports = function hasOwn(it, key) {
+module.exports = Object.hasOwn || function hasOwn(it, key) {
   return hasOwnProperty.call(toObject(it), key);
 };
