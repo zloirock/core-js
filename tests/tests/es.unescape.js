c9561f9e7659cfb831dc45638ad2f6ffb0f66c41
Denis Pushkarev <zloirock@zloirock.ru>
2021-06-11 00:21:21 +0700

    add some tests

---
+++ tests/tests/es.unescape.js
@@ -0,0 +1,10 @@
+QUnit.test('unescape', assert => {
+  assert.isFunction(unescape);
+  assert.name(unescape, 'unescape');
+  assert.arity(unescape, 1);
+  assert.looksNative(unescape);
+  assert.same(unescape('%21q2%u0444'), '!q2ф');
+  assert.same(unescape('%u044q2%21'), '%u044q2!');
+  assert.same(unescape(null), 'null');
+  assert.same(unescape(undefined), 'undefined');
+});
