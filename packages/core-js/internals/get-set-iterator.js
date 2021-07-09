9a1c5b5ffe727df759167477df0bc2ec163a5556
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-10 02:34:33 +0700

    split `get-(map|set)-iterator` to global and pure versions

--- packages/core-js/internals/get-set-iterator.js
+++ packages/core-js/internals/get-set-iterator.js
@@ -1,7 +1,4 @@
-var IS_PURE = require('../internals/is-pure');
-var getIterator = require('../internals/get-iterator');
-
-module.exports = IS_PURE ? getIterator : function (it) {
+module.exports = function (it) {
   // eslint-disable-next-line es/no-set -- safe
   return Set.prototype.values.call(it);
 };
