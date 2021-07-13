81f7da357b5f48f68e2bf5e0e67bc7e594ac889f
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-13 18:26:33 +0700

    rewrite `generate-indexes` script to `zx`

---
+++ scripts/generate-indexes.mjs
@@ -0,0 +1,28 @@
+import compat from 'core-js-compat/src/data.js';
+
+const modules = Object.keys(compat);
+
+async function generateNamespaceIndex(ns, filter) {
+  return fs.writeFile(`./packages/core-js/${ ns }/index.js`, `${ modules
+    .filter(it => filter.test(it))
+    .map(it => `require('../modules/${ it }');\n`)
+    .join('') }\nmodule.exports = require('../internals/path');\n`);
+}
+
+async function generateTestsIndex(name, pkg) {
+  const dir = `./tests/${ name }`;
+  const files = await fs.readdir(dir);
+  return fs.writeFile(`${ dir }/index.js`, `import '../helpers/qunit-helpers';\n\n${ files
+    .filter(it => /^(?:es|esnext|web)\./.test(it))
+    .map(it => `import './${ it.slice(0, -3) }';\n`)
+    .join('') }${ pkg !== 'core-js' ? `\nimport core from '${ pkg }';\ncore.globalThis.core = core;\n` : '' }`);
+}
+
+await generateNamespaceIndex('es', /^es\./);
+await generateNamespaceIndex('stable', /^(es|web)\./);
+await generateNamespaceIndex('features', /^(es|esnext|web)\./);
+
+await generateTestsIndex('tests', 'core-js');
+await generateTestsIndex('pure', 'core-js-pure');
+// eslint-disable-next-line no-console -- output
+console.log(chalk.green('indexes generated'));
