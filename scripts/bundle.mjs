5875a4c827467840dfeeb7a47afcb46273260857
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-13 18:55:14 +0700

    rewrite `bundle` script to `zx`

--- scripts/bundle.mjs
+++ scripts/bundle.mjs
@@ -1,25 +1,21 @@
-'use strict';
-const { writeFile } = require('fs').promises;
-const { cyan, green } = require('chalk');
-const { minify } = require('terser');
-const builder = require('core-js-builder');
-const { banner } = require('core-js-builder/config');
+import { minify } from 'terser';
+import builder from 'core-js-builder';
+import config from 'core-js-builder/config.js';
 
-const DENO = process.argv.includes('--deno');
-
+const DENO = argv.deno;
 const PATH = DENO ? './deno/corejs/' : './packages/core-js-bundle/';
 
 function log(kind, name, code) {
   const size = (code.length / 1024).toFixed(2);
   // eslint-disable-next-line no-console -- output
-  console.log(green(`${ kind }: ${ cyan(`${ PATH }${ name }.js`) }, size: ${ cyan(`${ size }KB`) }`));
+  console.log(chalk.green(`${ kind }: ${ chalk.cyan(`${ PATH }${ name }.js`) }, size: ${ chalk.cyan(`${ size }KB`) }`));
 }
 
 async function bundle({ bundled, minified, options = {} }) {
   const source = await builder(options);
 
   log('bundling', bundled, source);
-  await writeFile(`${ PATH }${ bundled }.js`, source);
+  await fs.writeFile(`${ PATH }${ bundled }.js`, source);
 
   if (!minified) return;
 
@@ -37,7 +33,7 @@ async function bundle({ bundled, minified, options = {} }) {
     },
     format: {
       max_line_len: 32000,
-      preamble: banner,
+      preamble: config.banner,
       webkit: false,
     },
     sourceMap: {
@@ -45,8 +41,8 @@ async function bundle({ bundled, minified, options = {} }) {
     },
   });
 
-  await writeFile(`${ PATH }${ minified }.js`, code);
-  await writeFile(`${ PATH }${ minified }.js.map`, map);
+  await fs.writeFile(`${ PATH }${ minified }.js`, code);
+  await fs.writeFile(`${ PATH }${ minified }.js.map`, map);
   log('minification', minified, code);
 }
 
