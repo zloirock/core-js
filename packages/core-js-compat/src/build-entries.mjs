a8d290aa164051fca1751eda6d353fe4cee65838
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-13 17:34:26 +0700

    rewrite `build-entries` script

---
+++ packages/core-js-compat/src/build-entries.mjs
@@ -0,0 +1,43 @@
+import detective from 'detective';
+import globby from 'globby';
+import helpers from '../helpers.js';
+
+const modules = require('../modules');
+
+async function getModulesForEntryPoint(path, parent) {
+  const entry = new URL(path, parent);
+
+  const match = entry.pathname.match(/[/\\]modules[/\\]([^/\\]+)$/);
+  if (match) return [match[1]];
+
+  entry.pathname += await fs.pathExists(entry) ? '/index.js' : '.js';
+
+  const file = await fs.readFile(entry);
+  const result = await Promise.all(detective(file).map(dependency => {
+    return getModulesForEntryPoint(dependency, entry);
+  }));
+
+  return helpers.intersection(result.flat(1), modules);
+}
+
+const entriesList = await globby([
+  'packages/core-js/index.js',
+  'packages/core-js/es/**/*.js',
+  'packages/core-js/features/**/*.js',
+  'packages/core-js/modules/*.js',
+  'packages/core-js/proposals/**/*.js',
+  'packages/core-js/stable/**/*.js',
+  'packages/core-js/stage/**/*.js',
+  'packages/core-js/web/**/*.js',
+]);
+
+const entriesMap = helpers.sortObjectByKey(Object.fromEntries(await Promise.all(entriesList.map(async file => {
+  // TODO: store entries without the package name in `core-js@4`
+  const entry = file.slice(9).replace(/\.js$/, '').replace(/\/index$/, '');
+  return [entry, await getModulesForEntryPoint(`../../${ entry }`, import.meta.url)];
+}))));
+
+await fs.writeJson(new URL('../entries.json', import.meta.url), entriesMap, { spaces: '  ' });
+
+// eslint-disable-next-line no-console -- output
+console.log(chalk.green('entries data rebuilt'));
