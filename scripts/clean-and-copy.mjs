7ccc6f636e1f57b191c76eacad956d816f67de37
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-13 18:18:29 +0700

    rewrite `clean-and-copy` script to `zx`

---
+++ scripts/clean-and-copy.mjs
@@ -0,0 +1,34 @@
+const { copy, lstat, pathExists, readdir, rm } = fs;
+let copied = 0;
+
+function options(overwrite) {
+  return {
+    async filter(from, to) {
+      if ((await lstat(from)).isDirectory()) return true;
+      if (!overwrite && await pathExists(to)) return false;
+      return !!++copied;
+    },
+  };
+}
+
+await Promise.all((await readdir('./packages/core-js-pure'))
+  .filter(entry => !['override', '.npmignore', 'package.json', 'README.md'].includes(entry))
+  .map(entry => rm(`./packages/core-js-pure/${ entry }`, { force: true, recursive: true })));
+
+await rm('./tests/bundles', { force: true, recursive: true });
+
+// eslint-disable-next-line no-console -- output
+console.log(chalk.green('old copies removed'));
+
+await copy('./LICENSE', './deno/corejs/LICENSE', options(true));
+
+for (const pkg of await readdir('./packages')) {
+  await copy('./LICENSE', `./packages/${ pkg }/LICENSE`, options(true));
+}
+
+await copy('./packages/core-js', './packages/core-js-pure', options(false));
+await copy('./packages/core-js-pure/override', './packages/core-js-pure', options(true));
+await copy('./packages/core-js/postinstall.js', './packages/core-js-bundle/postinstall.js', options(true));
+
+// eslint-disable-next-line no-console -- output
+console.log(chalk.green(`copied ${ chalk.cyan(copied) } files`));
