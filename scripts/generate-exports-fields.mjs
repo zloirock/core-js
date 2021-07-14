ce1306f8f747788ed48829951c13e133e667a831
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-14 21:39:33 +0700

    rewrite `generate-exports-fields` script to `zx`

--- scripts/generate-exports-fields.mjs
+++ scripts/generate-exports-fields.mjs
@@ -1,18 +1,14 @@
-'use strict';
-const { relative } = require('path');
-const { readFile, writeFile } = require('fs').promises;
-const { green } = require('chalk');
-const entries = require('@core-js/compat/entries');
+import entries from '@core-js/compat/entries';
 
-let core = Object.keys(entries).reduce((accumulator, it) => {
+const core = Object.entries(Object.keys(entries).reduce((accumulator, it) => {
   const entry = it.replace(/^core-js(\/)?/, './');
   // eslint-disable-next-line unicorn/no-unsafe-regex -- safe
-  const match = entry.match(/^(\.\/(?:es|stable|actual|full|web)(?:\/[\w-]+)?)(?:\/[\w-]+)?/);
+  const match = entry.match(/^(\.\/(?:es|stable|actual|full|web)(?:\/[\w-]+)?)/);
   if (match) {
     const [, scope] = match;
     if (entry === scope) {
-      const path = `./${ relative('./packages/core-js', require.resolve(`../packages/${ it }`)).replace(/\\/g, '/') }`;
-      accumulator[entry] = path;
+      const exists = fs.pathExistsSync(new URL(`../packages/${ it }.js`, import.meta.url));
+      accumulator[entry] = `${ entry }${ exists ? '' : '/index' }.js`;
     } else {
       accumulator[`${ scope }/*`] = `${ scope }/*.js`;
     }
@@ -26,9 +22,7 @@ let core = Object.keys(entries).reduce((accumulator, it) => {
   './proposals/*': './proposals/*.js',
   './stage': './stage/index.js',
   './stage/*': './stage/*.js',
-});
-
-core = Object.entries(core).reduce((accumulator, [key, value]) => {
+})).reduce((accumulator, [key, value]) => {
   accumulator[key] = value;
   accumulator[`./commonjs${ key.slice(1) }`] = `./commonjs${ value.slice(1) }`;
   return accumulator;
@@ -38,9 +32,9 @@ core = Object.entries(core).reduce((accumulator, [key, value]) => {
 });
 
 async function writeExportsField(path, exports) {
-  const pkg = JSON.parse(await readFile(path, 'utf8'));
+  const pkg = await fs.readJson(path);
   pkg.exports = exports;
-  await writeFile(path, `${ JSON.stringify(pkg, null, '  ') }\n`);
+  await fs.writeJson(path, pkg, { spaces: '  ' });
 }
 
 (async function () {
@@ -52,5 +46,5 @@ async function writeExportsField(path, exports) {
   });
   await writeExportsField('./packages/core-js-pure/package.json', core);
   // eslint-disable-next-line no-console -- output
-  console.log(green('`exports` fields updated'));
+  console.log(chalk.green('`exports` fields updated'));
 })();
