785c1058b5d024e4829cdf8e0833eb3cd583ef4f
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-18 00:53:00 +0700

    adapt to the actual spec draft

--- packages/core-js/modules/esnext.array.group-by.js
+++ packages/core-js/modules/esnext.array.group-by.js
@@ -1,14 +1,15 @@
 'use strict';
 var $ = require('../internals/export');
 var $groupBy = require('../internals/array-group-by');
-var arraySpeciesCreate = require('../internals/array-species-create');
+var arraySpeciesConstructor = require('../internals/array-species-constructor');
 var addToUnscopables = require('../internals/add-to-unscopables');
 
 // `Array.prototype.groupBy` method
 // https://github.com/tc39/proposal-array-grouping
 $({ target: 'Array', proto: true }, {
   groupBy: function groupBy(callbackfn /* , thisArg */) {
-    return $groupBy(this, callbackfn, arguments.length > 1 ? arguments[1] : undefined, arraySpeciesCreate);
+    var thisArg = arguments.length > 1 ? arguments[1] : undefined;
+    return $groupBy(this, callbackfn, thisArg, arraySpeciesConstructor);
   }
 });
 
--- packages/core-js/modules/esnext.typed-array.group-by.js
+++ packages/core-js/modules/esnext.typed-array.group-by.js
@@ -1,22 +1,14 @@
 'use strict';
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
 var $groupBy = require('../internals/array-group-by');
-var speciesConstructor = require('../internals/species-constructor');
+var typedArraySpeciesConstructor = require('../internals/typed-array-species-constructor');
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
-var aTypedArrayConstructor = ArrayBufferViewCore.aTypedArrayConstructor;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
 
 // `%TypedArray%.prototype.groupBy` method
 // https://github.com/tc39/proposal-array-grouping
 exportTypedArrayMethod('groupBy', function groupBy(callbackfn /* , thisArg */) {
-  var result = $groupBy(aTypedArray(this), callbackfn, arguments.length > 1 ? arguments[1] : undefined);
-  var TypedArray = aTypedArrayConstructor(speciesConstructor(this, this.constructor));
-  var key, array, typedArray, index, length;
-  for (key in result) {
-    array = result[key];
-    length = array.length;
-    result[key] = typedArray = new TypedArray(length);
-    for (index = 0; index < length; index++) typedArray[index] = array[index];
-  } return result;
+  var thisArg = arguments.length > 1 ? arguments[1] : undefined;
+  return $groupBy(aTypedArray(this), callbackfn, thisArg, typedArraySpeciesConstructor);
 });
