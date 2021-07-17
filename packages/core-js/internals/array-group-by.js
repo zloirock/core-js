624593613ebfea3d993d8800758bcac494a0b5ba
Denis Pushkarev <zloirock@zloirock.ru>
2021-07-18 00:53:00 +0700

    adapt to the actual spec draft

--- packages/core-js/internals/array-group-by.js
+++ packages/core-js/internals/array-group-by.js
@@ -3,24 +3,30 @@ var has = require('../internals/has');
 var IndexedObject = require('../internals/indexed-object');
 var toObject = require('../internals/to-object');
 var toLength = require('../internals/to-length');
-var toPrimitive = require('../internals/to-primitive');
+var toPropertyKey = require('../internals/to-property-key');
 var objectCreate = require('../internals/object-create');
+var arrayFromConstructorAndList = require('../internals/array-from-constructor-and-list');
 
 var push = [].push;
 
-module.exports = function ($this, callbackfn, that, specificCreate) {
+module.exports = function ($this, callbackfn, that, specificConstructor) {
   var O = toObject($this);
   var self = IndexedObject(O);
   var boundFunction = bind(callbackfn, that, 3);
   var target = objectCreate(null);
   var length = toLength(self.length);
   var index = 0;
-  var value, result, array;
+  var Constructor, key, value;
   for (;length > index; index++) {
     value = self[index];
-    result = toPrimitive(boundFunction(value, index, O), 'string');
-    if (has(target, result)) array = target[result];
-    else target[result] = array = specificCreate ? specificCreate($this, 0) : [];
-    push.call(array, value);
+    key = toPropertyKey(boundFunction(value, index, O));
+    if (has(target, key)) push.call(target[key], value);
+    else target[key] = [value];
+  }
+  if (specificConstructor) {
+    Constructor = specificConstructor(O);
+    if (Constructor !== Array) {
+      for (key in target) target[key] = arrayFromConstructorAndList(Constructor, target[key]);
+    }
   } return target;
 };
