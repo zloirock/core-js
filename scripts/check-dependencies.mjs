65ca5181194832d0989ec43475c11a764783c295
Denis Pushkarev <zloirock@zloirock.ru>
2021-08-31 02:02:14 +0700

    add ignore lists to `check-dependencies` script, add this script to `test` command

--- scripts/check-dependencies.mjs
+++ scripts/check-dependencies.mjs
@@ -6,12 +6,26 @@ import semver from 'semver';
 const { eq, coerce, minVersion } = semver;
 const getDependencies = promisify(david.getDependencies);
 
+const ignoreEverywhere = new Set([
+  'moon-unit',
+]);
+
+const ignoreInPackages = new Set([
+  'mkdirp',
+  'semver',
+  'webpack',
+]);
+
 await Promise.all((await globby(['package.json', 'packages/*/package.json'])).map(async path => {
   const pkg = JSON.parse(await fs.readFile(path));
   const dependencies = await getDependencies(pkg);
   const devDependencies = await getDependencies(pkg, { dev: true });
   Object.assign(dependencies, devDependencies);
   for (const name of Object.keys(dependencies)) {
+    if (ignoreEverywhere.has(name) || (path !== 'package.json' && ignoreInPackages.has(name))) {
+      delete dependencies[name];
+      continue;
+    }
     const { required, stable, warn } = dependencies[name];
     if (/^(?:file|git)/.test(required) || warn || eq(minVersion(required), coerce(stable))) {
       delete dependencies[name];
