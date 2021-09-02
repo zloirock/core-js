dd68478552a8d4c16e3ab33422209250323908a9
Denis Pushkarev <zloirock@zloirock.ru>
2021-09-02 20:24:37 +0700

    fix stage/4 entry dependencies

--- packages/core-js/stage/4.js
+++ packages/core-js/stage/4.js
@@ -1,6 +1,8 @@
+require('../proposals/accessible-object-hasownproperty');
 require('../proposals/global-this');
 require('../proposals/promise-all-settled');
 require('../proposals/promise-any');
+require('../proposals/relative-indexing-method');
 require('../proposals/string-match-all');
 require('../proposals/string-replace-all');
 var path = require('../internals/path');
