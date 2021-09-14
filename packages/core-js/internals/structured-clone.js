d807b2e2cef6fb559dcd7c9a2a842554e7ef2a1f
Basix <basix@basix.tech>
2021-09-15 02:22:04 +0900

    cache cloned value in a weakmap

--- packages/core-js/internals/structured-clone.js
+++ packages/core-js/internals/structured-clone.js
@@ -21,47 +21,44 @@ function structuredCloneInternal(weakmap, value) {
   if (value === null) return null;
   if (weakmap.has(value)) return weakmap.get(value); // effectively preserves circular references
 
-  if (value instanceof Boolean) return new Boolean(value.valueOf());
-  if (typeof BigInt === 'function' && value instanceof BigInt) return Object(value.valueOf());
-  if (value instanceof Number) return new Number(value.valueOf());
-  if (value instanceof String) return new String(value.valueOf());
-  if (value instanceof Date) return new Date(value.valueOf());
+  var cloned;
 
-  if (value instanceof RegExp) return new RegExp(value); // Not cloning [[RegExpMatcher]], but it will be created
+  if (value instanceof Boolean) cloned = new Boolean(value.valueOf());
+  else if (typeof BigInt === 'function' && value instanceof BigInt) cloned = Object(value.valueOf());
+  else if (value instanceof Number) cloned = new Number(value.valueOf());
+  else if (value instanceof String) cloned = new String(value.valueOf());
+  else if (value instanceof Date) cloned = new Date(value.valueOf());
+
+  else if (value instanceof RegExp) cloned = new RegExp(value); // Not cloning [[RegExpMatcher]], but it will be created
 
   // TODO: SharedArrayBuffer, ArrayBuffer, DataView, etc
 
-  if (typeof Map === 'function' && value instanceof Map) {
+  else if (typeof Map === 'function' && value instanceof Map) {
     var map = new Map();
     value.forEach(function (v, key) {
       map.set(structuredCloneInternal(key), structuredCloneInternal(weakmap, v));
     });
-    return map;
-  }
-
-  if (typeof Set === 'function' && value instanceof Set) {
+    cloned = map;
+  } else if (typeof Set === 'function' && value instanceof Set) {
     var set = new Set();
     value.forEach(function (v) {
       set.add(structuredCloneInternal(weakmap, v));
     });
-    return set;
-  }
-
-  if (isArray(value)) {
-    return value.map(function (v) {
+    cloned = set;
+  } else if (isArray(value)) {
+    cloned = value.map(function (v) {
       return structuredCloneInternal(weakmap, v);
     });
-  }
-
-  if (isObject(value)) {
+  } else if (isObject(value)) {
     var rv = {};
     getOwnPropertyNames.f(value).forEach(function (k) {
       rv[structuredCloneInternal(weakmap, k)] = structuredCloneInternal(weakmap, value[k]);
     });
-    return rv;
+    cloned = rv;
   }
 
-  return value; // TODO: REMOVE THIS
+  weakmap.set(value, cloned);
+  return cloned;
 }
 
 /**
