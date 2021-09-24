71adeb4a264f77df25931582493a290b94cd9c48
Denis Pushkarev <zloirock@zloirock.ru>
2021-09-25 00:43:05 +0300

    fix has own -> has

--- packages/core-js/internals/install-error-cause.js
+++ packages/core-js/internals/install-error-cause.js
@@ -1,11 +1,10 @@
 var isObject = require('../internals/is-object');
-var has = require('../internals/has');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 
 // `InstallErrorCause` abstract operation
 // https://tc39.es/proposal-error-cause/#sec-errorobjects-install-error-cause
 module.exports = function (O, options) {
-  if (isObject(options) && has(options, 'cause')) {
+  if (isObject(options) && 'cause' in options) {
     createNonEnumerableProperty(O, 'cause', O.cause);
   }
 };
