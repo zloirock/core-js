5381facdc9ce69910ee6d333b2cc9140bdb1ee63
Basix <basix@basix.tech>
2021-09-26 01:23:21 +0900

    implement Blob cloning
    although the type is different the interface is more or less the same.

--- packages/core-js/internals/structured-clone.js
+++ packages/core-js/internals/structured-clone.js
@@ -48,6 +48,7 @@ module.exports = function structuredCloneInternal(map, value) {
       if (!isArrayBufferDetached()) throw createDataCloneError('ArrayBuffer is deatched');
       // falls through
     case 'SharedArrayBuffer':
+    case 'Blob':
       cloned = value.slice(0);
       break;
     case 'Map':
