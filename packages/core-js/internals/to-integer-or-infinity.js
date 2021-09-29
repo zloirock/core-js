08d44c4eeb3851ce8c8bcd73a49ae3a5221c6990
Denis Pushkarev <zloirock@zloirock.ru>
2021-09-29 23:58:08 +0700

    optimize `ToIntegerOrInfinity`

--- packages/core-js/internals/to-integer-or-infinity.js
+++ packages/core-js/internals/to-integer-or-infinity.js
@@ -1,9 +1,9 @@
-var $isNaN = isNaN;
 var ceil = Math.ceil;
 var floor = Math.floor;
 
 // `ToIntegerOrInfinity` abstract operation
 // https://tc39.es/ecma262/#sec-tointegerorinfinity
 module.exports = function (argument) {
-  return $isNaN(argument = +argument) || argument === 0 ? 0 : (argument > 0 ? floor : ceil)(argument);
+  var number = +argument;
+  return number !== number || number === 0 ? 0 : (number > 0 ? floor : ceil)(number);
 };
