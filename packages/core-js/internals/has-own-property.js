057a0b92b0e73954a694a2fca77da04172c073e9
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-02 16:23:33 +0700

    add a comment

--- packages/core-js/internals/has-own-property.js
+++ packages/core-js/internals/has-own-property.js
@@ -2,6 +2,8 @@ var toObject = require('../internals/to-object');
 
 var hasOwnProperty = {}.hasOwnProperty;
 
+// `HasOwnProperty` abstract operation
+// https://tc39.es/ecma262/#sec-hasownproperty
 module.exports = Object.hasOwn || function hasOwn(it, key) {
   return hasOwnProperty.call(toObject(it), key);
 };
