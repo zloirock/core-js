071ffae5953e6fa9de9d2e8bbf5def742b11f07b
Basix <basix@basix.tech>
2021-10-19 01:05:12 +0900

    use map for caching
    since the map itself does not live longer then the function, it is ok

--- packages/core-js/modules/web.structured-clone.js
+++ packages/core-js/modules/web.structured-clone.js
@@ -1,12 +1,12 @@
 var $ = require('../internals/export');
 var getBuiltIn = require('../internals/get-built-in');
 var structuredCloneImpl = require('../internals/structured-clone');
-var WeakMap = getBuiltIn('WeakMap');
+var Map = getBuiltIn('Map');
 
 $({ global: true, enumerable: true, sham: true }, {
   structuredClone: function structuredClone(value/* , { transfer } */) {
     var transfer = arguments.length > 1 && arguments[1] !== undefined ? arguments[1].transfer : undefined;
 
-    return structuredCloneImpl(new WeakMap(), value, transfer || []);
+    return structuredCloneImpl(new Map(), value, transfer || []);
   }
 });
