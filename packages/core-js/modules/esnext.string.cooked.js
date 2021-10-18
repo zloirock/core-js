8eb5f39721b9a0430502d51d9efadc2c0e2f6ecb
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-18 07:12:30 +0700

    refactoring

--- packages/core-js/modules/esnext.string.cooked.js
+++ packages/core-js/modules/esnext.string.cooked.js
@@ -1,11 +1,14 @@
 var $ = require('../internals/export');
+var global = require('../internals/global');
+var uncurryThis = require('../internals/function-uncurry-this');
 var toIndexedObject = require('../internals/to-indexed-object');
 var toString = require('../internals/to-string');
 var lengthOfArrayLike = require('../internals/length-of-array-like');
 
+var TypeError = global.TypeError;
 var ArrayPrototype = Array.prototype;
-var push = ArrayPrototype.push;
-var join = ArrayPrototype.join;
+var push = uncurryThis(ArrayPrototype.push);
+var join = uncurryThis(ArrayPrototype.join);
 
 // `String.cooked` method
 // https://github.com/bathos/proposal-string-cooked
@@ -19,9 +22,9 @@ $({ target: 'String', stat: true }, {
     while (literalSegments > i) {
       var nextVal = cookedTemplate[i++];
       if (nextVal === undefined) throw TypeError('Incorrect template');
-      push.call(elements, toString(nextVal));
-      if (i === literalSegments) return join.call(elements, '');
-      if (i < argumentsLength) push.call(elements, toString(arguments[i]));
+      push(elements, toString(nextVal));
+      if (i === literalSegments) return join(elements, '');
+      if (i < argumentsLength) push(elements, toString(arguments[i]));
     }
   }
 });
