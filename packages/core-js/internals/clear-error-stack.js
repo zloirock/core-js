a4a4783381a4133ee4d6275d8039cf5cdeae5110
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-18 07:55:00 +0700

    refactoring

--- packages/core-js/internals/clear-error-stack.js
+++ packages/core-js/internals/clear-error-stack.js
@@ -1,7 +1,9 @@
-var replace = ''.replace;
-var split = ''.split;
-var slice = [].slice;
-var join = [].join;
+var uncurryThis = require('../internals/function-uncurry-this');
+var arraySlice = require('../internals/array-slice');
+
+var replace = uncurryThis(''.replace);
+var split = uncurryThis(''.split);
+var join = uncurryThis([].join);
 
 var TEST = (function (arg) { return String(Error(arg).stack); })('zxcasd');
 var V8_OR_CHAKRA_STACK_ENTRY = /\n\s*at [^:]*:[^\n]*/;
@@ -11,8 +13,8 @@ var IS_FIREFOX_OR_SAFARI_STACK = /@[^\n]*\n/.test(TEST) && !/zxcasd/.test(TEST);
 module.exports = function (stack, dropEntries) {
   if (typeof stack != 'string') return stack;
   if (IS_V8_OR_CHAKRA_STACK) {
-    while (dropEntries--) stack = replace.call(stack, V8_OR_CHAKRA_STACK_ENTRY, '');
+    while (dropEntries--) stack = replace(stack, V8_OR_CHAKRA_STACK_ENTRY, '');
   } else if (IS_FIREFOX_OR_SAFARI_STACK) {
-    return join.call(slice.call(split.call(stack, '\n'), dropEntries), '\n');
+    return join(arraySlice(split(stack, '\n'), dropEntries), '\n');
   } return stack;
 };
