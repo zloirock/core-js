36bd972638acb2b2f74d7a1899b4f679e9cd4415
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-18 07:14:53 +0700

    refatoring

--- packages/core-js/modules/esnext.function.is-callable.js
+++ packages/core-js/modules/esnext.function.is-callable.js
@@ -1,15 +1,16 @@
 var $ = require('../internals/export');
+var uncurryThis = require('../internals/function-uncurry-this');
 var $isCallable = require('../internals/is-callable');
 var inspectSource = require('../internals/inspect-source');
 
 var classRegExp = /^\s*class\b/;
-var exec = classRegExp.exec;
+var exec = uncurryThis(classRegExp.exec);
 
 // `Function.isCallable` method
 // https://github.com/caitp/TC39-Proposals/blob/trunk/tc39-reflect-isconstructor-iscallable.md
 $({ target: 'Function', stat: true, sham: true }, {
   isCallable: function isCallable(argument) {
     // we can't properly detect `[[IsClassConstructor]]` internal slot without `Function#toString` check
-    return $isCallable(argument) && !exec.call(classRegExp, inspectSource(argument));
+    return $isCallable(argument) && !exec(classRegExp, inspectSource(argument));
   }
 });
