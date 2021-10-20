fd0ec2ced5763893d467cb0964be143abd9b65f8
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-20 09:45:12 +0700

    fix props conversion in the base version of `defineProperties`

--- packages/core-js/internals/object-define-properties.js
+++ packages/core-js/internals/object-define-properties.js
@@ -1,6 +1,7 @@
 var DESCRIPTORS = require('../internals/descriptors');
 var definePropertyModule = require('../internals/object-define-property');
 var anObject = require('../internals/an-object');
+var toIndexedObject = require('../internals/to-indexed-object');
 var objectKeys = require('../internals/object-keys');
 
 // `Object.defineProperties` method
@@ -8,10 +9,11 @@ var objectKeys = require('../internals/object-keys');
 // eslint-disable-next-line es/no-object-defineproperties -- safe
 module.exports = DESCRIPTORS ? Object.defineProperties : function defineProperties(O, Properties) {
   anObject(O);
+  var props = toIndexedObject(Properties);
   var keys = objectKeys(Properties);
   var length = keys.length;
   var index = 0;
   var key;
-  while (length > index) definePropertyModule.f(O, key = keys[index++], Properties[key]);
+  while (length > index) definePropertyModule.f(O, key = keys[index++], props[key]);
   return O;
 };
