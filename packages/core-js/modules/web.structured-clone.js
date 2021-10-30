4707b7374d42db97e227b74968abd9f39f1dafdf
Denis Pushkarev <zloirock@zloirock.ru>
2021-10-30 12:24:48 +0700

    forced usage of polyfill in the pure version since native `structuredClone` can't work with wrapped `Map` / `Set`

--- packages/core-js/modules/web.structured-clone.js
+++ packages/core-js/modules/web.structured-clone.js
@@ -1,3 +1,4 @@
+var IS_PURE = require('../internals/is-pure');
 var $ = require('../internals/export');
 var global = require('../internals/global');
 var getBuiltIn = require('../internals/get-built-in');
@@ -7,7 +8,7 @@ var structuredCloneImpl = require('../internals/structured-clone');
 var Map = getBuiltIn('Map');
 var TypeError = global.TypeError;
 
-$({ global: true, enumerable: true, sham: true }, {
+$({ global: true, enumerable: true, sham: true, forced: IS_PURE }, {
   structuredClone: function structuredClone(value /* , { transfer } */) {
     var options = arguments.length > 1 ? anObject(arguments[1]) : undefined;
     var transfer = options && options.transfer;
