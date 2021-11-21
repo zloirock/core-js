76a9e04d68b0ae8fca67410701a0c3cfc617fb9b
Ildar Shaimordanov <ildar-shaimordanov@users.noreply.github.com>
2021-11-21 17:34:38 +0300

    Comprehensive function name lookup
    Supports any of:
    -- `function \s* name`
    -- `function \s* /* multi-line comment */ \s* name`
    -- `function \s* // single-line comment [\r\n]+ \s* name`

--- packages/core-js/modules/es.function.name.js
+++ packages/core-js/modules/es.function.name.js
@@ -5,7 +5,7 @@ var defineProperty = require('../internals/object-define-property').f;
 
 var FunctionPrototype = Function.prototype;
 var functionToString = uncurryThis(FunctionPrototype.toString);
-var nameRE = /^\s*function ([^ (]*)/;
+var nameRE = /function\b(?:\s|\/\*[\s\S]*?\*\/|\/\/[^\r\n]*[\r\n]+)*([^\s/(]*)/;
 var regExpExec = uncurryThis(nameRE.exec);
 var NAME = 'name';
 
