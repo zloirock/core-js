71e7c18c034051d13e546cae81d78791bd606242
Denis Pushkarev <zloirock@zloirock.ru>
2021-12-06 20:04:59 +0700

    replace `puppeteer` by `playwright` in `karma` tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,23 +1,27 @@
 'use strict';
-process.env.CHROME_BIN = require('puppeteer').executablePath();
+const { chromium, firefox, webkit } = require('playwright');
+
+Object.assign(process.env, {
+  CHROMIUM_BIN: chromium.executablePath(),
+  FIREFOX_BIN: firefox.executablePath(),
+  WEBKIT_BIN: webkit.executablePath(),
+});
 
 module.exports = function (config) {
-  const files = process.argv
-    .find(it => it.startsWith('-f='))
-    .slice(3)
-    .split(',');
-
   config.set({
-    files,
+    plugins: [
+      'karma-*',
+      '@onslip/karma-playwright-launcher',
+    ],
+    files: process.argv.find(it => it.startsWith('-f=')).slice(3).split(','),
     frameworks: ['qunit'],
     basePath: '.',
-    browsers: ['HeadlessChrome', 'PhantomJS'],
-    customLaunchers: {
-      HeadlessChrome: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-setuid-sandbox'],
-      },
-    },
+    browsers: [
+      'ChromiumHeadless',
+      'FirefoxHeadless',
+      'WebKitHeadless',
+      'PhantomJS',
+    ],
     logLevel: config.LOG_ERROR,
     singleRun: true,
   });
