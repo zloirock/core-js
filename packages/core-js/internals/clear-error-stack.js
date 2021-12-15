59e4effccdf4ae97aad4230c94bf37da3e28958d
Denis Pushkarev <zloirock@zloirock.ru>
2021-12-15 16:04:20 +0700

    avoiding Safari / FF `Error` stack cleaning since non-standard behavior causes some bugs

--- packages/core-js/internals/clear-error-stack.js
+++ packages/core-js/internals/clear-error-stack.js
@@ -1,20 +1,13 @@
 var uncurryThis = require('../internals/function-uncurry-this');
-var arraySlice = require('../internals/array-slice-simple');
 
 var replace = uncurryThis(''.replace);
-var split = uncurryThis(''.split);
-var join = uncurryThis([].join);
 
 var TEST = (function (arg) { return String(Error(arg).stack); })('zxcasd');
 var V8_OR_CHAKRA_STACK_ENTRY = /\n\s*at [^:]*:[^\n]*/;
 var IS_V8_OR_CHAKRA_STACK = V8_OR_CHAKRA_STACK_ENTRY.test(TEST);
-var IS_FIREFOX_OR_SAFARI_STACK = /@[^\n]*\n/.test(TEST) && !/zxcasd/.test(TEST);
 
 module.exports = function (stack, dropEntries) {
-  if (typeof stack != 'string') return stack;
-  if (IS_V8_OR_CHAKRA_STACK) {
+  if (IS_V8_OR_CHAKRA_STACK && typeof stack == 'string') {
     while (dropEntries--) stack = replace(stack, V8_OR_CHAKRA_STACK_ENTRY, '');
-  } else if (IS_FIREFOX_OR_SAFARI_STACK) {
-    return join(arraySlice(split(stack, '\n'), dropEntries), '\n');
   } return stack;
 };
