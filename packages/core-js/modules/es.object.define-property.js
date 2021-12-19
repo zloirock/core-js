7089ed4e267862c74025b07f595be68ed0f6c68a
Josh Soref <jsoref@users.noreply.github.com>
2021-12-19 09:22:53 -0500

    spelling: module
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- packages/core-js/modules/es.object.define-property.js
+++ packages/core-js/modules/es.object.define-property.js
@@ -1,9 +1,9 @@
 var $ = require('../internals/export');
 var DESCRIPTORS = require('../internals/descriptors');
-var objectDefinePropertyModile = require('../internals/object-define-property');
+var objectDefinePropertyModule = require('../internals/object-define-property');
 
 // `Object.defineProperty` method
 // https://tc39.es/ecma262/#sec-object.defineproperty
 $({ target: 'Object', stat: true, forced: !DESCRIPTORS, sham: !DESCRIPTORS }, {
-  defineProperty: objectDefinePropertyModile.f
+  defineProperty: objectDefinePropertyModule.f
 });
