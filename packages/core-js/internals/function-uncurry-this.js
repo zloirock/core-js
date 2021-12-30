b64a3a9820abbafd26fcfa2313da3f89d4f133a4
zhangenming <282126346@qq.com>
2021-12-30 22:13:04 +0800

    refactor: simplify expression. (#1029)

--- packages/core-js/internals/function-uncurry-this.js
+++ packages/core-js/internals/function-uncurry-this.js
@@ -1,10 +1,10 @@
 var FunctionPrototype = Function.prototype;
 var bind = FunctionPrototype.bind;
 var call = FunctionPrototype.call;
-var callBind = bind && bind.bind(call);
+var callBind = bind && bind.bind(call, call);
 
 module.exports = bind ? function (fn) {
-  return fn && callBind(call, fn);
+  return fn && callBind(fn);
 } : function (fn) {
   return fn && function () {
     return call.apply(fn, arguments);
