28913c681e2c6dfa13dd175a326c8bd69f9902af
zhangenming <282126346@qq.com>
2021-12-30 13:34:07 +0800

    Update function-uncurry-this.js

--- packages/core-js/internals/function-uncurry-this.js
+++ packages/core-js/internals/function-uncurry-this.js
@@ -3,7 +3,9 @@ var bind = FunctionPrototype.bind;
 var call = FunctionPrototype.call;
 var callBind = bind && bind.bind(call, call);
 
-module.exports = bind ? callBind : function (fn) {
+module.exports = bind ? function (fn) {
+  return fn && callBind(fn);
+} : function (fn) {
   return fn && function () {
     return call.apply(fn, arguments);
   };
