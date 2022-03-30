c65106cf7372a1320498ac906745515785f33ef6
Denis Pushkarev <zloirock@zloirock.ru>
2022-03-30 23:40:09 +0700

    improve node runner of compat data tests

--- tests/compat/node-runner.js
+++ tests/compat/node-runner.js
@@ -1,19 +1,25 @@
+/* eslint-disable no-console -- output */
 require('./tests');
 var tests = global.tests;
 var result = {};
 
-for (var key in tests) {
-  var test = tests[key];
+for (var name in tests) {
+  var test = tests[name];
   try {
     if (typeof test == 'function') {
-      result[key] = !!test();
-    } else result[key] = test.reduce(function (accumulator, $test) {
+      result[name] = !!test();
+    } else result[name] = test.reduce(function (accumulator, $test) {
       return accumulator && !!$test();
     }, true);
   } catch (error) {
-    result[key] = false;
+    result[name] = false;
   }
 }
 
-// eslint-disable-next-line no-console -- output
-console.log(JSON.stringify(result, null, '  '));
+if (process.argv.indexOf('--mode=JSON') !== -1) {
+  console.log(JSON.stringify(result, null, '  '));
+} else for (var name in result) {
+  var filled = name + '                                             | '.slice(name.length);
+  if (result[name]) console.log('\u001B[32m' + filled + 'not required\u001B[0m');
+  else console.log('\u001B[31m' + filled + 'required\u001B[0m');
+}
