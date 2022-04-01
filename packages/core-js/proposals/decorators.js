6e684235b1744d179b564d1e9e167e2894c9430f
Denis Pushkarev <zloirock@zloirock.ru>
2022-04-02 05:57:39 +0700

    add a comment

--- packages/core-js/proposals/decorators.js
+++ packages/core-js/proposals/decorators.js
@@ -1,2 +1,4 @@
+// TODO: The metadata API is splitting out into a separate Stage 2 proposal
+// so required a new entry point for it when it will be finally formed
 // https://github.com/tc39/proposal-decorators
 require('../modules/esnext.symbol.metadata');
