f8482ae258381e4367ccc297370830a9c53de958
Denis Pushkarev <zloirock@zloirock.ru>
2022-04-30 11:37:59 +0700

    simplify `define-built-in-accessor`

--- packages/core-js/internals/define-built-in-accessor.js
+++ packages/core-js/internals/define-built-in-accessor.js
@@ -2,7 +2,7 @@ var makeBuiltIn = require('../internals/make-built-in');
 var defineProperty = require('../internals/object-define-property');
 
 module.exports = function (target, name, descriptor) {
-  if (descriptor.get) descriptor.get = makeBuiltIn(descriptor.get, name, { getter: true });
-  if (descriptor.set) descriptor.set = makeBuiltIn(descriptor.set, name, { setter: true });
+  if (descriptor.get) makeBuiltIn(descriptor.get, name, { getter: true });
+  if (descriptor.set) makeBuiltIn(descriptor.set, name, { setter: true });
   return defineProperty.f(target, name, descriptor);
 };
