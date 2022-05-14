a9a619ae72e31db8952e1e304f39f0884c5a5e40
Denis Pushkarev <zloirock@zloirock.ru>
2022-05-15 01:02:26 +0700

    simplify `defineBuiltIn` helper

--- packages/core-js/internals/define-built-in.js
+++ packages/core-js/internals/define-built-in.js
@@ -1,25 +1,20 @@
-var global = require('../internals/global');
 var isCallable = require('../internals/is-callable');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var makeBuiltIn = require('../internals/make-built-in');
 var setGlobal = require('../internals/set-global');
 
 module.exports = function (O, key, value, options) {
-  var unsafe = options ? !!options.unsafe : false;
-  var simple = options ? !!options.enumerable : false;
-  var noTargetGet = options ? !!options.noTargetGet : false;
-  var name = options && options.name !== undefined ? options.name : key;
+  if (!options) options = {};
+  var simple = options.enumerable;
+  var name = options.name !== undefined ? options.name : key;
   if (isCallable(value)) makeBuiltIn(value, name, options);
-  if (O === global) {
+  if (options.global) {
     if (simple) O[key] = value;
     else setGlobal(key, value);
-    return O;
-  } else if (!unsafe) {
-    delete O[key];
-  } else if (!noTargetGet && O[key]) {
-    simple = true;
-  }
-  if (simple) O[key] = value;
-  else createNonEnumerableProperty(O, key, value);
-  return O;
+  } else {
+    if (!options.unsafe) delete O[key];
+    else if (O[key]) simple = true;
+    if (simple) O[key] = value;
+    else createNonEnumerableProperty(O, key, value);
+  } return O;
 };
