90a57dd0be3052cdc57b6613d7f5ce2b48d1ce27
Denis Pushkarev <zloirock@zloirock.ru>
2022-05-17 16:59:48 +0700

    minor stylistic change

--- packages/core-js/internals/add-to-unscopables.js
+++ packages/core-js/internals/add-to-unscopables.js
@@ -1,6 +1,6 @@
 var wellKnownSymbol = require('../internals/well-known-symbol');
 var create = require('../internals/object-create');
-var definePropertyModule = require('../internals/object-define-property');
+var defineProperty = require('../internals/object-define-property').f;
 
 var UNSCOPABLES = wellKnownSymbol('unscopables');
 var ArrayPrototype = Array.prototype;
@@ -8,7 +8,7 @@ var ArrayPrototype = Array.prototype;
 // Array.prototype[@@unscopables]
 // https://tc39.es/ecma262/#sec-array.prototype-@@unscopables
 if (ArrayPrototype[UNSCOPABLES] == undefined) {
-  definePropertyModule.f(ArrayPrototype, UNSCOPABLES, {
+  defineProperty(ArrayPrototype, UNSCOPABLES, {
     configurable: true,
     value: create(null)
   });
