c0508b57fd118c394231c0c3d087176a6f15db34
Denis Pushkarev <zloirock@zloirock.ru>
2022-06-15 00:43:12 +0700

    disable forced replacement of `Math` constants from proposals since anyway they are non-writable non-configurable

--- packages/core-js/modules/esnext.math.deg-per-rad.js
+++ packages/core-js/modules/esnext.math.deg-per-rad.js
@@ -1,9 +1,7 @@
 var $ = require('../internals/export');
 
-var DEG_PER_RAD = Math.PI / 180;
-
 // `Math.DEG_PER_RAD` constant
 // https://rwaldron.github.io/proposal-math-extensions/
-$({ target: 'Math', stat: true, nonConfigurable: true, nonWritable: true, forced: Math.DEG_PER_RAD !== DEG_PER_RAD }, {
-  DEG_PER_RAD: DEG_PER_RAD
+$({ target: 'Math', stat: true, nonConfigurable: true, nonWritable: true }, {
+  DEG_PER_RAD: Math.PI / 180
 });
--- packages/core-js/modules/esnext.math.rad-per-deg.js
+++ packages/core-js/modules/esnext.math.rad-per-deg.js
@@ -1,9 +1,7 @@
 var $ = require('../internals/export');
 
-var RAD_PER_DEG = 180 / Math.PI;
-
 // `Math.RAD_PER_DEG` constant
 // https://rwaldron.github.io/proposal-math-extensions/
-$({ target: 'Math', stat: true, nonConfigurable: true, nonWritable: true, forced: Math.RAD_PER_DEG !== RAD_PER_DEG }, {
-  RAD_PER_DEG: RAD_PER_DEG
+$({ target: 'Math', stat: true, nonConfigurable: true, nonWritable: true }, {
+  RAD_PER_DEG: 180 / Math.PI
 });
