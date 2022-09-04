b534518abce134c915860c2a4528337f8d041dbf
Denis Pushkarev <zloirock@zloirock.ru>
2022-09-05 04:00:34 +0700

    just in case detect correctness of `globalThis`

--- packages/core-js/modules/es.global-this.js
+++ packages/core-js/modules/es.global-this.js
@@ -3,6 +3,6 @@ var global = require('../internals/global');
 
 // `globalThis` object
 // https://tc39.es/ecma262/#sec-globalthis
-$({ global: true }, {
+$({ global: true, forced: global.globalThis !== global }, {
   globalThis: global
 });
