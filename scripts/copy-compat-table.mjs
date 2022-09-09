8f4ea364769f5f328925b5dbc65b52a2da86255c
Denis Pushkarev <zloirock@zloirock.ru>
2022-09-10 02:23:40 +0700

    simplify a script

--- scripts/copy-compat-table.mjs
+++ scripts/copy-compat-table.mjs
@@ -1,10 +1,8 @@
-const files = [
+await Promise.all([
   'browsers-runner.js',
   'compat-data.js',
   'tests.js',
   'index.html',
-];
-
-for (const file of files) await fs.copy(`tests/compat/${ file }`, `docs/compat/${ file }`);
+].map(file => fs.copy(`tests/compat/${ file }`, `docs/compat/${ file }`)))
 
 echo(chalk.green('compat table copied'));
