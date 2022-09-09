b243b0b07db6053cc63d93d605f352de32c06531
Denis Pushkarev <zloirock@zloirock.ru>
2022-09-10 02:37:38 +0700

    fix linting

--- scripts/copy-compat-table.mjs
+++ scripts/copy-compat-table.mjs
@@ -3,6 +3,6 @@ await Promise.all([
   'compat-data.js',
   'tests.js',
   'index.html',
-].map(file => fs.copy(`tests/compat/${ file }`, `docs/compat/${ file }`)))
+].map(file => fs.copy(`tests/compat/${ file }`, `docs/compat/${ file }`)));
 
 echo(chalk.green('compat table copied'));
