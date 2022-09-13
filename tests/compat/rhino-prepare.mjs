c8f73d0029cc8a5d56b4b479217d5d7cb4b53ff3
Denis Pushkarev <zloirock@zloirock.ru>
2022-09-14 00:48:32 +0700

    some `eslint` config fixes

--- tests/compat/rhino-prepare.mjs
+++ tests/compat/rhino-prepare.mjs
@@ -1,7 +1,7 @@
 const { version } = argv;
 
 const rhino = await fetch(
-  `https://github.com/mozilla/rhino/releases/download/Rhino${ version.replace(/\./g, '_') }_Release/rhino-${ version }.jar`
+  `https://github.com/mozilla/rhino/releases/download/Rhino${ version.replace(/\./g, '_') }_Release/rhino-${ version }.jar`,
 );
 
 await fs.writeFile('tests/compat/rhino.jar', Buffer.from(await rhino.arrayBuffer()));
