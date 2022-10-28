f4ea48469060d982c98a9f914342285118c6da4a
Denis Pushkarev <zloirock@zloirock.ru>
2022-10-29 02:44:45 +0700

    fix timeout param

--- tests/promises/runner.mjs
+++ tests/promises/runner.mjs
@@ -3,5 +3,5 @@ cd('tests/promises');
 await $`npm i --no-audit --no-fund --loglevel=error`;
 
 for (const set of ['aplus', 'es6']) {
-  await $`npx promises-${ set }-tests adapter --timeout=1000 --color`;
+  await $`npx promises-${ set }-tests adapter --timeout 1000 --color`;
 }
