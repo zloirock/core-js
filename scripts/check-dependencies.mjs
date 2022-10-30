3ea7e65266eaee54a9108fdf5f23ccd6b61395ac
Denis Pushkarev <zloirock@zloirock.ru>
2022-10-30 12:32:43 +0700

    replace `david` with `updates`

--- scripts/check-dependencies.mjs
+++ scripts/check-dependencies.mjs
@@ -1,41 +1,33 @@
-import { promisify } from 'util';
-import david from 'david';
-import semver from 'semver';
-
-const { eq, coerce, minVersion, validRange } = semver;
-const getDependencies = promisify(david.getDependencies);
-
-const ignoreEverywhere = new Set([
-  'moon-unit',
-]);
-
-const ignoreInPackages = new Set([
-  ...ignoreEverywhere,
-  'mkdirp',
-  'webpack',
-]);
+const ignore = {
+  'core-js-builder': [
+    'mkdirp',
+    'webpack',
+  ],
+  'tests/observables': [
+    'moon-unit',
+  ],
+};
 
 await Promise.all((await glob(['package.json', '@(packages|scripts|tests)/*/package.json'])).map(async path => {
   const pkg = await fs.readJson(path);
-  const dependencies = await getDependencies({
-    dependencies: {
-      ...pkg.dependencies,
-      ...pkg.devDependencies,
-    },
-  });
-  const ignore = path === 'package.json' ? ignoreEverywhere : ignoreInPackages;
-  for (const name of Object.keys(dependencies)) {
-    if (ignore.has(name)) {
-      delete dependencies[name];
-      continue;
-    }
-    const { required, stable, warn } = dependencies[name];
-    if (!validRange(required) || warn || eq(minVersion(required), coerce(stable))) {
-      delete dependencies[name];
-    }
-  }
+  if (!pkg.dependencies && !pkg.devDependencies) return;
+
+  const name = pkg.name ?? 'root';
+  const exclude = ignore[name];
+
+  $.verbose = false;
+
+  const { stdout } = await $`npx updates \
+    --json \
+    --file ${ path } \
+    --exclude ${ Array.isArray(exclude) ? exclude.join(',') : '' } \
+  `;
+
+  const { results } = JSON.parse(stdout);
+  const dependencies = { ...results.dependencies, ...results.devDependencies };
+
   if (Object.keys(dependencies).length) {
-    echo(chalk.cyan(`${ pkg.name ?? 'root' }:`));
+    echo(chalk.cyan(`${ name }:`));
     console.table(dependencies);
   }
 }));
