64b71d3155b02cefd2ac4cc04b4913e75e7123fa
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-03 00:26:08 +0700

    fix `global` -> `globalThis` for consistency

--- tests/observables/adapter.mjs
+++ tests/observables/adapter.mjs
@@ -6,8 +6,8 @@ const pkg = argv.pure ? 'core-js-pure' : 'core-js';
 // eslint-disable-next-line import/no-unresolved -- generated later
 const { runTests } = require('./bundles/observables-tests/default');
 
-global.Symbol = require(`../../packages/${ pkg }/full/symbol`);
-global.Promise = require(`../../packages/${ pkg }/full/promise`);
+globalThis.Symbol = require(`../../packages/${ pkg }/full/symbol`);
+globalThis.Promise = require(`../../packages/${ pkg }/full/promise`);
 const Observable = require(`../../packages/${ pkg }/full/observable`);
 
 runTests(Observable);
