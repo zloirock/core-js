43e759b2b7fa8f968eb6638e1902618ca6a3cdf1
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-11 02:45:34 +0700

    temporarily hardcode the current location

--- scripts/zxi.mjs
+++ scripts/zxi.mjs
@@ -1,4 +1,4 @@
-const { delimiter, dirname, join, normalize, relative } = path;
+const { delimiter, dirname, normalize } = path;
 const { pathExists } = fs;
 const { cwd, env } = process;
 const { _: args } = argv;
@@ -9,7 +9,6 @@ const CD = FILE === 'cd';
 if (CD) FILE = args.shift();
 
 const DIR = dirname(FILE);
-const SCRIPT = join('./', relative(dirname(import.meta.url.slice(7)), ROOT), FILE);
 const PKG = pathExists(`${ DIR }/package.json`);
 
 if (PKG) {
@@ -26,4 +25,4 @@ if (PKG) {
 
 env.FORCE_COLOR = '1';
 
-await import(SCRIPT);
+await import(`../${ FILE }`);
