a0a0719edf29b65b94ee82c6ebb683a2e3c226c7
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-13 01:58:49 +0700

    avoid extra `cd` in `zxi`

--- scripts/zxi.mjs
+++ scripts/zxi.mjs
@@ -2,26 +2,22 @@ const { delimiter, dirname, normalize } = path;
 const { pathExists } = fs;
 const { cwd, env } = process;
 const { _: args } = argv;
-const ROOT = cwd();
 let FILE = args.shift();
 const CD = FILE === 'cd';
 
 if (CD) FILE = args.shift();
 
 const DIR = dirname(FILE);
-const PKG = pathExists(`${ DIR }/package.json`);
 
-if (PKG) {
-  cd(DIR);
+if (await pathExists(`${ DIR }/package.json`)) {
+  await $`npm --prefix ${ DIR } install --no-audit --no-fund --loglevel=error`;
 
-  await $`npm install --no-audit --no-fund --loglevel=error`;
+  const BIN = normalize(`${ cwd() }/${ DIR }/node_modules/.bin`);
 
-  const BIN = normalize(`${ cwd() }/node_modules/.bin`);
+  if (await pathExists(BIN)) env.PATH = `${ BIN }${ delimiter }${ env.PATH }`;
+}
 
-  if (pathExists(BIN)) env.PATH = `${ BIN }${ delimiter }${ env.PATH }`;
-
-  if (!CD) cd(ROOT);
-} else if (CD) cd(DIR);
+if (CD) cd(DIR);
 
 env.FORCE_COLOR = '1';
 
