d22b8359b61ce13a1fc567f8035c009f7f8eb080
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-20 02:18:43 +0700

    add one more link

--- packages/core-js-compat/src/mapping.mjs
+++ packages/core-js-compat/src/mapping.mjs
@@ -212,6 +212,7 @@ export default {
   // https://developer.oculus.com/documentation/web/browser-release-notes/
   // Need to find another data source since release notes are not available for new versions.
   // https://github.com/mdn/browser-compat-data/blob/main/browsers/oculus.json
+  // https://whatmyuseragent.com/browser/oc/oculus-browser
   ChromeAndroidToQuest: [
     [57, '3.0'],
     [61, '4.0'],
