a121a768fbbbf908c02583b71d8e8ed15680414d
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-24 00:58:49 +0700

    add `String.dedent`

---
+++ packages/core-js/internals/string-cooked.js
@@ -0,0 +1,24 @@
+var uncurryThis = require('../internals/function-uncurry-this');
+var toIndexedObject = require('../internals/to-indexed-object');
+var toString = require('../internals/to-string');
+var lengthOfArrayLike = require('../internals/length-of-array-like');
+
+var $TypeError = TypeError;
+var ArrayPrototype = Array.prototype;
+var push = uncurryThis(ArrayPrototype.push);
+var join = uncurryThis(ArrayPrototype.join);
+
+module.exports = function cooked(template /* , ...substitutions */) {
+  var cookedTemplate = toIndexedObject(template);
+  var literalSegments = lengthOfArrayLike(cookedTemplate);
+  var argumentsLength = arguments.length;
+  var elements = [];
+  var i = 0;
+  while (literalSegments > i) {
+    var nextVal = cookedTemplate[i++];
+    if (nextVal === undefined) throw $TypeError('Incorrect template');
+    push(elements, toString(nextVal));
+    if (i === literalSegments) return join(elements, '');
+    if (i < argumentsLength) push(elements, toString(arguments[i]));
+  }
+};
--- packages/core-js/modules/esnext.string.cooked.js
+++ packages/core-js/modules/esnext.string.cooked.js
@@ -1,29 +1,8 @@
 var $ = require('../internals/export');
-var uncurryThis = require('../internals/function-uncurry-this');
-var toIndexedObject = require('../internals/to-indexed-object');
-var toString = require('../internals/to-string');
-var lengthOfArrayLike = require('../internals/length-of-array-like');
-
-var $TypeError = TypeError;
-var ArrayPrototype = Array.prototype;
-var push = uncurryThis(ArrayPrototype.push);
-var join = uncurryThis(ArrayPrototype.join);
+var cooked = require('../internals/string-cooked');
 
 // `String.cooked` method
 // https://github.com/tc39/proposal-string-cooked
 $({ target: 'String', stat: true, forced: true }, {
-  cooked: function cooked(template /* , ...substitutions */) {
-    var cookedTemplate = toIndexedObject(template);
-    var literalSegments = lengthOfArrayLike(cookedTemplate);
-    var argumentsLength = arguments.length;
-    var elements = [];
-    var i = 0;
-    while (literalSegments > i) {
-      var nextVal = cookedTemplate[i++];
-      if (nextVal === undefined) throw $TypeError('Incorrect template');
-      push(elements, toString(nextVal));
-      if (i === literalSegments) return join(elements, '');
-      if (i < argumentsLength) push(elements, toString(arguments[i]));
-    }
-  }
+  cooked: cooked
 });
