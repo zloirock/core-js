6deb74dba5a4f9bb981cb1e6513f3551ec1f58d7
Denis Pushkarev <zloirock@zloirock.ru>
2022-11-29 00:06:38 +0700

    some stylistic changes

--- scripts/bundle-tests/bundle-tests.mjs
+++ scripts/bundle-tests/bundle-tests.mjs
@@ -1,4 +1,7 @@
 await Promise.all([
   ['unit-global/index', 'unit-global'],
   ['unit-pure/index', 'unit-pure'],
-].map(([entry, output]) => $`webpack --entry ../../tests/${ entry }.js --output-filename ${ output }.js`));
+].map(([entry, output]) => $`webpack \
+  --entry ../../tests/${ entry }.js \
+  --output-filename ${ output }.js \
+`));
