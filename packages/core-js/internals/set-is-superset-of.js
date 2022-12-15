d410d8e2909f9fc86424f03c474645e9f2a8dfb9
Denis Pushkarev <zloirock@zloirock.ru>
2022-12-15 11:44:00 +0700

    smooth out a spec draft inconsistency between `Map.keyBy` and `Map.groupBy`

--- packages/core-js/internals/set-is-superset-of.js
+++ packages/core-js/internals/set-is-superset-of.js
@@ -1,11 +1,11 @@
 'use strict';
 var SetHelpers = require('../internals/set-helpers');
+var size = require('../internals/set-size');
 var getSetRecord = require('../internals/get-set-record');
 var iterateSimple = require('../internals/iterate-simple');
 
 var aSet = SetHelpers.aSet;
 var has = SetHelpers.has;
-var size = SetHelpers.size;
 
 // `Set.prototype.isSupersetOf` method
 // https://tc39.github.io/proposal-set-methods/#Set.prototype.isSupersetOf
--- packages/core-js/modules/esnext.map.group-by.js
+++ packages/core-js/modules/esnext.map.group-by.js
@@ -3,7 +3,6 @@ var $ = require('../internals/export');
 var call = require('../internals/function-call');
 var uncurryThis = require('../internals/function-uncurry-this');
 var aCallable = require('../internals/a-callable');
-var getIterator = require('../internals/get-iterator');
 var iterate = require('../internals/iterate');
 
 var push = uncurryThis([].push);
@@ -12,17 +11,16 @@ var push = uncurryThis([].push);
 // https://github.com/tc39/proposal-collection-methods
 $({ target: 'Map', stat: true, forced: true }, {
   groupBy: function groupBy(iterable, keyDerivative) {
-    aCallable(keyDerivative);
-    var iterator = getIterator(iterable);
     var newMap = new this();
+    aCallable(keyDerivative);
     var has = aCallable(newMap.has);
     var get = aCallable(newMap.get);
     var set = aCallable(newMap.set);
-    iterate(iterator, function (element) {
+    iterate(iterable, function (element) {
       var derivedKey = keyDerivative(element);
       if (!call(has, newMap, derivedKey)) call(set, newMap, derivedKey, [element]);
       else push(call(get, newMap, derivedKey), element);
-    }, { IS_ITERATOR: true });
+    });
     return newMap;
   }
 });
