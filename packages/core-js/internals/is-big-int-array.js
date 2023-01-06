46495acdeca498f20ffb482bbaac92e5411b9a66
Denis Pushkarev <zloirock@zloirock.ru>
2023-01-07 06:14:01 +0700

    make `is-big-int-array` a little stricter

--- packages/core-js/internals/is-big-int-array.js
+++ packages/core-js/internals/is-big-int-array.js
@@ -1,8 +1,6 @@
 var classof = require('../internals/classof');
-var uncurryThis = require('../internals/function-uncurry-this');
-
-var slice = uncurryThis(''.slice);
 
 module.exports = function (it) {
-  return slice(classof(it), 0, 3) === 'Big';
+  var klass = classof(it);
+  return klass == 'BigInt64Array' || klass == 'BigUint64Array';
 };
