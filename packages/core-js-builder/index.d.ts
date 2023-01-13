aa72c0529cc2d5621e3ea8d3b1a7de830916c42a
Denis Pushkarev <zloirock@zloirock.ru>
2023-01-14 03:41:45 +0700

    return some `tsdoc` comments

--- packages/core-js-builder/index.d.ts
+++ packages/core-js-builder/index.d.ts
@@ -42,16 +42,25 @@ type SummaryEntry = boolean | {
 };
 
 type Summary = {
+  /** in the console, you could specify required parts or set `true` for enable all of them */
   comment?: SummaryEntry,
+  /** in the comment, you could specify required parts or set `true` for enable all of them */
   console?: SummaryEntry,
 };
 
 type Options = {
+  /** entry / module / namespace / an array of them, by default - all `core-js` modules */
   modules?: Modules,
+  /** a blacklist, entry / module / namespace / an array of them, by default - empty list */
   exclude?: Modules,
+  /** optional browserslist or core-js-compat format query */
   targets?: Targets | BrowserslistQuery,
+  /** output format, 'bundle' by default, can be 'cjs' or 'esm', and in this case
+   *  the result will not be bundled and will contain imports of required modules */
   format?: Format,
+  /** optional target filename, if it's missed a file will not be created */
   filename?: string,
+  /** shows summary for the bundle, disabled by default */
   summary?: Summary,
 };
 
