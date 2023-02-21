1fc7ae42c5274fbb54f9405d306f83f07f6b947c
Denis Pushkarev <zloirock@zloirock.ru>
2023-02-22 02:47:14 +0700

    add a link to issue
    https://github.com/nodejs/node/issues/46755

--- tests/unit-global/web.url.js
+++ tests/unit-global/web.url.js
@@ -42,6 +42,7 @@ QUnit.test('URL constructor', assert => {
   assert.throws(() => new URL(), 'TypeError: Failed to construct URL: 1 argument required, but only 0 present.');
   assert.throws(() => new URL(''), 'TypeError: Failed to construct URL: Invalid URL');
   // Node 19.7
+  // https://github.com/nodejs/node/issues/46755
   // assert.throws(() => new URL('', 'about:blank'), 'TypeError: Failed to construct URL: Invalid URL');
   assert.throws(() => new URL('abc'), 'TypeError: Failed to construct URL: Invalid URL');
   assert.throws(() => new URL('//abc'), 'TypeError: Failed to construct URL: Invalid URL');
@@ -115,6 +116,7 @@ QUnit.test('URL#href', assert => {
     url = new URL('file:///var/log/system.log');
     url.href = 'http://0300.168.0xF0';
     // Node 19.7
+    // https://github.com/nodejs/node/issues/46755
     // assert.same(url.href, 'http://192.168.0.240/', 'file -> http');
     // assert.same(String(url), 'http://192.168.0.240/', 'file -> http');
 
