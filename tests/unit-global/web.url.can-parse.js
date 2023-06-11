0f8c5831c2ce1a550b82d920ba0bc5f00c647139
Denis Pushkarev <zloirock@zloirock.ru>
2023-06-12 02:35:21 +0700

    fix a typo

--- tests/unit-global/web.url.can-parse.js
+++ tests/unit-global/web.url.can-parse.js
@@ -14,8 +14,8 @@ QUnit.test('URL.canParse', assert => {
   assert.true(canParse('q:w', undefined), 'q:w, undefined');
   // assert.false(canParse(undefined, 'q:w'), 'undefined, q:w'); // fails in Chromium on Windows
   assert.true(canParse('q:/w'), 'q:/w');
-  assert.true(canParse('q:/w', undefined), 'q:w, undefined');
-  assert.true(canParse(undefined, 'q:/w'), 'undefined, q:w');
+  assert.true(canParse('q:/w', undefined), 'q:/w, undefined');
+  assert.true(canParse(undefined, 'q:/w'), 'undefined, q:/w');
   assert.false(canParse('https://login:password@examp:le.com:8080/?a=1&b=2&a=3&c=4#fragment'), 'https://login:password@examp:le.com:8080/?a=1&b=2&a=3&c=4#fragment');
   assert.true(canParse('https://login:password@example.com:8080/?a=1&b=2&a=3&c=4#fragment'), 'https://login:password@example.com:8080/?a=1&b=2&a=3&c=4#fragment');
   assert.true(canParse('https://login:password@example.com:8080/?a=1&b=2&a=3&c=4#fragment', undefined), 'https://login:password@example.com:8080/?a=1&b=2&a=3&c=4#fragment, undefined');
