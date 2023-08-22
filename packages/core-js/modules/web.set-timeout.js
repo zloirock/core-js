a1e734431fc8d4d269e9d61eea63a912f882a3eb
Philip White <pmw@amazon.com>
2023-08-22 09:18:03 -0700

    Add `clearTimeout`
    The functionality already existed and was tested by unit tests.
    This change exports it and clarifies misleading descriptions of its unit tests.

--- packages/core-js/modules/web.set-timeout.js
+++ packages/core-js/modules/web.set-timeout.js
@@ -4,9 +4,14 @@ var global = require('../internals/global');
 var schedulersFix = require('../internals/schedulers-fix');
 
 var setTimeout = schedulersFix(global.setTimeout, true);
+var clearTimeout = schedulersFix(global.clearTimeout, true);
 
 // Bun / IE9- setTimeout additional parameters fix
 // https://html.spec.whatwg.org/multipage/timers-and-user-prompts.html#dom-settimeout
 $({ global: true, bind: true, forced: global.setTimeout !== setTimeout }, {
   setTimeout: setTimeout
+});
+
+$({ global: true, bind: true, forced: global.clearTimeout !== clearTimeout }, {
+  clearTimeout: clearTimeout
 });
