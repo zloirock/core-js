95fc11e177e16230f0df4356b3aa43b8b183c019
Denis Pushkarev <zloirock@zloirock.ru>
2023-09-25 02:05:07 +0700

    unify `Math.f16round` and `Math.fround`

--- packages/core-js/internals/math-f16round.js
+++ packages/core-js/internals/math-f16round.js
@@ -1,29 +1,12 @@
 'use strict';
-var sign = require('../internals/math-sign');
+var floatRound = require('../internals/math-float-round');
 
-var abs = Math.abs;
-
-var EPSILON = 2.220446049250313e-16; // Number.EPSILON
-var EPSILON16 = 0.0009765625; // 2 ** -10
-var INVERSE_EPSILON = 1 / EPSILON;
-var MAX16 = 65504; // 2 ** 15 - 2 ** 10
-var MIN16 = 6.103515625e-05; // 2 ** -14
-
-var roundTiesToEven = function (n) {
-  return n + INVERSE_EPSILON - INVERSE_EPSILON;
-};
+var FLOAT16_EPSILON = 0.0009765625;
+var FLOAT16_MAX_VALUE = 65504;
+var FLOAT16_MIN_VALUE = 6.103515625e-05;
 
 // `Math.f16round` method implementation
 // https://github.com/tc39/proposal-float16array
 module.exports = Math.f16round || function f16round(x) {
-  var n = +x;
-  var $abs = abs(n);
-  var $sign = sign(n);
-  var a, result;
-  if ($abs < MIN16) return $sign * roundTiesToEven($abs / MIN16 / EPSILON16) * MIN16 * EPSILON16;
-  a = (1 + EPSILON16 / EPSILON) * $abs;
-  result = a - (a - $abs);
-  // eslint-disable-next-line no-self-compare -- NaN check
-  if (result > MAX16 || result !== result) return $sign * Infinity;
-  return $sign * result;
+  return floatRound(x, FLOAT16_EPSILON, FLOAT16_MAX_VALUE, FLOAT16_MIN_VALUE);
 };
--- packages/core-js/internals/math-fround.js
+++ packages/core-js/internals/math-fround.js
@@ -1,30 +1,13 @@
 'use strict';
-var sign = require('../internals/math-sign');
+var floatRound = require('../internals/math-float-round');
 
-var abs = Math.abs;
-
-var EPSILON = 2.220446049250313e-16; // Number.EPSILON
-var EPSILON32 = 1.1920928955078125e-7; // 2 ** -23;
-var INVERSE_EPSILON = 1 / EPSILON;
-var MAX32 = 3.4028234663852886e+38; // 2 ** 128 - 2 ** 104
-var MIN32 = 1.1754943508222875e-38; // 2 ** -126;
-
-var roundTiesToEven = function (n) {
-  return n + INVERSE_EPSILON - INVERSE_EPSILON;
-};
+var FLOAT32_EPSILON = 1.1920928955078125e-7; // 2 ** -23;
+var FLOAT32_MAX_VALUE = 3.4028234663852886e+38; // 2 ** 128 - 2 ** 104
+var FLOAT32_MIN_VALUE = 1.1754943508222875e-38; // 2 ** -126;
 
 // `Math.fround` method implementation
 // https://tc39.es/ecma262/#sec-math.fround
 // eslint-disable-next-line es/no-math-fround -- safe
 module.exports = Math.fround || function fround(x) {
-  var n = +x;
-  var $abs = abs(n);
-  var $sign = sign(n);
-  var a, result;
-  if ($abs < MIN32) return $sign * roundTiesToEven($abs / MIN32 / EPSILON32) * MIN32 * EPSILON32;
-  a = (1 + EPSILON32 / EPSILON) * $abs;
-  result = a - (a - $abs);
-  // eslint-disable-next-line no-self-compare -- NaN check
-  if (result > MAX32 || result !== result) return $sign * Infinity;
-  return $sign * result;
+  return floatRound(x, FLOAT32_EPSILON, FLOAT32_MAX_VALUE, FLOAT32_MIN_VALUE);
 };
