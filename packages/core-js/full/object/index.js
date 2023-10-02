6558061baf3c5331c10aeae58869d48f760bfa78
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 05:19:07 +0700

    drop `esnext.object.iterate-keys` (withdrawn)

--- packages/core-js/full/object/index.js
+++ packages/core-js/full/object/index.js
@@ -1,7 +1,6 @@
 'use strict';
 var parent = require('../../actual/object');
 // TODO: Remove from `core-js@4`
-require('../../modules/esnext.object.iterate-keys');
 require('../../modules/esnext.object.iterate-values');
 
 module.exports = parent;
