1442de133bb9c0b385d0d947a84759ee8fae4ad6
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:37:44 +0700

    drop `esnext.promise.any` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2764,8 +2764,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.promise.all-settled': null,
   // TODO: Remove from `core-js@4`
-  'esnext.promise.any': null,
-  // TODO: Remove from `core-js@4`
   'esnext.promise.try': null,
   // TODO: Remove from `core-js@4`
   'esnext.promise.with-resolvers': null,
@@ -3258,7 +3256,6 @@ export const renamed = new Map([
   ['esnext.object.has-own', 'es.object.has-own'],
   ['esnext.object.group-by', 'es.object.group-by'],
   ['esnext.promise.all-settled', 'es.promise.all-settled'],
-  ['esnext.promise.any', 'es.promise.any'],
   ['esnext.promise.try', 'es.promise.try'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.regexp.escape', 'es.regexp.escape'],
