8a6c84ddff674a3270fc1d77777e7438f8ca8e3d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:08:58 +0700

    drop `esnext.array.with` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2407,8 +2407,6 @@ export const data = {
   'esnext.array.unique-by': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.array.with': null,
-  // TODO: Remove from `core-js@4`
   'esnext.array-buffer.detached': null,
   // TODO: Remove from `core-js@4`
   'esnext.array-buffer.transfer': null,
@@ -3116,7 +3114,6 @@ export const renamed = new Map([
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
   ['esnext.array.from-async', 'es.array.from-async'],
-  ['esnext.array.with', 'es.array.with'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
   ['esnext.array-buffer.transfer', 'es.array-buffer.transfer'],
   ['esnext.array-buffer.transfer-to-fixed-length', 'es.array-buffer.transfer-to-fixed-length'],
