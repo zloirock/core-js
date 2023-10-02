37a5305c347edf328828dc8a69a6344d819c057b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:59:46 +0700

    drop `esnext.string.match-all` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2324,8 +2324,6 @@ export const data = {
   'esnext.string.dedent': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.string.match-all': null,
-  // TODO: Remove from `core-js@4`
   'esnext.string.replace-all': null,
   'esnext.symbol.async-dispose': {
     bun: '1.0.23',
@@ -2637,7 +2635,6 @@ export const renamed = new Map([
   ['esnext.map.group-by', 'es.map.group-by'],
   ['esnext.object.group-by', 'es.object.group-by'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
-  ['esnext.string.match-all', 'es.string.match-all'],
   ['esnext.string.replace-all', 'es.string.replace-all'],
   ['web.url', 'web.url.constructor'],
   ['web.url-search-params', 'web.url-search-params.constructor'],
