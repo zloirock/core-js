2db212eab3f8f2e08f50326f6324881392eb14b3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:08:58 +0700

    drop `esnext.array.with` (moved to stable es)

--- packages/core-js/actual/array/virtual/with.js
+++ packages/core-js/actual/array/virtual/with.js
@@ -1,6 +1,4 @@
 'use strict';
 var parent = require('../../../stable/array/virtual/with');
-// TODO: Remove from `core-js@4`
-require('../../../modules/esnext.array.with');
 
 module.exports = parent;
