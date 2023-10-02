911957f8bca873ac06290a95232c25eaa8a1a78e
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 03:55:17 +0700

    drop `esnext.array.to-sorted` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1930,8 +1930,6 @@ export const data = {
   'esnext.array.last-item': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.array.to-sorted': null,
-  // TODO: Remove from `core-js@4`
   'esnext.array.to-spliced': null,
   'esnext.array.unique-by': {
   },
@@ -2654,7 +2652,6 @@ export const renamed = new Map([
   ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
-  ['esnext.array.to-sorted', 'es.array.to-sorted'],
   ['esnext.array.to-spliced', 'es.array.to-spliced'],
   ['esnext.array.with', 'es.array.with'],
   ['esnext.global-this', 'es.global-this'],
