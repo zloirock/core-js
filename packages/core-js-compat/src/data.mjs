d8330394a7f590ef79a55d70f8d6c5b7507199bc
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:06:30 +0700

    drop `esnext.math.isubh` (withdrawn)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2346,9 +2346,6 @@ export const data = {
     firefox: '129',
     safari: '18.2',
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.isubh': {
-  },
   'esnext.math.rad-per-deg': {
   },
   'esnext.math.radians': {
