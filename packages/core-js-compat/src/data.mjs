c329373285917156034e1b730b2c2c5a4839b59c
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 05:43:58 +0700

    drop `esnext.math.seeded-prng` (changing of the API, waiting for the spec text)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2705,9 +2705,6 @@ export const data = {
   },
   'esnext.math.scale': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.seeded-prng': {
-  },
   'esnext.math.signbit': {
   },
   // TODO: Remove from `core-js@4`
