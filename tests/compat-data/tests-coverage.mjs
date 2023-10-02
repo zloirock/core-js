93a7e3ef7453d7431707d1b986c420459b0b7dc3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 05:43:58 +0700

    drop `esnext.math.seeded-prng` (changing of the API, waiting for the spec text)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -35,7 +35,6 @@ const ignore = new Set([
   'esnext.math.iaddh',
   'esnext.math.imulh',
   'esnext.math.isubh',
-  'esnext.math.seeded-prng',
   'esnext.math.umulh',
   'esnext.number.range',
   'esnext.observable',
