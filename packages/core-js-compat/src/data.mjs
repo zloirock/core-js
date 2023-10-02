22e66dad96717d213ebf3edc9d71d1c814585ed9
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:50:53 +0700

    drop `esnext.array.at` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2470,8 +2470,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.array.from-async': null,
   // TODO: Remove from `core-js@4`
-  'esnext.array.at': null,
-  // TODO: Remove from `core-js@4`
   'esnext.array.filter-out': {
   },
   'esnext.array.filter-reject': {
@@ -3209,7 +3207,6 @@ export const renamed = new Map([
   ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
-  ['esnext.array.at', 'es.array.at'],
   ['esnext.array.find-last', 'es.array.find-last'],
   ['esnext.array.find-last-index', 'es.array.find-last-index'],
   ['esnext.array.from-async', 'es.array.from-async'],
