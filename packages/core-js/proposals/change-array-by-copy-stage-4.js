5099330a12321e55f851cee8d00f0bdda34e036b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:21:30 +0700

    drop `esnext.typed-array.to-sorted` (moved to stable es)

--- packages/core-js/proposals/change-array-by-copy-stage-4.js
+++ packages/core-js/proposals/change-array-by-copy-stage-4.js
@@ -5,5 +5,5 @@ require('../modules/es.array.to-sorted');
 require('../modules/es.array.to-spliced');
 require('../modules/es.array.with');
 require('../modules/es.typed-array.to-reversed');
-require('../modules/esnext.typed-array.to-sorted');
+require('../modules/es.typed-array.to-sorted');
 require('../modules/esnext.typed-array.with');
