44c9b7d360939dd032a66ac57638f3c4dc6fb7a0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:26:54 +0700

    drop `esnext.string.at` (old version, replace by module with the same name with actual behavior)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -53,7 +53,6 @@ const ignore = new Set([
   'esnext.set.is-superset-of',
   'esnext.set.symmetric-difference',
   'esnext.set.union',
-  'esnext.string.at',
   'esnext.symbol.is-registered',
   'esnext.symbol.is-well-known',
   'esnext.symbol.matcher',
