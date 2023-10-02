bdeaf5b0962ad9add9d4795c11eee67c0a5bad89
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:13:45 +0700

    drop `esnext.aggregate-error` (moved to stable es)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -15,7 +15,6 @@ const ignore = new Set([
   'es.set',
   'es.weak-map',
   'es.weak-set',
-  'esnext.aggregate-error',
   'esnext.array.filter-out',
   'esnext.array.group',
   'esnext.array.group-by',
