96463c27ffad4e6029df150c06526a5c23e94dd4
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:59:46 +0700

    drop `esnext.string.match-all` (moved to stable es)

--- packages/core-js/full/string/virtual/index.js
+++ packages/core-js/full/string/virtual/index.js
@@ -4,7 +4,6 @@ var parent = require('../../../actual/string/virtual');
 require('../../../modules/esnext.string.at');
 require('../../../modules/esnext.string.code-points');
 // TODO: remove from `core-js@4`
-require('../../../modules/esnext.string.match-all');
 require('../../../modules/esnext.string.replace-all');
 
 module.exports = parent;
