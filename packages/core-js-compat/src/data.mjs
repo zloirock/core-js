80e3529ad17caac24011d9377360a8f69555637a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:54:05 +0700

    drop `esnext.string.to-well-formed` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2587,8 +2587,6 @@ export const data = {
   'esnext.string.match-all': null,
   // TODO: Remove from `core-js@4`
   'esnext.string.replace-all': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.string.to-well-formed': null,
   'esnext.symbol.async-dispose': {
     bun: '1.0.23',
     chrome: '127',
@@ -2977,7 +2975,6 @@ export const renamed = new Map([
   ['esnext.set.union.v2', 'es.set.union.v2'],
   ['esnext.string.match-all', 'es.string.match-all'],
   ['esnext.string.replace-all', 'es.string.replace-all'],
-  ['esnext.string.to-well-formed', 'es.string.to-well-formed'],
   ['web.url', 'web.url.constructor'],
   ['web.url-search-params', 'web.url-search-params.constructor'],
 ]);
