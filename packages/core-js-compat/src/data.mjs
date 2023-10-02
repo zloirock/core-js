623797867fea6192260a263f465e7a26b8737306
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:13:44 +0700

    drop `esnext.math.umulh` (withdrawn)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2441,9 +2441,6 @@ export const data = {
   },
   'esnext.math.sum-precise': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.umulh': {
-  },
   'esnext.number.from-string': {
   },
   // TODO: Remove from `core-js@4`
