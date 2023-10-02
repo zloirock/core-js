e7c95da9358cb5f52a077994b531397a2223c741
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:49:20 +0700

    drop `esnext.array.group` (obsolete)

--- tests/builder/builder.mjs
+++ tests/builder/builder.mjs
@@ -3,17 +3,18 @@ import builder from 'core-js-builder';
 
 const polyfills = await builder({
   modules: 'core-js/actual',
-  exclude: [/group-by/, 'esnext.typed-array.to-spliced'],
+  exclude: [/group-by/, 'es.typed-array.with'],
   targets: { node: 16 },
   format: 'esm',
 });
 
 ok(polyfills.includes("import 'core-js/modules/es.error.cause.js';"), 'actual node 16 #1');
 ok(polyfills.includes("import 'core-js/modules/es.array.push.js';"), 'actual node 16 #2');
-ok(polyfills.includes("import 'core-js/modules/esnext.array.group.js';"), 'actual node 16 #3');
+ok(polyfills.includes("import 'core-js/modules/esnext.json.parse.js';"), 'actual node 16 #3');
 ok(polyfills.includes("import 'core-js/modules/web.structured-clone.js';"), 'actual node 16 #4');
 ok(!polyfills.includes("import 'core-js/modules/es.weak-set.js';"), 'actual node 16 #5');
-ok(!polyfills.includes("import 'core-js/modules/esnext.weak-set.from.js';"), 'actual node 16 #6');
-ok(!polyfills.includes("import 'core-js/modules/esnext.array.group-by.js';"), 'actual node 16 #7');
+ok(!polyfills.includes("import 'core-js/modules/es.typed-array.with.js';"), 'actual node 16 #6');
+ok(!polyfills.includes("import 'core-js/modules/esnext.object.group-by.js';"), 'actual node 16 #7');
+ok(!polyfills.includes("import 'core-js/modules/esnext.weak-set.from.js';"), 'actual node 16 #8');
 
 echo(chalk.green('builder tested'));
