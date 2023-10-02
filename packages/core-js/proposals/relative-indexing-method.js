34420fcd67a9d7bd96d9902c270de44178753132
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 03:04:10 +0700

    drop `esnext.typed-array.at` (moved to stable es)

--- packages/core-js/proposals/relative-indexing-method.js
+++ packages/core-js/proposals/relative-indexing-method.js
@@ -2,4 +2,4 @@
 // https://github.com/tc39/proposal-relative-indexing-method
 require('../modules/es.string.at-alternative');
 require('../modules/es.array.at');
-require('../modules/esnext.typed-array.at');
+require('../modules/es.typed-array.at');
