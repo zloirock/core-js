7eb7370c4eba5a578c19c3f550a37fea41c2ca25
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 05:05:05 +0700

    drop `esnext.string.replace-all` (moved to stable es)

--- packages/core-js/full/string/virtual/index.js
+++ packages/core-js/full/string/virtual/index.js
@@ -3,7 +3,5 @@ var parent = require('../../../actual/string/virtual');
 // TODO: remove from `core-js@4`
 require('../../../modules/esnext.string.at');
 require('../../../modules/esnext.string.code-points');
-// TODO: remove from `core-js@4`
-require('../../../modules/esnext.string.replace-all');
 
 module.exports = parent;
