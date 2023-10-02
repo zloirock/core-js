cdcb22b6e7395848be4f7d538662083cb13b6944
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 03:22:36 +0700

    drop `esnext.typed-array.find-last` (moved to stable es)

--- packages/core-js/proposals/array-find-from-last.js
+++ packages/core-js/proposals/array-find-from-last.js
@@ -2,5 +2,5 @@
 // https://github.com/tc39/proposal-array-find-from-last/
 require('../modules/es.array.find-last');
 require('../modules/es.array.find-last-index');
-require('../modules/esnext.typed-array.find-last');
+require('../modules/es.typed-array.find-last');
 require('../modules/esnext.typed-array.find-last-index');
