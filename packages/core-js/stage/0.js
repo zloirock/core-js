388bf42b8d3c558cd9882f9013fe70565c0498b7
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:26:54 +0700

    drop `esnext.string.at` (old version, replace by module with the same name with actual behavior)

--- packages/core-js/stage/0.js
+++ packages/core-js/stage/0.js
@@ -3,7 +3,6 @@ var parent = require('./1');
 
 require('../proposals/function-demethodize');
 require('../proposals/function-is-callable-is-constructor');
-require('../proposals/string-at');
 require('../proposals/url');
 // TODO: Obsolete versions, remove from `core-js@4`:
 require('../proposals/array-filtering');
