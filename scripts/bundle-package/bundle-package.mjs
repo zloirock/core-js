c63a40291831396fc0efacb68f609289fea6c374
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:13:44 +0700

    drop `esnext.math.umulh` (withdrawn)

--- scripts/bundle-package/bundle-package.mjs
+++ scripts/bundle-package/bundle-package.mjs
@@ -62,7 +62,6 @@ await bundle(DENO ? {
       'esnext.array.filter-out',       // obsolete
       'esnext.map.update-or-insert',   // obsolete
       'esnext.map.upsert',             // obsolete
-      'esnext.math.umulh',             // withdrawn
       'esnext.string.at',              // withdrawn
       'esnext.symbol.pattern-match',   // is not a part of actual proposal, replaced by esnext.symbol.matcher
       'esnext.symbol.replace-all',     // obsolete
