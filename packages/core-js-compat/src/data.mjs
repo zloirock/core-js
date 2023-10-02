cd35fdb0cc5b4a635aaa4816fb90ef661f3e1de0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:59:17 +0700

    drop `esnext.array.group-by` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2480,9 +2480,6 @@ export const data = {
   'esnext.array.filter-reject': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.array.group-by': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.array.group-by-to-map': {
   },
   'esnext.array.group-to-map': {
