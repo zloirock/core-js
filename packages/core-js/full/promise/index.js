930ee0a818186e679e3154bdaed9f09e7ca370f6
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:37:44 +0700

    drop `esnext.promise.any` (moved to stable es)

--- packages/core-js/full/promise/index.js
+++ packages/core-js/full/promise/index.js
@@ -3,6 +3,5 @@ var parent = require('../../actual/promise');
 // TODO: Remove from `core-js@4`
 require('../../modules/esnext.promise.all-settled');
 require('../../modules/esnext.promise.try');
-require('../../modules/esnext.promise.any');
 
 module.exports = parent;
