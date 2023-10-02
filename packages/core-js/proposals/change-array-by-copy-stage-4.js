9f3bbeb71a4fbd55f74ee9490c755c3be6598ed2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:15:28 +0700

    drop `esnext.typed-array.to-reversed` (moved to stable es)

--- packages/core-js/proposals/change-array-by-copy-stage-4.js
+++ packages/core-js/proposals/change-array-by-copy-stage-4.js
@@ -4,6 +4,6 @@ require('../modules/es.array.to-reversed');
 require('../modules/es.array.to-sorted');
 require('../modules/es.array.to-spliced');
 require('../modules/es.array.with');
-require('../modules/esnext.typed-array.to-reversed');
+require('../modules/es.typed-array.to-reversed');
 require('../modules/esnext.typed-array.to-sorted');
 require('../modules/esnext.typed-array.with');
