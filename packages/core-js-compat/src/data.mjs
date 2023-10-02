ce48627a5ec7c9a4d4c528ec339f10270ae41af6
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 05:19:07 +0700

    drop `esnext.object.iterate-keys` (withdrawn)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2749,9 +2749,6 @@ export const data = {
   'esnext.number.range': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.object.iterate-keys': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.object.iterate-values': {
   },
   // TODO: Remove from `core-js@4`
--- packages/core-js-compat/src/modules-by-versions.mjs
+++ packages/core-js-compat/src/modules-by-versions.mjs
@@ -47,7 +47,6 @@ export default {
     'es.json.stringify',
   ],
   3.5: [
-    'esnext.object.iterate-keys',
     'esnext.object.iterate-values',
   ],
   3.6: [
