288ee8788850727ea2c33488fcb873d09eb9f4f8
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:50:53 +0700

    drop `esnext.array.at` (moved to stable es)

--- packages/core-js/proposals/relative-indexing-method.js
+++ packages/core-js/proposals/relative-indexing-method.js
@@ -1,5 +1,5 @@
 'use strict';
 // https://github.com/tc39/proposal-relative-indexing-method
 require('../modules/es.string.at-alternative');
-require('../modules/esnext.array.at');
+require('../modules/es.array.at');
 require('../modules/esnext.typed-array.at');
