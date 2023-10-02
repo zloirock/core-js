c8bab3527f64c2d11c65327cbf8509c04c7e3979
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:03:24 +0700

    drop `esnext.array.to-spliced` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2222,8 +2222,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.array.last-item': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.array.to-spliced': null,
   'esnext.array.unique-by': {
   },
   // TODO: Remove from `core-js@4`
@@ -2965,7 +2963,6 @@ export const renamed = new Map([
   ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
-  ['esnext.array.to-spliced', 'es.array.to-spliced'],
   ['esnext.array.with', 'es.array.with'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
   ['esnext.array-buffer.transfer', 'es.array-buffer.transfer'],
