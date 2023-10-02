609a8203e9fe6ca6fb010f682ca0636e2018bdea
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:00:34 +0700

    drop `esnext.math.imulh` (withdrawn)

--- packages/core-js/full/math/index.js
+++ packages/core-js/full/math/index.js
@@ -11,7 +11,6 @@ require('../../modules/esnext.math.scale');
 require('../../modules/esnext.math.signbit');
 // TODO: Remove from `core-js@4`
 require('../../modules/esnext.math.isubh');
-require('../../modules/esnext.math.imulh');
 require('../../modules/esnext.math.umulh');
 
 module.exports = parent;
