c2ed00efbeceed627b30b6bc052ef75a9bc7f8aa
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:26:54 +0700

    drop `esnext.string.at` (old version, replace by module with the same name with actual behavior)

--- packages/core-js/full/string/virtual/index.js
+++ packages/core-js/full/string/virtual/index.js
@@ -1,7 +1,5 @@
 'use strict';
 var parent = require('../../../actual/string/virtual');
-// TODO: remove from `core-js@4`
-require('../../../modules/esnext.string.at');
 require('../../../modules/esnext.string.code-points');
 
 module.exports = parent;
