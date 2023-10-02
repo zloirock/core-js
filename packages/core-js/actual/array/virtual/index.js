8a20a65963486e4aa322b609341e57a99fe75910
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:49:20 +0700

    drop `esnext.array.group` (obsolete)

--- packages/core-js/actual/array/virtual/index.js
+++ packages/core-js/actual/array/virtual/index.js
@@ -2,7 +2,6 @@
 var parent = require('../../../stable/array/virtual');
 require('../../../modules/es.map');
 require('../../../modules/es.object.to-string');
-require('../../../modules/esnext.array.group');
 require('../../../modules/esnext.array.group-to-map');
 // TODO: Remove from `core-js@4`
 require('../../../modules/esnext.array.group-by');
