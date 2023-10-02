fb4d25e9165006f9eaf973387beb297dcff19dc5
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:42:11 +0700

    drop `esnext.promise.all-settled` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2200,8 +2200,6 @@ export const data = {
   'esnext.observable.of': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.promise.all-settled': null,
-  // TODO: Remove from `core-js@4`
   'esnext.promise.try': {
   },
   // TODO: Remove from `core-js@4`
@@ -2681,7 +2679,6 @@ export const renamed = new Map([
   ['esnext.map.group-by', 'es.map.group-by'],
   ['esnext.object.has-own', 'es.object.has-own'],
   ['esnext.object.group-by', 'es.object.group-by'],
-  ['esnext.promise.all-settled', 'es.promise.all-settled'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.string.is-well-formed', 'es.string.is-well-formed'],
   ['esnext.string.match-all', 'es.string.match-all'],
