83e12dfa093c81202b6f83982fe804bcf6b4c07b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 03:04:10 +0700

    drop `esnext.typed-array.at` (moved to stable es)

--- packages/core-js/full/typed-array/index.js
+++ packages/core-js/full/typed-array/index.js
@@ -4,8 +4,6 @@ require('../../modules/es.map');
 require('../../modules/es.promise');
 require('../../modules/esnext.typed-array.from-async');
 // TODO: Remove from `core-js@4`
-require('../../modules/esnext.typed-array.at');
-// TODO: Remove from `core-js@4`
 require('../../modules/esnext.typed-array.filter-out');
 require('../../modules/esnext.typed-array.filter-reject');
 require('../../modules/esnext.typed-array.group-by');
