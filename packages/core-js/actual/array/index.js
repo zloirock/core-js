83153912e01ab4a3f82fc79ea8e29b402d4e3e8b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:49:20 +0700

    drop `esnext.array.group` (obsolete)

--- packages/core-js/actual/array/index.js
+++ packages/core-js/actual/array/index.js
@@ -3,7 +3,6 @@ var parent = require('../../stable/array');
 require('../../modules/es.promise');
 require('../../modules/es.object.to-string');
 require('../../modules/esnext.array.from-async');
-require('../../modules/esnext.array.group');
 require('../../modules/esnext.array.group-to-map');
 // TODO: Remove from `core-js@4`
 require('../../modules/esnext.array.group-by');
--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -652,11 +652,9 @@ for (PATH of ['core-js-pure', 'core-js']) {
 
   for (const NS of ['actual', 'full', 'features']) {
     ok(typeof load(NS, 'array/from-async') == 'function');
-    ok(typeof load(NS, 'array/group') == 'function');
     ok(typeof load(NS, 'array/group-to-map') == 'function');
     ok(typeof load(NS, 'array/group-by') == 'function');
     ok(typeof load(NS, 'array/group-by-to-map') == 'function');
-    ok(typeof load(NS, 'array/virtual/group') == 'function');
     ok(typeof load(NS, 'array/virtual/group-to-map') == 'function');
     ok(typeof load(NS, 'array/virtual/group-by') == 'function');
     ok(typeof load(NS, 'array/virtual/group-by-to-map') == 'function');
@@ -711,12 +709,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
     load(NS, 'async-iterator/async-dispose');
     ok(typeof load(NS, 'async-disposable-stack') == 'function');
     ok(typeof load(NS, 'async-disposable-stack/constructor') == 'function');
-
-    const instanceGroup = load(NS, 'instance/group');
-    ok(typeof instanceGroup == 'function');
-    ok(instanceGroup({}) === undefined);
-    ok(typeof instanceGroup([]) == 'function');
-    ok(instanceGroup([]).call([1, 2, 3], it => it % 2)[1].length === 2);
 
     const instanceGroupToMap = load(NS, 'instance/group-to-map');
     ok(typeof instanceGroupToMap == 'function');
