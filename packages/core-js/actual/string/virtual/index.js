f4c2f7f3ff217580146bccb724ec9c8de2a3a134
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 04:54:05 +0700

    drop `esnext.string.to-well-formed` (moved to stable es)

--- packages/core-js/actual/string/virtual/index.js
+++ packages/core-js/actual/string/virtual/index.js
@@ -1,7 +1,4 @@
 'use strict';
 var parent = require('../../../stable/string/virtual');
 
-// TODO: Remove from `core-js@4`
-require('../../../modules/esnext.string.to-well-formed');
-
 module.exports = parent;
