072b683da25d58c428604d7c7ff82120b605b4b0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:42:11 +0700

    drop `esnext.promise.all-settled` (moved to stable es)

--- packages/core-js/full/promise/all-settled.js
+++ packages/core-js/full/promise/all-settled.js
@@ -1,7 +1,4 @@
 'use strict';
-// TODO: Remove from `core-js@4`
-require('../../modules/esnext.promise.all-settled');
-
 var parent = require('../../actual/promise/all-settled');
 
 module.exports = parent;
--- packages/core-js/full/promise/index.js
+++ packages/core-js/full/promise/index.js
@@ -1,6 +1,4 @@
 'use strict';
 var parent = require('../../actual/promise');
-// TODO: Remove from `core-js@4`
-require('../../modules/esnext.promise.all-settled');
 
 module.exports = parent;
