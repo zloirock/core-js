be3178e76fe1828070694502bf3ef0b9a949c793
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:49:20 +0700

    drop `esnext.array.group` (obsolete)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -700,11 +700,9 @@ for (PATH of ['core-js-pure', 'core-js']) {
   }
 
   for (const NS of ['actual', 'full', 'features']) {
-    ok(typeof load(NS, 'array/group') == 'function');
     ok(typeof load(NS, 'array/group-to-map') == 'function');
     ok(typeof load(NS, 'array/group-by') == 'function');
     ok(typeof load(NS, 'array/group-by-to-map') == 'function');
-    ok(typeof load(NS, 'array/virtual/group') == 'function');
     ok(typeof load(NS, 'array/virtual/group-to-map') == 'function');
     ok(typeof load(NS, 'array/virtual/group-by') == 'function');
     ok(typeof load(NS, 'array/virtual/group-by-to-map') == 'function');
@@ -730,12 +728,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
     ok(load(NS, 'promise/all-keyed')({}) instanceof Promise);
     ok(load(NS, 'promise/all-settled-keyed')({}) instanceof Promise);
     ok(load(NS, 'symbol/metadata'));
-
-    const instanceGroup = load(NS, 'instance/group');
-    ok(typeof instanceGroup == 'function');
-    ok(instanceGroup({}) === undefined);
-    ok(typeof instanceGroup([]) == 'function');
-    ok(instanceGroup([]).call([1, 2, 3], it => it % 2)[1].length === 2);
 
     const instanceGroupToMap = load(NS, 'instance/group-to-map');
     ok(typeof instanceGroupToMap == 'function');
