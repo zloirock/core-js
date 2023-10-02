40a464d8dcc3f2d992b287ac402222e8add6a5e9
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 02:13:45 +0700

    drop `esnext.aggregate-error` (moved to stable es)

--- packages/core-js/full/promise/any.js
+++ packages/core-js/full/promise/any.js
@@ -2,7 +2,6 @@
 var parent = require('../../actual/promise/any');
 
 // TODO: Remove from `core-js@4`
-require('../../modules/esnext.aggregate-error');
 require('../../modules/esnext.promise.any');
 
 module.exports = parent;
