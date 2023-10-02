d0a680ce3b630cfa1a357ab2b4cf5a8a3c2a8abf
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 03:55:17 +0700

    drop `esnext.array.to-sorted` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1957,8 +1957,6 @@ export const data = {
   'esnext.array.last-item': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.array.to-sorted': null,
-  // TODO: Remove from `core-js@4`
   'esnext.array.to-spliced': null,
   'esnext.array.unique-by': {
   },
@@ -2681,7 +2679,6 @@ export const renamed = new Map([
   ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
-  ['esnext.array.to-sorted', 'es.array.to-sorted'],
   ['esnext.array.to-spliced', 'es.array.to-spliced'],
   ['esnext.array.with', 'es.array.with'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
