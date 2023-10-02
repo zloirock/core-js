eec6ce07aa563a646e9d0e00fd032a4f822907fd
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 06:59:17 +0700

    drop `esnext.array.group-by` (obsolete)

--- packages/core-js/actual/array/index.js
+++ packages/core-js/actual/array/index.js
@@ -3,7 +3,6 @@ var parent = require('../../stable/array');
 require('../../modules/esnext.array.from-async');
 require('../../modules/esnext.array.group-to-map');
 // TODO: Remove from `core-js@4`
-require('../../modules/esnext.array.group-by');
 require('../../modules/esnext.array.group-by-to-map');
 
 module.exports = parent;
