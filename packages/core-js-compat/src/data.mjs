eea7214b2ac29944bfb52afea3b8a8d103c3e6a2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 03:57:30 +0700

    drop `esnext.weak-map.upsert` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2708,9 +2708,6 @@ export const data = {
   },
   'esnext.weak-map.get-or-insert-computed': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.weak-map.upsert': {
-  },
   'esnext.weak-set.add-all': {
   },
   'esnext.weak-set.delete-all': {
