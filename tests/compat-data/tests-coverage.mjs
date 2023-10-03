dc03f6f79d65b345f4b76352de60d47af8049987
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:37:01 +0700

    drop `esnext.typed-array.group-by` (obsolete)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -58,7 +58,6 @@ const ignore = new Set([
   'esnext.symbol.replace-all',
   'esnext.typed-array.from-async',
   'esnext.typed-array.filter-out',
-  'esnext.typed-array.group-by',
   'esnext.typed-array.to-spliced',
   'esnext.weak-map.emplace',
   'esnext.weak-map.upsert',
