c88399daebb1feecc259c18be25e557e37356a29
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:40:28 +0700

    rename `es.map` -> `es.map.constructor`

--- packages/core-js/es/map/group-by.js
+++ packages/core-js/es/map/group-by.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../../modules/es.object.to-string');
-require('../../modules/es.map');
+require('../../modules/es.map.constructor');
 require('../../modules/es.map.group-by');
 var path = require('../../internals/path');
 
