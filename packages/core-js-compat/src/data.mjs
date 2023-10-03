dece46cb3e3a1ea5941c4e43baaf22943e1414a0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:07:20 +0700

    drop `esnext.symbol.metadata-key` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2713,9 +2713,6 @@ export const data = {
   'esnext.symbol.metadata': {
     deno: '1.40.4',
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.symbol.metadata-key': {
-  },
   'esnext.symbol.observable': {
   },
   // TODO: Remove from `core-js@4`
