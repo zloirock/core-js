b30a8432e1854f35b6e02dd9d03251c286ac2702
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:40:28 +0700

    rename `es.map` -> `es.map.constructor`

--- packages/core-js/full/map/from.js
+++ packages/core-js/full/map/from.js
@@ -1,7 +1,7 @@
 'use strict';
 require('../../modules/es.object.to-string');
 require('../../modules/es.array.iterator');
-require('../../modules/es.map');
+require('../../modules/es.map.constructor');
 require('../../modules/es.string.iterator');
 require('../../modules/esnext.map.from');
 require('../../modules/esnext.map.delete-all');
