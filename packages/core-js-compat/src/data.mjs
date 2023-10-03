8c550ff4298641461bac05baf6a6633f5cfdd607
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 03:57:30 +0700

    drop `esnext.weak-map.upsert` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2603,10 +2603,6 @@ export const data = {
   'esnext.map.from': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.map.get-or-insert': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.map.get-or-insert-computed': null,
-  // TODO: Remove from `core-js@4`
   'esnext.map.group-by': null,
   'esnext.map.includes': {
   },
@@ -2802,13 +2798,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.weak-map.emplace': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.weak-map.get-or-insert': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.weak-map.get-or-insert-computed': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.weak-map.upsert': {
   },
   'esnext.weak-set.add-all': {
   },
@@ -3091,8 +3080,6 @@ export const renamed = new Map([
   ['esnext.json.parse', 'es.json.parse'],
   ['esnext.json.raw-json', 'es.json.raw-json'],
   ['esnext.map.group-by', 'es.map.group-by'],
-  ['esnext.map.get-or-insert', 'es.map.get-or-insert'],
-  ['esnext.map.get-or-insert-computed', 'es.map.get-or-insert-computed'],
   ['esnext.math.f16round', 'es.math.f16round'],
   ['esnext.math.sum-precise', 'es.math.sum-precise'],
   ['esnext.object.group-by', 'es.object.group-by'],
@@ -3115,8 +3102,6 @@ export const renamed = new Map([
   ['esnext.uint8-array.set-from-hex', 'es.uint8-array.set-from-hex'],
   ['esnext.uint8-array.to-base64', 'es.uint8-array.to-base64'],
   ['esnext.uint8-array.to-hex', 'es.uint8-array.to-hex'],
-  ['esnext.weak-map.get-or-insert', 'es.weak-map.get-or-insert'],
-  ['esnext.weak-map.get-or-insert-computed', 'es.weak-map.get-or-insert-computed'],
   ['web.url', 'web.url.constructor'],
   ['web.url-search-params', 'web.url-search-params.constructor'],
 ]);
