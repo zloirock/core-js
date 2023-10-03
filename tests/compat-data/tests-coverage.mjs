31fe72352cc4b20b17b68799f2c0b8fc0304fbbf
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:33:15 +0700

    drop `esnext.symbol.is-registered` (obsolete)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -42,7 +42,6 @@ const ignore = new Set([
   'esnext.set.is-superset-of',
   'esnext.set.symmetric-difference',
   'esnext.set.union',
-  'esnext.symbol.is-registered',
   'esnext.symbol.is-well-known',
   'esnext.symbol.matcher',
   'esnext.weak-map.emplace',
