efb0f9b37d442ee24868dee790e29d0951f67e28
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:25:14 +0700

    drop `esnext.array.group-to-map` (obsolete)

--- tests/builder/builder.mjs
+++ tests/builder/builder.mjs
@@ -10,7 +10,7 @@ const polyfills = await builder({
 
 ok(polyfills.includes("import 'core-js/modules/es.error.cause.js';"), 'actual node 16 #1');
 ok(polyfills.includes("import 'core-js/modules/es.array.push.js';"), 'actual node 16 #2');
-ok(polyfills.includes("import 'core-js/modules/esnext.json.parse.js';"), 'actual node 16 #3');
+ok(polyfills.includes("import 'core-js/modules/esnext.iterator.zip.js';"), 'actual node 16 #3');
 ok(polyfills.includes("import 'core-js/modules/web.structured-clone.js';"), 'actual node 16 #4');
 ok(!polyfills.includes("import 'core-js/modules/es.weak-set.js';"), 'actual node 16 #5');
 ok(!polyfills.includes("import 'core-js/modules/es.typed-array.with.js';"), 'actual node 16 #6');
