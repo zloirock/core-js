7a1fc90ac3f54290bbce1edd0c5fc312bb6b5f06
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:28:11 +0700

    rename `es.data-view` -> `es.data-view.constructor`

--- packages/core-js/es/array-buffer/transfer.js
+++ packages/core-js/es/array-buffer/transfer.js
@@ -1,5 +1,5 @@
 'use strict';
 require('../../modules/es.array-buffer.constructor');
 require('../../modules/es.array-buffer.slice');
-require('../../modules/es.data-view');
+require('../../modules/es.data-view.constructor');
 require('../../modules/es.array-buffer.transfer');
