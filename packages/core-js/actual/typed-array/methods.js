85860d67abd67bd60ec22aa64d6b248f0032b309
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:13:46 +0700

    drop `esnext.typed-array.to-spliced` (obsolete)

--- packages/core-js/actual/typed-array/methods.js
+++ packages/core-js/actual/typed-array/methods.js
@@ -1,6 +1,4 @@
 'use strict';
 var parent = require('../../stable/typed-array/methods');
-// TODO: Remove from `core-js@4`
-require('../../modules/esnext.typed-array.to-spliced');
 
 module.exports = parent;
