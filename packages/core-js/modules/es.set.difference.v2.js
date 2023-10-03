2a637db67235283a5fa065cfa58a06ece8b82833
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:11:29 +0700

    drop `esnext.set.difference` (obsolete)

--- packages/core-js/modules/es.set.difference.v2.js
+++ packages/core-js/modules/es.set.difference.v2.js
@@ -1,10 +1,30 @@
 'use strict';
 var $ = require('../internals/export');
-var difference = require('../internals/set-difference');
+var aSet = require('../internals/a-set');
+var SetHelpers = require('../internals/set-helpers');
+var clone = require('../internals/set-clone');
+var size = require('../internals/set-size');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSet = require('../internals/set-iterate');
+var iterateSimple = require('../internals/iterate-simple');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
+
+var has = SetHelpers.has;
+var remove = SetHelpers.remove;
 
 // `Set.prototype.difference` method
 // https://github.com/tc39/proposal-set-methods
 $({ target: 'Set', proto: true, real: true, forced: !setMethodAcceptSetLike('difference') }, {
-  difference: difference
+  difference: function difference(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    var result = clone(O);
+    if (size(O) <= otherRec.size) iterateSet(O, function (e) {
+      if (otherRec.includes(e)) remove(result, e);
+    });
+    else iterateSimple(otherRec.getIterator(), function (e) {
+      if (has(O, e)) remove(result, e);
+    });
+    return result;
+  }
 });
