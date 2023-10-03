da4cd9f70f702f79b0c7284f52c5232fcfa65d61
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:12:45 +0700

    drop `esnext.array.group-by-to-map` (obsolete)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -653,9 +653,7 @@ for (PATH of ['core-js-pure', 'core-js']) {
   for (const NS of ['actual', 'full', 'features']) {
     ok(typeof load(NS, 'array/from-async') == 'function');
     ok(typeof load(NS, 'array/group-to-map') == 'function');
-    ok(typeof load(NS, 'array/group-by-to-map') == 'function');
     ok(typeof load(NS, 'array/virtual/group-to-map') == 'function');
-    ok(typeof load(NS, 'array/virtual/group-by-to-map') == 'function');
     load(NS, 'array-buffer/detached');
     load(NS, 'array-buffer/transfer');
     load(NS, 'array-buffer/transfer-to-fixed-length');
@@ -716,12 +714,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
     ok(instanceGroupToMap({}) === undefined);
     ok(typeof instanceGroupToMap([]) == 'function');
     ok(instanceGroupToMap([]).call([1, 2, 3], it => it % 2).get(1).length === 2);
-
-    const instanceGroupByToMap = load(NS, 'instance/group-by-to-map');
-    ok(typeof instanceGroupByToMap == 'function');
-    ok(instanceGroupByToMap({}) === undefined);
-    ok(typeof instanceGroupByToMap([]) == 'function');
-    ok(instanceGroupByToMap([]).call([1, 2, 3], it => it % 2).get(1).length === 2);
   }
 
   for (const NS of ['full', 'features']) {
@@ -877,7 +869,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   load('proposals/array-from-async');
   load('proposals/array-from-async-stage-2');
   load('proposals/array-grouping');
-  load('proposals/array-grouping-stage-3');
   load('proposals/array-grouping-stage-3-2');
   load('proposals/array-grouping-v2');
   load('proposals/array-includes');
