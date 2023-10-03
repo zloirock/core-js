06f19dbab853a4e2f8cad26f35f053d027f0b4ed
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:50:12 +0700

    rename `es.set` -> `es.set.constructor`

--- packages/core-js/web/structured-clone.js
+++ packages/core-js/web/structured-clone.js
@@ -2,7 +2,7 @@
 require('../modules/es.array.iterator');
 require('../modules/es.object.to-string');
 require('../modules/es.map.constructor');
-require('../modules/es.set');
+require('../modules/es.set.constructor');
 require('../modules/web.structured-clone');
 var path = require('../internals/path');
 
