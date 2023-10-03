cd2f0070e3a042d1afbc74a83ccd86837b1497e3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:37:01 +0700

    drop `esnext.typed-array.group-by` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2837,9 +2837,6 @@ export const data = {
   'esnext.typed-array.filter-reject': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.typed-array.group-by': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.typed-array.to-spliced': {
   },
   'esnext.typed-array.unique-by': {
