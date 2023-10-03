8ae6d506bbabba02e99cd2828e92b90c4ed7104f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:09:24 +0700

    drop `esnext.set.symmetric-difference` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2204,9 +2204,6 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=267494
     // safari: '17.0',
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.set.symmetric-difference': {
-  },
   'esnext.set.union.v2': {
     // v8 do not properly work with set-like objects
     // https://bugs.chromium.org/p/v8/issues/detail?id=14559
--- packages/core-js/modules/esnext.set.symmetric-difference.v2.js
+++ packages/core-js/modules/esnext.set.symmetric-difference.v2.js
@@ -1,10 +1,27 @@
 'use strict';
 var $ = require('../internals/export');
-var symmetricDifference = require('../internals/set-symmetric-difference');
+var aSet = require('../internals/a-set');
+var SetHelpers = require('../internals/set-helpers');
+var clone = require('../internals/set-clone');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSimple = require('../internals/iterate-simple');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
+
+var add = SetHelpers.add;
+var has = SetHelpers.has;
+var remove = SetHelpers.remove;
 
 // `Set.prototype.symmetricDifference` method
 // https://github.com/tc39/proposal-set-methods
 $({ target: 'Set', proto: true, real: true, forced: !setMethodAcceptSetLike('symmetricDifference') }, {
-  symmetricDifference: symmetricDifference
+  symmetricDifference: function symmetricDifference(other) {
+    var O = aSet(this);
+    var keysIter = getSetRecord(other).getIterator();
+    var result = clone(O);
+    iterateSimple(keysIter, function (e) {
+      if (has(O, e)) remove(result, e);
+      else add(result, e);
+    });
+    return result;
+  }
 });
