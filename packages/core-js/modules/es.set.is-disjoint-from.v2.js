8a72839f9cd353eef23b6023ff3ebd757197e0ac
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:32:25 +0700

    drop `esnext.set.is-disjoint-from` (obsolete)

--- packages/core-js/modules/es.set.is-disjoint-from.v2.js
+++ packages/core-js/modules/es.set.is-disjoint-from.v2.js
@@ -1,6 +1,12 @@
 'use strict';
 var $ = require('../internals/export');
-var isDisjointFrom = require('../internals/set-is-disjoint-from');
+var aSet = require('../internals/a-set');
+var has = require('../internals/set-helpers').has;
+var size = require('../internals/set-size');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSet = require('../internals/set-iterate');
+var iterateSimple = require('../internals/iterate-simple');
+var iteratorClose = require('../internals/iterator-close');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
 var INCORRECT = !setMethodAcceptSetLike('isDisjointFrom', function (result) {
@@ -10,5 +16,15 @@ var INCORRECT = !setMethodAcceptSetLike('isDisjointFrom', function (result) {
 // `Set.prototype.isDisjointFrom` method
 // https://tc39.es/ecma262/#sec-set.prototype.isdisjointfrom
 $({ target: 'Set', proto: true, real: true, forced: INCORRECT }, {
-  isDisjointFrom: isDisjointFrom
+  isDisjointFrom: function isDisjointFrom(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    if (size(O) <= otherRec.size) return iterateSet(O, function (e) {
+      if (otherRec.includes(e)) return false;
+    }, true) !== false;
+    var iterator = otherRec.getIterator();
+    return iterateSimple(iterator, function (e) {
+      if (has(O, e)) return iteratorClose(iterator, 'normal', false);
+    }) !== false;
+  }
 });
