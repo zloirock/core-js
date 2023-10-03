f0609635bbf0c1c2265dfccc39d2ac764880a043
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:22:00 +0700

    drop `esnext.set.union` (obsolete)

--- packages/core-js/modules/es.set.union.v2.js
+++ packages/core-js/modules/es.set.union.v2.js
@@ -1,6 +1,10 @@
 'use strict';
 var $ = require('../internals/export');
-var union = require('../internals/set-union');
+var aSet = require('../internals/a-set');
+var add = require('../internals/set-helpers').add;
+var clone = require('../internals/set-clone');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSimple = require('../internals/iterate-simple');
 var setMethodGetKeysBeforeCloning = require('../internals/set-method-get-keys-before-cloning-detection');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
@@ -9,5 +13,13 @@ var FORCED = !setMethodAcceptSetLike('union') || !setMethodGetKeysBeforeCloning(
 // `Set.prototype.union` method
 // https://tc39.es/ecma262/#sec-set.prototype.union
 $({ target: 'Set', proto: true, real: true, forced: FORCED }, {
-  union: union
+  union: function union(other) {
+    var O = aSet(this);
+    var keysIter = getSetRecord(other).getIterator();
+    var result = clone(O);
+    iterateSimple(keysIter, function (it) {
+      add(result, it);
+    });
+    return result;
+  }
 });
--- packages/core-js/stage/2.js
+++ packages/core-js/stage/2.js
@@ -6,6 +6,7 @@ require('../proposals/async-iterator-helpers');
 require('../proposals/extractors');
 require('../proposals/iterator-chunking');
 require('../proposals/iterator-range');
+require('../proposals/math-clamp-v2');
 require('../proposals/string-dedent');
 require('../proposals/symbol-predicates-v2');
 // TODO: Obsolete versions, remove from `core-js@4`
@@ -13,8 +14,6 @@ require('../proposals/async-explicit-resource-management');
 require('../proposals/decorators');
 require('../proposals/joint-iteration');
 require('../proposals/map-upsert-stage-2');
-require('../proposals/math-clamp-v2');
-require('../proposals/set-methods');
 require('../proposals/using-statement');
 
 module.exports = parent;
