bb0493b19518df35089e9f35315c13d7ad9e366f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:28:18 +0700

    drop `esnext.typed-array.from-async` (obsolete)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -1025,7 +1025,6 @@ for (const NS of ['actual', 'full', 'features']) {
 }
 
 for (const NS of ['full', 'features']) {
-  load(NS, 'typed-array/from-async');
   load(NS, 'typed-array/filter-reject');
   load(NS, 'typed-array/unique-by');
 }
