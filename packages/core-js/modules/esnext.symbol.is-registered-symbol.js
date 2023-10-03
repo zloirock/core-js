b917ff77928759ac0e24e43d340f8e943e4bacc1
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:33:15 +0700

    drop `esnext.symbol.is-registered` (obsolete)

--- packages/core-js/modules/esnext.symbol.is-registered-symbol.js
+++ packages/core-js/modules/esnext.symbol.is-registered-symbol.js
@@ -1,9 +1,20 @@
 'use strict';
 var $ = require('../internals/export');
-var isRegisteredSymbol = require('../internals/symbol-is-registered');
+var getBuiltIn = require('../internals/get-built-in');
+var uncurryThis = require('../internals/function-uncurry-this');
+
+var Symbol = getBuiltIn('Symbol');
+var keyFor = Symbol.keyFor;
+var thisSymbolValue = uncurryThis(Symbol.prototype.valueOf);
 
 // `Symbol.isRegisteredSymbol` method
 // https://tc39.es/proposal-symbol-predicates/#sec-symbol-isregisteredsymbol
 $({ target: 'Symbol', stat: true }, {
-  isRegisteredSymbol: isRegisteredSymbol
+  isRegisteredSymbol: Symbol.isRegisteredSymbol || function isRegisteredSymbol(value) {
+    try {
+      return keyFor(thisSymbolValue(value)) !== undefined;
+    } catch (error) {
+      return false;
+    }
+  }
 });
