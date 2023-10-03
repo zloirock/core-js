3af6bd0211bc4b5572e687f3898fe2b6d5d660af
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:40:38 +0700

    drop `esnext.set.is-subset-of` (obsolete)

--- packages/core-js/full/set/is-subset-of.js
+++ packages/core-js/full/set/is-subset-of.js
@@ -1,9 +1,4 @@
 'use strict';
-require('../../actual/set/is-subset-of');
-require('../../modules/es.array.iterator');
-require('../../modules/es.string.iterator');
-require('../../modules/esnext.set.is-subset-of');
-require('../../modules/web.dom-collections.iterator');
-var entryUnbind = require('../../internals/entry-unbind');
+var parent = require('../../actual/set/is-subset-of');
 
-module.exports = entryUnbind('Set', 'isSubsetOf');
+module.exports = parent;
