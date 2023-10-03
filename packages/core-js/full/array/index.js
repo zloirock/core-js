439d52d4b2797ce8b7271c2d6e475fb170f061fa
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 19:17:39 +0700

    drop `esnext.array.last-index` (suspended)

--- packages/core-js/full/array/index.js
+++ packages/core-js/full/array/index.js
@@ -4,7 +4,6 @@ require('../../modules/es.map');
 require('../../modules/esnext.array.filter-reject');
 require('../../modules/esnext.array.is-template-object');
 require('../../modules/esnext.array.last-item');
-require('../../modules/esnext.array.last-index');
 require('../../modules/esnext.array.unique-by');
 
 module.exports = parent;
