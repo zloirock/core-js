0d82393bb8c99c576b4b46f05ee85623d5721508
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:07:20 +0700

    drop `esnext.symbol.metadata-key` (obsolete)

--- tests/unit-pure/esnext.symbol.metadata-key.js
+++
@@ -1,6 +0,0 @@
-import Symbol from 'core-js-pure/full/symbol';
-
-QUnit.test('Symbol.metadataKey', assert => {
-  assert.true('metadataKey' in Symbol, 'Symbol.metadataKey available');
-  assert.true(Object(Symbol.metadataKey) instanceof Symbol, 'Symbol.metadataKey is symbol');
-});
