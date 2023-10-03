1cb26a75f145fd8500865843f739ecdd7992aba5
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:57:27 +0700

    drop `esnext.iterator.as-indexed-pairs` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2552,9 +2552,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.iterator.constructor': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.iterator.as-indexed-pairs': {
-  },
   'esnext.iterator.chunks': {
   },
   // TODO: Remove from `core-js@4`
