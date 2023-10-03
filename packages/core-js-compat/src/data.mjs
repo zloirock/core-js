61f9b1795013f9b621a966ad1045493426139c2f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:28:18 +0700

    drop `esnext.typed-array.from-async` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2824,9 +2824,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.symbol.replace-all': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.typed-array.from-async': {
-  },
   'esnext.typed-array.filter-reject': {
   },
   'esnext.typed-array.unique-by': {
