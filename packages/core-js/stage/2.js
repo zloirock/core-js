412cfca0008f1a01c6089ac51bd09dff01951747
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:44:21 +0700

    drop `esnext.symbol.is-well-known` (obsolete)

--- packages/core-js/stage/2.js
+++ packages/core-js/stage/2.js
@@ -13,7 +13,6 @@ require('../proposals/decorators');
 require('../proposals/map-upsert-stage-2');
 require('../proposals/math-clamp-v2');
 require('../proposals/set-methods');
-require('../proposals/symbol-predicates');
 require('../proposals/using-statement');
 
 module.exports = parent;
