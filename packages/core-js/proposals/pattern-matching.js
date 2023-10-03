d29003d1518d450464777f81c87e37abc2fcb522
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:16:34 +0700

    drop `esnext.symbol.pattern-match` (obsolete)

--- packages/core-js/proposals/pattern-matching.js
+++ packages/core-js/proposals/pattern-matching.js
@@ -1,5 +1,3 @@
 'use strict';
 // https://github.com/tc39/proposal-pattern-matching
 require('../modules/esnext.symbol.matcher');
-// TODO: remove from `core-js@4`
-require('../modules/esnext.symbol.pattern-match');
--- tests/unit-pure/esnext.symbol.pattern-match.js
+++
@@ -1,6 +0,0 @@
-import Symbol from 'core-js-pure/full/symbol';
-
-QUnit.test('Symbol.patternMatch', assert => {
-  assert.true('patternMatch' in Symbol, 'Symbol.patternMatch available');
-  assert.true(Object(Symbol.patternMatch) instanceof Symbol, 'Symbol.patternMatch is symbol');
-});
