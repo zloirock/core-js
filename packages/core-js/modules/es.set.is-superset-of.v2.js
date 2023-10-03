d6d60886eac7e97215917dbaa261e492b44604aa
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:01:27 +0700

    drop `esnext.set.is-superset-of` (obsolete)

--- packages/core-js/modules/es.set.is-superset-of.v2.js
+++ packages/core-js/modules/es.set.is-superset-of.v2.js
@@ -1,6 +1,11 @@
 'use strict';
 var $ = require('../internals/export');
-var isSupersetOf = require('../internals/set-is-superset-of');
+var aSet = require('../internals/a-set');
+var has = require('../internals/set-helpers').has;
+var size = require('../internals/set-size');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSimple = require('../internals/iterate-simple');
+var iteratorClose = require('../internals/iterator-close');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
 var INCORRECT = !setMethodAcceptSetLike('isSupersetOf', function (result) {
@@ -10,5 +15,13 @@ var INCORRECT = !setMethodAcceptSetLike('isSupersetOf', function (result) {
 // `Set.prototype.isSupersetOf` method
 // https://tc39.es/ecma262/#sec-set.prototype.issupersetof
 $({ target: 'Set', proto: true, real: true, forced: INCORRECT }, {
-  isSupersetOf: isSupersetOf
+  isSupersetOf: function isSupersetOf(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    if (size(O) < otherRec.size) return false;
+    var iterator = otherRec.getIterator();
+    return iterateSimple(iterator, function (e) {
+      if (!has(O, e)) return iteratorClose(iterator, 'normal', false);
+    }) !== false;
+  }
 });
