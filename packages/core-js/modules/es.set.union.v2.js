909753586c0dff5248681f83f96e3d7145de3b16
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:22:00 +0700

    drop `esnext.set.union` (obsolete)

--- packages/core-js/modules/es.set.union.v2.js
+++ packages/core-js/modules/es.set.union.v2.js
@@ -1,10 +1,22 @@
 'use strict';
 var $ = require('../internals/export');
-var union = require('../internals/set-union');
+var aSet = require('../internals/a-set');
+var add = require('../internals/set-helpers').add;
+var clone = require('../internals/set-clone');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSimple = require('../internals/iterate-simple');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
 // `Set.prototype.union` method
 // https://github.com/tc39/proposal-set-methods
 $({ target: 'Set', proto: true, real: true, forced: !setMethodAcceptSetLike('union') }, {
-  union: union
+  union: function union(other) {
+    var O = aSet(this);
+    var keysIter = getSetRecord(other).getIterator();
+    var result = clone(O);
+    iterateSimple(keysIter, function (it) {
+      add(result, it);
+    });
+    return result;
+  }
 });
