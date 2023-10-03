4f6cafaad9ccf04990c8a62e8e61ba6272263e38
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:13:46 +0700

    drop `esnext.typed-array.to-spliced` (obsolete)

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -6,7 +6,6 @@ require('../proposals/decorator-metadata-v2');
 require('../proposals/json-parse-with-source');
 require('../proposals/math-sum');
 // TODO: Obsolete versions, remove from `core-js@4`
-require('../proposals/change-array-by-copy');
 require('../proposals/iterator-helpers-stage-3');
 
 module.exports = parent;
