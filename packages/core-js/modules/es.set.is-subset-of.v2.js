446b79b2fd31c6d4e54bdbd0ed8a01bfa99139cd
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:40:38 +0700

    drop `esnext.set.is-subset-of` (obsolete)

--- packages/core-js/modules/es.set.is-subset-of.v2.js
+++ packages/core-js/modules/es.set.is-subset-of.v2.js
@@ -1,6 +1,9 @@
 'use strict';
 var $ = require('../internals/export');
-var isSubsetOf = require('../internals/set-is-subset-of');
+var aSet = require('../internals/a-set');
+var size = require('../internals/set-size');
+var iterate = require('../internals/set-iterate');
+var getSetRecord = require('../internals/get-set-record');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
 var INCORRECT = !setMethodAcceptSetLike('isSubsetOf', function (result) {
@@ -10,5 +13,12 @@ var INCORRECT = !setMethodAcceptSetLike('isSubsetOf', function (result) {
 // `Set.prototype.isSubsetOf` method
 // https://tc39.es/ecma262/#sec-set.prototype.issubsetof
 $({ target: 'Set', proto: true, real: true, forced: INCORRECT }, {
-  isSubsetOf: isSubsetOf
+  isSubsetOf: function isSubsetOf(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    if (size(O) > otherRec.size) return false;
+    return iterate(O, function (e) {
+      if (!otherRec.includes(e)) return false;
+    }, true) !== false;
+  }
 });
