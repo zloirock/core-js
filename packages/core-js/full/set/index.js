0e6ddb13596c373d69cf70603fbbe23177e04e2b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:22:00 +0700

    drop `esnext.set.union` (obsolete)

--- packages/core-js/full/set/index.js
+++ packages/core-js/full/set/index.js
@@ -11,6 +11,5 @@ require('../../modules/esnext.set.join');
 require('../../modules/esnext.set.map');
 require('../../modules/esnext.set.reduce');
 require('../../modules/esnext.set.some');
-require('../../modules/esnext.set.union');
 
 module.exports = parent;
