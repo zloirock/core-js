2a43b66f72db0d2d37c0478b05b8dd2ff4144bd5
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 19:32:34 +0700

    drop `esnext.array.last-item` (suspended)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2475,9 +2475,6 @@ export const data = {
   },
   'esnext.array.is-template-object': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.array.last-item': {
-  },
   'esnext.array.unique-by': {
   },
   // TODO: Remove from `core-js@4`
--- packages/core-js/full/array/index.js
+++ packages/core-js/full/array/index.js
@@ -3,7 +3,6 @@ var parent = require('../../actual/array');
 require('../../modules/es.map');
 require('../../modules/esnext.array.filter-reject');
 require('../../modules/esnext.array.is-template-object');
-require('../../modules/esnext.array.last-item');
 require('../../modules/esnext.array.unique-by');
 
 module.exports = parent;
