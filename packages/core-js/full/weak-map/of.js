2a2271865c792f242cfd42eda4f1ca1cd21019f2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 03:57:30 +0700

    drop `esnext.weak-map.upsert` (obsolete)

--- packages/core-js/full/weak-map/of.js
+++ packages/core-js/full/weak-map/of.js
@@ -2,11 +2,11 @@
 require('../../modules/es.object.to-string');
 require('../../modules/es.array.iterator');
 require('../../modules/es.weak-map');
+require('../../modules/es.weak-map.get-or-insert');
+require('../../modules/es.weak-map.get-or-insert-computed');
 require('../../modules/esnext.weak-map.of');
 require('../../modules/esnext.weak-map.delete-all');
 require('../../modules/esnext.weak-map.emplace');
-require('../../modules/esnext.weak-map.get-or-insert');
-require('../../modules/esnext.weak-map.get-or-insert-computed');
 var path = require('../../internals/path');
 
 module.exports = path.WeakMap.of;
