77a50fcb2a29568d47f154f5b088ce1e5c7e8b6a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:17:14 +0700

    rename `es.aggregate-error` -> `es.aggregate-error.constructor`

--- packages/core-js/es/aggregate-error.js
+++ packages/core-js/es/aggregate-error.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../modules/es.error.cause');
-require('../modules/es.aggregate-error');
+require('../modules/es.aggregate-error.constructor');
 require('../modules/es.aggregate-error.cause');
 require('../modules/es.array.iterator');
 require('../modules/es.string.iterator');
