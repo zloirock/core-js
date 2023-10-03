4848ed7bc83d4f1182d12d12480554e979f61fbb
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:07:20 +0700

    drop `esnext.symbol.metadata-key` (obsolete)

--- packages/core-js/stage/2.js
+++ packages/core-js/stage/2.js
@@ -11,7 +11,6 @@ require('../proposals/symbol-predicates-v2');
 // TODO: Obsolete versions, remove from `core-js@4`
 require('../proposals/async-explicit-resource-management');
 require('../proposals/decorators');
-require('../proposals/decorator-metadata');
 require('../proposals/map-upsert-stage-2');
 require('../proposals/math-clamp-v2');
 require('../proposals/set-methods');
