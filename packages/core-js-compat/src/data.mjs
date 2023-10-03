3039281665c303d6bdcd81f3f2d0bb2c35cc86c9
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 03:44:00 +0700

    drop `esnext.map.upsert` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2253,9 +2253,6 @@ export const data = {
   },
   'esnext.map.update': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.map.upsert': {
-  },
   'esnext.math.clamp': {
   },
   'esnext.math.deg-per-rad': {
