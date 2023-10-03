9f74ecbf09f65c0b5a6c124bdf66f010a68181d2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:44:21 +0700

    drop `esnext.symbol.is-well-known` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2783,9 +2783,6 @@ export const data = {
   'esnext.symbol.is-well-known-symbol': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.symbol.is-well-known': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.symbol.matcher': {
   },
   'esnext.symbol.metadata': {
