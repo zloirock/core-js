664fefed48617131135b149316dd1a05f3d312cc
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:59:10 +0700

    drop `esnext.typed-array.filter-out` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2749,9 +2749,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.typed-array.from-async': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.typed-array.filter-out': {
-  },
   'esnext.typed-array.filter-reject': {
   },
   // TODO: Remove from `core-js@4`
