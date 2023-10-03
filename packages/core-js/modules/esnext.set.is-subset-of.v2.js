c38923a793cb38e30af35beb2d3a4d74a031be74
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:40:38 +0700

    drop `esnext.set.is-subset-of` (obsolete)

--- packages/core-js/modules/esnext.set.is-subset-of.v2.js
+++ packages/core-js/modules/esnext.set.is-subset-of.v2.js
@@ -1,10 +1,20 @@
 'use strict';
 var $ = require('../internals/export');
-var isSubsetOf = require('../internals/set-is-subset-of');
+var aSet = require('../internals/a-set');
+var size = require('../internals/set-size');
+var iterate = require('../internals/set-iterate');
+var getSetRecord = require('../internals/get-set-record');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
 
 // `Set.prototype.isSubsetOf` method
 // https://github.com/tc39/proposal-set-methods
 $({ target: 'Set', proto: true, real: true, forced: !setMethodAcceptSetLike('isSubsetOf') }, {
-  isSubsetOf: isSubsetOf
+  isSubsetOf: function isSubsetOf(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    if (size(O) > otherRec.size) return false;
+    return iterate(O, function (e) {
+      if (!otherRec.includes(e)) return false;
+    }, true) !== false;
+  }
 });
--- tests/unit-pure/esnext.set.is-subset-of.js
+++ tests/unit-pure/esnext.set.is-subset-of.js
@@ -1,6 +1,6 @@
-import { createIterable, createSetLike } from '../helpers/helpers.js';
+import { createSetLike } from '../helpers/helpers.js';
 
-import Set from 'core-js-pure/full/set';
+import Set from 'core-js-pure/actual/set';
 
 QUnit.test('Set#isSubsetOf', assert => {
   const { isSubsetOf } = Set.prototype;
@@ -18,14 +18,6 @@ QUnit.test('Set#isSubsetOf', assert => {
   assert.false(new Set([1]).isSubsetOf(createSetLike([2, 3, 4])));
   assert.true(new Set([1, 2, 3]).isSubsetOf(createSetLike([5, 4, 3, 2, 1])));
   assert.false(new Set([1, 2, 3]).isSubsetOf(createSetLike([5, 4, 3, 2])));
-
-  // TODO: drop from core-js@4
-  assert.true(new Set([1]).isSubsetOf([1, 2, 3]));
-  assert.false(new Set([1]).isSubsetOf([2, 3, 4]));
-  assert.true(new Set([1, 2, 3]).isSubsetOf([5, 4, 3, 2, 1]));
-  assert.false(new Set([1, 2, 3]).isSubsetOf([5, 4, 3, 2]));
-  assert.true(new Set([1]).isSubsetOf(createIterable([1, 2, 3])));
-  assert.false(new Set([1]).isSubsetOf(createIterable([2, 3, 4])));
 
   assert.throws(() => new Set([1, 2, 3]).isSubsetOf(), TypeError);
   assert.throws(() => isSubsetOf.call({}, [1, 2, 3]), TypeError);
