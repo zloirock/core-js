3c4e0fa8137e4da4b63de648962b15491a40c7b4
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:25:27 +0700

    drop `esnext.set.intersection` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2709,9 +2709,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.set.intersection.v2': null,
   // TODO: Remove from `core-js@4`
-  'esnext.set.intersection': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.set.is-disjoint-from.v2': null,
   // TODO: Remove from `core-js@4`
   'esnext.set.is-disjoint-from': {
