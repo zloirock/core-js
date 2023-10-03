513864b740f38011f72512f0db3ac05f574af006
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:56:22 +0700

    drop `esnext.bigint.range` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2060,9 +2060,6 @@ export const data = {
   },
   'esnext.async-iterator.to-array': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.bigint.range': {
-  },
   'esnext.composite-key': {
   },
   'esnext.composite-symbol': {
