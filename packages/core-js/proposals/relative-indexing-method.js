f662c77290a3e64639b69170460751a9f7671988
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:44:03 +0700

    rename `es.string.at-alternative` -> `es.string.at`

--- packages/core-js/proposals/relative-indexing-method.js
+++ packages/core-js/proposals/relative-indexing-method.js
@@ -1,5 +1,5 @@
 'use strict';
 // https://github.com/tc39/proposal-relative-indexing-method
-require('../modules/es.string.at-alternative');
+require('../modules/es.string.at');
 require('../modules/es.array.at');
 require('../modules/es.typed-array.at');
