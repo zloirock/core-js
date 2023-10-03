6b0d7f6e10903f6e0b46481a1011d4024c292c78
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:40:28 +0700

    rename `es.map` -> `es.map.constructor`

--- packages/core-js/stable/structured-clone.js
+++ packages/core-js/stable/structured-clone.js
@@ -3,7 +3,7 @@ require('../modules/es.error.to-string');
 require('../modules/es.array.iterator');
 require('../modules/es.object.keys');
 require('../modules/es.object.to-string');
-require('../modules/es.map');
+require('../modules/es.map.constructor');
 require('../modules/es.set');
 require('../modules/web.dom-exception.constructor');
 require('../modules/web.dom-exception.stack');
