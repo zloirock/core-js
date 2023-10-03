d0e23c460e0433926ee4829fdf92198b7837227c
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:51:35 +0700

    drop `esnext.array.filter-out` (obsolete)

--- packages/core-js/proposals/array-filtering.js
+++ packages/core-js/proposals/array-filtering.js
@@ -1,7 +1,5 @@
 'use strict';
 // https://github.com/tc39/proposal-array-filtering
-// TODO: Remove from `core-js@4`
-require('../modules/esnext.array.filter-out');
 require('../modules/esnext.array.filter-reject');
 // TODO: Remove from `core-js@4`
 require('../modules/esnext.typed-array.filter-out');
