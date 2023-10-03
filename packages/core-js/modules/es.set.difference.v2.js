01038ecbfc62c8ece18716d3366bcdd486a0357f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:11:29 +0700

    drop `esnext.set.difference` (obsolete)

--- packages/core-js/modules/es.set.difference.v2.js
+++ packages/core-js/modules/es.set.difference.v2.js
@@ -1,8 +1,17 @@
 'use strict';
 var $ = require('../internals/export');
-var difference = require('../internals/set-difference');
+var aSet = require('../internals/a-set');
+var SetHelpers = require('../internals/set-helpers');
+var clone = require('../internals/set-clone');
+var size = require('../internals/set-size');
+var getSetRecord = require('../internals/get-set-record');
+var iterateSet = require('../internals/set-iterate');
+var iterateSimple = require('../internals/iterate-simple');
 var fails = require('../internals/fails');
 var setMethodAcceptSetLike = require('../internals/set-method-accept-set-like');
+
+var has = SetHelpers.has;
+var remove = SetHelpers.remove;
 
 var SET_LIKE_INCORRECT_BEHAVIOR = !setMethodAcceptSetLike('difference', function (result) {
   return result.size === 0;
@@ -33,5 +42,16 @@ var FORCED = SET_LIKE_INCORRECT_BEHAVIOR || fails(function () {
 // `Set.prototype.difference` method
 // https://tc39.es/ecma262/#sec-set.prototype.difference
 $({ target: 'Set', proto: true, real: true, forced: FORCED }, {
-  difference: difference
+  difference: function difference(other) {
+    var O = aSet(this);
+    var otherRec = getSetRecord(other);
+    var result = clone(O);
+    if (size(O) <= otherRec.size) iterateSet(O, function (e) {
+      if (otherRec.includes(e)) remove(result, e);
+    });
+    else iterateSimple(otherRec.getIterator(), function (e) {
+      if (has(result, e)) remove(result, e);
+    });
+    return result;
+  }
 });
