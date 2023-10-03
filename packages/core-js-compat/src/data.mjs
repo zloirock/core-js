ec674a5980ec73fcaa50bb249ee260d2504f5fb7
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:50:12 +0700

    rename `es.set` -> `es.set.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1443,8 +1443,6 @@ export const data = {
     rhino: '1.7.15',
     safari: '10.0',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'es.set': null,
   'es.set.constructor': {
     chrome: '51',
     edge: '15',
@@ -2721,7 +2719,6 @@ export const data = {
 
 export const renamed = new Map([
   // TODO: Clean in `core-js@4`
-  ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
@@ -2761,7 +2758,6 @@ export const dataWithIgnored = { ...data };
 
 export const ignored = [
   // TODO: Clean in `core-js@4`
-  'es.set.constructor',
   'es.string.trim-left',
   'es.string.trim-right',
   'es.symbol.constructor',
