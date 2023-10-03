9f3f2e5dbd08bd271cace3953747197766c68854
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:40:28 +0700

    rename `es.map` -> `es.map.constructor`

--- packages/core-js/full/map/key-by.js
+++ packages/core-js/full/map/key-by.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../../modules/es.object.to-string');
-require('../../modules/es.map');
+require('../../modules/es.map.constructor');
 require('../../modules/es.map.get-or-insert');
 require('../../modules/es.map.get-or-insert-computed');
 require('../../modules/esnext.map.key-by');
