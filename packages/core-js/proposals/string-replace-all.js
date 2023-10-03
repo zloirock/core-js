6e3b7a93d2b579944783123c5af6fc3c21bedeec
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:22:32 +0700

    drop `esnext.symbol.replace-all` (obsolete)

--- packages/core-js/proposals/string-replace-all.js
+++ packages/core-js/proposals/string-replace-all.js
@@ -1,5 +1,3 @@
 'use strict';
 // https://github.com/tc39/proposal-string-replaceall
 require('../modules/es.string.replace-all');
-// TODO: remove from `core-js@4`
-require('../modules/esnext.symbol.replace-all');
