9f2b08d7e4114bdc93a3dd08edcdbea90061a464
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:25:27 +0700

    drop `esnext.set.intersection` (obsolete)

--- packages/core-js/proposals/set-methods.js
+++ packages/core-js/proposals/set-methods.js
@@ -8,7 +8,6 @@ require('../modules/esnext.set.is-superset-of.v2');
 require('../modules/esnext.set.union.v2');
 require('../modules/esnext.set.symmetric-difference.v2');
 // TODO: Obsolete versions, remove from `core-js@4`
-require('../modules/esnext.set.intersection');
 require('../modules/esnext.set.is-disjoint-from');
 require('../modules/esnext.set.is-subset-of');
 require('../modules/esnext.set.is-superset-of');
