a4f9729a36e66fb83d2f44af9a879c73db2a6eec
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:22:00 +0700

    drop `esnext.set.union` (obsolete)

--- packages/core-js/full/set/union.js
+++ packages/core-js/full/set/union.js
@@ -1,9 +1,4 @@
 'use strict';
-require('../../actual/set/union');
-require('../../modules/es.array.iterator');
-require('../../modules/es.string.iterator');
-require('../../modules/esnext.set.union');
-require('../../modules/web.dom-collections.iterator');
-var entryUnbind = require('../../internals/entry-unbind');
+var parent = require('../../actual/set/union');
 
-module.exports = entryUnbind('Set', 'union');
+module.exports = parent;
