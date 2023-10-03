8feaaccb0d772c7df0a5118f0412c9f2f0b0c35a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 01:49:20 +0700

    drop `esnext.function.un-this` (obsolete)

--- tests/unit-pure/esnext.function.un-this.js
+++
@@ -1,8 +0,0 @@
-import unThis from 'core-js-pure/full/function/un-this';
-
-QUnit.test('Function#unThis', assert => {
-  assert.isFunction(unThis);
-  // eslint-disable-next-line prefer-arrow-callback -- required for testing
-  assert.same(unThis(function () { return 42; })(), 42);
-  assert.deepEqual(unThis(Array.prototype.slice)([1, 2, 3], 1), [2, 3]);
-});
