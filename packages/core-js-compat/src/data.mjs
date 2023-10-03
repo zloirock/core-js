c33ca7d3e1c6c9a0119bef6bd3406daf727f4a05
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:48:09 +0700

    drop `esnext.async-iterator.indexed` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2056,8 +2056,6 @@ export const data = {
   },
   'esnext.async-iterator.from': {
   },
-  'esnext.async-iterator.indexed': {
-  },
   'esnext.async-iterator.map': {
   },
   'esnext.async-iterator.reduce': {
