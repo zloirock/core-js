e0a35ea8771941030147391e2480bd101c1ec03d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:33:15 +0700

    drop `esnext.symbol.is-registered` (obsolete)

--- packages/core-js/full/symbol/index.js
+++ packages/core-js/full/symbol/index.js
@@ -5,7 +5,6 @@ require('../../modules/esnext.symbol.is-well-known-symbol');
 require('../../modules/esnext.symbol.custom-matcher');
 require('../../modules/esnext.symbol.observable');
 // TODO: Remove from `core-js@4`
-require('../../modules/esnext.symbol.is-registered');
 require('../../modules/esnext.symbol.is-well-known');
 require('../../modules/esnext.symbol.matcher');
 
