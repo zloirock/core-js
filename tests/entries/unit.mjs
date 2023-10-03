95fc6c037b1ef4ab599475150de8348224813c0b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 09:06:22 +0700

    drop `esnext.iterator.indexed` (obsolete)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -740,7 +740,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
     ok(!load(NS, 'function/is-constructor')(it => it));
     ok(load(NS, 'function/un-this')([].slice)([1, 2, 3], 1)[0] === 2);
     ok(load(NS, 'function/virtual/un-this').call([].slice)([1, 2, 3], 1)[0] === 2);
-    ok(typeof load(NS, 'iterator/indexed') == 'function');
     ok(load(NS, 'iterator/range')(1, 2).next().value === 1);
     ok(typeof load(NS, 'iterator/chunks') == 'function');
     ok(typeof load(NS, 'iterator/sliding') == 'function');
@@ -893,7 +892,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   load('proposals/function-un-this');
   load('proposals/global-this');
   load('proposals/is-error');
-  load('proposals/iterator-helpers');
   load('proposals/iterator-helpers-stage-3');
   load('proposals/iterator-helpers-stage-3-2');
   load('proposals/iterator-range');
