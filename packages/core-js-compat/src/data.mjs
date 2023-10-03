be8d953850f87d726098eb14ec86ba795636ffdd
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:11:29 +0700

    drop `esnext.set.difference` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2733,9 +2733,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.set.difference.v2': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.set.difference': {
-  },
   'esnext.set.every': {
   },
   'esnext.set.filter': {
