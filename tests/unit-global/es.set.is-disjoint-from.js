d0e974e1b89e25664b23e3c1bc63b98cb81d2cd6
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 04:32:25 +0700

    drop `esnext.set.is-disjoint-from` (obsolete)

--- tests/unit-global/es.set.is-disjoint-from.js
+++ tests/unit-global/es.set.is-disjoint-from.js
@@ -1,4 +1,4 @@
-import { createIterable, createSetLike } from '../helpers/helpers.js';
+import { createSetLike } from '../helpers/helpers.js';
 
 QUnit.test('Set#isDisjointFrom', assert => {
   const { isDisjointFrom } = Set.prototype;
@@ -17,14 +17,6 @@ QUnit.test('Set#isDisjointFrom', assert => {
   assert.false(new Set([1]).isDisjointFrom(createSetLike([1])));
   assert.true(new Set([1, 2, 3]).isDisjointFrom(createSetLike([4, 5, 6])));
   assert.false(new Set([1, 2, 3]).isDisjointFrom(createSetLike([5, 4, 3])));
-
-  // TODO: drop from core-js@4
-  assert.true(new Set([1]).isDisjointFrom([2]));
-  assert.false(new Set([1]).isDisjointFrom([1]));
-  assert.true(new Set([1, 2, 3]).isDisjointFrom([4, 5, 6]));
-  assert.false(new Set([1, 2, 3]).isDisjointFrom([5, 4, 3]));
-  assert.true(new Set([1]).isDisjointFrom(createIterable([2])));
-  assert.false(new Set([1]).isDisjointFrom(createIterable([1])));
 
   assert.false(new Set([42, 43]).isDisjointFrom({
     size: Infinity,
