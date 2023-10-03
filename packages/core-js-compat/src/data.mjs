53ce84b407b4c163f4eb21fbca56ffb416d5abac
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:01:27 +0700

    drop `esnext.set.is-superset-of` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2655,9 +2655,6 @@ export const data = {
   'esnext.set.is-subset-of.v2': null,
   // TODO: Remove from `core-js@4`
   'esnext.set.is-superset-of.v2': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.set.is-superset-of': {
-  },
   'esnext.set.join': {
   },
   'esnext.set.map': {
