fd36b11c6dc0935f7d4d51cf6619c3d4c768ec59
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:25:14 +0700

    drop `esnext.array.group-to-map` (obsolete)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -698,8 +698,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   }
 
   for (const NS of ['actual', 'full', 'features']) {
-    ok(typeof load(NS, 'array/group-to-map') == 'function');
-    ok(typeof load(NS, 'array/virtual/group-to-map') == 'function');
     ok(typeof load(NS, 'async-iterator') == 'function');
     ok(typeof load(NS, 'async-iterator/drop') == 'function');
     ok(typeof load(NS, 'async-iterator/every') == 'function');
@@ -722,12 +720,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
     ok(load(NS, 'promise/all-keyed')({}) instanceof Promise);
     ok(load(NS, 'promise/all-settled-keyed')({}) instanceof Promise);
     ok(load(NS, 'symbol/metadata'));
-
-    const instanceGroupToMap = load(NS, 'instance/group-to-map');
-    ok(typeof instanceGroupToMap == 'function');
-    ok(instanceGroupToMap({}) === undefined);
-    ok(typeof instanceGroupToMap([]) == 'function');
-    ok(instanceGroupToMap([]).call([1, 2, 3], it => it % 2).get(1).length === 2);
   }
 
   for (const NS of ['full', 'features']) {
@@ -891,7 +883,6 @@ for (PATH of ['core-js-pure', 'core-js']) {
   load('proposals/array-from-async');
   load('proposals/array-from-async-stage-2');
   load('proposals/array-grouping');
-  load('proposals/array-grouping-stage-3-2');
   load('proposals/array-grouping-v2');
   load('proposals/array-includes');
   load('proposals/array-is-template-object');
