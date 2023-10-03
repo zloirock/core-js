4bc709f466f343255d5dbbe64094f9c46e960fb3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 01:49:20 +0700

    drop `esnext.function.un-this` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2542,9 +2542,6 @@ export const data = {
   'esnext.function.metadata': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.function.un-this': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.iterator.constructor': null,
   'esnext.iterator.chunks': {
   },
