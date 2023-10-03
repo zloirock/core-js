cf2c281e6b010587e7dc97a9c687dcc9589b97d4
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:22:32 +0700

    drop `esnext.symbol.replace-all` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2282,9 +2282,6 @@ export const data = {
   },
   'esnext.symbol.observable': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.symbol.replace-all': {
-  },
   'esnext.typed-array.filter-reject': {
   },
   'esnext.typed-array.unique-by': {
