07b38fc1a114c7c6dc46c7dc5ecf3cfe0c2026dc
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 02:22:32 +0700

    drop `esnext.symbol.replace-all` (obsolete)

--- scripts/bundle-package/bundle-package.mjs
+++ scripts/bundle-package/bundle-package.mjs
@@ -66,7 +66,6 @@ await bundle(DENO ? {
     exclude: [
       'esnext.map.update-or-insert',   // obsolete
       'esnext.map.upsert',             // obsolete
-      'esnext.symbol.replace-all',     // obsolete
       'esnext.weak-map.upsert',        // obsolete
     ],
   },
