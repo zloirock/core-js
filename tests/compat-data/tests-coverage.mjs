74cb52aa63ea61ef6123ba27a89a5970828bc48d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:28:18 +0700

    drop `esnext.typed-array.from-async` (obsolete)

--- tests/compat-data/tests-coverage.mjs
+++ tests/compat-data/tests-coverage.mjs
@@ -54,7 +54,6 @@ const ignore = new Set([
   'esnext.symbol.metadata-key',
   'esnext.symbol.pattern-match',
   'esnext.symbol.replace-all',
-  'esnext.typed-array.from-async',
   'esnext.weak-map.emplace',
   'esnext.weak-map.upsert',
   'web.url-search-params',
