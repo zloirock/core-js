ac2339297f616eb2d9b51b3af0e0334dde34b861
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 03:07:18 +0700

    drop `esnext.number.range` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2574,9 +2574,6 @@ export const data = {
   'esnext.number.from-string': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.number.range': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.object.group-by': null,
   // TODO: Remove this module from `core-js@4` since it's split to modules listed below
   'esnext.observable': {
