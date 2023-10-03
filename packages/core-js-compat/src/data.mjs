27929cd9c8e3c3b92f6a6f09a09531fbea7988c2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 06:28:11 +0700

    rename `es.data-view` -> `es.data-view.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -438,8 +438,6 @@ export const data = {
     rhino: '1.7.13',
     safari: '12.1',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'es.data-view': null,
   'es.data-view.constructor': {
     chrome: '26',
     firefox: '15',
@@ -2487,7 +2485,6 @@ export const data = {
 
 export const renamed = new Map([
   // TODO: Clean in `core-js@4`
-  ['es.data-view', 'es.data-view.constructor'],
   ['es.map', 'es.map.constructor'],
   ['es.set', 'es.set.constructor'],
   ['es.weak-map', 'es.weak-map.constructor'],
@@ -2505,7 +2502,6 @@ export const dataWithIgnored = { ...data };
 
 export const ignored = [
   // TODO: Clean in `core-js@4`
-  'es.data-view.constructor',
   'es.map.constructor',
   'es.set.constructor',
   'es.string.trim-left',
--- packages/core-js/es/data-view/index.js
+++ packages/core-js/es/data-view/index.js
@@ -1,7 +1,7 @@
 'use strict';
 require('../../modules/es.array-buffer.constructor');
 require('../../modules/es.array-buffer.slice');
-require('../../modules/es.data-view');
+require('../../modules/es.data-view.constructor');
 require('../../modules/es.object.to-string');
 var path = require('../../internals/path');
 
