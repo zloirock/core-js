503f1a01824891b0c9e0fd301ae6cbc76ff9ffe4
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 09:06:22 +0700

    drop `esnext.iterator.indexed` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2474,8 +2474,6 @@ export const data = {
   'esnext.iterator.for-each': null,
   // TODO: Remove from `core-js@4`
   'esnext.iterator.from': null,
-  'esnext.iterator.indexed': {
-  },
   // TODO: Remove from `core-js@4`
   'esnext.iterator.map': null,
   'esnext.iterator.range': {
