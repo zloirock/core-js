73badc8a758b1cf13662722c3a740b34894984f6
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 08:13:46 +0700

    drop `esnext.typed-array.to-spliced` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2829,9 +2829,6 @@ export const data = {
   },
   'esnext.typed-array.filter-reject': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.typed-array.to-spliced': {
-  },
   'esnext.typed-array.unique-by': {
   },
   // TODO: Remove from `core-js@4`
