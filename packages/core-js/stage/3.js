7de0ef9d3279bcac0741b5d8489763244138dcc5
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:12:45 +0700

    drop `esnext.array.group-by-to-map` (obsolete)

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -10,7 +10,6 @@ require('../proposals/iterator-helpers-stage-3-2');
 require('../proposals/json-parse-with-source');
 require('../proposals/set-methods-v2');
 // TODO: Obsolete versions, remove from `core-js@4`
-require('../proposals/array-grouping-stage-3');
 require('../proposals/array-grouping-stage-3-2');
 require('../proposals/change-array-by-copy');
 require('../proposals/iterator-helpers-stage-3');
