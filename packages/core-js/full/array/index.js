475b0e5e340ac03bd2e6a24028a55960b7ad43a3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:51:35 +0700

    drop `esnext.array.filter-out` (obsolete)

--- packages/core-js/full/array/index.js
+++ packages/core-js/full/array/index.js
@@ -1,8 +1,6 @@
 'use strict';
 var parent = require('../../actual/array');
 require('../../modules/es.map');
-// TODO: Remove from `core-js@4`
-require('../../modules/esnext.array.filter-out');
 require('../../modules/esnext.array.filter-reject');
 require('../../modules/esnext.array.is-template-object');
 require('../../modules/esnext.array.last-item');
