ee291c46b047cbe0b4831e59cc068be6acaba268
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 05:09:24 +0700

    drop `esnext.set.symmetric-difference` (obsolete)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2729,9 +2729,6 @@ export const data = {
   // TODO: Remove from `core-js@4`
   'esnext.set.symmetric-difference.v2': null,
   // TODO: Remove from `core-js@4`
-  'esnext.set.symmetric-difference': {
-  },
-  // TODO: Remove from `core-js@4`
   'esnext.set.union.v2': null,
   // TODO: Remove from `core-js@4`
   'esnext.set.union': {
