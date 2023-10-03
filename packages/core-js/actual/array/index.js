201b72eed93761a2a00a66d64df575cbd5957d93
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 07:25:14 +0700

    drop `esnext.array.group-to-map` (obsolete)

--- packages/core-js/actual/array/index.js
+++ packages/core-js/actual/array/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var parent = require('../../stable/array');
 require('../../modules/esnext.array.from-async');
-require('../../modules/esnext.array.group-to-map');
 
 module.exports = parent;
