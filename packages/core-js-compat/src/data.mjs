8978922a612f428ff6a5658d3549734edfd61284
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-03 19:32:34 +0700

    drop `esnext.array.last-item` (suspended)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2122,9 +2122,6 @@ export const data = {
   },
   'esnext.array.is-template-object': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.array.last-item': {
-  },
   'esnext.array.unique-by': {
   },
   // TODO: Remove from `core-js@4`
