b489c3940a7320c2fbe3b1b3a6738ac22b311d79
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:37:40 +0700

    split `web.immediate` -> `web.{ set, clear }-immediate`

--- packages/core-js/web/immediate.js
+++ packages/core-js/web/immediate.js
@@ -1,5 +1,6 @@
 'use strict';
-require('../modules/web.immediate');
+require('../modules/web.clear-immediate');
+require('../modules/web.set-immediate');
 var path = require('../internals/path');
 
 module.exports = path;
