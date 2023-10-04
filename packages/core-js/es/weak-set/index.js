22e320e7de23f3408914337573f1483c69444d06
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:23:35 +0700

    rename `es.weak-set` -> `es.weak-set.constructor`

--- packages/core-js/es/weak-set/index.js
+++ packages/core-js/es/weak-set/index.js
@@ -1,7 +1,7 @@
 'use strict';
 require('../../modules/es.array.iterator');
 require('../../modules/es.object.to-string');
-require('../../modules/es.weak-set');
+require('../../modules/es.weak-set.constructor');
 var path = require('../../internals/path');
 
 module.exports = path.WeakSet;
