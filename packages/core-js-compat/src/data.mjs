52f245ca10c905b89e9201abeacc5c49dcd20fef
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:23:35 +0700

    rename `es.weak-set` -> `es.weak-set.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2466,8 +2466,6 @@ export const data = {
     firefox: '144',
     safari: '26.2',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'es.weak-set': null,
   'es.weak-set.constructor': {
     chrome: '51',
     edge: '15',
@@ -3049,7 +3047,6 @@ export const data = {
 
 export const renamed = new Map([
   // TODO: Clean in `core-js@4`
-  ['es.weak-set', 'es.weak-set.constructor'],
   ['esnext.array.from-async', 'es.array.from-async'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
   ['esnext.array-buffer.transfer', 'es.array-buffer.transfer'],
@@ -3125,7 +3122,6 @@ export const ignored = [
   'es.promise.race',
   'es.promise.reject',
   'es.promise.resolve',
-  'es.weak-set.constructor',
   'esnext.observable.constructor',
   'esnext.observable.from',
   'esnext.observable.of',
