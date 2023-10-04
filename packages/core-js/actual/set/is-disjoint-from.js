8ea1c0f2b85bc12beca9d62fa7849965eca58c5c
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:55:04 +0700

    rename `esnext.set.is-disjoint-from.v2` -> `esnext.set.is-disjoint-from`

--- packages/core-js/actual/set/is-disjoint-from.js
+++ packages/core-js/actual/set/is-disjoint-from.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../../modules/es.set.constructor');
-require('../../modules/esnext.set.is-disjoint-from.v2');
+require('../../modules/esnext.set.is-disjoint-from');
 var entryUnbind = require('../../internals/entry-unbind');
 
 module.exports = entryUnbind('Set', 'isDisjointFrom');
