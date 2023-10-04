a208e6a853f9f41433b43a22416907dd7f57011b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 09:12:15 +0700

    split `es.string.trim-end` -> `es.string.trim-{ end, right }`

--- packages/core-js/es/string/trim-end.js
+++ packages/core-js/es/string/trim-end.js
@@ -2,4 +2,4 @@
 require('../../modules/es.string.trim-end');
 var entryUnbind = require('../../internals/entry-unbind');
 
-module.exports = entryUnbind('String', 'trimRight');
+module.exports = entryUnbind('String', 'trimEnd');
