5d53c87e333842f7aef8a9b48b646ae3981b810c
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:59:56 +0700

    rename `esnext.set.is-subset-of.v2` -> `esnext.set.is-subset-of`

--- packages/core-js/es/set/is-subset-of.js
+++ packages/core-js/es/set/is-subset-of.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../../modules/es.set.constructor');
-require('../../modules/es.set.is-subset-of.v2');
+require('../../modules/es.set.is-subset-of');
 var entryUnbind = require('../../internals/entry-unbind');
 
 module.exports = entryUnbind('Set', 'isSubsetOf');
