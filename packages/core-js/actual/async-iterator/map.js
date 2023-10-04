540f0bb3732b2f23702f369506a11de4c5d9dfff
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 03:38:48 +0700

    split `es.promise` -> `es.promise.{ constructor, catch, resolve, reject, all, race }`

--- packages/core-js/actual/async-iterator/map.js
+++ packages/core-js/actual/async-iterator/map.js
@@ -1,6 +1,10 @@
 'use strict';
 require('../../modules/es.object.to-string');
-require('../../modules/es.promise');
+require('../../modules/es.promise.constructor');
+require('../../modules/es.promise.catch');
+require('../../modules/es.promise.finally');
+require('../../modules/es.promise.reject');
+require('../../modules/es.promise.resolve');
 require('../../modules/esnext.async-iterator.constructor');
 require('../../modules/esnext.async-iterator.map');
 
