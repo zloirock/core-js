d82ddc29330b0a91a3a9bdd0468d9f1f03438741
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:04:34 +0700

    rename `esnext.set.is-superset-of.v2` -> `esnext.set.is-superset-of`

--- packages/core-js/actual/set/index.js
+++ packages/core-js/actual/set/index.js
@@ -4,7 +4,7 @@ require('../../modules/esnext.set.difference');
 require('../../modules/esnext.set.intersection');
 require('../../modules/esnext.set.is-disjoint-from');
 require('../../modules/esnext.set.is-subset-of');
-require('../../modules/esnext.set.is-superset-of.v2');
+require('../../modules/esnext.set.is-superset-of');
 require('../../modules/esnext.set.symmetric-difference.v2');
 require('../../modules/esnext.set.union.v2');
 
