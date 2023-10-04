7e492805047106cb6629b262a46d390c919fa551
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:04:34 +0700

    rename `esnext.set.is-superset-of.v2` -> `esnext.set.is-superset-of`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1692,7 +1692,7 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=267494
     safari: '18.0', // '17.0',
   },
-  'es.set.is-superset-of.v2': {
+  'es.set.is-superset-of': {
     bun: '1.1.1',
     // v8 ~ Chrome 122 does not properly work with set-like objects
     // https://bugs.chromium.org/p/v8/issues/detail?id=14559
@@ -1706,7 +1706,7 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=267494
     safari: '18.0', // '17.0',
   },
-  'es.set.symmetric-difference.v2': {
+  'es.set.symmetric-difference': {
     // Should get iterator record of a set-like object before cloning this
     // https://bugs.webkit.org/show_bug.cgi?id=289430
     bun: '1.2.5', // '1.1.1',
@@ -1720,7 +1720,7 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=289430
     safari: '26.0', // '18.0', // '17.0',
   },
-  'es.set.union.v2': {
+  'es.set.union': {
     // Should get iterator record of a set-like object before cloning this
     // https://bugs.webkit.org/show_bug.cgi?id=289430
     bun: '1.2.5', // '1.1.1',
@@ -2615,8 +2615,6 @@ export const data = {
   },
   'esnext.set.from': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.set.is-superset-of.v2': null,
   'esnext.set.join': {
   },
   'esnext.set.map': {
@@ -2627,10 +2625,6 @@ export const data = {
   },
   'esnext.set.some': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.set.symmetric-difference.v2': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.set.union.v2': null,
   'esnext.string.cooked': {
   },
   'esnext.string.code-points': {
@@ -2962,9 +2956,6 @@ export const renamed = new Map([
   ['esnext.promise.try', 'es.promise.try'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.regexp.escape', 'es.regexp.escape'],
-  ['esnext.set.is-superset-of.v2', 'es.set.is-superset-of.v2'],
-  ['esnext.set.symmetric-difference.v2', 'es.set.symmetric-difference.v2'],
-  ['esnext.set.union.v2', 'es.set.union.v2'],
   ['esnext.suppressed-error.constructor', 'es.suppressed-error.constructor'],
   ['esnext.symbol.async-dispose', 'es.symbol.async-dispose'],
   ['esnext.symbol.dispose', 'es.symbol.dispose'],
