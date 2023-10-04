dc17c042fb591f0c021a78287297a178ba7d471f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:09:20 +0700

    rename `esnext.set.symmetric-difference.v2` -> `esnext.set.symmetric-difference`

--- packages/core-js/actual/set/index.js
+++ packages/core-js/actual/set/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var parent = require('../../stable/set');
-require('../../modules/esnext.set.symmetric-difference.v2');
 require('../../modules/esnext.set.union.v2');
 
 module.exports = parent;
