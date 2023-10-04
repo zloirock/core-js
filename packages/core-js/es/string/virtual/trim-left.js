726a449c136e2d8aa64a2f315682ca414a177880
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 09:24:45 +0700

    split `es.string.trim-start` -> `es.string.trim-{ left, start }`

--- packages/core-js/es/string/virtual/trim-left.js
+++ packages/core-js/es/string/virtual/trim-left.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../../../modules/es.string.trim-start');
+require('../../../modules/es.string.trim-left');
 var getBuiltInPrototypeMethod = require('../../../internals/get-built-in-prototype-method');
 
 module.exports = getBuiltInPrototypeMethod('String', 'trimLeft');
