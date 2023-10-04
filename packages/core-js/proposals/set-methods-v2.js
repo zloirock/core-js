a5a8c93f594590c1b71e088bfc486123139bbc52
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:09:20 +0700

    rename `esnext.set.symmetric-difference.v2` -> `esnext.set.symmetric-difference`

--- packages/core-js/proposals/set-methods-v2.js
+++ packages/core-js/proposals/set-methods-v2.js
@@ -5,5 +5,5 @@ require('../modules/esnext.set.intersection');
 require('../modules/esnext.set.is-disjoint-from');
 require('../modules/esnext.set.is-subset-of');
 require('../modules/esnext.set.is-superset-of');
+require('../modules/esnext.set.symmetric-difference');
 require('../modules/esnext.set.union.v2');
-require('../modules/esnext.set.symmetric-difference.v2');
