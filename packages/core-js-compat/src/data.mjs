f2e8ea9d98e5343b555d47116fc0b8ab72c4fb99
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:59:56 +0700

    rename `esnext.set.is-subset-of.v2` -> `esnext.set.is-subset-of`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1739,7 +1739,7 @@ export const data = {
     // https://bugs.webkit.org/show_bug.cgi?id=267494
     safari: '18.0', // '17.0',
   },
-  'es.set.is-subset-of.v2': {
+  'es.set.is-subset-of': {
     bun: '1.1.1',
     // v8 ~ Chrome 122 does not properly work with set-like objects
     // https://bugs.chromium.org/p/v8/issues/detail?id=14559
@@ -2694,8 +2694,6 @@ export const data = {
   'esnext.set.from': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.set.is-subset-of.v2': null,
-  // TODO: Remove from `core-js@4`
   'esnext.set.is-superset-of.v2': null,
   'esnext.set.join': {
   },
@@ -3038,7 +3036,6 @@ export const renamed = new Map([
   ['esnext.promise.try', 'es.promise.try'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.regexp.escape', 'es.regexp.escape'],
-  ['esnext.set.is-subset-of.v2', 'es.set.is-subset-of.v2'],
   ['esnext.set.is-superset-of.v2', 'es.set.is-superset-of.v2'],
   ['esnext.set.symmetric-difference.v2', 'es.set.symmetric-difference.v2'],
   ['esnext.set.union.v2', 'es.set.union.v2'],
