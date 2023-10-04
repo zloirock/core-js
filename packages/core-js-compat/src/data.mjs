8fcfe4a0f5684f0bb3213b5124b91eff02035a02
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:16:31 +0700

    rename `es.weak-map` -> `es.weak-map.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2339,8 +2339,6 @@ export const data = {
     rhino: '1.7.13',
     safari: '1',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'es.weak-map': null,
   'es.weak-map.constructor': {
     chrome: '51',
     // adding frozen arrays to WeakMap unfreeze them
@@ -2950,7 +2948,6 @@ export const data = {
 
 export const renamed = new Map([
   // TODO: Clean in `core-js@4`
-  ['es.weak-map', 'es.weak-map.constructor'],
   ['es.weak-set', 'es.weak-set.constructor'],
   ['esnext.array.from-async', 'es.array.from-async'],
   ['esnext.array-buffer.detached', 'es.array-buffer.detached'],
@@ -3021,7 +3018,6 @@ export const ignored = [
   'es.promise.race',
   'es.promise.reject',
   'es.promise.resolve',
-  'es.weak-map.constructor',
   'es.weak-set.constructor',
   'esnext.observable.constructor',
   'esnext.observable.from',
