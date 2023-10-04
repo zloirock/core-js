0b8a00fe2c97650c7b91078df3263194250e690c
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:31:04 +0700

    rename `web.url` -> `web.url.constructor`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2532,8 +2532,6 @@ export const data = {
     rhino: '1.7.13',
     safari: '1.0',
   },
-  // TODO: Remove this module from `core-js@4` since it's replaced to module below
-  'web.url': null,
   'web.url.constructor': {
     bun: '0.1.1',
     chrome: '67',
@@ -2619,7 +2617,6 @@ export const renamed = new Map([
   ['esnext.set.is-superset-of.v2', 'es.set.is-superset-of.v2'],
   ['esnext.set.symmetric-difference.v2', 'es.set.symmetric-difference.v2'],
   ['esnext.set.union.v2', 'es.set.union.v2'],
-  ['web.url', 'web.url.constructor'],
   ['web.url-search-params', 'web.url-search-params.constructor'],
 ]);
 
@@ -2648,7 +2645,6 @@ export const ignored = [
   'web.set-immediate',
   'web.set-interval',
   'web.set-timeout',
-  'web.url.constructor',
   'web.url-search-params.constructor',
 ];
 
