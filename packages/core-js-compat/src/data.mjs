58cf7b05da143063943eb70d2f6fe899461ddd9a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:58:34 +0700

    split `esnext.observable` -> `esnext.observable.{ constructor, from, of }`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2561,9 +2561,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.object.group-by': null,
-  // TODO: Remove this module from `core-js@4` since it's split to modules listed below
-  'esnext.observable': {
-  },
   'esnext.observable.constructor': {
   },
   'esnext.observable.from': {
@@ -2963,9 +2960,6 @@ export const ignored = [
   'es.promise.race',
   'es.promise.reject',
   'es.promise.resolve',
-  'esnext.observable.constructor',
-  'esnext.observable.from',
-  'esnext.observable.of',
 ];
 
 for (const ignore of ignored) delete data[ignore];
