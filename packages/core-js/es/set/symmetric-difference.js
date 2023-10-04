7b25a45e2cc19a2eaa766d7ccea42d10bf1173cf
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:04:34 +0700

    rename `esnext.set.is-superset-of.v2` -> `esnext.set.is-superset-of`

--- packages/core-js/es/set/symmetric-difference.js
+++ packages/core-js/es/set/symmetric-difference.js
@@ -1,6 +1,6 @@
 'use strict';
 require('../../modules/es.set.constructor');
-require('../../modules/es.set.symmetric-difference.v2');
+require('../../modules/es.set.symmetric-difference');
 var entryUnbind = require('../../internals/entry-unbind');
 
 module.exports = entryUnbind('Set', 'symmetricDifference');
