53b614ff8ce43999a42feef20af97970d2e2c09b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:59:56 +0700

    rename `esnext.set.is-subset-of.v2` -> `esnext.set.is-subset-of`

--- packages/core-js/actual/set/index.js
+++ packages/core-js/actual/set/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var parent = require('../../stable/set');
-require('../../modules/esnext.set.is-subset-of.v2');
 require('../../modules/esnext.set.is-superset-of.v2');
 require('../../modules/esnext.set.symmetric-difference.v2');
 require('../../modules/esnext.set.union.v2');
