920ab8b85a9ca41899d95f9c43835e8eb91c10af
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:39:37 +0700

    rename `web.url-search-params` -> `web.url-search-params.constructor`

--- packages/core-js/web/index.js
+++ packages/core-js/web/index.js
@@ -15,7 +15,7 @@ require('../modules/web.url.constructor');
 require('../modules/web.url.can-parse');
 require('../modules/web.url.parse');
 require('../modules/web.url.to-json');
-require('../modules/web.url-search-params');
+require('../modules/web.url-search-params.constructor');
 require('../modules/web.url-search-params.delete');
 require('../modules/web.url-search-params.has');
 require('../modules/web.url-search-params.size');
