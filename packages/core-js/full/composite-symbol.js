d4049ad848a9c318615ab54a677226bee32aed77
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 11:42:47 +0700

    split `es.symbol` -> `es.{ symbol.{ constructor, for, key-for }, object.get-own-property-symbols, json.stringify }`

--- packages/core-js/full/composite-symbol.js
+++ packages/core-js/full/composite-symbol.js
@@ -1,5 +1,6 @@
 'use strict';
-require('../modules/es.symbol');
+require('../modules/es.symbol.constructor');
+require('../modules/es.symbol.for');
 require('../modules/esnext.composite-symbol');
 var path = require('../internals/path');
 
