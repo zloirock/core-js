b439952f97752d3935761c6ec7949c30dab282d1
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:23:35 +0700

    rename `es.weak-set` -> `es.weak-set.constructor`

--- packages/core-js/actual/weak-map/get-or-insert-computed.js
+++ packages/core-js/actual/weak-map/get-or-insert-computed.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../../modules/es.weak-map');
+require('../../modules/es.weak-map.constructor');
 require('../../modules/esnext.weak-map.get-or-insert-computed');
 var entryUnbind = require('../../internals/entry-unbind');
 
