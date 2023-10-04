dc71711ccb7da8f01a550e066864905a8d0317b3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 03:38:48 +0700

    split `es.promise` -> `es.promise.{ constructor, catch, resolve, reject, all, race }`

--- packages/core-js/actual/array/from-async.js
+++ packages/core-js/actual/array/from-async.js
@@ -1,7 +1,9 @@
 'use strict';
 require('../../modules/es.array.iterator');
 require('../../modules/es.object.to-string');
-require('../../modules/es.promise');
+require('../../modules/es.promise.constructor');
+require('../../modules/es.promise.catch');
+require('../../modules/es.promise.finally');
 require('../../modules/es.string.iterator');
 require('../../modules/esnext.array.from-async');
 var path = require('../../internals/path');
