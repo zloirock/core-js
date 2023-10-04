34b175d2c1dba9f5de5d747640e12c5ae23d07b9
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 09:12:15 +0700

    split `es.string.trim-end` -> `es.string.trim-{ end, right }`

--- packages/core-js/es/string/virtual/trim-right.js
+++ packages/core-js/es/string/virtual/trim-right.js
@@ -1,5 +1,5 @@
 'use strict';
-require('../../../modules/es.string.trim-end');
+require('../../../modules/es.string.trim-right');
 var getBuiltInPrototypeMethod = require('../../../internals/get-built-in-prototype-method');
 
 module.exports = getBuiltInPrototypeMethod('String', 'trimRight');
