d33409b8173b75443e0e6ed4d26c88796bd14d54
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 07:59:56 +0700

    rename `esnext.set.is-subset-of.v2` -> `esnext.set.is-subset-of`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1371,7 +1371,7 @@ GLOBAL.tests = {
   'es.set.is-disjoint-from': createSetMethodTest('isDisjointFrom', function (result) {
     return !result;
   }),
-  'es.set.is-subset-of.v2': createSetMethodTest('isSubsetOf', function (result) {
+  'es.set.is-subset-of': createSetMethodTest('isSubsetOf', function (result) {
     return result;
   }),
   'es.set.is-superset-of.v2': createSetMethodTest('isSupersetOf', function (result) {
