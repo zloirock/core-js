0eca8fdc7e9280ae88805c6ccb5179ccb1dbc0ce
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-04 08:58:34 +0700

    split `esnext.observable` -> `esnext.observable.{ constructor, from, of }`

---
+++ packages/core-js/full/observable/from.js
@@ -0,0 +1,10 @@
+'use strict';
+require('../../modules/esnext.observable.constructor');
+require('../../modules/esnext.observable.from');
+require('../../modules/esnext.symbol.observable');
+require('../../modules/es.object.to-string');
+require('../../modules/es.string.iterator');
+require('../../modules/web.dom-collections.iterator');
+var path = require('../../internals/path');
+
+module.exports = path.Observable.from;
