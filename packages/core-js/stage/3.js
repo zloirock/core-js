9c0e2e4c4133c4761d7acc892b51e6efabc8a158
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 07:59:59 +0700

    actualize proposals entries

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -1,10 +1,6 @@
 'use strict';
-var parent = require('./4');
+require('./4');
 
-require('../proposals/decorator-metadata-v2');
+require('../proposals/decorator-metadata');
 require('../proposals/joint-iteration');
-require('../proposals/map-upsert-v4');
-// TODO: Obsolete versions, remove from `core-js@4`
-require('../proposals/iterator-helpers-stage-3');
-
-module.exports = parent;
+require('../proposals/map-upsert');
