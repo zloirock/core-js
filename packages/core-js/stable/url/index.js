6ecea28b8d5f3645800bfedc10717074bfabdd15
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 03:28:32 +0700

    drop `/web/` namespace as almost useless in favor of `/stable/`, `/actual/`, `/full/`

--- packages/core-js/stable/url/index.js
+++ packages/core-js/stable/url/index.js
@@ -1,4 +1,11 @@
 'use strict';
-var parent = require('../../web/url');
+require('../../modules/web.url.constructor');
+require('../../modules/web.url.can-parse');
+require('../../modules/web.url.to-json');
+require('../../modules/web.url-search-params.constructor');
+require('../../modules/web.url-search-params.delete');
+require('../../modules/web.url-search-params.has');
+require('../../modules/web.url-search-params.size');
+var path = require('../../internals/path');
 
-module.exports = parent;
+module.exports = path.URL;
