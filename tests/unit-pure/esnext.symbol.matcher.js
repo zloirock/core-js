dac745d864f692b9ad703d00ba957f3599a8fd35
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 07:59:59 +0700

    actualize proposals entries

--- tests/unit-pure/esnext.symbol.matcher.js
+++
@@ -1,6 +0,0 @@
-import Symbol from 'core-js-pure/full/symbol';
-
-QUnit.test('Symbol.matcher', assert => {
-  assert.true('matcher' in Symbol, 'Symbol.matcher available');
-  assert.true(Object(Symbol.matcher) instanceof Symbol, 'Symbol.matcher is symbol');
-});
