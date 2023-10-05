48cb6c914dd7e6e1e3f3d8a145b1915e745bfd4d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 07:59:59 +0700

    actualize proposals entries

--- packages/core-js/stage/2.js
+++ packages/core-js/stage/2.js
@@ -1,17 +1,10 @@
 'use strict';
-var parent = require('./2.7');
+require('./2.7');
 
 require('../proposals/array-is-template-object');
 require('../proposals/async-iterator-helpers');
 require('../proposals/extractors');
 require('../proposals/iterator-range');
-require('../proposals/math-clamp-v2');
+require('../proposals/math-clamp');
 require('../proposals/string-dedent');
-require('../proposals/symbol-predicates-v2');
-// TODO: Obsolete versions, remove from `core-js@4`
-require('../proposals/async-explicit-resource-management');
-require('../proposals/decorators');
-require('../proposals/map-upsert-stage-2');
-require('../proposals/using-statement');
-
-module.exports = parent;
+require('../proposals/symbol-predicates');
