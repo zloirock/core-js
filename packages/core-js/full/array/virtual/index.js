29ba0638dfe0237d72c5050da73d06bfff4ace93
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 06:02:57 +0700

    drop `*/virtual/index.js` entries as useless

--- packages/core-js/full/array/virtual/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var parent = require('../../../actual/array/virtual');
-require('../../../modules/esnext.array.filter-reject');
-require('../../../modules/esnext.array.unique-by');
-
-module.exports = parent;
