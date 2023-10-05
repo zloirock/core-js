5a32a3e92696eab6a2415a0bedfecac1066a31c8
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-05 07:59:59 +0700

    actualize proposals entries

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2516,9 +2516,6 @@ export const data = {
   },
   'esnext.map.update': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.clamp': {
-  },
   'esnext.math.deg-per-rad': {
   },
   'esnext.math.degrees': {
@@ -2619,9 +2616,6 @@ export const data = {
   },
   // We should patch it for newly added well-known symbols. If it's not required, this module just will not be injected
   'esnext.symbol.is-well-known-symbol': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.symbol.matcher': {
   },
   'esnext.symbol.metadata': {
     deno: '1.40.4',
