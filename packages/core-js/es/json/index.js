2366ba4ca3f2eb136155b67e741e566577c6e38b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 00:25:05 +0700

    drop `es.date.to-string` (required for IE8- only)

--- packages/core-js/es/json/index.js
+++ packages/core-js/es/json/index.js
@@ -4,5 +4,4 @@ require('../../modules/es.json.stringify');
 require('../../modules/es.json.to-string-tag');
 var path = require('../../internals/path');
 
-// eslint-disable-next-line es/no-json -- safe
 module.exports = path.JSON;
