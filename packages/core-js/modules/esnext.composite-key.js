07e65fccb3c7cae796600f081257ed0c43b694b4
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 04:02:31 +0700

    use native `Object.{ freeze, seal, preventExtensions }` without fallbacks in simple cases

--- packages/core-js/modules/esnext.composite-key.js
+++ packages/core-js/modules/esnext.composite-key.js
@@ -2,14 +2,13 @@
 var $ = require('../internals/export');
 var apply = require('../internals/function-apply');
 var getCompositeKeyNode = require('../internals/composite-key');
-var getBuiltIn = require('../internals/get-built-in');
 
 var $Object = Object;
-var create = Object.create;
+var create = $Object.create;
+var freeze = $Object.freeze;
 
 var initializer = function () {
-  var freeze = getBuiltIn('Object', 'freeze');
-  return freeze ? freeze(create(null)) : create(null);
+  return freeze(create(null));
 };
 
 // https://github.com/tc39/proposal-richer-keys/tree/master/compositeKey
