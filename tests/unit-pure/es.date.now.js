184a6efd533fdb54df1dcb38779c24a977bb03f8
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 09:26:38 +0700

    drop `es.date.now` (required for ES3 only)

--- tests/unit-pure/es.date.now.js
+++
@@ -1,6 +0,0 @@
-import now from 'core-js-pure/es/date/now';
-
-QUnit.test('Date.now', assert => {
-  assert.isFunction(now);
-  assert.epsilon(+new Date(), now(), 10, 'Date.now() ~ +new Date');
-});
