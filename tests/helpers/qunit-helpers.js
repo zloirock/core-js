72a46a01effab6bce83f84a8a677642d3079b3f8
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 08:28:08 +0700

    drop descriptors check

--- tests/helpers/qunit-helpers.js
+++ tests/helpers/qunit-helpers.js
@@ -1,17 +1,12 @@
-import { DESCRIPTORS } from './constants.js';
 import assign from 'core-js-pure/es/object/assign';
-import defineProperties from 'core-js-pure/es/object/define-properties';
-import getOwnPropertyNames from 'core-js-pure/es/object/get-own-property-names';
-import reduce from 'core-js-pure/es/array/reduce';
 import isIterable from 'core-js-pure/es/is-iterable';
 import ASYNC_ITERATOR from 'core-js-pure/es/symbol/async-iterator';
 import { is, arrayFromArrayLike } from './helpers.js';
 
 // for Babel template transform
 if (!Object.freeze) Object.freeze = Object;
-if (!DESCRIPTORS) Object.defineProperties = defineProperties;
 
-const { getOwnPropertyDescriptor } = Object;
+const { getOwnPropertyDescriptor, getOwnPropertyNames } = Object;
 const { toString, propertyIsEnumerable } = Object.prototype;
 
 const { assert } = QUnit;
@@ -39,14 +34,12 @@ assign(assert, {
     });
   },
   enumerable(O, key, message) {
-    const result = !DESCRIPTORS || propertyIsEnumerable.call(O, key);
+    const result = propertyIsEnumerable.call(O, key);
     this.pushResult({
       result,
       actual: result,
       expected: 'The property should be enumerable',
-      message: DESCRIPTORS
-        ? message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is enumerable`
-        : 'Enumerability is not applicable',
+      message: message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is enumerable`,
     });
   },
   // TODO: Drop from future `core-js` versions
@@ -108,36 +101,30 @@ assign(assert, {
     });
   },
   nonConfigurable(O, key, message) {
-    const result = !DESCRIPTORS || !getOwnPropertyDescriptor(O, key)?.configurable;
+    const result = !getOwnPropertyDescriptor(O, key)?.configurable;
     this.pushResult({
       result,
       actual: result,
       expected: 'The property should be non-configurable',
-      message: DESCRIPTORS
-        ? message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-configurable`
-        : 'Configurability is not applicable',
+      message: message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-configurable`,
     });
   },
   nonEnumerable(O, key, message) {
-    const result = !DESCRIPTORS || !propertyIsEnumerable.call(O, key);
+    const result = !propertyIsEnumerable.call(O, key);
     this.pushResult({
       result,
       actual: result,
       expected: 'The property should be non-enumerable',
-      message: DESCRIPTORS
-        ? message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-enumerable`
-        : 'Enumerability is not applicable',
+      message: message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-enumerable`,
     });
   },
   nonWritable(O, key, message) {
-    const result = !DESCRIPTORS || !getOwnPropertyDescriptor(O, key)?.writable;
+    const result = !getOwnPropertyDescriptor(O, key)?.writable;
     this.pushResult({
       result,
       actual: result,
       expected: 'The property should be non-writable',
-      message: DESCRIPTORS
-        ? message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-writable`
-        : 'Writability is not applicable',
+      message: message ?? `${ typeof key == 'symbol' ? 'property' : `'${ key }'` } is non-writable`,
     });
   },
   notSame(actual, expected, message) {
@@ -182,7 +169,7 @@ assign(assert, {
   },
 });
 
-assert.skip = reduce(getOwnPropertyNames(assert), (skip, method) => {
+assert.skip = getOwnPropertyNames(assert).reduce((skip, method) => {
   skip[method] = () => { /* empty */ };
   return skip;
 }, {});
