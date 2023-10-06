2b71bb2e5a5ec6e004536fae75b84a2f6002bb5d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 09:35:15 +0700

    allow unquoted keyword properties

--- packages/core-js/modules/es.typed-array.with.js
+++ packages/core-js/modules/es.typed-array.with.js
@@ -12,7 +12,7 @@ var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
 var PROPER_ORDER = !!function () {
   try {
     // eslint-disable-next-line no-throw-literal, es/no-typed-arrays, es/no-array-prototype-with -- required for testing
-    new Int8Array(1)['with'](2, { valueOf: function () { throw 8; } });
+    new Int8Array(1).with(2, { valueOf: function () { throw 8; } });
   } catch (error) {
     // some early implementations, like WebKit, does not follow the final semantic
     // https://github.com/tc39/proposal-change-array-by-copy/pull/86
@@ -22,9 +22,9 @@ var PROPER_ORDER = !!function () {
 
 // `%TypedArray%.prototype.with` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.with
-exportTypedArrayMethod('with', { 'with': function (index, value) {
+exportTypedArrayMethod('with', { with: function (index, value) {
   var O = aTypedArray(this);
   var relativeIndex = toIntegerOrInfinity(index);
   var actualValue = isBigIntArray(O) ? toBigInt(value) : +value;
   return arrayWith(O, getTypedArrayConstructor(O), relativeIndex, actualValue);
-} }['with'], !PROPER_ORDER);
+} }.with, !PROPER_ORDER);
