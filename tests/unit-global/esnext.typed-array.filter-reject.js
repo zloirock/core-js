6ab3247c654ea5ad0d4a593f627ba5f589c61ae3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 08:28:08 +0700

    drop descriptors check

--- tests/unit-global/esnext.typed-array.filter-reject.js
+++ tests/unit-global/esnext.typed-array.filter-reject.js
@@ -1,6 +1,6 @@
-import { DESCRIPTORS, TYPED_ARRAYS } from '../helpers/constants.js';
+import { TYPED_ARRAYS } from '../helpers/constants.js';
 
-if (DESCRIPTORS) QUnit.test('%TypedArrayPrototype%.filterReject', assert => {
+QUnit.test('%TypedArrayPrototype%.filterReject', assert => {
   // we can't implement %TypedArrayPrototype% in all engines, so run all tests for each typed array constructor
   for (const { name, TypedArray } of TYPED_ARRAYS) {
     const { filterReject } = TypedArray.prototype;
