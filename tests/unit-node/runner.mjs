a542c2f4186452d44dd6faa8c1de8f384d8447f0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 06:50:46 +0700

    make `core-js` entry an equal of `core-js/actual`

--- tests/unit-node/runner.mjs
+++ tests/unit-node/runner.mjs
@@ -1,5 +1,5 @@
 await Promise.all([
-  ['packages/core-js/index', 'tests/bundles/unit-global'],
-  ['packages/core-js/index', 'packages/core-js-bundle/index', 'tests/bundles/unit-global'],
+  ['packages/core-js/full/index', 'tests/bundles/unit-global'],
+  ['packages/core-js/full/index', 'packages/core-js-bundle/index', 'tests/bundles/unit-global'],
   ['tests/bundles/unit-pure'],
 ].map(files => $`qunit ${ files.map(file => `${ file }.js`) }`));
