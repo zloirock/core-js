e99ab4bc87f5b6219e015d641cb72a83554ea595
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 09:03:29 +0700

    drop `es.array.is-array` (required for ES3 only)

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -1436,7 +1436,6 @@ const forbidESAnnexBBuiltIns = {
 };
 
 const forbidES5BuiltIns = {
-  'es/no-array-isarray': ERROR,
   'es/no-array-prototype-every': ERROR,
   'es/no-array-prototype-filter': ERROR,
   'es/no-array-prototype-foreach': ERROR,
