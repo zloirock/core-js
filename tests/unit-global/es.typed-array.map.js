b60e0a1a7618fb7256e43076de186cafc6910670
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 08:28:08 +0700

    drop descriptors check

--- tests/unit-global/es.typed-array.map.js
+++ tests/unit-global/es.typed-array.map.js
@@ -1,6 +1,6 @@
-import { DESCRIPTORS, TYPED_ARRAYS } from '../helpers/constants.js';
+import { TYPED_ARRAYS } from '../helpers/constants.js';
 
-if (DESCRIPTORS) QUnit.test('%TypedArrayPrototype%.map', assert => {
+QUnit.test('%TypedArrayPrototype%.map', assert => {
   // we can't implement %TypedArrayPrototype% in all engines, so run all tests for each typed array constructor
   for (const { name, TypedArray } of TYPED_ARRAYS) {
     const { map } = TypedArray.prototype;
--- tests/unit-pure/es.reflect.get.js
+++ tests/unit-pure/es.reflect.get.js
@@ -1,5 +1,3 @@
-import { DESCRIPTORS } from '../helpers/constants.js';
-
 import create from 'core-js-pure/es/object/create';
 import defineProperty from 'core-js-pure/es/object/define-property';
 import get from 'core-js-pure/es/reflect/get';
@@ -10,27 +8,27 @@ QUnit.test('Reflect.get', assert => {
     assert.name(get, 'get');
   }
   assert.same(get({ qux: 987 }, 'qux'), 987);
-  if (DESCRIPTORS) {
-    const target = create(defineProperty({ z: 3 }, 'w', {
+
+  const target = create(defineProperty({ z: 3 }, 'w', {
+    get() {
+      return this;
+    },
+  }), {
+    x: {
+      value: 1,
+    },
+    y: {
       get() {
         return this;
       },
-    }), {
-      x: {
-        value: 1,
-      },
-      y: {
-        get() {
-          return this;
-        },
-      },
-    });
-    const receiver = {};
-    assert.same(get(target, 'x', receiver), 1, 'get x');
-    assert.same(get(target, 'y', receiver), receiver, 'get y');
-    assert.same(get(target, 'z', receiver), 3, 'get z');
-    assert.same(get(target, 'w', receiver), receiver, 'get w');
-    assert.same(get(target, 'u', receiver), undefined, 'get u');
-  }
+    },
+  });
+  const receiver = {};
+  assert.same(get(target, 'x', receiver), 1, 'get x');
+  assert.same(get(target, 'y', receiver), receiver, 'get y');
+  assert.same(get(target, 'z', receiver), 3, 'get z');
+  assert.same(get(target, 'w', receiver), receiver, 'get w');
+  assert.same(get(target, 'u', receiver), undefined, 'get u');
+
   assert.throws(() => get(42, 'constructor'), TypeError, 'throws on primitive');
 });
