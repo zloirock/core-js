a83c14fbb92972471f390ba4da16f7c0c36e280b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-06 08:28:08 +0700

    drop descriptors check

--- tests/unit-global/es.reflect.define-property.js
+++ tests/unit-global/es.reflect.define-property.js
@@ -1,5 +1,3 @@
-import { DESCRIPTORS } from '../helpers/constants.js';
-
 QUnit.test('Reflect.defineProperty', assert => {
   const { defineProperty } = Reflect;
   const { getOwnPropertyDescriptor, create } = Object;
@@ -11,30 +9,26 @@ QUnit.test('Reflect.defineProperty', assert => {
   let object = {};
   assert.true(defineProperty(object, 'foo', { value: 123 }));
   assert.same(object.foo, 123);
-  if (DESCRIPTORS) {
-    object = {};
-    defineProperty(object, 'foo', {
-      value: 123,
-      enumerable: true,
-    });
-    assert.deepEqual(getOwnPropertyDescriptor(object, 'foo'), {
-      value: 123,
-      enumerable: true,
-      configurable: false,
-      writable: false,
-    });
-    assert.false(defineProperty(object, 'foo', {
-      value: 42,
-    }));
-  }
+
+  object = {};
+  defineProperty(object, 'foo', {
+    value: 123,
+    enumerable: true,
+  });
+  assert.deepEqual(getOwnPropertyDescriptor(object, 'foo'), {
+    value: 123,
+    enumerable: true,
+    configurable: false,
+    writable: false,
+  });
+  assert.false(defineProperty(object, 'foo', {
+    value: 42,
+  }));
+
   assert.throws(() => defineProperty(42, 'foo', {
     value: 42,
   }), TypeError, 'throws on primitive');
   assert.throws(() => defineProperty(42, 1, {}));
   assert.throws(() => defineProperty({}, create(null), {}));
   assert.throws(() => defineProperty({}, 1, 1));
-});
-
-QUnit.test('Reflect.defineProperty.sham flag', assert => {
-  assert.same(Reflect.defineProperty.sham, DESCRIPTORS ? undefined : true);
 });
