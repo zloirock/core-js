d3371dfe0c8b86049269642bb43664f74259b106
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 07:23:21 +0700

    drop non-standard `Symbol.{ useSetter, useSimple }` methods control of `Symbol` polyfill

--- packages/core-js/modules/es.symbol.constructor.js
+++ packages/core-js/modules/es.symbol.constructor.js
@@ -223,11 +223,6 @@ $forEach(objectKeys(WellKnownSymbolsStore), function (name) {
   defineWellKnownSymbol(name);
 });
 
-$({ target: SYMBOL, stat: true, forced: !NATIVE_SYMBOL }, {
-  useSetter: function () { USE_SETTER = true; },
-  useSimple: function () { USE_SETTER = false; },
-});
-
 $({ target: 'Object', stat: true, forced: !NATIVE_SYMBOL }, {
   // `Object.create` method
   // https://tc39.es/ecma262/#sec-object.create
