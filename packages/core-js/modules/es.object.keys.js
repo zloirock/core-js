68e48852b2f22c3b197477d0fa02526f23d028cf
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-08 05:53:33 +0700

    use native `Object.{ keys, getOwnPropertyNames }` without fallbacks in simple cases

--- packages/core-js/modules/es.object.keys.js
+++ packages/core-js/modules/es.object.keys.js
@@ -1,15 +1,16 @@
 'use strict';
 var $ = require('../internals/export');
 var toObject = require('../internals/to-object');
-var nativeKeys = require('../internals/object-keys');
 var fails = require('../internals/fails');
 
-var FAILS_ON_PRIMITIVES = fails(function () { nativeKeys(1); });
+var nativeObjectKeys = Object.keys;
+
+var FAILS_ON_PRIMITIVES = fails(function () { nativeObjectKeys(1); });
 
 // `Object.keys` method
 // https://tc39.es/ecma262/#sec-object.keys
 $({ target: 'Object', stat: true, forced: FAILS_ON_PRIMITIVES }, {
   keys: function keys(it) {
-    return nativeKeys(toObject(it));
+    return nativeObjectKeys(toObject(it));
   },
 });
