99c00be4a0b5f1aa5689fb6befd69734b535ca8f
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 11:03:44 +0700

    drop `es.function.bind` (required for IE8- only)

--- packages/core-js/internals/function-uncurry-this.js
+++ packages/core-js/internals/function-uncurry-this.js
@@ -1,12 +1,4 @@
 'use strict';
-var NATIVE_BIND = require('../internals/function-bind-native');
+var call = Function.prototype.call;
 
-var FunctionPrototype = Function.prototype;
-var call = FunctionPrototype.call;
-var uncurryThisWithBind = NATIVE_BIND && FunctionPrototype.bind.bind(call, call);
-
-module.exports = NATIVE_BIND ? uncurryThisWithBind : function (fn) {
-  return function () {
-    return call.apply(fn, arguments);
-  };
-};
+module.exports = call.bind.bind(call, call);
