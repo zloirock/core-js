4841603aac6213be40bd8f69b3acc20a28ee9ec7
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-07 11:03:44 +0700

    drop `es.function.bind` (required for IE8- only)

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -1452,7 +1452,6 @@ const forbidES5BuiltIns = {
   'es/no-array-prototype-reduce': ERROR,
   'es/no-array-prototype-reduceright': ERROR,
   'es/no-array-prototype-some': ERROR,
-  'es/no-function-prototype-bind': ERROR,
   'es/no-object-defineproperties': ERROR,
   'es/no-object-defineproperty': ERROR,
   'es/no-object-getownpropertydescriptor': ERROR,
