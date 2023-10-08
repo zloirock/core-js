b10a024947a62407871e9cac28012dae63dff827
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-09 03:25:07 +0700

    use native `Object.{ defineProperty, getOwnPropertyDescriptor }` without fallbacks in simple cases

--- packages/core-js/internals/function-uncurry-this-accessor.js
+++ packages/core-js/internals/function-uncurry-this-accessor.js
@@ -4,7 +4,6 @@ var aCallable = require('../internals/a-callable');
 
 module.exports = function (object, key, method) {
   try {
-    // eslint-disable-next-line es/no-object-getownpropertydescriptor -- safe
     return uncurryThis(aCallable(Object.getOwnPropertyDescriptor(object, key)[method]));
   } catch (error) { /* empty */ }
 };
