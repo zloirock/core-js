9430582a7935af51ae291e62ffabb876ce9c57ab
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-09 03:25:07 +0700

    use native `Object.{ defineProperty, getOwnPropertyDescriptor }` without fallbacks in simple cases

--- packages/core-js/modules/es.array.push.js
+++ packages/core-js/modules/es.array.push.js
@@ -14,7 +14,6 @@ var INCORRECT_TO_LENGTH = fails(function () {
 // https://bugs.chromium.org/p/v8/issues/detail?id=12681
 var properErrorOnNonWritableLength = function () {
   try {
-    // eslint-disable-next-line es/no-object-defineproperty -- safe
     Object.defineProperty([], 'length', { writable: false }).push();
   } catch (error) {
     return error instanceof TypeError;
