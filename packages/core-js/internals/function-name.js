a108a58c16ef7cfd6a7ee9170c8f9114ab598c7d
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-09 03:25:07 +0700

    use native `Object.{ defineProperty, getOwnPropertyDescriptor }` without fallbacks in simple cases

--- packages/core-js/internals/function-name.js
+++ packages/core-js/internals/function-name.js
@@ -2,13 +2,11 @@
 var hasOwn = require('../internals/has-own-property');
 
 var FunctionPrototype = Function.prototype;
-// eslint-disable-next-line es/no-object-getownpropertydescriptor -- safe
-var getDescriptor = Object.getOwnPropertyDescriptor;
 
 var EXISTS = hasOwn(FunctionPrototype, 'name');
 // additional protection from minified / mangled / dropped function names
 var PROPER = EXISTS && function something() { /* empty */ }.name === 'something';
-var CONFIGURABLE = EXISTS && getDescriptor(FunctionPrototype, 'name').configurable;
+var CONFIGURABLE = EXISTS && Object.getOwnPropertyDescriptor(FunctionPrototype, 'name').configurable;
 
 module.exports = {
   EXISTS: EXISTS,
--- packages/core-js/modules/es.object.lookup-getter.js
+++ packages/core-js/modules/es.object.lookup-getter.js
@@ -3,9 +3,9 @@ var $ = require('../internals/export');
 var FORCED = require('../internals/object-prototype-accessors-forced');
 var toObject = require('../internals/to-object');
 var toPropertyKey = require('../internals/to-property-key');
-var getOwnPropertyDescriptor = require('../internals/object-get-own-property-descriptor').f;
 
 var getPrototypeOf = Object.getPrototypeOf;
+var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 
 // `Object.prototype.__lookupGetter__` method
 // https://tc39.es/ecma262/#sec-object.prototype.__lookupGetter__
