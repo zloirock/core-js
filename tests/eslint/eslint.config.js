0f1297e528d0db34609c149a69de68a4788674d3
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-10 04:47:34 +0700

    update `core-js-builder` dependencies

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -1116,7 +1116,7 @@ const base = {
 
   // suggest better alternatives to some dependencies
   'depend/ban-dependencies': [ERROR, { allowed: [
-    'mkdirp', // TODO: drop from `core-js@4`
+    // empty
   ] }],
 };
 
