78abe7faaf4cdcd8b12fbecbeefbb3f12ebc168b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-10 01:40:37 +0700

    `core-js` version in `core-js-compat` should be specified as a SemVer string with minor component

--- tests/compat-tools/get-modules-list-for-target-version.mjs
+++ tests/compat-tools/get-modules-list-for-target-version.mjs
@@ -8,8 +8,6 @@ const modules30 = modulesByVersions['3.0'];
 const filter = new Set([...modules30, ...modulesByVersions['3.1'] ?? []]);
 const modules31 = modules.filter(it => filter.has(it));
 
-deepEqual(getModulesListForTargetVersion(3), modules30, 'num 3'); // TODO: Make it throw in core-js@4
-deepEqual(getModulesListForTargetVersion('3'), modules30, '3'); // TODO: Make it throw in core-js@4
 deepEqual(getModulesListForTargetVersion('3.0'), modules30, '3.0');
 deepEqual(getModulesListForTargetVersion('3.0.0'), modules30, '3.0.0');
 deepEqual(getModulesListForTargetVersion('3.0.1'), modules30, '3.0.1');
@@ -18,6 +16,8 @@ deepEqual(getModulesListForTargetVersion('3.1'), modules31, '3.1');
 deepEqual(getModulesListForTargetVersion('3.1.0'), modules31, '3.1.0');
 deepEqual(getModulesListForTargetVersion('3.1.1'), modules31, '3.1.1');
 
+throws(() => getModulesListForTargetVersion(3), TypeError, 'num 3');
+throws(() => getModulesListForTargetVersion('3'), TypeError, '3');
 throws(() => getModulesListForTargetVersion('2.0'), RangeError, '2.0');
 throws(() => getModulesListForTargetVersion('4.0'), RangeError, '4.0');
 throws(() => getModulesListForTargetVersion('x'), TypeError, 'x');
