6380d28152e90e5a4bd5c6325f2efb0668ca7043
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-10 01:40:37 +0700

    `core-js` version in `core-js-compat` should be specified as a SemVer string with minor component

--- packages/core-js-compat/get-modules-list-for-target-version.js
+++ packages/core-js-compat/get-modules-list-for-target-version.js
@@ -4,9 +4,12 @@ const modulesByVersions = require('./modules-by-versions');
 const modules = require('./modules');
 
 module.exports = function (raw) {
-  const corejs = semver(raw);
+  if (!['string', 'object'].includes(typeof raw)) {
+    throw new TypeError('`core-js` version should be specified as a SemVer string with minor component');
+  }
+  const corejs = semver(raw, true);
   if (corejs.major !== 3) {
-    throw new RangeError('This version of `core-js-compat` works only with `core-js@3`.');
+    throw new RangeError('This version of `core-js-compat` works only with `core-js@3`');
   }
   const result = [];
   for (const version of Object.keys(modulesByVersions)) {
