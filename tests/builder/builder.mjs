cd106e84314bbefe7eb6d1de26d8029ebf34bd79
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-11 12:47:39 +0700

    make `core-js-builder` return an object (for future extensions)

--- tests/builder/builder.mjs
+++ tests/builder/builder.mjs
@@ -1,20 +1,20 @@
 import { ok } from 'node:assert/strict';
 import builder from 'core-js-builder';
 
-const polyfills = await builder({
+const { script } = await builder({
   modules: 'core-js/actual',
   exclude: [/group-by/, 'es.typed-array.with'],
   targets: { node: 16 },
   format: 'esm',
 });
 
-ok(polyfills.includes("import 'core-js/modules/es.error.cause.js';"), 'actual node 16 #1');
-ok(polyfills.includes("import 'core-js/modules/es.array.push.js';"), 'actual node 16 #2');
-ok(polyfills.includes("import 'core-js/modules/esnext.iterator.zip.js';"), 'actual node 16 #3');
-ok(polyfills.includes("import 'core-js/modules/web.structured-clone.js';"), 'actual node 16 #4');
-ok(!polyfills.includes("import 'core-js/modules/es.weak-set.constructor.js';"), 'actual node 16 #5');
-ok(!polyfills.includes("import 'core-js/modules/es.typed-array.with.js';"), 'actual node 16 #6');
-ok(!polyfills.includes("import 'core-js/modules/esnext.object.group-by.js';"), 'actual node 16 #7');
-ok(!polyfills.includes("import 'core-js/modules/esnext.weak-set.from.js';"), 'actual node 16 #8');
+ok(script.includes("import 'core-js/modules/es.error.cause.js';"), 'actual node 16 #1');
+ok(script.includes("import 'core-js/modules/es.array.push.js';"), 'actual node 16 #2');
+ok(script.includes("import 'core-js/modules/esnext.iterator.zip.js';"), 'actual node 16 #3');
+ok(script.includes("import 'core-js/modules/web.structured-clone.js';"), 'actual node 16 #4');
+ok(!script.includes("import 'core-js/modules/es.weak-set.constructor.js';"), 'actual node 16 #5');
+ok(!script.includes("import 'core-js/modules/es.typed-array.with.js';"), 'actual node 16 #6');
+ok(!script.includes("import 'core-js/modules/esnext.object.group-by.js';"), 'actual node 16 #7');
+ok(!script.includes("import 'core-js/modules/esnext.weak-set.from.js';"), 'actual node 16 #8');
 
 echo(chalk.green('builder tested'));
