9e7350598f6706b3e407f60d4e7110979144abf7
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 02:36:28 +0700

    some simplification and untangling

--- packages/core-js/modules/es.object.get-own-property-descriptor.js
+++ packages/core-js/modules/es.object.get-own-property-descriptor.js
@@ -2,14 +2,14 @@
 var $ = require('../internals/export');
 var fails = require('../internals/fails');
 var toIndexedObject = require('../internals/to-indexed-object');
-var nativeGetOwnPropertyDescriptor = require('../internals/object-get-own-property-descriptor').f;
+var getOwnPropertyDescriptorModule = require('../internals/object-get-own-property-descriptor');
 
-var FORCED = fails(function () { nativeGetOwnPropertyDescriptor(1); });
+var FORCED = fails(function () { getOwnPropertyDescriptorModule.f(1); });
 
 // `Object.getOwnPropertyDescriptor` method
 // https://tc39.es/ecma262/#sec-object.getownpropertydescriptor
 $({ target: 'Object', stat: true, forced: FORCED }, {
   getOwnPropertyDescriptor: function getOwnPropertyDescriptor(it, key) {
-    return nativeGetOwnPropertyDescriptor(toIndexedObject(it), key);
+    return getOwnPropertyDescriptorModule.f(toIndexedObject(it), key);
   },
 });
