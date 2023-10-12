0db8ef3f7ff161df0a73bdf5a591f7407fdb71d1
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-12 13:13:05 +0700

    move some `/modules/` dependencies to entries

--- packages/core-js/full/reflect/define-metadata.js
+++ packages/core-js/full/reflect/define-metadata.js
@@ -1,4 +1,6 @@
 'use strict';
+require('../../modules/es.map.constructor');
+require('../../modules/es.weak-map.constructor');
 require('../../modules/esnext.reflect.define-metadata');
 var path = require('../../internals/path');
 
--- packages/core-js/stable/url-search-params/index.js
+++ packages/core-js/stable/url-search-params/index.js
@@ -1,4 +1,6 @@
 'use strict';
+require('../../modules/es.array.iterator');
+require('../../modules/es.string.from-code-point');
 require('../../modules/web.url-search-params.constructor');
 require('../../modules/web.url-search-params.delete');
 require('../../modules/web.url-search-params.has');
