c6faa17a69d91c594fd0fad25dab2e6c2bf9134a
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 02:36:28 +0700

    some simplification and untangling

--- packages/core-js/full/number/from-string.js
+++ packages/core-js/full/number/from-string.js
@@ -1,4 +1,5 @@
 'use strict';
+require('../../modules/es.number.parse-int');
 require('../../modules/esnext.number.from-string');
 var path = require('../../internals/path');
 
--- packages/core-js/modules/esnext.function.metadata.js
+++ packages/core-js/modules/esnext.function.metadata.js
@@ -1,6 +1,5 @@
 'use strict';
 var wellKnownSymbol = require('../internals/well-known-symbol');
-var defineProperty = require('../internals/object-define-property').f;
 
 var METADATA = wellKnownSymbol('metadata');
 var FunctionPrototype = Function.prototype;
@@ -8,7 +7,7 @@ var FunctionPrototype = Function.prototype;
 // Function.prototype[@@metadata]
 // https://github.com/tc39/proposal-decorator-metadata
 if (FunctionPrototype[METADATA] === undefined) {
-  defineProperty(FunctionPrototype, METADATA, {
+  Object.defineProperty(FunctionPrototype, METADATA, {
     value: null,
   });
 }
