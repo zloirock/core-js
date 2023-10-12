fbbad4321239724dfcf16838689386e174c7d45b
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 02:36:28 +0700

    some simplification and untangling

--- packages/core-js-pure/override/internals/set-to-string-tag.js
+++ packages/core-js-pure/override/internals/set-to-string-tag.js
@@ -1,11 +1,11 @@
 'use strict';
 var TO_STRING_TAG_SUPPORT = require('../internals/to-string-tag-support');
-var defineProperty = require('../internals/object-define-property').f;
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var hasOwn = require('../internals/has-own-property');
 var toString = require('../internals/object-to-string');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
+var defineProperty = Object.defineProperty;
 var TO_STRING_TAG = wellKnownSymbol('toStringTag');
 
 module.exports = function (it, TAG, STATIC, SET_METHOD) {
