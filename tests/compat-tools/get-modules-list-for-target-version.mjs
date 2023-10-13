b4573d5fe65b2a18aebfad35fd2e718020669d6e
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 13:10:11 +0700

    bump version (without publishing)

--- tests/compat-tools/get-modules-list-for-target-version.mjs
+++ tests/compat-tools/get-modules-list-for-target-version.mjs
@@ -4,22 +4,22 @@ import getModulesListForTargetVersion from '@core-js/compat/get-modules-list-for
 const modules = await fs.readJson('packages/core-js-compat/modules.json');
 const modulesByVersions = await fs.readJson('packages/core-js-compat/modules-by-versions.json');
 
-const modules30 = modulesByVersions['3.0'];
-const filter = new Set([...modules30, ...modulesByVersions['3.1'] ?? []]);
-const modules31 = modules.filter(it => filter.has(it));
+const modules40 = modulesByVersions['4.0'];
+const filter = new Set([...modules40, ...modulesByVersions['4.1'] ?? []]);
+const modules41 = modules.filter(it => filter.has(it));
 
-deepEqual(getModulesListForTargetVersion('3.0'), modules30, '3.0');
-deepEqual(getModulesListForTargetVersion('3.0.0'), modules30, '3.0.0');
-deepEqual(getModulesListForTargetVersion('3.0.1'), modules30, '3.0.1');
-deepEqual(getModulesListForTargetVersion('3.0.0-alpha.1'), modules30, '3.0.0-alpha.1');
-deepEqual(getModulesListForTargetVersion('3.1'), modules31, '3.1');
-deepEqual(getModulesListForTargetVersion('3.1.0'), modules31, '3.1.0');
-deepEqual(getModulesListForTargetVersion('3.1.1'), modules31, '3.1.1');
+deepEqual(getModulesListForTargetVersion('4.0'), modules40, '4.0');
+deepEqual(getModulesListForTargetVersion('4.0.0'), modules40, '4.0.0');
+deepEqual(getModulesListForTargetVersion('4.0.1'), modules40, '4.0.1');
+deepEqual(getModulesListForTargetVersion('4.0.0-alpha.1'), modules40, '4.0.0-alpha.1');
+deepEqual(getModulesListForTargetVersion('4.1'), modules41, '4.1');
+deepEqual(getModulesListForTargetVersion('4.1.0'), modules41, '4.1.0');
+deepEqual(getModulesListForTargetVersion('4.1.1'), modules41, '4.1.1');
 
-throws(() => getModulesListForTargetVersion(3), TypeError, 'num 3');
-throws(() => getModulesListForTargetVersion('3'), TypeError, '3');
-throws(() => getModulesListForTargetVersion('2.0'), RangeError, '2.0');
-throws(() => getModulesListForTargetVersion('4.0'), RangeError, '4.0');
+throws(() => getModulesListForTargetVersion(4), TypeError, 'num 4');
+throws(() => getModulesListForTargetVersion('4'), TypeError, '4');
+throws(() => getModulesListForTargetVersion('3.0'), RangeError, '3.0');
+throws(() => getModulesListForTargetVersion('5.0'), RangeError, '5.0');
 throws(() => getModulesListForTargetVersion('x'), TypeError, 'x');
 throws(() => getModulesListForTargetVersion('*'), TypeError, '*');
 throws(() => getModulesListForTargetVersion(), TypeError, 'no arg');
