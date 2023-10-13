79095c9c2646540ee3fd189175e52f52febfa656
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 23:29:37 +0700

    drop old TC39 `Observable` proposal in favor of future adding new Web Platform `Observable` proposal

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -2047,9 +2047,6 @@ GLOBAL.tests = {
   'esnext.symbol.metadata': function () {
     return Symbol.metadata;
   },
-  'esnext.symbol.observable': function () {
-    return Symbol.observable;
-  },
   'esnext.typed-array.filter-reject': function () {
     return Int8Array.prototype.filterReject;
   },
