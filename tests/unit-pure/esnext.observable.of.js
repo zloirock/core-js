ffc1be4fdf565e26958074c528029a07501f08bd
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 23:29:37 +0700

    drop old TC39 `Observable` proposal in favor of future adding new Web Platform `Observable` proposal

--- tests/unit-pure/esnext.observable.of.js
+++
@@ -1,7 +0,0 @@
-import Observable from '@core-js/pure/full/observable';
-
-QUnit.test('Observable.of', assert => {
-  assert.isFunction(Observable.of);
-  assert.arity(Observable.of, 0);
-  assert.name(Observable.of, 'of');
-});
