845b3ad356f80ffd346986ac6cb23af73e47e153
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 23:29:37 +0700

    drop old TC39 `Observable` proposal in favor of future adding new Web Platform `Observable` proposal

--- packages/core-js/full/symbol/index.js
+++ packages/core-js/full/symbol/index.js
@@ -3,6 +3,5 @@ var parent = require('../../actual/symbol');
 require('../../modules/esnext.symbol.is-registered-symbol');
 require('../../modules/esnext.symbol.is-well-known-symbol');
 require('../../modules/esnext.symbol.custom-matcher');
-require('../../modules/esnext.symbol.observable');
 
 module.exports = parent;
--- tests/unit-pure/esnext.observable.from.js
+++
@@ -1,6 +0,0 @@
-import Observable from '@core-js/pure/full/observable';
-
-QUnit.test('Observable.from', assert => {
-  assert.isFunction(Observable.from);
-  assert.arity(Observable.from, 1);
-});
