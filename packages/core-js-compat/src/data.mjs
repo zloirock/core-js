be3a4b87d37ed9c720e6352aead57156f2286555
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-13 23:29:37 +0700

    drop old TC39 `Observable` proposal in favor of future adding new Web Platform `Observable` proposal

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2034,12 +2034,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.object.group-by': null,
-  'esnext.observable.constructor': {
-  },
-  'esnext.observable.from': {
-  },
-  'esnext.observable.of': {
-  },
   // TODO: Remove from `core-js@4`
   'esnext.promise.with-resolvers': null,
   // TODO: Remove from `core-js@4`
@@ -2178,8 +2172,6 @@ export const data = {
   },
   'esnext.symbol.metadata': {
     deno: '1.40.4',
-  },
-  'esnext.symbol.observable': {
   },
   'esnext.typed-array.filter-reject': {
   },
