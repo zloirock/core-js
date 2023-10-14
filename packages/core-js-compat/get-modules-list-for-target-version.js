4c5edc6c7b194f9bf15992b16022730a803a7ebf
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-15 02:58:57 +0700

    prototype magic options as `core-js` version for tooling

--- packages/core-js-compat/get-modules-list-for-target-version.js
+++ packages/core-js-compat/get-modules-list-for-target-version.js
@@ -1,16 +1,10 @@
 'use strict';
-const { compare, intersection, semver } = require('./helpers');
+const { compare, intersection, normalizeCoreJSVersion } = require('./helpers');
 const modulesByVersions = require('./modules-by-versions');
 const modules = require('./modules');
 
 module.exports = function (raw) {
-  if (!['string', 'object'].includes(typeof raw)) {
-    throw new TypeError('`core-js` version should be specified as a SemVer string with minor component');
-  }
-  const corejs = semver(raw, true);
-  if (corejs.major !== 4) {
-    throw new RangeError('This version of `@core-js/compat` works only with `core-js@4`');
-  }
+  const corejs = normalizeCoreJSVersion(raw);
   const result = [];
   for (const version of Object.keys(modulesByVersions)) {
     if (compare(version, '<=', corejs)) {
