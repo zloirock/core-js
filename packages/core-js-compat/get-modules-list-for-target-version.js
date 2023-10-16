5af9068891cd7b053b4ff01a262f8796ef72b341
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-16 07:38:22 +0700

    simplify helper

--- packages/core-js-compat/get-modules-list-for-target-version.js
+++ packages/core-js-compat/get-modules-list-for-target-version.js
@@ -1,15 +1,12 @@
 'use strict';
 const { compare, intersection, normalizeCoreJSVersion } = require('./helpers');
 const modulesByVersions = require('./modules-by-versions');
-const modules = require('./modules');
+const allModules = require('./modules');
 
 module.exports = function (raw) {
   const corejs = normalizeCoreJSVersion(raw);
-  const result = [];
-  for (const version of Object.keys(modulesByVersions)) {
-    if (compare(version, '<=', corejs)) {
-      result.push(...modulesByVersions[version]);
-    }
-  }
-  return intersection(result, modules);
+  const result = Object.entries(modulesByVersions).flatMap(([version, modules]) => {
+    return compare(version, '<=', corejs) ? modules : [];
+  });
+  return intersection(result, allModules);
 };
