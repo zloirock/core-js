f936e602ffbf263bdb2acb240ef15fb486c9a51e
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-19 01:57:58 +0700

    split `get-built-in` and `get-built-in-static-method` helpers

--- packages/core-js/internals/get-built-in.js
+++ packages/core-js/internals/get-built-in.js
@@ -2,10 +2,7 @@
 var global = require('../internals/global');
 var isCallable = require('../internals/is-callable');
 
-var aFunction = function (argument) {
-  return isCallable(argument) ? argument : undefined;
-};
-
-module.exports = function (namespace, method) {
-  return arguments.length < 2 ? aFunction(global[namespace]) : global[namespace] && global[namespace][method];
+module.exports = function (CONSTRUCTOR) {
+  var C = global[CONSTRUCTOR];
+  return isCallable(C) ? C : undefined;
 };
