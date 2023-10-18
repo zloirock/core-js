71d5dbe13e927cf0b80f52442d2dd3f18c4bcf43
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-19 00:54:21 +0700

    update after rebase

--- packages/core-js/modules/esnext.array.from-async.js
+++ packages/core-js/modules/esnext.array.from-async.js
@@ -9,8 +9,8 @@ var getIterator = require('../internals/get-iterator');
 var getIteratorDirect = require('../internals/get-iterator-direct');
 var getIteratorMethod = require('../internals/get-iterator-method');
 var getMethod = require('../internals/get-method');
-var getVirtual = require('../internals/entry-virtual');
 var getBuiltIn = require('../internals/get-built-in');
+var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 var AsyncFromSyncIterator = require('../internals/async-from-sync-iterator');
 var toArray = require('../internals/async-iterator-iteration').toArray;
@@ -18,7 +18,7 @@ var fails = require('../internals/fails');
 
 var ASYNC_ITERATOR = wellKnownSymbol('asyncIterator');
 var nativeFromAsync = Array.fromAsync;
-var arrayIterator = uncurryThis(getVirtual('Array').values);
+var arrayIterator = uncurryThis(getBuiltInPrototypeMethod('Array', 'values'));
 var arrayIteratorNext = uncurryThis(arrayIterator([]).next);
 
 var safeArrayIterator = function () {
