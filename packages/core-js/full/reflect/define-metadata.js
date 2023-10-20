5df4aeec7de7b98f5635e4dbfe81060f8c3cac94
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-20 15:22:28 +0700

    drop Metadata Reflection API
    https://github.com/zloirock/core-js/issues/1299

--- packages/core-js/full/reflect/define-metadata.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-require('../../modules/es.map.constructor');
-require('../../modules/es.weak-map.constructor');
-require('../../modules/esnext.reflect.define-metadata');
-var path = require('../../internals/path');
-
-module.exports = path.Reflect.defineMetadata;
