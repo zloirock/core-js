5dde2fe4116c1f10e7e6632910d3bfe075224d60
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-20 15:22:28 +0700

    drop Metadata Reflection API
    https://github.com/zloirock/core-js/issues/1299

--- tests/unit-pure/esnext.reflect.define-metadata.js
+++
@@ -1,9 +0,0 @@
-import defineMetadata from '@core-js/pure/full/reflect/define-metadata';
-
-QUnit.test('Reflect.defineMetadata', assert => {
-  assert.isFunction(defineMetadata);
-  assert.arity(defineMetadata, 3);
-  assert.throws(() => defineMetadata('key', 'value', undefined, undefined), TypeError);
-  assert.same(defineMetadata('key', 'value', {}, undefined), undefined);
-  assert.same(defineMetadata('key', 'value', {}, 'name'), undefined);
-});
