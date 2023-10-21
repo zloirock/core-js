72c4929140fe42c15a4e043ad759caa481c9d4e2
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-22 04:50:24 +0700

    untangle `es.array.iterator` deps

--- packages/core-js/modules/web.dom-collections.iterator.js
+++ packages/core-js/modules/web.dom-collections.iterator.js
@@ -1,14 +1,14 @@
 'use strict';
 var globalThis = require('../internals/global-this');
+var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
 var DOMIterables = require('../internals/dom-iterables');
 var DOMTokenListPrototype = require('../internals/dom-token-list-prototype');
-var ArrayIteratorMethods = require('../modules/es.array.iterator');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 var setToStringTag = require('../internals/set-to-string-tag');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
 var ITERATOR = wellKnownSymbol('iterator');
-var ArrayValues = ArrayIteratorMethods.values;
+var ArrayValues = getBuiltInPrototypeMethod('Array', 'values');
 
 var handlePrototype = function (CollectionPrototype, COLLECTION_NAME) {
   if (CollectionPrototype) {
@@ -19,14 +19,15 @@ var handlePrototype = function (CollectionPrototype, COLLECTION_NAME) {
       CollectionPrototype[ITERATOR] = ArrayValues;
     }
     setToStringTag(CollectionPrototype, COLLECTION_NAME, true);
-    if (DOMIterables[COLLECTION_NAME]) for (var METHOD_NAME in ArrayIteratorMethods) {
+    if (DOMIterables[COLLECTION_NAME]) ['entries', 'keys', 'values'].forEach(function (METHOD_NAME) {
+      var method = getBuiltInPrototypeMethod('Array', METHOD_NAME);
       // some Chrome versions have non-configurable methods on DOMTokenList
-      if (CollectionPrototype[METHOD_NAME] !== ArrayIteratorMethods[METHOD_NAME]) try {
-        createNonEnumerableProperty(CollectionPrototype, METHOD_NAME, ArrayIteratorMethods[METHOD_NAME]);
+      if (CollectionPrototype[METHOD_NAME] !== method) try {
+        createNonEnumerableProperty(CollectionPrototype, METHOD_NAME, method);
       } catch (error) {
-        CollectionPrototype[METHOD_NAME] = ArrayIteratorMethods[METHOD_NAME];
+        CollectionPrototype[METHOD_NAME] = method;
       }
-    }
+    });
   }
 };
 
