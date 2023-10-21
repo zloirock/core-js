a364a23080ab553a674f4dd9bf4a62c8cfa27057
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-22 04:50:24 +0700

    untangle `es.array.iterator` deps

--- packages/core-js/es/typed-array/methods.js
+++ packages/core-js/es/typed-array/methods.js
@@ -1,5 +1,6 @@
 'use strict';
 require('../../modules/es.object.to-string');
+require('../../modules/es.array.iterator');
 require('../../modules/es.string.iterator');
 require('../../modules/es.typed-array.from');
 require('../../modules/es.typed-array.of');
