cd863bdb1729f1c4377f9fd1c27e6008012b3dda
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-22 04:50:24 +0700

    untangle `es.array.iterator` deps

--- packages/core-js-pure/override/modules/web.dom-collections.iterator.js
+++ packages/core-js-pure/override/modules/web.dom-collections.iterator.js
@@ -1,11 +1,13 @@
 'use strict';
-require('../modules/es.array.iterator');
-var DOMIterables = require('../internals/dom-iterables');
 var global = require('../internals/global');
+var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
 var setToStringTag = require('../internals/set-to-string-tag');
+var DOMIterables = require('../internals/dom-iterables');
 var Iterators = require('../internals/iterators');
+
+var ArrayValues = getBuiltInPrototypeMethod('Array', 'values');
 
 for (var COLLECTION_NAME in DOMIterables) {
   setToStringTag(global[COLLECTION_NAME], COLLECTION_NAME);
-  Iterators[COLLECTION_NAME] = Iterators.Array;
+  Iterators[COLLECTION_NAME] = ArrayValues;
 }
