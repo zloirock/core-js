39ae42334049212cae6b7099779c95e7baf45703
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-25 15:19:30 +0700

    specify `Array#forEach` as an implicit dependency where it's required

--- packages/core-js/stable/dom-collections/index.js
+++ packages/core-js/stable/dom-collections/index.js
@@ -1,4 +1,5 @@
 'use strict';
+require('../../modules/es.array.for-each');
 require('../../modules/es.array.iterator');
 require('../../modules/es.object.to-string');
 require('../../modules/web.dom-collections.for-each');
