9aa59962137acc8167b65a9c1809e9ca2d917727
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-25 15:19:30 +0700

    specify `Array#forEach` as an implicit dependency where it's required

--- packages/core-js/modules/es.array.for-each.js
+++ packages/core-js/modules/es.array.for-each.js
@@ -1,9 +1,12 @@
 'use strict';
 var $ = require('../internals/export');
-var forEach = require('../internals/array-for-each');
+var $forEach = require('../internals/array-iteration').forEach;
+var arrayMethodIsStrict = require('../internals/array-method-is-strict');
 
 // `Array.prototype.forEach` method
 // https://tc39.es/ecma262/#sec-array.prototype.foreach
-$({ target: 'Array', proto: true, forced: [].forEach !== forEach }, {
-  forEach: forEach,
+$({ target: 'Array', proto: true, forced: arrayMethodIsStrict('forEach') }, {
+  forEach: function forEach(callbackfn /* , thisArg */) {
+    return $forEach(this, callbackfn, arguments.length > 1 ? arguments[1] : undefined);
+  },
 });
