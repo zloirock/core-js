c25847ab86a0612e5095d91a6c8c0300f56565a0
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-25 15:19:30 +0700

    specify `Array#forEach` as an implicit dependency where it's required

--- packages/core-js/stable/dom-collections/for-each.js
+++ packages/core-js/stable/dom-collections/for-each.js
@@ -1,6 +1,6 @@
 'use strict';
+require('../../modules/es.array.for-each');
 require('../../modules/web.dom-collections.for-each');
+var entryUnbind = require('../../internals/entry-unbind');
 
-var parent = require('../../internals/array-for-each');
-
-module.exports = parent;
+module.exports = entryUnbind('Array', 'forEach');
