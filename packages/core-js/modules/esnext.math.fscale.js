e920608e21f889412160050b9b9d9b985f5033f9
Denis Pushkarev <zloirock@zloirock.ru>
2023-10-25 16:57:28 +0700

    specify `Math.fround` as an implicit dependency

--- packages/core-js/modules/esnext.math.fscale.js
+++ packages/core-js/modules/esnext.math.fscale.js
@@ -1,8 +1,9 @@
 'use strict';
 var $ = require('../internals/export');
-
+var getBuiltInStaticMethod = require('../internals/get-built-in-static-method');
 var scale = require('../internals/math-scale');
-var fround = require('../internals/math-fround');
+
+var fround = getBuiltInStaticMethod('Math', 'fround');
 
 // `Math.fscale` method
 // https://rwaldron.github.io/proposal-math-extensions/
