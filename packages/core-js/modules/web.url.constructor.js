ea9f440a5899e8922bb752bd7c64d2af2221c07c
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-02 21:20:04 +0700

    declare some dependencies

--- packages/core-js/modules/web.url.constructor.js
+++ packages/core-js/modules/web.url.constructor.js
@@ -26,7 +26,9 @@ var getInternalSearchParamsState = URLSearchParamsModule.getState;
 var NativeURL = globalThis.URL;
 var TypeError = globalThis.TypeError;
 var parseInt = globalThis.parseInt;
+// dependency: es.object.assign
 var assign = getBuiltInStaticMethod('Object', 'assign');
+// dependency: es.array.from
 var arrayFrom = getBuiltInStaticMethod('Array', 'from');
 var floor = Math.floor;
 var pow = Math.pow;
