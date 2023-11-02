826d8e8e5a09a49b1016229e17cccfebfe05097c
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-02 21:20:04 +0700

    declare some dependencies

--- packages/core-js/internals/async-iterator-create-proxy.js
+++ packages/core-js/internals/async-iterator-create-proxy.js
@@ -12,6 +12,11 @@ var AsyncIteratorPrototype = require('../internals/async-iterator-prototype');
 var createIterResultObject = require('../internals/create-iter-result-object');
 var iteratorClose = require('../internals/iterator-close');
 
+// dependency: es.promise.constructor
+// dependency: es.promise.catch
+// dependency: es.promise.finally
+// dependency: es.promise.reject
+// dependency: es.promise.resolve
 var Promise = getBuiltIn('Promise');
 var create = Object.create;
 
@@ -84,6 +89,7 @@ var createAsyncIteratorProxyPrototype = function (IS_ITERATOR) {
 var WrapForValidAsyncIteratorPrototype = createAsyncIteratorProxyPrototype(true);
 var AsyncIteratorHelperPrototype = createAsyncIteratorProxyPrototype(false);
 
+// dependency: es.object.to-string
 createNonEnumerableProperty(AsyncIteratorHelperPrototype, TO_STRING_TAG, 'Async Iterator Helper');
 
 module.exports = function (nextHandler, IS_ITERATOR) {
