0cf3b0ced9d3100389b2dff0b59ac3aa277cfb71
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-03 01:49:05 +0700

    build indexes

--- scripts/build-entries/index.mjs
+++ scripts/build-entries/index.mjs
@@ -1,6 +1,6 @@
 import { getListOfDependencies, sort, unique } from './get-dependencies.mjs';
 import { features, proposals } from './entries-definitions.mjs';
-import { $justImport } from './templates.mjs';
+import { $justImport, $path } from './templates.mjs';
 import { modules as AllModules } from '@core-js/compat/src/data.mjs';
 
 const { mkdir, writeFile } = fs;
@@ -13,7 +13,7 @@ const ActualModules = sort([...StableModules, ...Stage3Modules]);
 
 const ESSet = new Set(ESModules);
 const StableSet = new Set(StableModules);
-const ActualSet = new Set([...StableModules, ...ActualModules]);
+const ActualSet = new Set(ActualModules);
 
 async function buildEntry(entry, template, modules, filter) {
   if (filter) modules = modules.filter(it => filter.has(it));
@@ -37,3 +37,9 @@ for (const [entry, { modules, template }] of Object.entries(features)) {
 for (const [name, { modules }] of Object.entries(proposals)) {
   await buildEntry(`proposals/${ name }`, $justImport, modules);
 }
+
+await buildEntry('es/index', $path, ESModules, ESSet);
+await buildEntry('stable/index', $path, StableModules, StableSet);
+await buildEntry('actual/index', $path, ActualModules);
+await buildEntry('full/index', $path, AllModules);
+await buildEntry('index', $path, ActualModules);
