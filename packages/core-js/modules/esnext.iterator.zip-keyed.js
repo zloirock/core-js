99ba735c9c2db8b21477eab71899f5c492595ed9
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-02 21:20:04 +0700

    declare some dependencies

--- packages/core-js/modules/esnext.iterator.zip-keyed.js
+++ packages/core-js/modules/esnext.iterator.zip-keyed.js
@@ -5,7 +5,7 @@ var anObjectOrUndefined = require('../internals/an-object-or-undefined');
 var createProperty = require('../internals/create-property');
 var call = require('../internals/function-call');
 var uncurryThis = require('../internals/function-uncurry-this');
-var getBuiltIn = require('../internals/get-built-in');
+var getBuiltInStaticMethod = require('../internals/get-built-in-static-method');
 var propertyIsEnumerableModule = require('../internals/object-property-is-enumerable');
 var getIteratorFlattenable = require('../internals/get-iterator-flattenable');
 var getModeOption = require('../internals/get-mode-option');
@@ -13,7 +13,8 @@ var iteratorCloseAll = require('../internals/iterator-close-all');
 var iteratorZip = require('../internals/iterator-zip');
 
 var create = Object.create;
-var ownKeys = getBuiltIn('Reflect', 'ownKeys');
+// dependency: es.reflect.own-keys
+var ownKeys = getBuiltInStaticMethod('Reflect', 'ownKeys');
 var push = uncurryThis([].push);
 var THROW = 'throw';
 
