b7c812670791cb21d7f7e8a920e81a11afcafefa
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-02 21:20:04 +0700

    declare some dependencies

--- packages/core-js/internals/async-iterator-close.js
+++ packages/core-js/internals/async-iterator-close.js
@@ -3,11 +3,17 @@ var call = require('../internals/function-call');
 var getBuiltIn = require('../internals/get-built-in');
 var getMethod = require('../internals/get-method');
 
+// dependency: es.promise.constructor
+// dependency: es.promise.catch
+// dependency: es.promise.finally
+// dependency: es.promise.resolve
+var Promise = getBuiltIn('Promise');
+
 module.exports = function (iterator, method, argument, reject) {
   try {
     var returnMethod = getMethod(iterator, 'return');
     if (returnMethod) {
-      return getBuiltIn('Promise').resolve(call(returnMethod, iterator)).then(function () {
+      return Promise.resolve(call(returnMethod, iterator)).then(function () {
         method(argument);
       }, function (error) {
         reject(error);
