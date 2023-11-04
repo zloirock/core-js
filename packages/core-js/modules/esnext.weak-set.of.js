2dace7fa6c66f6e7c1f1c0a40948b6821ec964a4
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-05 06:51:22 +0700

    add entries definitions for `/set/`

--- packages/core-js/modules/esnext.weak-set.of.js
+++ packages/core-js/modules/esnext.weak-set.of.js
@@ -5,6 +5,7 @@ var createCollectionOf = require('../internals/collection-of');
 
 // `WeakSet.of` method
 // https://tc39.github.io/proposal-setmap-offrom/#sec-weakset.of
+// dependency: es.weak-set.constructor
 $({ target: 'WeakSet', stat: true, forced: true }, {
   of: createCollectionOf(WeakSetHelpers.WeakSet, WeakSetHelpers.add, false),
 });
