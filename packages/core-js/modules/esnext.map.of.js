d1366888ef0e0fc5a716db101d64e81fe7c378d0
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-05 06:51:22 +0700

    add entries definitions for `/set/`

--- packages/core-js/modules/esnext.map.of.js
+++ packages/core-js/modules/esnext.map.of.js
@@ -5,6 +5,7 @@ var createCollectionOf = require('../internals/collection-of');
 
 // `Map.of` method
 // https://tc39.github.io/proposal-setmap-offrom/#sec-map.of
+// dependency: es.map.constructor
 $({ target: 'Map', stat: true, forced: true }, {
   of: createCollectionOf(MapHelpers.Map, MapHelpers.set, true),
 });
