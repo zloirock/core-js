f38ec656fc5b48eff944094e025c3137fd8279c6
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-05 06:51:22 +0700

    add entries definitions for `/set/`

--- packages/core-js/modules/esnext.weak-map.from.js
+++ packages/core-js/modules/esnext.weak-map.from.js
@@ -5,6 +5,7 @@ var createCollectionFrom = require('../internals/collection-from');
 
 // `WeakMap.from` method
 // https://tc39.github.io/proposal-setmap-offrom/#sec-weakmap.from
+// dependency: es.weak-map.constructor
 $({ target: 'WeakMap', stat: true, forced: true }, {
   from: createCollectionFrom(WeakMapHelpers.WeakMap, WeakMapHelpers.set, true),
 });
