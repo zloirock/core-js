2c993fd3f537d6828241b3dc59ae9099963bcb0a
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-21 09:14:33 +0700

    drop extra entries from base64 map

--- packages/core-js/internals/base64-map.js
+++ packages/core-js/internals/base64-map.js
@@ -1,8 +1,8 @@
 'use strict';
-var itoc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+var itoc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
 var ctoi = {};
 
-for (var index = 0; index < 66; index++) ctoi[itoc.charAt(index)] = index;
+for (var index = 0; index < 64; index++) ctoi[itoc.charAt(index)] = index;
 
 module.exports = {
   itoc: itoc,
