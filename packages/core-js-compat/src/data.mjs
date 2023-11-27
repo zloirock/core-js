cddafa9edf8d634dc6f3cb9783eac0ec1af64911
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-28 05:36:28 +0700

    drop `esnext.object.group-by` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2027,8 +2027,6 @@ export const data = {
   'esnext.number.from-string': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.object.group-by': null,
-  // TODO: Remove from `core-js@4`
   'esnext.promise.with-resolvers': null,
   'esnext.regexp.escape': {
   },
@@ -2364,7 +2362,6 @@ export const renamed = new Map([
   ['esnext.array-buffer.transfer', 'es.array-buffer.transfer'],
   ['esnext.array-buffer.transfer-to-fixed-length', 'es.array-buffer.transfer-to-fixed-length'],
   ['esnext.map.group-by', 'es.map.group-by'],
-  ['esnext.object.group-by', 'es.object.group-by'],
   ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
 ]);
 
