878d43c45623e301d163de1e1cb81c704bc3a364
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-28 05:45:10 +0700

    drop `esnext.map.group-by` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2452,8 +2452,6 @@ export const data = {
     firefox: '144',
     safari: '26.2',
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.map.group-by': null,
   'esnext.map.includes': {
   },
   'esnext.map.key-by': {
@@ -2819,7 +2817,6 @@ export const renamed = new Map([
   ['esnext.iterator.some', 'es.iterator.some'],
   ['esnext.iterator.take', 'es.iterator.take'],
   ['esnext.iterator.to-array', 'es.iterator.to-array'],
-  ['esnext.map.group-by', 'es.map.group-by'],
   ['esnext.math.f16round', 'es.math.f16round'],
   ['esnext.math.sum-precise', 'es.math.sum-precise'],
   ['esnext.promise.try', 'es.promise.try'],
