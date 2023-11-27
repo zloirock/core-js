874c48c8afbe320d8353d8b36e7be7307cc652e9
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-28 05:36:28 +0700

    drop `esnext.object.group-by` (moved to stable es)

--- packages/core-js/proposals/array-grouping.js
+++ packages/core-js/proposals/array-grouping.js
@@ -1,4 +1,4 @@
 'use strict';
 // https://github.com/tc39/proposal-array-grouping
 require('../modules/esnext.map.group-by');
-require('../modules/esnext.object.group-by');
+require('../modules/es.object.group-by');
