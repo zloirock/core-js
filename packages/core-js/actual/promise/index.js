3a30c391b66c01e1467922979d841cc71350a792
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-28 07:41:54 +0700

    drop `esnext.promise.with-resolvers` (moved to stable es)

--- packages/core-js/actual/promise/index.js
+++ packages/core-js/actual/promise/index.js
@@ -2,6 +2,5 @@
 var parent = require('../../stable/promise');
 // TODO: Remove from `core-js@4`
 require('../../modules/esnext.promise.try');
-require('../../modules/esnext.promise.with-resolvers');
 
 module.exports = parent;
