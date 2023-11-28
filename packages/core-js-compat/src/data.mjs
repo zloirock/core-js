014446f22a0ed8d818e295eedab744c8f0f59d88
Denis Pushkarev <zloirock@zloirock.ru>
2023-11-28 07:41:54 +0700

    drop `esnext.promise.with-resolvers` (moved to stable es)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2566,8 +2566,6 @@ export const data = {
   },
   // TODO: Remove from `core-js@4`
   'esnext.promise.try': null,
-  // TODO: Remove from `core-js@4`
-  'esnext.promise.with-resolvers': null,
   'esnext.set.add-all': {
   },
   'esnext.set.delete-all': {
@@ -2885,7 +2883,6 @@ export const renamed = new Map([
   ['esnext.math.f16round', 'es.math.f16round'],
   ['esnext.math.sum-precise', 'es.math.sum-precise'],
   ['esnext.promise.try', 'es.promise.try'],
-  ['esnext.promise.with-resolvers', 'es.promise.with-resolvers'],
   ['esnext.regexp.escape', 'es.regexp.escape'],
   ['esnext.suppressed-error.constructor', 'es.suppressed-error.constructor'],
   ['esnext.symbol.async-dispose', 'es.symbol.async-dispose'],
