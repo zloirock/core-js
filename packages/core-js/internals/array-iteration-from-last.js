e4e9fbb04df0b4014b2a0e8cef64c7b16e556be1
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-01 20:57:21 +0700

    drop workarounds for non-enumerable string chars

--- packages/core-js/internals/array-iteration-from-last.js
+++ packages/core-js/internals/array-iteration-from-last.js
@@ -1,6 +1,5 @@
 'use strict';
 var bind = require('../internals/function-bind-context');
-var IndexedObject = require('../internals/indexed-object');
 var toObject = require('../internals/to-object');
 var lengthOfArrayLike = require('../internals/length-of-array-like');
 
@@ -9,12 +8,11 @@ var createMethod = function (TYPE) {
   var IS_FIND_LAST_INDEX = TYPE === 1;
   return function ($this, callbackfn, that) {
     var O = toObject($this);
-    var self = IndexedObject(O);
-    var index = lengthOfArrayLike(self);
+    var index = lengthOfArrayLike(O);
     var boundFunction = bind(callbackfn, that);
     var value, result;
     while (index-- > 0) {
-      value = self[index];
+      value = O[index];
       result = boundFunction(value, index, O);
       if (result) switch (TYPE) {
         case 0: return value; // findLast
