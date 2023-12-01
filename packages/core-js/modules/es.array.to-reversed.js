9790fe7826f2c578b80f90d1f0cb1cfa0a7f1bba
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-01 20:57:21 +0700

    drop workarounds for non-enumerable string chars

--- packages/core-js/modules/es.array.to-reversed.js
+++ packages/core-js/modules/es.array.to-reversed.js
@@ -1,7 +1,7 @@
 'use strict';
 var $ = require('../internals/export');
 var arrayToReversed = require('../internals/array-to-reversed');
-var toIndexedObject = require('../internals/to-indexed-object');
+var toObject = require('../internals/to-object');
 var addToUnscopables = require('../internals/add-to-unscopables');
 
 var $Array = Array;
@@ -10,7 +10,7 @@ var $Array = Array;
 // https://tc39.es/ecma262/#sec-array.prototype.toreversed
 $({ target: 'Array', proto: true }, {
   toReversed: function toReversed() {
-    return arrayToReversed(toIndexedObject(this), $Array);
+    return arrayToReversed(toObject(this), $Array);
   },
 });
 
