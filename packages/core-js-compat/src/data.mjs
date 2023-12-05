2d0968dd02fcbb13064a9892dedf08d44b94afb4
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:24:16 +0700

    drop `Math.signbit` (proposal is dead)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2588,8 +2588,6 @@ export const data = {
   },
   'esnext.math.scale': {
   },
-  'esnext.math.signbit': {
-  },
   // TODO: Remove from `core-js@4`
   'esnext.math.sum-precise': null,
   'esnext.number.clamp': {
