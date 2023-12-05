dbb71c4bad3264d7d317d8c464a26c1f4733f395
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 19:54:26 +0700

    allow brackets notation on strings instead of `.charAt`

--- packages/core-js/modules/es.uint8-array.to-base64.js
+++ packages/core-js/modules/es.uint8-array.to-base64.js
@@ -1,7 +1,6 @@
 'use strict';
 var $ = require('../internals/export');
 var globalThis = require('../internals/global-this');
-var uncurryThis = require('../internals/function-uncurry-this');
 var anObjectOrUndefined = require('../internals/an-object-or-undefined');
 var anUint8Array = require('../internals/an-uint8-array');
 var notDetached = require('../internals/array-buffer-not-detached');
@@ -10,8 +9,6 @@ var getAlphabetOption = require('../internals/get-alphabet-option');
 
 var base64Alphabet = base64Map.i2c;
 var base64UrlAlphabet = base64Map.i2cUrl;
-
-var charAt = uncurryThis(''.charAt);
 
 var Uint8Array = globalThis.Uint8Array;
 
@@ -40,7 +37,7 @@ if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIO
     var triplet;
 
     var at = function (shift) {
-      return charAt(alphabet, (triplet >> (6 * shift)) & 63);
+      return alphabet[(triplet >> (6 * shift)) & 63];
     };
 
     for (; i + 2 < length; i += 3) {
