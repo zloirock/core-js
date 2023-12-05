4c6d44d5c4c029f2eb75eb58407ab6d52ceac4e4
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:54:05 +0700

    drop `Math.{ clamp, DEG_PER_RAD, degrees, fscale, RAD_PER_DEG, radians, scale }` (proposal is dead)

--- packages/core-js/full/math/index.js
+++ packages/core-js/full/math/index.js
@@ -1,10 +1,4 @@
 'use strict';
 var parent = require('../../actual/math');
-require('../../modules/esnext.math.deg-per-rad');
-require('../../modules/esnext.math.degrees');
-require('../../modules/esnext.math.fscale');
-require('../../modules/esnext.math.rad-per-deg');
-require('../../modules/esnext.math.radians');
-require('../../modules/esnext.math.scale');
 
 module.exports = parent;
