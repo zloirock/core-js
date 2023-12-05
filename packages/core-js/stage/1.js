a9337d8204b51a79c66033f0ab3dee1eed3c5d0b
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:33:04 +0700

    drop `Number.fromString` (proposal is dead, semantics is not clear)

--- packages/core-js/stage/1.js
+++ packages/core-js/stage/1.js
@@ -9,7 +9,6 @@ require('../proposals/data-view-get-set-uint8-clamped');
 require('../proposals/keys-composition');
 require('../proposals/math-extensions');
 require('../proposals/math-signbit');
-require('../proposals/number-from-string');
 require('../proposals/pattern-matching');
 require('../proposals/string-code-points');
 require('../proposals/string-cooked');
