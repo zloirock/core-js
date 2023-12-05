4a482f8f93e4598e6e8e9d816c86045c17aa76c1
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:33:04 +0700

    drop `Number.fromString` (proposal is dead, semantics is not clear)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2598,8 +2598,6 @@ export const data = {
   'esnext.math.sum-precise': null,
   'esnext.number.clamp': {
   },
-  'esnext.number.from-string': {
-  },
   'esnext.promise.all-keyed': {
     firefox: '155',
   },
