515960de235fcdbbc7ab8b4272186de032dc3eb1
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:49:31 +0700

    drop `Function.{ isCallable, isConstructor}` (proposal is dead)

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1583,12 +1583,6 @@ GLOBAL.tests = {
   'esnext.function.demethodize': function () {
     return Function.prototype.demethodize;
   },
-  'esnext.function.is-callable': function () {
-    return Function.isCallable;
-  },
-  'esnext.function.is-constructor': function () {
-    return Function.isConstructor;
-  },
   'esnext.function.metadata': function () {
     return Function.prototype[Symbol.metadata] === null;
   },
