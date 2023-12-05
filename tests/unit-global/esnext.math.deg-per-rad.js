95e2276ca8e26a236cea10f32222011e4f95b696
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:54:05 +0700

    drop `Math.{ clamp, DEG_PER_RAD, degrees, fscale, RAD_PER_DEG, radians, scale }` (proposal is dead)

--- tests/unit-global/esnext.math.deg-per-rad.js
+++
@@ -1,8 +0,0 @@
-QUnit.test('Math.DEG_PER_RAD', assert => {
-  const { DEG_PER_RAD, PI } = Math;
-  assert.true('DEG_PER_RAD' in Math, 'DEG_PER_RAD in Math');
-  assert.nonEnumerable(Math, 'DEG_PER_RAD');
-  assert.nonConfigurable(Number, 'DEG_PER_RAD');
-  assert.nonWritable(Number, 'DEG_PER_RAD');
-  assert.same(DEG_PER_RAD, PI / 180, 'Is Math.PI / 180');
-});
