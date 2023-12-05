41d3e4a43a7573ce6c6ef899bdacec1b650ac024
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:33:04 +0700

    drop `Number.fromString` (proposal is dead, semantics is not clear)

--- packages/core-js/full/number/from-string.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('../../modules/es.number.parse-int');
-require('../../modules/esnext.number.from-string');
-var path = require('../../internals/path');
-
-module.exports = path.Number.fromString;
