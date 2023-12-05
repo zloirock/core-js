d9225a3ba0a688c9e230f95a613834f24b89742b
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:54:05 +0700

    drop `Math.{ clamp, DEG_PER_RAD, degrees, fscale, RAD_PER_DEG, radians, scale }` (proposal is dead)

--- packages/core-js/full/math/fscale.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('../../modules/es.math.fround');
-require('../../modules/esnext.math.fscale');
-var path = require('../../internals/path');
-
-module.exports = path.Math.fscale;
