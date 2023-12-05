34671bf598a298bde7d7f18aee27d3a62255e2da
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:49:31 +0700

    drop `Function.{ isCallable, isConstructor}` (proposal is dead)

--- tests/entries/unit.mjs
+++ tests/entries/unit.mjs
@@ -704,8 +704,6 @@ for (PATH of ['@core-js/pure', 'core-js']) {
     ok(typeof load(NS, 'composite-symbol')({}, 1, {}) === 'symbol');
     ok(load(NS, 'function/demethodize')([].slice)([1, 2, 3], 1)[0] === 2);
     ok(load(NS, 'function/virtual/demethodize').call([].slice)([1, 2, 3], 1)[0] === 2);
-    ok(!load(NS, 'function/is-callable')(class { /* empty */ }));
-    ok(!load(NS, 'function/is-constructor')(it => it));
     ok(load(NS, 'iterator/concat')([2]).next().value === 2);
     ok(load(NS, 'iterator/range')(1, 2).next().value === 1);
     ok(load(NS, 'map/delete-all')(new Map(), 1, 2) === false);
@@ -814,7 +812,6 @@ for (PATH of ['@core-js/pure', 'core-js']) {
   load('proposals/extractors');
   load('proposals/float16');
   load('proposals/function-demethodize');
-  load('proposals/function-is-callable-is-constructor');
   load('proposals/global-this');
   load('proposals/iterator-helpers');
   load('proposals/iterator-range');
