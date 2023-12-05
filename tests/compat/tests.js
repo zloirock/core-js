5a0e3af23653f87d36cc42c87d0bba4d49a8af5e
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:33:04 +0700

    drop `Number.fromString` (proposal is dead, semantics is not clear)

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1743,9 +1743,6 @@ GLOBAL.tests = {
   'esnext.math.sum-precise': function () {
     return Math.sumPrecise;
   },
-  'esnext.number.from-string': function () {
-    return Number.fromString;
-  },
   'esnext.regexp.escape': function () {
     return RegExp.escape('ab') === '\\x61b';
   },
