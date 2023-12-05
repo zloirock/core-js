d6211ca15eb5f786d8c48e50197d174efa8e2946
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 22:33:04 +0700

    drop `Number.fromString` (proposal is dead, semantics is not clear)

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1960,9 +1960,6 @@ GLOBAL.tests = {
   'esnext.number.clamp': function () {
     return Number.prototype.clamp;
   },
-  'esnext.number.from-string': function () {
-    return Number.fromString;
-  },
   'esnext.set.add-all': function () {
     return Set.prototype.addAll;
   },
