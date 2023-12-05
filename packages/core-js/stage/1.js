aa7f01fab9a21779870775caab5967a275adf7dc
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-05 23:54:05 +0700

    drop `Math.{ clamp, DEG_PER_RAD, degrees, fscale, RAD_PER_DEG, radians, scale }` (proposal is dead)

--- packages/core-js/stage/1.js
+++ packages/core-js/stage/1.js
@@ -7,7 +7,6 @@ require('../proposals/collection-methods');
 require('../proposals/collection-of-from');
 require('../proposals/data-view-get-set-uint8-clamped');
 require('../proposals/keys-composition');
-require('../proposals/math-extensions');
 require('../proposals/pattern-matching');
 require('../proposals/string-code-points');
 require('../proposals/string-cooked');
