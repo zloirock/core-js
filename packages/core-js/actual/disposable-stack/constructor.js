37e9950f9905ed87e3da08027ab268f288039cec
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 22:46:23 +0700

    drop `es.error.to-string` (required for IE8-, V8 ~ Chrome 32-, FF10-)

--- packages/core-js/actual/disposable-stack/constructor.js
+++ packages/core-js/actual/disposable-stack/constructor.js
@@ -1,6 +1,5 @@
 'use strict';
 require('../../modules/es.error.cause');
-require('../../modules/es.error.to-string');
 require('../../modules/es.object.to-string');
 require('../../modules/esnext.suppressed-error.constructor');
 require('../../modules/esnext.disposable-stack.constructor');
