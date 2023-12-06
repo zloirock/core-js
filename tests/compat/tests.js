65264ad24f4a4d75ecc95ee3e1375f7022f1b41d
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 14:39:04 +0700

    drop `String.prototype.codePoints` (proposal is dead)

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1999,9 +1999,6 @@ GLOBAL.tests = {
   'esnext.set.some': function () {
     return Set.prototype.some;
   },
-  'esnext.string.code-points': function () {
-    return String.prototype.codePoints;
-  },
   'esnext.string.cooked': function () {
     return String.cooked;
   },
