cb6f65f6709d6558a3803461bc756303857c12e5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 22:46:23 +0700

    drop `es.error.to-string` (required for IE8-, V8 ~ Chrome 32-, FF10-)

--- packages/core-js/es/suppressed-error.js
+++ packages/core-js/es/suppressed-error.js
@@ -1,6 +1,5 @@
 'use strict';
 require('../modules/es.error.cause');
-require('../modules/es.error.to-string');
 require('../modules/es.suppressed-error.constructor');
 var path = require('../internals/path');
 
