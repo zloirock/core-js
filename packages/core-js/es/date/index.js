db1da78b4708bb4de107ece48cc94ad9048dc3ea
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:42:43 +0700

    drop `es.date.to-gmt-string` (supported in all popular modern engines)

--- packages/core-js/es/date/index.js
+++ packages/core-js/es/date/index.js
@@ -1,7 +1,6 @@
 'use strict';
 require('../../modules/es.date.get-year');
 require('../../modules/es.date.set-year');
-require('../../modules/es.date.to-gmt-string');
 require('../../modules/es.date.to-json');
 require('../../modules/es.date.to-primitive');
 var path = require('../../internals/path');
