db44113cd94db5f496c993a1b2fffb326de8cce0
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:56:34 +0700

    drop `es.date. set-year` (supported in all popular modern engines)

--- tests/unit-pure/es.date.set-year.js
+++
@@ -1,8 +0,0 @@
-import setYear from '@core-js/pure/es/date/set-year';
-
-QUnit.test('Date#setYear', assert => {
-  assert.isFunction(setYear);
-  const date = new Date();
-  setYear(date, 1);
-  assert.same(date.getFullYear(), 1901);
-});
