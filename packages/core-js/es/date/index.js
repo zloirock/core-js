1722acd4b150baa407ecc5464cc0ca48becd795b
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:56:34 +0700

    drop `es.date. set-year` (supported in all popular modern engines)

--- packages/core-js/es/date/index.js
+++ packages/core-js/es/date/index.js
@@ -1,6 +1,5 @@
 'use strict';
 require('../../modules/es.date.get-year');
-require('../../modules/es.date.set-year');
 require('../../modules/es.date.to-json');
 require('../../modules/es.date.to-primitive');
 var path = require('../../internals/path');
