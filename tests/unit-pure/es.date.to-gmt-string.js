84b3aa587dc865028aabb5337fae6f6de14eb8cf
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:42:43 +0700

    drop `es.date.to-gmt-string` (supported in all popular modern engines)

--- tests/unit-pure/es.date.to-gmt-string.js
+++
@@ -1,7 +0,0 @@
-import toGMTString from '@core-js/pure/es/date/to-gmt-string';
-
-QUnit.test('Date#toGMTString', assert => {
-  assert.isFunction(toGMTString);
-  const date = new Date();
-  assert.same(toGMTString(date), date.toUTCString());
-});
