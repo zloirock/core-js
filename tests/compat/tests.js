0dfac95b47bc7b2f3ec98a32b3ec829ce6dde020
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 04:44:59 +0700

    consider `ArrayBuffer` and `DataView` as existent in compat tests

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -133,10 +133,6 @@ var SAFE_ITERATION_CLOSING_SUPPORT = function () {
   }
 };
 
-var ARRAY_BUFFER_SUPPORT = function () {
-  return ArrayBuffer && DataView;
-};
-
 var TYPED_ARRAY_CONSTRUCTORS_LIST = {
   Int8Array: 1,
   Uint8Array: 1,
@@ -151,7 +147,7 @@ var TYPED_ARRAY_CONSTRUCTORS_LIST = {
 
 var ARRAY_BUFFER_VIEWS_SUPPORT = function () {
   for (var constructor in TYPED_ARRAY_CONSTRUCTORS_LIST) if (!GLOBAL[constructor]) return false;
-  return ARRAY_BUFFER_SUPPORT();
+  return true;
 };
 
 var TYPED_ARRAY_CONSTRUCTORS_NOT_REQUIRES_WRAPPERS = function () {
@@ -633,7 +629,7 @@ GLOBAL.tests = {
       return error === 4;
     }
   },
-  'es.array-buffer.constructor': [ARRAY_BUFFER_SUPPORT, function () {
+  'es.array-buffer.constructor': function () {
     try {
       return !ArrayBuffer(1);
     } catch (error) { /* empty */ }
@@ -644,13 +640,13 @@ GLOBAL.tests = {
     new ArrayBuffer(1.5);
     new ArrayBuffer(NaN);
     return ArrayBuffer.length === 1 && ArrayBuffer.name === 'ArrayBuffer';
-  }],
-  'es.array-buffer.is-view': [ARRAY_BUFFER_VIEWS_SUPPORT, function () {
+  },
+  'es.array-buffer.is-view': function () {
     return ArrayBuffer.isView;
-  }],
-  'es.array-buffer.slice': [ARRAY_BUFFER_SUPPORT, function () {
+  },
+  'es.array-buffer.slice': function () {
     return new ArrayBuffer(2).slice(1, undefined).byteLength;
-  }],
+  },
   'es.array-buffer.detached': function () {
     return 'detached' in ArrayBuffer.prototype;
   },
@@ -660,13 +656,15 @@ GLOBAL.tests = {
   'es.array-buffer.transfer-to-fixed-length': function () {
     return ArrayBuffer.prototype.transferToFixedLength;
   },
-  'es.data-view.constructor': ARRAY_BUFFER_SUPPORT,
-  'es.data-view.get-float16': [ARRAY_BUFFER_SUPPORT, function () {
+  'es.data-view.constructor': function () {
+    return DataView;
+  },
+  'es.data-view.get-float16': function () {
     return DataView.prototype.getFloat16;
-  }],
-  'es.data-view.set-float16': [ARRAY_BUFFER_SUPPORT, function () {
+  },
+  'es.data-view.set-float16': function () {
     return DataView.prototype.setFloat16;
-  }],
+  },
   'es.date.to-json': function () {
     return new Date(NaN).toJSON() === null
       && Date.prototype.toJSON.call({ toISOString: function () { return 1; } }) === 1;
@@ -1749,12 +1747,12 @@ GLOBAL.tests = {
   'esnext.composite-symbol': function () {
     return compositeSymbol;
   },
-  'esnext.data-view.get-uint8-clamped': [ARRAY_BUFFER_SUPPORT, function () {
+  'esnext.data-view.get-uint8-clamped': function () {
     return DataView.prototype.getUint8Clamped;
-  }],
-  'esnext.data-view.set-uint8-clamped': [ARRAY_BUFFER_SUPPORT, function () {
+  },
+  'esnext.data-view.set-uint8-clamped': function () {
     return DataView.prototype.setUint8Clamped;
-  }],
+  },
   'esnext.function.demethodize': function () {
     return Function.prototype.demethodize;
   },
