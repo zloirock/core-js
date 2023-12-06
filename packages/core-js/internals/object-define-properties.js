923f3a89d08c2323217080774b142ecee44bd51d
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 23:06:59 +0700

    remove a workaround of V8 ~ Chrome 36- `Object.{ defineProperty, defineProperties }` `prototype` define bug

--- packages/core-js/internals/object-define-properties.js
+++ packages/core-js/internals/object-define-properties.js
@@ -1,20 +1,2 @@
 'use strict';
-var V8_PROTOTYPE_DEFINE_BUG = require('../internals/v8-prototype-define-bug');
-var definePropertyModule = require('../internals/object-define-property');
-var anObject = require('../internals/an-object');
-var toObject = require('../internals/to-object');
-
-var objectKeys = Object.keys;
-
-// `Object.defineProperties` method
-// https://tc39.es/ecma262/#sec-object.defineproperties
-exports.f = V8_PROTOTYPE_DEFINE_BUG ? function defineProperties(O, Properties) {
-  anObject(O);
-  var props = toObject(Properties);
-  var keys = objectKeys(Properties);
-  var length = keys.length;
-  var index = 0;
-  var key;
-  while (length > index) definePropertyModule.f(O, key = keys[index++], props[key]);
-  return O;
-} : Object.defineProperties;
+exports.f = Object.defineProperties;
