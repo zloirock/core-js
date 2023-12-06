3141bdd9883613565fac041bb4646bd51b3bb803
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 23:40:27 +0700

    drop `es.object.define-properties` (required for ES3 engines and V8 ~ Chrme 36-)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1034,15 +1034,6 @@ export const data = {
     rhino: '1.7.13',
     safari: '7.1',
   },
-  'es.object.define-properties': {
-    chrome: '37',
-    firefox: '4',
-    hermes: '0.1',
-    ie: '9',
-    opera: '12',
-    rhino: '1.7.13',
-    safari: '5.1',
-  },
   'es.object.define-setter': {
     chrome: '62',
     edge: '16',
--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -959,9 +959,6 @@ GLOBAL.tests = {
     return Object.assign({}, A)[symbol] === 7 && Object.keys(Object.assign({}, B)).join('') === alphabet;
   },
   'es.object.define-getter': OBJECT_PROTOTYPE_ACCESSORS_SUPPORT,
-  'es.object.define-properties': function () {
-    return Object.defineProperties;
-  },
   'es.object.define-setter': OBJECT_PROTOTYPE_ACCESSORS_SUPPORT,
   'es.object.entries': function () {
     return Object.entries;
