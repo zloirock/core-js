c7306e943f457dc6b89a0d55333bf7965ae100cc
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:03:31 +0700

    drop `es.escape` (supported in all popular modern engines)

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -517,15 +517,6 @@ export const data = {
     rhino: '1.8.0',
     safari: '10.0',
   },
-  'es.escape': {
-    chrome: '1',
-    firefox: '1',
-    hermes: '0.1',
-    ie: '3',
-    opera: '3',
-    rhino: '1.7.13',
-    safari: '1',
-  },
   'es.function.has-instance': {
     chrome: '51',
     edge: '15',
