32eefdceac69f0f0418da87e2b2a4135add9b324
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 23:32:29 +0700

    drop `es.object.define-property` (required for ES3 engines and V8 ~ Chrome 36-)

--- packages/core-js/modules/es.object.define-property.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var $ = require('../internals/export');
-var defineProperty = require('../internals/object-define-property').f;
-
-// `Object.defineProperty` method
-// https://tc39.es/ecma262/#sec-object.defineproperty
-$({ target: 'Object', stat: true, forced: Object.defineProperty !== defineProperty }, {
-  defineProperty: defineProperty,
-});
