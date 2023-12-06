2c37adb6022271655d98fe77b06b62d13a392a7a
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 04:36:04 +0700

    consider strict mode as supported in tests

--- tests/unit-pure/es.object.lookup-getter.js
+++ tests/unit-pure/es.object.lookup-getter.js
@@ -1,6 +1,4 @@
 /* eslint-disable id-match -- unification with global tests */
-import { STRICT } from '../helpers/constants.js';
-
 import create from '@core-js/pure/es/object/create';
 import __defineGetter__ from '@core-js/pure/es/object/define-getter';
 import __lookupGetter__ from '@core-js/pure/es/object/lookup-getter';
@@ -15,8 +13,7 @@ QUnit.test('Object#__lookupGetter__', assert => {
   assert.same(__lookupGetter__(object, 'key'), getter, 'own getter');
   assert.same(__lookupGetter__(create(object), 'key'), getter, 'proto getter');
   assert.same(__lookupGetter__(create(object), 'foo'), undefined, 'empty proto');
-  if (STRICT) {
-    assert.throws(() => __lookupGetter__(null, 1, () => { /* empty */ }), TypeError, 'Throws on null as `this`');
-    assert.throws(() => __lookupGetter__(undefined, 1, () => { /* empty */ }), TypeError, 'Throws on undefined as `this`');
-  }
+
+  assert.throws(() => __lookupGetter__(null, 1, () => { /* empty */ }), TypeError, 'Throws on null as `this`');
+  assert.throws(() => __lookupGetter__(undefined, 1, () => { /* empty */ }), TypeError, 'Throws on undefined as `this`');
 });
