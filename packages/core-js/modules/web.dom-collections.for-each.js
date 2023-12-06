b25f2d1ad023777e7ae6cceea39a7e4f6b619ce5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 01:34:04 +0700

    drop `es.array.for-each` (required for ES3 only)

--- packages/core-js/modules/web.dom-collections.for-each.js
+++ packages/core-js/modules/web.dom-collections.for-each.js
@@ -1,12 +1,10 @@
 'use strict';
 var globalThis = require('../internals/global-this');
-var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
 var DOMIterables = require('../internals/dom-iterables');
 var DOMTokenListPrototype = require('../internals/dom-token-list-prototype');
 var createNonEnumerableProperty = require('../internals/create-non-enumerable-property');
 
-// dependency: es.array.for-each
-var forEach = getBuiltInPrototypeMethod('Array', 'forEach');
+var forEach = [].forEach;
 
 var handlePrototype = function (CollectionPrototype) {
   // some Chrome versions have non-configurable methods on DOMTokenList
