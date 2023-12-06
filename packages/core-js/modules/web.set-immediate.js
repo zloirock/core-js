59e26b897427142001e78beec63c986b39c31ec7
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 19:39:01 +0700

    drop `set(Timeout|Interval)` fixes (required for IE9-)

--- packages/core-js/modules/web.set-immediate.js
+++ packages/core-js/modules/web.set-immediate.js
@@ -1,14 +1,33 @@
 'use strict';
 var $ = require('../internals/export');
 var globalThis = require('../internals/global-this');
+var apply = require('../internals/function-apply');
+var isCallable = require('../internals/is-callable');
+var ENVIRONMENT = require('../internals/environment');
+var arraySlice = require('../internals/array-slice');
+var validateArgumentsLength = require('../internals/validate-arguments-length');
 var setTask = require('../internals/task').set;
-var schedulersFix = require('../internals/schedulers-fix');
 
+var Function = globalThis.Function;
+var $setImmediate = globalThis.setImmediate;
+
+// Bun 0.3.0- checks
 // https://github.com/oven-sh/bun/issues/1633
-var setImmediate = globalThis.setImmediate ? schedulersFix(setTask, false) : setTask;
+var WRAP = $setImmediate && ENVIRONMENT === 'BUN' && (function () {
+  var version = globalThis.Bun.version.split('.');
+  return version.length < 3 || version[0] === '0' && (version[1] < 3 || version[1] === '3' && version[2] === '0');
+})();
 
 // `setImmediate` method
 // http://w3c.github.io/setImmediate/#si-setImmediate
-$({ global: true, bind: true, enumerable: true, forced: globalThis.setImmediate !== setImmediate }, {
-  setImmediate: setImmediate,
+$({ global: true, bind: true, enumerable: true, forced: $setImmediate !== setTask }, {
+  setImmediate: WRAP ? function setImmediate(handler /* , ...arguments */) {
+    var boundArgs = validateArgumentsLength(arguments.length, 1) > 1;
+    var fn = isCallable(handler) ? handler : Function(handler);
+    var params = boundArgs ? arraySlice(arguments, 1) : [];
+    var callback = boundArgs ? function () {
+      apply(fn, this, params);
+    } : fn;
+    return setTask(callback);
+  } : setTask,
 });
