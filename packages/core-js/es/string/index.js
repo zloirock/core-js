ccf4a6cbc82df8e41de1222a669b9b9ac9179896
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 00:29:11 +0700

    drop `es.string.substr` (supported in all popular modern engines)

--- packages/core-js/es/string/index.js
+++ packages/core-js/es/string/index.js
@@ -18,7 +18,6 @@ require('../../modules/es.string.replace-all');
 require('../../modules/es.string.search');
 require('../../modules/es.string.split');
 require('../../modules/es.string.starts-with');
-require('../../modules/es.string.substr');
 require('../../modules/es.string.to-well-formed');
 require('../../modules/es.string.trim');
 require('../../modules/es.string.trim-end');
