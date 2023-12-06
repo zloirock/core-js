179715442ba7dd954f6a7a62f0dc7b0f30083bdd
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-07 04:36:04 +0700

    consider strict mode as supported in tests

--- tests/unit-global/es.array.index-of.js
+++ tests/unit-global/es.array.index-of.js
@@ -1,5 +1,3 @@
-import { STRICT } from '../helpers/constants.js';
-
 QUnit.test('Array#indexOf', assert => {
   const { indexOf } = Array.prototype;
   assert.isFunction(indexOf);
@@ -16,8 +14,7 @@ QUnit.test('Array#indexOf', assert => {
   assert.same(3, Array(2).concat([1, 2, 3]).indexOf(2));
   assert.same(-1, Array(1).indexOf(undefined));
   assert.same(0, [1].indexOf(1, -0), "shouldn't return negative zero");
-  if (STRICT) {
-    assert.throws(() => indexOf.call(null, 0), TypeError);
-    assert.throws(() => indexOf.call(undefined, 0), TypeError);
-  }
+
+  assert.throws(() => indexOf.call(null, 0), TypeError);
+  assert.throws(() => indexOf.call(undefined, 0), TypeError);
 });
