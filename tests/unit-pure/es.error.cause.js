72aa4d7e0b4f85bdc63a30c5e4cbd53115f9b094
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-06 20:34:35 +0700

    consider a way setting of a prototype - `Object.setPrototypeOf` or `__proto__` - as existent

--- tests/unit-pure/es.error.cause.js
+++ tests/unit-pure/es.error.cause.js
@@ -1,6 +1,4 @@
 /* eslint-disable sonarjs/inconsistent-function-call -- required for testing */
-import { PROTO } from '../helpers/constants.js';
-
 import path from '@core-js/pure/es/error';
 import create from '@core-js/pure/es/object/create';
 
@@ -10,7 +8,7 @@ function runErrorTestCase($Error, ERROR_NAME, WEB_ASSEMBLY) {
     assert.arity($Error, 1);
     assert.name($Error, ERROR_NAME);
 
-    if (PROTO && $Error !== path.Error) {
+    if ($Error !== path.Error) {
       // eslint-disable-next-line no-prototype-builtins -- safe
       assert.true(path.Error.isPrototypeOf($Error), 'constructor has `Error` in the prototype chain');
     }
