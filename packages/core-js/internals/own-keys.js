aa7a6055a57f31d47b44d54905e311b68a7e0085
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-08 16:22:08 +0700

    direct access to `Reflect.ownKeys` in `own-keys` helper

--- packages/core-js/internals/own-keys.js
+++ packages/core-js/internals/own-keys.js
@@ -1,5 +1,4 @@
 'use strict';
-var getBuiltInStaticMethod = require('../internals/get-built-in-static-method');
 var uncurryThis = require('../internals/function-uncurry-this');
 var getOwnPropertyNamesModule = require('../internals/object-get-own-property-names');
 var getOwnPropertySymbolsModule = require('../internals/object-get-own-property-symbols');
@@ -8,7 +7,8 @@ var anObject = require('../internals/an-object');
 var concat = uncurryThis([].concat);
 
 // all object keys, includes non-enumerable and symbols
-module.exports = getBuiltInStaticMethod('Reflect', 'ownKeys') || function ownKeys(it) {
+// eslint-disable-next-line es/no-reflect -- safe
+module.exports = typeof Reflect == 'object' && Reflect.ownKeys || function ownKeys(it) {
   var keys = getOwnPropertyNamesModule.f(anObject(it));
   var getOwnPropertySymbols = getOwnPropertySymbolsModule.f;
   return getOwnPropertySymbols ? concat(keys, getOwnPropertySymbols(it)) : keys;
