310da8441ba57f80a929234304b79cce0a14904f
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/es/typed-array/uint32-array.js
+++ packages/core-js/es/typed-array/uint32-array.js
@@ -3,6 +3,6 @@ require('../../modules/es.array-buffer.constructor');
 require('../../modules/es.array-buffer.slice');
 require('../../modules/es.typed-array.uint32-array');
 require('./methods');
-var global = require('../../internals/global-this');
+var globalThis = require('../../internals/global-this');
 
-module.exports = global.Uint32Array;
+module.exports = globalThis.Uint32Array;
