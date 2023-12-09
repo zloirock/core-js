2400716095ff86a52db9215562c22157615587d5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/es.uint8-array.to-base64.js
+++ packages/core-js/modules/es.uint8-array.to-base64.js
@@ -1,6 +1,6 @@
 'use strict';
+/* eslint-disable es/no-typed-arrays, es/no-uint8array-prototype-tobase64 -- safe */
 var $ = require('../internals/export');
-var globalThis = require('../internals/global-this');
 var anObjectOrUndefined = require('../internals/an-object-or-undefined');
 var anUint8Array = require('../internals/an-uint8-array');
 var notDetached = require('../internals/array-buffer-not-detached');
@@ -10,9 +10,7 @@ var getAlphabetOption = require('../internals/get-alphabet-option');
 var base64Alphabet = base64Map.i2c;
 var base64UrlAlphabet = base64Map.i2cUrl;
 
-var Uint8Array = globalThis.Uint8Array;
-
-var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.toBase64 || !function () {
+var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array.prototype.toBase64 || !function () {
   try {
     var target = new Uint8Array();
     target.toBase64(null);
@@ -23,7 +21,7 @@ var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.t
 
 // `Uint8Array.prototype.toBase64` method
 // https://tc39.es/ecma262/#sec-uint8array.prototype.tobase64
-if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
+$({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
   toBase64: function toBase64(/* options */) {
     var array = anUint8Array(this);
     var options = arguments.length ? anObjectOrUndefined(arguments[0]) : undefined;
