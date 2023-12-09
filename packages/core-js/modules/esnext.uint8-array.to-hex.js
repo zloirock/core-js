2b6c7bc9fcca7434bf67ff8fdf83e4feeb166be3
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/esnext.uint8-array.to-hex.js
+++ packages/core-js/modules/esnext.uint8-array.to-hex.js
@@ -1,6 +1,5 @@
 'use strict';
 var $ = require('../internals/export');
-var globalThis = require('../internals/global-this');
 var uncurryThis = require('../internals/function-uncurry-this');
 var anUint8Array = require('../internals/an-uint8-array');
 var notDetached = require('../internals/array-buffer-not-detached');
@@ -9,7 +8,7 @@ var numberToString = uncurryThis(1.0.toString);
 
 // `Uint8Array.prototype.toHex` method
 // https://github.com/tc39/proposal-arraybuffer-base64
-if (globalThis.Uint8Array) $({ target: 'Uint8Array', proto: true }, {
+$({ target: 'Uint8Array', proto: true }, {
   toHex: function toHex() {
     anUint8Array(this);
     notDetached(this.buffer);
