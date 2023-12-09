7df1091b072b1dcbfb4e4039bfc5e7e57ae29e71
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 18:51:26 +0700

    drop extra dependency declaration from `array-buffer-byte-length`

--- packages/core-js/internals/array-buffer-byte-length.js
+++ packages/core-js/internals/array-buffer-byte-length.js
@@ -7,7 +7,6 @@ var $TypeError = TypeError;
 // Includes
 // - Perform ? RequireInternalSlot(O, [[ArrayBufferData]]).
 // - If IsSharedArrayBuffer(O) is true, throw a TypeError exception.
-// dependency: es.array-buffer.constructor
 module.exports = uncurryThisAccessor(ArrayBuffer.prototype, 'byteLength', 'get') || function (O) {
   if (classof(O) !== 'ArrayBuffer') throw new $TypeError('ArrayBuffer expected');
   return O.byteLength;
