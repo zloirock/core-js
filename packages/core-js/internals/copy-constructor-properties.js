d6b6f926694ca15e60c2a82501a1137af1697270
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 18:30:44 +0700

    drop third argument from `copy-constructor-properties` as not used

--- packages/core-js/internals/copy-constructor-properties.js
+++ packages/core-js/internals/copy-constructor-properties.js
@@ -5,11 +5,11 @@ var ownKeys = require('../internals/own-keys');
 var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 var defineProperty = Object.defineProperty;
 
-module.exports = function (target, source, exceptions) {
+module.exports = function (target, source) {
   var keys = ownKeys(source);
   for (var i = 0; i < keys.length; i++) {
     var key = keys[i];
-    if (!hasOwn(target, key) && !(exceptions && hasOwn(exceptions, key))) {
+    if (!hasOwn(target, key)) {
       defineProperty(target, key, getOwnPropertyDescriptor(source, key));
     }
   }
