f03c1ae872e8b34f5ced2efbdfb6bd0cdf8c1560
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/esnext.uint8-array.from-base64.js
+++ packages/core-js/modules/esnext.uint8-array.from-base64.js
@@ -8,7 +8,7 @@ var Uint8Array = globalThis.Uint8Array;
 
 // `Uint8Array.fromBase64` method
 // https://github.com/tc39/proposal-arraybuffer-base64
-if (Uint8Array) $({ target: 'Uint8Array', stat: true }, {
+$({ target: 'Uint8Array', stat: true }, {
   fromBase64: function fromBase64(string /* , options */) {
     var result = $fromBase64(string, arguments.length > 1 ? arguments[1] : undefined, null, 0x1FFFFFFFFFFFFF);
     return arrayFromConstructorAndList(Uint8Array, result.bytes);
