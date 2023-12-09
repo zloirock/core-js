ce3e391631f37dffb22196d7b7b6b07b03ed0116
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/es.uint8-array.set-from-base64.js
+++ packages/core-js/modules/es.uint8-array.set-from-base64.js
@@ -1,12 +1,10 @@
 'use strict';
+/* eslint-disable es/no-typed-arrays, es/no-uint8array-prototype-setfrombase64 -- safe */
 var $ = require('../internals/export');
-var globalThis = require('../internals/global-this');
 var $fromBase64 = require('../internals/uint8-from-base64');
 var anUint8Array = require('../internals/an-uint8-array');
 
-var Uint8Array = globalThis.Uint8Array;
-
-var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.setFromBase64 || !function () {
+var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array.prototype.setFromBase64 || !function () {
   var target = new Uint8Array([255, 255, 255, 255, 255]);
   try {
     target.setFromBase64('', null);
@@ -26,7 +24,7 @@ var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.s
 
 // `Uint8Array.prototype.setFromBase64` method
 // https://tc39.es/ecma262/#sec-uint8array.prototype.setfrombase64
-if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
+$({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
   setFromBase64: function setFromBase64(string /* , options */) {
     anUint8Array(this);
 
