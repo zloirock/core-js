7ee56c947b7e0fc02a23d18a0bfd64db50f6bc20
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/es.uint8-array.to-hex.js
+++ packages/core-js/modules/es.uint8-array.to-hex.js
@@ -1,15 +1,13 @@
 'use strict';
+/* eslint-disable es/no-typed-arrays, es/no-uint8array-prototype-tohex -- safe */
 var $ = require('../internals/export');
-var globalThis = require('../internals/global-this');
 var uncurryThis = require('../internals/function-uncurry-this');
 var anUint8Array = require('../internals/an-uint8-array');
 var notDetached = require('../internals/array-buffer-not-detached');
 
 var numberToString = uncurryThis(1.1.toString);
 
-var Uint8Array = globalThis.Uint8Array;
-
-var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.toHex || !(function () {
+var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array.prototype.toHex || !(function () {
   try {
     var target = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255]);
     return target.toHex() === 'ffffffffffffffff';
@@ -20,7 +18,7 @@ var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.t
 
 // `Uint8Array.prototype.toHex` method
 // https://tc39.es/ecma262/#sec-uint8array.prototype.tohex
-if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
+$({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
   toHex: function toHex() {
     anUint8Array(this);
     notDetached(this.buffer);
