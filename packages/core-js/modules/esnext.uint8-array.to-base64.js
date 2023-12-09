278bb3acae4e3904a818fe5d764d054bdd7ec1f5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-09 22:12:22 +0700

    consider `Uint8Array` as existent in some more cases

--- packages/core-js/modules/esnext.uint8-array.to-base64.js
+++ packages/core-js/modules/esnext.uint8-array.to-base64.js
@@ -1,6 +1,5 @@
 'use strict';
 var $ = require('../internals/export');
-var global = require('../internals/global');
 var anObjectOrUndefined = require('../internals/an-object-or-undefined');
 var anUint8Array = require('../internals/an-uint8-array');
 var base64Map = require('../internals/base64-map');
@@ -9,11 +8,9 @@ var getAlphabetOption = require('../internals/get-alphabet-option');
 var base64Alphabet = base64Map.i2c;
 var base64UrlAlphabet = base64Map.i2cUrl;
 
-var Uint8Array = global.Uint8Array;
-
 // `Uint8Array.prototype.toBase64` method
 // https://github.com/tc39/proposal-arraybuffer-base64
-if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: true }, {
+$({ target: 'Uint8Array', proto: true, forced: true }, {
   toBase64: function toBase64(/* options */) {
     var array = anUint8Array(this);
     var options = arguments.length ? anObjectOrUndefined(arguments[0]) : undefined;
