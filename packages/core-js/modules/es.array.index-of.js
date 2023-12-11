bfa50457248b17b77cd57ac67708517c9d2363f3
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 19:21:58 +0700

    consider `Array#lastIndexOf` as existent

--- packages/core-js/modules/es.array.index-of.js
+++ packages/core-js/modules/es.array.index-of.js
@@ -7,12 +7,12 @@ var arrayMethodIsStrict = require('../internals/array-method-is-strict');
 // eslint-disable-next-line es/no-array-prototype-indexof -- safe
 var $indexOf = uncurryThis([].indexOf);
 
-var NEGATIVE_ZERO = 1 / $indexOf([1], 1, -0) < 0;
-var FORCED = NEGATIVE_ZERO || !arrayMethodIsStrict('indexOf');
+var NEGATIVE_ZERO_BUG = 1 / $indexOf([1], 1, -0) < 0;
+var STRICT = arrayMethodIsStrict('indexOf');
 
 // `Array.prototype.indexOf` method
 // https://tc39.es/ecma262/#sec-array.prototype.indexof
-$({ target: 'Array', proto: true, forced: FORCED }, {
+$({ target: 'Array', proto: true, forced: NEGATIVE_ZERO_BUG || !STRICT }, {
   indexOf: function indexOf(searchElement /* , fromIndex = 0 */) {
     return $indexOf(toObject(this), searchElement, arguments.length > 1 ? arguments[1] : undefined) || 0;
   },
