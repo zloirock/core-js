d05b3757745c2a4defc2748165949a7ced52d636
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 19:21:58 +0700

    consider `Array#lastIndexOf` as existent

--- packages/core-js/modules/es.array.last-index-of.js
+++ packages/core-js/modules/es.array.last-index-of.js
@@ -1,10 +1,19 @@
 'use strict';
 var $ = require('../internals/export');
-var lastIndexOf = require('../internals/array-last-index-of');
+var uncurryThis = require('../internals/function-uncurry-this');
+var toObject = require('../internals/to-object');
+var arrayMethodIsStrict = require('../internals/array-method-is-strict');
+
+// eslint-disable-next-line es/no-array-prototype-lastindexof -- safe
+var $lastIndexOf = uncurryThis([].lastIndexOf);
+
+var NEGATIVE_ZERO_BUG = 1 / $lastIndexOf([1], 1, -0) < 0;
+var STRICT = arrayMethodIsStrict('lastIndexOf');
 
 // `Array.prototype.lastIndexOf` method
 // https://tc39.es/ecma262/#sec-array.prototype.lastindexof
-// eslint-disable-next-line es/no-array-prototype-lastindexof -- required for testing
-$({ target: 'Array', proto: true, forced: lastIndexOf !== [].lastIndexOf }, {
-  lastIndexOf: lastIndexOf,
+$({ target: 'Array', proto: true, forced: NEGATIVE_ZERO_BUG || !STRICT }, {
+  lastIndexOf: function lastIndexOf(searchElement /* , fromIndex = 0 */) {
+    return $lastIndexOf(toObject(this), searchElement, arguments.length > 1 ? arguments[1] : undefined) || 0;
+  },
 });
