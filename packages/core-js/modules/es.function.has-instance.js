8244443412abc30630491b2502a3a678e1f13bc2
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 21:13:16 +0700

    simplify `Function#@@hasInstance` with `isPrototypeOf`

--- packages/core-js/modules/es.function.has-instance.js
+++ packages/core-js/modules/es.function.has-instance.js
@@ -2,7 +2,7 @@
 var isCallable = require('../internals/is-callable');
 var isObject = require('../internals/is-object');
 var definePropertyModule = require('../internals/object-define-property');
-var getPrototypeOf = require('../internals/object-get-prototype-of');
+var isPrototypeOf = require('../internals/object-is-prototype-of');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 var makeBuiltIn = require('../internals/make-built-in');
 
@@ -15,9 +15,6 @@ if (!(HAS_INSTANCE in FunctionPrototype)) {
   definePropertyModule.f(FunctionPrototype, HAS_INSTANCE, { value: makeBuiltIn(function (O) {
     if (!isCallable(this) || !isObject(O)) return false;
     var P = this.prototype;
-    if (!isObject(P)) return O instanceof this;
-    // for environment w/o native `@@hasInstance` logic enough `instanceof`, but add this:
-    while (O = getPrototypeOf(O)) if (P === O) return true;
-    return false;
+    return isObject(P) ? isPrototypeOf(P, O) : O instanceof this;
   }, HAS_INSTANCE) });
 }
