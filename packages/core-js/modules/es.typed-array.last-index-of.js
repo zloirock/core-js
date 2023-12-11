29e4bb2c99d704eb5cbfcc0e40dad084919ebe18
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 19:21:58 +0700

    consider `Array#lastIndexOf` as existent

--- packages/core-js/modules/es.typed-array.last-index-of.js
+++ packages/core-js/modules/es.typed-array.last-index-of.js
@@ -1,14 +1,15 @@
 'use strict';
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
-var apply = require('../internals/function-apply');
-var $lastIndexOf = require('../internals/array-last-index-of');
+var uncurryThis = require('../internals/function-uncurry-this');
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
 
+// eslint-disable-next-line es/no-array-prototype-lastindexof -- safe
+var $lastIndexOf = uncurryThis([].lastIndexOf);
+
 // `%TypedArray%.prototype.lastIndexOf` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.lastindexof
 exportTypedArrayMethod('lastIndexOf', function lastIndexOf(searchElement /* , fromIndex */) {
-  var length = arguments.length;
-  return apply($lastIndexOf, aTypedArray(this), length > 1 ? [searchElement, arguments[1]] : [searchElement]);
+  return $lastIndexOf(aTypedArray(this), searchElement, arguments.length > 1 ? arguments[1] : undefined) || 0;
 });
