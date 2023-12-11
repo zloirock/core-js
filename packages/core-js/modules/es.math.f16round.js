5e84bf64c6041ea3e46ec3aad49fa14f608db2d3
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 20:35:15 +0700

    use `Float32Array` in `Math.fround` polyfill

--- packages/core-js/modules/es.math.f16round.js
+++ packages/core-js/modules/es.math.f16round.js
@@ -1,7 +1,11 @@
 'use strict';
 var $ = require('../internals/export');
-var floatRound = require('../internals/math-float-round');
+var sign = require('../internals/math-sign');
+var roundTiesToEven = require('../internals/math-round-ties-to-even');
 
+var abs = Math.abs;
+
+var EPSILON = 2.220446049250313e-16; // Number.EPSILON
 var FLOAT16_EPSILON = 0.0009765625;
 var FLOAT16_MAX_VALUE = 65504;
 var FLOAT16_MIN_VALUE = 6.103515625e-05;
@@ -10,6 +14,16 @@ var FLOAT16_MIN_VALUE = 6.103515625e-05;
 // https://tc39.es/ecma262/#sec-math.f16round
 $({ target: 'Math', stat: true }, {
   f16round: function f16round(x) {
-    return floatRound(x, FLOAT16_EPSILON, FLOAT16_MAX_VALUE, FLOAT16_MIN_VALUE);
+    var n = +x;
+    var absolute = abs(n);
+    var s = sign(n);
+    if (absolute < FLOAT16_MIN_VALUE) {
+      return s * roundTiesToEven(absolute / FLOAT16_MIN_VALUE / FLOAT16_EPSILON) * FLOAT16_MIN_VALUE * FLOAT16_EPSILON;
+    }
+    var a = (1 + FLOAT16_EPSILON / EPSILON) * absolute;
+    var result = a - (a - absolute);
+    // eslint-disable-next-line no-self-compare -- NaN check
+    if (result > FLOAT16_MAX_VALUE || result !== result) return s * Infinity;
+    return s * result;
   },
 });
