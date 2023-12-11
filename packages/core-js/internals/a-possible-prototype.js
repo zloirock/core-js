d689b3206460a849e404c41b6dbf6d6115bc388f
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 23:48:09 +0700

    extract `is-possible-prototype` helper

--- packages/core-js/internals/a-possible-prototype.js
+++ packages/core-js/internals/a-possible-prototype.js
@@ -1,10 +1,10 @@
 'use strict';
-var isCallable = require('../internals/is-callable');
+var isPossiblePrototype = require('../internals/is-possible-prototype');
 
 var $String = String;
 var $TypeError = TypeError;
 
 module.exports = function (argument) {
-  if (typeof argument == 'object' || isCallable(argument)) return argument;
+  if (isPossiblePrototype(argument)) return argument;
   throw new $TypeError("Can't set " + $String(argument) + ' as a prototype');
 };
--- packages/core-js/modules/es.object.proto.js
+++ packages/core-js/modules/es.object.proto.js
@@ -2,6 +2,7 @@
 var DESCRIPTORS = require('../internals/descriptors');
 var defineBuiltInAccessor = require('../internals/define-built-in-accessor');
 var isObject = require('../internals/is-object');
+var isPossiblePrototype = require('../internals/is-possible-prototype');
 var toObject = require('../internals/to-object');
 var requireObjectCoercible = require('../internals/require-object-coercible');
 
@@ -22,8 +23,9 @@ if (DESCRIPTORS && getPrototypeOf && setPrototypeOf && !(PROTO in ObjectPrototyp
     },
     set: function __proto__(proto) {
       var O = requireObjectCoercible(this);
-      if (!isObject(proto) && proto !== null || !isObject(O)) return;
-      setPrototypeOf(O, proto);
+      if (isPossiblePrototype(proto) && isObject(O)) {
+        setPrototypeOf(O, proto);
+      }
     }
   });
 } catch (error) { /* empty */ }
