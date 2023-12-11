f90965d6610fc2f8ae30146503549a6617beb263
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 18:36:48 +0700

    consider `Array#indexOf` as existent

--- packages/core-js/modules/es.typed-array.index-of.js
+++ packages/core-js/modules/es.typed-array.index-of.js
@@ -1,12 +1,15 @@
 'use strict';
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
-var $indexOf = require('../internals/array-includes').indexOf;
+var uncurryThis = require('../internals/function-uncurry-this');
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
 
+// eslint-disable-next-line es/no-array-prototype-indexof -- safe
+var $indexOf = uncurryThis([].indexOf);
+
 // `%TypedArray%.prototype.indexOf` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.indexof
 exportTypedArrayMethod('indexOf', function indexOf(searchElement /* , fromIndex */) {
-  return $indexOf(aTypedArray(this), searchElement, arguments.length > 1 ? arguments[1] : undefined);
+  return $indexOf(aTypedArray(this), searchElement, arguments.length > 1 ? arguments[1] : undefined) || 0;
 });
