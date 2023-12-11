d4f00875863b19507bf45e134d71393cbcee4a32
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-12 02:42:10 +0700

    drop a workaround for a IE bug with non-enumerable integer keys on `null` proto object

--- packages/core-js/internals/object-to-array.js
+++ packages/core-js/internals/object-to-array.js
@@ -1,35 +1,24 @@
 'use strict';
-var fails = require('../internals/fails');
 var uncurryThis = require('../internals/function-uncurry-this');
 var toObject = require('../internals/to-object');
 var $propertyIsEnumerable = require('../internals/object-property-is-enumerable').f;
 
 var objectKeys = Object.keys;
-var getPrototypeOf = Object.getPrototypeOf;
 var propertyIsEnumerable = uncurryThis($propertyIsEnumerable);
 var push = uncurryThis([].push);
-
-// in some IE versions, `propertyIsEnumerable` returns incorrect result on integer keys
-// of `null` prototype objects
-var IE_BUG = fails(function () {
-  var O = Object.create(null);
-  O[2] = 2;
-  return !propertyIsEnumerable(O, 2);
-});
 
 // `Object.{ entries, values }` methods implementation
 var createMethod = function (TO_ENTRIES) {
   return function (it) {
     var O = toObject(it);
     var keys = objectKeys(O);
-    var IE_WORKAROUND = IE_BUG && getPrototypeOf(O) === null;
     var length = keys.length;
     var i = 0;
     var result = [];
     var key;
     while (length > i) {
       key = keys[i++];
-      if (IE_WORKAROUND ? key in O : propertyIsEnumerable(O, key)) {
+      if (propertyIsEnumerable(O, key)) {
         push(result, TO_ENTRIES ? [key, O[key]] : O[key]);
       }
     }
