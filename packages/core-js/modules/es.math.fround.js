f373aa98aa22babbc76a348c83492259ea77f099
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-11 20:35:15 +0700

    use `Float32Array` in `Math.fround` polyfill

--- packages/core-js/modules/es.math.fround.js
+++ packages/core-js/modules/es.math.fround.js
@@ -1,7 +1,14 @@
 'use strict';
 var $ = require('../internals/export');
-var fround = require('../internals/math-fround');
+
+var f32array = new Float32Array(1);
 
 // `Math.fround` method
 // https://tc39.es/ecma262/#sec-math.fround
-$({ target: 'Math', stat: true }, { fround: fround });
+$({ target: 'Math', stat: true }, {
+  fround: function fround(x) {
+    // call `ToNumber` directly since old FF typed arrays do not convert objects to numbers
+    f32array[0] = +x;
+    return f32array[0];
+  },
+});
