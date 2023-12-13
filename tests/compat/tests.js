d0f4287f8c8aadebb491fa4e7b668bfcccd6228b
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-14 01:46:44 +0700

    drop some unused `eslint` directives

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1,6 +1,5 @@
 'use strict';
 /* eslint-disable prefer-regex-literals, radix -- required for testing */
-/* eslint-disable regexp/no-empty-capturing-group, regexp/no-lazy-ends, regexp/no-useless-quantifier -- required for testing */
 var GLOBAL = typeof global != 'undefined' ? global : Function('return this')();
 var WHITESPACES = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u2000\u2001\u2002' +
   '\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF';
