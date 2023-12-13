8e118bae89afadb7f7139c8080c81bb35e1ef22b
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-14 01:43:58 +0700

    implicit `parseInt` / `parseFloat` dependencies in `Number.{ parseInt, parseFloat }`

--- packages/core-js/es/number/parse-float.js
+++ packages/core-js/es/number/parse-float.js
@@ -1,4 +1,5 @@
 'use strict';
+require('../../modules/es.parse-float');
 require('../../modules/es.number.parse-float');
 var path = require('../../internals/path');
 
--- packages/core-js/modules/es.parse-float.js
+++ packages/core-js/modules/es.parse-float.js
@@ -1,9 +1,24 @@
 'use strict';
 var $ = require('../internals/export');
-var $parseFloat = require('../internals/number-parse-float');
+var globalThis = require('../internals/global-this');
+var fails = require('../internals/fails');
+var toString = require('../internals/to-string');
+var trim = require('../internals/string-trim').trim;
+var whitespaces = require('../internals/whitespaces');
+
+var $parseFloat = globalThis.parseFloat;
+var Symbol = globalThis.Symbol;
+var ITERATOR = Symbol && Symbol.iterator;
+var FORCED = 1 / $parseFloat(whitespaces + '-0') !== -Infinity
+  // MS Edge 18- broken with boxed symbols
+  || (ITERATOR && !fails(function () { $parseFloat(Object(ITERATOR)); }));
 
 // `parseFloat` method
 // https://tc39.es/ecma262/#sec-parsefloat-string
-$({ global: true, forced: parseFloat !== $parseFloat }, {
-  parseFloat: $parseFloat,
+$({ global: true, forced: FORCED }, {
+  parseFloat: function parseFloat(string) {
+    var trimmedString = trim(toString(string));
+    var result = $parseFloat(trimmedString);
+    return result === 0 && trimmedString[0] === '-' ? -0 : result;
+  },
 });
