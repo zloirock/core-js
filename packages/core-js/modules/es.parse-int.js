4bbc8babaacfd711e06caee44779f04abaff7ab4
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-14 01:43:58 +0700

    implicit `parseInt` / `parseFloat` dependencies in `Number.{ parseInt, parseFloat }`

--- packages/core-js/modules/es.parse-int.js
+++ packages/core-js/modules/es.parse-int.js
@@ -1,9 +1,26 @@
 'use strict';
 var $ = require('../internals/export');
-var $parseInt = require('../internals/number-parse-int');
+var globalThis = require('../internals/global-this');
+var fails = require('../internals/fails');
+var uncurryThis = require('../internals/function-uncurry-this');
+var toString = require('../internals/to-string');
+var trim = require('../internals/string-trim').trim;
+var whitespaces = require('../internals/whitespaces');
+
+var $parseInt = globalThis.parseInt;
+var Symbol = globalThis.Symbol;
+var ITERATOR = Symbol && Symbol.iterator;
+var hex = /^[+-]?0x/i;
+var exec = uncurryThis(hex.exec);
+var FORCED = $parseInt(whitespaces + '08') !== 8 || $parseInt(whitespaces + '0x16') !== 22
+  // MS Edge 18- broken with boxed symbols
+  || (ITERATOR && !fails(function () { $parseInt(Object(ITERATOR)); }));
 
 // `parseInt` method
 // https://tc39.es/ecma262/#sec-parseint-string-radix
-$({ global: true, forced: parseInt !== $parseInt }, {
-  parseInt: $parseInt,
+$({ global: true, forced: FORCED }, {
+  parseInt: function parseInt(string, radix) {
+    var S = trim(toString(string));
+    return $parseInt(S, (radix >>> 0) || (exec(hex, S) ? 16 : 10));
+  },
 });
