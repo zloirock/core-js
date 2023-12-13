506fbd358de46d2d8f2c02e07babe75618b67902
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-14 01:43:58 +0700

    implicit `parseInt` / `parseFloat` dependencies in `Number.{ parseInt, parseFloat }`

--- packages/core-js/modules/es.number.parse-int.js
+++ packages/core-js/modules/es.number.parse-int.js
@@ -1,6 +1,9 @@
 'use strict';
 var $ = require('../internals/export');
-var parseInt = require('../internals/number-parse-int');
+var getBuiltIn = require('../internals/get-built-in');
+
+// dependency: es.parse-int
+var parseInt = getBuiltIn('parseInt');
 
 // `Number.parseInt` method
 // https://tc39.es/ecma262/#sec-number.parseint
