9943ef375189c862be54061d9fb6ce6cb014642c
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-16 05:27:40 +0700

    finally drop `internal-metadata` and `hidden-keys` logic, use native (maybe incorrect) `WeakMap` and `Map` in weak collections polyfills

--- packages/core-js/modules/es.object.seal.js
+++ packages/core-js/modules/es.object.seal.js
@@ -1,7 +1,6 @@
 'use strict';
 var $ = require('../internals/export');
 var isObject = require('../internals/is-object');
-var onFreeze = require('../internals/internal-metadata').onFreeze;
 var fails = require('../internals/fails');
 
 var $seal = Object.seal;
@@ -11,6 +10,6 @@ var FAILS_ON_PRIMITIVES = fails(function () { $seal(1); });
 // https://tc39.es/ecma262/#sec-object.seal
 $({ target: 'Object', stat: true, forced: FAILS_ON_PRIMITIVES }, {
   seal: function seal(it) {
-    return isObject(it) ? $seal(onFreeze(it)) : it;
+    return isObject(it) ? $seal(it) : it;
   },
 });
