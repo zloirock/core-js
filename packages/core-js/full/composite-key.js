1a5189d7d1c0d8b9b822c2582c2de2546b0f0ea6
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-16 05:44:08 +0700

    use native `Map` in `composite-key`

--- packages/core-js/full/composite-key.js
+++ packages/core-js/full/composite-key.js
@@ -1,5 +1,4 @@
 'use strict';
-require('../modules/es.map.constructor');
 require('../modules/es.weak-map.constructor');
 require('../modules/esnext.composite-key');
 var path = require('../internals/path');
