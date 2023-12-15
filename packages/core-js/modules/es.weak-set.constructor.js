80c61415262eadd072cb1ce316af04f39b0d827e
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-16 05:27:40 +0700

    finally drop `internal-metadata` and `hidden-keys` logic, use native (maybe incorrect) `WeakMap` and `Map` in weak collections polyfills

--- packages/core-js/modules/es.weak-set.constructor.js
+++ packages/core-js/modules/es.weak-set.constructor.js
@@ -1,9 +1,11 @@
 'use strict';
 var collection = require('../internals/collection');
 var collectionWeak = require('../internals/collection-weak');
+var IS_IE11 = require('../internals/environment-is-ie11');
+var WEAK_COLLECTIONS_UNFREEZING_BUG = require('../internals/weak-collections-unfreezing-bug');
 
 // `WeakSet` constructor
 // https://tc39.es/ecma262/#sec-weakset-constructor
 collection('WeakSet', function (init) {
   return function WeakSet() { return init(this, arguments.length ? arguments[0] : undefined); };
-}, collectionWeak);
+}, collectionWeak, IS_IE11 || WEAK_COLLECTIONS_UNFREEZING_BUG);
