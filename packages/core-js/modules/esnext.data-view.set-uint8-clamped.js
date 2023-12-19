ae687f45cb0ad3e54f99d3f81d50b729fb4d0beb
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-20 06:19:32 +0700

    inline `to-uint8-clamped`

--- packages/core-js/modules/esnext.data-view.set-uint8-clamped.js
+++ packages/core-js/modules/esnext.data-view.set-uint8-clamped.js
@@ -3,8 +3,8 @@ var $ = require('../internals/export');
 var uncurryThis = require('../internals/function-uncurry-this');
 var aDataView = require('../internals/a-data-view');
 var toIndex = require('../internals/to-index');
-var toUint8Clamped = require('../internals/to-uint8-clamped');
 
+var round = Math.round;
 var setUint8 = uncurryThis(DataView.prototype.setUint8);
 
 // `DataView.prototype.setUint8Clamped` method
@@ -13,6 +13,7 @@ $({ target: 'DataView', proto: true, forced: true }, {
   setUint8Clamped: function setUint8Clamped(byteOffset, value) {
     aDataView(this);
     var offset = toIndex(byteOffset);
-    return setUint8(this, offset, toUint8Clamped(value));
+    var integer = round(value);
+    return setUint8(this, offset, integer < 0 ? 0 : integer > 0xFF ? 0xFF : integer & 0xFF);
   },
 });
