d946c070fce0b391400b4eef7c20b9029b65313a
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-20 09:07:15 +0700

    use array iteration methods in some simple cases

--- packages/core-js/internals/copy-constructor-properties.js
+++ packages/core-js/internals/copy-constructor-properties.js
@@ -6,11 +6,9 @@ var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
 var defineProperty = Object.defineProperty;
 
 module.exports = function (target, source) {
-  var keys = ownKeys(source);
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
+  ownKeys(source).forEach(function (key) {
     if (!hasOwn(target, key)) {
       defineProperty(target, key, getOwnPropertyDescriptor(source, key));
     }
-  }
+  });
 };
