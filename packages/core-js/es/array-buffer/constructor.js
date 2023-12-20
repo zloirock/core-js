154df5d06de0d2c8376beb8a0e750deec4c4de74
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-21 06:46:39 +0700

    extract `ArrayBuffer[@@species]` as a separate module

--- packages/core-js/es/array-buffer/constructor.js
+++ packages/core-js/es/array-buffer/constructor.js
@@ -3,9 +3,10 @@ require('../../modules/es.array-buffer.constructor');
 require('../../modules/es.array-buffer.slice');
 require('../../modules/es.data-view.constructor');
 require('../../modules/es.array-buffer.detached');
+require('../../modules/es.array-buffer.species');
+require('../../modules/es.array-buffer.to-string-tag');
 require('../../modules/es.array-buffer.transfer');
 require('../../modules/es.array-buffer.transfer-to-fixed-length');
-require('../../modules/es.array-buffer.to-string-tag');
 require('../../modules/es.object.to-string');
 var path = require('../../internals/path');
 
