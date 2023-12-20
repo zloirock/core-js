5ba885ab1e0e0f2f24a0181136b8a4a22a1a9dee
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-20 18:02:04 +0700

    move `DataView`-related features from `internals/array-buffer` to `es.data-view.constructor`

--- packages/core-js/modules/es.data-view.constructor.js
+++ packages/core-js/modules/es.data-view.constructor.js
@@ -1,10 +1,33 @@
 'use strict';
-var $ = require('../internals/export');
-var ArrayBufferModule = require('../internals/array-buffer');
+var uncurryThis = require('../internals/function-uncurry-this');
+var defineBuiltIns = require('../internals/define-built-ins');
+var aDataView = require('../internals/a-data-view');
+var toIndex = require('../internals/to-index');
+var setPrototypeOf = require('../internals/object-set-prototype-of');
+var setToStringTag = require('../internals/set-to-string-tag');
 
-// `DataView` constructor
-// https://tc39.es/ecma262/#sec-dataview-constructor
-// TODO: Rework and fix some cases
-$({ global: true, constructor: true }, {
-  DataView: ArrayBufferModule.DataView,
-});
+var $DataView = DataView;
+var DataViewPrototype = $DataView.prototype;
+var ObjectPrototype = Object.prototype;
+var getPrototypeOf = Object.getPrototypeOf;
+var $setInt8 = uncurryThis(DataViewPrototype.setInt8);
+
+// WebKit bug - the same parent prototype for typed arrays and data view
+if (getPrototypeOf(DataViewPrototype) !== ObjectPrototype) {
+  setPrototypeOf(DataViewPrototype, ObjectPrototype);
+}
+
+// iOS Safari 7.x bug
+var testView = new $DataView(new ArrayBuffer(2));
+testView.setInt8(0, 2147483648);
+testView.setInt8(1, 2147483649);
+if (testView.getInt8(0) || !testView.getInt8(1)) defineBuiltIns(DataViewPrototype, {
+  setInt8: function setInt8(byteOffset, value) {
+    $setInt8(aDataView(this), toIndex(byteOffset), value << 24 >> 24);
+  },
+  setUint8: function setUint8(byteOffset, value) {
+    $setInt8(aDataView(this), toIndex(byteOffset), value << 24 >> 24);
+  },
+}, { unsafe: true });
+
+setToStringTag($DataView, 'DataView');
