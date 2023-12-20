cd8053ce353be8ecaa7fa14b98cc3f966187e6eb
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-21 03:03:08 +0700

    extract `DataView.prototype[@@toStringTag]` as a separate module

--- packages/core-js/es/data-view/index.js
+++ packages/core-js/es/data-view/index.js
@@ -6,6 +6,7 @@ require('../../modules/es.data-view.set-int8');
 require('../../modules/es.data-view.set-uint8');
 require('../../modules/es.data-view.get-float16');
 require('../../modules/es.data-view.set-float16');
+require('../../modules/es.data-view.to-string-tag');
 require('../../modules/es.object.to-string');
 var path = require('../../internals/path');
 
