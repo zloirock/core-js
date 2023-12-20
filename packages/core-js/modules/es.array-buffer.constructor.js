2435116390e0084a0b8a31a9acfb317fa06f70f7
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-20 18:02:04 +0700

    move `DataView`-related features from `internals/array-buffer` to `es.data-view.constructor`

--- packages/core-js/modules/es.array-buffer.constructor.js
+++ packages/core-js/modules/es.array-buffer.constructor.js
@@ -1,11 +1,10 @@
 'use strict';
 var $ = require('../internals/export');
 var global = require('../internals/global');
-var arrayBufferModule = require('../internals/array-buffer');
+var ArrayBuffer = require('../internals/array-buffer');
 var setSpecies = require('../internals/set-species');
 
 var ARRAY_BUFFER = 'ArrayBuffer';
-var ArrayBuffer = arrayBufferModule[ARRAY_BUFFER];
 var NativeArrayBuffer = global[ARRAY_BUFFER];
 
 // `ArrayBuffer` constructor
