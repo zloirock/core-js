bc658051c14ecbf1d268ea34b66e677b354a8e96
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-21 04:12:06 +0700

    extract `ArrayBuffer.prototype[@@toStringTag]` as a separate module

--- packages/core-js/es/array-buffer/constructor.js
+++ packages/core-js/es/array-buffer/constructor.js
@@ -5,6 +5,7 @@ require('../../modules/es.data-view.constructor');
 require('../../modules/es.array-buffer.detached');
 require('../../modules/es.array-buffer.transfer');
 require('../../modules/es.array-buffer.transfer-to-fixed-length');
+require('../../modules/es.array-buffer.to-string-tag');
 require('../../modules/es.object.to-string');
 var path = require('../../internals/path');
 
