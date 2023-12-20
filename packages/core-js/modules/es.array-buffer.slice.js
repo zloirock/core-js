745f2c4876b61d392a7a9db44cc36fd3696f711f
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-20 18:14:07 +0700

    move `ArrayBuffer`-related features from `internals/array-buffer` to `es.array-buffer.constructor`

--- packages/core-js/modules/es.array-buffer.slice.js
+++ packages/core-js/modules/es.array-buffer.slice.js
@@ -2,19 +2,19 @@
 var $ = require('../internals/export');
 var uncurryThis = require('../internals/function-uncurry-this-clause');
 var fails = require('../internals/fails');
-var ArrayBuffer = require('../internals/array-buffer');
 var anObject = require('../internals/an-object');
 var toAbsoluteIndex = require('../internals/to-absolute-index');
 var toLength = require('../internals/to-length');
 
+var $ArrayBuffer = ArrayBuffer;
 var $DataView = DataView;
 var DataViewPrototype = $DataView.prototype;
-var nativeArrayBufferSlice = uncurryThis(ArrayBuffer.prototype.slice);
+var nativeArrayBufferSlice = uncurryThis($ArrayBuffer.prototype.slice);
 var getUint8 = uncurryThis(DataViewPrototype.getUint8);
 var setUint8 = uncurryThis(DataViewPrototype.setUint8);
 
 var INCORRECT_SLICE = fails(function () {
-  return !new ArrayBuffer(2).slice(1, undefined).byteLength;
+  return !new $ArrayBuffer(2).slice(1, undefined).byteLength;
 });
 
 // `ArrayBuffer.prototype.slice` method
@@ -27,7 +27,7 @@ $({ target: 'ArrayBuffer', proto: true, unsafe: true, forced: INCORRECT_SLICE },
     var length = anObject(this).byteLength;
     var first = toAbsoluteIndex(start, length);
     var fin = toAbsoluteIndex(end === undefined ? length : end, length);
-    var result = new ArrayBuffer(toLength(fin - first));
+    var result = new $ArrayBuffer(toLength(fin - first));
     var viewSource = new $DataView(this);
     var viewTarget = new $DataView(result);
     var index = 0;
