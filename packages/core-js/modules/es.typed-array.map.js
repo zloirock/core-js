34424a1dc000027fe21c804ed3cd4f14e5b51bd9
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:54:25 +0700

    extract `a-typed-array` helper to a separate module

--- packages/core-js/modules/es.typed-array.map.js
+++ packages/core-js/modules/es.typed-array.map.js
@@ -1,6 +1,6 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
-var aTypedArray = require('../internals/array-buffer-view-core').aTypedArray;
+var aTypedArray = require('../internals/a-typed-array');
 var fromSameTypeAndList = require('../internals/typed-array-from-same-type-and-list');
 var $map = require('../internals/array-iteration').map;
 
