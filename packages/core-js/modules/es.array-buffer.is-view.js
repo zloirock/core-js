5c3808437b9fed837554921c54224a6c58329267
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 02:56:46 +0700

    move `ArrayBuffer.isView` logic directly to `es.array-buffer.is-view`

--- packages/core-js/modules/es.array-buffer.is-view.js
+++ packages/core-js/modules/es.array-buffer.is-view.js
@@ -1,9 +1,16 @@
 'use strict';
 var $ = require('../internals/export');
-var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
+var isObject = require('../internals/is-object');
+var hasOwn = require('../internals/has-own-property');
+var classof = require('../internals/classof');
+var TypedArrayConstructors = require('../internals/typed-array-constructors');
 
 // `ArrayBuffer.isView` method
 // https://tc39.es/ecma262/#sec-arraybuffer.isview
 $({ target: 'ArrayBuffer', stat: true }, {
-  isView: ArrayBufferViewCore.isView,
+  isView: function isView(it) {
+    if (!isObject(it)) return false;
+    var klass = classof(it);
+    return klass === 'DataView' || hasOwn(TypedArrayConstructors, klass);
+  },
 });
