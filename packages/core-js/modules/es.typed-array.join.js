c7fc57746b915d0537464911bae7da9c01cfa46f
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:54:25 +0700

    extract `a-typed-array` helper to a separate module

--- packages/core-js/modules/es.typed-array.join.js
+++ packages/core-js/modules/es.typed-array.join.js
@@ -1,6 +1,6 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
-var aTypedArray = require('../internals/array-buffer-view-core').aTypedArray;
+var aTypedArray = require('../internals/a-typed-array');
 var uncurryThis = require('../internals/function-uncurry-this');
 
 var $join = uncurryThis([].join);
--- packages/core-js/modules/es.typed-array.slice.js
+++ packages/core-js/modules/es.typed-array.slice.js
@@ -1,6 +1,6 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
-var aTypedArray = require('../internals/array-buffer-view-core').aTypedArray;
+var aTypedArray = require('../internals/a-typed-array');
 var typedArraySpeciesConstructor = require('../internals/typed-array-species-constructor');
 var fails = require('../internals/fails');
 var arraySlice = require('../internals/array-slice');
