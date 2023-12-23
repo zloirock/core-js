49ea052521a180b85ac322c02bfeb5d2131aae97
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:54:25 +0700

    extract `a-typed-array` helper to a separate module

--- packages/core-js/modules/es.typed-array.find-last-index.js
+++ packages/core-js/modules/es.typed-array.find-last-index.js
@@ -1,6 +1,6 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
-var aTypedArray = require('../internals/array-buffer-view-core').aTypedArray;
+var aTypedArray = require('../internals/a-typed-array');
 var $findLastIndex = require('../internals/array-iteration-from-last').findLastIndex;
 
 // `%TypedArray%.prototype.findLastIndex` method
