929f114af3ed50bfc82d065826ab32cfab71121f
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:54:25 +0700

    extract `a-typed-array` helper to a separate module

--- packages/core-js/modules/es.typed-array.to-reversed.js
+++ packages/core-js/modules/es.typed-array.to-reversed.js
@@ -1,6 +1,6 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
-var aTypedArray = require('../internals/array-buffer-view-core').aTypedArray;
+var aTypedArray = require('../internals/a-typed-array');
 var arrayToReversed = require('../internals/array-to-reversed');
 var getTypedArrayConstructor = require('../internals/get-typed-array-constructor');
 
