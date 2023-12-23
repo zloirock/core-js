0b8eff000f6321ad91a26aacd590bbb791b738c8
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:11:53 +0700

    extract `export-typed-array-static` helper to a separate module

--- packages/core-js/modules/es.typed-array.of.js
+++ packages/core-js/modules/es.typed-array.of.js
@@ -1,11 +1,11 @@
 'use strict';
 var aTypedArrayConstructor = require('../internals/a-typed-array-constructor');
-var exportTypedArrayStaticMethod = require('../internals/array-buffer-view-core').exportTypedArrayStaticMethod;
+var exportTypedArrayStatic = require('../internals/export-typed-array-static');
 var TYPED_ARRAYS_CONSTRUCTORS_REQUIRES_WRAPPERS = require('../internals/typed-array-constructors-require-wrappers');
 
 // `%TypedArray%.of` method
 // https://tc39.es/ecma262/#sec-%typedarray%.of
-exportTypedArrayStaticMethod('of', function of(/* ...items */) {
+exportTypedArrayStatic('of', function of(/* ...items */) {
   var index = 0;
   var length = arguments.length;
   var result = new (aTypedArrayConstructor(this))(length);
