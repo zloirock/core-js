7f4b83da223b34fa45d4706d77c8c2f917e96f12
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 03:50:15 +0700

    extract `get-typed-array-constructor` helper to a separate module

--- packages/core-js/modules/es.typed-array.to-sorted.js
+++ packages/core-js/modules/es.typed-array.to-sorted.js
@@ -1,11 +1,11 @@
 'use strict';
-var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
 var uncurryThis = require('../internals/function-uncurry-this');
 var aCallable = require('../internals/a-callable');
+var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
+var getTypedArrayConstructor = require('../internals/get-typed-array-constructor');
 var arrayFromConstructorAndList = require('../internals/array-from-constructor-and-list');
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
-var getTypedArrayConstructor = ArrayBufferViewCore.getTypedArrayConstructor;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
 var sort = uncurryThis(ArrayBufferViewCore.TypedArrayPrototype.sort);
 
