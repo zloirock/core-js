96023ccc81af12cd44910f3ba93d837e427eb353
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:41:24 +0700

    extract `export-typed-array-method` helper to a separate module

---
+++ packages/core-js/internals/export-typed-array-method.js
@@ -0,0 +1,24 @@
+'use strict';
+var globalThis = require('../internals/global-this');
+var defineBuiltIn = require('../internals/define-built-in');
+var hasOwn = require('../internals/has-own-property');
+var TypedArrayConstructors = require('../internals/typed-array-constructors');
+var TypedArrayPrototype = require('../internals/array-buffer-view-core').TypedArrayPrototype;
+
+module.exports = function (KEY, property, forced, options) {
+  if (forced) for (var ARRAY in TypedArrayConstructors) {
+    var TypedArrayConstructor = globalThis[ARRAY];
+    if (TypedArrayConstructor && hasOwn(TypedArrayConstructor.prototype, KEY)) try {
+      delete TypedArrayConstructor.prototype[KEY];
+    } catch (error) {
+      // old WebKit bug - some methods are non-configurable
+      try {
+        TypedArrayConstructor.prototype[KEY] = property;
+      } catch (error2) { /* empty */ }
+    }
+  }
+  if (!TypedArrayPrototype[KEY] || forced) {
+    defineBuiltIn(TypedArrayPrototype, KEY, forced ? property
+      : Int8Array.prototype[KEY] || property, options);
+  }
+};
