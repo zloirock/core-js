98685a8d46a430293d95ab16edb98860f86b157a
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:11:53 +0700

    extract `export-typed-array-static` helper to a separate module

--- packages/core-js/internals/array-buffer-view-core.js
+++ packages/core-js/internals/array-buffer-view-core.js
@@ -53,29 +53,6 @@ var exportTypedArrayMethod = function (KEY, property, forced, options) {
   }
 };
 
-// TODO: Rewrite!!!
-var exportTypedArrayStaticMethod = function (KEY, property, forced) {
-  var ARRAY, TypedArrayConstructor;
-  if (forced) for (ARRAY in TypedArrayConstructors) {
-    TypedArrayConstructor = globalThis[ARRAY];
-    if (TypedArrayConstructor && hasOwn(TypedArrayConstructor, KEY)) try {
-      delete TypedArrayConstructor[KEY];
-    } catch (error) { /* empty */ }
-  }
-  if (!TypedArray[KEY] || forced) {
-    // V8 ~ Chrome 49-50 `%TypedArray%` methods are non-writable non-configurable
-    try {
-      return defineBuiltIn(TypedArray, KEY, forced ? property : TypedArray[KEY] || property);
-    } catch (error) { /* empty */ }
-  } else return;
-  for (ARRAY in TypedArrayConstructors) {
-    TypedArrayConstructor = globalThis[ARRAY];
-    if (TypedArrayConstructor && (!TypedArrayConstructor[KEY] || forced)) {
-      defineBuiltIn(TypedArrayConstructor, KEY, property);
-    }
-  }
-};
-
 var getTypedArrayMetadata = function (it, key) {
   var proto = getPrototypeOf(it);
   if (!isObject(proto)) return;
@@ -128,7 +105,6 @@ if (!hasOwn(TypedArrayPrototype, TO_STRING_TAG)) {
 module.exports = {
   aTypedArray: aTypedArray,
   exportTypedArrayMethod: exportTypedArrayMethod,
-  exportTypedArrayStaticMethod: exportTypedArrayStaticMethod,
   isTypedArray: isTypedArray,
   TypedArray: TypedArray,
   TypedArrayPrototype: TypedArrayPrototype,
