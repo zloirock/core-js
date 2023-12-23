f7b90cdf24c5f11ff590a565cf3ce929b5307ce4
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 05:11:53 +0700

    extract `export-typed-array-static` helper to a separate module

---
+++ packages/core-js/internals/export-typed-array-static.js
@@ -0,0 +1,29 @@
+'use strict';
+var globalThis = require('../internals/global-this');
+var hasOwn = require('../internals/has-own-property');
+var defineBuiltIn = require('../internals/define-built-in');
+var TypedArrayConstructors = require('../internals/typed-array-constructors');
+var TypedArray = require('../internals/array-buffer-view-core').TypedArray;
+
+// TODO: Rewrite!!!
+module.exports = function (KEY, property, forced) {
+  var ARRAY, TypedArrayConstructor;
+  if (forced) for (ARRAY in TypedArrayConstructors) {
+    TypedArrayConstructor = globalThis[ARRAY];
+    if (TypedArrayConstructor && hasOwn(TypedArrayConstructor, KEY)) try {
+      delete TypedArrayConstructor[KEY];
+    } catch (error) { /* empty */ }
+  }
+  if (!TypedArray[KEY] || forced) {
+    // V8 ~ Chrome 49-50 `%TypedArray%` methods are non-writable non-configurable
+    try {
+      return defineBuiltIn(TypedArray, KEY, forced ? property : TypedArray[KEY] || property);
+    } catch (error) { /* empty */ }
+  } else return;
+  for (ARRAY in TypedArrayConstructors) {
+    TypedArrayConstructor = globalThis[ARRAY];
+    if (TypedArrayConstructor && (!TypedArrayConstructor[KEY] || forced)) {
+      defineBuiltIn(TypedArrayConstructor, KEY, property);
+    }
+  }
+};
