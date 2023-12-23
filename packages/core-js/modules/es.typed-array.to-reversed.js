d24c275339beb1205b3c1da267069b3f01949785
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 03:50:15 +0700

    extract `get-typed-array-constructor` helper to a separate module

--- packages/core-js/modules/es.typed-array.to-reversed.js
+++ packages/core-js/modules/es.typed-array.to-reversed.js
@@ -1,10 +1,10 @@
 'use strict';
 var lengthOfArrayLike = require('../internals/length-of-array-like');
 var ArrayBufferViewCore = require('../internals/array-buffer-view-core');
+var getTypedArrayConstructor = require('../internals/get-typed-array-constructor');
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
-var getTypedArrayConstructor = ArrayBufferViewCore.getTypedArrayConstructor;
 
 // `%TypedArray%.prototype.toReversed` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.toreversed
