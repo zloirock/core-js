fe20f86fc21eb0ed3eb43d23e4452b2e6db8807c
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-24 03:50:15 +0700

    extract `get-typed-array-constructor` helper to a separate module

--- packages/core-js/internals/typed-array-from-same-type-and-list.js
+++ packages/core-js/internals/typed-array-from-same-type-and-list.js
@@ -1,6 +1,6 @@
 'use strict';
 var arrayFromConstructorAndList = require('../internals/array-from-constructor-and-list');
-var getTypedArrayConstructor = require('../internals/array-buffer-view-core').getTypedArrayConstructor;
+var getTypedArrayConstructor = require('../internals/get-typed-array-constructor');
 
 module.exports = function (instance, list) {
   return arrayFromConstructorAndList(getTypedArrayConstructor(instance), list);
