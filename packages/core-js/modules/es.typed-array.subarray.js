8227eb2eff5538e73437542bc91f4003cdc724d0
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-25 05:58:30 +0700

    rework `export-typed-array-static` helper, fix some `%TypedArray%` methods

--- packages/core-js/modules/es.typed-array.subarray.js
+++ packages/core-js/modules/es.typed-array.subarray.js
@@ -10,6 +10,10 @@ var DOES_NOT_WORK_WITH_DIFFERENT_TYPED_ARRAYS = fails(function () {
   return !(Int8Array.prototype.subarray.call(new Float32Array([1, 2, 3]), 0, 1) instanceof Float32Array);
 });
 
+var DOES_NOT_WORK_WITH_DIFFERENT_TYPED_ARRAYS = fails(function () {
+  return !(Int8Array.prototype.subarray.call(new Float32Array([1, 2, 3]), 0, 1) instanceof Float32Array);
+});
+
 // `%TypedArray%.prototype.subarray` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.subarray
 exportTypedArrayMethod('subarray', function subarray(begin, end) {
