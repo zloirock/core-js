aedcbdbe7c338cd846881e3377179a9006404be6
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-25 05:58:30 +0700

    rework `export-typed-array-static` helper, fix some `%TypedArray%` methods

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1718,6 +1718,7 @@ export const data = {
     // 10.0 - 14.0 accept incorrect arguments
     safari: '14.1',
   },
+  // TODO: recheck
   'es.typed-array.subarray': {
     chrome: '26',
     edge: '13',
@@ -1726,7 +1727,7 @@ export const data = {
     safari: '7.1',
   },
   'es.typed-array.to-locale-string': {
-    chrome: '45',
+    chrome: '49',
     firefox: '51',
     hermes: '0.1',
     safari: '10.0',
