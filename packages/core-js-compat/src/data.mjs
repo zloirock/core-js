a33d4c844db0fc607395c2f7b1185ec98f7e78e9
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-25 05:58:30 +0700

    rework `export-typed-array-static` helper, fix some `%TypedArray%` methods

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -1813,6 +1813,7 @@ export const data = {
     // 10.0 - 14.0 accept incorrect arguments
     safari: '14.1',
   },
+  // TODO: recheck
   'es.typed-array.subarray': {
     chrome: '26',
     edge: '13',
@@ -1821,7 +1822,7 @@ export const data = {
     safari: '7.1',
   },
   'es.typed-array.to-locale-string': {
-    chrome: '45',
+    chrome: '49',
     firefox: '51',
     hermes: '0.1',
     safari: '10.0',
--- packages/core-js/modules/es.typed-array.subarray.js
+++ packages/core-js/modules/es.typed-array.subarray.js
@@ -1,9 +1,14 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
+var fails = require('../internals/fails');
 var aTypedArray = require('../internals/a-typed-array');
 var toLength = require('../internals/to-length');
 var toAbsoluteIndex = require('../internals/to-absolute-index');
 var typedArraySpeciesConstructor = require('../internals/typed-array-species-constructor');
+
+var DOES_NOT_WORK_WITH_DIFFERENT_TYPED_ARRAYS = fails(function () {
+  return !(Int8Array.prototype.subarray.call(new Float32Array([1, 2, 3]), 0, 1) instanceof Float32Array);
+});
 
 // `%TypedArray%.prototype.subarray` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.subarray
@@ -17,4 +22,4 @@ exportTypedArrayMethod('subarray', function subarray(begin, end) {
     O.byteOffset + beginIndex * O.BYTES_PER_ELEMENT,
     toLength((end === undefined ? length : toAbsoluteIndex(end, length)) - beginIndex)
   );
-});
+}, DOES_NOT_WORK_WITH_DIFFERENT_TYPED_ARRAYS);
