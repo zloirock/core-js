51af7f1e8d23bc5abd9ed2002ee756d46063ebac
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 02:28:02 +0700

    use `Object.keys().forEach()` instead of `for-in` in some cases

--- packages/core-js/internals/define-built-ins.js
+++ packages/core-js/internals/define-built-ins.js
@@ -2,6 +2,8 @@
 var defineBuiltIn = require('../internals/define-built-in');
 
 module.exports = function (target, src, options) {
-  for (var key in src) defineBuiltIn(target, key, src[key], options);
+  Object.keys(src).forEach(function (key) {
+    defineBuiltIn(target, key, src[key], options);
+  });
   return target;
 };
