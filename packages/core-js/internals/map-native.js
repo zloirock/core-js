ed93106171f71990675cec85cd56ad6ba9e7ae73
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 06:44:03 +0700

    allow `WeakMap`, `Map` and `Set` in `eslint` config

--- packages/core-js/internals/map-native.js
+++ packages/core-js/internals/map-native.js
@@ -1,11 +1,9 @@
 'use strict';
 var uncurryThis = require('../internals/function-uncurry-this');
 
-// eslint-disable-next-line es/no-map -- safe
 var MapPrototype = Map.prototype;
 
 module.exports = {
-  // eslint-disable-next-line es/no-map -- safe
   Map: Map,
   set: uncurryThis(MapPrototype.set),
   get: uncurryThis(MapPrototype.get),
