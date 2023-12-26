44dd07a62f881125d8ae4ab8fa2efe8872cac281
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-26 11:17:22 +0700

    rework `inherit-if-required`

--- packages/core-js/modules/es.array-buffer.constructor.js
+++ packages/core-js/modules/es.array-buffer.constructor.js
@@ -34,7 +34,7 @@ if (WRAP) {
   /* eslint-enable no-new -- required for testing */
   $ArrayBuffer = function ArrayBuffer(length) {
     anInstance(this, ArrayBufferPrototype);
-    return inheritIfRequired(new NativeArrayBuffer(toIndex(length)), this, $ArrayBuffer);
+    return inheritIfRequired(new NativeArrayBuffer(toIndex(length)), this, ArrayBufferPrototype);
   };
 
   $ArrayBuffer[PROTOTYPE] = ArrayBufferPrototype;
