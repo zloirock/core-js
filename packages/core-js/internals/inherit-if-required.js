87be1c3b3c943cd394fd08964922b430fdc15121
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-26 11:17:22 +0700

    rework `inherit-if-required`

--- packages/core-js/internals/inherit-if-required.js
+++ packages/core-js/internals/inherit-if-required.js
@@ -1,17 +1,11 @@
 'use strict';
-var isCallable = require('../internals/is-callable');
-var isObject = require('../internals/is-object');
 var setPrototypeOf = require('../internals/object-set-prototype-of');
 
+var getPrototypeOf = Object.getPrototypeOf;
+
 // makes subclassing work correct for wrapped built-ins
-module.exports = function ($this, dummy, Wrapper) {
-  var NewTarget, NewTargetPrototype;
-  if (
-    // we haven't completely correct pre-ES6 way for getting `new.target`, so use this
-    isCallable(NewTarget = dummy.constructor) &&
-    NewTarget !== Wrapper &&
-    isObject(NewTargetPrototype = NewTarget.prototype) &&
-    NewTargetPrototype !== Wrapper.prototype
-  ) setPrototypeOf($this, NewTargetPrototype);
+module.exports = function ($this, dummy, basePrototype) {
+  var dummyPrototype = getPrototypeOf(dummy);
+  if (dummyPrototype !== basePrototype) setPrototypeOf($this, dummyPrototype);
   return $this;
 };
