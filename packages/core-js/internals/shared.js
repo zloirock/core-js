136675bb6c1072a62fcbfd39e5bf581f405982a5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 08:41:56 +0700

    some changes in `shared-store`

--- packages/core-js/internals/shared.js
+++ packages/core-js/internals/shared.js
@@ -1,8 +1,6 @@
 'use strict';
 var store = require('../internals/shared-store');
 
-var create = Object.create;
-
 module.exports = function (key, value) {
-  return store[key] || (store[key] = value || create(null));
+  return store[key] || (store[key] = value || Object.create(null));
 };
