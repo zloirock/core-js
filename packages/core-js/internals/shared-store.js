c9465af1228ba0022884a76ae85c1487cd2a3576
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 08:41:56 +0700

    some changes in `shared-store`

--- packages/core-js/internals/shared-store.js
+++ packages/core-js/internals/shared-store.js
@@ -1,8 +1,15 @@
 'use strict';
+var IS_PURE = require('../internals/is-pure');
 var global = require('../internals/global');
 var defineGlobalProperty = require('../internals/define-global-property');
 
 var SHARED = '__core-js_shared__';
-var store = global[SHARED] || defineGlobalProperty(SHARED, {});
+var store = module.exports = global[SHARED] || defineGlobalProperty(SHARED, {});
 
-module.exports = store;
+(store.versions || (store.versions = [])).push({
+  version: '4.0.0-alpha.0',
+  mode: IS_PURE ? 'pure' : 'global',
+  copyright: '© 2014-2024 Denis Pushkarev (zloirock.ru)',
+  license: 'MIT',
+  source: 'https://github.com/zloirock/core-js',
+});
--- packages/core-js/internals/shared.js
+++ packages/core-js/internals/shared.js
@@ -1,12 +1,6 @@
 'use strict';
-var IS_PURE = require('../internals/is-pure');
 var store = require('../internals/shared-store');
 
-(module.exports = function (key, value) {
-  return store[key] || (store[key] = value !== undefined ? value : {});
-})('versions', []).push({
-  version: '4.0.0-alpha.0',
-  mode: IS_PURE ? 'pure' : 'global',
-  copyright: '© 2014-2024 Denis Pushkarev (zloirock.ru)',
-  license: 'https://github.com/zloirock/core-js/blob/v4.0.0-alpha.0/LICENSE',
-});
+module.exports = function (key, value) {
+  return store[key] || (store[key] = value || Object.create(null));
+};
