45ae4e416763ebbf117387800d45ce959c6804cd
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 12:46:51 +0700

    drop `is-forced` logic as not viable in the current form

--- packages/core-js/configurator.js
+++ packages/core-js/configurator.js
@@ -1,24 +1,12 @@
 'use strict';
 var hasOwn = require('./internals/has-own-property');
-var isForced = require('./internals/is-forced');
 var shared = require('./internals/shared-store');
 
-var data = isForced.data;
-var normalize = isForced.normalize;
 var USE_FUNCTION_CONSTRUCTOR = 'USE_FUNCTION_CONSTRUCTOR';
 var ASYNC_ITERATOR_PROTOTYPE = 'AsyncIteratorPrototype';
 
-var setAggressivenessLevel = function (object, constant) {
-  if (Array.isArray(object)) object.forEach(function (key) {
-    data[normalize(key)] = constant;
-  });
-};
-
 module.exports = function (options) {
   if (typeof options == 'object') {
-    setAggressivenessLevel(options.useNative, isForced.NATIVE);
-    setAggressivenessLevel(options.usePolyfill, isForced.POLYFILL);
-    setAggressivenessLevel(options.useFeatureDetection, null);
     if (hasOwn(options, USE_FUNCTION_CONSTRUCTOR)) {
       shared[USE_FUNCTION_CONSTRUCTOR] = !!options[USE_FUNCTION_CONSTRUCTOR];
     }
