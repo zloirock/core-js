ada65e2a67473c5d016699f40d093914b01049a2
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-28 05:29:17 +0700

    rename internal `global` reference to `globalThis` for consistency with the standard

--- packages/core-js/internals/symbol-constructor-detection.js
+++ packages/core-js/internals/symbol-constructor-detection.js
@@ -1,10 +1,8 @@
 'use strict';
 /* eslint-disable es/no-symbol -- required for testing */
-var V8_VERSION = require('../internals/environment-v8-version');
-var fails = require('../internals/fails');
 var globalThis = require('../internals/global-this');
-
-var $String = globalThis.String;
+var fails = require('../internals/fails');
+var V8_VERSION = require('../internals/environment-v8-version');
 
 // eslint-disable-next-line es/no-object-getownpropertysymbols -- required for testing
 module.exports = !!Object.getOwnPropertySymbols && !fails(function () {
@@ -13,7 +11,7 @@ module.exports = !!Object.getOwnPropertySymbols && !fails(function () {
   // `get-own-property-symbols` polyfill symbols converted to object are not Symbol instances
   // nb: Do not call `String` directly to avoid this being optimized out to `symbol+''` which will,
   // of course, fail.
-  return !$String(symbol) || !(Object(symbol) instanceof Symbol) ||
+  return !globalThis.String(symbol) || !(Object(symbol) instanceof Symbol) ||
     // Chrome 38-40 symbols are not inherited from DOM collections prototypes to instances
     !Symbol.sham && V8_VERSION && V8_VERSION < 41;
 });
