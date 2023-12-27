86deea933fb99fae301a8ffb7c2822c8acc121e5
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 12:46:51 +0700

    drop `is-forced` logic as not viable in the current form

--- packages/core-js/internals/promise-constructor-detection.js
+++ packages/core-js/internals/promise-constructor-detection.js
@@ -2,7 +2,7 @@
 var globalThis = require('../internals/global-this');
 var NativePromiseConstructor = require('../internals/promise-native-constructor');
 var isCallable = require('../internals/is-callable');
-var isForced = require('../internals/is-forced');
+var fails = require('../internals/fails');
 var inspectSource = require('../internals/inspect-source');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 var ENVIRONMENT = require('../internals/environment');
@@ -14,7 +14,7 @@ var SPECIES = wellKnownSymbol('species');
 var SUBCLASSING = false;
 var NATIVE_PROMISE_REJECTION_EVENT = isCallable(globalThis.PromiseRejectionEvent);
 
-var FORCED_PROMISE_CONSTRUCTOR = isForced('Promise', function () {
+var FORCED_PROMISE_CONSTRUCTOR = fails(function () {
   var PROMISE_CONSTRUCTOR_SOURCE = inspectSource(NativePromiseConstructor);
   var GLOBAL_CORE_JS_PROMISE = PROMISE_CONSTRUCTOR_SOURCE !== String(NativePromiseConstructor);
   // V8 6.6 (Node 10 and Chrome 66) have a bug with resolving custom thenables
