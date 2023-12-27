c4f2a7f5e119828c8dee019f4de648c32b39f1f6
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-27 13:45:43 +0700

    drop extra `global` usage

--- packages/core-js/es/typed-array/float64-array.js
+++ packages/core-js/es/typed-array/float64-array.js
@@ -3,6 +3,5 @@ require('../../modules/es.array-buffer.constructor');
 require('../../modules/es.array-buffer.slice');
 require('../../modules/es.typed-array.float64-array');
 require('./methods');
-var globalThis = require('../../internals/global-this');
 
-module.exports = globalThis.Float64Array;
+module.exports = Float64Array;
--- packages/core-js/modules/es.typed-array.to-string.js
+++ packages/core-js/modules/es.typed-array.to-string.js
@@ -1,11 +1,8 @@
 'use strict';
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
 var fails = require('../internals/fails');
-var globalThis = require('../internals/global-this');
 var uncurryThis = require('../internals/function-uncurry-this');
 
-var Uint8Array = globalThis.Uint8Array;
-var Uint8ArrayPrototype = Uint8Array && Uint8Array.prototype || {};
 var arrayToString = [].toString;
 var join = uncurryThis([].join);
 
@@ -15,7 +12,7 @@ if (fails(function () { arrayToString.call({}); })) {
   };
 }
 
-var IS_NOT_ARRAY_METHOD = Uint8ArrayPrototype.toString !== arrayToString;
+var IS_NOT_ARRAY_METHOD = Int8Array.prototype.toString !== arrayToString;
 
 // `%TypedArray%.prototype.toString` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.tostring
