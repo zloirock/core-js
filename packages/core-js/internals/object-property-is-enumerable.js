6f8594237899d18b5ad609b10a132e1c401537ec
Denis Pushkarev <zloirock@zloirock.ru>
2023-12-28 09:20:10 +0700

    drop a workaround of ancient Nashorn `Object.prototype.propertyIsEnumerable` bug

--- packages/core-js/internals/object-property-is-enumerable.js
+++ packages/core-js/internals/object-property-is-enumerable.js
@@ -1,13 +1,2 @@
 'use strict';
-var $propertyIsEnumerable = {}.propertyIsEnumerable;
-var getOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
-
-// Nashorn ~ JDK8 bug
-var NASHORN_BUG = getOwnPropertyDescriptor && !$propertyIsEnumerable.call({ 1: 2 }, 1);
-
-// `Object.prototype.propertyIsEnumerable` method implementation
-// https://tc39.es/ecma262/#sec-object.prototype.propertyisenumerable
-exports.f = NASHORN_BUG ? function propertyIsEnumerable(V) {
-  var descriptor = getOwnPropertyDescriptor(this, V);
-  return !!descriptor && descriptor.enumerable;
-} : $propertyIsEnumerable;
+exports.f = {}.propertyIsEnumerable;
