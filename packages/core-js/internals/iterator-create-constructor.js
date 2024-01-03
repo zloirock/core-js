b37d8debd6efff0ab244d1965edd6da6723e381b
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-03 18:01:57 +0700

    drop internal `Iterators` abstraction

--- packages/core-js/internals/iterator-create-constructor.js
+++ packages/core-js/internals/iterator-create-constructor.js
@@ -2,12 +2,10 @@
 var IteratorPrototype = require('../internals/iterators-core').IteratorPrototype;
 var createPropertyDescriptor = require('../internals/create-property-descriptor');
 var setToStringTag = require('../internals/set-to-string-tag');
-var Iterators = require('../internals/iterators');
 
 module.exports = function (IteratorConstructor, NAME, next, ENUMERABLE_NEXT) {
   var TO_STRING_TAG = NAME + ' Iterator';
   IteratorConstructor.prototype = Object.create(IteratorPrototype, { next: createPropertyDescriptor(+!ENUMERABLE_NEXT, next) });
   setToStringTag(IteratorConstructor, TO_STRING_TAG, false, true);
-  Iterators[TO_STRING_TAG] = function () { return this; };
   return IteratorConstructor;
 };
