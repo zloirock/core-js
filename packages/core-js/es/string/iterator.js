e75daa1d365780fd90d34c7b79a90d6bb8b693fd
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-03 18:01:57 +0700

    drop internal `Iterators` abstraction

--- packages/core-js/es/string/iterator.js
+++ packages/core-js/es/string/iterator.js
@@ -1,7 +1,9 @@
 'use strict';
+var uncurryThis = require('../../internals/function-uncurry-this');
+var wellKnownSymbol = require('../../internals/well-known-symbol');
 require('../../modules/es.object.to-string');
 require('../../modules/es.string.iterator');
-var uncurryThis = require('../../internals/function-uncurry-this');
-var Iterators = require('../../internals/iterators');
 
-module.exports = uncurryThis(Iterators.String);
+var ITERATOR = wellKnownSymbol('iterator');
+
+module.exports = uncurryThis(String.prototype[ITERATOR]);
