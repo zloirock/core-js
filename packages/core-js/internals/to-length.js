3928aec65d742313de57837ff2428ca7c88d09e7
Minseok Choe <choems@outlook.com>
2024-01-03 16:06:41 +0900

    fix  abstract operation

--- packages/core-js/internals/to-length.js
+++ packages/core-js/internals/to-length.js
@@ -6,5 +6,6 @@ var min = Math.min;
 // `ToLength` abstract operation
 // https://tc39.es/ecma262/#sec-tolength
 module.exports = function (argument) {
-  return argument > 0 ? min(toIntegerOrInfinity(argument), 0x1FFFFFFFFFFFFF) : 0; // 2 ** 53 - 1 == 9007199254740991
+  var len = toIntegerOrInfinity(argument);
+  return len > 0 ? min(len, 0x1FFFFFFFFFFFFF) : 0; // 2 ** 53 - 1 == 9007199254740991
 };
