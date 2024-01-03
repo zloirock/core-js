dd1f64c6bd4b38f607b7f0687c64045ad1c13ddb
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-03 18:01:57 +0700

    drop internal `Iterators` abstraction

--- packages/core-js/internals/get-iterator-method.js
+++ packages/core-js/internals/get-iterator-method.js
@@ -1,12 +1,12 @@
 'use strict';
-var classof = require('../internals/classof');
+var classof = require('../internals/classof-raw');
 var getMethod = require('../internals/get-method');
 var isNullOrUndefined = require('../internals/is-null-or-undefined');
-var Iterators = require('../internals/iterators');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
 var ITERATOR = wellKnownSymbol('iterator');
+var ArrayPrototype = Array.prototype;
 
 module.exports = function (it) {
-  if (!isNullOrUndefined(it)) return getMethod(it, ITERATOR) || Iterators[classof(it)];
+  if (!isNullOrUndefined(it)) return getMethod(it, ITERATOR) || (classof(it) === 'Arguments' ? ArrayPrototype[ITERATOR] : undefined);
 };
