7184c8c474022e236cbcf0601dccb8e61a88cd33
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-05 10:29:42 +0700

    return internal `Iterators` store for the pure version

--- packages/core-js/internals/iterator-create-constructor.js
+++ packages/core-js/internals/iterator-create-constructor.js
@@ -4,8 +4,7 @@ var createPropertyDescriptor = require('../internals/create-property-descriptor'
 var setToStringTag = require('../internals/set-to-string-tag');
 
 module.exports = function (IteratorConstructor, NAME, next, ENUMERABLE_NEXT) {
-  var TO_STRING_TAG = NAME + ' Iterator';
   IteratorConstructor.prototype = Object.create(IteratorPrototype, { next: createPropertyDescriptor(+!ENUMERABLE_NEXT, next) });
-  setToStringTag(IteratorConstructor, TO_STRING_TAG, false, true);
+  setToStringTag(IteratorConstructor, NAME + ' Iterator', false, true);
   return IteratorConstructor;
 };
