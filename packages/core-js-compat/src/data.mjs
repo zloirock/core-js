a19eeed64fd9434597a689861de6e8f52ec1151d
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-05 15:10:34 +0700

    ensure some dependencies

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2019,6 +2019,25 @@ export const data = {
     rhino: '1.7.15',
     safari: '15.4',
   },
+  'es.typed-array.from': {
+    chrome: '54',
+    edge: '15',
+    firefox: '55',
+    safari: '14.0',
+  },
+  'es.typed-array.of': {
+    chrome: '54',
+    edge: '15',
+    firefox: '55',
+    safari: '14.0',
+  },
+  'es.typed-array.iterator': {
+    chrome: '51',
+    edge: '13',
+    firefox: '37',
+    hermes: '0.1',
+    safari: '10.0',
+  },
   'es.typed-array.copy-within': {
     chrome: '45',
     edge: '13',
@@ -2081,12 +2100,6 @@ export const data = {
     hermes: '0.1',
     safari: '10.0',
   },
-  'es.typed-array.from': {
-    chrome: '54',
-    edge: '15',
-    firefox: '55',
-    safari: '14.0',
-  },
   'es.typed-array.includes': {
     chrome: '49',
     edge: '14',
@@ -2096,13 +2109,6 @@ export const data = {
   },
   'es.typed-array.index-of': {
     chrome: '45',
-    edge: '13',
-    firefox: '37',
-    hermes: '0.1',
-    safari: '10.0',
-  },
-  'es.typed-array.iterator': {
-    chrome: '51',
     edge: '13',
     firefox: '37',
     hermes: '0.1',
@@ -2128,12 +2134,6 @@ export const data = {
     firefox: '38',
     hermes: '0.1',
     safari: '10.0',
-  },
-  'es.typed-array.of': {
-    chrome: '54',
-    edge: '15',
-    firefox: '55',
-    safari: '14.0',
   },
   'es.typed-array.reduce': {
     chrome: '45',
