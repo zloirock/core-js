c35cd08ceec3efd0f9b1d2fa79706190e8c5c782
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-05 12:52:30 +0700

    simplify some cases of `Array` normalization logic, split `es.array.iterator`

--- packages/core-js/modules/es.array.iterator.js
+++ packages/core-js/modules/es.array.iterator.js
@@ -1,57 +1,10 @@
 'use strict';
-var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
-var toObject = require('../internals/to-object');
-var addToUnscopables = require('../internals/add-to-unscopables');
-var setInternalState = require('../internals/internal-state').set;
-var internalStateGetterFor = require('../internals/internal-state-getter-for');
-var defineIterator = require('../internals/iterator-define');
-var createIterResultObject = require('../internals/create-iter-result-object');
-var IS_PURE = require('../internals/is-pure');
+var defineIterator = require('../internals/iterator-set');
+var normalizeIteratorMethod = require('../internals/iterator-normalize-method');
+var ArrayIterator = require('../internals/array-iterator-constructor');
 
-var ARRAY_ITERATOR = 'Array Iterator';
-var getInternalState = internalStateGetterFor(ARRAY_ITERATOR);
-
-// `Array.prototype.entries` method
-// https://tc39.es/ecma262/#sec-array.prototype.entries
-// `Array.prototype.keys` method
-// https://tc39.es/ecma262/#sec-array.prototype.keys
-// `Array.prototype.values` method
-// https://tc39.es/ecma262/#sec-array.prototype.values
 // `Array.prototype[@@iterator]` method
 // https://tc39.es/ecma262/#sec-array.prototype-@@iterator
-// `CreateArrayIterator` internal method
-// https://tc39.es/ecma262/#sec-createarrayiterator
-defineIterator(Array, 'Array', function (iterated, kind) {
-  setInternalState(this, {
-    type: ARRAY_ITERATOR,
-    target: toObject(iterated), // target
-    index: 0,                          // next index
-    kind: kind,                        // kind
-  });
-// `%ArrayIteratorPrototype%.next` method
-// https://tc39.es/ecma262/#sec-%arrayiteratorprototype%.next
-}, function () {
-  var state = getInternalState(this);
-  var target = state.target;
-  var index = state.index++;
-  if (!target || index >= target.length) {
-    state.target = null;
-    return createIterResultObject(undefined, true);
-  }
-  switch (state.kind) {
-    case 'keys': return createIterResultObject(index, false);
-    case 'values': return createIterResultObject(target[index], false);
-  } return createIterResultObject([index, target[index]], false);
-}, 'values');
-
-// https://tc39.es/ecma262/#sec-array.prototype-@@unscopables
-addToUnscopables('keys');
-addToUnscopables('values');
-addToUnscopables('entries');
-
-var values = getBuiltInPrototypeMethod('Array', 'values');
-
-// V8 ~ Chrome 45- bug
-if (!IS_PURE && values.name !== 'values') try {
-  Object.defineProperty(values, 'name', { value: 'values' });
-} catch (error) { /* empty */ }
+defineIterator('Array', normalizeIteratorMethod(Array, 'Array') || function values() {
+  return new ArrayIterator(this, 'values');
+});
