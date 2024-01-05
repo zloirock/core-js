2c636e5bd25948ba1434fa3a04302520d190042c
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-05 12:52:30 +0700

    simplify some cases of `Array` normalization logic, split `es.array.iterator`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -471,6 +471,15 @@ GLOBAL.tests = {
   'es.promise.with-resolvers': [PROMISES_SUPPORT, function () {
     return Promise.withResolvers;
   }],
+  'es.array.iterator': [SYMBOLS_SUPPORT, function () {
+    return [][Symbol.iterator] === [].values
+      && [][Symbol.iterator].name === 'values'
+      && [].entries()[Symbol.toStringTag] === 'Array Iterator'
+      && [].keys().next()
+      && [][Symbol.unscopables].keys
+      && [][Symbol.unscopables].values
+      && [][Symbol.unscopables].entries;
+  }],
   'es.array.at': function () {
     return [].at;
   },
@@ -489,6 +498,13 @@ GLOBAL.tests = {
   'es.array.copy-within': function () {
     return Array.prototype.copyWithin && Array.prototype[Symbol.unscopables].copyWithin;
   },
+  'es.array.entries': [SYMBOLS_SUPPORT, function () {
+    var iterator = [].entries();
+    return iterator.next()
+      && iterator[Symbol.iterator]() === iterator
+      && iterator[Symbol.toStringTag] === 'Array Iterator'
+      && [][Symbol.unscopables].entries;
+  }],
   'es.array.fill': function () {
     return Array.prototype.fill && Array.prototype[Symbol.unscopables].fill;
   },
@@ -534,15 +550,6 @@ GLOBAL.tests = {
       return 1 / [1].indexOf(1, -0) > 0;
     }
   },
-  'es.array.iterator': [SYMBOLS_SUPPORT, function () {
-    return [][Symbol.iterator] === [].values
-      && [][Symbol.iterator].name === 'values'
-      && [].entries()[Symbol.toStringTag] === 'Array Iterator'
-      && [].keys().next()
-      && [][Symbol.unscopables].keys
-      && [][Symbol.unscopables].values
-      && [][Symbol.unscopables].entries;
-  }],
   'es.array.join': function () {
     try {
       if (!Object.prototype.propertyIsEnumerable.call(Object('z'), 0)) return false;
@@ -555,6 +562,13 @@ GLOBAL.tests = {
     } catch (error) { /* empty */ }
     return true;
   },
+  'es.array.keys': [SYMBOLS_SUPPORT, function () {
+    var iterator = [].keys();
+    return iterator.next()
+      && iterator[Symbol.iterator]() === iterator
+      && iterator[Symbol.toStringTag] === 'Array Iterator'
+      && [][Symbol.unscopables].keys;
+  }],
   'es.array.last-index-of': function () {
     try {
       [].lastIndexOf.call(null);
@@ -680,6 +694,15 @@ GLOBAL.tests = {
       return error instanceof TypeError;
     }
   },
+  'es.array.values': [SYMBOLS_SUPPORT, function () {
+    var iterator = [].values();
+    return iterator.next()
+      && iterator[Symbol.iterator]() === iterator
+      && iterator[Symbol.toStringTag] === 'Array Iterator'
+      && [][Symbol.unscopables].values
+      && [].values.name === 'values'
+      && [][Symbol.iterator] === [].values;
+  }],
   'es.array.with': function () {
     return [].with;
   },
