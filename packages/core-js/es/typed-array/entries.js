692637529832e06653ad66b3f47e4970562309ce
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-05 15:10:34 +0700

    ensure some dependencies

--- packages/core-js/es/typed-array/entries.js
+++ packages/core-js/es/typed-array/entries.js
@@ -1,4 +1,6 @@
 'use strict';
 require('../../modules/es.object.to-string');
-require('../../modules/es.array.iterator');
+require('../../modules/es.array.entries');
+require('../../modules/es.array.keys');
+require('../../modules/es.array.values');
 require('../../modules/es.typed-array.iterator');
--- packages/core-js/stable/dom-collections/iterator.js
+++ packages/core-js/stable/dom-collections/iterator.js
@@ -1,5 +1,7 @@
 'use strict';
-require('../../modules/es.array.iterator');
+require('../../modules/es.array.entries');
+require('../../modules/es.array.keys');
+require('../../modules/es.array.values');
 require('../../modules/es.object.to-string');
 require('../../modules/web.dom-collections.iterator');
 var entryUnbind = require('../../internals/entry-unbind');
