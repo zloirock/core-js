f7677006935e8d1a19d63c9237ed9b0a933a34c2
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-06 18:48:57 +0700

    split `es.typed-array.iterator`

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2049,14 +2049,6 @@ export const data = {
     firefox: '55',
     safari: '14.0',
   },
-  'es.typed-array.at': {
-    chrome: '92',
-    firefox: '90',
-    hermes: '0.13',
-    'react-native': '0.71',
-    rhino: '1.7.15',
-    safari: '15.4',
-  },
   'es.typed-array.from': {
     chrome: '54',
     edge: '15',
@@ -2069,6 +2061,7 @@ export const data = {
     firefox: '55',
     safari: '14.0',
   },
+  // TODO: recheck
   'es.typed-array.iterator': {
     chrome: '51',
     edge: '13',
@@ -2076,12 +2069,28 @@ export const data = {
     hermes: '0.1',
     safari: '10.0',
   },
+  'es.typed-array.at': {
+    chrome: '92',
+    firefox: '90',
+    hermes: '0.13',
+    'react-native': '0.71',
+    rhino: '1.7.15',
+    safari: '15.4',
+  },
   'es.typed-array.copy-within': {
     chrome: '45',
     edge: '13',
     firefox: '34',
     hermes: '0.1',
     rhino: '1.9.0',
+    safari: '10.0',
+  },
+  // TODO: recheck
+  'es.typed-array.entries': {
+    chrome: '51',
+    edge: '13',
+    firefox: '37',
+    hermes: '0.1',
     safari: '10.0',
   },
   'es.typed-array.every': {
@@ -2167,6 +2176,14 @@ export const data = {
     firefox: '37',
     hermes: '0.1',
     rhino: '1.9.0',
+    safari: '10.0',
+  },
+  // TODO: recheck
+  'es.typed-array.keys': {
+    chrome: '51',
+    edge: '13',
+    firefox: '37',
+    hermes: '0.1',
     safari: '10.0',
   },
   'es.typed-array.last-index-of': {
@@ -2291,6 +2308,14 @@ export const data = {
     firefox: '51',
     hermes: '0.1',
     rhino: '1.7.13',
+    safari: '10.0',
+  },
+  // TODO: recheck
+  'es.typed-array.values': {
+    chrome: '51',
+    edge: '13',
+    firefox: '37',
+    hermes: '0.1',
     safari: '10.0',
   },
   'es.typed-array.with': {
