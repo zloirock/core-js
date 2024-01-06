edd92280aff0deb7ba80f243dbabb2e8fe1c706e
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-06 21:19:55 +0700

    split `web.dom-collections.iterator`

--- packages/core-js/stable/dom-collections/index.js
+++ packages/core-js/stable/dom-collections/index.js
@@ -1,7 +1,10 @@
 'use strict';
+require('../../modules/es.array.iterator');
 require('../../modules/es.array.entries');
 require('../../modules/es.array.keys');
-require('../../modules/es.array.values');
 require('../../modules/es.object.to-string');
-require('../../modules/web.dom-collections.for-each');
 require('../../modules/web.dom-collections.iterator');
+require('../../modules/web.dom-collections.entries');
+require('../../modules/web.dom-collections.for-each');
+require('../../modules/web.dom-collections.keys');
+require('../../modules/web.dom-collections.values');
