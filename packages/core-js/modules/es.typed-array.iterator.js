6b423a290ff9c8c973bfd1ba3211853c4284dd33
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-06 18:48:57 +0700

    split `es.typed-array.iterator`

--- packages/core-js/modules/es.typed-array.iterator.js
+++ packages/core-js/modules/es.typed-array.iterator.js
@@ -1,50 +1,25 @@
 'use strict';
-var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
 var fails = require('../internals/fails');
 var uncurryThis = require('../internals/function-uncurry-this');
 var exportTypedArrayMethod = require('../internals/export-typed-array-method');
 var aTypedArray = require('../internals/a-typed-array');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
-var getArrayIteratorMethod = function (METHOD_NAME) {
-  // dependency: es.array.entries
-  // dependency: es.array.keys
-  // dependency: es.array.values
-  return uncurryThis(getBuiltInPrototypeMethod('Array', METHOD_NAME));
-};
-
 var ITERATOR = wellKnownSymbol('iterator');
-var arrayValues = getArrayIteratorMethod('values');
-var arrayKeys = getArrayIteratorMethod('keys');
-var arrayEntries = getArrayIteratorMethod('entries');
-var Int8ArrayPrototype = Int8Array.prototype;
+
+// dependency: es.array.iterator
+var arrayMethod = uncurryThis([][ITERATOR]);
+var typedArrayIterator = Int8Array.prototype[ITERATOR];
 
 var GENERIC = !fails(function () {
-  Int8ArrayPrototype[ITERATOR].call([1]);
+  typedArrayIterator.call([1]);
 });
 
-var ITERATOR_IS_VALUES = !!Int8ArrayPrototype
-  && Int8ArrayPrototype.values
-  && Int8ArrayPrototype[ITERATOR] === Int8ArrayPrototype.values
-  && Int8ArrayPrototype.values.name === 'values';
+var PROPER_NAME = typedArrayIterator
+  && typedArrayIterator.name === 'values';
 
-var typedArrayValues = function values() {
-  return arrayValues(aTypedArray(this));
-};
-
-// `%TypedArray%.prototype.entries` method
-// https://tc39.es/ecma262/#sec-%typedarray%.prototype.entries
-exportTypedArrayMethod('entries', function entries() {
-  return arrayEntries(aTypedArray(this));
-}, GENERIC);
-// `%TypedArray%.prototype.keys` method
-// https://tc39.es/ecma262/#sec-%typedarray%.prototype.keys
-exportTypedArrayMethod('keys', function keys() {
-  return arrayKeys(aTypedArray(this));
-}, GENERIC);
-// `%TypedArray%.prototype.values` method
-// https://tc39.es/ecma262/#sec-%typedarray%.prototype.values
-exportTypedArrayMethod('values', typedArrayValues, GENERIC || !ITERATOR_IS_VALUES, { name: 'values' });
 // `%TypedArray%.prototype[@@iterator]` method
-// https://tc39.es/ecma262/#sec-%typedarray%.prototype-@@iterator
-exportTypedArrayMethod(ITERATOR, typedArrayValues, GENERIC || !ITERATOR_IS_VALUES, { name: 'values' });
+// https://tc39.es/ecma262/#sec-%typedarray%.prototype-@@iterator]
+exportTypedArrayMethod(ITERATOR, function values() {
+  return arrayMethod(aTypedArray(this));
+}, GENERIC || !PROPER_NAME, { name: 'values' });
