9d38fdbc3102be51a7aa5a3a467ad803dffcfdcd
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-06 18:48:57 +0700

    split `es.typed-array.iterator`

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1482,10 +1482,7 @@ GLOBAL.tests = {
     try {
       Int8Array.prototype[Symbol.iterator].call([1]);
     } catch (error) {
-      return Int8Array.prototype[Symbol.iterator].name === 'values'
-        && Int8Array.prototype[Symbol.iterator] === Int8Array.prototype.values
-        && Int8Array.prototype.keys
-        && Int8Array.prototype.entries;
+      return Int8Array.prototype[Symbol.iterator].name === 'values';
     }
   },
   'es.typed-array.at': function () {
@@ -1493,6 +1490,13 @@ GLOBAL.tests = {
   },
   'es.typed-array.copy-within': function () {
     return Int8Array.prototype.copyWithin;
+  },
+  'es.typed-array.entries': function () {
+    try {
+      Int8Array.prototype.entries.call([1]);
+    } catch (error) {
+      return Int8Array.prototype.entries;
+    }
   },
   'es.typed-array.every': function () {
     return Int8Array.prototype.every;
@@ -1528,6 +1532,13 @@ GLOBAL.tests = {
   },
   'es.typed-array.join': function () {
     return Int8Array.prototype.join;
+  },
+  'es.typed-array.keys': function () {
+    try {
+      Int8Array.prototype.keys.call([1]);
+    } catch (error) {
+      return Int8Array.prototype.keys;
+    }
   },
   'es.typed-array.last-index-of': function () {
     return Int8Array.prototype.lastIndexOf;
@@ -1606,6 +1617,14 @@ GLOBAL.tests = {
   },
   'es.typed-array.to-sorted': function () {
     return Int8Array.prototype.toSorted;
+  },
+  'es.typed-array.values': function () {
+    try {
+      Int8Array.prototype.values.call([1]);
+    } catch (error) {
+      return Int8Array.prototype.values.name === 'values'
+        && Int8Array.prototype[Symbol.iterator] === Int8Array.prototype.values;
+    }
   },
   'es.typed-array.with': [function () {
     try {
