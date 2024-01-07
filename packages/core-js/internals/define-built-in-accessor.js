72d7071ff758f998cbde303cb83df1c1cccafe63
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-07 20:00:27 +0700

    `make-built-in` `options.{ getter, setter -> prefix }`

--- packages/core-js/internals/define-built-in-accessor.js
+++ packages/core-js/internals/define-built-in-accessor.js
@@ -3,7 +3,7 @@ var makeBuiltIn = require('../internals/make-built-in');
 var defineProperty = require('../internals/object-define-property');
 
 module.exports = function (target, name, descriptor) {
-  if (descriptor.get) makeBuiltIn(descriptor.get, name, { getter: true });
-  if (descriptor.set) makeBuiltIn(descriptor.set, name, { setter: true });
+  if (descriptor.get) makeBuiltIn(descriptor.get, name, { prefix: 'get ' });
+  if (descriptor.set) makeBuiltIn(descriptor.set, name, { prefix: 'set ' });
   return defineProperty.f(target, name, descriptor);
 };
