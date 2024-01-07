3fdac61ccdb8fa9e7bfb25ea1b41722e195d760c
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-07 13:27:02 +0700

    use `String#repeat` as an implicit dependency

--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -609,6 +609,9 @@ GLOBAL.tests = {
   'es.promise.with-resolvers': [PROMISES_SUPPORT, function () {
     return Promise.withResolvers;
   }],
+  'es.string.repeat': function () {
+    return String.prototype.repeat;
+  },
   'es.array.iterator': [SYMBOLS_SUPPORT, function () {
     return [][Symbol.iterator] === [].values
       && [][Symbol.iterator].name === 'values'
