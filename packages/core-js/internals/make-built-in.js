66432d767a7ad9815eec61f728650367e59c9e36
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-08 15:35:01 +0700

    simplify `make-built-in`

--- packages/core-js/internals/make-built-in.js
+++ packages/core-js/internals/make-built-in.js
@@ -1,8 +1,6 @@
 'use strict';
-var fails = require('../internals/fails');
 var isCallable = require('../internals/is-callable');
 var hasOwn = require('../internals/has-own-property');
-var CONFIGURABLE_FUNCTION_NAME = require('../internals/function-name').CONFIGURABLE;
 var inspectSource = require('../internals/inspect-source');
 var InternalStateModule = require('../internals/internal-state');
 
@@ -11,23 +9,18 @@ var getInternalState = InternalStateModule.get;
 var $String = String;
 var defineProperty = Object.defineProperty;
 
-var CONFIGURABLE_LENGTH = !fails(function () {
-  return defineProperty(function () { /* empty */ }, 'length', { value: 8 }).length !== 8;
-});
-
 var TEMPLATE = $String($String).split('String');
 
 var makeBuiltIn = module.exports = function (value, name, options) {
-  if ($String(name).slice(0, 7) === 'Symbol(') {
-    name = '[' + $String(name).replace(/^Symbol\(([^)]*)\).*$/, '$1') + ']';
-  }
+  name = $String(name);
+  if (name.slice(0, 7) === 'Symbol(') name = '[' + name.replace(/^Symbol\(([^)]*)\).*$/, '$1') + ']';
   if (options && options.prefix) name = options.prefix + name;
-  if (!hasOwn(value, 'name') || (CONFIGURABLE_FUNCTION_NAME && value.name !== name)) {
+  if (!hasOwn(value, 'name') || value.name !== name) try {
     defineProperty(value, 'name', { value: name, configurable: true });
-  }
-  if (CONFIGURABLE_LENGTH && options && hasOwn(options, 'arity') && value.length !== options.arity) {
+  } catch (error) { /* empty */ }
+  if (options && hasOwn(options, 'arity') && value.length !== options.arity) try {
     defineProperty(value, 'length', { value: options.arity });
-  }
+  } catch (error) { /* empty */ }
   try {
     if (options && hasOwn(options, 'constructor') && options.constructor) {
       defineProperty(value, 'prototype', { writable: false });
@@ -35,9 +28,8 @@ var makeBuiltIn = module.exports = function (value, name, options) {
     } else if (value.prototype) value.prototype = undefined;
   } catch (error) { /* empty */ }
   var state = enforceInternalState(value);
-  if (!hasOwn(state, 'source')) {
-    state.source = TEMPLATE.join(typeof name == 'string' ? name : '');
-  } return value;
+  if (!hasOwn(state, 'source')) state.source = TEMPLATE.join(name);
+  return value;
 };
 
 // add fake Function#toString for correct work wrapped methods / constructors with methods like LoDash isNative
