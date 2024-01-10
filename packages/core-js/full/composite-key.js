a6a0504e20b664f1880274611338d34b51b5c338
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-10 14:59:19 +0700

    drop keys composition proposal as dead (no activity 5+ years), I hope that it will be resurrected soon

--- packages/core-js/full/composite-key.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('../modules/es.weak-map.constructor');
-require('../modules/esnext.composite-key');
-var path = require('../internals/path');
-
-module.exports = path.compositeKey;
