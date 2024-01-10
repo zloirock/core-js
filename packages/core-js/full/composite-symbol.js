1a44b8251075cab86aec56d9908106456df9ff16
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-10 14:59:19 +0700

    drop keys composition proposal as dead (no activity 5+ years), I hope that it will be resurrected soon

--- packages/core-js/full/composite-symbol.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-require('../modules/es.symbol.constructor');
-require('../modules/es.symbol.for');
-require('../modules/es.weak-map.constructor');
-require('../modules/esnext.composite-symbol');
-var path = require('../internals/path');
-
-module.exports = path.compositeSymbol;
