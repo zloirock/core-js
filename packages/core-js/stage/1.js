04d6b22e41791fb83342195b4b7d6cf30d3c61ef
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-10 14:59:19 +0700

    drop keys composition proposal as dead (no activity 5+ years), I hope that it will be resurrected soon

--- packages/core-js/stage/1.js
+++ packages/core-js/stage/1.js
@@ -6,6 +6,5 @@ require('../proposals/array-unique');
 require('../proposals/collection-methods');
 require('../proposals/collection-of-from');
 require('../proposals/data-view-get-set-uint8-clamped');
-require('../proposals/keys-composition');
 require('../proposals/pattern-matching');
 require('../proposals/string-cooked');
