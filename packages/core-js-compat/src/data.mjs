50de7d9a6f15577cfff0c9dda04c01a058e34960
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2427,18 +2427,8 @@ export const data = {
     firefox: '135', // '132',
     safari: '18.4',
   },
-  'esnext.map.delete-all': {
-  },
   // TODO: Remove from `core-js@4`
   'esnext.map.emplace': {
-  },
-  'esnext.map.every': {
-  },
-  'esnext.map.filter': {
-  },
-  'esnext.map.find': {
-  },
-  'esnext.map.find-key': {
   },
   'esnext.map.from': {
   },
@@ -2450,54 +2440,17 @@ export const data = {
     bun: '1.2.20',
     firefox: '144',
   },
-  'esnext.map.includes': {
-  },
-  'esnext.map.key-by': {
-  },
-  'esnext.map.key-of': {
-  },
-  'esnext.map.map-keys': {
-  },
-  'esnext.map.map-values': {
-  },
-  'esnext.map.merge': {
-  },
   'esnext.map.of': {
   },
-  'esnext.map.reduce': {
-  },
-  'esnext.map.some': {
-  },
-  'esnext.map.update': {
-  },
-  'esnext.math.f16round': null,
   // TODO: Remove from `core-js@4`
   'esnext.math.sum-precise': null,
   'esnext.number.clamp': {
   },
   // TODO: Remove from `core-js@4`
   'esnext.promise.try': null,
-  'esnext.set.add-all': {
-  },
-  'esnext.set.delete-all': {
-  },
-  'esnext.set.every': {
-  },
-  'esnext.set.filter': {
-  },
-  'esnext.set.find': {
-  },
   'esnext.set.from': {
   },
-  'esnext.set.join': {
-  },
-  'esnext.set.map': {
-  },
   'esnext.set.of': {
-  },
-  'esnext.set.reduce': {
-  },
-  'esnext.set.some': {
   },
   'esnext.string.cooked': {
   },
@@ -2533,8 +2486,6 @@ export const data = {
   'esnext.uint8-array.to-base64': null,
   // TODO: Remove from `core-js@4`
   'esnext.uint8-array.to-hex': null,
-  'esnext.weak-map.delete-all': {
-  },
   'esnext.weak-map.from': {
   },
   'esnext.weak-map.of': {
@@ -2549,10 +2500,6 @@ export const data = {
   'esnext.weak-map.get-or-insert-computed': {
     bun: '1.2.20',
     firefox: '144',
-  },
-  'esnext.weak-set.add-all': {
-  },
-  'esnext.weak-set.delete-all': {
   },
   'esnext.weak-set.from': {
   },
--- packages/core-js/full/map/index.js
+++ packages/core-js/full/map/index.js
@@ -2,22 +2,8 @@
 var parent = require('../../actual/map');
 require('../../modules/esnext.map.from');
 require('../../modules/esnext.map.of');
-require('../../modules/esnext.map.key-by');
-require('../../modules/esnext.map.delete-all');
 require('../../modules/esnext.map.emplace');
-require('../../modules/esnext.map.every');
-require('../../modules/esnext.map.filter');
-require('../../modules/esnext.map.find');
-require('../../modules/esnext.map.find-key');
-require('../../modules/esnext.map.includes');
 require('../../modules/esnext.map.get-or-insert');
 require('../../modules/esnext.map.get-or-insert-computed');
-require('../../modules/esnext.map.key-of');
-require('../../modules/esnext.map.map-keys');
-require('../../modules/esnext.map.map-values');
-require('../../modules/esnext.map.merge');
-require('../../modules/esnext.map.reduce');
-require('../../modules/esnext.map.some');
-require('../../modules/esnext.map.update');
 
 module.exports = parent;
