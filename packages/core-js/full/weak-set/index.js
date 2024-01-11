d008b6bf0169e743c55a813ad99f392823a66cd7
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js/full/weak-set/index.js
+++ packages/core-js/full/weak-set/index.js
@@ -1,8 +1,6 @@
 'use strict';
 var parent = require('../../actual/weak-set');
 require('../../modules/es.string.iterator');
-require('../../modules/esnext.weak-set.add-all');
-require('../../modules/esnext.weak-set.delete-all');
 require('../../modules/esnext.weak-set.from');
 require('../../modules/esnext.weak-set.of');
 
