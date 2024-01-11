f103952e6b78e1ce2aa64c4f930b51386f66330e
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js/full/set/index.js
+++ packages/core-js/full/set/index.js
@@ -2,14 +2,5 @@
 var parent = require('../../actual/set');
 require('../../modules/esnext.set.from');
 require('../../modules/esnext.set.of');
-require('../../modules/esnext.set.add-all');
-require('../../modules/esnext.set.delete-all');
-require('../../modules/esnext.set.every');
-require('../../modules/esnext.set.filter');
-require('../../modules/esnext.set.find');
-require('../../modules/esnext.set.join');
-require('../../modules/esnext.set.map');
-require('../../modules/esnext.set.reduce');
-require('../../modules/esnext.set.some');
 
 module.exports = parent;
