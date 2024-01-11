38ba89cf110b0ef8243ef2cf6fbc5b4fe9735f53
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2139,39 +2139,11 @@ export const data = {
     chrome: '114',
     firefox: '132',
   },
-  'esnext.map.delete-all': {
-  },
   'esnext.map.emplace': {
-  },
-  'esnext.map.every': {
-  },
-  'esnext.map.filter': {
-  },
-  'esnext.map.find': {
-  },
-  'esnext.map.find-key': {
   },
   'esnext.map.from': {
   },
-  'esnext.map.includes': {
-  },
-  'esnext.map.key-by': {
-  },
-  'esnext.map.key-of': {
-  },
-  'esnext.map.map-keys': {
-  },
-  'esnext.map.map-values': {
-  },
-  'esnext.map.merge': {
-  },
   'esnext.map.of': {
-  },
-  'esnext.map.reduce': {
-  },
-  'esnext.map.some': {
-  },
-  'esnext.map.update': {
   },
   'esnext.math.f16round': {
     bun: '1.1.23',
@@ -2185,27 +2157,9 @@ export const data = {
   'esnext.regexp.escape': {
     bun: '1.1.22',
   },
-  'esnext.set.add-all': {
-  },
-  'esnext.set.delete-all': {
-  },
-  'esnext.set.every': {
-  },
-  'esnext.set.filter': {
-  },
-  'esnext.set.find': {
-  },
   'esnext.set.from': {
   },
-  'esnext.set.join': {
-  },
-  'esnext.set.map': {
-  },
   'esnext.set.of': {
-  },
-  'esnext.set.reduce': {
-  },
-  'esnext.set.some': {
   },
   'esnext.string.cooked': {
   },
@@ -2259,17 +2213,11 @@ export const data = {
   'esnext.uint8-array.to-hex': {
     bun: '1.1.22',
   },
-  'esnext.weak-map.delete-all': {
-  },
   'esnext.weak-map.from': {
   },
   'esnext.weak-map.of': {
   },
   'esnext.weak-map.emplace': {
-  },
-  'esnext.weak-set.add-all': {
-  },
-  'esnext.weak-set.delete-all': {
   },
   'esnext.weak-set.from': {
   },
--- packages/core-js/full/weak-map/from.js
+++ packages/core-js/full/weak-map/from.js
@@ -4,7 +4,6 @@ require('../../modules/es.array.iterator');
 require('../../modules/es.string.iterator');
 require('../../modules/es.weak-map.constructor');
 require('../../modules/esnext.weak-map.from');
-require('../../modules/esnext.weak-map.delete-all');
 require('../../modules/esnext.weak-map.emplace');
 require('../../modules/web.dom-collections.iterator');
 var path = require('../../internals/path');
