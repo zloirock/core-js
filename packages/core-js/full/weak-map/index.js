5038a69025b0cb5c20d10553c680308670f5bbc2
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js/full/weak-map/index.js
+++ packages/core-js/full/weak-map/index.js
@@ -4,6 +4,5 @@ require('../../modules/es.string.iterator');
 require('../../modules/esnext.weak-map.from');
 require('../../modules/esnext.weak-map.of');
 require('../../modules/esnext.weak-map.emplace');
-require('../../modules/esnext.weak-map.delete-all');
 
 module.exports = parent;
--- packages/core-js/full/weak-set/of.js
+++ packages/core-js/full/weak-set/of.js
@@ -3,8 +3,6 @@ require('../../modules/es.object.to-string');
 require('../../modules/es.array.iterator');
 require('../../modules/es.weak-set.constructor');
 require('../../modules/esnext.weak-set.of');
-require('../../modules/esnext.weak-set.add-all');
-require('../../modules/esnext.weak-set.delete-all');
 var path = require('../../internals/path');
 
 module.exports = path.WeakSet.of;
