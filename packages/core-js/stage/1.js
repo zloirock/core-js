cba16b5207cea6cee0582c84a3b86a807c059e22
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-11 20:34:42 +0700

    drop new collections methods proposal as dead (no activity 6+ years), I hope that it will be resurrected soon

--- packages/core-js/stage/1.js
+++ packages/core-js/stage/1.js
@@ -3,7 +3,6 @@ require('./2');
 
 require('../proposals/array-filtering');
 require('../proposals/array-unique');
-require('../proposals/collection-methods');
 require('../proposals/collection-of-from');
 require('../proposals/data-view-get-set-uint8-clamped');
 require('../proposals/pattern-matching');
