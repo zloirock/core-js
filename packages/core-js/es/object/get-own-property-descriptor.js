838aeca85e53bc2552c38068ae33764ba3ab19b5
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-14 01:23:28 +0700

    drop unneded `.sham` flag check

--- packages/core-js/es/object/get-own-property-descriptor.js
+++ packages/core-js/es/object/get-own-property-descriptor.js
@@ -4,8 +4,6 @@ var path = require('../../internals/path');
 
 var Object = path.Object;
 
-var $getOwnPropertyDescriptor = module.exports = function getOwnPropertyDescriptor(it, key) {
+module.exports = function getOwnPropertyDescriptor(it, key) {
   return Object.getOwnPropertyDescriptor(it, key);
 };
-
-if (Object.getOwnPropertyDescriptor.sham) $getOwnPropertyDescriptor.sham = true;
