6e3b50b9326c04315bafa033106798f597dc11b4
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-19 01:33:34 +0700

    use common export on `Object#toString`

--- packages/core-js/modules/es.object.to-string.js
+++ packages/core-js/modules/es.object.to-string.js
@@ -1,10 +1,10 @@
 'use strict';
+var $ = require('../internals/export');
 var TO_STRING_TAG_SUPPORT = require('../internals/to-string-tag-support');
-var defineBuiltIn = require('../internals/define-built-in');
 var toString = require('../internals/object-to-string');
 
 // `Object.prototype.toString` method
 // https://tc39.es/ecma262/#sec-object.prototype.tostring
-if (!TO_STRING_TAG_SUPPORT) {
-  defineBuiltIn(Object.prototype, 'toString', toString, { unsafe: true });
-}
+$({ target: 'Object', proto: true, forced: !TO_STRING_TAG_SUPPORT }, {
+  toString: toString,
+});
