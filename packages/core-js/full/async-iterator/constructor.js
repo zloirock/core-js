eaea02517c202266eb4419efc21b36c6003033c3
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-26 08:26:41 +0700

    drop /actual/ entries for async iterator helpers since now it's on stage 2

--- packages/core-js/full/async-iterator/constructor.js
+++ packages/core-js/full/async-iterator/constructor.js
@@ -1,20 +1,20 @@
 'use strict';
-require('../../modules/es.array.iterator');
 require('../../modules/es.object.to-string');
 require('../../modules/es.promise.constructor');
 require('../../modules/es.promise.catch');
 require('../../modules/es.promise.finally');
 require('../../modules/es.promise.reject');
 require('../../modules/es.promise.resolve');
+require('../../modules/es.array.iterator');
 require('../../modules/es.string.iterator');
 require('../../modules/esnext.async-iterator.constructor');
+require('../../modules/esnext.async-iterator.async-dispose');
 require('../../modules/esnext.async-iterator.drop');
 require('../../modules/esnext.async-iterator.every');
 require('../../modules/esnext.async-iterator.filter');
 require('../../modules/esnext.async-iterator.find');
 require('../../modules/esnext.async-iterator.flat-map');
 require('../../modules/esnext.async-iterator.for-each');
-require('../../modules/esnext.async-iterator.from');
 require('../../modules/esnext.async-iterator.map');
 require('../../modules/esnext.async-iterator.reduce');
 require('../../modules/esnext.async-iterator.some');
@@ -24,4 +24,4 @@ require('../../modules/web.dom-collections.iterator');
 
 var path = require('../../internals/path');
 
-module.exports = path.AsyncIterator.from;
+module.exports = path.AsyncIterator;
--- packages/core-js/full/iterator/to-async.js
+++ packages/core-js/full/iterator/to-async.js
@@ -1,4 +1,14 @@
 'use strict';
-var parent = require('../../actual/iterator/to-async');
+require('../../modules/es.object.to-string');
+require('../../modules/es.promise.constructor');
+require('../../modules/es.promise.catch');
+require('../../modules/es.promise.finally');
+require('../../modules/es.promise.resolve');
+require('../../modules/es.iterator.constructor');
+// TODO: Drop from `core-js@4`
+require('../../modules/esnext.iterator.constructor');
+require('../../modules/esnext.iterator.to-async');
 
-module.exports = parent;
+var entryUnbind = require('../../internals/entry-unbind');
+
+module.exports = entryUnbind('Iterator', 'toAsync');
