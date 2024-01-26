a3eddb0bcad24515c49a1bc0d0901843e0b4033a
Minseok Choe <choems@outlook.com>
2024-01-26 13:22:34 +0900

    Fix create-property.js

--- packages/core-js/internals/create-property.js
+++ packages/core-js/internals/create-property.js
@@ -5,6 +5,5 @@ var createPropertyDescriptor = require('../internals/create-property-descriptor'
 
 module.exports = function (object, key, value) {
   var propertyKey = toPropertyKey(key);
-  if (propertyKey in object) definePropertyModule.f(object, propertyKey, createPropertyDescriptor(0, value));
-  else object[propertyKey] = value;
+  definePropertyModule.f(object, propertyKey, createPropertyDescriptor(0, value));
 };
