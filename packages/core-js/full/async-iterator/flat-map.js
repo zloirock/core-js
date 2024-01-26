729a6b7be5d3b0779887939835215bf7b37aafc0
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-26 08:26:41 +0700

    drop /actual/ entries for async iterator helpers since now it's on stage 2

--- packages/core-js/full/async-iterator/flat-map.js
+++ packages/core-js/full/async-iterator/flat-map.js
@@ -1,4 +1,15 @@
 'use strict';
-var parent = require('../../actual/async-iterator/flat-map');
+require('../../modules/es.object.to-string');
+require('../../modules/es.promise.constructor');
+require('../../modules/es.promise.catch');
+require('../../modules/es.promise.finally');
+require('../../modules/es.promise.reject');
+require('../../modules/es.promise.resolve');
+require('../../modules/es.array.iterator');
+require('../../modules/es.string.iterator');
+require('../../modules/esnext.async-iterator.flat-map');
+require('../../modules/web.dom-collections.iterator');
 
-module.exports = parent;
+var entryUnbind = require('../../internals/entry-unbind');
+
+module.exports = entryUnbind('AsyncIterator', 'flatMap');
