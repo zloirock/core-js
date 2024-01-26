66280da489b6f928664618764dc6d81f40f99f4d
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-26 08:26:41 +0700

    drop /actual/ entries for async iterator helpers since now it's on stage 2

--- packages/core-js/full/iterator/to-async.js
+++ packages/core-js/full/iterator/to-async.js
@@ -1,4 +1,12 @@
 'use strict';
-var parent = require('../../actual/iterator/to-async');
+require('../../modules/es.object.to-string');
+require('../../modules/es.promise.constructor');
+require('../../modules/es.promise.catch');
+require('../../modules/es.promise.finally');
+require('../../modules/es.promise.resolve');
+require('../../modules/esnext.iterator.constructor');
+require('../../modules/esnext.iterator.to-async');
 
-module.exports = parent;
+var entryUnbind = require('../../internals/entry-unbind');
+
+module.exports = entryUnbind('Iterator', 'toAsync');
