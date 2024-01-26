198caf77afde11ef51c257d3c4f489e81ffa7f5d
Denis Pushkarev <zloirock@zloirock.ru>
2024-01-26 08:36:53 +0700

    drop some extra arity tests from the pure version

--- tests/unit-pure/es.object.define-properties.js
+++ tests/unit-pure/es.object.define-properties.js
@@ -2,7 +2,6 @@ import defineProperties from '@core-js/pure/es/object/define-properties';
 
 QUnit.test('Object.defineProperties', assert => {
   assert.isFunction(defineProperties);
-  assert.arity(defineProperties, 2);
   const source = {};
   const result = defineProperties(source, { q: { value: 42 }, w: { value: 33 } });
   assert.same(result, source);
