0a31960a61f5a6a4a4fa923be8a0c0163f5d06cd
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 04:29:15 +0700

    declare some missed dependencies

--- packages/core-js/modules/esnext.async-iterator.map.js
+++ packages/core-js/modules/esnext.async-iterator.map.js
@@ -49,6 +49,7 @@ var AsyncIteratorProxy = createAsyncIteratorProxy(function (Promise) {
 
 // `AsyncIterator.prototype.map` method
 // https://github.com/tc39/proposal-async-iterator-helpers
+// dependency: esnext.async-iterator.constructor
 $({ target: 'AsyncIterator', proto: true, real: true, forced: true }, {
   map: function map(mapper) {
     anObject(this);
