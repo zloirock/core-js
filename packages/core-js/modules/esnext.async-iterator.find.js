e120ce6bc8ddf96faa3da41353e3cc4fb88dce0d
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 04:29:15 +0700

    declare some missed dependencies

--- packages/core-js/modules/esnext.async-iterator.find.js
+++ packages/core-js/modules/esnext.async-iterator.find.js
@@ -4,6 +4,7 @@ var $find = require('../internals/async-iterator-iteration').find;
 
 // `AsyncIterator.prototype.find` method
 // https://github.com/tc39/proposal-async-iterator-helpers
+// dependency: esnext.async-iterator.constructor
 $({ target: 'AsyncIterator', proto: true, real: true, forced: true }, {
   find: function find(predicate) {
     return $find(this, predicate);
