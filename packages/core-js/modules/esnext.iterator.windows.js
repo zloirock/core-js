87ca03c05896d2cbc558bd4bc156b403f36e19e4
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 04:29:15 +0700

    declare some missed dependencies

--- packages/core-js/modules/esnext.iterator.windows.js
+++ packages/core-js/modules/esnext.iterator.windows.js
@@ -4,6 +4,7 @@ var iteratorWindow = require('../internals/iterator-window');
 
 // `Iterator.prototype.windows` method
 // https://github.com/tc39/proposal-iterator-chunking
+// dependency: es.iterator.constructor
 $({ target: 'Iterator', proto: true, real: true, forced: true }, {
   windows: function windows(windowSize) {
     return iteratorWindow(this, windowSize, false);
