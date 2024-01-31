6d7febbe8180455b8ce4bc9cfb0b0e078ad79100
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 04:29:15 +0700

    declare some missed dependencies

--- packages/core-js/modules/es.iterator.zip-keyed.js
+++ packages/core-js/modules/es.iterator.zip-keyed.js
@@ -19,6 +19,7 @@ var THROW = 'throw';
 
 // `Iterator.zipKeyed` method
 // https://github.com/tc39/proposal-joint-iteration
+// dependency: es.iterator.constructor
 $({ target: 'Iterator', stat: true, forced: IS_PURE }, {
   zipKeyed: function zipKeyed(iterables /* , options */) {
     anObject(iterables);
