3da6a491308e5bd3235856175a64f8d7cb7413a2
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 01:59:08 +0700

    don't use `defineProperty` abstraction in `create-property` helper in IE8-

--- packages/core-js/internals/create-property.js
+++ packages/core-js/internals/create-property.js
@@ -1,9 +1,9 @@
 'use strict';
-var toPropertyKey = require('../internals/to-property-key');
+var DESCRIPTORS = require('../internals/descriptors');
 var definePropertyModule = require('../internals/object-define-property');
 var createPropertyDescriptor = require('../internals/create-property-descriptor');
 
 module.exports = function (object, key, value) {
-  var propertyKey = toPropertyKey(key);
-  definePropertyModule.f(object, propertyKey, createPropertyDescriptor(0, value));
+  if (DESCRIPTORS) definePropertyModule.f(object, key, createPropertyDescriptor(0, value));
+  else object[key] = value;
 };
