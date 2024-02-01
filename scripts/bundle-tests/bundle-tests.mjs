8935824df668805b2823d589e0b90f6f8ce7ad58
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-02 00:36:00 +0700

    move tests indexes building to `bundle-tests` script

--- scripts/bundle-tests/bundle-tests.mjs
+++ scripts/bundle-tests/bundle-tests.mjs
@@ -1,3 +1,17 @@
+async function generateTestsIndex(name, pkg) {
+  const dir = `../../tests/${ name }`;
+  const files = await fs.readdir(dir);
+  return fs.writeFile(`${ dir }/index.js`, `import '../helpers/qunit-helpers';\n\n${ files
+    .filter(it => /^(?:es|esnext|web)\./.test(it))
+    .map(it => `import './${ it.slice(0, -3) }';\n`)
+    .join('') }${ pkg !== 'core-js' ? `\nimport core from '${ pkg }';\ncore.globalThis.core = core;\n` : '' }`);
+}
+
+await generateTestsIndex('unit-global', 'core-js');
+await generateTestsIndex('unit-pure', '@core-js/pure');
+
+echo(chalk.green('tests indexes generated'));
+
 await Promise.all([
   ['unit-global/index', 'unit-global'],
   ['unit-pure/index', 'unit-pure'],
@@ -10,3 +24,5 @@ fs.copyFile('../../packages/core-js-bundle/index.js', '../../tests/bundles/core-
 
 fs.copyFile('./node_modules/@slowcheetah/qunitjs-1/qunit/qunit.js', '../../tests/bundles/qunit.js');
 fs.copyFile('./node_modules/@slowcheetah/qunitjs-1/qunit/qunit.css', '../../tests/bundles/qunit.css');
+
+echo(chalk.green('tests bundled'));
