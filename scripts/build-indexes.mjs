db8f29bab0dd63ef99e8232940a7a72d2e8d8248
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- scripts/build-indexes.mjs
+++ scripts/build-indexes.mjs
@@ -1,12 +1,3 @@
-import { modules } from '@core-js/compat/src/data.mjs';
-
-async function generateNamespaceIndex(ns, filter) {
-  return fs.writeFile(`./packages/core-js/${ ns }/index.js`, `'use strict';\n${ modules
-    .filter(it => filter.test(it))
-    .map(it => `require('../modules/${ it }');\n`)
-    .join('') }\nmodule.exports = require('../internals/path');\n`);
-}
-
 async function generateTestsIndex(name, pkg) {
   const dir = `tests/${ name }`;
   const files = await fs.readdir(dir);
@@ -15,10 +6,6 @@ async function generateTestsIndex(name, pkg) {
     .map(it => `import './${ it.slice(0, -3) }';\n`)
     .join('') }${ pkg !== 'core-js' ? `\nimport core from '${ pkg }';\ncore.globalThis.core = core;\n` : '' }`);
 }
-
-await generateNamespaceIndex('es', /^es\./);
-await generateNamespaceIndex('stable', /^(?:es|web)\./);
-await generateNamespaceIndex('full', /^(?:es|esnext|web)\./);
 
 await generateTestsIndex('unit-global', 'core-js');
 await generateTestsIndex('unit-pure', '@core-js/pure');
