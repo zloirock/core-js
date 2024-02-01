eeab7c5970d96afcdda55e0a58c0a37203125553
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/full/map/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-var parent = require('../../actual/map');
-require('../../modules/esnext.map.from');
-require('../../modules/esnext.map.of');
-require('../../modules/esnext.map.emplace');
-require('../../modules/esnext.map.get-or-insert');
-require('../../modules/esnext.map.get-or-insert-computed');
-
-module.exports = parent;
