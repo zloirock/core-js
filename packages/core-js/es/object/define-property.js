a1b91a8c58f73b1a9863583b4c4018de29502a20
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/es/object/define-property.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var getBuiltInStaticMethod = require('../../internals/get-built-in-static-method');
-
-module.exports = function create(it, key, desc) {
-  return getBuiltInStaticMethod('Object', 'defineProperty')(it, key, desc);
-};
--- scripts/clean-dependencies.mjs
+++ scripts/clean-dependencies.mjs
@@ -1,7 +1,6 @@
 import { readdir, rm } from 'node:fs/promises';
 
 const ignore = new Set([
-  'scripts/build-entries',
   'scripts/check-actions',
   'scripts/usage',
   'tests/test262',
