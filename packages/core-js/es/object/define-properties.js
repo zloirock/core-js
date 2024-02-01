5de78757757c885e8c42e75b661a93659e9ae7ec
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/es/object/define-properties.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var getBuiltInStaticMethod = require('../../internals/get-built-in-static-method');
-
-module.exports = function defineProperties(T, D) {
-  return getBuiltInStaticMethod('Object', 'defineProperties')(T, D);
-};
--- packages/core-js/es/string/iterator.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var uncurryThis = require('../../internals/function-uncurry-this');
-var getIteratorMethod = require('../../internals/get-iterator-method');
-require('../../modules/es.object.to-string');
-require('../../modules/es.string.iterator');
-
-module.exports = uncurryThis(getIteratorMethod(''));
