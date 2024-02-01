b45c3e867cb2a1d0bd3855ac283ece124dff9ab5
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/es/weak-map/get-or-insert.js
+++ packages/core-js/es/weak-map/get-or-insert.js
@@ -1,6 +1,10 @@
 'use strict';
+require('../../modules/es.object.to-string');
+require('../../modules/es.array.iterator');
+require('../../modules/es.string.iterator');
 require('../../modules/es.weak-map.constructor');
 require('../../modules/es.weak-map.get-or-insert');
+
 var entryUnbind = require('../../internals/entry-unbind');
 
 module.exports = entryUnbind('WeakMap', 'getOrInsert');
--- packages/core-js/full/set/is-superset-of.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-var parent = require('../../actual/set/is-superset-of');
-
-module.exports = parent;
