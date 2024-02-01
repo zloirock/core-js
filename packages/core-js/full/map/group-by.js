6c8e63006710458fe695837270dacfe24182c167
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/full/map/group-by.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-var parent = require('../../actual/map/group-by');
-require('../../modules/esnext.map.emplace');
-require('../../modules/esnext.map.get-or-insert');
-require('../../modules/esnext.map.get-or-insert-computed');
-
-module.exports = parent;
--- packages/core-js/stage/3.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-require('../proposals/array-buffer-base64');
-require('../proposals/decorator-metadata');
-require('../proposals/json-parse-with-source');
-require('../proposals/math-sum');
