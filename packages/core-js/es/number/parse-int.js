9f01efe026d8fdb63e2d456a722f9aaaa8a56386
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/es/number/parse-int.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('../../modules/es.parse-int');
-require('../../modules/es.number.parse-int');
-var path = require('../../internals/path');
-
-module.exports = path.Number.parseInt;
