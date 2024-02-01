c8e6afef5d3b47a0fd882907450f82ee5f34ab35
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-01 10:52:08 +0700

    use generated entries

--- packages/core-js/stage/3.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-require('../proposals/decorator-metadata');
-require('../proposals/iterator-sequencing');
-require('../proposals/json-parse-with-source');
-require('../proposals/map-upsert');
