ae7ba5ae5399c35df2a6b112a6a46bf98bc1c0fc
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-19 03:03:30 +0700

    rework building entries

--- packages/core-js/es/regexp/escape.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-require('../../modules/es.string.repeat');
-require('../../modules/es.regexp.escape');
-
-var getBuiltInStaticMethod = require('../../internals/get-built-in-static-method');
-
-module.exports = getBuiltInStaticMethod('RegExp', 'escape');
--- packages/core-js/proposals/iterator-chunking.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-require('../modules/es.object.to-string');
-require('../modules/es.iterator.constructor');
-require('../modules/esnext.iterator.chunks');
-require('../modules/esnext.iterator.windows');
