1e6ab585f93d8088d884fcb82ada2705de5dbf25
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-19 03:03:30 +0700

    rework building entries

--- tests/unit-bun/runner.mjs
+++ tests/unit-bun/runner.mjs
@@ -2,6 +2,6 @@ if (await which('bun', { nothrow: true })) {
   await Promise.all([
     ['packages/core-js/full/index', 'tests/bundles/unit-global'],
     ['packages/core-js-bundle/index', 'tests/bundles/unit-global'],
-    // ['tests/bundles/unit-pure'],
+    ['tests/bundles/unit-pure'],
   ].map(files => $`bun qunit ${ files.map(file => `../../${ file }.js`) }`));
 } else echo(chalk.cyan('bun is not found'));
