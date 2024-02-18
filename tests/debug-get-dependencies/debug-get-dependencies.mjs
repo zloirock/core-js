747b26dd56a4920ebf9b0e12e1e77eca4e368db7
Denis Pushkarev <zloirock@zloirock.ru>
2024-02-19 00:36:42 +0700

    move `debug-get-dependencies` to tests folder

--- tests/debug-get-dependencies/debug-get-dependencies.mjs
+++ tests/debug-get-dependencies/debug-get-dependencies.mjs
@@ -1,4 +1,4 @@
 import { modules } from '@core-js/compat/src/data.mjs';
-import { getListOfDependencies } from './get-dependencies.mjs';
+import { getListOfDependencies } from '../../scripts/build-entries/get-dependencies.mjs';
 
 for (const module of modules) console.log(module, await getListOfDependencies([module]));
