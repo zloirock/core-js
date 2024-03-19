b5cb86ba228c6ffb8ceff74eb20e310f78ee9689
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-19 15:00:08 +0700

    change the style of `$helper` entry template usage

--- scripts/build-entries/entries-definitions.mjs
+++ scripts/build-entries/entries-definitions.mjs
@@ -1595,7 +1595,8 @@ export const features = {
   },
   'regexp/flags': {
     modules: ['es.regexp.flags'],
-    template: $helper({ name: 'regexp-get-flags' }),
+    template: $helper,
+    helper: 'regexp-get-flags',
   },
   'regexp/match': {
     modules: ['es.string.match'],
@@ -2461,15 +2462,18 @@ export const features = {
   },
   'get-iterator': {
     modules: ['es.array.iterator', 'es.string.iterator', 'web.dom-collections.iterator'],
-    template: $helper({ name: 'get-iterator' }),
+    template: $helper,
+    helper: 'get-iterator',
   },
   'get-iterator-method': {
     modules: ['es.array.iterator', 'es.string.iterator', 'web.dom-collections.iterator'],
-    template: $helper({ name: 'get-iterator-method' }),
+    template: $helper,
+    helper: 'get-iterator-method',
   },
   'is-iterable': {
     modules: ['es.array.iterator', 'es.string.iterator', 'web.dom-collections.iterator'],
-    template: $helper({ name: 'is-iterable' }),
+    template: $helper,
+    helper: 'is-iterable',
   },
 };
 
