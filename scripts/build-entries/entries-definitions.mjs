faefb342f5d5ee4b1db5174004c212e9cc0fc937
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-19 15:43:56 +0700

    change the style of `$virtualIterator` entry template usage

--- scripts/build-entries/entries-definitions.mjs
+++ scripts/build-entries/entries-definitions.mjs
@@ -302,7 +302,8 @@ export const features = {
   },
   'array/virtual/iterator': {
     modules: ['es.array.iterator'],
-    template: $virtualIterator({ source: '[]' }),
+    template: $virtualIterator,
+    source: '[]',
   },
   'array/join': {
     modules: ['es.array.join'],
@@ -1789,7 +1790,8 @@ export const features = {
   },
   'string/virtual/iterator': {
     modules: ['es.string.iterator'],
-    template: $virtualIterator({ source: "''" }),
+    template: $virtualIterator,
+    source: "''",
   },
   'string/link': {
     modules: ['es.string.link'],
