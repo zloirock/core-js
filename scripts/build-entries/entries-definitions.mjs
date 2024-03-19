e6c8e5d370a731e6a6e0d252f2884de7c0258c1a
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-19 15:25:23 +0700

    change the style of `$prototypeIterator` entry template usage

--- scripts/build-entries/entries-definitions.mjs
+++ scripts/build-entries/entries-definitions.mjs
@@ -297,7 +297,8 @@ export const features = {
   },
   'array/iterator': {
     modules: ['es.array.iterator'],
-    template: $prototypeIterator({ source: '[]' }),
+    template: $prototypeIterator,
+    source: '[]',
   },
   'array/virtual/iterator': {
     modules: ['es.array.iterator'],
@@ -1783,7 +1784,8 @@ export const features = {
   },
   'string/iterator': {
     modules: ['es.string.iterator'],
-    template: $prototypeIterator({ source: "''" }),
+    template: $prototypeIterator,
+    source: "''",
   },
   'string/virtual/iterator': {
     modules: ['es.string.iterator'],
