a57225004af749b799da57d967af8fc26daf5a26
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-20 09:09:32 +0700

    fix a path

--- tests/unit-bun/runner.mjs
+++ tests/unit-bun/runner.mjs
@@ -2,4 +2,4 @@ await Promise.all([
   ['packages/core-js/index', 'tests/bundles/unit-global'],
   ['packages/core-js/index', 'packages/core-js-bundle/index', 'tests/bundles/unit-global'],
   ['tests/bundles/unit-pure'],
-].map(files => $`bun ./tests/unit-node/node_modules/.bin/qunit ${ files.map(file => `${ file }.js`) }`));
+].map(files => $`bun ./tests/unit-bun/node_modules/.bin/qunit ${ files.map(file => `${ file }.js`) }`));
