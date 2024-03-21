c5f16d7063092c4f33d7becb0dfbf7563fd5d212
Denis Pushkarev <zloirock@zloirock.ru>
2024-03-21 12:14:11 +0700

    update dependencies

--- packages/core-js/modules/es.data-view.constructor.js
+++ packages/core-js/modules/es.data-view.constructor.js
@@ -5,6 +5,7 @@ var setPrototypeOf = require('../internals/object-set-prototype-of-simple');
 var inheritIfRequired = require('../internals/inherit-if-required');
 var fails = require('../internals/fails');
 
+var NativeArrayBuffer = ArrayBuffer;
 var NativeDataView = DataView;
 var DataViewPrototype = NativeDataView.prototype;
 var ObjectPrototype = Object.prototype;
@@ -14,25 +15,30 @@ if (Object.getPrototypeOf(DataViewPrototype) !== ObjectPrototype) {
   setPrototypeOf(DataViewPrototype, ObjectPrototype);
 }
 
-var NEW_REQUIRED = fails(function () {
-  NativeDataView(new ArrayBuffer(1));
+var PROPER_UNDEFINED_HANDLING = !fails(function () {
+  return new NativeDataView(new NativeArrayBuffer(1), undefined, undefined).byteLength !== 1;
 });
+
+var NEW_REQUIRED = fails(function () {
+  // eslint-disable-next-line sonarjs/inconsistent-function-call -- required for testing
+  NativeDataView(new NativeArrayBuffer(1));
+});
+
+var WRAP = !PROPER_UNDEFINED_HANDLING || !NEW_REQUIRED;
 
 var $DataView = function DataView(buffer) {
   anInstance(this, DataViewPrototype);
-  var view;
-  switch (arguments.length) {
-    case 1: view = new NativeDataView(buffer); break;
-    case 2: view = new NativeDataView(buffer, arguments[1]); break;
-    default: view = new NativeDataView(buffer, arguments[1], arguments[2]);
-  }
+  var length = arguments.length;
+  var view = length > 2 && arguments[2] !== undefined ? new NativeDataView(buffer, arguments[1], arguments[2])
+    : length > 1 && arguments[1] !== undefined ? new NativeDataView(buffer, arguments[1])
+    : new NativeDataView(buffer);
   return inheritIfRequired(view, this, DataViewPrototype);
 };
 
 $DataView.prototype = DataViewPrototype;
 
-if (!NEW_REQUIRED) DataViewPrototype.constructor = $DataView;
+if (WRAP) DataViewPrototype.constructor = $DataView;
 
-$({ global: true, constructor: true, forced: !NEW_REQUIRED }, {
+$({ global: true, constructor: true, forced: WRAP }, {
   DataView: $DataView,
 });
