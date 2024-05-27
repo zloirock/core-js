fa30e1821e382ce7dad408060f3f26716bd36d0a
Denis Pushkarev <zloirock@zloirock.ru>
2024-05-28 02:51:13 +0700

    enable `duplicate-named-capturing-groups-regex` transform

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   // use transforms which does not use ES5+ builtins
   plugins: [
+    ['@babel/proposal-duplicate-named-capturing-groups-regex'],
     ['@babel/transform-member-expression-literals'],
     ['@babel/transform-property-literals'],
     ['@babel/transform-arrow-functions'],
