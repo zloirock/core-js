918357ea81af1dece60a9f81bf39e623acac9930
Denis Pushkarev <zloirock@zloirock.ru>
2024-06-05 08:43:59 +0700

    make qunit bun adapter properly work and add it to common tests workflow

--- tests/unit-bun/runner.mjs
+++ tests/unit-bun/runner.mjs
@@ -1,5 +1,8 @@
-await Promise.all([
-  ['packages/core-js/index', 'tests/bundles/unit-global'],
-  ['packages/core-js/index', 'packages/core-js-bundle/index', 'tests/bundles/unit-global'],
-  ['tests/bundles/unit-pure'],
-].map(files => $`bun ./tests/unit-bun/node_modules/.bin/qunit ${ files.map(file => `${ file }.js`) }`));
+if (await which('bun', { nothrow: true })) {
+  await Promise.all([
+    // some cases loading from modules works incorrectly in Bun, so test only bundled core-js version
+    // ['packages/core-js/index', 'tests/bundles/unit-global'],
+    ['packages/core-js-bundle/index', 'tests/bundles/unit-global'],
+    ['tests/bundles/unit-pure'],
+  ].map(files => $`bun ./tests/unit-bun/node_modules/.bin/qunit ${ files.map(file => `${ file }.js`) }`));
+} else echo(chalk.cyan('bun is not found'));
