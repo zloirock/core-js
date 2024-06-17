fe8ac842bdec5232f8331e7070ef8ed60d49297d
Denis Pushkarev <zloirock@zloirock.ru>
2024-06-17 17:44:54 +0700

    fix a typo

--- packages/core-js/internals/get-built-in-node-module.js
+++ packages/core-js/internals/get-built-in-node-module.js
@@ -3,7 +3,7 @@ var IS_NODE = require('../internals/engine-is-node');
 var globalThis = require('../internals/global');
 
 module.exports = function (name) {
-  if (!IS_NODE) {
+  if (IS_NODE) {
     try {
       return globalThis.process.getBuiltinModule(name);
     } catch (error) { /* empty */ }
