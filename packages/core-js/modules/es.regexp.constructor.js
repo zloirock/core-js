6b83ac16c6caab2c1fcbe76f1e8602087a2dcf23
Alex <UlopLT@gmail.com>
2024-07-02 14:15:49 +0300

    Fix `es.regexp.constructor - handleNCG`  - consider captured groups inside non-capturing groups (#1352)
    Co-authored-by: alazarev <alazarev@megaputer.ru>

--- packages/core-js/modules/es.regexp.constructor.js
+++ packages/core-js/modules/es.regexp.constructor.js
@@ -95,11 +95,13 @@ var handleNCG = function (string) {
         brackets = true;
         break;
       case chr === '(':
-        if (exec(IS_NCG, stringSlice(string, index + 1))) {
+        result += chr;
+        if (stringSlice(string, index + 1, index + 3) === '?:') { // avoid groupid increment in non-capturing group
+          continue;
+        } else if (exec(IS_NCG, stringSlice(string, index + 1))) {
           index += 2;
           ncg = true;
         }
-        result += chr;
         groupid++;
         continue;
       case chr === '>' && ncg:
