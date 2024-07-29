b3fc49fec10e522d48d76378cb8cec56c574a1b0
Denis Pushkarev <zloirock@zloirock.ru>
2024-07-30 00:19:27 +0700

    drop `postinstall`

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -2361,12 +2361,6 @@ export default [
   },
   {
     files: [
-      '**/postinstall.js',
-    ],
-    rules: disable(forbidES5BuiltIns),
-  },
-  {
-    files: [
       'packages/core-js?(-pure)/**/instance/**',
     ],
     rules: {
