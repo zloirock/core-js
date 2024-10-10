54b5a9c0df4eb6a2fe5266c334c1133062d7b5f2
Denis Pushkarev <zloirock@zloirock.ru>
2024-10-10 11:20:19 +0700

    drop legacy `Promise.try` modules

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2153,8 +2153,6 @@ export const data = {
   },
   'esnext.math.sum-precise': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.promise.try': null,
   'esnext.regexp.escape': {
     bun: '1.1.22',
     firefox: '134',
@@ -2456,8 +2454,7 @@ export const data = {
 };
 
 export const renamed = new Map([
-  // TODO: Clean in `core-js@4`
-  ['esnext.promise.try', 'es.promise.try'],
+  // empty
 ]);
 
 for (const [old, nw] of renamed) data[old] = data[nw];
