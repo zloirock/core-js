b2d5bab527e028481f0271f28ef40ca7c386fe49
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2024-11-11 14:04:46 +1030

    Don't use exponentiation operator + lint

--- packages/core-js/modules/esnext.data-view.set-float16.js
+++ packages/core-js/modules/esnext.data-view.set-float16.js
@@ -12,21 +12,24 @@ var roundTiesToEven = function (n) {
   return n + INVERSE_EPSILON - INVERSE_EPSILON;
 };
 
-const minInfinity16 = (2 - 2 ** -11) * 2 ** 15, minNormal16 = (1 - 2 ** -11) * 2 ** -14, recMinSubnormal16 = 2 ** 10 * 2 ** 14, recSignificandDenom16 = 2 ** 10;
+const minInfinity16 = 65520; // (2 - 2 ** -11) * 2 ** 15
+const minNormal16 = 0.000061005353927612305; // (1 - 2 ** -11) * 2 ** -14
+const recMinSubnormal16 = 16777216; // 2 ** 10 * 2 ** 14
+const recSignificandDenom16 = 1024; // 2 ** 10;
 
 function packFloat16(value) {
-    if (Number.isNaN(value)) return 0x7e00; // NaN
-    if (value === 0) return (1 / value === -Infinity) << 15; // +0 or -0
-    const neg = value < 0;
-    if (neg) value = -value;
-    if (value >= minInfinity16) return neg << 15 | 0x7c00; // Infinity
-    if (value < minNormal16) return neg << 15 | roundTiesToEven(value * recMinSubnormal16); // subnormal
-    // normal
-    const exponent = Math.log2(value) | 0;
-    if (exponent === -15) return neg << 15 | recSignificandDenom16; // we round from a value between 2 ** -15 * (1 + 1022/1024) (the largest subnormal) and 2 ** -14 * (1 + 0/1024) (the smallest normal) to the latter (former impossible because of the subnormal check above)
-    const significand = roundTiesToEven((value * 2 ** -exponent - 1) * recSignificandDenom16);
-    if (significand === recSignificandDenom16) return neg << 15 | exponent + 16 << 10; // we round from a value between 2 ** n * (1 + 1023/1024) and 2 ** (n + 1) * (1 + 0/1024) to the latter
-    return neg << 15 | exponent + 15 << 10 | significand;
+  if (Number.isNaN(value)) return 0x7e00; // NaN
+  if (value === 0) return (1 / value === -Infinity) << 15; // +0 or -0
+  const neg = value < 0;
+  if (neg) value = -value;
+  if (value >= minInfinity16) return neg << 15 | 0x7c00; // Infinity
+  if (value < minNormal16) return neg << 15 | roundTiesToEven(value * recMinSubnormal16); // subnormal
+  // normal
+  const exponent = Math.log2(value) | 0;
+  if (exponent === -15) return neg << 15 | recSignificandDenom16; // we round from a value between 2 ** -15 * (1 + 1022/1024) (the largest subnormal) and 2 ** -14 * (1 + 0/1024) (the smallest normal) to the latter (former impossible because of the subnormal check above)
+  const significand = roundTiesToEven((value * Math.pow(2, -exponent) - 1) * recSignificandDenom16);
+  if (significand === recSignificandDenom16) return neg << 15 | exponent + 16 << 10; // we round from a value between 2 ** n * (1 + 1023/1024) and 2 ** (n + 1) * (1 + 0/1024) to the latter
+  return neg << 15 | exponent + 15 << 10 | significand;
 }
 
 // eslint-disable-next-line es/no-typed-arrays -- safe
