b809a971f2424bdf1ac67fd3e4d0e43857bb03b4
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2024-11-11 15:26:10 +1030

    Case + Math.pow cache

--- packages/core-js/modules/esnext.data-view.get-float16.js
+++ packages/core-js/modules/esnext.data-view.get-float16.js
@@ -2,18 +2,20 @@
 var $ = require('../internals/export');
 var uncurryThis = require('../internals/function-uncurry-this');
 
-var expMask16 = 31; // 2 ** 5 - 1
-var significandMask16 = 1023; // 2 ** 10 - 1
-var minSubnormal16 = Math.pow(2, -24); // 2 ** -10 * 2 ** -14
-var significandDenom16 = 0.0009765625; // 2 ** -10
+var pow = Math.pow;
+
+var EXP_MASK16 = 31; // 2 ** 5 - 1
+var SIGNIFICAND_MASK16 = 1023; // 2 ** 10 - 1
+var MIN_SUBNORMAL16 = pow(2, -24); // 2 ** -10 * 2 ** -14
+var SIGNIFICAND_DENOM16 = 0.0009765625; // 2 ** -10
 
 function unpackFloat16(bytes) {
   var sign = bytes >>> 15;
-  var exponent = bytes >>> 10 & expMask16;
-  var significand = bytes & significandMask16;
-  if (exponent === expMask16) return significand === 0 ? (sign === 0 ? Infinity : -Infinity) : NaN;
-  if (exponent === 0) return significand * (sign === 0 ? minSubnormal16 : -minSubnormal16);
-  return Math.pow(2, exponent - 15) * (sign === 0 ? 1 + significand * significandDenom16 : -1 - significand * significandDenom16);
+  var exponent = bytes >>> 10 & EXP_MASK16;
+  var significand = bytes & SIGNIFICAND_MASK16;
+  if (exponent === EXP_MASK16) return significand === 0 ? (sign === 0 ? Infinity : -Infinity) : NaN;
+  if (exponent === 0) return significand * (sign === 0 ? MIN_SUBNORMAL16 : -MIN_SUBNORMAL16);
+  return pow(2, exponent - 15) * (sign === 0 ? 1 + significand * SIGNIFICAND_DENOM16 : -1 - significand * SIGNIFICAND_DENOM16);
 }
 
 // eslint-disable-next-line es/no-typed-arrays -- safe
