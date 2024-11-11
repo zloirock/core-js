6e34f8d2c714daa328ffa4184989080d9ba2e0e3
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2024-11-11 15:29:31 +1030

    Functions

--- packages/core-js/modules/esnext.data-view.get-float16.js
+++ packages/core-js/modules/esnext.data-view.get-float16.js
@@ -9,14 +9,14 @@ var SIGNIFICAND_MASK16 = 1023; // 2 ** 10 - 1
 var MIN_SUBNORMAL16 = pow(2, -24); // 2 ** -10 * 2 ** -14
 var SIGNIFICAND_DENOM16 = 0.0009765625; // 2 ** -10
 
-function unpackFloat16(bytes) {
+var unpackFloat16 = function (bytes) {
   var sign = bytes >>> 15;
   var exponent = bytes >>> 10 & EXP_MASK16;
   var significand = bytes & SIGNIFICAND_MASK16;
   if (exponent === EXP_MASK16) return significand === 0 ? (sign === 0 ? Infinity : -Infinity) : NaN;
   if (exponent === 0) return significand * (sign === 0 ? MIN_SUBNORMAL16 : -MIN_SUBNORMAL16);
   return pow(2, exponent - 15) * (sign === 0 ? 1 + significand * SIGNIFICAND_DENOM16 : -1 - significand * SIGNIFICAND_DENOM16);
-}
+};
 
 // eslint-disable-next-line es/no-typed-arrays -- safe
 var getUint16 = uncurryThis(DataView.prototype.getUint16);
