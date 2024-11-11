4b280d4a146cfc09dcd01c235e35f99464121025
Levi Pesin <35454228+LeviPesin@users.noreply.github.com>
2024-11-11 14:02:07 +1030

    Don't use exponentiation operator + lint

--- packages/core-js/modules/esnext.data-view.get-float16.js
+++ packages/core-js/modules/esnext.data-view.get-float16.js
@@ -2,15 +2,18 @@
 var $ = require('../internals/export');
 var uncurryThis = require('../internals/function-uncurry-this');
 
-const expMask16 = 2 ** 5 - 1, significandMask16 = 2 ** 10 - 1, minSubnormal16 = 2 ** -10 * 2 ** -14, significandDenom16 = 2 ** -10;
+const expMask16 = 31; // 2 ** 5 - 1
+const significandMask16 = 1023; // 2 ** 10 - 1
+const minSubnormal16 = Math.pow(2, -24); // 2 ** -10 * 2 ** -14
+const significandDenom16 = 0.0009765625; // 2 ** -10
 
 function unpackFloat16(bytes) {
-	const sign = bytes >>> 15;
-	const exponent = bytes >>> 10 & expMask16;
-	const significand = bytes & significandMask16;
-	if (exponent === expMask16) return significand === 0 ? (sign === 0 ? Infinity : -Infinity) : NaN;
-	if (exponent === 0) return significand * (sign === 0 ? minSubnormal16 : -minSubnormal16);
-	return 2 ** (exponent - 15) * (sign === 0 ? 1 + significand * significandDenom16 : -1 - significand * significandDenom16);
+  const sign = bytes >>> 15;
+  const exponent = bytes >>> 10 & expMask16;
+  const significand = bytes & significandMask16;
+  if (exponent === expMask16) return significand === 0 ? (sign === 0 ? Infinity : -Infinity) : NaN;
+  if (exponent === 0) return significand * (sign === 0 ? minSubnormal16 : -minSubnormal16);
+  return Math.pow(2, exponent - 15) * (sign === 0 ? 1 + significand * significandDenom16 : -1 - significand * significandDenom16);
 }
 
 // eslint-disable-next-line es/no-typed-arrays -- safe
