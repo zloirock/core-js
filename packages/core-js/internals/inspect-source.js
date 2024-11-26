bb0d2a2a263229c9fcd69cc2b840d3854d0edb0d
Roshan Sharma <120376468+RoshanSharma8245@users.noreply.github.com>
2024-11-26 18:01:13 +0530

    Update inspect-source.js
    Solved
    RangeError: Maximum call stack size exceeded (native stack depth), js engine: hermes

--- packages/core-js/internals/inspect-source.js
+++ packages/core-js/internals/inspect-source.js
@@ -3,12 +3,17 @@ var uncurryThis = require('../internals/function-uncurry-this');
 var isCallable = require('../internals/is-callable');
 var store = require('../internals/shared-store');
 
-var functionToString = uncurryThis(Function.toString);
+var functionToString = uncurryThis(Function.prototype.toString);
 
-// this helper broken in `core-js@3.4.1-3.4.4`, so we can't use `shared` helper
+// Avoid recursion in `store.inspectSource`
 if (!isCallable(store.inspectSource)) {
   store.inspectSource = function (it) {
-    return functionToString(it);
+    try {
+      return isCallable(it) ? functionToString(it) : '';
+    } catch (error) {
+      // Fallback to an empty string if `functionToString` fails
+      return '';
+    }
   };
 }
 
