900d32d795c5de95412a969d69e9187cae997588
Roshan Sharma <120376468+RoshanSharma8245@users.noreply.github.com>
2024-11-27 12:29:29 +0530

    remove prototype keyword as the older one works

--- packages/core-js/internals/inspect-source.js
+++ packages/core-js/internals/inspect-source.js
@@ -3,7 +3,7 @@ var uncurryThis = require('../internals/function-uncurry-this');
 var isCallable = require('../internals/is-callable');
 var store = require('../internals/shared-store');
 
-var functionToString = uncurryThis(Function.prototype.toString);
+var functionToString = uncurryThis(Function.toString);
 
 // this helper broken in `core-js@3.4.1-3.4.4`, so we can't use `shared` helper
 if (!isCallable(store.inspectSource)) {
