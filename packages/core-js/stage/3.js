594ecedbe16820613998a53384fd54a2769847d5
Denis Pushkarev <zloirock@zloirock.ru>
2024-12-03 00:20:22 +0700

    move iterator sequencing proposal to stage 3

--- packages/core-js/stage/3.js
+++ packages/core-js/stage/3.js
@@ -6,6 +6,7 @@ require('../proposals/array-from-async-stage-2');
 require('../proposals/decorator-metadata-v2');
 require('../proposals/explicit-resource-management');
 require('../proposals/is-error');
+require('../proposals/iterator-sequencing');
 require('../proposals/json-parse-with-source');
 require('../proposals/math-sum');
 // TODO: Obsolete versions, remove from `core-js@4`
