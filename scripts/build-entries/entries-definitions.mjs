b315de31225c4eddda7a563a0e83e533bb344bc8
Denis Pushkarev <zloirock@zloirock.ru>
2024-12-29 02:31:40 +0700

    update after rebasing

--- scripts/build-entries/entries-definitions.mjs
+++ scripts/build-entries/entries-definitions.mjs
@@ -897,6 +897,7 @@ export const features = {
     template: $static,
     namespace: 'Error',
     name: 'isError',
+    injectOn: 'static',
   },
   'function/index': {
     modules: [/^(?:es|esnext)\.function\./],
