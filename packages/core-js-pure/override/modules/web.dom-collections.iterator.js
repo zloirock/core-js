dfcbeece6f4634dc63e4ef46dcc481b3c3a4a59d
Denis Pushkarev <zloirock@zloirock.ru>
2024-12-29 02:31:40 +0700

    update after rebasing

--- packages/core-js-pure/override/modules/web.dom-collections.iterator.js
+++ packages/core-js-pure/override/modules/web.dom-collections.iterator.js
@@ -1,12 +1,12 @@
 'use strict';
 var globalThis = require('../internals/global-this');
-var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
+var getIteratorMethod = require('../internals/get-iterator-method');
 var setToStringTag = require('../internals/set-to-string-tag');
 var DOMIterables = require('../internals/dom-iterables');
 var Iterators = require('../internals/iterators');
 
 // dependency: es.array.iterator
-var ArrayValues = getBuiltInPrototypeMethod('Array', 'values');
+var ArrayValues = getIteratorMethod([]);
 
 Object.keys(DOMIterables).forEach(function (collectionName) {
   setToStringTag(globalThis[collectionName], collectionName);
