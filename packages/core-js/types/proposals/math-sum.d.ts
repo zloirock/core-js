c810006f38578fb5ed48ff01c23b83ff804f2c5a
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-22 02:28:59 +0700

    Tests for types

--- packages/core-js/types/proposals/math-sum.d.ts
+++ packages/core-js/types/proposals/math-sum.d.ts
@@ -1,5 +1,15 @@
 // proposal stage: 3
 // https://github.com/tc39/proposal-math-sum
+declare module 'core-js-pure/actual/math/sum-precise' {
+  function sumPrecise(...values: number[]): number;
+  export default sumPrecise;
+}
+
+declare module 'core-js-pure/full/math/sum-precise' {
+  function sumPrecise(...values: number[]): number;
+  export default sumPrecise;
+}
+
 interface Math {
   sumPrecise(...values: number[]): number;
 }
