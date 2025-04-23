8ebe243d0e02858fbec4cebb4878bad48bee23d5
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-23 22:33:00 +0700

    Tests for types & types fixes

---
+++ tests/types/tests/json-parse-with-source.test.ts
@@ -0,0 +1,20 @@
+const r: RawJSONObject = JSON.rawJSON('{"a":123}');
+
+const isr1: boolean = JSON.isRawJSON(r);
+const isr2: boolean = JSON.isRawJSON({});
+const isr3: boolean = JSON.isRawJSON('abc');
+const isr4: boolean = JSON.isRawJSON(undefined);
+
+declare const smth: unknown;
+
+if (JSON.isRawJSON(smth)) {
+  smth.rawJSON;
+  const s: string = smth.rawJSON;
+  // @ts-expect-error
+  smth.noProp;
+}
+
+// @ts-expect-error
+JSON.rawJSON(123);
+// @ts-expect-error
+JSON.rawJSON();
