993c8e46c05af497dfa9506de423bb19186d40b1
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-23 22:33:00 +0700

    Tests for types & types fixes

--- packages/core-js/types/proposals/json-parse-with-source.d.ts
+++ packages/core-js/types/proposals/json-parse-with-source.d.ts
@@ -1,5 +1,9 @@
 // proposal stage: 3
 // https://github.com/tc39/proposal-json-parse-with-source
+type RawJSONObject = {
+  rawJSON: string;
+}
 interface JSON {
-  isRawJSON(value: unknown): value is string;
+  isRawJSON(value: unknown): value is RawJSONObject;
+  rawJSON(value: string): RawJSONObject;
 }
