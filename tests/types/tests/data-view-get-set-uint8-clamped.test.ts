4e483024cb0d9379be6c9b1cab1fe2ed97fa22d3
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-23 22:33:00 +0700

    Tests for types & types fixes

---
+++ tests/types/tests/data-view-get-set-uint8-clamped.test.ts
@@ -0,0 +1,19 @@
+declare const dv: DataView;
+
+const rdv: number = dv.getUint8Clamped(0);
+dv.setUint8Clamped(0, 255);
+
+// @ts-expect-error
+dv.getUint8Clamped();
+// @ts-expect-error
+dv.getUint8Clamped('0');
+// @ts-expect-error
+dv.getUint8Clamped(0, 2);
+// @ts-expect-error
+dv.setUint8Clamped(0);
+// @ts-expect-error
+dv.setUint8Clamped('0', 2);
+// @ts-expect-error
+dv.setUint8Clamped(0, '2');
+// @ts-expect-error
+dv.setUint8Clamped();
