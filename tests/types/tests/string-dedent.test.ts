3b77568c8c505aefae037f535325e05cb3827a1c
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-24 21:30:42 +0700

    Tests for types & types fixes

---
+++ tests/types/tests/string-dedent.test.ts
@@ -0,0 +1,14 @@
+const rdedent1: string = String.dedent`foo\nbar`;
+const rdedent2: string = String.dedent`line1
+  line2
+  line3`;
+
+const tpl = Object.assign(['foo', 'bar'], { raw: ['foo', 'bar'] }) as TemplateStringsArray;
+String.dedent(tpl, 1, 2);
+
+// @ts-expect-error
+String.dedent(['foo', 'bar'], 1, 2);
+// @ts-expect-error
+String.dedent('foo', 1, 2);
+// @ts-expect-error
+String.dedent();
