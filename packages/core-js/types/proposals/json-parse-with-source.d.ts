bb055b0d46b7506a56fc5cc62edbccdf13e20691
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-25 00:19:49 +0700

    Tests for types & types fixes

--- packages/core-js/types/proposals/json-parse-with-source.d.ts
+++ packages/core-js/types/proposals/json-parse-with-source.d.ts
@@ -1,9 +1,16 @@
 // proposal stage: 3
 // https://github.com/tc39/proposal-json-parse-with-source
+interface ReviverContext {
+  source: string;
+  keys: string[];
+}
+
 type RawJSONObject = {
   rawJSON: string;
 }
+
 interface JSON {
   isRawJSON(value: unknown): value is RawJSONObject;
+  parse<T = any>(text: string, reviver?: (key: string, value: any, context: ReviverContext) => any): T;
   rawJSON(value: string): RawJSONObject;
 }
