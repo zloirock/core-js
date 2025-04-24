db1e4709bb112dc43b99d4633129fa93f4281a13
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-24 21:30:42 +0700

    Tests for types & types fixes

---
+++ tests/types/tests/symbol-predicates.test.ts
@@ -0,0 +1,16 @@
+const rsymbol1: boolean = Symbol.isRegisteredSymbol(Symbol.for('foo'));
+const rsymbol2: boolean = Symbol.isRegisteredSymbol(undefined);
+const rsymbol3: boolean = Symbol.isRegisteredSymbol(Symbol('bar'));
+
+const rsymbol4: boolean = Symbol.isWellKnownSymbol(Symbol.iterator);
+const rsymbol5: boolean = Symbol.isWellKnownSymbol({});
+const rsymbol6: boolean = Symbol.isWellKnownSymbol(Symbol('baz'));
+
+declare const u: unknown;
+Symbol.isRegisteredSymbol(u);
+Symbol.isWellKnownSymbol(u);
+
+// @ts-expect-error
+Symbol.isRegisteredSymbol();
+// @ts-expect-error
+Symbol.isWellKnownSymbol();
