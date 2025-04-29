68eca5474314864fd72b90593e34ff64baceb7b6
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-30 01:45:41 +0700

    Fix definition comments & build types logic

--- packages/core-js/modules/esnext.uint8-array.to-hex.js
+++ packages/core-js/modules/esnext.uint8-array.to-hex.js
@@ -1,4 +1,4 @@
-// type: proposals/array-buffer-base64.d.ts
+// types: proposals/array-buffer-base64
 'use strict';
 /* eslint-disable es/no-nonstandard-typed-array-prototype-properties -- safe */
 var $ = require('../internals/export');
