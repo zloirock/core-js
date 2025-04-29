36542ae70231f5cc2ad472382af852863ed14430
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-30 01:45:41 +0700

    Fix definition comments & build types logic

--- packages/core-js/modules/esnext.set.of.js
+++ packages/core-js/modules/esnext.set.of.js
@@ -1,4 +1,4 @@
-// type: proposals/collection-of-from.d.ts
+// types: proposals/collection-of-from
 'use strict';
 var $ = require('../internals/export');
 var SetHelpers = require('../internals/set-helpers');
