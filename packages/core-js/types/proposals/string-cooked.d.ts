62e3fdbd7339ec7de5108f3ffbff83c167f02250
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-30 22:14:11 +0700

    Fix types & build types script

--- packages/core-js/types/proposals/string-cooked.d.ts
+++ packages/core-js/types/proposals/string-cooked.d.ts
@@ -1,7 +1,7 @@
 // proposal stage: 1
 // https://github.com/tc39/proposal-string-cooked
 interface StringConstructor {
-  cooked(template: readonly string[], ...substitutions: unknown[]): string;
+  cooked(template: readonly string[], ...substitutions: any[]): string;
 
-  cooked(template: string, ...substitutions: unknown[]): string;
+  cooked(template: string, ...substitutions: any[]): string;
 }
