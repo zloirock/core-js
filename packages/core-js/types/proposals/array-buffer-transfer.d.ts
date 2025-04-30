ebc47c094fb4c1760e73216b305dcc67152e0198
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-04-30 22:14:11 +0700

    Fix types & build types script

--- packages/core-js/types/proposals/array-buffer-transfer.d.ts
+++ packages/core-js/types/proposals/array-buffer-transfer.d.ts
@@ -3,7 +3,6 @@
 interface ArrayBuffer {
   readonly detached: boolean;
 
-  slice(start?: number, end?: number): ArrayBuffer;
   transfer(newByteLength?: number): ArrayBuffer;
   transferToFixedLength(newByteLength?: number): ArrayBuffer;
 }
--- packages/core-js/types/proposals/json-parse-with-source.d.ts
+++ packages/core-js/types/proposals/json-parse-with-source.d.ts
@@ -2,7 +2,6 @@
 // https://github.com/tc39/proposal-json-parse-with-source
 interface ReviverContext {
   source: string;
-  keys: string[];
 }
 
 type RawJSONObject = {
@@ -10,7 +9,7 @@ type RawJSONObject = {
 }
 
 interface JSON {
-  isRawJSON(value: unknown): value is RawJSONObject;
+  isRawJSON(value: any): value is RawJSONObject;
   parse<T = any>(text: string, reviver?: (key: string, value: any, context: ReviverContext) => any): T;
   rawJSON(value: string): RawJSONObject;
 }
