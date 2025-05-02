3471d30811bb9abe63e6e2a32542ca7e959904f7
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-05-02 22:05:11 +0700

    The entry point tests are split into two files

---
+++ tests/type-definitions/entries.require.ts
@@ -0,0 +1,56 @@
+// $justImport
+require('@core-js/pure/full/array-buffer/detached');
+
+// $virtual
+require('@core-js/pure/full/array/virtual/at').call([1, 2, 3], -2);
+
+// $virtualIterator
+require('@core-js/pure/full/array/virtual/iterator').call([1]).next().value;
+
+// $prototype
+require('@core-js/pure/full/array/at')([1, 2, 3], -2);
+
+// $prototypeIterator
+require('@core-js/pure/full/array/iterator')([]).next().value;
+
+// $static
+require('@core-js/pure/full/array/from')('qwe');
+
+// $staticWithContext
+require('@core-js/pure/full/promise/all-settled')([1, 2, 3]);
+
+// $patchableStatic
+require('@core-js/pure/full/json/stringify')([1]);
+
+// $namespace
+require('@core-js/pure/full/async-disposable-stack/constructor');
+
+// $helper
+require('@core-js/pure/full/get-iterator')([]);
+
+// $path
+new (require('@core-js/pure/full/error/constructor').Error)();
+
+// $instanceArray
+require('@core-js/pure/full/instance/concat')(1);
+
+// $instanceString
+require('@core-js/pure/full/instance/code-point-at')('');
+
+// $instanceFunction
+require('@core-js/pure/full/instance/demethodize')({});
+
+// $instanceDOMIterables
+require('@core-js/pure/full/instance/for-each')({});
+
+// $instanceArrayString
+require('@core-js/pure/full/instance/at')('');
+
+// $instanceArrayDOMIterables
+require('@core-js/pure/full/instance/entries')({});
+
+// $instanceRegExpFlags
+require('@core-js/pure/full/instance/flags')({});
+
+// $proposal
+require('@core-js/pure/proposals/accessible-object-hasownproperty');
