0f5942f4f44ff9576ec8ee042d8c6106fac3f719
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-05-02 21:50:01 +0700

    Change Iterator to IteratorObject in types

--- packages/core-js/types/proposals/async-iterator-helpers.d.ts
+++ packages/core-js/types/proposals/async-iterator-helpers.d.ts
@@ -1,35 +1,35 @@
 // proposal stage: 2
 // https://github.com/tc39/proposal-async-iterator-helpers
 interface AsyncIteratorConstructor {
-  from<T>(iterable: AsyncIterable<T> | Iterable<T> | AsyncIterator<T>): AsyncIterator<T>;
+  from<T>(iterable: AsyncIterable<T> | Iterable<T> | AsyncIteratorObject<T>): AsyncIteratorObject<T>;
 }
 
 declare var AsyncIterator: AsyncIteratorConstructor;
 
 interface AsyncIterator<T> {
-  drop(limit: number): AsyncIterator<T>;
+  drop(limit: number): AsyncIteratorObject<T>;
 
   every(predicate: (value: T, index: number) => boolean): Promise<boolean>;
 
-  filter(predicate: (value: T, index: number) => boolean): AsyncIterator<T>;
+  filter(predicate: (value: T, index: number) => boolean): AsyncIteratorObject<T>;
 
   find(predicate: (value: T, index: number) => boolean): Promise<T>;
 
-  flatMap(mapper: (value: T, index: number) => any): AsyncIterator<any>;
+  flatMap(mapper: (value: T, index: number) => any): AsyncIteratorObject<any>;
 
   forEach(callbackFn: (value: T, index: number) => void): Promise<void>;
 
-  map(mapper: (value: T, index: number) => any): AsyncIterator<T>;
+  map(mapper: (value: T, index: number) => any): AsyncIteratorObject<T>;
 
   reduce(reducer: (accumulator: any, value: T, index: number) => any, initialValue?: any): Promise<any>;
 
   some(predicate: (value: T, index: number) => boolean): Promise<boolean>;
 
-  take(limit: number): AsyncIterator<T>;
+  take(limit: number): AsyncIteratorObject<T>;
 
   toArray(): Promise<T[]>;
 }
 
 interface Iterator<T> {
-  toAsync(): AsyncIterator<T>;
+  toAsync(): AsyncIteratorObject<T>;
 }
