47a91ff70d3c1f796999483801add05ecd0f21bb
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2025-06-29 15:27:44 +0530

    fix: update `Uint8Array.prototype.toHex` method to handle incorrect behavior in v8 staging

--- packages/core-js/modules/esnext.uint8-array.to-hex.js
+++ packages/core-js/modules/esnext.uint8-array.to-hex.js
@@ -7,9 +7,20 @@ var notDetached = require('../internals/array-buffer-not-detached');
 
 var numberToString = uncurryThis(1.1.toString);
 
+var Uint8Array = globalThis.Uint8Array;
+
+var INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS = !Uint8Array || !Uint8Array.prototype.toHex || !(function () {
+  try {
+    var target = new Uint8Array([255, 255, 255, 255, 255, 255, 255, 255]);
+    return target.toHex() === 'ffffffffffffffff';
+  } catch (error) {
+    return false;
+  }
+})();
+
 // `Uint8Array.prototype.toHex` method
 // https://github.com/tc39/proposal-arraybuffer-base64
-if (globalThis.Uint8Array) $({ target: 'Uint8Array', proto: true }, {
+if (Uint8Array) $({ target: 'Uint8Array', proto: true, forced: INCORRECT_BEHAVIOR_OR_DOESNT_EXISTS }, {
   toHex: function toHex() {
     anUint8Array(this);
     notDetached(this.buffer);
