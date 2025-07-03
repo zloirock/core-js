0eddcc631fd32e397bbc98c10c1bc32eed7a0382
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-07-03 16:12:14 +0700

    Add transpiling to assets

--- web/scripts/copy.mjs
+++ web/scripts/copy.mjs
@@ -1,5 +1,6 @@
 const { copy } = fs;
 
 await copy('web/templates/assets', 'web/dist/assets');
+await copy('web/templates/core-js-bundle.js', 'web/dist/core-js-bundle.js');
 
 echo(chalk.green(`Assets copied`));
