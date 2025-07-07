53d85cb3159bbc77eca9be6bc73f14c118f70fe7
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-07-07 23:01:46 +0700

    Playground link without .html

--- web/src/js/hljs-run.js
+++ web/src/js/hljs-run.js
@@ -1,6 +1,6 @@
 export default class RunButtonPlugin {
   constructor() {
-    this.playgroundUrl = '/playground.html';
+    this.playgroundUrl = '/playground';
     this.text = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">' +
       '<path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />' +
       '</svg>'
