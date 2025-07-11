4c1789f7bffac43550dfcc5df72cbd5b61a21b9f
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-07-11 17:12:57 +0700

    Fix types for all targets

--- tests/type-definitions/proposals/array-filtering.test.ts
+++ tests/type-definitions/proposals/array-filtering.test.ts
@@ -22,41 +22,41 @@ testFilterReject(new Int32Array([1, 2]), 1);
 testFilterReject(new Uint32Array([1, 2]), 1);
 testFilterReject(new Float32Array([1, 2]), 1);
 testFilterReject(new Float64Array([1, 2]), 1);
-testFilterReject(new BigInt64Array([1n, 2n]), 1n);
-testFilterReject(new BigUint64Array([1n, 2n]), 1n);
+testFilterReject(new BigInt64Array([BigInt(1), BigInt(2)]), BigInt(1));
+testFilterReject(new BigUint64Array([BigInt(1), BigInt(2)]), BigInt(1));
 
 // @ts-expect-error
-[1,2,3].filterReject((x: string) => false);
+[1, 2, 3].filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Int8Array([1,2,3])).filterReject((x: string) => false);
+(new Int8Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Uint8Array([1,2,3])).filterReject((x: string) => false);
+(new Uint8Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Uint8ClampedArray([1,2,3])).filterReject((x: string) => false);
+(new Uint8ClampedArray([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Int16Array([1,2,3])).filterReject((x: string) => false);
+(new Int16Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Uint16Array([1,2,3])).filterReject((x: string) => false);
+(new Uint16Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Int32Array([1,2,3])).filterReject((x: string) => false);
+(new Int32Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Uint32Array([1,2,3])).filterReject((x: string) => false);
+(new Uint32Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Float32Array([1,2,3])).filterReject((x: string) => false);
+(new Float32Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new Float64Array([1,2,3])).filterReject((x: string) => false);
+(new Float64Array([1, 2, 3])).filterReject((x: string) => false);
 
 // @ts-expect-error
-(new BigInt64Array([1n,2n,3n])).filterReject((x: number) => false);
+(new BigInt64Array([BigInt(1), BigInt(2), BigInt(3)])).filterReject((x: number) => false);
 
 // @ts-expect-error
-(new BigUint64Array([1n,2n,3n])).filterReject((x: number) => false);
+(new BigUint64Array([BigInt(1), BigInt(2), BigInt(3)])).filterReject((x: number) => false);
