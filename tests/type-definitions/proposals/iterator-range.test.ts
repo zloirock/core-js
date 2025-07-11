40b2fa9c66ade926524e33ace4f1af05e50f51c0
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-07-11 17:12:57 +0700

    Fix types for all targets

--- tests/type-definitions/proposals/iterator-range.test.ts
+++ tests/type-definitions/proposals/iterator-range.test.ts
@@ -4,12 +4,12 @@ const rir1: Iterator<number> = Iterator.range(1, 10);
 Iterator.range(1, 10, 1);
 Iterator.range(1, 10, { step: 1 });
 Iterator.range(1, 10, { inclusive: true });
-const rir2: Iterator< bigint> = Iterator.range(0n, 10n, { step: 2n, inclusive: true });
+const rir2: Iterator< bigint> = Iterator.range(BigInt(0), BigInt(10), { step: BigInt(2), inclusive: true });
 
 // @ts-expect-error
 Iterator.range(0, 10, 'not-a-number');
 // @ts-expect-error
-Iterator.range(0, 10, { step: 1n });
+Iterator.range(0, 10, { step: BigInt(1) });
 // @ts-expect-error
 Iterator.range(0, 10, { inclusive: 3 });
 // @ts-expect-error
