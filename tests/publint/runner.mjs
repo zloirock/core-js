6daf64a6dc6e0e515e8201314359b11c12d01ba4
Denis Pushkarev <zloirock@zloirock.ru>
2025-07-19 23:28:34 +0300

    ignore empty folders in publint

--- tests/publint/runner.mjs
+++ tests/publint/runner.mjs
@@ -1,3 +1,10 @@
-const pkgs = await fs.readdir('packages');
+const { access, readdir } = fs;
 
-await Promise.all(pkgs.map(pkg => $`publint packages/${ pkg }`));
+const pkgs = await readdir('packages');
+
+await Promise.all(pkgs.map(async pkg => {
+  // eslint-disable-next-line promise/prefer-await-to-then -- ok
+  if (await access(`./packages/${ pkg }/package.json`).then(() => true, () => false)) {
+    return $`publint packages/${ pkg }`;
+  }
+}));
