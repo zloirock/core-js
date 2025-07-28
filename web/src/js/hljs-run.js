b768c5dd387dae255a3b748eff0480feb484b94c
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-07-29 02:07:53 +0700

    Fix run code url

--- web/src/js/hljs-run.js
+++ web/src/js/hljs-run.js
@@ -1,5 +1,7 @@
 export default class RunButtonPlugin {
-  playgroundUrl = '/playground';
+  BASE_URL = document.querySelector('base')?.href || window.location.origin;
+  PLAYGROUND_URL = 'playground';
+
   text = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">' +
     '<path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.347a1.125 1.125 0 0 1 0 ' +
     '1.972l-11.54 6.347a1.125 1.125 0 0 1-1.667-.986V5.653Z" />' +
@@ -17,7 +19,7 @@ export default class RunButtonPlugin {
       const urlParams = new URLSearchParams();
       urlParams.set('code', text);
       const hash = urlParams.toString();
-      globalThis.location.href = `.${ this.playgroundUrl }#${ hash }`;
+      globalThis.location.href = `${ this.BASE_URL }${ this.PLAYGROUND_URL }#${ hash }`;
     });
     const wrapper = document.createElement('div');
     wrapper.classList.add('hljs-run');
