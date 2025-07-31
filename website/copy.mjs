c28486fc18ca0a768e6c85de9ef8a5bbd5b19113
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-08-01 03:05:51 +0700

    2 versions of core-js-bundle

--- website/copy.mjs
+++ website/copy.mjs
@@ -2,5 +2,6 @@ const { copy } = fs;
 
 await copy('website/templates/assets', 'website/dist/assets');
 await copy('website/templates/core-js-bundle.js', 'website/dist/core-js-bundle.js');
+await copy('website/templates/core-js-bundle-modern.js', 'website/dist/core-js-bundle-modern.js');
 
 echo(chalk.green('Assets copied'));
