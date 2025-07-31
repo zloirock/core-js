8bc468d29b384547a52bc90a9c1d0bb67984ab68
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-08-01 03:23:54 +0700

    Build modern bundle core js

---
+++ website/build-modern-core-js.mjs
@@ -0,0 +1,9 @@
+import builder from '@core-js/builder';
+import { minify } from 'terser';
+
+const { script } = await builder({
+  modules: 'core-js/full',
+  targets: { esmodules: true },
+});
+const { code } = await minify(script);
+await fs.writeFile(`website/src/public/core-js-bundle-modern.js`, code, 'utf8');
