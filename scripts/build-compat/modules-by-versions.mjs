afbe8bf71eaefeded3cc0b0dfca4547c8914e969
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-02 22:38:52 +0300

    no longer require manual adding new modules to `modules-by-versions`

--- scripts/build-compat/modules-by-versions.mjs
+++ scripts/build-compat/modules-by-versions.mjs
@@ -1,15 +1,28 @@
+import coerce from 'semver/functions/coerce.js';
 import { modules } from '@core-js/compat/src/data.mjs';
-import modulesByVersions from '@core-js/compat/src/modules-by-versions.mjs';
 
-const defaults = new Set(modules);
+const { version } = await fs.readJson('package.json');
+const $version = coerce(version);
 
-for (const version of Object.values(modulesByVersions)) {
-  for (const module of version) defaults.delete(module);
+// TODO: set to `true` after the first core-js@4 stable release
+const HAS_STABLE_RELEASE = false;
+
+const $modules = new Set(modules);
+let data;
+
+if (HAS_STABLE_RELEASE) {
+  const response = await fetch(`https://cdn.jsdelivr.net/npm/@core-js/compat@${ $version.major }/modules-by-versions.json`);
+  data = await response.json();
+} else {
+  data = {};
 }
 
-await fs.writeJson('packages/core-js-compat/modules-by-versions.json', {
-  '4.0': [...defaults],
-  ...modulesByVersions,
-}, { spaces: '  ' });
+for (const prev of Object.values(data)) {
+  for (const module of prev) $modules.delete(module);
+}
+
+if ($modules.size) data[`${ $version }-unreleased`] = [...$modules];
+
+await fs.writeJson('packages/core-js-compat/modules-by-versions.json', data, { spaces: '  ' });
 
 echo(chalk.green('modules-by-versions data rebuilt'));
