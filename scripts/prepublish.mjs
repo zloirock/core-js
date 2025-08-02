a7ecaf3d2b28aa9631f4d51c2a5a502420856c9e
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-02 23:00:53 +0300

    add `entries-by-versions` data

--- scripts/prepublish.mjs
+++ scripts/prepublish.mjs
@@ -5,3 +5,11 @@ for (const version of Object.keys(modulesByVersions)) {
     throw echo(chalk.red('`modules-by-versions` should not have `unreleased` pseudo-version. Please, rerun `update-version`.'));
   }
 }
+
+const entriesByVersions = await fs.readJson('packages/core-js-compat/entries-by-versions.json');
+
+for (const version of Object.keys(entriesByVersions)) {
+  if (version.includes('unreleased')) {
+    throw echo(chalk.red('`entries-by-versions` should not have `unreleased` pseudo-version. Please, rerun `update-version`.'));
+  }
+}
