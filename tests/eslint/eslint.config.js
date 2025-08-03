fd4c799cc1f4905a044928db55fdce0d5ba5538d
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-03 17:14:06 +0300

    add a template of a babel plugin and tests runner

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -2270,6 +2270,7 @@ export default [
       'packages/core-js-bundle/!(package.json)',
       'packages/core-js-compat/!(package).json',
       'packages/core-js-pure/override/**',
+      'tests/babel-plugin/fixtures/**/*.mjs',
       'tests/**/bundles/**',
       'tests/compat/compat-data.js',
       'tests/unit-@(global|pure)/index.js',
@@ -2406,7 +2407,7 @@ export default [
   },
   {
     files: [
-      'packages/core-js-@(builder|compat)/**',
+      'packages/core-js-@(babel-plugin|builder|compat)/**',
     ],
     rules: nodePackages,
   },
