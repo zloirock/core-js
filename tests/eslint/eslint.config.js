3a0ff1b0d42a5d0b684e98ec148a18eb9bff01bd
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-15 20:23:06 +0300

    update dependencies

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -2105,6 +2105,8 @@ const packageJSON = {
   'package-json/valid-dependencies': ERROR,
   // enforce that the `description` property is valid
   'package-json/valid-description': ERROR,
+  // enforce that the `exports` property is valid
+  'package-json/valid-exports': ERROR,
   // enforce that the `license` property is valid
   'package-json/valid-license': ERROR,
   // enforce that if repository directory is specified, it matches the path to the package.json file
