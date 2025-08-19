0a54947a1952079815d9beba873c1d29245a8a9c
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-08-19 22:28:19 +0700

    Test runner cancelling

--- website/scripts/runner.sh
+++ website/scripts/runner.sh
@@ -2,20 +2,26 @@
 LOCK_FILE=./runner.lock
 PID_FILE=./runner.pid
 
-lock_acquired=false
-
-for i in {1..60}; do
-    if ln -s "$$" "$LOCK_FILE" 2>/dev/null; then
-        lock_acquired=true
-        break
-    else
-        echo "Another process running, waiting..."
-        sleep 5
+kill_old_build() {
+    if [ -f "$PID_FILE" ]; then
+        OLD_PID=$(cat "$PID_FILE")
+        if ps -p "$OLD_PID" > /dev/null 2>&1; then
+            kill "$OLD_PID"
+            echo "Previous build $OLD_PID in progress. Terminating..."
+            sleep 2
+            if ps -p "$OLD_PID" > /dev/null 2>&1; then
+                kill -9 "$OLD_PID"
+                echo "PID $OLD_PID still alive, sending SIGKILL!"
+            fi
+        fi
+        rm -f "$LOCK_FILE" "$PID_FILE"
     fi
-done
+}
 
-if ! $lock_acquired; then
-    echo "Timeout waiting for lock! Exiting."
+kill_old_build
+
+if ! ln -s "$$" "$LOCK_FILE" 2>/dev/null; then
+    echo "Another build still running, exit"
     exit 1
 fi
 
@@ -27,6 +33,8 @@ cleanup() {
     pkill -P $$
 }
 trap cleanup EXIT HUP INT TERM
+
+echo "Lock acquired by $$, starting build..."
 
 node ./runner.mjs
 EXIT_CODE=$?
