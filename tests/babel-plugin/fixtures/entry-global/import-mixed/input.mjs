d97abde18feb0c515f46efee69e99f52c28b9fe8
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-23 04:37:20 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/entry-global/import-mixed/input.mjs
@@ -0,0 +1,4 @@
+import 'core-js/es/object';
+import 'core-js/full/reflect';
+import 'core-js/modules/es.array.reverse';
+import 'core-js/modules/es.math.sum-precise';
---
+++ tests/babel-plugin/fixtures/usage-global/mixed/output.mjs
@@ -0,0 +1,12 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.from";
+import "core-js/modules/es.array.push";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+Array.from();
+new Promise();
+[].push();
