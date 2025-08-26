91eee201a930eceed38826354debbb50af4b5678
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-26 17:05:50 +0300

    drop one more ancient IE workaround

--- packages/core-js/internals/document-create-element.js
+++ packages/core-js/internals/document-create-element.js
@@ -1,10 +1,8 @@
 'use strict';
 var globalThis = require('../internals/global-this');
-var isObject = require('../internals/is-object');
 
 var document = globalThis.document;
-// typeof document.createElement is 'object' in old IE
-var EXISTS = isObject(document) && isObject(document.createElement);
+var EXISTS = document && typeof document.createElement == 'function';
 
 module.exports = function (it) {
   return EXISTS ? document.createElement(it) : {};
