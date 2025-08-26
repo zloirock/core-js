b8e3279ec4419e17570ce7cacfec6a2681491186
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-26 17:53:22 +0300

    drop Safari 6 `%TypedArray%.prototype.toLocaleString` fix

--- packages/core-js/modules/es.typed-array.to-locale-string.js
+++ packages/core-js/modules/es.typed-array.to-locale-string.js
@@ -9,11 +9,6 @@ var $Int8Array = Int8Array;
 var $toLocaleString = [].toLocaleString;
 var Int8ArrayToLocaleString = $Int8Array.prototype.toLocaleString;
 
-// iOS Safari 6.x fails here
-var TO_LOCALE_STRING_BUG = fails(function () {
-  $toLocaleString.call(new $Int8Array(1));
-});
-
 var FORCED = fails(function () {
   return [1, 2].toLocaleString() !== Int8ArrayToLocaleString.call(new Float32Array([1, 2]));
 }) || !fails(function () {
@@ -23,9 +18,5 @@ var FORCED = fails(function () {
 // `%TypedArray%.prototype.toLocaleString` method
 // https://tc39.es/ecma262/#sec-%typedarray%.prototype.tolocalestring
 exportTypedArrayMethod('toLocaleString', function toLocaleString() {
-  return apply(
-    $toLocaleString,
-    TO_LOCALE_STRING_BUG ? arraySlice(aTypedArray(this)) : aTypedArray(this),
-    arraySlice(arguments)
-  );
+  return apply($toLocaleString, aTypedArray(this), arraySlice(arguments));
 }, FORCED);
