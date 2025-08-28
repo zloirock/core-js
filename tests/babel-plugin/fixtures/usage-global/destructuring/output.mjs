12ed8c5bb9529fe56530b757bb8dfb79db6f7fb5
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-28 04:40:06 +0300

    inject polyfills in visitor in `usage-global`

---
+++ tests/babel-plugin/fixtures/usage-global/destructuring/output.mjs
@@ -0,0 +1,5 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const [a, b] = c;
