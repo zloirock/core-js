b28acf581085999f1fea916417b9848bb1fb95f1
Denis Pushkarev <zloirock@zloirock.ru>
2025-08-31 21:37:33 +0700

    simplify publint runner

--- tests/publint/runner.mjs
+++ tests/publint/runner.mjs
@@ -1,10 +1,5 @@
-const { access, readdir } = fs;
-
-const pkgs = await readdir('packages');
+const pkgs = await glob('./packages/*/package.json');
 
 await Promise.all(pkgs.map(async pkg => {
-  // eslint-disable-next-line promise/prefer-await-to-then -- ok
-  if (await access(`./packages/${ pkg }/package.json`).then(() => true, () => false)) {
-    return $`publint packages/${ pkg }`;
-  }
+  return $`publint ${ pkg.slice(2, -13) }`;
 }));
