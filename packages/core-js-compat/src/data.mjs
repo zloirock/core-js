026846e6718334f3a11d3d9683536f82493bf14d
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-09-15 22:11:16 +0700

    Await dictionary of Promises proposal

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2390,6 +2390,8 @@ export const data = {
   },
   'esnext.number.clamp': {
   },
+  'esnext.promise.all-keyed': {
+  },
   'esnext.set.from': {
   },
   'esnext.set.of': {
--- tests/compat/tests.js
+++ tests/compat/tests.js
@@ -1877,6 +1877,9 @@ GLOBAL.tests = {
   'esnext.number.clamp': function () {
     return Number.prototype.clamp;
   },
+  'esnext.promise.all-keyed': function () {
+    return Promise.allKeyed;
+  },
   'esnext.set.from': function () {
     return Set.from;
   },
