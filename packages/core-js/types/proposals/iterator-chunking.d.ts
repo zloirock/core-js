7066edb1ba8b71c27fabe3bccaec10dfaac3d432
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-09-25 15:02:51 +0700

    Update type for iterator chunking

--- packages/core-js/types/proposals/iterator-chunking.d.ts
+++ packages/core-js/types/proposals/iterator-chunking.d.ts
@@ -1,7 +1,7 @@
-// proposal stage: 2
+// proposal stage: 2.7
 // https://github.com/tc39/proposal-iterator-chunking
 interface Iterator<T> {
   chunks(size: number): IteratorObject<T[]>;
-  sliding(size: number): IteratorObject<T[]>;
-  windows(size: number): IteratorObject<T[]>;
+  sliding(size: number): IteratorObject<T[]>;  // todo remove after rebase
+  windows(windowSize: number, undersized?: 'only-full' | 'allow-partial' | undefined): IteratorObject<T[]>;
 }
