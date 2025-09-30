e62e6f6304188bd56e68b170b2a20907756b60db
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-09-30 18:51:41 +0700

    Fixes after rebase

--- packages/core-js/types/proposals/iterator-chunking.d.ts
+++ packages/core-js/types/proposals/iterator-chunking.d.ts
@@ -1,7 +1,6 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-iterator-chunking
 interface Iterator<T> {
-  chunks(size: number): IteratorObject<T[]>;
-  sliding(size: number): IteratorObject<T[]>;  // todo remove after rebase
+  chunks(chunkSize: number): IteratorObject<T[]>;
   windows(windowSize: number, undersized?: 'only-full' | 'allow-partial' | undefined): IteratorObject<T[]>;
 }
