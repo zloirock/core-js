69524e31cef8435fbe9752f8f9bbf8f3170c93d3
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-02 02:33:35 +0700

    Update proposal types

--- packages/core-js/types/proposals/iterator-joint.d.ts
+++ packages/core-js/types/proposals/iterator-joint.d.ts
@@ -1,6 +1,6 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-joint-iteration
-type IteratorObjectCoreJs<T> = typeof globalThis extends { IteratorObject: infer O }
+type CoreJsIteratorObject<T> = typeof globalThis extends { IteratorObject: infer O }
   ? O
   : Iterator<T>;
 
@@ -10,8 +10,8 @@ type ZipOptions = {
 };
 
 interface Iterator<T> {
-  zip<U>(iterables: Iterable<U>, options?: ZipOptions): IteratorObjectCoreJs<[T, U]>;
+  zip<U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[T, U]>;
 
-  zipKeyed<U>(iterables: Iterable<U>, options?: ZipOptions): IteratorObjectCoreJs<[number, T, U]>;
-  zipKeyed<U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): IteratorObjectCoreJs<[PropertyKey, T, U]>;
+  zipKeyed<U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[number, T, U]>;
+  zipKeyed<U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): CoreJsIteratorObject<[PropertyKey, T, U]>;
 }
