3841c9b841d1b436a58643d14cccd092b1ea5491
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-07 19:57:11 +0700

    Update proposal types & type tests

--- packages/core-js/types/proposals/iterator-sequencing.d.ts
+++ packages/core-js/types/proposals/iterator-sequencing.d.ts
@@ -1,5 +1,7 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-iterator-sequencing
-interface Iterator<T> {
-  concat<U>(...iterators: Iterable<U>[]): CoreJsIteratorObject<T | U>;
+interface IteratorConstructor {
+  concat<T, U>(...iterators: Iterable<U>[]): CoreJsIteratorObject<T | U>;
 }
+
+declare var Iterator: IteratorConstructor;
---
+++ tests/type-definitions/proposals/pure/decorator-metadata.test.ts
@@ -0,0 +1,16 @@
+import metadata from '@core-js/pure/full/symbol/metadata';
+
+const rsmd1: symbol = metadata;
+const rsmd2: typeof metadata = metadata;
+
+type T = {
+  [metadata]?: object;
+};
+
+const obj: T = {};
+obj[metadata] = { foo: 1 };
+
+const maybeMeta: object | undefined = obj[metadata];
+
+// @ts-expect-error
+obj[metadata] = 123;
