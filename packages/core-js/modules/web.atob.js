9be5d63f1041da030c3b7cc0a966bb06b385ef58
lancanrong <lancanrong@bytedance.com>
2025-10-11 15:13:02 +0800

    feat: pre-allocating array

--- packages/core-js/modules/web.atob.js
+++ packages/core-js/modules/web.atob.js
@@ -1,5 +1,6 @@
 'use strict';
 var $ = require('../internals/export');
+var arrayFrom = require('../internals/array-from');
 var globalThis = require('../internals/global-this');
 var getBuiltIn = require('../internals/get-built-in');
 var uncurryThis = require('../internals/function-uncurry-this');
@@ -47,7 +48,8 @@ $({ global: true, bind: true, enumerable: true, forced: FORCED }, {
     // `webpack` dev server bug on IE global methods - use call(fn, global, ...)
     if (BASIC && !NO_SPACES_IGNORE && !NO_ENCODING_CHECK) return call($atob, globalThis, data);
     var string = replace(toString(data), whitespaces, '');
-    var output = [];
+    // every 4 characters produce 3 bytes, so ceil(length * 3 / 4) is the number of bytes
+    var output = arrayFrom({ length: Math.ceil(string.length * 3 / 4) });
     var position = 0;
     var bc = 0;
     var length, chr, bs;
