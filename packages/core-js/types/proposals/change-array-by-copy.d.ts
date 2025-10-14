e3efbc80979a5c90421175a2ca28d62abb051241
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-15 01:22:44 +0700

    Use just one link to source code

--- packages/core-js/types/proposals/change-array-by-copy.d.ts
+++ packages/core-js/types/proposals/change-array-by-copy.d.ts
@@ -2,10 +2,7 @@
 // https://github.com/tc39/proposal-change-array-by-copy
 
 // For ensuring compatibility with TypeScript standard types, this code is based on:
-// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts#L28
-// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts#L39
-// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts#L48
-// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts#L67
+// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
 interface Array<T> {
   toReversed(): T[];
