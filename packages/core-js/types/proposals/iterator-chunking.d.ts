1065aeac5b02f3ead997a9a090ce11f6cf6aa322
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-16 01:40:23 +0700

    Fix types for proposals and add type tests with various targets

--- packages/core-js/types/proposals/iterator-chunking.d.ts
+++ packages/core-js/types/proposals/iterator-chunking.d.ts
@@ -1,6 +1,6 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-iterator-chunking
 interface Iterator<T> {
-  chunks(chunkSize: number): IteratorObject<T[]>;
-  windows(windowSize: number, undersized?: 'only-full' | 'allow-partial' | undefined): IteratorObject<T[]>;
+  chunks(chunkSize: number): CoreJsIteratorObject<T[]>;
+  windows(windowSize: number, undersized?: 'only-full' | 'allow-partial' | undefined): CoreJsIteratorObject<T[]>;
 }
