eb9d8ca7328d515f5d4d6a0312b2950dde3c9de5
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-16 01:40:23 +0700

    Fix types for proposals and add type tests with various targets

--- packages/core-js/types/proposals/async-iterator-helpers.d.ts
+++ packages/core-js/types/proposals/async-iterator-helpers.d.ts
@@ -1,35 +1,39 @@
 // proposal stage: 2
 // https://github.com/tc39/proposal-async-iterator-helpers
+type CoreJsAsyncIteratorObject<T> = typeof globalThis extends { AsyncIteratorObject: infer O }
+  ? O
+  : AsyncIterator<T>;
+
 interface AsyncIteratorConstructor {
-  from<T>(iterable: AsyncIterable<T> | Iterable<T> | AsyncIteratorObject<T>): AsyncIteratorObject<T>;
+  from<T>(iterable: AsyncIterable<T> | Iterable<T> | CoreJsAsyncIteratorObject<T>): CoreJsAsyncIteratorObject<T>;
 }
 
 declare var AsyncIterator: AsyncIteratorConstructor;
 
 interface AsyncIterator<T> {
-  drop(limit: number): AsyncIteratorObject<T>;
+  drop(limit: number): CoreJsAsyncIteratorObject<T>;
 
   every(predicate: (value: T, index: number) => boolean): Promise<boolean>;
 
-  filter(predicate: (value: T, index: number) => boolean): AsyncIteratorObject<T>;
+  filter(predicate: (value: T, index: number) => boolean): CoreJsAsyncIteratorObject<T>;
 
   find(predicate: (value: T, index: number) => boolean): Promise<T>;
 
-  flatMap(mapper: (value: T, index: number) => any): AsyncIteratorObject<any>;
+  flatMap(mapper: (value: T, index: number) => any): CoreJsAsyncIteratorObject<any>;
 
   forEach(callbackFn: (value: T, index: number) => void): Promise<void>;
 
-  map(mapper: (value: T, index: number) => any): AsyncIteratorObject<T>;
+  map(mapper: (value: T, index: number) => any): CoreJsAsyncIteratorObject<T>;
 
   reduce(reducer: (accumulator: any, value: T, index: number) => any, initialValue?: any): Promise<any>;
 
   some(predicate: (value: T, index: number) => boolean): Promise<boolean>;
 
-  take(limit: number): AsyncIteratorObject<T>;
+  take(limit: number): CoreJsAsyncIteratorObject<T>;
 
   toArray(): Promise<T[]>;
 }
 
 interface Iterator<T> {
-  toAsync(): AsyncIteratorObject<T>;
+  toAsync(): CoreJsAsyncIteratorObject<T>;
 }
