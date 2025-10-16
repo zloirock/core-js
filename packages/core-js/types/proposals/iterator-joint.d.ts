99d5674dbabf53837517bf6ef25d1494855316d6
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-16 13:45:07 +0700

    core-js object types refactoring

--- packages/core-js/types/proposals/iterator-joint.d.ts
+++ packages/core-js/types/proposals/iterator-joint.d.ts
@@ -1,19 +1,23 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-joint-iteration
-type CoreJsIteratorObject<T> = typeof globalThis extends { IteratorObject: infer O }
-  ? O
-  : Iterator<T>;
 
-type ZipOptions = {
-  mode?: "shortest" | "longest" | "strict";
-  padding?: object;
-};
+import { CoreJsIteratorObject } from './core-js-types/core-js-types';
 
-interface IteratorConstructor {
-  zip<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[T, U]>;
+declare global {
+  type ZipOptions = {
+    mode?: "shortest" | "longest" | "strict";
+    padding?: object;
+  };
 
-  zipKeyed<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[number, T, U]>;
-  zipKeyed<T, U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): CoreJsIteratorObject<[PropertyKey, T, U]>;
+  interface IteratorConstructor {
+    zip<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[T, U]>;
+
+    zipKeyed<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[number, T, U]>;
+
+    zipKeyed<T, U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): CoreJsIteratorObject<[PropertyKey, T, U]>;
+  }
+
+  var Iterator: IteratorConstructor;
 }
 
-declare var Iterator: IteratorConstructor;
+export {};
