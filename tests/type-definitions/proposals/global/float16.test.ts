13eb70e59b3fcbf1c778e7b87539d203d116e45b
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-18 22:56:52 +0700

    Add proposal types for stages 3-4

---
+++ tests/type-definitions/proposals/global/float16.test.ts
@@ -0,0 +1,18 @@
+import 'core-js/full';
+
+const res: number = Math.f16round(1);
+
+// @ts-expect-error
+Math.f16round('123');
+
+const view = new DataView(new ArrayBuffer(4));
+view.setFloat16(0, 1.5);
+const res2: number = view.getFloat16(0);
+// @ts-expect-error
+view.setFloat16(0, '123');
+// @ts-expect-error
+view.getFloat16('123');
+// @ts-expect-error
+view.getFloat16(0, '123');
+// @ts-expect-error
+view.setFloat16(0, 1.5, '123');
