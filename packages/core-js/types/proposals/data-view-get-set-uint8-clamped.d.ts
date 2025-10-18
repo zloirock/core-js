d105cc47e819b9b8c9949076f03cf615e69f02d4
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-18 22:56:52 +0700

    Add proposal types for stages 3-4

--- packages/core-js/types/proposals/data-view-get-set-uint8-clamped.d.ts
+++ packages/core-js/types/proposals/data-view-get-set-uint8-clamped.d.ts
@@ -1,6 +1,6 @@
 // proposal stage: 1
-// https://github.com/tc39/proposal-setmap-offrom
-interface DataView {
+// https://github.com/tc39/proposal-dataview-get-set-uint8clamped
+interface DataView<TArrayBuffer extends ArrayBufferLike = ArrayBufferLike> {
   getUint8Clamped(byteOffset: number): number;
   setUint8Clamped(byteOffset: number, value: number): void;
 }
