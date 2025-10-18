13c4bce3f4a4354c9e2631a100f666c2b074b769
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-18 22:56:52 +0700

    Add proposal types for stages 3-4

--- packages/core-js/types/proposals/map-upsert.d.ts
+++ packages/core-js/types/proposals/map-upsert.d.ts
@@ -1,11 +1,12 @@
 // proposal stage: 2
 // https://github.com/tc39/proposal-upsert
+
 interface Map<K, V> {
   getOrInsert(key: K, value: V): V;
   getOrInsertComputed<R extends V>(key: K, callbackFn: (key: K) => R): R;
 }
 
-interface WeakMap<K extends WeakKey, V> {
+interface WeakMap<K, V> {
   getOrInsert(key: K, value: V): V;
   getOrInsertComputed<R extends V>(key: K, callbackFn: (key: K) => R): R;
 }
