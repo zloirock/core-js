0c2325954c4887f3cbcbbb11aefa46c64dc0de37
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-23 23:05:19 +0700

    Add proposal types for stages 3-4

---
+++ tests/type-definitions/proposals/pure/regexp-escaping.test.ts
@@ -0,0 +1,25 @@
+import regexpEscape from '@core-js/pure/full/regexp/escape';
+
+const escaped1: string = regexpEscape('foo.*+?^${}()|[]\\');
+const escaped2: string = regexpEscape('');
+const s: string = 'abc';
+const escaped3: string = regexpEscape(s);
+
+// Некорректные вызовы
+
+// @ts-expect-error
+regexpEscape();
+// @ts-expect-error
+regexpEscape(123);
+// @ts-expect-error
+regexpEscape();
+// @ts-expect-error
+regexpEscape({});
+// @ts-expect-error
+regexpEscape(/abc/);
+// @ts-expect-error
+regexpEscape([]);
+// @ts-expect-error
+const wrong: number = regexpEscape('boo');
+// @ts-expect-error
+regexpEscape('foo', 'bar');
