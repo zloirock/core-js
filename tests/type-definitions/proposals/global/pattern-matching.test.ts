703d8a162d44d326a76072f823b75ce848a7fa1e
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-24 18:49:34 +0700

    Add proposal types for stages 3-4

---
+++ tests/type-definitions/proposals/global/pattern-matching.test.ts
@@ -0,0 +1,8 @@
+import 'core-js/full';
+
+const sym: symbol = Symbol.customMatcher;
+
+// @ts-expect-error
+const bad1: string = Symbol.customMatcher;
+// @ts-expect-error
+Symbol['customMatcher'] = Symbol("other");
--- tests/type-definitions/proposals/pure/async-iteration.test.ts
+++ tests/type-definitions/proposals/pure/async-iteration.test.ts
@@ -1,11 +1,9 @@
 import symbolAsyncIterator from '@core-js/pure/full/symbol/async-iterator';
 import $symbol from '@core-js/pure/full/symbol';
 
-const aiter: symbol = symbolAsyncIterator;
-const sym: symbol = symbolAsyncIterator;
-const s: typeof symbolAsyncIterator = symbolAsyncIterator;
+const sym: symbol = $symbol.asyncIterator;
 
 // @ts-expect-error
-const bad1: string = symbolAsyncIterator;
+const bad1: string = $symbol.asyncIterator;
 // @ts-expect-error
 $symbol['asyncIterator'] = $symbol("other");
