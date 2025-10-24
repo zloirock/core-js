18bd7a42d02809c87e909624e6e4081a22d8e738
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-24 21:05:31 +0700

    Add custom types

--- packages/core-js/types/proposals/array-flat-map.d.ts
+++ packages/core-js/types/proposals/array-flat-map.d.ts
@@ -4,8 +4,15 @@
 // For ensuring compatibility with TypeScript standard types, this code is based on:
 // https://github.com/microsoft/TypeScript/blob/069de743dbd17b47cc2fc58e1d16da5410911284/src/lib/es2019.array.d.ts#L46
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
-interface Array<T> {
-  flatMap<U, This = undefined>(callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>, thisArg?: This): U[];
 
-  flat<A, D extends number = 1>(this: A, depth?: D): FlatArray<A, D>[];
+declare global {
+  interface Array<T> {
+    flatMap<U, This = undefined>(callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>, thisArg?: This): U[];
+
+    flat<A, D extends number = 1>(this: A, depth?: D): FlatArray<A, D>[];
+  }
 }
+
+export {};
+
+export type CoreJsArrayFlatMap<T, U, This = undefined> = (callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>, thisArg?: This) => U[];
