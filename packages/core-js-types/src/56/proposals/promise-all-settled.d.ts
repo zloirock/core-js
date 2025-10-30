c60491bf47f5c6f85ec5afaacbaac00150364028
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-30 23:42:20 +0700

    Create package with core-js types

---
+++ packages/core-js-types/src/56/proposals/promise-all-settled.d.ts
@@ -0,0 +1,18 @@
+// proposal stage: 4
+// https://github.com/tc39/proposal-promise-allSettled
+
+// For ensuring compatibility with TypeScript standard types, this code is based on:
+// https://github.com/microsoft/TypeScript/blob/2a90a739c1c1e87e3c3d0c93e16f7e5baadf8035/src/lib/es2020.promise.d.ts
+// License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
+
+import { CoreJsPromiseSettledResult } from '../core-js-types/core-js-types';
+
+declare global {
+  interface PromiseConstructor {
+    allSettled<T extends readonly unknown[] | []>(values: T): Promise<{ -readonly [P in keyof T]: CoreJsPromiseSettledResult<Awaited<T[P]>>; }>;
+
+    allSettled<T>(values: Iterable<T | PromiseLike<T>>): Promise<CoreJsPromiseSettledResult<Awaited<T>>[]>;
+  }
+}
+
+export {};
