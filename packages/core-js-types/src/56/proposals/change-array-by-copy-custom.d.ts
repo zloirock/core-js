9b758de03af2770f1dd47b4358bfae3b05af35c1
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-30 23:42:20 +0700

    Create package with core-js types

---
+++ packages/core-js-types/src/56/proposals/change-array-by-copy-custom.d.ts
@@ -0,0 +1,10 @@
+// proposal stage: 4
+// https://github.com/tc39/proposal-change-array-by-copy
+
+// For ensuring compatibility with TypeScript standard types, this code is based on:
+// https://github.com/microsoft/TypeScript/blob/6afd0fb73fa18a48021ed54f44a0c51794519bf6/src/lib/es2023.array.d.ts
+// License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
+
+declare namespace CoreJs {
+  export type ArrayToSpliced<T> = ((start: number, deleteCount: number, ...items: T[]) => T[]) | ((start: number, deleteCount?: number)=> T[]);
+}
