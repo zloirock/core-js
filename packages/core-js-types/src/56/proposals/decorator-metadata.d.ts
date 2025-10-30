cd7d9f09eed613b0f08f060864d1248a67fd6062
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-10-30 23:42:20 +0700

    Create package with core-js types

---
+++ packages/core-js-types/src/56/proposals/decorator-metadata.d.ts
@@ -0,0 +1,16 @@
+// proposal stage: 3
+// https://github.com/tc39/proposal-decorator-metadata
+
+import { CoreJsDecoratorMetadataObject } from '../core-js-types/core-js-types.js';
+
+declare global {
+  interface SymbolConstructor {
+    readonly metadata: unique symbol;
+  }
+
+  interface Function {
+    [Symbol.metadata]: CoreJsDecoratorMetadataObject | null;
+  }
+}
+
+export {};
