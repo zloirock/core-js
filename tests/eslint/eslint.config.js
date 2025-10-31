a69c64c011bc00a6fb19873073ffb788dcf97335
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-01 01:15:51 +0700

    Proposal await dictionary allSettledKeyed method

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -1335,6 +1335,7 @@ const forbidCompletelyNonExistentBuiltIns = {
   ] }],
   'es/no-nonstandard-promise-properties': [ERROR, { allow: [
     'allKeyed',
+    'allSettledKeyed',
   ] }],
   'es/no-nonstandard-reflect-properties': [ERROR, { allow: [
     // TODO: drop from `core-js@4`
