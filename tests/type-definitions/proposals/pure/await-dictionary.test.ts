ae8848f270f3ecc8451b13eae08dd1a6492fe2ce
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-05 01:44:07 +0700

    allSettledKeyed tests & some improvements

--- tests/type-definitions/proposals/pure/await-dictionary.test.ts
+++ tests/type-definitions/proposals/pure/await-dictionary.test.ts
@@ -1,4 +1,5 @@
 import allKeyed from '@core-js/pure/full/promise/all-keyed';
+import allSettledKeyed from '@core-js/pure/full/promise/all-settled-keyed';
 
 const res: Promise<{ a: number, b: string, c: boolean }> = allKeyed({
   a: Promise.resolve(1),
@@ -17,3 +18,21 @@ allKeyed();
 allKeyed({ a: 1, b: Promise.resolve(2) });
 // @ts-expect-error
 allKeyed([ Promise.resolve(1), Promise.resolve(2) ]);
+
+declare type AllSettledKeyedResult<T> = PromiseFulfilledResult<T> | PromiseRejectedResult;
+const resASK: Promise<{ a: AllSettledKeyedResult<number>, b: AllSettledKeyedResult<string>, c: AllSettledKeyedResult<boolean> }> = allSettledKeyed({
+  a: Promise.resolve(1),
+  b: Promise.resolve('string'),
+  c: Promise.resolve(true),
+});
+
+const resASK2: Promise<{ [sym]: AllSettledKeyedResult<number> }> = allSettledKeyed({
+  [sym]: Promise.resolve(1)
+});
+
+// @ts-expect-error
+allSettledKeyed();
+// @ts-expect-error
+allSettledKeyed({ a: 1, b: Promise.resolve(2) });
+// @ts-expect-error
+allSettledKeyed([ Promise.resolve(1), Promise.resolve(2) ]);
