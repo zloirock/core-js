4692fcf872a00f00df5132be9bcd5c935f05fb10
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-05 01:44:07 +0700

    allSettledKeyed tests & some improvements

--- tests/type-definitions/proposals/global/await-dictionary.test.ts
+++ tests/type-definitions/proposals/global/await-dictionary.test.ts
@@ -18,3 +18,21 @@ Promise.allKeyed();
 Promise.allKeyed({ a: 1, b: Promise.resolve(2) });
 // @ts-expect-error
 Promise.allKeyed([ Promise.resolve(1), Promise.resolve(2) ]);
+
+declare type AllSettledKeyedResult<T> = PromiseFulfilledResult<T> | PromiseRejectedResult;
+const resASK: Promise<{ a: AllSettledKeyedResult<number>, b: AllSettledKeyedResult<string>, c: AllSettledKeyedResult<boolean> }> = Promise.allSettledKeyed({
+  a: Promise.resolve(1),
+  b: Promise.resolve('string'),
+  c: Promise.resolve(true),
+});
+
+const resASK2: Promise<{ [sym]: AllSettledKeyedResult<number> }> = Promise.allSettledKeyed({
+  [sym]: Promise.resolve(1)
+});
+
+// @ts-expect-error
+Promise.allSettledKeyed();
+// @ts-expect-error
+Promise.allSettledKeyed({ a: 1, b: Promise.resolve(2) });
+// @ts-expect-error
+Promise.allSettledKeyed([ Promise.resolve(1), Promise.resolve(2) ]);
