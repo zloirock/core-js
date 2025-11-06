b31a3e152e4d37fe1ae7e5d08bb0fccf306f3dd6
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-06 16:54:04 +0700

    Types improvements

--- packages/core-js-types/src/56/proposals/array-grouping.d.ts
+++ packages/core-js-types/src/56/proposals/array-grouping.d.ts
@@ -5,6 +5,7 @@
 // https://github.com/microsoft/TypeScript/blob/069de743dbd17b47cc2fc58e1d16da5410911284/src/lib/es2024.object.d.ts#L7
 // https://github.com/microsoft/TypeScript/blob/069de743dbd17b47cc2fc58e1d16da5410911284/src/lib/es2024.collection.d.ts#L7
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
+
 interface ObjectConstructor {
   groupBy<K extends PropertyKey, T>(items: Iterable<T>, keySelector: (item: T, index: number) => K): Partial<Record<K, T[]>>;
 }
--- packages/core-js-types/src/56/proposals/iterator-joint.d.ts
+++ packages/core-js-types/src/56/proposals/iterator-joint.d.ts
@@ -1,20 +1,21 @@
 // proposal stage: 2.7
 // https://github.com/tc39/proposal-joint-iteration
 
-import { CoreJsIteratorObject } from '../core-js-types/core-js-types';
+import { CoreJSIteratorObject } from '../core-js-types/core-js-types';
 
 declare global {
   type ZipOptions = {
     mode?: "shortest" | "longest" | "strict";
+
     padding?: object;
   };
 
   interface IteratorConstructor {
-    zip<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[T, U]>;
+    zip<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJSIteratorObject<[T, U]>;
 
-    zipKeyed<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJsIteratorObject<[number, T, U]>;
+    zipKeyed<T, U>(iterables: Iterable<U>, options?: ZipOptions): CoreJSIteratorObject<[number, T, U]>;
 
-    zipKeyed<T, U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): CoreJsIteratorObject<[PropertyKey, T, U]>;
+    zipKeyed<T, U>(record: Record<PropertyKey, Iterable<U>>, options?: ZipOptions): CoreJSIteratorObject<[PropertyKey, T, U]>;
   }
 
   var Iterator: IteratorConstructor;
