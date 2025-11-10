31c05e272a3bc24eb4447ac0a0d7daa940dd5ad6
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-11 00:16:42 +0700

    Iterator join proposal

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -1290,6 +1290,7 @@ const forbidCompletelyNonExistentBuiltIns = {
   ] }],
   'es/no-nonstandard-iterator-prototype-properties': [ERROR, { allow: [
     'chunks',
+    'join',
     'sliding',
     'toAsync',
     'windows',
