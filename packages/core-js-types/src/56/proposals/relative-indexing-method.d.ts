99d302931ec21380bae763fb20a926517598441b
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-13 00:54:43 +0700

    JSDocs for types

--- packages/core-js-types/src/56/proposals/relative-indexing-method.d.ts
+++ packages/core-js-types/src/56/proposals/relative-indexing-method.d.ts
@@ -7,57 +7,113 @@
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
 
 interface String {
+  /**
+   * Returns a new String consisting of the single UTF-16 code unit located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): string | undefined;
 }
 
 interface Array<T> {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): T | undefined;
 }
 
 interface ReadonlyArray<T> {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): T | undefined;
 }
 
 interface Int8Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Uint8Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Uint8ClampedArray {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Int16Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Uint16Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Int32Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Uint32Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Float32Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface Float64Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): number | undefined;
 }
 
 interface BigInt64Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): bigint | undefined;
 }
 
 interface BigUint64Array {
+  /**
+   * Returns the item located at the specified index.
+   * @param index The zero-based index of the desired code unit. A negative index will count back from the last item.
+   */
   at(index: number): bigint | undefined;
 }
--- packages/core-js-types/src/56/proposals/symbol-predicates.d.ts
+++ packages/core-js-types/src/56/proposals/symbol-predicates.d.ts
@@ -2,7 +2,15 @@
 // https://github.com/tc39/proposal-symbol-predicates
 
 interface SymbolConstructor {
+  /**
+   * Determines whether the given value is a registered symbol.
+   * @param value
+   */
   isRegisteredSymbol(value: any): boolean;
 
+  /**
+   * Determines whether the given value is a well-known symbol.
+   * @param value
+   */
   isWellKnownSymbol(value: any): boolean;
 }
