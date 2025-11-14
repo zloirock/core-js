07fc048a29ba54e8dfde6055ecace2e47da259f0
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-14 22:14:08 +0700

    Type definition test dir refactoring & add atob test

---
+++ tests/type-definitions/global/web/atob.global.test.ts
@@ -0,0 +1,11 @@
+import '@core-js/full';
+import '@core-js/types';
+
+const s: string = atob("SGVsbG8gd29ybGQ=");
+
+// @ts-expect-error
+atob();
+// @ts-expect-error
+atob(123);
+// @ts-expect-error
+atob({});
