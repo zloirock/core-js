585cdd4ea94a05e7381644c1d9100fa3cb3a6478
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-18 21:06:28 +0700

    Web types & tests

---
+++ tests/type-definitions/pure/web/btoa.test.ts
@@ -0,0 +1,10 @@
+import $btoa from '@core-js/pure/full/btoa';
+
+const s: string = $btoa("SGVsbG8gd29ybGQ=");
+
+// @ts-expect-error
+$btoa();
+// @ts-expect-error
+$btoa(123);
+// @ts-expect-error
+$btoa({});
