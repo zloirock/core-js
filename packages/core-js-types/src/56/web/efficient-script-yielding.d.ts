1ac7649d9b48ceb97c1b71add430425a7901635b
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-18 21:06:28 +0700

    Web types & tests

---
+++ packages/core-js-types/src/56/web/efficient-script-yielding.d.ts
@@ -0,0 +1,15 @@
+type Immediate = number | object;
+
+/**
+ * Schedules the execution of a function as soon as possible after the current script yields.
+ * @param handler The function to execute.
+ * @param args Arguments to pass to the handler function.
+ * @returns An identifier that can be used to cancel the scheduled function.
+ */
+declare function setImmediate(handler: (...args: any[]) => void, ...args: any[]): Immediate;
+
+/**
+ * Cancels a function scheduled with setImmediate.
+ * @param immediate The identifier of the scheduled function to cancel.
+ */
+declare function clearImmediate(immediate: Immediate): void;
