d91a12f1c7687a1d2779c292588282959eda6579
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-20 00:16:28 +0700

    Fixes after rebase

---
+++ tests/type-definitions/global/proposals/iterator-join.test.ts
@@ -0,0 +1,16 @@
+import 'core-js/full';
+import '@core-js/types';
+
+declare const it: Iterator<number>;
+
+const res1: string = it.join();
+const res2: string = it.join(' ');
+const res3: string = it.join(5);
+const res4: string = it.join(Symbol('x'));
+const res5: string = it.join(undefined);
+const res6: string = it.join(null);
+
+// @ts-expect-error
+it.join('+', '_');
+// @ts-expect-error
+const res7: number = it.join();
