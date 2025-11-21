c8cdccc6de3963a71623629cfc45d513972dcb3c
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-11-21 23:45:21 +0700

    Added proposal type entry-points & tests

--- tests/type-definitions/runner.mjs
+++ tests/type-definitions/runner.mjs
@@ -113,7 +113,10 @@ const prepareEnvironment = async function (environments, coreJsTypes) {
 };
 
 await $`npx -p typescript@5.9 tsc`;
-await $`npx -p typescript@5.9 -p @types/node@24 tsc -p tsconfig.require.json`;
+await $`npx -p typescript@5.9 tsc -p tsconfig.templates.import.json`;
+await $`npx -p typescript@5.9 tsc -p tsconfig.entries.json`;
+await $`npx -p typescript@5.9 -p @types/node@24 tsc -p tsconfig.templates.require.json`;
+
 const numCPUs = os.cpus().length;
 await prepareEnvironment(envs, types);
 await runLimited(taskConfigs, Math.max(numCPUs - 1, 1));
