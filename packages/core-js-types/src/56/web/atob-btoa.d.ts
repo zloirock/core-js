9f71daeba4fd7c1d68a28afb5e278a078e70746a
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-06 03:13:12 +0700

    Pure types refactoring & some improvements

--- packages/core-js-types/src/56/web/atob-btoa.d.ts
+++ packages/core-js-types/src/56/web/atob-btoa.d.ts
@@ -1,4 +1,11 @@
 /**
+ * Decodes a string of data which has been encoded using Base64 encoding.
+ * @param data A base64-encoded string, using the alphabet produced by btoa().
+ * @returns A binary string containing raw bytes decoded from encodedData.
+ */
+declare function atob(data: string): string;
+
+/**
  * Creates a Base64-encoded ASCII string from a binary string
  * (i.e., a string in which each character in the string is treated as a byte of binary data)
  * @param data The binary string to encode
