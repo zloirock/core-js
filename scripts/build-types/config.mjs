1f9f5b6c81bc855c347f74bdedde966021fe7282
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-09 00:23:53 +0700

    package.json builder for types

--- scripts/build-types/config.mjs
+++ scripts/build-types/config.mjs
@@ -3,5 +3,7 @@ export default {
   bundleName: 'core-js-types',
   latestTsVersion: 59,
   packageName: '@core-js/',
+  packageTemplate: 'scripts/build-types/package.tpl.json',
+  packageDir: 'packages/core-js-types/',
   srcDir: 'packages/core-js-types/src/',
 };
