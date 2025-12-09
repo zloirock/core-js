d4762562cacd2f73d4a7616a6ae6d1e5e8252368
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-09 14:13:42 +0700

    Types dir refactoring & some fixes & some improvements

--- packages/core-js-types/src/52/web/btoa.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Creates a Base64-encoded ASCII string from a binary string
- * (i.e., a string in which each character in the string is treated as a byte of binary data)
- * @param data The binary string to encode
- * @returns An ASCII string containing the Base64 representation of data
- */
-declare function btoa(data: string): string;
