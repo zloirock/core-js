123a4a9724fe01ff841302edd7c4f64fd36c3c21
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-09 14:13:42 +0700

    Types dir refactoring & some fixes & some improvements

--- packages/core-js-types/src/52/web/atob.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Decodes a string of data which has been encoded using Base64 encoding.
- * @param data A base64-encoded string, using the alphabet produced by btoa().
- * @returns A binary string containing raw bytes decoded from encodedData.
- */
-declare function atob(data: string): string;
