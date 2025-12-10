a95878e3e92fdb0e81c8b72d42c5a858211083a7
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-10 23:44:54 +0700

    Types package refactoring

--- packages/core-js-types/src/base/proposals/promise-try.d.ts
+++ packages/core-js-types/src/base/proposals/promise-try.d.ts
@@ -1,7 +1,7 @@
 // proposal stage: 4
 // https://github.com/tc39/proposal-promise-try
 
-// For ensuring compatibility with TypeScript standard types, this code is based on:
+// For ensuring compatibility with TypeScript standard types, this code is aligned with:
 // https://github.com/microsoft/TypeScript/blob/af3a3779de6bc27619c85077e1b4d1de8feddd35/src/lib/esnext.promise.d.ts
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
 
