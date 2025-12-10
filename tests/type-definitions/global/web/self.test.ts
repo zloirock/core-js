d83c6696e35bb5be0bfc5c03a1c195abc34b92da
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-10 20:01:59 +0700

    Add index.d.ts & rebuild all type entry points & move test-type-definitions to test-raw command

--- tests/type-definitions/global/web/self.test.ts
+++ tests/type-definitions/global/web/self.test.ts
@@ -1,6 +1,5 @@
 // todo add after it becomes possible to create a type
-// import '@core-js/full';
-// import '@core-js/types';
+// import 'core-js/full';
 //
 // const ref: typeof globalThis = self;
 //
