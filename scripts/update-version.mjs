a4361f495038f9316c36d8030e1c43a8e35401a9
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-19 19:07:06 +0700

    Add update types README.md in update-version script

--- scripts/update-version.mjs
+++ scripts/update-version.mjs
@@ -18,6 +18,7 @@ const CHANGELOG = 'CHANGELOG.md';
 const LICENSE = 'LICENSE';
 const README = 'README.md';
 const README_COMPAT = 'packages/core-js-compat/README.md';
+const README_TYPES = 'packages/core-js-types/README.md';
 const SHARED = 'packages/core-js/internals/shared-store.js';
 const BUILDER_CONFIG = 'packages/core-js-builder/config.js';
 const USAGE = 'docs/web/docs/usage.md';
@@ -33,6 +34,9 @@ await writeFile(README, readme.replaceAll(PREV_VERSION, NEW_VERSION).replaceAll(
 
 const readmeCompat = await readFile(README_COMPAT, 'utf8');
 await writeFile(README_COMPAT, readmeCompat.replaceAll(PREV_VERSION_MINOR, NEW_VERSION_MINOR));
+
+const readmeTypes = await readFile(README_TYPES, 'utf8');
+await writeFile(README_TYPES, readmeTypes.replaceAll(PREV_VERSION_MINOR, NEW_VERSION_MINOR));
 
 const shared = await readFile(SHARED, 'utf8');
 await writeFile(SHARED, shared.replaceAll(PREV_VERSION, NEW_VERSION).replaceAll(OLD_YEAR, CURRENT_YEAR));
