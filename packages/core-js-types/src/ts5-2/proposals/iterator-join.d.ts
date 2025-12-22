8a3af808eb188f986e9cfd47eadb9460ec639a00
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-23 02:40:10 +0700

    Types for TS 5.5 stabilization

--- packages/core-js-types/src/ts5-2/proposals/iterator-join.d.ts
+++
@@ -1,6 +0,0 @@
-// proposal stage: 0
-// https://github.com/bakkot/proposal-iterator-join
-
-interface Iterator<T> {
-  join(separator?: unknown): string;
-}
--- packages/core-js-types/src/ts5-2/proposals/string-dedent.d.ts
+++
@@ -1,6 +0,0 @@
-// proposal stage: 2
-// https://github.com/tc39/proposal-string-dedent
-
-interface StringConstructor {
-  dedent(strings: TemplateStringsArray, ...values: any[]): string;
-}
