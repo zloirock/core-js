98e029650b2dc23fe57ddbb519e1d03639344ccc
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-23 02:39:51 +0700

    Types stabilization

--- tests/type-definitions/global/proposals/promise-all-settled.test.ts
+++ tests/type-definitions/global/proposals/promise-all-settled.test.ts
@@ -5,26 +5,30 @@ const arr = [Promise.resolve(1), Promise.resolve(2)];
 const strArr = ["a", "b", "c"];
 const promiseLike = { then: (cb: (val: number) => void) => cb(42) };
 
-type PromiseResult<T> = PromiseFulfilledResult<T> | PromiseRejectedResult;
+interface CoreJSPromiseResult<T> {
+  status: string;
+  value?: T;
+  reason?: any;
+}
 
 const settled1: Promise<[
-  PromiseResult<number>,
-  PromiseResult<string>,
-  PromiseResult<number>
+  CoreJSPromiseResult<number>,
+  CoreJSPromiseResult<string>,
+  CoreJSPromiseResult<number>
 ]> = Promise.allSettled(promises);
 
-const settled2: Promise<PromiseResult<number>[]> = Promise.allSettled([Promise.resolve(10), Promise.resolve(20), 30]);
-const settled3: Promise<PromiseResult<string>[]> = Promise.allSettled(strArr);
-const settled4: Promise<PromiseResult<number>[]> = Promise.allSettled(new Set([1, 2, 3]));
-const settled5: Promise<PromiseResult<number>[]> = Promise.allSettled([promiseLike]);
+const settled2: Promise<CoreJSPromiseResult<number>[]> = Promise.allSettled([Promise.resolve(10), Promise.resolve(20), 30]);
+const settled3: Promise<CoreJSPromiseResult<string>[]> = Promise.allSettled(strArr);
+const settled4: Promise<CoreJSPromiseResult<number>[]> = Promise.allSettled(new Set([1, 2, 3]));
+const settled5: Promise<CoreJSPromiseResult<number>[]> = Promise.allSettled([promiseLike]);
 
 const emptyTuple: [] = [];
 const settled6: Promise<[]> = Promise.allSettled(emptyTuple);
 
 const mixedTuple = [42, Promise.resolve("bar")] as const;
 const settled7: Promise<[
-  PromiseResult<number>,
-  PromiseResult<string>
+  CoreJSPromiseResult<number>,
+  CoreJSPromiseResult<string>
 ]> = Promise.allSettled(mixedTuple);
 
 // @ts-expect-error
