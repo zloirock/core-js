9eb17d0f615b523a91ffcd2f09997008b287e487
Sergey Nechaev <slow_cheetah@inbox.ru>
2025-12-23 02:40:10 +0700

    Types for TS 5.5 stabilization

--- packages/core-js-types/src/ts5-2/proposals/regexp-escaping.d.ts
+++
@@ -1,8 +0,0 @@
-// proposal stage: 4
-// https://github.com/tc39/proposal-regex-escaping
-
-interface RegExpConstructor {
-  escape(string: string): string;
-}
-
-declare var RegExp: RegExpConstructor;
