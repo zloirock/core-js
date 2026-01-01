993dd21f81fde28c8aa9a5b2e5d8d820dbb507c8
Denis Pushkarev <zloirock@zloirock.ru>
2026-01-01 02:21:40 +0200

    update `eslint-plugin-package-json`

--- tests/eslint/eslint.config.js
+++ tests/eslint/eslint.config.js
@@ -2228,6 +2228,8 @@ const packagesPackageJSON = {
   // requires the `exports` property to be present
   // TODO: core-js@4
   // 'package-json/require-exports': ERROR,
+  // requires the `homepage` property to be present
+  'package-json/require-homepage': ERROR,
   // requires the `license` property to be present
   'package-json/require-license': ERROR,
   // requires the `name` property to be present
