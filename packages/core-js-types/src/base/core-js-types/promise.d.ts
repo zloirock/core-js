e3d4d464d3fd727f7f7b8316ad8cdbf7f315697e
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-06 03:36:59 +0700

    Return our own Promise type in pure types

---
+++ packages/core-js-types/src/base/core-js-types/promise.d.ts
@@ -0,0 +1,58 @@
+declare namespace CoreJS {
+  export interface CoreJSPromise<T> extends Promise<T> {}
+
+  export interface CoreJSPromiseConstructor {
+    readonly prototype: CoreJSPromise<any>;
+
+    /**
+     * Creates a new Promise.
+     * @param executor A callback used to initialize the promise. This callback is passed two arguments:
+     * a resolve callback used to resolve the promise with a value or the result of another promise,
+     * and a reject callback used to reject the promise with a provided reason or error.
+     */
+    new <T>(executor: (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): CoreJSPromise<T>;
+
+    /**
+     * Creates a Promise that is resolved with an array of results when all of the provided Promises
+     * resolve, or rejected when any Promise is rejected.
+     * @param values An array of Promises.
+     * @returns A new Promise.
+     */
+    all<T extends readonly unknown[] | []>(values: T): CoreJSPromise<{ -readonly [P in keyof T]: Awaited<T[P]>; }>;
+
+    /**
+     * Creates a Promise that is resolved or rejected when any of the provided Promises are resolved
+     * or rejected.
+     * @param values An array of Promises.
+     * @returns A new Promise.
+     */
+    race<T extends readonly unknown[] | []>(values: T): CoreJSPromise<Awaited<T[number]>>;
+
+    /**
+     * Creates a new rejected promise for the provided reason.
+     * @param reason The reason the promise was rejected.
+     * @returns A new rejected Promise.
+     */
+    reject<T = never>(reason?: any): CoreJSPromise<T>;
+
+    /**
+     * Creates a new resolved promise.
+     * @returns A resolved promise.
+     */
+    resolve(): CoreJSPromise<void>;
+    /**
+     * Creates a new resolved promise for the provided value.
+     * @param value A promise.
+     * @returns A promise whose internal state matches the provided promise.
+     */
+    resolve<T>(value: T): CoreJSPromise<Awaited<T>>;
+    /**
+     * Creates a new resolved promise for the provided value.
+     * @param value A promise.
+     * @returns A promise whose internal state matches the provided promise.
+     */
+    resolve<T>(value: T | PromiseLike<T>): CoreJSPromise<Awaited<T>>;
+  }
+
+  var CoreJSPromise: CoreJSPromiseConstructor;
+}
--- tests/type-definitions/pure/proposals/promise-try.test.ts
+++ tests/type-definitions/pure/proposals/promise-try.test.ts
@@ -1,18 +1,19 @@
 import promiseTry from '@core-js/pure/full/promise/try';
 import promiseResolve from '@core-js/pure/full/promise/resolve';
+import CoreJSPromiseLike from '../../helpers';
 
-const pt1: Promise<number> = promiseTry(() => 42);
-const pt2: Promise<string> = promiseTry(() => promiseResolve("hi"));
-const pt3: Promise<number> = promiseTry((a: number, b: number) => a + b, 1, 2);
-const pt4: Promise<string> = promiseTry((x: string) => x + "!!", "test");
-const pt5: Promise<void> = promiseTry(() => {});
-const pt6: Promise<boolean> = promiseTry((b: boolean) => b, false);
+const pt1: CoreJSPromiseLike<number> = promiseTry(() => 42);
+const pt2: CoreJSPromiseLike<string> = promiseTry(() => promiseResolve("hi"));
+const pt3: CoreJSPromiseLike<number> = promiseTry((a: number, b: number) => a + b, 1, 2);
+const pt4: CoreJSPromiseLike<string> = promiseTry((x: string) => x + "!!", "test");
+const pt5: CoreJSPromiseLike<void> = promiseTry(() => {});
+const pt6: CoreJSPromiseLike<boolean> = promiseTry((b: boolean) => b, false);
 
-const pt7: Promise<number> = promiseTry((a: number, b: string, c: boolean) => c ? a : Number(b), 10, "100", true);
-const pt8: Promise<string> = promiseTry((a: string) => promiseResolve(a), "bar");
+const pt7: CoreJSPromiseLike<number> = promiseTry((a: number, b: string, c: boolean) => c ? a : Number(b), 10, "100", true);
+const pt8: CoreJSPromiseLike<string> = promiseTry((a: string) => promiseResolve(a), "bar");
 
-declare function returnsPromise<T>(): Promise<T>;
-const pt9: Promise<number> = promiseTry(() => returnsPromise<number>());
+declare function returnsPromise<T>(): CoreJSPromiseLike<T>;
+const pt9: CoreJSPromiseLike<number> = promiseTry(() => returnsPromise<number>());
 
 // @ts-expect-error
 promiseTry();
