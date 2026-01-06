891e84adbe3d93d9e3e6d8bc53e7a8e3a10f69cf
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-07 03:53:05 +0700

    Return our own types in pure types

---
+++ packages/core-js-types/src/base/pure/proposals/set-methods-custom.d.ts
@@ -0,0 +1,23 @@
+/// <reference types="../../core-js-types/set.d.ts" />
+
+// Motivation: Custom type needed to keep generics strict & return our CoreJSSet type
+
+// https://github.com/tc39/proposal-set-methods
+
+// For ensuring compatibility with TypeScript standard types, this code is aligned with:
+// https://github.com/microsoft/TypeScript/blob/f450c1b80ce6dc7b04e81899db00534018932234/src/lib/esnext.collection.d.ts
+// License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
+
+declare namespace CoreJS {
+  interface ReadonlySetLike<T> {
+    keys(): Iterator<T>;
+
+    has(value: T): boolean;
+
+    readonly size: number;
+  }
+
+  export type SetUnion<T, U> = (other: ReadonlySetLike<U>) => CoreJS.CoreJSSet<T | U>;
+
+  export type SetSymmetricDifference<T, U> = (other: ReadonlySetLike<U>) => CoreJS.CoreJSSet<T | U>;
+}
