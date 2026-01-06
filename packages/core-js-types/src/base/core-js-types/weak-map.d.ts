85276e7c6604d7364426b68b532a5d0af45acd4c
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-07 03:53:05 +0700

    Return our own types in pure types

---
+++ packages/core-js-types/src/base/core-js-types/weak-map.d.ts
@@ -0,0 +1,12 @@
+declare namespace CoreJS {
+  export interface CoreJSWeakMap<K extends WeakKey, V> extends WeakMap<K, V> {}
+
+  export interface CoreJSWeakMapConstructor {
+    readonly prototype: CoreJSWeakMap<WeakKey, any>;
+
+    new <K extends WeakKey = WeakKey, V = any>(entries?: readonly (readonly [K, V])[] | null): CoreJSWeakMap<K, V>;
+    new <K extends WeakKey, V>(iterable: Iterable<readonly [K, V]>): CoreJSWeakMap<K, V>;
+  }
+
+  var CoreJSWeakMap: CoreJSWeakMapConstructor;
+}
