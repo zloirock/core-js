7a8695284a7de776e0f9a861ba8e74ddfc9beaa9
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 03:54:58 +0700

    Fix String dedent

--- packages/core-js-types/src/base/proposals/string-dedent.d.ts
+++ packages/core-js-types/src/base/proposals/string-dedent.d.ts
@@ -4,20 +4,13 @@ interface StringConstructor {
   /**
    * Template tag that removes common leading whitespace from every line in the resulting string,
    * preserving relative indentation and blank lines.
-   * @param template The template strings array, strings array, or a single string.
+   * @param template The template strings array, a single string, and function.
    * @param values Values to be interpolated into the template string.
    * @returns The dedented string.
    */
-  dedent(template: TemplateStringsArray | ArrayLike<string>, ...values: any[]): string;
   dedent(template: { raw: readonly string[] }, ...values: any[]): string;
   dedent(template: string): string;
-}
-
-interface String {
-  /**
-   * Template tag that removes common leading whitespace from every line in the resulting string,
-   * preserving relative indentation and blank lines.
-   * @returns The dedented string.
-   */
-  dedent(): string;
+  dedent<R extends string | { raw: readonly string[] }>(
+    fn: (template: { raw: readonly string[] }, ...values: any[]) => R
+  ): (template: { raw: readonly string[] }, ...values: any[]) => string;
 }
