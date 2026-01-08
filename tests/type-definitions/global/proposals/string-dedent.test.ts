c19b0e1cab0fb4df50bce5de6e1c2371996b167c
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 03:54:58 +0700

    Fix String dedent

--- tests/type-definitions/global/proposals/string-dedent.test.ts
+++ tests/type-definitions/global/proposals/string-dedent.test.ts
@@ -5,11 +5,14 @@ const rdedent2: string = String.dedent`line1
   line2
   line3`;
 
-const tpl = Object.assign(['foo', 'bar'], { raw: ['foo', 'bar'] }) as TemplateStringsArray;
+const tpl = Object.assign(['foo', 'bar'], { raw: ['foo', 'bar'] });
 String.dedent(tpl, 1, 2);
 
 String.dedent({ raw: ["a\n  b\n", "\n  c\n"] }, 1, 2);
 
+String.dedent(() => 'template string');
+
+// @ts-expect-error
 'string\ndedent'.dedent();
 
 // @ts-expect-error
