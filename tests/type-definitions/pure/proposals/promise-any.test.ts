c137c37c3361ab69cb1c8ddbf9945cda87c2ec78
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-08 22:10:09 +0700

    Check promise like types

--- tests/type-definitions/pure/proposals/promise-any.test.ts
+++ tests/type-definitions/pure/proposals/promise-any.test.ts
@@ -1,6 +1,6 @@
 import promiseAny from '@core-js/pure/full/promise/any';
 import promiseResolve from '@core-js/pure/full/promise/resolve';
-import { CoreJSPromiseLike } from '../../helpers';
+import { AnyPromiseLike } from '../../helpers';
 
 const arr = [promiseResolve(1), promiseResolve("foo"), 3] as const;
 const justNumbers = [1, 2, 3];
@@ -9,16 +9,16 @@ const promiseLike = { then: (cb: (val: number) => void) => cb(123) };
 const emptyTuple: [] = [];
 const mixed = [true, promiseResolve("z")] as const;
 
-const any1: CoreJSPromiseLike<string | number> = promiseAny(arr);
-const any2: CoreJSPromiseLike<string | number> = promiseAny(["x", "y", promiseResolve(5)]);
-const any3: CoreJSPromiseLike<never> = promiseAny(emptyTuple);
-const any4: CoreJSPromiseLike<boolean | string> = promiseAny(mixed);
+const any1: AnyPromiseLike<string | number> = promiseAny(arr);
+const any2: AnyPromiseLike<string | number> = promiseAny(["x", "y", promiseResolve(5)]);
+const any3: AnyPromiseLike<never> = promiseAny(emptyTuple);
+const any4: AnyPromiseLike<boolean | string> = promiseAny(mixed);
 
-const any5: CoreJSPromiseLike<number> = promiseAny(justNumbers);
-const any6: CoreJSPromiseLike<string> = promiseAny(setOfStrings);
-const any7: CoreJSPromiseLike<number> = promiseAny([promiseLike]);
-const any8: CoreJSPromiseLike<number> = promiseAny(new Set([1]));
-const any9: CoreJSPromiseLike<unknown> = promiseAny([promiseResolve()]);
+const any5: AnyPromiseLike<number> = promiseAny(justNumbers);
+const any6: AnyPromiseLike<string> = promiseAny(setOfStrings);
+const any7: AnyPromiseLike<number> = promiseAny([promiseLike]);
+const any8: AnyPromiseLike<number> = promiseAny(new Set([1]));
+const any9: AnyPromiseLike<unknown> = promiseAny([promiseResolve()]);
 
 // @ts-expect-error
 promiseAny();
