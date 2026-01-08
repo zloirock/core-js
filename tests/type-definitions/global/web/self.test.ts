32143e7c4198805e2e6bd2ba61bb3d2bd18f3d67
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-08 22:44:29 +0700

    Self test

--- tests/type-definitions/global/web/self.test.ts
+++ tests/type-definitions/global/web/self.test.ts
@@ -1,9 +1,8 @@
-// todo add after it becomes possible to create a type
-// import 'core-js/full';
-//
-// const ref: typeof globalThis = self;
-//
-// // @ts-expect-error
-// self();
-// // @ts-expect-error
-// self = {};
+import 'core-js/full';
+
+const ref: typeof globalThis = self;
+
+// @ts-expect-error
+self();
+// @ts-expect-error
+self = {};
