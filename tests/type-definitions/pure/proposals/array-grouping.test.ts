4d26238babb89bb276f06ecbe13b80b12cdb41af
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 03:27:17 +0700

    Improve returning types checking & fix iterator-helpers types

--- tests/type-definitions/pure/proposals/array-grouping.test.ts
+++ tests/type-definitions/pure/proposals/array-grouping.test.ts
@@ -1,12 +1,12 @@
 import objectGroupBy from '@core-js/pure/full/object/group-by';
 import mapGroupBy from '@core-js/pure/full/map/group-by';
-import { CoreJSMapLike } from '../../helpers';
+import { CoreJSMapAndMapLike } from '../../helpers';
 
 const arr = [1, 2, 3, 4, 5];
 const objGroup: Partial<Record<'even' | 'odd', number[]>> = objectGroupBy(arr, x => x % 2 === 0 ? 'even' : 'odd');
-const mapGroup: CoreJSMapLike<'even' | 'odd', number[]> = mapGroupBy(arr, x => x % 2 === 0 ? 'even' : 'odd');
+const mapGroup: CoreJSMapAndMapLike<'even' | 'odd', number[]> = mapGroupBy(arr, x => x % 2 === 0 ? 'even' : 'odd');
 const objGroup2: Partial<Record<string, string[]>> = objectGroupBy(['foo', 'bar', 'baz'], (s, i) => i > 1 ? s[0] : 'x');
-const mapGroup2: CoreJSMapLike<string, string[]> = mapGroupBy(['foo', 'bar', 'baz'], (s, i) => i > 1 ? s[0] : 'x');
+const mapGroup2: CoreJSMapAndMapLike<string, string[]> = mapGroupBy(['foo', 'bar', 'baz'], (s, i) => i > 1 ? s[0] : 'x');
 
 objectGroupBy('test', c => c);
 objectGroupBy(new Set([1, 2, 3]), item => item > 2 ? 'big' : 'small');
