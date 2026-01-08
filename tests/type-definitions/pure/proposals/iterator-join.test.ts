f68267b582b8f7cf4c5182c4f506d4c55d20970a
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 01:51:58 +0700

    Some fixes & improvements

--- tests/type-definitions/pure/proposals/iterator-join.test.ts
+++ tests/type-definitions/pure/proposals/iterator-join.test.ts
@@ -1,15 +1,15 @@
-import $join from '@core-js/pure/full/iterator/join';
+import iteratorJoin from '@core-js/pure/full/iterator/join';
 
 declare const it: Iterator<number>;
 
-const res1: string = $join(it, );
-const res2: string = $join(it, ' ');
-const res3: string = $join(it, 5);
-const res4: string = $join(it, Symbol('x'));
-const res5: string = $join(it, undefined);
-const res6: string = $join(it, null);
+const res1: string = iteratorJoin(it, );
+const res2: string = iteratorJoin(it, ' ');
+const res3: string = iteratorJoin(it, 5);
+const res4: string = iteratorJoin(it, Symbol('x'));
+const res5: string = iteratorJoin(it, undefined);
+const res6: string = iteratorJoin(it, null);
 
 // @ts-expect-error
-$join(it, '+', '_');
+iteratorJoin(it, '+', '_');
 // @ts-expect-error
-const res7: number = $join(it, );
+const res7: number = iteratorJoin(it, );
