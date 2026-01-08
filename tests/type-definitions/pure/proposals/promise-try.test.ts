90a7fc8d4caaab512509f4e33542159c7c92a883
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 03:27:17 +0700

    Improve returning types checking & fix iterator-helpers types

--- tests/type-definitions/pure/proposals/promise-try.test.ts
+++ tests/type-definitions/pure/proposals/promise-try.test.ts
@@ -1,19 +1,19 @@
 import promiseTry from '@core-js/pure/full/promise/try';
 import promiseResolve from '@core-js/pure/full/promise/resolve';
-import { CoreJSPromiseOrPromiseLike } from '../../helpers';
+import { CoreJSPromiseAndPromiseLike } from '../../helpers';
 
-const pt1: CoreJSPromiseOrPromiseLike<number> = promiseTry(() => 42);
-const pt2: CoreJSPromiseOrPromiseLike<string> = promiseTry(() => promiseResolve("hi"));
-const pt3: CoreJSPromiseOrPromiseLike<number> = promiseTry((a: number, b: number) => a + b, 1, 2);
-const pt4: CoreJSPromiseOrPromiseLike<string> = promiseTry((x: string) => x + "!!", "test");
-const pt5: CoreJSPromiseOrPromiseLike<void> = promiseTry(() => {});
-const pt6: CoreJSPromiseOrPromiseLike<boolean> = promiseTry((b: boolean) => b, false);
+const pt1: CoreJSPromiseAndPromiseLike<number> = promiseTry(() => 42);
+const pt2: CoreJSPromiseAndPromiseLike<string> = promiseTry(() => promiseResolve("hi"));
+const pt3: CoreJSPromiseAndPromiseLike<number> = promiseTry((a: number, b: number) => a + b, 1, 2);
+const pt4: CoreJSPromiseAndPromiseLike<string> = promiseTry((x: string) => x + "!!", "test");
+const pt5: CoreJSPromiseAndPromiseLike<void> = promiseTry(() => {});
+const pt6: CoreJSPromiseAndPromiseLike<boolean> = promiseTry((b: boolean) => b, false);
 
-const pt7: CoreJSPromiseOrPromiseLike<number> = promiseTry((a: number, b: string, c: boolean) => c ? a : Number(b), 10, "100", true);
-const pt8: CoreJSPromiseOrPromiseLike<string> = promiseTry((a: string) => promiseResolve(a), "bar");
+const pt7: CoreJSPromiseAndPromiseLike<number> = promiseTry((a: number, b: string, c: boolean) => c ? a : Number(b), 10, "100", true);
+const pt8: CoreJSPromiseAndPromiseLike<string> = promiseTry((a: string) => promiseResolve(a), "bar");
 
-declare function returnsPromise<T>(): CoreJSPromiseOrPromiseLike<T>;
-const pt9: CoreJSPromiseOrPromiseLike<number> = promiseTry(() => returnsPromise<number>());
+declare function returnsPromise<T>(): CoreJSPromiseAndPromiseLike<T>;
+const pt9: CoreJSPromiseAndPromiseLike<number> = promiseTry(() => returnsPromise<number>());
 
 // @ts-expect-error
 promiseTry();
