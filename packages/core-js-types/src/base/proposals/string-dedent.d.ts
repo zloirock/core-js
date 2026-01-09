141f19cb82b01b2d39ffbc2eb43d56179f9f8a52
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-09 22:24:07 +0700

    Fix String dedent

--- packages/core-js-types/src/base/proposals/string-dedent.d.ts
+++ packages/core-js-types/src/base/proposals/string-dedent.d.ts
@@ -1,16 +1,15 @@
 // https://github.com/tc39/proposal-string-dedent
 
+type CoreJSTemplateTag = (template: { raw: readonly string[] }, ...values: any[]) => any;
+
 interface StringConstructor {
   /**
    * Template tag that removes common leading whitespace from every line in the resulting string,
    * preserving relative indentation and blank lines.
-   * @param template The template strings array, a single string, and function.
+   * @param template The template strings array or function.
    * @param values Values to be interpolated into the template string.
    * @returns The dedented string.
    */
   dedent(template: { raw: readonly string[] }, ...values: any[]): string;
-  dedent(template: string): string;
-  dedent<R extends string | { raw: readonly string[] }>(
-    fn: (template: { raw: readonly string[] }, ...values: any[]) => R
-  ): (template: { raw: readonly string[] }, ...values: any[]) => string;
+  dedent<T extends CoreJSTemplateTag>(tag: T): T;
 }
