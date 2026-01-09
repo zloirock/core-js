1b524a3391d71bc89060dc56bb79333115b6b380
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-10 01:09:43 +0700

    Improve type tests

--- tests/type-definitions/runner.mjs
+++ tests/type-definitions/runner.mjs
@@ -123,6 +123,11 @@ await $`npx -p typescript@5.9 tsc -p tsconfig.entries.pure.json`;
 await $`npx -p typescript@5.9 -p @types/node@24 tsc -p tsconfig.templates.require.json`;
 
 if (!ALL_TESTS) {
+  await $`npx -p typescript@5.6 tsc -p pure/tsconfig.json --target es6 --lib es6`;
+  await $`npx -p typescript@5.6 tsc -p pure/tsconfig.json --target esnext --lib esnext`;
+  await $`npx -p typescript@5.6 tsc -p global/tsconfig.json --target es6 --lib es6,dom`;
+  await $`npx -p typescript@5.6 tsc -p global/tsconfig.json --target esnext --lib esnext,dom`;
+
   await $`npx -p typescript@5.9 tsc -p pure/tsconfig.json --target es6 --lib es6`;
   await $`npx -p typescript@5.9 tsc -p pure/tsconfig.json --target es2023 --lib es2023`;
   await $`npx -p typescript@5.9 tsc -p pure/tsconfig.json --target esnext --lib esnext`;
@@ -135,4 +140,5 @@ if (!ALL_TESTS) {
   await runLimited(taskConfigs, Math.max(numCPUs - 1, 1));
   await clearTmpDir();
   echo(`Tested: ${ chalk.green(tested) }, Failed: ${ chalk.red(failed) }`);
+  if (failed) process.exit(1);
 }
