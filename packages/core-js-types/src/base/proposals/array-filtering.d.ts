967f71487b3c8990fd89b67338758057d5bef095
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-12 01:04:51 +0700

    Add types for ReadonlyArray methods

--- packages/core-js-types/src/base/proposals/array-filtering.d.ts
+++ packages/core-js-types/src/base/proposals/array-filtering.d.ts
@@ -11,6 +11,17 @@ interface Array<T> { // @type-options no-redefine
   filterReject(callbackFn: (value: T, index: number, target: T[]) => boolean, thisArg?: any): T[];
 }
 
+interface ReadonlyArray<T> { // @type-options no-export
+  /**
+   * Removes the items that return true
+   * @param callbackFn A function that accepts up to three arguments. The filterReject method calls the
+   * callbackFn function one time for each element in the array.
+   * @param thisArg If provided, it will be used as this value for each invocation of
+   * predicate. If it is not provided, undefined is used instead.
+   */
+  filterReject(callbackFn: (value: T, index: number, target: T[]) => boolean, thisArg?: any): T[];
+}
+
 interface Int8Array { // @type-options no-export
   /**
    * Removes the items that return true
--- packages/core-js-types/src/base/proposals/array-includes.d.ts
+++ packages/core-js-types/src/base/proposals/array-includes.d.ts
@@ -13,6 +13,15 @@ interface Array<T> { // @type-options no-redefine
   includes(searchElement: T, fromIndex?: number): boolean;
 }
 
+interface ReadonlyArray<T> { // @type-options no-export
+  /**
+   * Determines whether an array includes a certain element, returning true or false as appropriate.
+   * @param searchElement The element to search for.
+   * @param fromIndex The position in this array at which to begin searching for searchElement.
+   */
+  includes(searchElement: T, fromIndex?: number): boolean;
+}
+
 interface Int8Array { // @type-options no-export
   /**
    * Determines whether an array includes a certain element, returning true or false as appropriate.
