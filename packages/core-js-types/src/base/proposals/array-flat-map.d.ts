82cffa2b3b7e43151c725eb14aba4e427107f94f
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-12 01:04:51 +0700

    Add types for ReadonlyArray methods

--- packages/core-js-types/src/base/proposals/array-flat-map.d.ts
+++ packages/core-js-types/src/base/proposals/array-flat-map.d.ts
@@ -25,3 +25,23 @@ interface Array<T> { // @type-options no-redefine
    */
   flat<A, D extends number = 1>(this: A, depth?: D): CoreJS.CoreJSFlatArray<A, D>[];
 }
+
+interface ReadonlyArray<T> { // @type-options no-export
+  /**
+   * Calls a defined callback function on each element of an array. Then, flattens the result into
+   * a new array.
+   * This is identical to a map followed by flat with depth 1.
+   * @param callback A function that accepts up to three arguments. The flatMap method calls the
+   * callback function one time for each element in the array.
+   * @param thisArg An object to which this keyword can refer in the callback function. If
+   * thisArg is omitted, undefined is used as this value.
+   */
+  flatMap<U, This = undefined>(callback: (this: This, value: T, index: number, array: T[]) => U | ReadonlyArray<U>, thisArg?: This): U[];
+
+  /**
+   * Returns a new array with all sub-array elements concatenated into it recursively up to the
+   * specified depth.
+   * @param depth The maximum recursion depth
+   */
+  flat<A, D extends number = 1>(this: A, depth?: D): CoreJS.CoreJSFlatArray<A, D>[];
+}
