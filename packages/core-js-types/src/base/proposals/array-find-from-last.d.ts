6ca9901f7699c30cb3c7cc7ce02f0381024f59ce
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-12 01:04:51 +0700

    Add types for ReadonlyArray methods

--- packages/core-js-types/src/base/proposals/array-find-from-last.d.ts
+++ packages/core-js-types/src/base/proposals/array-find-from-last.d.ts
@@ -29,6 +29,31 @@ interface Array<T> { // @type-options no-redefine
   findLastIndex(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): number;
 }
 
+interface ReadonlyArray<T> { // @type-options no-export
+  /**
+   * Returns the value of the last element in the array where predicate is true, and undefined
+   * otherwise.
+   * @param predicate findLast calls predicate once for each element of the array, in descending
+   * order, until it finds one where predicate returns true. If such an element is found, findLast
+   * immediately returns that element value. Otherwise, findLast returns undefined.
+   * @param thisArg If provided, it will be used as this value for each invocation of
+   * predicate. If it is not provided, undefined is used instead.
+   */
+  findLast<S extends T>(predicate: (value: T, index: number, array: T[]) => value is S, thisArg?: any): S | undefined;
+  findLast(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): T | undefined;
+
+  /**
+   * Returns the index of the last element in the array where predicate is true, and -1
+   * otherwise.
+   * @param predicate findLastIndex calls predicate once for each element of the array, in descending
+   * order, until it finds one where predicate returns true. If such an element is found,
+   * findLastIndex immediately returns that element index. Otherwise, findLastIndex returns -1.
+   * @param thisArg If provided, it will be used as this value for each invocation of
+   * predicate. If it is not provided, undefined is used instead.
+   */
+  findLastIndex(predicate: (value: T, index: number, array: T[]) => unknown, thisArg?: any): number;
+}
+
 interface Int8Array { // @type-options no-export
   /**
    * Returns the value of the last element in the array where predicate is true, and undefined
