36a0fc66b4bf37aae8d5e1a873e6e9dbe4d8f141
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-14 00:39:56 +0700

    Fallbacks for DOM

--- packages/core-js/modules/web.dom-exception.constructor.js
+++ packages/core-js/modules/web.dom-exception.constructor.js
@@ -1,4 +1,4 @@
-// no types: has types only for the pure version
+// types: web/dom-exception
 'use strict';
 var $ = require('../internals/export');
 var getBuiltIn = require('../internals/get-built-in');
