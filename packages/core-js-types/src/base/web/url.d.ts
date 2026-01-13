ddeb9a921866e15be1fd3aa37629360cf9482c5c
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-14 00:39:56 +0700

    Fallbacks for DOM

---
+++ packages/core-js-types/src/base/web/url.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="../core-js-modules/url" />
+
+import * as url from 'core-js/internals/web/url';
+
+declare global {
+  interface URL extends url.URL {}
+  var URL: typeof globalThis extends { onmessage: any; URL: infer T } ? T : typeof url.URL;
+
+  interface URLSearchParams extends url.URLSearchParams {}
+  var URLSearchParams: typeof globalThis extends { onmessage: any; URLSearchParams: infer T } ? T
+    : typeof url.URLSearchParams;
+}
+
+export {};
