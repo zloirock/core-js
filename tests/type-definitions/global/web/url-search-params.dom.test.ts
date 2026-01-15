872f534c5c1bffda1b7d799344f7b429b484a3c8
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-15 16:40:48 +0700

    Enable URLSearchParams tests & some web types & tests improvements

---
+++ tests/type-definitions/global/web/url-search-params.dom.test.ts
@@ -0,0 +1,11 @@
+import 'core-js/full';
+
+const u0 = new URLSearchParams();
+const u1 = new URLSearchParams('a=1&b=2');
+const u2 = new URLSearchParams([['a', '1'], ['b', '2']]);
+const u3 = new URLSearchParams({ a: '1', b: '2' });
+
+// @ts-expect-error
+new URLSearchParams(123);
+// @ts-expect-error
+new URLSearchParams([1, 2, 3]);
