9cafff36a41ab48de884f8b72901bc48ae5725ff
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-22 21:54:53 +0700

    Types linting: TSDocs

--- packages/core-js-types/src/base/proposals/collection-of-from.d.ts
+++ packages/core-js-types/src/base/proposals/collection-of-from.d.ts
@@ -4,18 +4,18 @@ interface MapConstructor {
   /**
    * Creates a new `Map` instance from an iterable or array-like object of [key, value] pairs.
    * Optionally, applies a mapping function to each pair.
-   * @param source Iterable or array-like object of [key, value] pairs.
-   * @param mapFn Function to call on every [key, value] pair before adding to the `Map`.
-   * @param thisArg Value to use as this when executing mapFn.
-   * @return A new `Map` instance.
+   * @param source - Iterable or array-like object of [key, value] pairs.
+   * @param mapFn - Function to call on every [key, value] pair before adding to the `Map`.
+   * @param thisArg - Value to use as this when executing mapFn.
+   * @returns A new `Map` instance.
    */
   from<K, V, VOut = V>(source: Iterable<[K, V]>, mapFn?: (value: V, key: K) => VOut, thisArg?: any): Map<K, VOut>;
 
   /**
    * Creates a new `Map` instance from a variable number of arguments,
    * where each pair of arguments is interpreted as a key and a value.
-   * @param items An even number of arguments representing key-value pairs.
-   * @return A new `Map` instance.
+   * @param items - An even number of arguments representing key-value pairs.
+   * @returns A new `Map` instance.
    */
   of<K, V>(...items: [K, V][]): Map<K, V>;
 }
@@ -26,18 +26,18 @@ interface SetConstructor {
   /**
    * Creates a new `Set` instance from an iterable or array-like object of [key, value] pairs.
    * Optionally, applies a mapping function to each pair.
-   * @param source Iterable or array-like object of [key, value] pairs.
-   * @param mapFn Function to call on every [key, value] pair before adding to the `Set`.
-   * @param thisArg Value to use as this when executing mapFn.
-   * @return A new `Set` instance.
+   * @param source - Iterable or array-like object of [key, value] pairs.
+   * @param mapFn - Function to call on every [key, value] pair before adding to the `Set`.
+   * @param thisArg - Value to use as this when executing mapFn.
+   * @returns A new `Set` instance.
    */
   from<T, U = T>(source: Iterable<T>, mapFn?: (value: T, index: number) => U, thisArg?: any): Set<U>;
 
   /**
    * Creates a new `Set` instance from a variable number of arguments,
    * where each pair of arguments is interpreted as a key and a value.
-   * @param items An even number of arguments representing key-value pairs.
-   * @return A new `Set` instance.
+   * @param items - An even number of arguments representing key-value pairs.
+   * @returns A new `Set` instance.
    */
   of<T>(...items: T[]): Set<T>;
 }
@@ -48,18 +48,18 @@ interface WeakMapConstructor {
   /**
    * Creates a new `WeakMap` instance from an iterable or array-like object of [key, value] pairs.
    * Optionally, applies a mapping function to each pair.
-   * @param source Iterable or array-like object of [key, value] pairs.
-   * @param mapFn Function to call on every [key, value] pair before adding to the `WeakMap`.
-   * @param thisArg Value to use as this when executing mapFn.
-   * @return A new `WeakMap` instance.
+   * @param source - Iterable or array-like object of [key, value] pairs.
+   * @param mapFn - Function to call on every [key, value] pair before adding to the `WeakMap`.
+   * @param thisArg - Value to use as this when executing mapFn.
+   * @returns A new `WeakMap` instance.
    */
   from<K extends object, V, VOut = V>(source: Iterable<[K, V]>, mapFn?: (value: V, key: K) => VOut, thisArg?: any): WeakMap<K, VOut>;
 
   /**
    * Creates a new `WeakMap` instance from a variable number of arguments,
    * where each pair of arguments is interpreted as a key and a value.
-   * @param items An even number of arguments representing key-value pairs.
-   * @return A new `Weak` instance.
+   * @param items - An even number of arguments representing key-value pairs.
+   * @returns A new `Weak` instance.
    */
   of<K extends object, V>(...items: [K, V][]): WeakMap<K, V>;
 }
@@ -70,18 +70,18 @@ interface WeakSetConstructor {
   /**
    * Creates a new `WeakSet` instance from an iterable or array-like object of [key, value] pairs.
    * Optionally, applies a mapping function to each pair.
-   * @param source Iterable or array-like object of [key, value] pairs.
-   * @param mapFn Function to call on every [key, value] pair before adding to the `WeakSet`.
-   * @param thisArg Value to use as this when executing mapFn.
-   * @return A new `WeakSet` instance.
+   * @param source - Iterable or array-like object of [key, value] pairs.
+   * @param mapFn - Function to call on every [key, value] pair before adding to the `WeakSet`.
+   * @param thisArg - Value to use as this when executing mapFn.
+   * @returns A new `WeakSet` instance.
    */
   from<T extends object, U extends object = T>(source: Iterable<T>, mapFn?: (value: T, index: number) => U, thisArg?: any): WeakSet<U>;
 
   /**
    * Creates a new `WeakSet` instance from a variable number of arguments,
    * where each pair of arguments is interpreted as a key and a value.
-   * @param items An even number of arguments representing key-value pairs.
-   * @return A new `WeakSet` instance.
+   * @param items - An even number of arguments representing key-value pairs.
+   * @returns A new `WeakSet` instance.
    */
   of<T extends object>(...items: T[]): WeakSet<T>;
 }
