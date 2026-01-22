249c22503b4911d9acf1254c609f3cd3f7ebe408
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-22 21:54:53 +0700

    Types linting: TSDocs

--- packages/core-js-types/src/base/proposals/function-demethodize.d.ts
+++ packages/core-js-types/src/base/proposals/function-demethodize.d.ts
@@ -3,7 +3,7 @@
 interface Function { // @type-options no-constructor
   /**
    * Creates a function that calls the original with its first argument as `this` and the rest as regular arguments.
-   * @returns {Function} A new function that applies the original function with its `this` set to the first argument.
+   * @returns A new function that applies the original function with its `this` set to the first argument.
    */
   demethodize<T, Args extends any[], R>(this: (this: T, ...args: Args) => R): (thisArg: T, ...args: Args) => R;
 }
--- packages/core-js-types/src/base/proposals/math-sum.d.ts
+++ packages/core-js-types/src/base/proposals/math-sum.d.ts
@@ -2,7 +2,7 @@
 interface Math { // @type-options no-constructor
   /**
    * Returns the sum of all given values.
-   * @param items
+   * @param items - An iterable of numbers to sum.
    * @returns The sum of all given values.
    */
   sumPrecise(items: Iterable<number>): number;
