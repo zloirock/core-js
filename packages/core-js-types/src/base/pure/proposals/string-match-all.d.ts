07a7bc02b48b1c3d1fcef908b25b5b72a273bb13
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-22 21:54:53 +0700

    Types linting: TSDocs

--- packages/core-js-types/src/base/pure/proposals/string-match-all.d.ts
+++ packages/core-js-types/src/base/pure/proposals/string-match-all.d.ts
@@ -20,7 +20,7 @@ declare namespace CoreJS {
     /**
      * Matches a string with a regular expression, and returns an iterable of matches
      * containing the results of that search.
-     * @param regexp A variable name or string literal containing the regular expression pattern and flags.
+     * @param regexp - A variable name or string literal containing the regular expression pattern and flags.
      */
     matchAll(regexp: RegExp): CoreJSRegExpStringIterator<RegExpExecArray>;
   }
