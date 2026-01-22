56c980a704244e1ca7d70def3049ada9cfb13c55
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-22 21:54:53 +0700

    Types linting: TSDocs

--- packages/core-js-types/src/base/proposals/change-array-by-copy.d.ts
+++ packages/core-js-types/src/base/proposals/change-array-by-copy.d.ts
@@ -12,7 +12,7 @@ interface Array<T> { // @type-options no-redefine
 
   /**
    * Returns a copy of an array with its elements sorted.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending, UTF-16 code unit order.
    * ```ts
@@ -23,16 +23,16 @@ interface Array<T> { // @type-options no-redefine
 
   /**
    * Copies an array and removes elements and, if necessary, inserts new elements in their place. Returns the copied array.
-   * @param start The zero-based location in the array from which to start removing elements.
-   * @param deleteCount The number of elements to remove.
-   * @param items Elements to insert into the copied array in place of the deleted elements.
+   * @param start - The zero-based location in the array from which to start removing elements.
+   * @param deleteCount - The number of elements to remove.
+   * @param items - Elements to insert into the copied array in place of the deleted elements.
    * @returns The copied array.
    */
   toSpliced(start: number, deleteCount: number, ...items: T[]): T[];
   /**
    * Copies an array and removes elements while returning the remaining elements.
-   * @param start The zero-based location in the array from which to start removing elements.
-   * @param deleteCount The number of elements to remove.
+   * @param start - The zero-based location in the array from which to start removing elements.
+   * @param deleteCount - The number of elements to remove.
    * @returns A copy of the original array with the remaining elements.
    */
   toSpliced(start: number, deleteCount?: number): T[];
@@ -41,9 +41,9 @@ interface Array<T> { // @type-options no-redefine
    * Copies an array, then overwrites the value at the provided index with the
    * given value. If the index is negative, then it replaces from the end
    * of the array.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to write into the copied array.
+   * @param value - The value to write into the copied array.
    * @returns The copied array with the updated value.
    */
   with(index: number, value: T): T[];
@@ -57,7 +57,7 @@ interface ReadonlyArray<T> { // @type-options no-export
 
   /**
    * Returns a copy of an array with its elements sorted.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending, UTF-16 code unit order.
    * ```ts
@@ -68,16 +68,16 @@ interface ReadonlyArray<T> { // @type-options no-export
 
   /**
    * Copies an array and removes elements and, if necessary, inserts new elements in their place. Returns the copied array.
-   * @param start The zero-based location in the array from which to start removing elements.
-   * @param deleteCount The number of elements to remove.
-   * @param items Elements to insert into the copied array in place of the deleted elements.
+   * @param start - The zero-based location in the array from which to start removing elements.
+   * @param deleteCount - The number of elements to remove.
+   * @param items - Elements to insert into the copied array in place of the deleted elements.
    * @returns The copied array.
    */
   toSpliced(start: number, deleteCount: number, ...items: T[]): T[];
   /**
    * Copies an array and removes elements while returning the remaining elements.
-   * @param start The zero-based location in the array from which to start removing elements.
-   * @param deleteCount The number of elements to remove.
+   * @param start - The zero-based location in the array from which to start removing elements.
+   * @param deleteCount - The number of elements to remove.
    * @returns A copy of the original array with the remaining elements.
    */
   toSpliced(start: number, deleteCount?: number): T[];
@@ -86,9 +86,9 @@ interface ReadonlyArray<T> { // @type-options no-export
    * Copies an array, then overwrites the value at the provided index with the
    * given value. If the index is negative, then it replaces from the end
    * of the array.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to write into the copied array.
+   * @param value - The value to write into the copied array.
    * @returns The copied array with the updated value.
    */
   with(index: number, value: T): T[];
@@ -102,7 +102,7 @@ interface Int8Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -114,9 +114,9 @@ interface Int8Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Int8Array;
@@ -130,7 +130,7 @@ interface Uint8Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -142,9 +142,9 @@ interface Uint8Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Uint8Array;
@@ -158,7 +158,7 @@ interface Uint8ClampedArray { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -170,9 +170,9 @@ interface Uint8ClampedArray { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Uint8ClampedArray;
@@ -186,7 +186,7 @@ interface Int16Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -198,9 +198,9 @@ interface Int16Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Int16Array;
@@ -214,7 +214,7 @@ interface Uint16Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -226,9 +226,9 @@ interface Uint16Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Uint16Array;
@@ -242,7 +242,7 @@ interface Int32Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -254,9 +254,9 @@ interface Int32Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Int32Array;
@@ -270,7 +270,7 @@ interface Uint32Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -282,9 +282,9 @@ interface Uint32Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Uint32Array;
@@ -298,7 +298,7 @@ interface Float32Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -310,9 +310,9 @@ interface Float32Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Float32Array;
@@ -326,7 +326,7 @@ interface Float64Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -338,9 +338,9 @@ interface Float64Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given number at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: number): Float64Array;
@@ -354,7 +354,7 @@ interface BigInt64Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -366,9 +366,9 @@ interface BigInt64Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given bigint at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: bigint): BigInt64Array;
@@ -382,7 +382,7 @@ interface BigUint64Array { // @type-options no-export
 
   /**
    * Copies and sorts the array.
-   * @param compareFn Function used to determine the order of the elements. It is expected to return
+   * @param compareFn - Function used to determine the order of the elements. It is expected to return
    * a negative value if the first argument is less than the second argument, zero if they're equal, and a positive
    * value otherwise. If omitted, the elements are sorted in ascending order.
    * ```ts
@@ -394,9 +394,9 @@ interface BigUint64Array { // @type-options no-export
 
   /**
    * Copies the array and inserts the given bigint at the provided index.
-   * @param index The index of the value to overwrite. If the index is
+   * @param index - The index of the value to overwrite. If the index is
    * negative, then it replaces from the end of the array.
-   * @param value The value to insert into the copied array.
+   * @param value - The value to insert into the copied array.
    * @returns A copy of the original array with the inserted value.
    */
   with(index: number, value: bigint): BigUint64Array;
