10c92663ae08dcc1057dfb11e1415c78e0d10c4b
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-23 22:37:45 +0700

    Exclude type's export ability

--- packages/core-js-types/src/base/web/url.d.ts
+++ packages/core-js-types/src/base/web/url.d.ts
@@ -1,14 +1,15 @@
-/// <reference types="../core-js-modules/url" />
+// URL has conflicts with @types/node
+// /// <reference types="../core-js-modules/url" />
 
-import * as url from 'core-js/internals/web/url';
-
-declare global {
-  interface URL extends url.URL {}
-  var URL: typeof globalThis extends { onmessage: any; URL: infer T } ? T : typeof url.URL;
-
-  interface URLSearchParams extends url.URLSearchParams {}
-  var URLSearchParams: typeof globalThis extends { onmessage: any; URLSearchParams: infer T } ? T
-    : typeof url.URLSearchParams;
-}
-
-export {};
+// import * as url from 'core-js/internals/web/url';
+//
+// declare global {
+//   interface URL extends url.URL {}
+//   var URL: typeof globalThis extends { onmessage: any; URL: infer T } ? T : typeof url.URL;
+//
+//   interface URLSearchParams extends url.URLSearchParams {}
+//   var URLSearchParams: typeof globalThis extends { onmessage: any; URLSearchParams: infer T } ? T
+//     : typeof url.URLSearchParams;
+// }
+//
+// export {};
