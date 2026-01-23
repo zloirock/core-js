ddd547e6388cb1907c411c2c0fecb260c7967760
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-23 22:37:45 +0700

    Exclude type's export ability

--- tests/type-definitions/global/web/url-search-params.dom.test.ts
+++ tests/type-definitions/global/web/url-search-params.dom.test.ts
@@ -1,11 +1,12 @@
-import 'core-js/full';
-
-const u0 = new URLSearchParams();
-const u1 = new URLSearchParams('a=1&b=2');
-const u2 = new URLSearchParams([['a', '1'], ['b', '2']]);
-const u3 = new URLSearchParams({ a: '1', b: '2' });
-
-// @ts-expect-error
-new URLSearchParams(123);
-// @ts-expect-error
-new URLSearchParams([1, 2, 3]);
+// URL has conflicts with @types/node
+// import 'core-js/full';
+//
+// const u0 = new URLSearchParams();
+// const u1 = new URLSearchParams('a=1&b=2');
+// const u2 = new URLSearchParams([['a', '1'], ['b', '2']]);
+// const u3 = new URLSearchParams({ a: '1', b: '2' });
+//
+// // @ts-expect-error
+// new URLSearchParams(123);
+// // @ts-expect-error
+// new URLSearchParams([1, 2, 3]);
