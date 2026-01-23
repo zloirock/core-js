cdd94077e8631880c8148c773caa5d7f96ba824e
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-23 22:37:45 +0700

    Exclude type's export ability

--- tests/type-definitions/global/web/url-parse.test.ts
+++ tests/type-definitions/global/web/url-parse.test.ts
@@ -1,17 +1,18 @@
-import 'core-js/full';
-
-const u1: URL | null = URL.parse('https://example.com/path?name=value#hash');
-URL.parse('/path', 'https://example.com');
-
-if (u1) {
-  let str: string;
-  str = u1.pathname;
-  str = u1.hostname;
-  str = u1.pathname;
-
-  str = u1.toJSON();
-  str = u1.toString();
-}
-
-// @ts-expect-error
-URL.parse(null);
+// URL has conflicts with @types/node
+// import 'core-js/full';
+//
+// const u1: URL | null = URL.parse('https://example.com/path?name=value#hash');
+// URL.parse('/path', 'https://example.com');
+//
+// if (u1) {
+//   let str: string;
+//   str = u1.pathname;
+//   str = u1.hostname;
+//   str = u1.pathname;
+//
+//   str = u1.toJSON();
+//   str = u1.toString();
+// }
+//
+// // @ts-expect-error
+// URL.parse(null);
