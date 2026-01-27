6d55b37275a84846058d893c71eb60d19f756e94
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-27 23:55:57 +0700

    use @ in @types and @no-types directive

--- packages/core-js/modules/es.map.get-or-insert.js
+++ packages/core-js/modules/es.map.get-or-insert.js
@@ -1,4 +1,4 @@
-// types: proposals/map-upsert
+// @types: proposals/map-upsert
 'use strict';
 var $ = require('../internals/export');
 var aMap = require('../internals/a-map');
--- packages/core-js/modules/es.uint8-array.to-hex.js
+++ packages/core-js/modules/es.uint8-array.to-hex.js
@@ -1,4 +1,4 @@
-// types: proposals/array-buffer-base64
+// @types: proposals/array-buffer-base64
 'use strict';
 /* eslint-disable es/no-uint8array-prototype-tohex -- safe */
 var $ = require('../internals/export');
