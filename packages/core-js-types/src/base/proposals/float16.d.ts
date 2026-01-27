fc24204d3df009e82bc75dcca11fce3fcdcaf415
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-27 23:39:14 +0700

    use @ in @type-options directive

--- packages/core-js-types/src/base/proposals/float16.d.ts
+++ packages/core-js-types/src/base/proposals/float16.d.ts
@@ -4,7 +4,7 @@
 // https://github.com/microsoft/TypeScript/blob/d3be7e171bf3149fe93c3ce5a85280f1eba3ef8d/src/lib/esnext.float16.d.ts
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
 
-interface Math { // @type-options no-constructor
+interface Math { // @type-options: no-constructor
   /**
    * Returns the nearest half precision float representation of a number.
    * @param x - A numeric expression.
@@ -12,7 +12,7 @@ interface Math { // @type-options no-constructor
   f16round(x: number): number;
 }
 
-interface DataView { // @type-options no-constructor
+interface DataView { // @type-options: no-constructor
   /**
    * Gets the Float16 value at the specified byte offset from the start of the view. There is
    * no alignment constraint; multibyte values may be fetched from any offset.
