e2705696578601a76af9eaf3a6e9422bb108db65
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-27 23:39:14 +0700

    use @ in @type-options directive

--- packages/core-js-types/src/base/proposals/math-sum.d.ts
+++ packages/core-js-types/src/base/proposals/math-sum.d.ts
@@ -1,5 +1,5 @@
 // https://github.com/tc39/proposal-math-sum
-interface Math { // @type-options no-constructor
+interface Math { // @type-options: no-constructor
   /**
    * Returns the sum of all given values.
    * @param items - An iterable of numbers to sum.
