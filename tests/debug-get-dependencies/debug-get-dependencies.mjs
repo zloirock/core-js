f7815daefa7382f467d9f828ca7e31c2f3700ce5
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-01-31 01:36:45 +0700

    Build entries and types scripts refactoring

--- tests/debug-get-dependencies/debug-get-dependencies.mjs
+++ tests/debug-get-dependencies/debug-get-dependencies.mjs
@@ -1,4 +1,4 @@
 import { modules } from '@core-js/compat/src/data.mjs';
-import { getModulesMetadata } from '../../scripts/build-entries/get-dependencies.mjs';
+import { getModulesMetadata } from '../../scripts/build-entries-and-types/get-dependencies.mjs';
 
 for (const module of modules) console.log(module, await getModulesMetadata([module]));
