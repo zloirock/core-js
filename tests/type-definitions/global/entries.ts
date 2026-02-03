17afbc068574318982fb918e400654605bd8923f
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-04 01:48:13 +0700

    Entries namespaces tests & build-types script improvements

--- tests/type-definitions/global/entries.ts
+++
@@ -1,7 +0,0 @@
-import '@core-js/types/stable';
-Iterator.concat([]);
-// @ts-expect-error
-Iterator.zipKeyed([]);
-
-import '@core-js/types/proposals/array-unique';
-const uniqueArr: number[] = [1, 2, 2, 3, 3, 3].uniqueBy();
