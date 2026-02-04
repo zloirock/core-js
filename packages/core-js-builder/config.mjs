504c6cce9dfb9319e6a21e1de8e84ba77a049cc3
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-04 02:16:54 +0200

    call swc only when required

--- packages/core-js-builder/config.mjs
+++ packages/core-js-builder/config.mjs
@@ -1,10 +1,10 @@
 const version = '4.0.0-alpha.0';
 
 /* eslint-disable prefer-template -- for better formatting */
-export default '/**\n' +
-               ' * core-js ' + version + '\n' +
-               ' * © 2013–2025 Denis Pushkarev (zloirock.ru), 2025–2026 CoreJS Company (core-js.io). All rights reserved.\n' +
-               ' * license: https://github.com/zloirock/core-js/blob/v' + version + '/LICENSE\n' +
-               ' * source: https://github.com/zloirock/core-js\n' +
-               ' */';
+export const banner = '/**\n' +
+                      ' * core-js ' + version + '\n' +
+                      ' * © 2013–2025 Denis Pushkarev (zloirock.ru), 2025–2026 CoreJS Company (core-js.io). All rights reserved.\n' +
+                      ' * license: https://github.com/zloirock/core-js/blob/v' + version + '/LICENSE\n' +
+                      ' * source: https://github.com/zloirock/core-js\n' +
+                      ' */';
 /* eslint-enable prefer-template -- for better formatting */
