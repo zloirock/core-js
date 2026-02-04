b153de77635457a5b117156795943814ed6e5e5a
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-04 16:36:01 +0200

    extract configs

--- packages/core-js-builder/config.mjs
+++ packages/core-js-builder/config.mjs
@@ -8,3 +8,52 @@ export const banner = '/**\n' +
                       ' * source: https://github.com/zloirock/core-js\n' +
                       ' */';
 /* eslint-enable prefer-template -- for better formatting */
+
+export function getRolldownOptions(input, output) {
+  return {
+    input,
+    platform: 'neutral',
+    treeshake: false,
+    output: {
+      externalLiveBindings: false,
+      format: 'iife',
+      file: output,
+      keepNames: true,
+      minifyInternalExports: true,
+    },
+  };
+}
+
+export const ModernSyntax = [
+  'arrow-functions',
+  'shorthand-properties',
+];
+
+export const MinifyOptions = {
+  minify: true,
+  jsc: {
+    minify: {
+      compress: {
+        arrows: false,
+        ecma: 5,
+        hoist_funs: true,
+        keep_fnames: true,
+        pure_getters: true,
+        reduce_funcs: true,
+        // document.all detection case
+        typeofs: false,
+        unsafe_proto: true,
+        unsafe_undefined: true,
+      },
+      mangle: {
+        keep_fnames: true,
+        safari10: true,
+        toplevel: true,
+      },
+      format: {
+        comments: false,
+        ecma: 5,
+      },
+    },
+  },
+};
