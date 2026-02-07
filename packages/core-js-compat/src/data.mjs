0c7bf999f4084ccc328bf814be2c1da63ed3917e
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-07 17:56:40 +0700

    Object.keysLength stage 2 proposal

--- packages/core-js-compat/src/data.mjs
+++ packages/core-js-compat/src/data.mjs
@@ -2480,6 +2480,8 @@ export const data = {
   },
   'esnext.number.clamp': {
   },
+  'esnext.object.keys-length': {
+  },
   'esnext.promise.all-keyed': {
   },
   'esnext.promise.all-settled-keyed': {
