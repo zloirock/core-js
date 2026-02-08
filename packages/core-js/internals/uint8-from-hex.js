a222c506cd3ef4880b6f1269ae7bb3b9a76deddf
John Zhou <john-xyz@outlook.com>
2026-02-08 17:13:23 -0600

    Fix

--- packages/core-js/internals/uint8-from-hex.js
+++ packages/core-js/internals/uint8-from-hex.js
@@ -4,21 +4,20 @@ var uncurryThis = require('../internals/function-uncurry-this');
 
 var Uint8Array = globalThis.Uint8Array;
 var SyntaxError = globalThis.SyntaxError;
-var parseInt = globalThis.parseInt;
-var NOT_HEX = /[^\da-f]/i;
-var exec = uncurryThis(NOT_HEX.exec);
+var $Number = globalThis.Number;
+var $isNaN = $Number.isNaN;
 var stringMatch = uncurryThis(''.match);
 
 module.exports = function (string, into) {
   var stringLength = string.length;
   if (stringLength & 1) throw new SyntaxError('String should be an even number of characters');
-  if (exec(NOT_HEX, string)) throw new SyntaxError('String should only contain hex characters');
   var maxLength = (into && into.length < (stringLength >> 1) ? into.length : (stringLength >> 1));
   var bytes = into || new Uint8Array(maxLength);
-  var segments = stringMatch(string, /.{2}/g)
-  console.log(segments)
+  var segments = stringMatch(string, /.{2}/g);
   for (var written = 0; written < maxLength; written++) {
-    bytes[written] = parseInt(segments[written], 16);
+    var result = $Number("0x" + segments[written]);
+    if ($isNaN(result)) throw new SyntaxError('String should only contain hex characters');
+    bytes[written] = result;
   }
   return { bytes: bytes, read: written << 1 };
 };
