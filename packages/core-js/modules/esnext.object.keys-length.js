aef0553fa7deefb0bab98a00bc2b115e0e6d3f04
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-09 14:14:36 +0200

    move `Object.keys` back to the top level

--- packages/core-js/modules/esnext.object.keys-length.js
+++ packages/core-js/modules/esnext.object.keys-length.js
@@ -1,12 +1,13 @@
 'use strict';
 var $ = require('../internals/export');
-var getBuiltInStaticMethod = require('../internals/get-built-in-static-method');
 var toObject = require('../internals/to-object');
+
+var keys = Object.keys;
 
 // `Object.keysLength` method
 // https://github.com/tc39/proposal-object-keys-length
 $({ target: 'Object', stat: true, forced: true }, {
   keysLength: function keysLength(O) {
-    return getBuiltInStaticMethod('Object', 'keys')(toObject(O)).length;
+    return keys(toObject(O)).length;
   },
 });
