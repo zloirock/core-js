a7ba67cdbcc364e8b2a6ac94645925edaed14417
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-12 02:50:15 +0700

    Update hack comment in array buffer transfer types

--- packages/core-js-types/src/base/pure/proposals/array-buffer-transfer.d.ts
+++ packages/core-js-types/src/base/pure/proposals/array-buffer-transfer.d.ts
@@ -37,7 +37,8 @@ declare namespace CoreJS {
   var CoreJSArrayBuffer: CoreJSArrayBufferConstructor;
 }
 
-// Type compatibility hack for AllowSharedBufferSource in TypeScript < 5.9: https://github.com/microsoft/TypeScript/issues/61480
+// Type compatibility hack for AllowSharedBufferSource in TypeScript < 5.9
+// https://github.com/microsoft/TypeScript/issues/61480
 interface SharedArrayBuffer {
   readonly resizable: boolean;
 
