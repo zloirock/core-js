087ed52fcb998673a42d6061682fbbd51cb4577d
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-12 21:10:34 +0700

    Types refactoring

--- packages/core-js-types/src/base/pure/core-js-types/iterator-object.d.ts
+++ packages/core-js-types/src/base/pure/core-js-types/iterator-object.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="../proposals/iterator.ts" />
+/// <reference types="../proposals/iterator" />
 
 declare namespace CoreJS {
   export interface CoreJSIteratorObject<T, TReturn = any, TNext = any> extends CoreJSIterator<T, TReturn, TNext> {}
