35917c73a0a4978931636d8edc3745b35f57d9d9
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-13 18:44:47 +0200

    split `bundle-tests` into 2 sequential tasks to prevent directory-related issues, add a message

--- scripts/bundle-tests/bundle-tests.mjs
+++ scripts/bundle-tests/bundle-tests.mjs
@@ -4,8 +4,12 @@ await Promise.all([
 ].map(([entry, output]) => $`webpack \
   --entry ../../tests/${ entry }.js \
   --output-filename ${ output }.js \
-`).concat([
+`));
+
+await Promise.all([
   fs.copyFile('../../packages/core-js-bundle/index.js', '../../tests/bundles/core-js-bundle.js'),
   fs.copyFile('./node_modules/@slowcheetah/qunitjs-1/qunit/qunit.js', '../../tests/bundles/qunit.js'),
   fs.copyFile('./node_modules/@slowcheetah/qunitjs-1/qunit/qunit.css', '../../tests/bundles/qunit.css'),
-]));
+]);
+
+echo(chalk.green('\ntests bundled, qunit and core-js bundles copied into /tests/bundles/'));
