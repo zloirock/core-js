5c5984dbb324c583f012ae9d187cfa4f7adaf526
John <johnzhou721@gmail.com>
2026-02-15 17:49:26 -0600

    Remove extra import

--- packages/core-js/modules/web.request-idle-callback.js
+++ packages/core-js/modules/web.request-idle-callback.js
@@ -1,6 +1,5 @@
 'use strict';
 var $ = require('../internals/export');
 var requestIdleCallback = require('../internals/idle-callbacks').request;
-var globalThis = require('../internals/global-this');
 
 $({ global: true }, { requestIdleCallback });
