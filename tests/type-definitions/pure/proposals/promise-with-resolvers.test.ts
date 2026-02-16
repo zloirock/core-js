870db48896deb0f2c4189bf7238ad49af57c59df
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-16 20:28:45 +0700

    Type definition helpers move & fix IteratorLike pure helper

--- tests/type-definitions/pure/proposals/promise-with-resolvers.test.ts
+++ tests/type-definitions/pure/proposals/promise-with-resolvers.test.ts
@@ -1,6 +1,6 @@
 import promiseWithResolvers from '@core-js/pure/full/promise/with-resolvers';
 import promiseResolve from '@core-js/pure/full/promise/resolve';
-import { assertCoreJSPromiseLike } from '../../helpers.pure.js';
+import { assertCoreJSPromiseLike } from '../../helpers/helpers.pure.js';
 
 const pr = promiseWithResolvers<number>();
 const pr2 = promiseWithResolvers<string>();
