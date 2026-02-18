e2c2222629bbdad74ba1ca1b55bdc814849957f3
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-18 23:06:08 +0700

    Aligning the type of CoreJSAsyncIterator and CoreJSAsyncIteratorObject

--- packages/core-js-types/src/base/pure/core-js-types/async-iterator-object.d.ts
+++ packages/core-js-types/src/base/pure/core-js-types/async-iterator-object.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="../proposals/symbol" />
 
 declare namespace CoreJS {
-  export interface CoreJSAsyncIteratorObject<T, TReturn = unknown, TNext = unknown> extends CoreJS.CoreJSAsyncIterator<T, TReturn, TNext> {
+  export interface CoreJSAsyncIteratorObject<T, TReturn = undefined, TNext = undefined> extends CoreJS.CoreJSAsyncIterator<T, TReturn, TNext> {
     [CoreJS.CoreJSSymbol.asyncIterator](): CoreJSAsyncIteratorObject<T, TReturn, TNext>;
   }
 }
