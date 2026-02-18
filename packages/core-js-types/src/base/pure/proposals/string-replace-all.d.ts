cca0439cac3e2d524585e1d9cfe38fce3956af8f
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-19 00:35:24 +0700

    Remove proposal stage comments from types

--- packages/core-js-types/src/base/pure/proposals/string-replace-all.d.ts
+++ packages/core-js-types/src/base/pure/proposals/string-replace-all.d.ts
@@ -2,7 +2,6 @@
 
 // Motivation: We should use String without the matchAll method to avoid signature conflicts
 
-// proposal stage: 4
 // https://github.com/tc39/proposal-string-replaceall
 
 // For ensuring compatibility with TypeScript standard types, this code is aligned with:
