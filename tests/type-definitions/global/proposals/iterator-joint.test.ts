bc1756eb1e002011691e473b1531a1ddda8000e1
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-19 00:43:10 +0700

    Add iterator joint proposal tests with strict mode

--- tests/type-definitions/global/proposals/iterator-joint.test.ts
+++ tests/type-definitions/global/proposals/iterator-joint.test.ts
@@ -11,13 +11,17 @@ $Iterator.zipKeyed({ a: [1, 2, 3], b: [4, 5, 6] });
 Iterator.zip([[1, 2, 3], [4, 5, 6]]);
 Iterator.zip([['a', 'b', 'c'], ['d', 'e', 'f']]);
 Iterator.zip([[1, 2, 3], [4, 5, 6]], { mode: 'shortest' });
+Iterator.zip([[1, 2, 3], [4, 5, 6]], { mode: 'strict' });
 Iterator.zipKeyed({ a: [1, 2, 3], b: [4, 5, 6] });
 Iterator.zipKeyed({ a: ['a', 'b', 'c', 'd'], b: ['e', 'f'] }, { mode: 'shortest' });
+Iterator.zipKeyed({ a: [1, 2, 3], b: [4, 5, 6] }, { mode: 'strict' });
 
 // @ts-expect-error
 Iterator.zip(true);
 // @ts-expect-error
 Iterator.zip([[1, 2, 3], [4, 5, 6]], { mode: 'incorrect' });
+// @ts-expect-error
+Iterator.zip(arrays, { mode: 'invalid' });
 // @ts-expect-error
 Iterator.zipKeyed(42);
 // @ts-expect-error
