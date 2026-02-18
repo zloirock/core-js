720e7768dd368f829219e39e3438b428f93484cd
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-18 23:15:08 +0700

    Aligning the type of Iterator, IteratorObject, AsyncIteratorObject

--- packages/core-js-types/src/base/proposals/iterator-joint.d.ts
+++ packages/core-js-types/src/base/proposals/iterator-joint.d.ts
@@ -16,7 +16,7 @@ interface IteratorConstructor { // @type-options: no-extends
    *  - padding: an object specifying padding values for each key when mode is 'longest'.
    * @returns An iterator yielding objects with keys from the input iterables and values from the corresponding iterables.
    */
-  zip<T>(iterables: Iterable<Iterable<T>>, options?: ZipOptions): IteratorObject<T[]>; // @type-options: prefix-return-type
+  zip<T>(iterables: Iterable<Iterable<T>>, options?: ZipOptions): IteratorObject<T[], undefined, unknown>; // @type-options: prefix-return-type
 
   /**
    * takes an object whose values are iterables and produces an iterable of objects where keys.
@@ -27,7 +27,7 @@ interface IteratorConstructor { // @type-options: no-extends
    *  - padding: an object specifying padding values for each key when mode is 'longest'.
    * @returns An iterator yielding objects with keys from the input record and values from the corresponding iterables.
    */
-  zipKeyed<T extends { [K in PropertyKey]: Iterable<any> }>(record: T, options?: ZipOptions): IteratorObject<{ [K in keyof T]: T[K] extends Iterable<infer V> ? V : never; }>;  // @type-options: prefix-return-type
+  zipKeyed<T extends { [K in PropertyKey]: Iterable<any> }>(record: T, options?: ZipOptions): IteratorObject<{ [K in keyof T]: T[K] extends Iterable<infer V> ? V : never; }, undefined, unknown>;  // @type-options: prefix-return-type
 }
 
 declare var Iterator: IteratorConstructor;
