e422dc3f91c2a4878839c2a99a8370f38250a711
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-18 22:23:47 +0700

    Fix callback return type of typed arrays filterReject methods

--- packages/core-js-types/src/base/proposals/array-filtering.d.ts
+++ packages/core-js-types/src/base/proposals/array-filtering.d.ts
@@ -8,7 +8,7 @@ interface Array<T> { // @type-options: no-redefine
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: T, index: number, target: T[]) => boolean, thisArg?: any): T[];
+  filterReject(callbackFn: (value: T, index: number, target: T[]) => unknown, thisArg?: any): T[];
 }
 
 interface ReadonlyArray<T> { // @type-options: no-export
@@ -19,7 +19,7 @@ interface ReadonlyArray<T> { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: T, index: number, target: readonly T[]) => boolean, thisArg?: any): T[];
+  filterReject(callbackFn: (value: T, index: number, target: readonly T[]) => unknown, thisArg?: any): T[];
 }
 
 interface Int8Array { // @type-options: no-export
@@ -30,7 +30,7 @@ interface Int8Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Int8Array) => boolean, thisArg?: any): Int8Array;
+  filterReject(callbackFn: (value: number, index: number, target: Int8Array) => unknown, thisArg?: any): Int8Array;
 }
 
 interface Uint8Array { // @type-options: no-export
@@ -41,7 +41,7 @@ interface Uint8Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Uint8Array) => boolean, thisArg?: any): Uint8Array;
+  filterReject(callbackFn: (value: number, index: number, target: Uint8Array) => unknown, thisArg?: any): Uint8Array;
 }
 
 interface Uint8ClampedArray { // @type-options: no-export
@@ -52,7 +52,7 @@ interface Uint8ClampedArray { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Uint8ClampedArray) => boolean, thisArg?: any): Uint8ClampedArray;
+  filterReject(callbackFn: (value: number, index: number, target: Uint8ClampedArray) => unknown, thisArg?: any): Uint8ClampedArray;
 }
 
 interface Int16Array { // @type-options: no-export
@@ -63,7 +63,7 @@ interface Int16Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Int16Array) => boolean, thisArg?: any): Int16Array;
+  filterReject(callbackFn: (value: number, index: number, target: Int16Array) => unknown, thisArg?: any): Int16Array;
 }
 
 interface Uint16Array { // @type-options: no-export
@@ -74,7 +74,7 @@ interface Uint16Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Uint16Array) => boolean, thisArg?: any): Uint16Array;
+  filterReject(callbackFn: (value: number, index: number, target: Uint16Array) => unknown, thisArg?: any): Uint16Array;
 }
 
 interface Int32Array { // @type-options: no-export
@@ -85,7 +85,7 @@ interface Int32Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Int32Array) => boolean, thisArg?: any): Int32Array;
+  filterReject(callbackFn: (value: number, index: number, target: Int32Array) => unknown, thisArg?: any): Int32Array;
 }
 
 interface Uint32Array { // @type-options: no-export
@@ -96,7 +96,7 @@ interface Uint32Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Uint32Array) => boolean, thisArg?: any): Uint32Array;
+  filterReject(callbackFn: (value: number, index: number, target: Uint32Array) => unknown, thisArg?: any): Uint32Array;
 }
 
 interface Float32Array { // @type-options: no-export
@@ -107,7 +107,7 @@ interface Float32Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Float32Array) => boolean, thisArg?: any): Float32Array;
+  filterReject(callbackFn: (value: number, index: number, target: Float32Array) => unknown, thisArg?: any): Float32Array;
 }
 
 interface Float64Array { // @type-options: no-export
@@ -118,7 +118,7 @@ interface Float64Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: number, index: number, target: Float64Array) => boolean, thisArg?: any): Float64Array;
+  filterReject(callbackFn: (value: number, index: number, target: Float64Array) => unknown, thisArg?: any): Float64Array;
 }
 
 interface BigInt64Array { // @type-options: no-export
@@ -129,7 +129,7 @@ interface BigInt64Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: bigint, index: number, target: BigInt64Array) => boolean, thisArg?: any): BigInt64Array;
+  filterReject(callbackFn: (value: bigint, index: number, target: BigInt64Array) => unknown, thisArg?: any): BigInt64Array;
 }
 
 interface BigUint64Array { // @type-options: no-export
@@ -140,5 +140,5 @@ interface BigUint64Array { // @type-options: no-export
    * @param thisArg - If provided, it will be used as this value for each invocation of
    * predicate. If it is not provided, undefined is used instead.
    */
-  filterReject(callbackFn: (value: bigint, index: number, target: BigUint64Array) => boolean, thisArg?: any): BigUint64Array;
+  filterReject(callbackFn: (value: bigint, index: number, target: BigUint64Array) => unknown, thisArg?: any): BigUint64Array;
 }
