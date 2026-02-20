deebeda653c998d82e501b9ac9c3b546cab89e3f
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-21 00:07:30 +0700

    Add Promise.any & Promise.try to pure types

--- packages/core-js-types/src/base/pure/core-js-types/promise.d.ts
+++ packages/core-js-types/src/base/pure/core-js-types/promise.d.ts
@@ -1,6 +1,17 @@
 /// <reference types="./promise-like" />
 
+// For ensuring compatibility with TypeScript standard types, this code is aligned with:
+// https://github.com/microsoft/TypeScript/blob/347254895823a36a1b1b1c80471422da54ad77de/src/lib/es2021.promise.d.ts#L16
+// https://github.com/microsoft/TypeScript/blob/347254895823a36a1b1b1c80471422da54ad77de/src/lib/es2025.promise.d.ts#L4
+// License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
+
 declare namespace CoreJS {
+  interface CoreJSPromiseWithResolvers<T> {
+    promise: CoreJSPromise<T>;
+    resolve: (value: T | CoreJS.CoreJSPromiseLike<T>) => void;
+    reject: (reason?: any) => void;
+  }
+
   export interface CoreJSPromise<T> extends Promise<T> {}
 
   export interface CoreJSPromiseConstructor {
@@ -48,6 +59,36 @@ declare namespace CoreJS {
      * @returns A promise whose internal state matches the provided promise.
      */
     resolve<T>(value: T | CoreJS.CoreJSPromiseLike<T>): CoreJSPromise<Awaited<T>>;
+
+    // allSettled, any, try
+
+    /**
+     * The any function returns a promise that is fulfilled by the first given promise to be fulfilled, or rejected with an AggregateError containing an array of rejection reasons if all of the given promises are rejected. It resolves all elements of the passed iterable to promises as it runs this algorithm.
+     * @param values An array or iterable of Promises.
+     * @returns A new Promise.
+     */
+    any<T extends readonly unknown[] | []>(values: T): CoreJSPromise<Awaited<T[number]>>;
+    /**
+     * The any function returns a promise that is fulfilled by the first given promise to be fulfilled, or rejected with an AggregateError containing an array of rejection reasons if all of the given promises are rejected. It resolves all elements of the passed iterable to promises as it runs this algorithm.
+     * @param values An array or iterable of Promises.
+     * @returns A new Promise.
+     */
+    any<T>(values: Iterable<T | CoreJS.CoreJSPromiseLike<T>>): CoreJSPromise<Awaited<T>>;
+
+    /**
+     * Takes a callback of any kind (returns or throws, synchronously or asynchronously) and wraps its result
+     * in a Promise.
+     *
+     * @param callbackFn A function that is called synchronously. It can do anything: either return
+     * a value, throw an error, or return a promise.
+     * @param args Additional arguments, that will be passed to the callback.
+     *
+     * @returns A Promise that is:
+     * - Already fulfilled, if the callback synchronously returns a value.
+     * - Already rejected, if the callback synchronously throws an error.
+     * - Asynchronously fulfilled or rejected, if the callback returns a promise.
+     */
+    try<T, U extends unknown[]>(callbackFn: (...args: U) => T | CoreJS.CoreJSPromiseLike<T>, ...args: U): CoreJSPromise<Awaited<T>>;
   }
 
   var CoreJSPromise: CoreJSPromiseConstructor;
