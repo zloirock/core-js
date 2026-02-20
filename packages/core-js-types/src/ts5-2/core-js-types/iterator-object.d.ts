c1749d99a07c4312ee6ca3a3fb64a643d2469786
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-20 22:58:29 +0700

    Make IteratorObject iterable

--- packages/core-js-types/src/ts5-2/core-js-types/iterator-object.d.ts
+++ packages/core-js-types/src/ts5-2/core-js-types/iterator-object.d.ts
@@ -1,5 +1,7 @@
 // Motivation: Iterator until TS 5.6 used undefined as TNext defaults
 
 declare namespace CoreJS {
-  export interface CoreJSIteratorObject<T, TReturn = any, TNext = undefined> extends Iterator<T, TReturn, TNext> {}
+  export interface CoreJSIteratorObject<T, TReturn = any, TNext = undefined> extends Iterator<T, TReturn, TNext> {
+    [Symbol.iterator](): CoreJSIteratorObject<T, TReturn, TNext>;
+  }
 }
--- packages/core-js-types/src/ts5-2/pure/core-js-types/iterator-object.d.ts
+++ packages/core-js-types/src/ts5-2/pure/core-js-types/iterator-object.d.ts
@@ -3,5 +3,7 @@
 // Motivation: Iterator until TS 5.6 used undefined as TNext defaults
 
 declare namespace CoreJS {
-  export interface CoreJSIteratorObject<T, TReturn = any, TNext = undefined> extends CoreJSIterator<T, TReturn, TNext> {}
+  export interface CoreJSIteratorObject<T, TReturn = any, TNext = undefined> extends CoreJSIterator<T, TReturn, TNext> {
+    [Symbol.iterator](): CoreJSIteratorObject<T, TReturn, TNext>;
+  }
 }
