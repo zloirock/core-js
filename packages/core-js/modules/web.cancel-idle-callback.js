509047a041f23ad376db6c97643eca49ef8c25bd
John <john-xyz@outlook.com>
2026-02-20 17:50:39 -0600

    If global, export requestIdleCallback in cancelIdleCallback

--- packages/core-js/modules/web.cancel-idle-callback.js
+++ packages/core-js/modules/web.cancel-idle-callback.js
@@ -4,8 +4,15 @@ var globalThis = require('../internals/global-this');
 var IS_PURE = require('../internals/is-pure');
 
 var BASIC = !!globalThis.requestIdleCallback && !!globalThis.cancelIdleCallback;
+var requestIdleCallback = require('../internals/idle-callbacks').request;
 var cancelIdleCallback = require('../internals/idle-callbacks').cancel;
 
-$({ global: true, forced: IS_PURE || !BASIC }, {
+$({ global: true, forced: !BASIC }, {
   cancelIdleCallback: cancelIdleCallback
 });
+
+if (!IS_PURE) {
+  $({ global: true, arity: 1, forced: !BASIC }, {
+    requestIdleCallback: requestIdleCallback
+  });
+}
