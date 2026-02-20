bc21c6a344a33b66448c17ff4a9657ac2f6332ba
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-21 02:47:47 +0700

    Add type guard to symbol predicates proposal

--- packages/core-js-types/src/base/proposals/symbol-predicates.d.ts
+++ packages/core-js-types/src/base/proposals/symbol-predicates.d.ts
@@ -5,11 +5,11 @@ interface SymbolConstructor {
    * Determines whether the given value is a registered symbol.
    * @param value - The value to check.
    */
-  isRegisteredSymbol(value: any): boolean;
+  isRegisteredSymbol(value: unknown): value is symbol;
 
   /**
    * Determines whether the given value is a well-known symbol.
    * @param value - The value to check.
    */
-  isWellKnownSymbol(value: any): boolean;
+  isWellKnownSymbol(value: unknown): value is symbol;
 }
