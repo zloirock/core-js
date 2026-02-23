ebcb7109bf18fd60d61a8c144fb32642034c714f
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-23 19:07:14 +0200

    fix helpers testing

--- scripts/build-indexes.mjs
+++ scripts/build-indexes.mjs
@@ -11,7 +11,7 @@ async function generateTestsIndex(name, pkg) {
   const dir = `tests/${ name }`;
   const files = await fs.readdir(dir);
   return fs.writeFile(`${ dir }/index.js`, `import '../helpers/qunit-helpers';\n\n${ files
-    .filter(it => /^(?:es|esnext|web)\./.test(it))
+    .filter(it => /^(?:es|esnext|helpers|web)\./.test(it))
     .map(it => `import './${ it.slice(0, -3) }';\n`)
     .join('') }${ pkg !== 'core-js' ? `\nimport core from '${ pkg }';\ncore.globalThis.core = core;\n` : '' }`);
 }
