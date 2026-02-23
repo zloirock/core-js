227e9371068499c829e2d750f7815e933cbb1d84
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-23 13:15:27 +0200

    fix some assert messages

--- tests/unit-global/es.object.define-getter.js
+++ tests/unit-global/es.object.define-getter.js
@@ -15,8 +15,8 @@ if (DESCRIPTORS) {
       this.foo = 43;
     });
     object.key = 44;
-    assert.same(object.key, 42, 'works with getter #1');
-    assert.same(object.foo, 43, 'works with getter #2');
+    assert.same(object.key, 42, 'works with setter #1');
+    assert.same(object.foo, 43, 'works with setter #2');
     if (STRICT) {
       assert.throws(() => __defineGetter__.call(null, 1, () => { /* empty */ }), TypeError, 'Throws on null as `this`');
       assert.throws(() => __defineGetter__.call(undefined, 1, () => { /* empty */ }), TypeError, 'Throws on undefined as `this`');
