f16607675107b620c22b6d64b10fef851a1a3342
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-24 00:11:50 +0700

    Fix AsyncIterator types

--- packages/core-js-types/src/base/proposals/async-iterator-helpers-custom.d.ts
+++ packages/core-js-types/src/base/proposals/async-iterator-helpers-custom.d.ts
@@ -7,7 +7,7 @@
 declare namespace CoreJS {
   export type AsyncIteratorFlatMap<T, U> = (callback: (value: T, index: number) => Iterator<U, unknown, undefined> | Iterable<U, unknown, undefined> | AsyncIterator<U> | AsyncIterable<U>) => AsyncIteratorObject<U, undefined, unknown>;
 
-  export type AsyncIteratorMap<T, U> = (callback: (value: T, index: number) => U) => AsyncIteratorObject<U, undefined, unknown>;
+  export type AsyncIteratorMap<T, U> = (callback: (value: T, index: number) => U) => AsyncIteratorObject<Awaited<U>, undefined, unknown>;
 
   export type AsyncIteratorReduce<T, U = T> = (callback: (accumulator: U, value: T, index: number) => U, initialValue?: U) => Promise<U>;
 }
