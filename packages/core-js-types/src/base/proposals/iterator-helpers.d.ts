5d34a7593b3b8d42b019dac1bdb1b3d745e3f7ea
Sergey Nechaev <slow_cheetah@inbox.ru>
2026-02-25 00:31:47 +0700

    TSDocs fixes

--- packages/core-js-types/src/base/proposals/iterator-helpers.d.ts
+++ packages/core-js-types/src/base/proposals/iterator-helpers.d.ts
@@ -101,7 +101,7 @@ interface IteratorConstructor { // @type-options: no-extends
   /**
    * Creates a native iterator from an iterator or iterable object.
    * Returns its input if the input already inherits from the built-in Iterator class.
-   * @param value - An iterator or iterable object to convert a native iterator.
+   * @param value - An iterator or iterable object to convert to a native iterator.
    */
   from<T>(value: Iterator<T, unknown, undefined> | Iterable<T, unknown, undefined>): IteratorObject<T, undefined, unknown>;
 }
--- packages/core-js-types/src/base/proposals/map-upsert.d.ts
+++ packages/core-js-types/src/base/proposals/map-upsert.d.ts
@@ -8,13 +8,17 @@ interface Map<K, V> { // @type-options: no-redefine
   /**
    * Returns a specified element from the Map object.
    * If no element is associated with the specified key, a new element with the value `defaultValue` will be inserted into the Map and returned.
+   * @param key - The key of the element to return.
+   * @param defaultValue - The value to insert if the key is not already associated with an element.
    * @returns The element associated with the specified key, which will be `defaultValue` if no element previously existed.
    */
   getOrInsert(key: K, defaultValue: V): V;
   /**
    * Returns a specified element from the Map object.
    * If no element is associated with the specified key, the result of passing the specified key to the `callback` function will be inserted into the Map and returned.
-   * @returns The element associated with the specific key, which will be the newly computed value if no element previously existed.
+   * @param key - The key of the element to return.
+   * @param callback - A function that computes the value to insert if the key is not already associated with an element. It will be passed the key as an argument.
+   * @returns The element associated with the specified key, which will be the newly computed value if no element previously existed.
    */
   getOrInsertComputed(key: K, callback: (key: K) => V): V;
 }
@@ -23,13 +27,17 @@ interface WeakMap<K extends WeakKey, V> { // @type-options: no-redefine
   /**
    * Returns a specified element from the WeakMap object.
    * If no element is associated with the specified key, a new element with the value `defaultValue` will be inserted into the WeakMap and returned.
+   * @param key - The key of the element to return.
+   * @param defaultValue - The value to insert if the key is not already associated with an element.
    * @returns The element associated with the specified key, which will be `defaultValue` if no element previously existed.
    */
   getOrInsert(key: K, defaultValue: V): V;
   /**
    * Returns a specified element from the WeakMap object.
    * If no element is associated with the specified key, the result of passing the specified key to the `callback` function will be inserted into the WeakMap and returned.
-   * @returns The element associated with the specific key, which will be the newly computed value if no element previously existed.
+   * @param key - The key of the element to return.
+   * @param callback - A function that computes the value to insert if the key is not already associated with an element. It will be passed the key as an argument.
+   * @returns The element associated with the specified key, which will be the newly computed value if no element previously existed.
    */
   getOrInsertComputed(key: K, callback: (key: K) => V): V;
 }
