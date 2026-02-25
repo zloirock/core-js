e9d84cf674096f4491a1d5eef68547faeba812ba
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-25 13:00:49 +0200

    fix some jsdoc and param names

--- packages/core-js-types/src/base/proposals/async-iterator-helpers-custom.d.ts
+++ packages/core-js-types/src/base/proposals/async-iterator-helpers-custom.d.ts
@@ -7,7 +7,7 @@
 declare namespace CoreJS {
   export type AsyncIteratorFlatMap<T, U> = (callback: (value: T, index: number) => Iterator<U, unknown, undefined> | Iterable<U, unknown, undefined> | AsyncIterator<U> | AsyncIterable<U>) => AsyncIteratorObject<U, undefined, unknown>;
 
-  export type AsyncIteratorMap<T, U> = (callback: (value: T, index: number) => U) => AsyncIteratorObject<Awaited<U>, undefined, unknown>;
+  export type AsyncIteratorMap<T, U> = (callbackfn: (value: T, index: number) => U) => AsyncIteratorObject<Awaited<U>, undefined, unknown>;
 
-  export type AsyncIteratorReduce<T, U = T> = (callback: (accumulator: U, value: T, index: number) => U, initialValue?: U) => Promise<U>;
+  export type AsyncIteratorReduce<T, U = T> = (callbackfn: (accumulator: U, value: T, index: number) => U, initialValue?: U) => Promise<U>;
 }
