28cf2e9b16f45430f35ef8658c7a461d50cca69e
John <johnzhou721@gmail.com>
2026-02-28 05:20:58 -0600

    feat: Improve performance of Uint8Array Hex functions (#1510)
    * feat: Improve performance of Uint8Array Hex functions
    
    * Typo fix
    
    * op prec
    
    * Update uint8-from-hex.js
    
    * Update uint8-from-hex.js
    
    * Update uint8-from-hex.js
    
    * Fix
    
    * Improvements
    
    * Lint
    
    * Lint, cl
    
    * Requested Changes.
    
    * Cache min
    
    * Fix tests
    
    * some fixes
    
    ---------
    
    Co-authored-by: Denis Pushkarev <zloirock@zloirock.ru>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 ### Unreleased
-- Improved performance of `atob` and `btoa`, [#1503](https://github.com/zloirock/core-js/issues/1503), [#1464](https://github.com/zloirock/core-js/issues/1464)
+- Improved performance of `atob`, `btoa`, `Uint8Array.fromHex`, `Uint8Array.prototype.setFromHex`, and `Uint8Array.prototype.toHex`, [#1503](https://github.com/zloirock/core-js/issues/1503), [#1464](https://github.com/zloirock/core-js/issues/1464), [#1510](https://github.com/zloirock/core-js/issues/1510)
 - [`Iterator.range`](https://github.com/tc39/proposal-iterator.range) updated following the actual spec version
   - Throw a `RangeError` on `NaN` `start` / `end` / `step`
   - Allow `null` as `optionOrStep`
--- packages/core-js/internals/uint8-from-hex.js
+++ packages/core-js/internals/uint8-from-hex.js
@@ -4,23 +4,24 @@ var uncurryThis = require('../internals/function-uncurry-this');
 
 var Uint8Array = globalThis.Uint8Array;
 var SyntaxError = globalThis.SyntaxError;
-var parseInt = globalThis.parseInt;
 var min = Math.min;
-var NOT_HEX = /[^\da-f]/i;
-var exec = uncurryThis(NOT_HEX.exec);
-var stringSlice = uncurryThis(''.slice);
+var stringMatch = uncurryThis(''.match);
 
 module.exports = function (string, into) {
   var stringLength = string.length;
   if (stringLength % 2 !== 0) throw new SyntaxError('String should be an even number of characters');
   var maxLength = into ? min(into.length, stringLength / 2) : stringLength / 2;
   var bytes = into || new Uint8Array(maxLength);
-  var read = 0;
+  // This splitting is faster than using substrings each time.
+  var segments = stringMatch(string, /.{2}/g);
   var written = 0;
-  while (written < maxLength) {
-    var hexits = stringSlice(string, read, read += 2);
-    if (exec(NOT_HEX, hexits)) throw new SyntaxError('String should only contain hex characters');
-    bytes[written++] = parseInt(hexits, 16);
+  for (; written < maxLength; written++) {
+    var result = +('0x' + segments[written] + '0');
+    // eslint-disable-next-line no-self-compare -- NaN check
+    if (result !== result) {
+      throw new SyntaxError('String should only contain hex characters');
+    }
+    bytes[written] = result >> 4;
   }
-  return { bytes: bytes, read: read };
+  return { bytes: bytes, read: written << 1 };
 };
