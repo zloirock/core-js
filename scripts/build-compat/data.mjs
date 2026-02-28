1df27d49b798893041c9008d02daffaf24d80b1f
Denis Pushkarev <zloirock@zloirock.ru>
2026-02-28 11:53:17 +0200

    fix builder optimization for non-browser engines

--- scripts/build-compat/data.mjs
+++ scripts/build-compat/data.mjs
@@ -10,6 +10,7 @@ const { hasOwn } = Object;
 for (const scope of [data, external]) {
   for (const [key, module] of Object.entries(scope)) {
     const { chrome, ie } = module;
+    const nonModulesExternal = scope === external && key !== 'modules';
 
     function map(mappingKey) {
       const [engine, targetKey] = mappingKey.split('To')
@@ -28,7 +29,7 @@ for (const scope of [data, external]) {
       }
     }
 
-    if (/^(?:es|esnext)\./.test(key)) {
+    if (nonModulesExternal || /^(?:es|esnext)\./.test(key)) {
       map('ChromeToDeno');
       map('ChromeToNode');
     }
@@ -39,7 +40,7 @@ for (const scope of [data, external]) {
         module.edge = String(Math.max(chrome, 79));
       }
     }
-    if (/^(?:es|esnext|web)\./.test(key)) {
+    if (nonModulesExternal || /^(?:es|esnext|web)\./.test(key)) {
       map('ChromeToElectron');
     }
     map('ChromeToOpera');
@@ -59,7 +60,7 @@ for (const scope of [data, external]) {
       module.quest = Math.ceil(chrome / 2) * 2;
     }
     map('ChromeAndroidToSamsung');
-    if (/^(?:es|esnext)\./.test(key)) {
+    if (nonModulesExternal || /^(?:es|esnext)\./.test(key)) {
       map('SafariToBun');
     }
     map('FirefoxToFirefoxAndroid');
