1f09866931be50f88fc1ac2c8df84c5b59030cd0
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-01 19:25:32 +0200

    add a naive adaptation of `usagePure` babel plugin mode

---
+++ tests/babel-plugin/fixtures/usage-pure/global-map/output.mjs
@@ -0,0 +1,2 @@
+import _Map from "@core-js/pure/actual/map/constructor";
+new _Map();
---
+++ tests/babel-plugin/fixtures/usage-pure/instance-includes-array/output.mjs
@@ -0,0 +1,3 @@
+var _context;
+import _includesInstanceProperty from "@core-js/pure/actual/array/includes";
+_includesInstanceProperty(_context = []).call(_context, 1);
