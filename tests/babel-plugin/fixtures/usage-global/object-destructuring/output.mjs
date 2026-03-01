93b0431bc20c12f124457d971ea479baf12aec05
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-02 01:43:12 +0200

    add some tests

---
+++ tests/babel-plugin/fixtures/usage-global/object-destructuring/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.from";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const {
+  from,
+  foo
+} = Array;
