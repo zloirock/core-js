26c634ef9f48ba41ca6178177184777aed325e27
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-01 19:25:32 +0200

    add a naive adaptation of `usagePure` babel plugin mode

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-find/output.mjs
@@ -0,0 +1,2 @@
+import _findInstanceProperty from "@core-js/pure/actual/instance/find";
+_findInstanceProperty(arr).call(arr, x => x > 0);
---
+++ tests/babel-plugin/fixtures/usage-pure/mixed/output.mjs
@@ -0,0 +1,7 @@
+var _context;
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+import _pushInstanceProperty from "@core-js/pure/actual/instance/push";
+_Array$from();
+new _Promise();
+_pushInstanceProperty(_context = []).call(_context);
