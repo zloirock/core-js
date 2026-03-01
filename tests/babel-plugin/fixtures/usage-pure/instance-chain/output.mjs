96c877cc1c75569942445214e1f009801b8013f5
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-02 01:43:12 +0200

    add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-chain/output.mjs
@@ -0,0 +1,4 @@
+var _context;
+import _mapInstanceProperty from "@core-js/pure/actual/instance/map";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+_mapInstanceProperty(_context = _flatInstanceProperty(foo).call(foo)).call(_context, x => x);
