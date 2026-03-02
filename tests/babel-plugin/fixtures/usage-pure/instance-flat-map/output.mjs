11363395c2d20d53b44942349d7b9b5f95b33057
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-02 21:06:53 +0200

    add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-flat-map/output.mjs
@@ -0,0 +1,2 @@
+import _flatMapInstanceProperty from "@core-js/pure/actual/instance/flat-map";
+_flatMapInstanceProperty(arr).call(arr, x => [x]);
---
+++ tests/babel-plugin/fixtures/usage-pure/instance-to-reversed/output.mjs
@@ -0,0 +1,2 @@
+import _toReversedInstanceProperty from "@core-js/pure/actual/instance/to-reversed";
+_toReversedInstanceProperty(arr).call(arr);
