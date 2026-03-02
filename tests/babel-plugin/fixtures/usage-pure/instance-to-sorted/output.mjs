d4c747ee67e44e5f9e224ba2d2f15322ed918cad
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-02 21:06:53 +0200

    add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-to-sorted/output.mjs
@@ -0,0 +1,2 @@
+import _toSortedInstanceProperty from "@core-js/pure/actual/instance/to-sorted";
+_toSortedInstanceProperty(arr).call(arr);
---
+++ tests/babel-plugin/fixtures/usage-pure/instance-trim/output.mjs
@@ -0,0 +1,2 @@
+import _trimInstanceProperty from "@core-js/pure/actual/instance/trim";
+_trimInstanceProperty(s).call(s);
