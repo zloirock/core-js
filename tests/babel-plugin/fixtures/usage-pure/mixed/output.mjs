fd09bc3cf3cf3f7df1924dbdb5ca87432c016df8
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-03 14:48:42 +0200

    refactoring, fix some cases of opt chaining and iterators

--- tests/babel-plugin/fixtures/usage-pure/mixed/output.mjs
+++ tests/babel-plugin/fixtures/usage-pure/mixed/output.mjs
@@ -1,7 +1,7 @@
-var _context;
+var _ref;
 import _Array$from from "@core-js/pure/actual/array/from";
 import _Promise from "@core-js/pure/actual/promise/constructor";
 import _pushInstanceProperty from "@core-js/pure/actual/instance/push";
 _Array$from();
 new _Promise();
-_pushInstanceProperty(_context = []).call(_context);
+_pushInstanceProperty(_ref = []).call(_ref);
