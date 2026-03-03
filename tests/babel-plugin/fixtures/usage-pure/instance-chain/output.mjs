a9d2beba470e6fea9b36cc45468b917fe5268c38
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-03 14:48:42 +0200

    refactoring, fix some cases of opt chaining and iterators

--- tests/babel-plugin/fixtures/usage-pure/instance-chain/output.mjs
+++ tests/babel-plugin/fixtures/usage-pure/instance-chain/output.mjs
@@ -1,4 +1,4 @@
-var _context;
+var _ref;
 import _mapInstanceProperty from "@core-js/pure/actual/instance/map";
 import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
-_mapInstanceProperty(_context = _flatInstanceProperty(foo).call(foo)).call(_context, x => x);
+_mapInstanceProperty(_ref = _flatInstanceProperty(foo).call(foo)).call(_ref, x => x);
