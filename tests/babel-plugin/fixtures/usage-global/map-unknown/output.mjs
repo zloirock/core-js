a1693f7801f4225d7286fefb5554c4abf9465f38
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-06 00:32:00 +0200

    improve some built-in definitions

---
+++ tests/babel-plugin/fixtures/usage-global/map-unknown/output.mjs
@@ -0,0 +1,5 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.map";
+import "core-js/modules/es.iterator.constructor";
+import "core-js/modules/es.iterator.map";
+arr.map(fn);
