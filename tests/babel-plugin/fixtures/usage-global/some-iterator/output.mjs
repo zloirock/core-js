1f0697fe0dd044824aee2bf98e77ecdeee36a4b3
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-06 10:29:36 +0200

    add `iterator` hint

---
+++ tests/babel-plugin/fixtures/usage-global/some-iterator/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.values";
+import "core-js/modules/es.iterator.constructor";
+import "core-js/modules/es.iterator.some";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+import "core-js/modules/web.dom-collections.values";
+[].values().some(fn);
