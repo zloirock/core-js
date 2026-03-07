7c43a46fb8be8c23bd71de1ce63295647e3af472
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 10:59:48 +0200

    fix optimization of symbols injection on generators, add some tests

---
+++ tests/babel-plugin/fixtures/usage-global/for-await-of-generator/output.mjs
@@ -0,0 +1,12 @@
+import "core-js/modules/es.symbol.async-iterator";
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+async function* f() {
+  for await (const x of y);
+}
