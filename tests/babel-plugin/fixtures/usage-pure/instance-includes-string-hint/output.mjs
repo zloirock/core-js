23e582c6e198f1a86f138f628901bea21df2728c
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 16:22:10 +0200

    fix plugin output for instance properties in `usege-pure`

--- tests/babel-plugin/fixtures/usage-pure/instance-includes-string-hint/output.mjs
+++ tests/babel-plugin/fixtures/usage-pure/instance-includes-string-hint/output.mjs
@@ -1,3 +1,3 @@
 var _ref;
-import _includesInstanceProperty from "@core-js/pure/actual/string/includes";
+import _includesInstanceProperty from "@core-js/pure/actual/instance/includes";
 _includesInstanceProperty(_ref = 'str').call(_ref, 'a');
