c52f39e48493cbeb210e0c80722febf0300562db
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 20:23:57 +0200

    fix some `usage-pure` destructuring handling cases, refactoring

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-instance-call-assignment/output.mjs
@@ -0,0 +1,6 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+let at, flat;
+const _ref = getObj();
+at = _atInstanceProperty(_ref);
+flat = _flatInstanceProperty(_ref);
