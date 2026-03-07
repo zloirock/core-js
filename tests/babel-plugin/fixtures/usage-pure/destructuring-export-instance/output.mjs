c817fa7859ec5c4b4d8f5a391f48f237f73a5705
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 21:35:59 +0200

    fix some `usage-pure` destructuring handling cases

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-export-instance/output.mjs
@@ -0,0 +1,4 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+export const at = _atInstanceProperty(obj);
+export const flat = _flatInstanceProperty(obj);
---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-instance-for-loop-skip/input.mjs
@@ -0,0 +1,1 @@
+for (const { at, flat } = getObj();;) {}
