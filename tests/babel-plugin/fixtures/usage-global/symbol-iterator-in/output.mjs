d1cee8eb45d1a52c2369c5bd1757cb62e47b7bc5
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 23:44:16 +0200

    add one missed entry to built-in definitions, add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/symbol-iterator-in/output.mjs
@@ -0,0 +1,6 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+Symbol.iterator in [];
---
+++ tests/babel-plugin/fixtures/usage-pure/static-access/output.mjs
@@ -0,0 +1,6 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Object$entries from "@core-js/pure/actual/object/entries";
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+_Array$from;
+_Object$entries;
+_Promise$resolve;
