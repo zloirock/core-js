6c278c62b8502fb7467a668dcba24409891ab0e1
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 21:35:59 +0200

    fix some `usage-pure` destructuring handling cases

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-export-instance-call/output.mjs
@@ -0,0 +1,5 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+const _ref = getObj();
+export const at = _atInstanceProperty(_ref);
+export const flat = _flatInstanceProperty(_ref);
