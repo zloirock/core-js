6261fc924b47950e41d9cc5bab0ac7bc23cf78ce
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 23:59:33 +0200

    add some more babel plugin fixtures

--- tests/babel-plugin/fixtures/usage-global/destructuring-iife/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/destructuring-iife/output.mjs
@@ -1,14 +1,16 @@
-import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.entries";
 import "core-js/modules/es.object.to-string";
 import "core-js/modules/es.array.iterator";
-import "core-js/modules/es.string.repeat";
-import "core-js/modules/es.string.pad-start";
-import "core-js/modules/es.array.includes";
+import "core-js/modules/es.array.from";
 import "core-js/modules/es.string.includes";
 import "core-js/modules/es.string.iterator";
 import "core-js/modules/web.dom-collections.iterator";
-for (const {
+!function ({
+  entries
+}) {}(Object);
+new function ({
+  from
+}) {}(Array);
+(({
   includes
-} of foo) {
-  bar.padStart(5);
-}
+}) => {})('42');
