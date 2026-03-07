86e67eedd7f602580ce3abb0d2581425d3e90fb9
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 22:39:41 +0200

    refactoring, add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-instance-member-init-multiple/output.mjs
@@ -0,0 +1,5 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+const _ref = obj.list;
+const at = _atInstanceProperty(_ref);
+const flat = _flatInstanceProperty(_ref);
