6bb73a250e8efe5fe31bca50c8d3a707c1d7d7fc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 17:18:23 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-entries-array/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _entriesInstanceProperty from "@core-js/pure/actual/instance/entries";
+_entriesInstanceProperty(_ref = []).call(_ref);
---
+++ tests/babel-plugin/fixtures/usage-pure/instance-keys-array/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _keysInstanceProperty from "@core-js/pure/actual/instance/keys";
+_keysInstanceProperty(_ref = []).call(_ref);
