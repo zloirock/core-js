a77b55b6628c48d3dfb22fe175b5004a52f265df
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 17:18:23 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/instance-at-mixed-types/output.mjs
@@ -0,0 +1,5 @@
+var _ref, _ref2;
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+_atInstanceProperty(foo).call(foo, -1);
+_atInstanceProperty(_ref = []).call(_ref, -1);
+_atInstanceProperty(_ref2 = 'str').call(_ref2, -1);
