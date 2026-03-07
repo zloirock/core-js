84a0d417801dafeba2aa8c164da1cefa1fda1cdd
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 15:33:37 +0200

    fix parenthesized optional chain, add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/optional-instance-through-call/output.mjs
@@ -0,0 +1,3 @@
+var _ref, _ref2;
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+(_ref = foo?.bar()) == null ? void 0 : _flatInstanceProperty(_ref2 = _ref.baz).call(_ref2);
