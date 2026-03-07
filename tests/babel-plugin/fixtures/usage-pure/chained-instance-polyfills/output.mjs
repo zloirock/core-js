25b649bdd5ea2f8b3bb08e58c40035a9993988bd
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-07 15:33:37 +0200

    fix parenthesized optional chain, add some tests

---
+++ tests/babel-plugin/fixtures/usage-pure/chained-instance-polyfills/output.mjs
@@ -0,0 +1,4 @@
+var _ref, _ref2, _ref3;
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _flatInstanceProperty from "@core-js/pure/actual/instance/flat";
+_atInstanceProperty(_ref = _flatInstanceProperty(_ref3 = [1, 2, 3]).call(_ref3)).call(_ref, -1);
