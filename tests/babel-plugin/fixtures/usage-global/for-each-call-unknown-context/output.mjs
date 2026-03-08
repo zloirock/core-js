d430b95b56256032c939a07d2dcb618fa96ac395
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-08 18:38:20 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/for-each-call-unknown-context/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.iterator.constructor";
+import "core-js/modules/es.iterator.for-each";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+import "core-js/modules/web.dom-collections.for-each";
+x.forEach(fn);
---
+++ tests/babel-plugin/fixtures/usage-pure/to-fixed-call-number-context/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _toFixedInstanceProperty from "@core-js/pure/actual/instance/to-fixed";
+_toFixedInstanceProperty(_ref = Number(x)).call(_ref, 2);
