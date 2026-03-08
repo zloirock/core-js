4708a4d660c7d14830d42ee930b78c0d06ab486b
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-08 10:23:48 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/resolve-path-depth-limit/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.regexp.exec";
+import "core-js/modules/es.string.split";
+const a = 'x';
+const b = a;
+const c = b;
+const d = c;
+const e = d;
+const f = e;
+'test'.split(f);
