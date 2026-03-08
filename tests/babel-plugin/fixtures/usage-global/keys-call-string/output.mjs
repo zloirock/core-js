4a4f941b7092a254e13ecd037232fa906b055220
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-08 23:00:41 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/keys-call-string/output.mjs
@@ -0,0 +1,2 @@
+import "core-js/modules/es.object.keys";
+Object.keys("str");
---
+++ tests/babel-plugin/fixtures/usage-global/toString-call-string-context/output.mjs
@@ -0,0 +1,3 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.regexp.to-string";
+'str'.toString();
