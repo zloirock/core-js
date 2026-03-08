8feb52ce71f4ca836a040d7325fe543d9d77be21
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-08 23:00:41 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/toString-call-ts-map-context/output.mjs
@@ -0,0 +1,10 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.map.constructor";
+import "core-js/modules/es.map.species";
+import "core-js/modules/es.map.get-or-insert";
+import "core-js/modules/es.map.get-or-insert-computed";
+import "core-js/modules/es.regexp.to-string";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+(x as Map<any, any>).toString();
