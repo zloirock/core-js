894a95e2b392a9a74fe4b3a0cd81769851c99b41
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-09 18:51:30 +0200

    add some tests for `configPath` / `ignoreBrowserslistConfig`

---
+++ tests/babel-plugin/fixtures/entry-global/browserslist-config-path/options.mjs
@@ -0,0 +1,15 @@
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+export default {
+  plugins: [
+    [
+      '@core-js',
+      {
+        method: 'entry-global',
+        version: '4.0',
+        configPath: dirname(fileURLToPath(import.meta.url)),
+      },
+    ],
+  ],
+};
