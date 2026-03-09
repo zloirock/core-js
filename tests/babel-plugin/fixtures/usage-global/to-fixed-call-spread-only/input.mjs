26ecab1a990a0fe49f05bdebfac8456f5c4d78eb
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-09 18:34:41 +0200

    add `shouldInjectPolyfill` support, refactoring, add some babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/to-fixed-call-spread-only/input.mjs
@@ -0,0 +1,1 @@
+n.toFixed(...args);
---
+++ tests/babel-plugin/fixtures/usage-global/to-fixed-call-spread-only/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.repeat";
+import "core-js/modules/es.number.to-fixed";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+n.toFixed(...args);
