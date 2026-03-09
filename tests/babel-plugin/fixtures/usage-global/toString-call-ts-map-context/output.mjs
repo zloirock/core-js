bae7cb21b6ea43f36fe8346ab26e5229102b0b6f
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-09 21:09:01 +0200

    add `rest` hint

--- tests/babel-plugin/fixtures/usage-global/toString-call-ts-map-context/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/toString-call-ts-map-context/output.mjs
@@ -4,7 +4,6 @@ import "core-js/modules/es.map.constructor";
 import "core-js/modules/es.map.species";
 import "core-js/modules/es.map.get-or-insert";
 import "core-js/modules/es.map.get-or-insert-computed";
-import "core-js/modules/es.regexp.to-string";
 import "core-js/modules/es.string.iterator";
 import "core-js/modules/web.dom-collections.iterator";
 (x as Map<any, any>).toString();
