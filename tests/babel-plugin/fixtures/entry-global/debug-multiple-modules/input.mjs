7a559837c3a6c8bb86a7414387e27e736ad24c32
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-09 14:40:51 +0200

    add support and test `debug` plugin option

---
+++ tests/babel-plugin/fixtures/entry-global/debug-multiple-modules/input.mjs
@@ -0,0 +1,2 @@
+import 'core-js/actual/object/has-own';
+import 'core-js/actual/array/at';
---
+++ tests/babel-plugin/fixtures/usage-global/debug-multiple/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es.object.has-own";
+import "core-js/modules/es.array.at";
+[].at(-1);
+Object.hasOwn({}, 'x');
