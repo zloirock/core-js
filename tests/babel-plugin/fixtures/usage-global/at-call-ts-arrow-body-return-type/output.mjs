6ea2d280533f8389ca9f581c41489f2d1cb262fc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-10 16:06:29 +0200

    resolve functions return type

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-arrow-body-return-type/output.mjs
@@ -0,0 +1,3 @@
+import "core-js/modules/es.array.at";
+const getArr = () => [1, 2, 3];
+getArr().at(-1);
---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-object-arrow-body-return-type/output.mjs
@@ -0,0 +1,5 @@
+import "core-js/modules/es.array.at";
+const obj = {
+  getArr: () => [1, 2, 3]
+};
+obj.getArr().at(-1);
