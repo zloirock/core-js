9e682358030fd86a2ba53a1cb555224341dd2e66
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-11 01:14:56 +0200

    resolve TS `await`

---
+++ tests/babel-plugin/fixtures/usage-global/freeze-call-ts-await-promise-object/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+async function foo(p: Promise<{
+  key: string;
+}>) {
+  Object.freeze(await p);
+}
