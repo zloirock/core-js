8f3552bf950c3c319e209b0a62610687e116c440
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-11 01:14:56 +0200

    resolve TS `await`

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-await-non-promise/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.array.at";
+async function foo(x: number[]) {
+  (await x).at(-1);
+}
