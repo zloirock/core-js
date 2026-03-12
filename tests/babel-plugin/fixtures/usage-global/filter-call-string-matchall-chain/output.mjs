45d7c385a492f870aa12107517218ffd2510fd30
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-12 14:26:45 +0200

    increase `known-built-in-return-types` coverage

---
+++ tests/babel-plugin/fixtures/usage-global/filter-call-string-matchall-chain/output.mjs
@@ -0,0 +1,6 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.iterator.constructor";
+import "core-js/modules/es.iterator.filter";
+import "core-js/modules/es.regexp.exec";
+import "core-js/modules/es.string.match-all";
+'foo'.matchAll(/o/g).filter(fn);
