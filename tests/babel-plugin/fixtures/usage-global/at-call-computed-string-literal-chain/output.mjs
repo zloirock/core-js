b104e634435ce5647716b388b9cb5081b3640ea3
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-12 14:44:27 +0200

    improve resolving of computed keys

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-computed-string-literal-chain/output.mjs
@@ -0,0 +1,3 @@
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.trim";
+' foo '['trim']().at(-1);
---
+++ tests/babel-plugin/fixtures/usage-global/at-call-self-array-from-chain/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.array.from";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+import "core-js/modules/web.self";
+self.Array.from(x).at(-1);
