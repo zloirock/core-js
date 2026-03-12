63796018c9a6bdfc95e1816fbd556ddeacd13091
John <john-xyz@outlook.com>
2026-03-12 17:25:36 -0500

    Remove cross imports

--- packages/core-js/modules/web.request-idle-callback.js
+++ packages/core-js/modules/web.request-idle-callback.js
@@ -1,18 +1,10 @@
 'use strict';
 var $ = require('../internals/export');
 var globalThis = require('../internals/global-this');
-var IS_PURE = require('../internals/is-pure');
 
 var BASIC = !!globalThis.requestIdleCallback && !!globalThis.cancelIdleCallback;
 var requestIdleCallback = require('../internals/idle-callbacks').request;
-var cancelIdleCallback = require('../internals/idle-callbacks').cancel;
 
 $({ global: true, forced: !BASIC }, {
   requestIdleCallback: requestIdleCallback
 });
-
-if (!IS_PURE) {
-  $({ global: true, forced: !BASIC }, {
-    cancelIdleCallback: cancelIdleCallback
-  });
-}
