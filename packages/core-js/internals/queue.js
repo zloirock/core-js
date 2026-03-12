c1b626f527cbac31b357a2326e6af3e1868fee10
John <john-xyz@outlook.com>
2026-03-12 17:06:07 -0500

    Add removal functionality to queue

--- packages/core-js/internals/queue.js
+++ packages/core-js/internals/queue.js
@@ -6,19 +6,29 @@ var Queue = function () {
 
 Queue.prototype = {
   add: function (item) {
-    var entry = { item: item, next: null };
+    var entry = { item: item, next: null, prev: this.tail };
     var tail = this.tail;
     if (tail) tail.next = entry;
     else this.head = entry;
     this.tail = entry;
+    return entry;
   },
   get: function () {
     var entry = this.head;
     if (entry) {
       var next = this.head = entry.next;
       if (next === null) this.tail = null;
+      else next.prev = null;
       return entry.item;
     }
+  },
+  remove: function (entry) {
+    var prev = entry.prev;
+    var next = entry.next;
+    if (prev) prev.next = next;
+    else this.head = next;
+    if (next) next.prev = prev;
+    else this.tail = prev;
   },
   empty: function () {
     return !this.head;
