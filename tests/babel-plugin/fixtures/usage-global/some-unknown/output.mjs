c025831652a08b6b0d3f3e1d0f697a8608f3d954
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-12 20:42:21 +0200

    implement `Iterator.prototype.includes`

--- tests/babel-plugin/fixtures/usage-global/some-unknown/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/some-unknown/output.mjs
@@ -1,7 +1,4 @@
 import "core-js/modules/es.object.to-string";
-import "core-js/modules/es.array.iterator";
 import "core-js/modules/es.iterator.constructor";
 import "core-js/modules/es.iterator.some";
-import "core-js/modules/es.string.iterator";
-import "core-js/modules/web.dom-collections.iterator";
 foo.some(fn);
