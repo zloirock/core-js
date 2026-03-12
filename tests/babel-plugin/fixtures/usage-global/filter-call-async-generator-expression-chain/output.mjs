b3105d85772cb2d9ae2fd50fe4e64a6cb7309fd8
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-12 13:49:13 +0200

    resolve generator types

---
+++ tests/babel-plugin/fixtures/usage-global/filter-call-async-generator-expression-chain/output.mjs
@@ -0,0 +1,13 @@
+import "core-js/modules/es.symbol.async-iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.promise.reject";
+import "core-js/modules/es.promise.resolve";
+import "core-js/modules/esnext.async-iterator.constructor";
+import "core-js/modules/esnext.async-iterator.filter";
+const gen = async function* () {
+  yield 1;
+};
+gen().filter(fn);
---
+++ tests/babel-plugin/fixtures/usage-global/filter-call-unknown-mode-es/output.mjs
@@ -0,0 +1,5 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.filter";
+import "core-js/modules/es.iterator.constructor";
+import "core-js/modules/es.iterator.filter";
+x.filter(fn);
