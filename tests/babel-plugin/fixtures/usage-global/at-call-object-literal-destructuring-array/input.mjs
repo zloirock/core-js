f7e8b02adb66deda4cea8deb4cfecceacb5a3cea
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-13 19:21:23 +0200

    resolve js object destructuring

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-object-literal-destructuring-array/input.mjs
@@ -0,0 +1,2 @@
+const { items } = { items: [1, 2, 3] };
+items.at(0);
---
+++ tests/babel-plugin/fixtures/usage-global/at-call-object-literal-destructuring-conditional/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.string.at";
+const flag = true;
+const {
+  val
+} = {
+  val: flag ? 'yes' : 'no'
+};
+val.at(0);
