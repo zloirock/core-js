4eb1aec225877f956e96b08d32ef149f1d6f89cd
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-13 20:13:43 +0200

    resolve for-of and destructuring on string

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-array-destructuring-string-template/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const [a] = `hello`;
+a.at(0);
