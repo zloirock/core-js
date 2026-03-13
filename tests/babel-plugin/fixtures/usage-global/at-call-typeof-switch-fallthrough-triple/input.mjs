8ab74ce73089a8de9d321ae43fe2ffd277f45cef
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-14 00:47:35 +0200

    resolve switch without fallthrough as an OR chain of guards

--- tests/babel-plugin/fixtures/usage-global/at-call-typeof-switch-fallthrough-triple/input.mjs
+++ tests/babel-plugin/fixtures/usage-global/at-call-typeof-switch-fallthrough-triple/input.mjs
@@ -1,8 +1,8 @@
-import "core-js/modules/es.string.at";
 function foo(x) {
   switch (typeof x) {
     case 'string':
     case 'number':
+    case 'bigint':
       x.at(0);
       break;
   }
--- tests/babel-plugin/fixtures/usage-global/at-call-typeof-switch-fallthrough-triple/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/at-call-typeof-switch-fallthrough-triple/output.mjs
@@ -3,6 +3,7 @@ function foo(x) {
   switch (typeof x) {
     case 'string':
     case 'number':
+    case 'bigint':
       x.at(0);
       break;
   }
