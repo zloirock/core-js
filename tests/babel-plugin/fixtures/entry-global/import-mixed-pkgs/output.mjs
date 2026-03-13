08553643810bd596e193c4c8e365b8e3fdae6013
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-13 19:42:39 +0200

    rename plugin options

--- tests/babel-plugin/fixtures/entry-global/import-mixed-pkgs/output.mjs
+++
@@ -1,5 +0,0 @@
-import "@x/y/modules/es.iterator.dispose";
-import "@x/y/modules/es.math.sum-precise";
-import "@x/y/modules/esnext.iterator.to-async";
-import "@x/y/modules/esnext.iterator.zip";
-import 'unknown-pkg/es/array';
