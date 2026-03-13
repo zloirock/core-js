19f64bded3c02b8fd097c91c2559be3a1373abb5
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-13 21:36:22 +0200

    resolve for-of and destructuring on array literal

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-array-destructuring-array-literal-variable/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const arr = ['a', 'b', 'c'];
+const [x] = arr;
+x.at(0);
