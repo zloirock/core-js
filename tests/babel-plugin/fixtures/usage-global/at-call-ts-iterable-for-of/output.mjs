299db5dae39cb06275e6eb8fdbe8284941bd4512
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-14 00:25:06 +0200

    fix some issues

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-iterable-for-of/output.mjs
@@ -0,0 +1,11 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+function foo(x: Iterable<string>) {
+  for (const item of x) {
+    item.at(0);
+  }
+}
