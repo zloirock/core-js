13ad75ede9e8b02109aec2ed79f86edcaf97ccc8
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-14 13:37:00 +0200

    fix some cases of type resolving

---
+++ tests/babel-plugin/fixtures/usage-global/name-destructured-for-of-object-literal/output.mjs
@@ -0,0 +1,17 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const items = [{
+  name: 'hello'
+}, {
+  name: 'world'
+}];
+for (const {
+  name
+} of items) {
+  name.at(0);
+}
