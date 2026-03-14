7b1295c28a3a9aa6aa7c270b039ff05e13acd6b5
John Zhou <john-xyz@outlook.com>
2026-03-14 12:44:33 -0500

    Work around firefox bug

--- packages/core-js/modules/web.request-idle-callback.js
+++ packages/core-js/modules/web.request-idle-callback.js
@@ -1,9 +1,24 @@
 'use strict';
 var $ = require('../internals/export');
+var globalThis = require('../internals/global-this');
+var uncurryThis = require('../internals/function-uncurry-this');
 
-var requestIdleCallback = require('../internals/idle-callbacks').request;
-var FORCED = require('../internals/idle-callbacks').forced;
+var $requestIdleCallback = require('../internals/idle-callbacks').request;
 
-$({ global: true, forced: FORCED }, {
-  requestIdleCallback: requestIdleCallback
+var nativeRequestIdleCallback = globalThis.requestIdleCallback;
+var apply = uncurryThis(Function.prototype.apply);
+var NEEDED = require('../internals/idle-callbacks').forced;
+
+$({ global: true, forced: true }, {
+  requestIdleCallback: function requestIdleCallback(callback) {
+    // No support at all; use the custom polyfill
+    if (NEEDED) return apply($requestIdleCallback, null, arguments);
+    // We have basic support. SpiderMonkey / Firefox handles negative timeouts incorrectly.
+    // Since there's no good way to detect Firefox in production, always normalize timeout.
+    var options = arguments[1];
+    var timeout;
+    if (options && options.timeout !== undefined) timeout = +options.timeout;
+    if (timeout <= 0) timeout = undefined;
+    return nativeRequestIdleCallback(callback, { timeout: timeout });
+  }
 });
