d09dc3ded69cb7291e912c5567e94f0a0480e093
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-14 15:38:03 +0200

    resolve some cases of `x as Type`, `x!`, `x satisfies Type`

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-for-of-as-const-array/output.mjs
@@ -0,0 +1,10 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const items = ['hello', 'world'] as const;
+for (const item of items) {
+  item.at(0);
+}
