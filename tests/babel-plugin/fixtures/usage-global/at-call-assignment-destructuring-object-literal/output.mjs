1aa143311a14c9b3786bc732e0e60b181b13dd41
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 20:39:10 +0200

    resolve AssignmentExpression destructuring

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-assignment-destructuring-object-literal/output.mjs
@@ -0,0 +1,10 @@
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.bold";
+let name;
+({
+  name
+} = {
+  name: 'alice'
+});
+name.at(0).bold();
