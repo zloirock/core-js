52d7320042436c545edbd90f0e41473dbb56ae9a
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 12:07:57 +0200

    resolve async functions inner type

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-await-async-body-string/output.mjs
@@ -0,0 +1,9 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.string.at";
+async function getName() {
+  return 'hello';
+}
+(await getName()).at(0);
