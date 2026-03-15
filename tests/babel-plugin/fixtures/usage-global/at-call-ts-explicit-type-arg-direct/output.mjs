375a851b46ea00a2af5f39ff74867e21109e2d0a
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 14:29:37 +0200

    resolve explicit type arguments on call site

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-explicit-type-arg-direct/output.mjs
@@ -0,0 +1,7 @@
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.includes";
+function cast<T>(x: unknown): T {
+  return x as T;
+}
+cast<string>(0).at(0);
+cast<string>(1).includes('x');
---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-generic-array-param-inner-type/input.mjs
@@ -0,0 +1,4 @@
+function first<T>(arr: T[]): T {
+  return arr[0];
+}
+first(Object.keys(x)).at(0);
