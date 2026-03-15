859d82a80b4d97220f4e11614f7d4240084564a0
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 23:03:28 +0200

    fix TSRestType

---
+++ tests/babel-plugin/fixtures/usage-global/bold-call-ts-tuple-rest-inner/input.mjs
@@ -0,0 +1,3 @@
+declare const tuple: [string, ...string[]];
+tuple.at(0).bold();
+tuple.at(1).fontcolor('red');
---
+++ tests/babel-plugin/fixtures/usage-global/destructure-ts-tuple-rest-element/input.mjs
@@ -0,0 +1,4 @@
+declare const tuple: [string, ...string[]];
+const [first, second] = tuple;
+first.bold();
+second.strike();
