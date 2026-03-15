0d323c2ef71d98cec4ad6d2a8edcd2220acf092c
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 11:40:17 +0200

    take into account try-catch in resolveBodyReturnType

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-body-return-null/input.mjs
@@ -0,0 +1,5 @@
+function getData(x) {
+  if (!x) return null;
+  return [1, 2, 3];
+}
+getData(x).at(-1);
--- tests/babel-plugin/fixtures/usage-global/at-call-body-return-null/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/at-call-body-return-null/output.mjs
@@ -1,6 +1,6 @@
 import "core-js/modules/es.array.at";
 function getData(x) {
-  if (x) return [1, 2, 3];
-  return;
+  if (!x) return null;
+  return [1, 2, 3];
 }
-getData(true).at(-1);
+getData(x).at(-1);
