add434ca14448f42b437721ce4efce77105a392d
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-16 00:46:33 +0200

    fix resolving types in TSModuleDeclaration / ExportDefaultDeclaration

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-declare-global-type/output.mjs
@@ -0,0 +1,21 @@
+import "core-js/modules/es.string.repeat";
+import "core-js/modules/es.string.pad-end";
+import "core-js/modules/es.array.at";
+declare global {
+  // alias chain: followTypeAliasChain path
+  type Strings = string[];
+  type MyStrings = Strings;
+  // interface: resolveUserDefinedType path
+  interface Config {
+    keys: Strings;
+  }
+}
+function foo(x: MyStrings) {
+  x.at(-1).padEnd(5);
+}
+function bar(c: Config) {
+  const {
+    keys
+  } = c;
+  keys.at(-1).padEnd(5);
+}
