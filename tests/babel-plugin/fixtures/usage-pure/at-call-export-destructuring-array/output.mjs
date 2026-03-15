e68747f02294db34d4fd30afbfcd1597520622b1
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 22:10:00 +0200

    add a fixture

---
+++ tests/babel-plugin/fixtures/usage-pure/at-call-export-destructuring-array/output.mjs
@@ -0,0 +1,7 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _includesInstanceProperty from "@core-js/pure/actual/instance/includes";
+import _findIndexInstanceProperty from "@core-js/pure/actual/instance/find-index";
+const _ref = [1, 2, 3];
+export const at = _atInstanceProperty(_ref);
+export const includes = _includesInstanceProperty(_ref);
+export const findIndex = _findIndexInstanceProperty(_ref);
