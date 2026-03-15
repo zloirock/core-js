bc3d54ed79112bd163c425121b85932b95e9cfd0
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 13:45:16 +0200

    improve resolution of array elements

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-array-literal-filter-at-includes-chain/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.array.filter";
+import "core-js/modules/es.string.includes";
+['hello', 'world'].filter(Boolean).at(0).includes('h');
