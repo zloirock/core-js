0920dab5bb3064e9a856f262561f220c6d16dc0d
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 20:51:10 +0200

    add validation to resolvePureEntry

---
+++ tests/babel-plugin/fixtures/usage-pure/at-call-assignment-destructuring-array/output.mjs
@@ -0,0 +1,6 @@
+import _atInstanceProperty from "@core-js/pure/actual/instance/at";
+import _includesInstanceProperty from "@core-js/pure/actual/instance/includes";
+let at, includes;
+const _ref = [1, 2, 3];
+at = _atInstanceProperty(_ref);
+includes = _includesInstanceProperty(_ref);
