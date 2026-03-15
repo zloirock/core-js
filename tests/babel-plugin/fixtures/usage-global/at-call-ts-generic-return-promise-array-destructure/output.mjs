1136390c9e44888030494adf8d13de97bd551177
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-15 13:04:29 +0200

    fix inner type propogation for containers

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-generic-return-promise-array-destructure/output.mjs
@@ -0,0 +1,14 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+async function fetchItems<T>(item: T): Promise<T[]> {
+  return [item];
+}
+const [first] = await fetchItems('hello');
+first.at(0);
