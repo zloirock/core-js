f178045c3b8bb23142e8bf19960a5118966693cf
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-16 14:10:52 +0200

    add missed null check

---
+++ tests/babel-plugin/fixtures/usage-global/destructuring-ts-declare-no-init/output.mjs
@@ -0,0 +1,10 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.repeat";
+import "core-js/modules/es.number.to-fixed";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+declare const [a, b]: [number, string];
+a.toFixed(2);
+b.repeat(3);
