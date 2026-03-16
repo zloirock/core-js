ae383b39de1cdd1b10c3cd98367b2364138ea4c7
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-16 18:57:08 +0200

    add resolving T[indexed access with keyword]

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-indexed-access-number-tuple/input.mjs
@@ -0,0 +1,3 @@
+type Pair = [string, string];
+declare const x: Pair[number];
+x.at(-1);
---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-indexed-access-number-tuple/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es.string.at";
+type Pair = [string, string];
+declare const x: Pair[number];
+x.at(-1);
