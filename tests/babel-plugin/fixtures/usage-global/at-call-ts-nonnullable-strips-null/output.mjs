95bcd86349cffadc364339e25a299aa7bf9002bc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-16 15:41:52 +0200

    fix NonNullable<null>

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-nonnullable-strips-null/output.mjs
@@ -0,0 +1,7 @@
+import "core-js/modules/es.string.repeat";
+import "core-js/modules/es.number.to-fixed";
+import "core-js/modules/es.string.at";
+declare const a: NonNullable<string | null>;
+a.at(0);
+declare const b: NonNullable<null>;
+b.toFixed(2);
