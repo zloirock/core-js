c9ea6b90d8e2ef8c0bb157b9dae722613e2f227a
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 01:51:56 +0200

    move tooling to esm

--- packages/core-js-compat/get-entries-list-for-target-version.js
+++ packages/core-js-compat/get-entries-list-for-target-version.js
@@ -1,14 +1,13 @@
-'use strict';
-const { compare, intersection, normalizeCoreJSVersion } = require('./helpers');
-const entriesByVersions = require('./entries-by-versions');
-const allEntries = require('./entries');
+import { compare, intersection, normalizeCoreJSVersion } from './helpers.js';
+import entriesByVersions from './entries-by-versions.json' with { type: 'json' };
+import allEntries from './entries.json' with { type: 'json' };
 
 const allEntriesList = Object.keys(allEntries);
 
-module.exports = function (raw) {
+export default function (raw) {
   const corejs = normalizeCoreJSVersion(raw);
   const result = Object.entries(entriesByVersions).flatMap(([version, entries]) => {
     return compare(version, '<=', corejs) ? entries : [];
   });
   return intersection(result, allEntriesList);
-};
+}
