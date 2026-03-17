1454e1dafc6c0c6205cd4a61e755752935d0c783
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-17 14:46:50 +0200

    fix some issues

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-flow-tuple-string-iteration/output.mjs
@@ -0,0 +1,11 @@
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.string.at";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+function foo(x: [string, string]) {
+  for (const s of x) {
+    s.at(0);
+  }
+}
