b5addcf1d47fe84038a85e4be199945adbad1ffd
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-17 14:46:50 +0200

    fix some issues

---
+++ tests/babel-plugin/fixtures/usage-global/catch-call-async-unresolvable-body/output.mjs
@@ -0,0 +1,8 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+async function fetchData() {
+  return unknownFunction();
+}
+fetchData().catch(console.error);
