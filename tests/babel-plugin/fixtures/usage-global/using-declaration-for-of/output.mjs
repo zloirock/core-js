7246f3f0ff27283a2de3d1ec0612b484e59c13cc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 02:54:37 +0200

    inject polyfills on `using`

---
+++ tests/babel-plugin/fixtures/usage-global/using-declaration-for-of/output.mjs
@@ -0,0 +1,17 @@
+import "core-js/modules/es.symbol.async-dispose";
+import "core-js/modules/es.symbol.dispose";
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.suppressed-error.constructor";
+import "core-js/modules/es.promise.constructor";
+import "core-js/modules/es.promise.catch";
+import "core-js/modules/es.promise.finally";
+import "core-js/modules/es.promise.resolve";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.async-iterator.async-dispose";
+import "core-js/modules/es.iterator.dispose";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+for (using resource of getResources()) {
+  resource.use();
+}
