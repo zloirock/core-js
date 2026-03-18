bf3bc2d4d3d6383bebc957cbb59085671f6b6da0
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 02:59:33 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/skip-core-js-bundle-file/options.mjs
@@ -0,0 +1,10 @@
+export default {
+  filename: '/node_modules/@core-js/bundle/index.js',
+  plugins: [
+    ['@core-js', {
+      method: 'usage-global',
+      version: '4.0',
+      targets: { ie: 11 },
+    }],
+  ],
+};
