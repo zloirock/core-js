c7c6bd59958686f303ea9e33fd8e38e6cb67b3cc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 02:19:27 +0200

    fix typeParamMap for TSIndexedAccessType

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-indexed-access-type-param-explicit/output.mjs
@@ -0,0 +1,11 @@
+import "core-js/modules/es.array.at";
+function getValue<T extends {
+  data: number[];
+}>(obj: T): T["data"] {
+  return obj.data;
+}
+getValue<{
+  data: number[];
+}>({
+  data: [1, 2]
+}).at(0);
