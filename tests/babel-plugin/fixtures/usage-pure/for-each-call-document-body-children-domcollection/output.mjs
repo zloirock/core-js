294be67fc00e03590d6097b86d2dd5510763e936
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 21:39:41 +0200

    improve known-built-in-return-types

---
+++ tests/babel-plugin/fixtures/usage-pure/for-each-call-document-body-children-domcollection/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _forEachMaybeDomCollections from "@core-js/pure/actual/dom-collections/instance/for-each";
+_forEachMaybeDomCollections(_ref = document.body.childNodes).call(_ref, el => console.log(el));
