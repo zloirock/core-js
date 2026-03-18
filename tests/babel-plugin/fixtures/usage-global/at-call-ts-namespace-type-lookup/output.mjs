afb41b6fd8cffe91892fe98ea6f0c7564bfeb830
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-18 02:59:33 +0200

    add some more babel plugin fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-ts-namespace-type-lookup/output.mjs
@@ -0,0 +1,7 @@
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.string.at";
+namespace Models {
+  export type Items = string[];
+}
+declare const items: Models.Items;
+items.at(0);
