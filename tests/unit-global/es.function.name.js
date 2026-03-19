1c0b3659a4907835241cb3a4db2e3ff0dcf7c877
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-19 21:13:55 +0200

    add injectable pure Function#name polyfill

--- tests/unit-global/es.function.name.js
+++ tests/unit-global/es.function.name.js
@@ -1,11 +1,11 @@
 QUnit.test('Function#name', assert => {
-  assert.true('name' in Function.prototype);
+  assert.true('name' in Function.prototype, 'name in Function.prototype');
   assert.nonEnumerable(Function.prototype, 'name');
   function foo() { /* empty */ }
-  assert.same(foo.name, 'foo');
-  assert.same(function () { /* empty */ }.name, '');
+  assert.same(foo.name, 'foo', 'named function');
+  assert.same(function () { /* empty */ }.name, '', 'anonymous function');
   if (Object.freeze) {
-    assert.same(Object.freeze(() => { /* empty */ }).name, '');
+    assert.same(Object.freeze(() => { /* empty */ }).name, '', 'frozen arrow function');
   }
   function bar() { /* empty */ }
   bar.toString = function () {
@@ -16,23 +16,23 @@ QUnit.test('Function#name', assert => {
   baz.toString = function () {
     return '';
   };
-  assert.same(baz.name, '');
+  assert.same(baz.name, '', 'empty `.toString`');
 
   assert.same(function /*
-  multi-line comment */() { /* empty */ }.name, '');
+  multi-line comment */() { /* empty */ }.name, '', 'anonymous with multi-line comment');
 
   function /*
   multi-line comment */
   foobar() { /* empty */ }
-  assert.same(foobar.name, 'foobar');
+  assert.same(foobar.name, 'foobar', 'named with multi-line comment before name');
 
   function // simple-line comment
   foobaz() { /* empty */ }
-  assert.same(foobaz.name, 'foobaz');
+  assert.same(foobaz.name, 'foobaz', 'named with line comment before name');
 
   function // simple-line comment
   /* multi-line comment */quux/*
   multi-line comment
   */() { /* empty */ }
-  assert.same(quux.name, 'quux');
+  assert.same(quux.name, 'quux', 'named with mixed comments around name');
 });
