300637ada147250320a0f6c1c409528dcf2b047b
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-19 21:13:55 +0200

    add injectable pure Function#name polyfill

--- tests/babel-plugin/fixtures/usage-pure/optional-instance-chain-access/output.mjs
+++ tests/babel-plugin/fixtures/usage-pure/optional-instance-chain-access/output.mjs
@@ -1,2 +1,3 @@
+import _nameMaybeFunction from "@core-js/pure/actual/function/instance/name";
 import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
-foo == null ? void 0 : _flatMaybeArray(foo).name;
+foo == null ? void 0 : _nameMaybeFunction(_flatMaybeArray(foo));
