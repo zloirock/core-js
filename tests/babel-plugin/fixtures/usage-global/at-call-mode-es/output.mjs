9c8bce409942a52fbd1fcae4b8c27f55c2122626
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-20 23:41:07 +0200

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/at-call-mode-es/output.mjs
@@ -0,0 +1,7 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.array.from";
+import "core-js/modules/es.string.iterator";
+[].at(0);
+Array.from(x);
