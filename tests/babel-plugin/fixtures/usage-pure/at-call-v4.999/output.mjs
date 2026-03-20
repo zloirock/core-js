cbe8067c5a8c7861f682fc70e30f15c1d77362d3
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-20 23:41:07 +0200

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/at-call-v4.999/output.mjs
@@ -0,0 +1,5 @@
+var _ref;
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+import _Array$from from "@core-js/pure/actual/array/from";
+_atMaybeArray(_ref = []).call(_ref, 0);
+_Array$from(x);
