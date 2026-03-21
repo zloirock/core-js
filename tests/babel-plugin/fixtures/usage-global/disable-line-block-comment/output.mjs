e6c4790acd2572809ebe84ceb46273c1a748d2eb
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-21 16:53:28 +0200

    add directives support to babel plugin

---
+++ tests/babel-plugin/fixtures/usage-global/disable-line-block-comment/output.mjs
@@ -0,0 +1,4 @@
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.string.at";
+arr.includes(x); /* core-js-disable-line */
+arr.at(0);
---
+++ tests/babel-plugin/fixtures/usage-pure/disable-line/input.mjs
@@ -0,0 +1,2 @@
+arr.includes(x); // core-js-disable-line
+arr.at(0);
