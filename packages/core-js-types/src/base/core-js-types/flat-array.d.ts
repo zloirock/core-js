82589ac7274a9f4ef1a9736fef13679fb698323f
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-22 12:34:03 +0200

    drop dead fallback

--- packages/core-js-types/src/base/core-js-types/flat-array.d.ts
+++ packages/core-js-types/src/base/core-js-types/flat-array.d.ts
@@ -3,9 +3,7 @@
 // License: https://github.com/microsoft/TypeScript/blob/v5.9.3/LICENSE.txt
 
 declare namespace CoreJS {
-  export type CoreJSFlatArray<Arr, Depth extends number> = typeof globalThis extends { FlatArray: infer O } // from ts 4.4
-    ? O
-    : {
+  export type CoreJSFlatArray<Arr, Depth extends number> = {
       done: Arr;
       recur: Arr extends ReadonlyArray<infer InnerArr> ? CoreJSFlatArray<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
         : Arr;
