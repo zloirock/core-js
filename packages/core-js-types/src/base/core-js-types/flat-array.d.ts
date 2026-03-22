ccd27fa160d0ae0d1af59a227e540a2666045b99
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-22 12:46:23 +0200

    improve types tests

--- packages/core-js-types/src/base/core-js-types/flat-array.d.ts
+++ packages/core-js-types/src/base/core-js-types/flat-array.d.ts
@@ -4,8 +4,8 @@
 
 declare namespace CoreJS {
   export type CoreJSFlatArray<Arr, Depth extends number> = {
-      done: Arr;
-      recur: Arr extends ReadonlyArray<infer InnerArr> ? CoreJSFlatArray<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
-        : Arr;
-    }[Depth extends -1 ? 'done' : 'recur'];
+    done: Arr;
+    recur: Arr extends ReadonlyArray<infer InnerArr> ? CoreJSFlatArray<InnerArr, [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20][Depth]>
+      : Arr;
+  }[Depth extends -1 ? 'done' : 'recur'];
 }
