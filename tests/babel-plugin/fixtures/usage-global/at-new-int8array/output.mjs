982a767abe3364b3427d144615935dde435e03cf
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-24 18:34:42 +0200

    exclude string iterator dependency from typed array constructors

--- tests/babel-plugin/fixtures/usage-global/at-new-int8array/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/at-new-int8array/output.mjs
@@ -2,7 +2,6 @@ import "core-js/modules/es.object.to-string";
 import "core-js/modules/es.array.iterator";
 import "core-js/modules/es.array.entries";
 import "core-js/modules/es.array.keys";
-import "core-js/modules/es.string.iterator";
 import "core-js/modules/es.typed-array.int8-array";
 import "core-js/modules/es.typed-array.iterator";
 import "core-js/modules/es.typed-array.at";
