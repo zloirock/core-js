cf2163340aba89c0a073010c8bf66c7c41f3a04a
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-24 16:47:52 +0200

    tweak some iterator dependencies

--- tests/babel-plugin/fixtures/usage-global/at-call-filter-map-chain/output.mjs
+++ tests/babel-plugin/fixtures/usage-global/at-call-filter-map-chain/output.mjs
@@ -1,5 +1,4 @@
 import "core-js/modules/es.array.at";
 import "core-js/modules/es.array.filter";
 import "core-js/modules/es.array.map";
-import "core-js/modules/es.array.species";
 [1, 2, 3].filter(x => x > 0).map(x => x * 2).at(-1);
