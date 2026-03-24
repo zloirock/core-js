d3a6ac76ca741fd6c6b53f127048a8edf6f0e11b
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-24 18:34:42 +0200

    exclude string iterator dependency from typed array constructors

--- packages/core-js/internals/typed-array-from.js
+++ packages/core-js/internals/typed-array-from.js
@@ -19,7 +19,7 @@ module.exports = function from(source /* , mapfn, thisArg */) {
   var mapping = mapfn !== undefined;
   if (mapping) aCallable(mapfn);
   var O = toObject(source);
-  // @dependency: es.string.iterator
+  // es.string.iterator dependency declared in es.typed-array.from since it's not required for constructors
   var iteratorMethod = getIteratorMethod(O);
   var i, length, result, thisIsBigIntArray, value, step, iterator, next;
   if (iteratorMethod && !isArrayIteratorMethod(iteratorMethod)) {
