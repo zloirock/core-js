80b959cf4c52347df0602338d2b8edab878c61a7
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-30 20:04:18 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-multi-template-literal/output.mjs
@@ -0,0 +1,6 @@
+import _trimMaybeString from "@core-js/pure/actual/string/instance/trim";
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+const x = 'world';
+const _ref = `hello ${x}`;
+const trim = _trimMaybeString(_ref);
+const at = _atMaybeString(_ref);
