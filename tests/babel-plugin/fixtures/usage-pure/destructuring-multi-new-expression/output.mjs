423c5926a1d6218bd2b21e69cd1dea76714e5ed4
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-30 20:04:18 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-multi-new-expression/output.mjs
@@ -0,0 +1,5 @@
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+import _includesMaybeArray from "@core-js/pure/actual/array/instance/includes";
+const _ref = new Array(3);
+const at = _atMaybeArray(_ref);
+const includes = _includesMaybeArray(_ref);
