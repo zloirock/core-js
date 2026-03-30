c4da3980c6a029fc33feb358d7074eb9a82729dc
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-30 22:04:17 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/var-shadow-skip/output.mjs
@@ -0,0 +1,7 @@
+import "core-js/modules/es.array.at";
+import "core-js/modules/es.string.at";
+var Array = require('./array');
+var Promise = require('./promise');
+Array.from(x);
+new Promise(fn);
+Array.prototype.at.call(y, -1);
