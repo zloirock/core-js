746d088b1daf8bf30a7e11b397972052612fcd34
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-30 20:04:18 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-pure/absolute-imports-string-path/output.mjs
@@ -0,0 +1,2 @@
+import _at from "<CWD>/packages/core-js-pure/actual/instance/at.js";
+_at(foo).call(foo, -1);
---
+++ tests/babel-plugin/fixtures/usage-pure/destructuring-multi-string-literal/output.mjs
@@ -0,0 +1,5 @@
+import _trimMaybeString from "@core-js/pure/actual/string/instance/trim";
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+const _ref = 'hello';
+const trim = _trimMaybeString(_ref);
+const at = _atMaybeString(_ref);
