47d4e4929141f3a3cafc13073168423b7c26de02
Denis Pushkarev <zloirock@zloirock.ru>
2026-03-31 11:32:39 +0300

    add some fixtures

---
+++ tests/babel-plugin/fixtures/usage-global/const-alias-chain/output.mjs
@@ -0,0 +1,6 @@
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.from";
+import "core-js/modules/es.string.iterator";
+const Arr = Array;
+const MyArr = Arr;
+MyArr.from([1, 2, 3]);
