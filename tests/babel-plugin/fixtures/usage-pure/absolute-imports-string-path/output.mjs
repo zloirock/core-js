ba1b48cc158ee074152d0bf802d4670e07aa8b34
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-01 22:57:44 +0300

    add some fixtures

--- tests/babel-plugin/fixtures/usage-pure/absolute-imports-string-path/output.mjs
+++
@@ -1,2 +0,0 @@
-import _at from "<CWD>/packages/core-js-pure/actual/instance/at.js";
-_at(foo).call(foo, -1);
---
+++ tests/babel-plugin/fixtures/usage-pure/symbol-dispose-in/output.mjs
@@ -0,0 +1,2 @@
+import _Symbol$dispose from "@core-js/pure/actual/symbol/dispose";
+_Symbol$dispose in obj;
