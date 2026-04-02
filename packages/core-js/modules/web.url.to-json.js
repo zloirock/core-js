ffa50d7a0911499bcd606b7ed04852c6904058d5
John <john-xyz@outlook.com>
2026-04-02 17:35:08 -0500

    Refactor URL to use getBuiltInPrototypeMethod
    Refactor URL.prototype.toString to use getBuiltInPrototypeMethod.

--- packages/core-js/modules/web.url.to-json.js
+++ packages/core-js/modules/web.url.to-json.js
@@ -1,7 +1,9 @@
 'use strict';
 var $ = require('../internals/export');
 var call = require('../internals/function-call');
-var toString = URL.prototype.toString;
+var getBuiltInPrototypeMethod = require('../internals/get-built-in-prototype-method');
+
+var toString = getBuiltInPrototypeMethod('URL', 'toString');
 
 // `URL.prototype.toJSON` method
 // https://url.spec.whatwg.org/#dom-url-tojson
