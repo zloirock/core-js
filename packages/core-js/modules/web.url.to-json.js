d1a249172185b75dfaeef4c422ad29fd696c806a
John <john-xyz@outlook.com>
2026-04-02 17:23:02 -0500

    Cache URL#toString

--- packages/core-js/modules/web.url.to-json.js
+++ packages/core-js/modules/web.url.to-json.js
@@ -1,11 +1,12 @@
 'use strict';
 var $ = require('../internals/export');
 var call = require('../internals/function-call');
+var toString = URL.prototype.toString;
 
 // `URL.prototype.toJSON` method
 // https://url.spec.whatwg.org/#dom-url-tojson
 $({ target: 'URL', proto: true, enumerable: true }, {
   toJSON: function toJSON() {
-    return call(URL.prototype.toString, this);
+    return call(toString, this);
   }
 });
