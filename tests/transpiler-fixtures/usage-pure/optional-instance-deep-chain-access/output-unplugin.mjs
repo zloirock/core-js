1d13da93cdf7fc77c951337e6a76bd153d02f4d9
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-04 23:47:07 +0300

    improve unplugin testing

---
+++ tests/transpiler-fixtures/usage-pure/optional-instance-deep-chain-access/output-unplugin.mjs
@@ -0,0 +1,4 @@
+var _ref, _ref1;
+import _nameMaybeFunction from "@core-js/pure/actual/function/instance/name";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+_nameMaybeFunction(_ref = _flatMaybeArray(_ref1 = foo?.bar) == null ? void 0 : _flatMaybeArray(_ref1)) == null ? void 0 : _nameMaybeFunction(_ref);
