0ee867a47bffd1438abe1b9194019e3086f1df12
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-04 23:52:34 +0300

    fix unneeded context memoization in unplugin

--- tests/transpiler-fixtures/usage-pure/var-shadow-skip/output-unplugin.mjs
+++ tests/transpiler-fixtures/usage-pure/var-shadow-skip/output-unplugin.mjs
@@ -1,8 +1,7 @@
-var _ref;
 import _at from "@core-js/pure/actual/instance/at";
 var Array = require('./array');
 var Promise = require('./promise');
 
 Array.from(x);
 new Promise(fn);
-_at(_ref = Array.prototype).call(y, -1);
+_at(Array.prototype).call(y, -1);
