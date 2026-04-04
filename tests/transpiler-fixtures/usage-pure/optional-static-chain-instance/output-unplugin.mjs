eb7fd03576573d8ec35860ffa837f1282bd19ec0
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 00:21:31 +0300

    normalize memoization style

--- tests/transpiler-fixtures/usage-pure/optional-static-chain-instance/output-unplugin.mjs
+++
@@ -1,4 +0,0 @@
-var _ref;
-import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
-import _Array$from from "@core-js/pure/actual/array/from";
-_flatMaybeArray(_ref = _Array$from(x)) == null ? void 0 : _flatMaybeArray(_ref).call(_ref);
