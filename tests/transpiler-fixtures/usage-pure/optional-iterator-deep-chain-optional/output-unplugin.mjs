c89045af55ebc1072c0e60459195cd15ff09bf1a
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 00:04:55 +0300

    fix incorrect getIterator target memoization in unplugin

--- tests/transpiler-fixtures/usage-pure/optional-iterator-deep-chain-optional/output-unplugin.mjs
+++ tests/transpiler-fixtures/usage-pure/optional-iterator-deep-chain-optional/output-unplugin.mjs
@@ -1,3 +1,3 @@
 var _ref;
 import _getIterator from "@core-js/pure/actual/get-iterator";
-_getIterator(_ref = foo?.bar) == null ? void 0 : _getIterator(_ref)?.next();
+_ref = foo?.bar, _ref == null ? void 0 : _getIterator(_ref)?.next();
