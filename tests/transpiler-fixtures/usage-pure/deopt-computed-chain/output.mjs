97f6ee8f0129b9e8e4a8698262387399b7b3e4c8
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 19:35:32 +0300

    fix source-based deoptionalization

---
+++ tests/transpiler-fixtures/usage-pure/deopt-computed-chain/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _at from "@core-js/pure/actual/instance/at";
+foo == null ? void 0 : _at(_ref = foo[key]).call(_ref, 0);
---
+++ tests/transpiler-fixtures/usage-pure/deopt-string-literal-safe/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _at from "@core-js/pure/actual/instance/at";
+foo == null ? void 0 : _at(_ref = foo["?."]).call(_ref, 0);
