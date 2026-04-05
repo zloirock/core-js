84a0e93fdb7749930cc0458a65ae4cfa9870f7ba
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 13:26:10 +0300

    fix injection to `for` in unplugin

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-for-loop-two-injections/output.mjs
@@ -0,0 +1,3 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Object$entries from "@core-js/pure/actual/object/entries";
+for (const from = _Array$from, entries = _Object$entries, i = 0; i < 10; i++) {}
