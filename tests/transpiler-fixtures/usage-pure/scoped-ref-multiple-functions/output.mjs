0e22d138124c30373a976350f18e2473a50e9911
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 16:34:52 +0300

    move refs to related scopes in simple cases in unplugin

---
+++ tests/transpiler-fixtures/usage-pure/scoped-ref-multiple-functions/output.mjs
@@ -0,0 +1,10 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+function f() {
+  var _ref;
+  _at(_ref = getA()).call(_ref, -1);
+}
+function g() {
+  var _ref2;
+  _flatMaybeArray(_ref2 = getB()).call(_ref2);
+}
