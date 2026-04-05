6696020564a52ad0ea03477c252d94053de812a1
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 15:52:21 +0300

    stripe spaces in unpugin tests runner + refactoring

--- tests/transpiler-fixtures/usage-pure/destructuring-param-skip/output-unplugin.mjs
+++
@@ -1,3 +0,0 @@
-function foo({ from }) {
-  return from;
-}
--- tests/transpiler-fixtures/usage-pure/optional-chain-ts-string-prototype/output-unplugin.mjs
+++
@@ -1,2 +0,0 @@
-import _trimMaybeString from "@core-js/pure/actual/string/instance/trim";
-function f(s: string | null) { s == null ? void 0 : _trimMaybeString(s).call(s); }
