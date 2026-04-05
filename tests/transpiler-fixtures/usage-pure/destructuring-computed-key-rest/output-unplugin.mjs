ae9154d380b79b921354a77e70f42f804205efe2
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 12:45:54 +0300

    sync some more cases of naming

--- tests/transpiler-fixtures/usage-pure/destructuring-computed-key-rest/output-unplugin.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-computed-key-rest/output-unplugin.mjs
@@ -1,4 +1,4 @@
 import _Array$from from "@core-js/pure/actual/array/from";
 const key = 'from';
 const value = _Array$from;
-const { [key]: _unused1, ...rest } = Array;
+const { [key]: _unused, ...rest } = Array;
