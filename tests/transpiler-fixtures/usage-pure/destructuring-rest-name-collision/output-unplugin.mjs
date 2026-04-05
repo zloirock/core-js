51907489aaa7025675e87aa4baa0c1fd049a49d7
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 12:45:54 +0300

    sync some more cases of naming

--- tests/transpiler-fixtures/usage-pure/destructuring-rest-name-collision/output-unplugin.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-rest-name-collision/output-unplugin.mjs
@@ -1,4 +1,4 @@
 import _Array$from from "@core-js/pure/actual/array/from";
 const _unused = 'used';
 const from = _Array$from;
-const { from: _unused1, ...rest } = Array;
+const { from: _unused2, ...rest } = Array;
