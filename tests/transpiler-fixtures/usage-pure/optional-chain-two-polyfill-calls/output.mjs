358142f6831df1c5aab8ad40df395946b9d30a74
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 12:16:11 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/optional-chain-two-polyfill-calls/output.mjs
@@ -0,0 +1,4 @@
+var _ref;
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+foo == null ? void 0 : _at(_ref = _flatMaybeArray(foo).call(foo)).call(_ref, -1);
