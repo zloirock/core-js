50475e142b0af1f3fe834ee11695ba82b55477ff
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 16:34:52 +0300

    move refs to related scopes in simple cases in unplugin

--- tests/transpiler-fixtures/usage-pure/class-getter-polyfill/output-unplugin.mjs
+++
@@ -1,7 +0,0 @@
-var _ref;
-import _toSortedMaybeArray from "@core-js/pure/actual/array/instance/to-sorted";
-class Foo {
-  get sorted() {
-    return _toSortedMaybeArray(_ref = [3, 1, 2]).call(_ref);
-  }
-}
