fe8954a00b8689be98e14229aa7b7b1b26c88a51
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 11:39:10 +0300

    sync parens

--- tests/transpiler-fixtures/usage-pure/instance-at-conditional-mixed/output-unplugin.mjs
+++
@@ -1,3 +0,0 @@
-var _ref;
-import _at from "@core-js/pure/actual/instance/at";
-_at(_ref = (c ? 'a' : [])).call(_ref, -1);
--- tests/transpiler-fixtures/usage-pure/instance-at-nullish-assign/output-unplugin.mjs
+++
@@ -1,3 +0,0 @@
-var _ref;
-import _at from "@core-js/pure/actual/instance/at";
-_at(_ref = (x ??= [])).call(_ref, -1);
