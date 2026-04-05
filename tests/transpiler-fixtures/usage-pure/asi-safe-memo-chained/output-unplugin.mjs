172bfd5b3fe986b565bc18289631f6e99f56e5df
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 11:18:35 +0300

    sync ref naming in babel and unplugin

--- tests/transpiler-fixtures/usage-pure/asi-safe-memo-chained/output-unplugin.mjs
+++ tests/transpiler-fixtures/usage-pure/asi-safe-memo-chained/output-unplugin.mjs
@@ -1,5 +1,5 @@
-var _ref, _ref1;
+var _ref, _ref2;
 import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
 import _at from "@core-js/pure/actual/instance/at";
 _flatMaybeArray(_ref = getA()).call(_ref)
-_at(_ref1 = getB()).call(_ref1, -1)
+_at(_ref2 = getB()).call(_ref2, -1)
--- tests/transpiler-fixtures/usage-pure/optional-instance-through-call/output-unplugin.mjs
+++
@@ -1,3 +0,0 @@
-var _ref, _ref1;
-import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
-(_ref = foo?.bar()) == null ? void 0 : _flatMaybeArray(_ref1 = _ref.baz).call(_ref1);
