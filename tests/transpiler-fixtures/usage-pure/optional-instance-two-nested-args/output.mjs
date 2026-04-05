f3e65054264af5cc457c6331305a30f27ef71969
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 19:20:37 +0300

    add some fixes

---
+++ tests/transpiler-fixtures/usage-pure/optional-instance-two-nested-args/output.mjs
@@ -0,0 +1,3 @@
+import _includes from "@core-js/pure/actual/instance/includes";
+import _at from "@core-js/pure/actual/instance/at";
+arr == null ? void 0 : _includes(arr).call(arr, _at(a).call(a, 0), _at(b).call(b, 0));
