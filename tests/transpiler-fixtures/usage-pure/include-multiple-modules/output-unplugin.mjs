c6327bbd0ffca5f620448b2e8dec8583c6810b52
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-05 11:18:35 +0300

    sync ref naming in babel and unplugin

--- tests/transpiler-fixtures/usage-pure/include-multiple-modules/output-unplugin.mjs
+++
@@ -1,5 +0,0 @@
-var _ref, _ref1;
-import _atMaybeString from "@core-js/pure/actual/string/instance/at";
-import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
-_atMaybeString(_ref = 'str').call(_ref, -1);
-_atMaybeArray(_ref1 = []).call(_ref1, -1);
