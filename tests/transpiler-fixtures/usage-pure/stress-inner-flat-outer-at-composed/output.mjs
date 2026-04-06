4ec537dc6f3176ca8475d22b97ade398dd153575
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 17:33:45 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/stress-inner-flat-outer-at-composed/output.mjs
@@ -0,0 +1,4 @@
+var _ref;
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+obj == null ? void 0 : _at(_ref = _flatMaybeArray(obj).call(obj)).call(_ref, 0);
