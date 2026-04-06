406c6022bfc01229faea6ca596c9b65b180cbcce
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 15:57:39 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/deopt-regex-computed-key/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _at from "@core-js/pure/actual/instance/at";
+obj == null ? void 0 : _at(_ref = obj[/x/g]).call(_ref, 0);
---
+++ tests/transpiler-fixtures/usage-pure/optional-nested-optional-argument/output.mjs
@@ -0,0 +1,3 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+foo == null ? void 0 : _at(foo).call(foo, bar == null ? void 0 : _flatMaybeArray(bar).call(bar));
