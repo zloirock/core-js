34ed705e4de293692980aceb463ad1ce787c9790
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 17:33:45 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/stress-optional-plus-two-polyfills/output-unplugin.mjs
@@ -0,0 +1,4 @@
+var _ref, _ref2;
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+a == null ? void 0 : _at(_ref = _flatMaybeArray(_ref2 = a.b).call(_ref2)).call(_ref, 0);
