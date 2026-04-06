36735ad920600c4dacc3098acce6d7c2da29fee2
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 23:22:14 +0300

    avoid extra import on destructuring in usage-pure

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-constructor-global-and-usage/output.mjs
@@ -0,0 +1,4 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise$all from "@core-js/pure/actual/promise/all";
+const resolve = _Promise$resolve;
+_Promise$all([]);
