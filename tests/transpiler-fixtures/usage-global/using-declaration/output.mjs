bf839143e82b9f48d5a5e8862989448fa99a7d30
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 01:36:30 +0300

    some fixes

--- tests/transpiler-fixtures/usage-global/using-declaration/output.mjs
+++ tests/transpiler-fixtures/usage-global/using-declaration/output.mjs
@@ -1,13 +1,6 @@
-import "core-js/modules/es.symbol.async-dispose";
 import "core-js/modules/es.symbol.dispose";
-import "core-js/modules/es.object.to-string";
 import "core-js/modules/es.error.is-error";
 import "core-js/modules/es.suppressed-error.constructor";
-import "core-js/modules/es.promise.constructor";
-import "core-js/modules/es.promise.catch";
-import "core-js/modules/es.promise.finally";
-import "core-js/modules/es.promise.resolve";
-import "core-js/modules/es.async-iterator.async-dispose";
 import "core-js/modules/es.iterator.dispose";
 {
   using resource = getResource();
