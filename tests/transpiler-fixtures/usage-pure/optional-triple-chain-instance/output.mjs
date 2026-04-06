1800255cad73d6f58ff61b418a4c903c5fa6b357
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 12:18:44 +0300

    some fixes

---
+++ tests/transpiler-fixtures/usage-pure/optional-triple-chain-instance/output.mjs
@@ -0,0 +1,5 @@
+var _ref, _ref2, _ref3;
+import _padStartMaybeString from "@core-js/pure/actual/string/instance/pad-start";
+import _trimMaybeString from "@core-js/pure/actual/string/instance/trim";
+import _at from "@core-js/pure/actual/instance/at";
+(_ref = (_ref3 = arr == null ? void 0 : _at(arr).call(arr, -1)) == null ? void 0 : _trimMaybeString(_ref3).call(_ref3)) == null ? void 0 : _padStartMaybeString(_ref).call(_ref, 2);
