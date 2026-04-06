a8b678205cb5b3b39328e53d8f15e180afcf92df
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 15:57:39 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-static-rename-default/output.mjs
@@ -0,0 +1,4 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Array$of from "@core-js/pure/actual/array/of";
+const from = _Array$from === void 0 ? [] : _Array$from;
+const myOf = _Array$of === void 0 ? 0 : _Array$of;
