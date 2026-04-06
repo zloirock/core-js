a2644b9f6fbb3f66f3a1aa81248583066f719513
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 15:57:39 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/optional-dual-chains-binary/output.mjs
@@ -0,0 +1,3 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+(foo == null ? void 0 : _at(foo).call(foo, 0)) + (foo == null ? void 0 : _flatMaybeArray(foo).call(foo));
