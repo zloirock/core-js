0226bf38965e442542c7e95621f4924c04d915a4
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 23:22:14 +0300

    avoid extra import on destructuring in usage-pure

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-assignment-rest-constructor-global/output.mjs
@@ -0,0 +1,8 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+let resolve, rest;
+resolve = _Promise$resolve;
+({
+  resolve: _unused,
+  ...rest
+} = _Promise);
