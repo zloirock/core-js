33826f1b828836279c41f6c86081f8272759bd07
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 13:27:54 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/optional-guard-template-interpolation/output.mjs
@@ -0,0 +1,2 @@
+import _at from "@core-js/pure/actual/instance/at";
+`${arr == null ? void 0 : _at(arr).call(arr, 0)}`;
---
+++ tests/transpiler-fixtures/usage-pure/optional-guard-yield/output.mjs
@@ -0,0 +1,4 @@
+import _at from "@core-js/pure/actual/instance/at";
+function* gen() {
+  yield arr == null ? void 0 : _at(arr).call(arr, 0);
+}
