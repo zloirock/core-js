b28b4856596dfcbf7bab85c610b63980cd34e7e6
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 13:54:05 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/compose-spread-iterator-instance/output.mjs
@@ -0,0 +1,4 @@
+var _ref;
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+import _values from "@core-js/pure/actual/instance/values";
+_atMaybeArray(_ref = [..._values(arr).call(arr)]).call(_ref, -1);
---
+++ tests/transpiler-fixtures/usage-pure/compose-static-value-in-static/output.mjs
@@ -0,0 +1,5 @@
+import _Object$assign from "@core-js/pure/actual/object/assign";
+import _Array$from from "@core-js/pure/actual/array/from";
+_Object$assign({}, {
+  from: _Array$from
+});
