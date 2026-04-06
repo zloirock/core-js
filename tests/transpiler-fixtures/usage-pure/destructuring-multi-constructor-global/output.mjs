1028f640ecf8dab06f8b367cf94e0d5de03e60d5
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 23:22:14 +0300

    avoid extra import on destructuring in usage-pure

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-multi-constructor-global/output.mjs
@@ -0,0 +1,4 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Array$from from "@core-js/pure/actual/array/from";
+const resolve = _Promise$resolve,
+  from = _Array$from;
