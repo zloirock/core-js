b43d02ed26e3edf80b1612d94cb51e0a6a6a0558
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 17:33:45 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/stress-optional-plus-two-polyfills/output.mjs
@@ -0,0 +1,4 @@
+var _ref, _ref2, _ref3;
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+a == null ? void 0 : _at(_ref = _flatMaybeArray(_ref3 = a.b).call(_ref3)).call(_ref, 0);
---
+++ tests/transpiler-fixtures/usage-pure/stress-three-optional-polyfill-args/output.mjs
@@ -0,0 +1,4 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+import _includes from "@core-js/pure/actual/instance/includes";
+f(a == null ? void 0 : _at(a).call(a, 0), b == null ? void 0 : _flatMaybeArray(b).call(b), c == null ? void 0 : _includes(c).call(c, 'x'));
