356cc943f2ec7eb35523d123c67fd8f29e7a1d9b
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 17:33:45 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/stress-destructuring-optional-nullish/output.mjs
@@ -0,0 +1,3 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _sliceMaybeArray from "@core-js/pure/actual/array/instance/slice";
+const at = _at((arr == null ? void 0 : _sliceMaybeArray(arr).call(arr, 0)) ?? []);
---
+++ tests/transpiler-fixtures/usage-pure/stress-three-optional-polyfill-args/input.mjs
@@ -0,0 +1,1 @@
+f(a?.at(0), b?.flat(), c?.includes('x'));
