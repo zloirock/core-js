d21344c2021e9fee0e685be375ba79ec274b156d
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 13:54:05 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/chained-instance-no-optional/output.mjs
@@ -0,0 +1,4 @@
+var _ref;
+import _includes from "@core-js/pure/actual/instance/includes";
+import _at from "@core-js/pure/actual/instance/at";
+_includes(_ref = _at(arr).call(arr, 0)).call(_ref, 'x');
---
+++ tests/transpiler-fixtures/usage-pure/compose-triple-nesting/output.mjs
@@ -0,0 +1,4 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Array$from from "@core-js/pure/actual/array/from";
+import _at from "@core-js/pure/actual/instance/at";
+_Promise$resolve(_Array$from(_at(arr).call(arr, 0)));
