88ad413da9bcf66777f00cf3b78f756a7753edad
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-06 22:44:47 +0300

    fix double underscore

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-rest-constructor-global/output.mjs
@@ -0,0 +1,7 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+const resolve = _Promise$resolve;
+const {
+  resolve: _unused,
+  ...rest
+} = _Promise;
