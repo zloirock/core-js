27fef937941ffc0332a2f199a72c050cfe137420
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 12:11:13 +0300

    some parens-related fixes in unplugin

---
+++ tests/transpiler-fixtures/usage-pure/parenthesized-instance-prototype/output-unplugin.mjs
@@ -0,0 +1,4 @@
+import _includesMaybeArray from "@core-js/pure/actual/array/instance/includes";
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+_includesMaybeArray(Array.prototype).call(arr, x);
+_atMaybeArray((Array.prototype)).call(arr, 0);
