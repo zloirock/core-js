2b83a6e9c48f8803e46aed89a25f02dd88dc89c9
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 12:24:54 +0300

    fix names collision in babel plugin

---
+++ tests/transpiler-fixtures/usage-pure/static-method-numeric-suffix/output.mjs
@@ -0,0 +1,6 @@
+import _Math$log2 from "@core-js/pure/actual/math/log2";
+import _Math$log10 from "@core-js/pure/actual/math/log10";
+import _Math$f16round from "@core-js/pure/actual/math/f16round";
+_Math$log2(2);
+_Math$log10(10);
+_Math$f16round(x);
