795dd13c21912514c05255f9772b341491f21823
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 12:18:10 +0300

    add some fixtures

--- tests/transpiler-fixtures/usage-pure/parenthesized-static-callee/output.mjs
+++ tests/transpiler-fixtures/usage-pure/parenthesized-static-callee/output.mjs
@@ -1,9 +1,13 @@
 import _Array$from from "@core-js/pure/actual/array/from";
 import _Object$assign from "@core-js/pure/actual/object/assign";
 import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise$reject from "@core-js/pure/actual/promise/reject";
+import _Promise$any from "@core-js/pure/actual/promise/any";
 import _Promise$all from "@core-js/pure/actual/promise/all";
 _Array$from(iter);
 _Object$assign({}, a);
 _Promise$resolve(1);
+_Promise$reject(err);
+_Promise$any([p1, p2]);
 _Promise$all([1]);
 _Promise$resolve(2);
