c8e7fda389aea82bd040639e4b78521a48c36d7e
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 12:11:13 +0300

    some parens-related fixes in unplugin

---
+++ tests/transpiler-fixtures/usage-pure/parenthesized-instance-prototype/input.mjs
@@ -0,0 +1,2 @@
+(Array.prototype).includes.call(arr, x);
+((Array.prototype)).at.call(arr, 0);
---
+++ tests/transpiler-fixtures/usage-pure/parenthesized-static-callee/output.mjs
@@ -0,0 +1,9 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Object$assign from "@core-js/pure/actual/object/assign";
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise$all from "@core-js/pure/actual/promise/all";
+_Array$from(iter);
+_Object$assign({}, a);
+_Promise$resolve(1);
+_Promise$all([1]);
+_Promise$resolve(2);
