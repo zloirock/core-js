81cc198f9b156e2c7d835906282670c395372b50
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-07 14:01:48 +0300

    normalize refs naming convention

---
+++ tests/transpiler-fixtures/usage-pure/ref-numbering-past-ten/output.mjs
@@ -0,0 +1,16 @@
+var _ref, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9, _ref10, _ref11;
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+// regression for own UID generator: babel's scope.generateUidIdentifier
+// strips trailing digits, so after `_ref9` it would emit `_ref0`, `_ref1` instead
+// of `_ref10`, `_ref11`. eleven optional-chain method calls forces eleven temp refs.
+(_ref = a().b) == null ? void 0 : _flatMaybeArray(_ref).call(_ref);
+(_ref2 = c().d) == null ? void 0 : _flatMaybeArray(_ref2).call(_ref2);
+(_ref3 = e().f) == null ? void 0 : _flatMaybeArray(_ref3).call(_ref3);
+(_ref4 = g().h) == null ? void 0 : _flatMaybeArray(_ref4).call(_ref4);
+(_ref5 = i().j) == null ? void 0 : _flatMaybeArray(_ref5).call(_ref5);
+(_ref6 = k().l) == null ? void 0 : _flatMaybeArray(_ref6).call(_ref6);
+(_ref7 = m().n) == null ? void 0 : _flatMaybeArray(_ref7).call(_ref7);
+(_ref8 = o().p) == null ? void 0 : _flatMaybeArray(_ref8).call(_ref8);
+(_ref9 = q().r) == null ? void 0 : _flatMaybeArray(_ref9).call(_ref9);
+(_ref10 = s().t) == null ? void 0 : _flatMaybeArray(_ref10).call(_ref10);
+(_ref11 = u().v) == null ? void 0 : _flatMaybeArray(_ref11).call(_ref11);
