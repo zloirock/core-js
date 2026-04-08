b2948e093ef3b067ac4a05d03f801e463621b77f
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-08 03:20:25 +0300

    some fixes and improvements

--- packages/core-js-babel-plugin/internals/detect-entry.js
+++ packages/core-js-babel-plugin/internals/detect-entry.js
@@ -4,12 +4,12 @@ import { babelAdapter } from './detect-usage.js';
 export default function createEntryVisitors(onEntry) {
   return {
     ImportDeclaration(path) {
-      const source = getEntrySource(path.node, babelAdapter);
+      const source = getEntrySource(path.node, babelAdapter, path.scope);
       if (source !== null) onEntry(source, path);
     },
     Program(path) {
       for (const bodyPath of path.get('body')) {
-        const source = getEntrySource(bodyPath.node, babelAdapter);
+        const source = getEntrySource(bodyPath.node, babelAdapter, bodyPath.scope);
         if (source !== null) onEntry(source, bodyPath);
       }
     },
