acc5a258df4c0705b5353eb15b3da8dccb5cc823
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-08 15:24:21 +0300

    some fixes

---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-typeof-fn-call-return/output.mjs
@@ -0,0 +1,5 @@
+var _ref;
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+const fn = (): string => '';
+declare const x: typeof fn;
+_atMaybeString(_ref = x()).call(_ref, 0);
---
+++ tests/transpiler-fixtures/usage-pure/destructure-assign-if-bodyless/input.mjs
@@ -0,0 +1,1 @@
+if (cond) ({ Promise, Map } = globalThis);
