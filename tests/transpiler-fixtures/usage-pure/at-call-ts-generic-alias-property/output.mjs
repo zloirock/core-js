80b04b12867720aa4d24aa37823608bc1117aae9
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-08 11:29:36 +0300

    some fixes

---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-generic-alias-property/output.mjs
@@ -0,0 +1,7 @@
+var _ref;
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+type Box<T> = {
+  value: T;
+};
+declare const b: Box<string>;
+_atMaybeString(_ref = b.value).call(_ref, 0);
