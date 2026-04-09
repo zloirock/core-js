ea7dc40dcc2edd430e41a198cb9dcc32e95247fb
Namhyeon, Go <gnh1201@catswords.re.kr>
2026-04-09 13:36:18 +0900

    Format function declaration for NullProtoObject

--- packages/core-js/internals/object-create.js
+++ packages/core-js/internals/object-create.js
@@ -61,7 +61,7 @@ var NullProtoObject = function () {
     ? document.domain && activeXDocument
       ? NullProtoObjectViaActiveX(activeXDocument) // old IE
       : NullProtoObjectViaIFrame()
-    : function() { return {}; }; // WSH
+    : function () { return {}; }; // WSH
   var length = enumBugKeys.length;
   while (length--) delete NullProtoObject[PROTOTYPE][enumBugKeys[length]];
   return NullProtoObject();
