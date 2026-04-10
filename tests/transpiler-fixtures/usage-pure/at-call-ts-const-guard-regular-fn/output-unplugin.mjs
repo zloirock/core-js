d5930f3fd835421ca1a1067659d7387adbf1e79e
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-10 19:49:49 +0300

    improve resolving types of reassignable variables

---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-const-guard-regular-fn/output-unplugin.mjs
@@ -0,0 +1,5 @@
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+declare const x: string | number[];
+if (typeof x === "string") {
+  const fn = function() { return _atMaybeString(x).call(x, -1); };
+}
