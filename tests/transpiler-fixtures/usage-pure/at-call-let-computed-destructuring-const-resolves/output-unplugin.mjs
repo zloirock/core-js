008b66d27802899160526bd09709bfec0895a4de
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 00:12:43 +0300

    improve types resolving

---
+++ tests/transpiler-fixtures/usage-pure/at-call-let-computed-destructuring-const-resolves/output-unplugin.mjs
@@ -0,0 +1,5 @@
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+const key = 'name';
+let name;
+({ [key]: name } = { name: 'alice' });
+_atMaybeString(name).call(name, -1);
---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-interface-extends-conditional-negated/output-unplugin.mjs
@@ -0,0 +1,6 @@
+var _ref;
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+interface Base<T> { val: T extends number ? never : T }
+interface Child extends Base<string> {}
+declare const c: Child;
+_atMaybeString(_ref = c.val).call(_ref, -1);
