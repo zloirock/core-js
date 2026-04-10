6d56891050fd6ff3aa6f23a7aa44648250aa8fc7
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-10 14:11:35 +0300

    update dependencies

--- tests/transpiler-fixtures/usage-global/decorator-arg-polyfill/output.mjs
+++ tests/transpiler-fixtures/usage-global/decorator-arg-polyfill/output.mjs
@@ -1,7 +1,5 @@
 import "core-js/modules/es.array.at";
 import "core-js/modules/es.string.at";
-import "core-js/modules/esnext.function.metadata";
-import "core-js/modules/esnext.symbol.metadata";
 @dec(arr.at(0))
 class A {
   @dec2(arr.at(1))
