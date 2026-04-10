6facd288a88ac3b4d50c5826fd433f44216a0b72
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-10 19:02:33 +0300

    drop some duplicated fixtures

--- tests/transpiler-fixtures/usage-pure/optional-ts-as-unwrap/output.mjs
+++
@@ -1,3 +0,0 @@
-var _ref;
-import _includesMaybeString from "@core-js/pure/actual/string/instance/includes";
-null == (_ref = x as string) ? void 0 : _includesMaybeString(_ref).call(_ref, "o");
--- tests/transpiler-fixtures/usage-pure/stress-optional-symbol-iterator/output.mjs
+++
@@ -1,2 +0,0 @@
-import _getIterator from "@core-js/pure/actual/get-iterator";
-foo == null ? void 0 : _getIterator(foo);
