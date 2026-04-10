722779b45a973581bf7f0c6e6c102d58dfd3058f
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-10 20:59:17 +0300

    improve resolving IIFE

---
+++ tests/transpiler-fixtures/usage-pure/at-call-let-reassigned-in-iife-after-usage-bail/output.mjs
@@ -0,0 +1,6 @@
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+let x = [];
+_atMaybeArray(x).call(x, -1);
+(() => {
+  x = "hello";
+})();
---
+++ tests/transpiler-fixtures/usage-pure/at-call-let-reassigned-in-nested-iife/output.mjs
@@ -0,0 +1,8 @@
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+let x = [];
+(() => {
+  (() => {
+    x = "hello";
+  })();
+})();
+_atMaybeString(x).call(x, -1);
