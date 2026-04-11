5d0039744d93042a5f26f90d07f78d26f5246f52
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 15:55:32 +0300

    fix some more side effect cases

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-nested-fn-side-effect/output.mjs
@@ -0,0 +1,6 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+function f() {
+  _Promise(), Array;
+  const from = _Array$from;
+}
