d22f0a4e442f84555122d3ec5315436f2619501d
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 20:02:33 +0300

    fix some more injection cases

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-export-multi-decl-non-polyfillable/output-unplugin.mjs
@@ -0,0 +1,7 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Array$of from "@core-js/pure/actual/array/of";
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+export const from = _Array$from;
+export const of = _Array$of;
+export const resolve = _Promise$resolve;
+export const { foo } = obj;
