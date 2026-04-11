73610409c1a9871d71a4de15d133a6b01241b64e
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 17:45:12 +0300

    fix some more side effect and order cases

--- tests/transpiler-fixtures/usage-pure/destructuring-export-two-patterns-multiple/output-unplugin.mjs
+++
@@ -1,8 +0,0 @@
-import _Array$from from "@core-js/pure/actual/array/from";
-import _Array$of from "@core-js/pure/actual/array/of";
-import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
-import _Promise$all from "@core-js/pure/actual/promise/all";
-export const from = _Array$from;
-export const of = _Array$of;
-export const resolve = _Promise$resolve;
-export const all = _Promise$all;
--- tests/transpiler-fixtures/usage-pure/destructuring-logical-call-left/output-unplugin.mjs
+++
@@ -1,4 +0,0 @@
-import _Array$from from "@core-js/pure/actual/array/from";
-import _Promise from "@core-js/pure/actual/promise/constructor";
-_Promise() && Array;
-const from = _Array$from;
