91dd880caf2ffb4d274306c462117379521a2c78
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 21:43:12 +0300

    some fixes

---
+++ tests/transpiler-fixtures/usage-pure/class-static-computed-symbol-species/output.mjs
@@ -0,0 +1,7 @@
+import _Map from "@core-js/pure/actual/map/constructor";
+import _Symbol$species from "@core-js/pure/actual/symbol/species";
+class C extends _Map {
+  static [_Symbol$species]() {
+    return _Map;
+  }
+}
