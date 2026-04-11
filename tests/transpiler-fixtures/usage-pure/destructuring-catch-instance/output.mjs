eabc10644f7b315259ec9fc388965b0fe9954bc2
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 00:17:07 +0300

    some improvements of polyfills injection

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-catch-instance/output.mjs
@@ -0,0 +1,5 @@
+import _includes from "@core-js/pure/actual/instance/includes";
+try {} catch (_ref) {
+  var includes = _includes(_ref);
+  includes("x");
+}
---
+++ tests/transpiler-fixtures/usage-pure/destructuring-default-multi-decl-se/output.mjs
@@ -0,0 +1,7 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+sideEffect(), Array;
+sideEffect2(), _Promise;
+const from = _Array$from === void 0 ? [] : _Array$from;
+const resolve = _Promise$resolve === void 0 ? fn : _Promise$resolve;
