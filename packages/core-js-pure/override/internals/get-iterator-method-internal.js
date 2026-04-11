508e6d7e5edef629f157c74c4f7af608e1ff2086
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 13:23:49 +0300

    some get-iterator fixes

--- packages/core-js-pure/override/internals/get-iterator-method-internal.js
+++ packages/core-js-pure/override/internals/get-iterator-method-internal.js
@@ -1,14 +1,13 @@
 'use strict';
-var classof = require('../internals/classof-raw');
-var isNullOrUndefined = require('../internals/is-null-or-undefined');
 var getMethod = require('../internals/get-method');
+var isNullOrUndefined = require('../internals/is-null-or-undefined');
+var getInternalIterator = require('../internals/get-internal-iterator');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
 var ITERATOR = wellKnownSymbol('iterator');
-var ArrayPrototype = Array.prototype;
 
 module.exports = function (it) {
   if (!isNullOrUndefined(it)) return getMethod(it, ITERATOR)
     || getMethod(it, '@@iterator')
-    || (classof(it) === 'Arguments' ? ArrayPrototype[ITERATOR] : undefined);
+    || getInternalIterator(it);
 };
--- packages/core-js/internals/get-iterator-method.js
+++ packages/core-js/internals/get-iterator-method.js
@@ -1,13 +1,11 @@
 'use strict';
 var classof = require('../internals/classof-raw');
-var isNullOrUndefined = require('../internals/is-null-or-undefined');
-var Iterators = require('../internals/iterators');
 var wellKnownSymbol = require('../internals/well-known-symbol');
 
 var ITERATOR = wellKnownSymbol('iterator');
+var ArrayPrototype = Array.prototype;
 
 module.exports = function (it) {
-  if (!isNullOrUndefined(it)) return it[ITERATOR]
-    || it['@@iterator']
-    || Iterators[classof(it)];
+  return it[ITERATOR] || it['@@iterator']
+    || (classof(it) === 'Arguments' ? ArrayPrototype[ITERATOR] : undefined);
 };
