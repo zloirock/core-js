80178bc780e3456e5bb0608893eae714429ec07f
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-11 17:45:12 +0300

    fix some more side effect and order cases

--- tests/transpiler-fixtures/usage-pure/destructuring-instance-call-multi-declarator/output.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-instance-call-multi-declarator/output.mjs
@@ -2,5 +2,5 @@ import _at from "@core-js/pure/actual/instance/at";
 import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
 const _ref = getObj();
 const at = _at(_ref);
-const flat = _flatMaybeArray(_ref),
-  x = 1;
+const flat = _flatMaybeArray(_ref);
+const x = 1;
--- tests/transpiler-fixtures/usage-pure/destructuring-multi-declarator-multiple/output.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-multi-declarator-multiple/output.mjs
@@ -1,5 +1,5 @@
 import _Array$from from "@core-js/pure/actual/array/from";
 import _Array$of from "@core-js/pure/actual/array/of";
 const from = _Array$from;
-const of = _Array$of,
-  x = 1;
+const of = _Array$of;
+const x = 1;
