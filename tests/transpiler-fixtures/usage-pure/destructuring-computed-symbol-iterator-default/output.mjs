e5db485e5ea94acf5d577beaaebce6da61282e7e
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 03:03:36 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-computed-symbol-iterator-default/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _getIteratorMethod from "@core-js/pure/actual/get-iterator-method";
+const iter = (_ref = _getIteratorMethod(obj)) === void 0 ? fallback : _ref;
---
+++ tests/transpiler-fixtures/usage-pure/destructuring-computed-symbol-iterator-multi-decl/output.mjs
@@ -0,0 +1,4 @@
+import _getIteratorMethod from "@core-js/pure/actual/get-iterator-method";
+import _Array$from from "@core-js/pure/actual/array/from";
+const iter = _getIteratorMethod(obj);
+const from = _Array$from;
