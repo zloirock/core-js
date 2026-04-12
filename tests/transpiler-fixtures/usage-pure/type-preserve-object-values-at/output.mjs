2dd45b16ba1f4734d29e29f5313e659badb7869c
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 15:28:32 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/type-preserve-object-values-at/output.mjs
@@ -0,0 +1,4 @@
+import _Object$values from "@core-js/pure/actual/object/values";
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+const vals = _Object$values(obj);
+_atMaybeArray(vals).call(vals, -1);
