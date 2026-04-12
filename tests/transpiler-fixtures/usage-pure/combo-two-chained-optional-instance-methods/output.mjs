d876077e66745644fa463e16dffb70ba5dd349f6
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 13:50:09 +0300

    add some fixtures

--- tests/transpiler-fixtures/usage-pure/combo-two-chained-optional-instance-methods/output.mjs
+++
@@ -1,4 +0,0 @@
-var _ref;
-import _at from "@core-js/pure/actual/instance/at";
-import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
-null == (_ref = arr == null ? void 0 : _flatMaybeArray(arr).call(arr)) ? void 0 : _at(_ref).call(_ref, -1);
