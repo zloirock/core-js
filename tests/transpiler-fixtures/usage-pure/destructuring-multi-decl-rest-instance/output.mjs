d706db90d290be8072594e5864103c7a7a525aa8
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 03:12:30 +0300

    fix some variables order cases

--- tests/transpiler-fixtures/usage-pure/destructuring-multi-decl-rest-instance/output.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-multi-decl-rest-instance/output.mjs
@@ -1,12 +1,12 @@
 import _includes from "@core-js/pure/actual/instance/includes";
 import _at from "@core-js/pure/actual/instance/at";
 const includes = _includes(obj);
+const {
+  includes: _unused,
+  ...rest
+} = obj;
 const at = _at(obj2);
 const {
-    includes: _unused,
-    ...rest
-  } = obj,
-  {
-    at: _unused2,
-    ...rest2
-  } = obj2;
+  at: _unused2,
+  ...rest2
+} = obj2;
