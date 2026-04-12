c13d8ec9cd521f55f373219a5fbf503bcbdd2dba
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 20:26:21 +0300

    improve fixtures comparison logic

--- tests/transpiler-fixtures/usage-pure/at-call-let-reassigned-in-iife-function-expr/output-unplugin.mjs
+++
@@ -1,4 +0,0 @@
-import _atMaybeString from "@core-js/pure/actual/string/instance/at";
-let x = [];
-(function() { x = "hello"; })();
-_atMaybeString(x).call(x, -1);
--- tests/transpiler-fixtures/usage-pure/iife-destructure-with-rest/output-unplugin.mjs
+++
@@ -1,2 +0,0 @@
-import _Array$from from "@core-js/pure/actual/array/from";
-(function({ from = _Array$from, ...rest }) { return [from, rest]; })(Array);
