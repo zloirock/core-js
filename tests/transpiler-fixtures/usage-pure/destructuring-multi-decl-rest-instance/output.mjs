a4bab9d891f08f50a60d134b4c669ecebf63ed0a
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 03:03:36 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-multi-decl-rest-instance/output.mjs
@@ -0,0 +1,12 @@
+import _includes from "@core-js/pure/actual/instance/includes";
+import _at from "@core-js/pure/actual/instance/at";
+const includes = _includes(obj);
+const at = _at(obj2);
+const {
+    includes: _unused,
+    ...rest
+  } = obj,
+  {
+    at: _unused2,
+    ...rest2
+  } = obj2;
