af7b97af1ba2b8e7dc5938323ec9f9bdac7b8b1c
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 16:00:27 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/type-preserve-inline-three-level-chain/output.mjs
@@ -0,0 +1,5 @@
+var _ref, _ref2, _ref3;
+import _includes from "@core-js/pure/actual/instance/includes";
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+import _Array$from from "@core-js/pure/actual/array/from";
+_includes(_ref = _atMaybeArray(_ref3 = _Array$from([1])).call(_ref3, -1)).call(_ref, "x");
