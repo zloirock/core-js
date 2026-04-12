1afb941538c629c4c36dfe7be9cefb0d87520ee6
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 10:45:55 +0300

    some rest-related fixes

---
+++ tests/transpiler-fixtures/usage-pure/destructuring-multi-decl-symbol-iterator-instance-rest/output-unplugin.mjs
@@ -0,0 +1,5 @@
+import _getIteratorMethod from "@core-js/pure/actual/get-iterator-method";
+import _includes from "@core-js/pure/actual/instance/includes";
+const iter = _getIteratorMethod(a);
+const includes = _includes(b);
+const { includes: _unused, ...rest } = b;
