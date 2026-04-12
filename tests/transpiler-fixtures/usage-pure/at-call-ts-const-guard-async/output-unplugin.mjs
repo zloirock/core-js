20d8a5dfd2dfbfd3adc238223b827ab7b79dc11e
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 20:26:21 +0300

    improve fixtures comparison logic

--- tests/transpiler-fixtures/usage-pure/at-call-ts-const-guard-async/output-unplugin.mjs
+++
@@ -1,5 +0,0 @@
-import _atMaybeString from "@core-js/pure/actual/string/instance/at";
-declare const x: string | number[];
-if (typeof x === "string") {
-  const fn = async function() { return _atMaybeString(x).call(x, -1); };
-}
