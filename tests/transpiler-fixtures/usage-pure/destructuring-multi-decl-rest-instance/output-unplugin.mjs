cd9188a8cc746a0dce5686f0fa351b230486c57f
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 03:12:30 +0300

    fix some variables order cases

--- tests/transpiler-fixtures/usage-pure/destructuring-multi-decl-rest-instance/output-unplugin.mjs
+++
@@ -1,6 +0,0 @@
-import _includes from "@core-js/pure/actual/instance/includes";
-import _at from "@core-js/pure/actual/instance/at";
-const includes = _includes(obj);
-const { includes: _unused, ...rest } = obj;
-const at = _at(obj2);
-const { at: _unused2, ...rest2 } = obj2;
