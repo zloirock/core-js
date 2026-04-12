15657092936b1d3c81d3f8a0b97da96ebc117fba
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 12:17:03 +0300

    fix super.static with injected constructor

--- tests/transpiler-fixtures/usage-pure/class-extends-globalthis-promise/output-unplugin.mjs
+++
@@ -1,7 +0,0 @@
-import _Promise from "@core-js/pure/actual/promise/constructor";
-import _Promise$allSettled from "@core-js/pure/actual/promise/all-settled";
-class MyPromise extends _Promise {
-  static allResolved(promises) {
-    return _Promise$allSettled.call(this, promises);
-  }
-}
--- tests/transpiler-fixtures/usage-pure/class-extends-globalthis-promise/output.mjs
+++ tests/transpiler-fixtures/usage-pure/class-extends-globalthis-promise/output.mjs
@@ -1,6 +1,7 @@
 import _Promise from "@core-js/pure/actual/promise/constructor";
+import _Promise$allSettled from "@core-js/pure/actual/promise/all-settled";
 class MyPromise extends _Promise {
   static allResolved(promises) {
-    return super.allSettled(promises);
+    return _Promise$allSettled.call(this, promises);
   }
 }
