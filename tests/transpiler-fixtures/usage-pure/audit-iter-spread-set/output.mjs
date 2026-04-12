3e199e3381bdc46c7293f0b41cf535b096966b11
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 18:46:56 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/audit-iter-spread-set/output.mjs
@@ -0,0 +1,2 @@
+import _Set from "@core-js/pure/actual/set/constructor";
+const a = [...new _Set()];
---
+++ tests/transpiler-fixtures/usage-pure/dom-collection-foreach-queryselectorall/output.mjs
@@ -0,0 +1,3 @@
+var _ref;
+import _forEachMaybeDomCollections from "@core-js/pure/actual/dom-collections/instance/for-each";
+_forEachMaybeDomCollections(_ref = document.querySelectorAll("div")).call(_ref, fn);
