27bce9c02ab576956bb637b12c4cd70570695802
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-12 11:04:42 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/combo-proxy-global-remaining-symbol-iterator/output-unplugin.mjs
@@ -0,0 +1,5 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _getIteratorMethod from "@core-js/pure/actual/get-iterator-method";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+const resolve = _Promise$resolve;
+const { then } = _Promise; const iter = _getIteratorMethod(obj);
