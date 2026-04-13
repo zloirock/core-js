2eedd1ee9d0cc849815fff6c6383d0aade8ce84c
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-13 21:51:24 +0300

    some fixtures

--- tests/transpiler-fixtures/usage-pure/audit-optional-chain-ts-non-null-between-polyfills/output.mjs
+++
@@ -1,4 +0,0 @@
-var _ref;
-import _includes from "@core-js/pure/actual/instance/includes";
-import _at from "@core-js/pure/actual/instance/at";
-arr == null ? void 0 : _includes(_ref = _at(arr).call(arr, -1)!).call(_ref, "x");
---
+++ tests/transpiler-fixtures/usage-pure/promise-try/output.mjs
@@ -0,0 +1,2 @@
+import _Promise$try from "@core-js/pure/actual/promise/try";
+_Promise$try(() => 42);
