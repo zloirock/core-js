30772b491f0a3d4637225b413a945064e238cf1d
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-14 11:28:32 +0300

    add isCallExpression check to annotateCallReturnType

---
+++ tests/transpiler-fixtures/usage-pure/audit-instance-on-optional-global-result-chain/output.mjs
@@ -0,0 +1,4 @@
+var _ref;
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+import _Array$from from "@core-js/pure/actual/array/from";
+_atMaybeArray(_ref = _Array$from(x)).call(_ref, 0);
