6b1ccb93b9d985cf11580f3eaefea370db976c12
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 02:12:38 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/jsx-template-child-multi-polyfill/output.mjs
@@ -0,0 +1,4 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _Array$from from "@core-js/pure/actual/array/from";
+import _trimStartMaybeString from "@core-js/pure/actual/string/instance/trim-start";
+const el = <div>{`prefix-${_at(arr).call(arr, 0)}-${_Array$from(items).length}-${_trimStartMaybeString(str).call(str)}`}</div>;
