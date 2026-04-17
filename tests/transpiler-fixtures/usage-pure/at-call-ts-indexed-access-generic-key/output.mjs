1ee0fc8bc7ad0025dca623512e3fcd53c7e65af0
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 00:07:31 +0300

    fix resolving TSIndexedAccessType substitute

---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-indexed-access-generic-key/output.mjs
@@ -0,0 +1,10 @@
+import _atMaybeString from "@core-js/pure/actual/string/instance/at";
+function f<T extends {
+  id: string;
+}>(obj: T): T['id'] {
+  return obj.id;
+}
+const r = f({
+  id: 'x'
+});
+_atMaybeString(r).call(r, 0);
