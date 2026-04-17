92ecaef57a9cb92c8da4d8f4c405727855b4936a
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 12:13:53 +0300

    prune unused refs in babel

--- tests/transpiler-fixtures/usage-pure/optional-triple-chain-instance/output.mjs
+++ tests/transpiler-fixtures/usage-pure/optional-triple-chain-instance/output.mjs
@@ -1,5 +1,5 @@
-var _ref, _ref2, _ref3;
+var _ref, _ref2;
 import _padStartMaybeString from "@core-js/pure/actual/string/instance/pad-start";
 import _trimMaybeString from "@core-js/pure/actual/string/instance/trim";
 import _at from "@core-js/pure/actual/instance/at";
-null == (_ref = null == (_ref3 = arr == null ? void 0 : _at(arr).call(arr, -1)) ? void 0 : _trimMaybeString(_ref3).call(_ref3)) ? void 0 : _padStartMaybeString(_ref).call(_ref, 2);
+null == (_ref = null == (_ref2 = arr == null ? void 0 : _at(arr).call(arr, -1)) ? void 0 : _trimMaybeString(_ref2).call(_ref2)) ? void 0 : _padStartMaybeString(_ref).call(_ref, 2);
--- tests/transpiler-fixtures/usage-pure/type-chain-array-from-filter-at/output-unplugin.mjs
+++
@@ -1,5 +0,0 @@
-var _ref, _ref2;
-import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
-import _filterMaybeArray from "@core-js/pure/actual/array/instance/filter";
-import _Array$from from "@core-js/pure/actual/array/from";
-_atMaybeArray(_ref = _filterMaybeArray(_ref2 = _Array$from(x)).call(_ref2, v => v > 0)).call(_ref, -1);
