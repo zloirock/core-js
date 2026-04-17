fc76f120ac769dac6fd8207734429c63efaf1574
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 19:48:02 +0300

    fix one more case of shadowing

---
+++ tests/transpiler-fixtures/usage-pure/at-call-member-for-of-target-scoped/output.mjs
@@ -0,0 +1,11 @@
+import _at from "@core-js/pure/actual/instance/at";
+import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
+import _includesMaybeArray from "@core-js/pure/actual/array/instance/includes";
+let arr = [];
+for (arr.at of items) {
+  var _ref;
+  use(arr.at);
+  _at(_ref = arr.at).call(_ref, 0);
+  _flatMaybeArray(other).call(other);
+}
+use(_includesMaybeArray(arr).call(arr, 1));
--- tests/transpiler-fixtures/usage-pure/at-call-member-for-of-target-skip/output.mjs
+++ tests/transpiler-fixtures/usage-pure/at-call-member-for-of-target-skip/output.mjs
@@ -1,5 +1,4 @@
-import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
 let arr = [];
 for (arr.at of items) {
-  use(_atMaybeArray(arr));
+  use(arr.at);
 }
