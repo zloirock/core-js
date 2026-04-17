fa8933c0b8d85ba97edb3d8ae1272e23bbab877f
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 00:22:03 +0300

    param decorators visitors

---
+++ tests/transpiler-fixtures/usage-pure/decorator-param-polyfill/output.mjs
@@ -0,0 +1,8 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _Object$fromEntries from "@core-js/pure/actual/object/from-entries";
+class Foo {
+  constructor(@inject(_Array$from('abc'))
+  foo: string) {}
+  method(@log(_Object$fromEntries([]))
+  arg: number) {}
+}
