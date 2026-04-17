28dcbc43618cb7740193d866b50d42882e585805
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 20:04:11 +0300

    fix one more destructuring case

--- tests/transpiler-fixtures/usage-pure/combo-for-init-default-se-rest/output.mjs
+++ tests/transpiler-fixtures/usage-pure/combo-for-init-default-se-rest/output.mjs
@@ -1,5 +1,6 @@
 import _Array$from from "@core-js/pure/actual/array/from";
-for (var _ref = (se(), Array), from = _Array$from === void 0 ? [] : _Array$from, {
-    includes,
+import _includes from "@core-js/pure/actual/instance/includes";
+for (var _ref = (se(), Array), from = _Array$from === void 0 ? [] : _Array$from, includes = _includes(obj), {
+    includes: _unused,
     ...rest
   } = obj;;) break;
