eb98c9dc490387f90fb334f34a67cf424e0c87c1
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 18:24:04 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-global/private-field-instance-method-polyfill/input.mjs
@@ -0,0 +1,9 @@
+class C {
+  #bag = { at: () => null, flat: () => null };
+  peek(i) {
+    return this.#bag.at(i);
+  }
+  spread() {
+    return this.#bag.flat();
+  }
+}
---
+++ tests/transpiler-fixtures/usage-pure/assignment-computed-symbol-iterator-skip/output.mjs
@@ -0,0 +1,4 @@
+import _Symbol$iterator from "@core-js/pure/actual/symbol/iterator";
+import _Symbol$asyncIterator from "@core-js/pure/actual/symbol/async-iterator";
+obj[_Symbol$iterator] = fn;
+obj[_Symbol$asyncIterator] = fn;
