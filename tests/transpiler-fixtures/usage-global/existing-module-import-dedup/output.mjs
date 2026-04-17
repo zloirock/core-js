717ec4b51ad401153dc685430e02a4440330903b
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-17 22:13:49 +0300

    avoid duplications by user imports

--- tests/transpiler-fixtures/usage-global/existing-module-import-dedup/output.mjs
+++ tests/transpiler-fixtures/usage-global/existing-module-import-dedup/output.mjs
@@ -1,4 +1,4 @@
 import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.from";
 import "core-js/modules/es.string.iterator";
-import 'core-js/modules/es.array.from';
 Array.from(x);
