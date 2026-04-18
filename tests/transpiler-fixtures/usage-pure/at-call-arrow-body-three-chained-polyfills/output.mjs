e45e320f452e23ac5febae59727d56a59db4e193
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 17:55:51 +0300

    fix babel-plugin internal gaps

--- tests/transpiler-fixtures/usage-pure/at-call-arrow-body-three-chained-polyfills/output.mjs
+++ tests/transpiler-fixtures/usage-pure/at-call-arrow-body-three-chained-polyfills/output.mjs
@@ -2,6 +2,6 @@ import _includes from "@core-js/pure/actual/instance/includes";
 import _at from "@core-js/pure/actual/instance/at";
 import _flatMaybeArray from "@core-js/pure/actual/array/instance/flat";
 const f = x => {
-  var _ref, _ref2, _ref3, _ref4, _ref5;
-  return _includes(_ref = _at(_ref5 = _flatMaybeArray(_ref3 = getArr(x)).call(_ref3)).call(_ref5, -1)).call(_ref, 1);
+  var _ref, _ref2, _ref3;
+  return _includes(_ref = _at(_ref2 = _flatMaybeArray(_ref3 = getArr(x)).call(_ref3)).call(_ref2, -1)).call(_ref, 1);
 };
