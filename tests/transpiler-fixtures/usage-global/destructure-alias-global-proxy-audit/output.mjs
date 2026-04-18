1a8b1c2d9620e3a9abd67d661361125118cc77ad
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 15:36:48 +0300

    extend type inference

---
+++ tests/transpiler-fixtures/usage-global/destructure-alias-global-proxy-audit/output.mjs
@@ -0,0 +1,12 @@
+import "core-js/modules/es.symbol.constructor";
+import "core-js/modules/es.symbol.description";
+import "core-js/modules/es.symbol.iterator";
+import "core-js/modules/es.object.to-string";
+import "core-js/modules/es.array.iterator";
+import "core-js/modules/es.global-this";
+import "core-js/modules/es.string.iterator";
+import "core-js/modules/web.dom-collections.iterator";
+const {
+  Symbol
+} = globalThis;
+const key = Symbol.iterator;
