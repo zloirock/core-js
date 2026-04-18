81cf4439f610a29ef1cf66b21eaa1dbe622704db
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 11:08:08 +0300

    extend type resolving

---
+++ tests/transpiler-fixtures/usage-pure/at-call-ts-discriminated-union-not-narrowed/output-unplugin.mjs
@@ -0,0 +1,8 @@
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+type Foo = { kind: "a"; data: string[] } | { kind: "b"; data: number };
+function f(x: Foo) {
+  if (x.kind === "a") {
+var _ref;
+    _atMaybeArray(_ref = x.data).call(_ref, 0);
+  }
+}
