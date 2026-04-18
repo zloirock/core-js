37de19dab26fe2c15916c3670c3e0dc4024e8989
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-18 16:00:32 +0300

    `Map ||= 1` warning, `const { from } = cond && Array` skip

--- tests/transpiler-fixtures/usage-pure/destructuring-logical-call-left/output.mjs
+++ tests/transpiler-fixtures/usage-pure/destructuring-logical-call-left/output.mjs
@@ -1,4 +1,4 @@
-import _Array$from from "@core-js/pure/actual/array/from";
 import _Promise from "@core-js/pure/actual/promise/constructor";
-_Promise() && Array;
-const from = _Array$from;
+const {
+  from
+} = _Promise() && Array;
