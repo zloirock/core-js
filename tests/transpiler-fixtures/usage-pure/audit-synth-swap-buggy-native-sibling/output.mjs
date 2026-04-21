a5f301d62be6fc8355119c072b6e6b2312dec9bc
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-22 01:58:12 +0300

    fix one more synth-swap case

---
+++ tests/transpiler-fixtures/usage-pure/audit-synth-swap-buggy-native-sibling/output.mjs
@@ -0,0 +1,16 @@
+import _Promise$resolve from "@core-js/pure/actual/promise/resolve";
+import _Promise from "@core-js/pure/actual/promise/constructor";
+import _globalThis from "@core-js/pure/actual/global-this";
+function f({
+  resolve,
+  custom
+} = {
+  resolve: _Promise$resolve,
+  custom: _Promise.custom
+}) {
+  return [resolve(1), custom];
+}
+_globalThis.__call = () => f({
+  resolve: x => x,
+  custom: 'ok'
+});
