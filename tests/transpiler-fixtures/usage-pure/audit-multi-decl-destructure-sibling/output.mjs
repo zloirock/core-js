80799ae956d39dc220a5c277943422ac736071b2
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-22 00:36:55 +0300

    fix destructuring with multi declaration

---
+++ tests/transpiler-fixtures/usage-pure/audit-multi-decl-destructure-sibling/output.mjs
@@ -0,0 +1,9 @@
+import _Array$from from "@core-js/pure/actual/array/from";
+import _globalThis from "@core-js/pure/actual/global-this";
+const from = _Array$from,
+  {
+    noSuch
+  } = Array,
+  y = 1;
+from(noSuch);
+_globalThis.__y = y;
