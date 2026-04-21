91a09e30f29d4c3275a16deec99f72c50aacad1a
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-21 23:28:27 +0300

    update dependencies

--- packages/core-js/proposals/iterator-join.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-// proposal stage: 3
-// https://github.com/tc39/proposal-iterator-join
-require('../modules/es.object.to-string');
-require('../modules/es.iterator.constructor');
-require('../modules/esnext.iterator.join');
