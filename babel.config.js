fe21e803f2507ae5f526b8a8a45ef6b116ef0845
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-23 01:35:13 +0300

    disable `lose` for class transforms

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
-  // use transforms which does not use ES6+ builtins
   plugins: [
     ['@babel/transform-arrow-functions'],
     ['@babel/transform-block-scoped-functions'],
     ['@babel/transform-block-scoping'],
-    // it seems `setClassMethods` unlike `loose` does not work
-    ['@babel/transform-classes', { loose: true }],
     ['@babel/transform-class-properties'],
     ['@babel/transform-class-static-block'],
+    ['@babel/transform-private-methods'],
+    ['@babel/transform-private-property-in-object'],
+    ['@babel/transform-classes'],
     ['@babel/transform-computed-properties'],
     ['@babel/transform-destructuring'],
     ['@babel/transform-duplicate-named-capturing-groups-regex'],
@@ -25,8 +25,6 @@ module.exports = {
     ['@babel/transform-optional-catch-binding'],
     ['@babel/transform-optional-chaining'],
     ['@babel/transform-parameters'],
-    ['@babel/transform-private-methods'],
-    ['@babel/transform-private-property-in-object'],
     ['@babel/transform-regenerator', { generators: true, async: true, asyncGenerators: true }],
     ['@babel/transform-regexp-modifiers'],
     ['@babel/transform-shorthand-properties'],
