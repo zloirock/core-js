bf0b444f07153ffa7a4deb0072b9e09bd9f33961
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-25 14:24:33 +0300

    improve handling enums / namespaces

--- packages/core-js-polyfill-provider/helpers.js
+++ packages/core-js-polyfill-provider/helpers.js
@@ -8,7 +8,7 @@ export {
   findIifeArgForParam,
   findIifeCallSite,
   getSuperTypeArgs,
-  getTSImportEqualsBindings,
+  getTSRuntimeBindings,
   getTypeArgs,
   hasTopLevelESM,
   isDeleteTarget,
