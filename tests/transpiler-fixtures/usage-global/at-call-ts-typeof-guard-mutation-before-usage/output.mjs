02ee055ebd190bb83cd192bccb76b43aefc18b11
Denis Pushkarev <zloirock@zloirock.ru>
2026-04-28 02:35:58 +0300

    improve type resolving

--- tests/transpiler-fixtures/usage-global/at-call-ts-typeof-guard-mutation-before-usage/output.mjs
+++ tests/transpiler-fixtures/usage-global/at-call-ts-typeof-guard-mutation-before-usage/output.mjs
@@ -1,6 +1,4 @@
 import "core-js/modules/es.array.at";
-import "core-js/modules/es.array.includes";
-import "core-js/modules/es.string.at";
 import "core-js/modules/es.string.includes";
 function test(x: string | number[]) {
   if (typeof x === 'string') {
