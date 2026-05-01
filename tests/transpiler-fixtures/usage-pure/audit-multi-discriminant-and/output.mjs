0b034eb80da832c89c019f150a8e1f589f41af48
Denis Pushkarev <zloirock@zloirock.ru>
2026-05-01 18:22:22 +0300

    add some fixtures

---
+++ tests/transpiler-fixtures/usage-pure/audit-multi-discriminant-and/output.mjs
@@ -0,0 +1,20 @@
+import _atMaybeArray from "@core-js/pure/actual/array/instance/at";
+type Box = {
+  kind: 'a';
+  sub: 'A1';
+  data: string[];
+} | {
+  kind: 'a';
+  sub: 'A2';
+  data: number[];
+} | {
+  kind: 'b';
+  data: number;
+};
+function readFirst(box: Box) {
+  if (box.kind === 'a' && box.sub === 'A1') {
+    var _ref;
+    return _atMaybeArray(_ref = box.data).call(_ref, 0);
+  }
+  return null;
+}
