d9cc10c7dfe0f095d13eb128ed5c97b2e747535d
Denis Pushkarev <zloirock@zloirock.ru>
2026-05-28 15:41:44 +0700

    sync type param names with ts
    https://github.com/microsoft/TypeScript/pull/63504

--- packages/core-js-types/src/base/proposals/string-padding.d.ts
+++ packages/core-js-types/src/base/proposals/string-padding.d.ts
@@ -9,25 +9,25 @@ interface String { // @type-options: no-redefine
    * Pads the current string with a given string (possibly repeated) so that the resulting string reaches a given length.
    * The padding is applied from the start (left) of the current string.
    *
-   * @param maxLength - The length of the resulting string once the current string has been padded.
+   * @param targetLength - The length of the resulting string once the current string has been padded.
    *        If this parameter is smaller than the current string's length, the current string will be returned as it is.
    *
-   * @param fillString - The string to pad the current string with.
+   * @param padString - The string to pad the current string with.
    *        If this string is too long, it will be truncated and the left-most part will be applied.
    *        The default value for this parameter is " " (U+0020).
    */
-  padStart(maxLength: number, fillString?: string): string;
+  padStart(targetLength: number, padString?: string): string;
 
   /**
    * Pads the current string with a given string (possibly repeated) so that the resulting string reaches a given length.
    * The padding is applied from the end (right) of the current string.
    *
-   * @param maxLength - The length of the resulting string once the current string has been padded.
+   * @param targetLength - The length of the resulting string once the current string has been padded.
    *        If this parameter is smaller than the current string's length, the current string will be returned as it is.
    *
-   * @param fillString - The string to pad the current string with.
+   * @param padString - The string to pad the current string with.
    *        If this string is too long, it will be truncated and the left-most part will be applied.
    *        The default value for this parameter is " " (U+0020).
    */
-  padEnd(maxLength: number, fillString?: string): string;
+  padEnd(targetLength: number, padString?: string): string;
 }
