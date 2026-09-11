a04a46b2a141e45ea6815a5ff07c9cf9f5ce96a0
Denis Pushkarev <zloirock@zloirock.ru>
2026-09-11 17:19:27 +0300

    store latest quest compat data versions as strings for compatibility with babel types

--- scripts/build-compat/data.mjs
+++ scripts/build-compat/data.mjs
@@ -59,7 +59,7 @@ for (const scope of [data, external]) {
     }
     map('ChromeAndroidToQuest');
     if (!has(module, 'quest') && chrome >= 144) {
-      module.quest = Math.ceil(chrome / 2) * 2;
+      module.quest = String(Math.ceil(chrome / 2) * 2);
     }
     // TODO: Remove from `core-js@4`
     if (has(module, 'quest')) {
